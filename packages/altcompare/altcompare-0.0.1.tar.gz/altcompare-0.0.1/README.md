bbcbdc237f5ca2d52d401b45b3017a77088582d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:24 +0000

    Add package altcompare/0.0.1/altcompare-0.0.1.tar.gz

---
+++ packages/altcompare/altcompare-0.0.1.tar.gz/altcompare-0.0.1/README.md
@@ -0,0 +1,3 @@
+# altcompare
+
+This is a Data Compare utility built using python, pandas and pyodbc. The compare utility is source agnostic and you dont need to specify the data structure.
---
+++ packages/altcompare/altcompare-0.0.1.tar.gz/altcompare-0.0.1/setup.py
@@ -0,0 +1,28 @@
+
+
+from setuptools import setup, find_packages
+
+setup(
+    name='altcompare',
+    version='0.0.1',
+    description='Data Compare Utility in Python',
+    url='https://github.com/pvparuchuri/altcompare',
+    author='Venkata Paruchuri',
+    classifiers=[  
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+    ],
+    python_requires='>=3',
+    keywords='Data Comparison odbc compare',
+    packages=find_packages(exclude=['contrib', 'docs', 'tests']),  
+    project_urls={
+        'Bug Reports': 'https://github.com/pvparuchuri/altcompare/issues',
+        'Source': 'https://github.com/pvparuchuri/altcompare/',
+    },
+)
