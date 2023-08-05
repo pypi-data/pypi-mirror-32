1ee8ac227a17b2c5ea3ebbe8b5e46d492cc6c67f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:26:15 +0000

    Add package hejing/0.1/hejing-0.1.tar.gz

---
+++ packages/hejing/hejing-0.1.tar.gz/hejing-0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+setup(name='hejing',
+      version='0.1',
+      description='clinical trial information retriver',
+      url='https://github.com/kobyant/clinicaltrial',
+      author='jing',
+      author_email='jinghedeveloper@163.com',
+      license='MIT',
+      packages=['hejing'],
+      zip_safe=False)
