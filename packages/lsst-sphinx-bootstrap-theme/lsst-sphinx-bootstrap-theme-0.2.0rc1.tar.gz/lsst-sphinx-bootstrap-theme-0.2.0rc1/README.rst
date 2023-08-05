66b3b2ec7264607c51879b22fb7b33fe641c1ed6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:55 +0000

    Add package lsst-sphinx-bootstrap-theme/0.2.0rc1/lsst-sphinx-bootstrap-theme-0.2.0rc1.tar.gz

---
+++ packages/lsst-sphinx-bootstrap-theme/lsst-sphinx-bootstrap-theme-0.2.0rc1.tar.gz/lsst-sphinx-bootstrap-theme-0.2.0rc1/README.rst
@@ -0,0 +1,5 @@
+###########################
+lsst-sphinx-bootstrap-theme
+###########################
+
+This is a prototype Sphinx theme for LSST Stack documentation, based on the Astropy Sphinx theme.
---
+++ packages/lsst-sphinx-bootstrap-theme/lsst-sphinx-bootstrap-theme-0.2.0rc1.tar.gz/lsst-sphinx-bootstrap-theme-0.2.0rc1/setup.py
@@ -0,0 +1,40 @@
+from setuptools import setup
+
+
+setup(
+    name='lsst-sphinx-bootstrap-theme',
+    version='0.2.0rc1',
+    # use_scm_version=True,
+    # setup_requires=['setuptools_scm'],
+    description=(
+        "Sphinx theme for LSST user documentation built on Bootstrap and "
+        "Astropy's theme."
+    ),
+    author='Association of Universities for Research in Astronomy',
+    author_email='jsick@lsst.org',
+    license='BSD',
+    url='https://github.com/lsst-sqre/lsst-sphinx-bootstrap-theme',
+    long_description=open('README.rst').read(),
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Framework :: Sphinx :: Theme',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+    ],
+    zip_safe=False,
+    packages=['lsst_sphinx_bootstrap_theme'],
+    package_data={'lsst_sphinx_bootstrap_theme': [
+        'theme.conf',
+        '*.html',
+        'static/*.css',
+        'static/*.js',
+        'static/*.ico',
+        'static/*.svg',
+        'static/*.png',
+        'templates/autosummary/*.rst'
+    ]},
+    include_package_data=True,
+)
