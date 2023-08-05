96fa3413e42c0c7e44cde2e297a7d3ab42b77161
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:18 +0000

    Add package auto-everything/1.3/auto_everything-1.3.tar.gz

---
+++ packages/auto-everything/auto_everything-1.3.tar.gz/auto_everything-1.3/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+from os.path import dirname, join, abspath
+
+setup(name='auto_everything',
+        version='1.3',
+        description='do automate things on Linux',
+        #long_description=open(join(abspath(dirname(__file__)), "README.md")).read(),
+        classifiers=[
+            'Operating System :: POSIX :: Linux',
+            'Programming Language :: Python :: 3',
+            'Topic :: System',
+            'License :: OSI Approved :: GNU General Public License v3 (GPLv3)'
+            ],
+        keywords='Linux system automation',
+        url='http://github.com/yingshaoxo/auto_everything',
+        author='yingshaoxo',
+        license='GPLv3',
+        packages=find_packages(),
+        include_package_data=False,
+        )
