81356b791d5eeb189251b99dd8246331b8cb532b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:26 +0000

    Add package refnx/0.0.15/refnx-0.0.15.tar.gz

---
+++ packages/refnx/refnx-0.0.15.tar.gz/refnx-0.0.15/refnx/version.py
@@ -0,0 +1,9 @@
+
+# THIS FILE IS GENERATED FROM REFNX SETUP.PY
+short_version = '0.0.15'
+version = '0.0.15'
+full_version = '0.0.15'
+git_revision = '556f2187af27b57277037880147790c32dc4eb76'
+release = True
+if not release:
+    version = full_version
