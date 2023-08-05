b62d1239cc3c864434edf7c1df5a3cbc8b3ddc93
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:12:55 +0000

    Add package configurationutil/3.2.2/configurationutil-3.2.2-py2.py3-none-any.whl

---
+++ packages/configurationutil/configurationutil-3.2.2-py2.py3-none-any.whl/configurationutil/__init__.py
@@ -0,0 +1,29 @@
+# encoding: utf-8
+
+# Get module version
+from ._metadata import __version__
+
+# Import key items from module
+import configurationutil.configuration as cfg_params
+
+from configurationutil.configuration import (
+    Configuration
+)
+
+from configurationutil.cfg_providers import (
+    JSONConfig,
+    YAMLConfig
+)
+
+from configurationutil.cfg_objects import (
+    CfgItem,
+    CfgItems,
+    InheritableCfgItem,
+    ContainerCfgItem,
+    ContainerItemDefaults,
+    DefaultInheritableConstant
+)
+
+# Set default logging handler to avoid "No handler found" warnings.
+from logging import NullHandler, getLogger
+getLogger(__name__).addHandler(NullHandler())
