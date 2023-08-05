9cf57225854c2c79e025ee4523ca487b5d94a40c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:28 +0000

    Add package labelprinterkit/0.0.2/labelprinterkit-0.0.2.tar.gz

---
+++ packages/labelprinterkit/labelprinterkit-0.0.2.tar.gz/labelprinterkit-0.0.2/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+with open('README.md', 'r') as readme:
+    README_TEXT = readme.read()
+
+setup(
+    name="labelprinterkit",
+    description="A library for creating and printing labels",
+    use_scm_version=True,
+    long_description=README_TEXT,
+    long_description_content_type="text/markdown",
+    url="https://git.scc.kit.edu/scc-net/labelprinterkit",
+    author="Adrian Tschira",
+    author_email="packages@notafile.com",
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+    keywords='',
+    packages=find_packages(where='labelprinterkit/*'),
+    install_requires=[
+        'pillow',
+        'pyusb',
+        'packbits',
+    ],
+    setup_requires=[
+        'setuptools_scm'
+    ],
+    python_requires='>=3.4'
+)
