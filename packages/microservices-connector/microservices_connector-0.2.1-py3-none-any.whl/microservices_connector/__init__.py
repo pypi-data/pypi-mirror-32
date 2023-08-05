175cb997b75a115d1e1963a250f722fb532380c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:08 +0000

    Add package microservices-connector/0.2.1/microservices_connector-0.2.1-py3-none-any.whl

---
+++ packages/microservices-connector/microservices_connector-0.2.1-py3-none-any.whl/microservices_connector/__init__.py
@@ -0,0 +1,10 @@
+
+
+__author__ = 'Minh Tuan Nguyen'
+__version__ = '0.1.0'
+__copyright__ = "Copyright 2018, Minh Tuan Nguyen"
+__credits__ = ["Minh Tuan Nguyen", ]
+
+# python setup.py bdist_wheel
+# python setup.py sdist bdist_wheel
+# twine upload dist/*
