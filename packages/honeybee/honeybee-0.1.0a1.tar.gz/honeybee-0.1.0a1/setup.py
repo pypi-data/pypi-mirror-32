9c073bd9b0fad2626a56cee03fbf5b67e52922ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:11:33 +0000

    Add package honeybee/0.1.0a1/honeybee-0.1.0a1.tar.gz

---
+++ packages/honeybee/honeybee-0.1.0a1.tar.gz/honeybee-0.1.0a1/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+from distutils.util import convert_path
+from os import path
+
+
+here = path.abspath(path.dirname(__file__))
+version_dict = {}
+version_path = convert_path('honeybee/_version.py')
+
+# Get the version number from the version path.
+with open(version_path, 'r') as ver_file:
+    exec(ver_file.read(), version_dict)
+
+# Get the long description from the README file.
+#with open(path.join(here, 'README.md')) as a_file:
+#    long_description = a_file.read()
+
+setup(name='honeybee',
+      packages=['honeybee'],
+      version=version_dict['__version__'],
+      description='An artificial bee colony implementation in Python',
+      #long_description=long_description,
+      #long_description_content_type='text/markdown',
+      author='Engineero',
+      author_email='engineerolabs@gmail.com',
+      url='https://github.com/Engineero/honeybee',
+      keywords=['abc artificial bee colony optimization'],
+      classifiers=[])
