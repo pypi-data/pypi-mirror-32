4de946c6525b8043f4350df1063216a30bfeb831
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:53 +0000

    Add package chromedriver-install/0.1/chromedriver_install-0.1.tar.gz

---
+++ packages/chromedriver-install/chromedriver_install-0.1.tar.gz/chromedriver_install-0.1/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+
+def readme(file='', split=False):
+	with open(file) as f:
+		if split:
+			return f.readlines()
+		else:
+			return f.read()
+
+
+setup(
+	name='chromedriver_install',
+	version='0.1',
+	description='Package for installing the latest chromedriver automatically.',
+	long_description=readme('README.md'),
+	url='http://github.com/shadowmoose/chrome_driver',
+	author='ShadowMoose',
+	author_email='shadowmoose@github.com',
+	license='MIT',
+	packages=['chromedriver_install'],
+	install_requires=readme('requirements.txt', split=True),
+	entry_points={
+		'console_scripts': ['install-chromedriver=chromedriver_install:install'],
+	},
+	zip_safe=False)
