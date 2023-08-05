50060bf8046f1cb629ba3827baac8664fb523220
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:12:59 +0000

    Add package ks3sdk/1.0.10/ks3sdk-1.0.10.zip

---
+++ packages/ks3sdk/ks3sdk-1.0.10.zip/ks3sdk-1.0.10/setup.py
@@ -0,0 +1,34 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+with open('README.md', 'rb') as f:
+    readme = f.read().decode('utf-8')
+
+setup(
+    name='ks3sdk',
+    version='1.0.10',
+    description='Kingsoft Standard Storage Service SDK',
+    long_description=readme,
+    packages=['ks3'],
+    install_requires=[
+        'six',
+        'pycrypto==2.6.1'
+    ],
+    include_package_data=True,
+    url='https://github.com/ks3sdk/ks3-python-sdk',
+    author='ksc_ks3',
+    author_email='ksc_ks3@kingsoft.com',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7'
+    ]
+)
