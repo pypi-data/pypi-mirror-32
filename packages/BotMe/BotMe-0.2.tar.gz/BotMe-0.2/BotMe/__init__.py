bbb4a0712936b0e1f892da87bd4745a5fd40c26a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:33:24 +0000

    Add package BotMe/0.2/BotMe-0.2.tar.gz

---
+++ packages/BotMe/BotMe-0.2.tar.gz/BotMe-0.2/BotMe/__init__.py
@@ -0,0 +1,1 @@
+from BotMe import *
---
+++ packages/BotMe/BotMe-0.2.tar.gz/BotMe-0.2/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'BotMe',
+  packages = ['BotMe'],
+  version = '0.2',
+  description = 'A basic library for building and maintaining GroupMe Bots',
+  author = 'James Marshall Reber',
+  author_email = 'jamesmarshallreber@gmail.com',
+  url = 'https://github.com/marshareb/BotMe',
+  download_url = 'https://github.com/marshareb/BotMe/archive/0.2.tar.gz',
+  keywords = ['GroupMe', 'bot'],
+  classifiers = [],
+)
