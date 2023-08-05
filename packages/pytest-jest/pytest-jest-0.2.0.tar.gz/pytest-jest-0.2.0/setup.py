0596330f46aa1975ba762caa60dc3b99930c2d57
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:42 +0000

    Add package pytest-jest/0.2.0/pytest-jest-0.2.0.tar.gz

---
+++ packages/pytest-jest/pytest-jest-0.2.0.tar.gz/pytest-jest-0.2.0/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+setup(
+    name='pytest-jest',
+    description='A custom jest-pytest oriented Pytest reporter',
+    packages=['pytest_jest'],
+    author='numirias, jondot',
+    author_email='jondotan@gmail.com',
+    version='0.2.0',
+    url='https://github.com/jondot/pytest-jest',
+    license='MIT',
+    install_requires=[
+        'pytest>=3.3.2',
+        'pytest-metadata',
+    ],
+    entry_points={'pytest11': [
+        'pytest_jest = pytest_jest.plugin',
+    ]},
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Framework :: Pytest',
+    ],
+)
