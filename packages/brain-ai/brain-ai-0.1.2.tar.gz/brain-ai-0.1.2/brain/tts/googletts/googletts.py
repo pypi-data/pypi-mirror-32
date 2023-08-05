702a875299ef7121e992e099aa2a2637e29e1955
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:43 +0000

    Add package brain-ai/0.1.2/brain-ai-0.1.2.tar.gz

---
+++ packages/brain-ai/brain-ai-0.1.2.tar.gz/brain-ai-0.1.2/brain/tts/googletts/googletts.py
@@ -0,0 +1,77 @@
+import requests
+from brain.core import FileManager
+from brain.core.TTS.TTSModule import TTSModule, FailToLoadSoundFile, MissingTTSParameter
+import logging
+
+logging.basicConfig()
+logger = logging.getLogger("brain")
+
+TTS_URL = "http://translate.google.com/translate_tts"
+TTS_CONTENT_TYPE = "audio/mpeg"
+TTS_TIMEOUT_SEC = 30
+
+
+class Googletts(TTSModule):
+    def __init__(self, **kwargs):
+        super(Googletts, self).__init__(**kwargs)
+
+        self._check_parameters()
+
+    def say(self, words):
+        """
+        :param words: The sentence to say
+        """
+
+        self.generate_and_play(words, self._generate_audio_file)
+
+    def _check_parameters(self):
+        """
+        Check parameters are ok, raise MissingTTSParameterException exception otherwise.
+        :return: true if parameters are ok, raise an exception otherwise
+
+               .. raises:: MissingTTSParameterException
+        """
+        if self.language == "default" or self.language is None:
+            raise MissingTTSParameter("[GoogleTTS] Missing parameters, check documentation !")
+        return True
+
+    def _generate_audio_file(self):
+        """
+        Generic method used as a Callback in TTSModule
+            - must provided the audio file and write it on the disk
+
+        .. raises:: FailToLoadSoundFile
+        """
+
+        # Prepare payload
+        payload = self.get_payload()
+
+        # getting the audio
+        r = requests.get(TTS_URL, params=payload, stream=True, timeout=TTS_TIMEOUT_SEC)
+        content_type = r.headers['Content-Type']
+
+        logger.debug("Googletts : Trying to get url: %s response code: %s and content-type: %s",
+                     r.url,
+                     r.status_code,
+                     content_type)
+        # Verify the response status code and the response content type
+        if r.status_code != requests.codes.ok or content_type != TTS_CONTENT_TYPE:
+            raise FailToLoadSoundFile("Googletts : Fail while trying to remotely access the audio file")
+
+        # OK we get the audio we can write the sound file
+        FileManager.write_in_file(self.file_path, r.content)
+
+    def get_payload(self):
+        """
+        Generic method used load the payload used to access the remote api
+
+        :return: Payload to use to access the remote api
+        """
+
+        return {
+            "q": self.words,
+            "tl": self.language,
+            "ie": "UTF-8",
+            "total": "1",
+            "client": "tw-ob"
+        }
