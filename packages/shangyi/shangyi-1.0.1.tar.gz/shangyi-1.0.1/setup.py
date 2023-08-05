49c5ab04c61f395a5b5385eb8b8cf37ec25aa239
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:10:50 +0000

    Add package shangyi/1.0.1/shangyi-1.0.1.tar.gz

---
+++ packages/shangyi/shangyi-1.0.1.tar.gz/shangyi-1.0.1/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name='shangyi',
+    version='1.0.1',
+    py_modules=['nester'],
+    author='shangyipythom',
+    author_email='shangyix@163.com',
+    url='https://blog.csdn.net/tw18761720160/article/details/52826450',
+    description='A new pythoner',
+    )
