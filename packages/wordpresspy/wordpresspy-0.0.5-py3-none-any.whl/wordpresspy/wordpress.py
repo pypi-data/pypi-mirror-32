f2ff2439593b5650103fc2a8951049e662f1d1d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:09:52 +0000

    Add package wordpresspy/0.0.5/wordpresspy-0.0.5-py3-none-any.whl

---
+++ packages/wordpresspy/wordpresspy-0.0.5-py3-none-any.whl/wordpresspy/wordpress.py
@@ -0,0 +1,41 @@
+import json
+
+from .api import API
+from .utils import str_to_slug
+
+POST_STATUS_PUBLISH = 'publish'
+POST_STATUS_FUTURE = 'future'
+POST_STATUS_DRAFT = 'draft'
+POST_STATUS_PENDING = 'pending'
+POST_STATUS_PRIVATE = 'private'
+
+
+class WordPressAPI:
+    api = None
+
+    def __init__(self, **kwargs):
+        kwargs['url_prefix'] = '/wp-json/wp/v2'
+        self.api = API(**kwargs)
+
+    def create_post(self, **kwargs):
+        return json.loads(self.api.post('/posts', json.dumps({
+            'author': kwargs.get('author', 0),
+            'content': kwargs.get('content', ''),
+            'excerpt': kwargs.get('excerpt', ''),
+            'slug': kwargs.get('slug', str_to_slug(kwargs.get('title'))),
+            'status': kwargs.get('status', POST_STATUS_PUBLISH),
+            'title': kwargs.get('title')
+        })))
+
+    def create_media(self, binary, filename, **kwargs):
+        res = json.loads(self.api.upload('/media', binary, filename))
+        self.update_media(res['id'], **kwargs)
+        return res
+
+    def update_media(self, id, **kwargs):
+        return json.loads(self.api.post('/media/' + str(id), json.dumps({
+            'alt_text': kwargs.get('alt_text', ''),
+            'caption': kwargs.get('caption', ''),
+            'description': kwargs.get('description', ''),
+            'title': kwargs.get('title', '')
+        })))
