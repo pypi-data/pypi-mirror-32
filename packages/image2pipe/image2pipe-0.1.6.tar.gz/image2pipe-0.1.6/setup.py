22248936de44ebbc1354b39cf90ac0e59845e626
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:54 +0000

    Add package image2pipe/0.1.6/image2pipe-0.1.6.tar.gz

---
+++ packages/image2pipe/image2pipe-0.1.6.tar.gz/image2pipe-0.1.6/setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+
+setup(
+    name='image2pipe',
+    version='0.1.6',
+    author='Anton P. Linevich',
+    author_email='anton@linevich.com',
+    keywords="ffmpeg yuv image2pipe",
+    packages=['image2pipe', ],
+    scripts=[],
+    url='https://github.com/chexov/image2pipe',
+    license='LICENSE.txt',
+    description='Simple ffmpeg wrapper for image2pipe',
+    long_description=open('README').read(),
+    install_requires=['tblib', 'numpy', 'websocket==0.2.1'],
+    python_requires='>=2.6',
+)
