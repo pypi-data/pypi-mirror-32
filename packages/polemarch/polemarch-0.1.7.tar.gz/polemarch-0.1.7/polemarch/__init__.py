93a44f172f25523e65002584669d8618a17e90c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:15:40 +0000

    Add package polemarch/0.1.7/polemarch-0.1.7.tar.gz

---
+++ packages/polemarch/polemarch-0.1.7.tar.gz/polemarch-0.1.7/polemarch/__init__.py
@@ -0,0 +1,18 @@
+from .environment import prepare_environment
+
+__version__ = "0.1.7"
+
+def _main(**kwargs):
+    # pylint: disable=unused-variable
+    import sys
+    from django.core.management import execute_from_command_line
+    prepare_environment(**kwargs)
+    execute_from_command_line(sys.argv)
+
+def get_app(**kwargs):
+    from celery import Celery
+    prepare_environment(**kwargs)
+    celery_app = Celery('polemarch')
+    celery_app.config_from_object('django.conf:settings', namespace='CELERY')
+    celery_app.autodiscover_tasks()
+    return celery_app
