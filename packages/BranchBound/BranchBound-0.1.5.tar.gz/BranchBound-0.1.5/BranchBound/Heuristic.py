322bc36858526328d2fbc5ed58bfaa7e8a42aac3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:15:47 +0000

    Add package BranchBound/0.1.5/BranchBound-0.1.5.tar.gz

---
+++ packages/BranchBound/BranchBound-0.1.5.tar.gz/BranchBound-0.1.5/BranchBound/Heuristic.py
@@ -0,0 +1,15 @@
+from solve import solve_main
+
+def compute_initial_heuristic(tree_seq, init_alg, edge_weight_func, cpu_affinity=1, 
+                                develop_tree=False, init_branching_factor=1,
+                                dynamic_heuristic=False, recursive_decisions=False):
+    # develop_tree represents the assemble variable
+    solve(''.join(tree_seq), assemble=develop_tree)
+    
+    return 0
+    
+def compute_recursive_heuristic(init_heuristic_object):
+    return 1
+
+def update_dynamic_bound(heuristic_object, bound):
+    return 0
---
+++ packages/BranchBound/BranchBound-0.1.5.tar.gz/BranchBound-0.1.5/setup.py
@@ -0,0 +1,38 @@
+from distutils.core import setup
+
+setup(
+    name = 'BranchBound',
+    packages = ['BranchBound'],
+    version = '0.1.5',  # Ideally should be same as your GitHub release tag varsion
+    description = 'Utility for reducing practical runtime in NP problems',
+    author = 'John Skeet',
+    author_email = 'jskeet314@gmail.com',
+    url = 'https://github.com/jskeet314/branch_bound_helper',
+    download_url = 'https://github.com/jskeet314/branch_bound_helper/archive/0.1.4.tar.gz',
+    keywords = ['branch', 'bound'],
+    classifiers = [],
+)
+
+# from setuptools import setup, find_packages  # Always prefer setuptools over distutils
+# from codecs import open  # To use a consistent encoding
+# from os import path
+# 
+# with open("README.md", "r") as fh:
+#     long_description = fh.read()
+# 
+# setup(
+#     name="branch_bound",
+#     version="0.0.1",
+#     author="John Skeet",
+#     author_email="jskeet314@gmail.com",
+#     description="A small example package",
+#     long_description=long_description,
+#     long_description_content_type="text/markdown",
+#     url="https://github.com/jskeet314/branch_bound_helper",
+#     packages=find_packages(),
+#     classifiers=(
+#         "Programming Language :: Python :: 3",
+#         "License :: OSI Approved :: MIT License",
+#         "Operating System :: OS Independent",
+#     ),
+# )
