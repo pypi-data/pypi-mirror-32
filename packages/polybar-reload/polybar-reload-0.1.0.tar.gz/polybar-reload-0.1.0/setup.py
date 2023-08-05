0d43a77cfb6d0a9c4e4dd25d9a0ada7d6c736367
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:52 +0000

    Add package polybar-reload/0.1.0/polybar-reload-0.1.0.tar.gz

---
+++ packages/polybar-reload/polybar-reload-0.1.0.tar.gz/polybar-reload-0.1.0/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+setup(
+    name = 'polybar-reload',
+    packages = ['polybar-reload'],
+    version = '0.1.0',
+    description = 'reloads polybar when screen resolution changes',
+    author = 'Patrick Motard',
+    author_email = 'motard19@gmail.com',
+    url = 'https://github.com/patrick-motard/polybar-reload',
+    download_url = 'https://github.com/patrick-motard/polybar-reload/archive/0.1.0.tar.gz',
+    keywords = ['polybar', 'reload', 'resize', 'resolution'],
+    classifiers = []
+)
