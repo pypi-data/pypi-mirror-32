0d59087f4f36e81ddcf3dfc915de91c42e3fff11
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:29 +0000

    Add package pachong/0.0.65/pachong-0.0.65-py2-none-any.whl

---
+++ packages/pachong/pachong-0.0.65-py2-none-any.whl/pachong/fetcher/base.py
@@ -0,0 +1,26 @@
+#-*-coding:utf-8-*-
+__author__ = 'cchen'
+
+
+class Fetcher(object):
+    session = None
+    proxy = None
+    network = False
+
+    def get(self, url, *kwargs):
+        raise NotImplementedError
+
+    def get_json(self, url, **kwargs):
+        raise NotImplementedError
+
+    def find(self, *args):
+        raise NotImplementedError
+
+    def find_all(self, *args):
+        raise NotImplementedError
+
+    def source_code(self):
+        raise NotImplementedError
+
+    def build(self, source_page, *args, **kwargs):
+        raise NotImplementedError
