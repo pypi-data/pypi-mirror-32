808c5e97976f1e98083d9f1b6ea413328fe8ede8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:21 +0000

    Add package bbindex/0.1.0/bbindex-0.1.0-py2-none-any.whl

---
+++ packages/bbindex/bbindex-0.1.0-py2-none-any.whl/bbindex/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+from __future__ import print_function
+from __future__ import absolute_import
+from __future__ import unicode_literals
+
+__version__ = '0.1.0'
+__author__ = 'tushare.org'
