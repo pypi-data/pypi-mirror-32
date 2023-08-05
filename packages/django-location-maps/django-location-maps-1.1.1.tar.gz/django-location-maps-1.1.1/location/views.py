73cfa1128bd59f56da67123a3aec74d08b16d286
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:10:08 +0000

    Add package django-location-maps/1.1.1/django-location-maps-1.1.1.tar.gz

---
+++ packages/django-location-maps/django-location-maps-1.1.1.tar.gz/django-location-maps-1.1.1/location/views.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import viewsets, filters
+
+from .api.filters import CityFilterSet, CountryFilterSet, StateFilterSet
+from .models import City, State, Country, Location
+from .serializers import CitySerializer, StateSerializer, CountrySerializer, LocationSerializer
+
+
+class CityViewSet(viewsets.ModelViewSet):
+    queryset = City.objects.all()
+    serializer_class = CitySerializer
+    filter_class = CityFilterSet
+    filter_backends = (DjangoFilterBackend, filters.OrderingFilter,)
+    ordering_fields = '__all__'
+    http_method_names = ['get', ]
+
+
+class StateViewSet(viewsets.ModelViewSet):
+    queryset = State.objects.all()
+    serializer_class = StateSerializer
+    filter_class = StateFilterSet
+    filter_backends = (DjangoFilterBackend, filters.OrderingFilter,)
+    ordering_fields = '__all__'
+    http_method_names = ['get', ]
+
+
+class CountryViewSet(viewsets.ModelViewSet):
+    queryset = Country.objects.all()
+    serializer_class = CountrySerializer
+    filter_class = CountryFilterSet
+    filter_backends = (DjangoFilterBackend, filters.OrderingFilter,)
+    ordering_fields = '__all__'
+    http_method_names = ['get', ]
+
+
+class LocationViewSet(viewsets.ModelViewSet):
+    model = Location
+    serializer_class = LocationSerializer
+    filter_backends = (DjangoFilterBackend, filters.OrderingFilter,)
+    ordering_fields = '__all__'
+    http_method_names = ['get', ]
+
