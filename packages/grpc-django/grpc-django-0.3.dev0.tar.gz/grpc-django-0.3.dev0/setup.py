824cdc588ea1845f2c9b5af080cade77dca2f73a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:37 +0000

    Add package grpc-django/0.3.dev0/grpc-django-0.3.dev0.tar.gz

---
+++ packages/grpc-django/grpc-django-0.3.dev0.tar.gz/grpc-django-0.3.dev0/setup.py
@@ -0,0 +1,26 @@
+import codecs
+try:
+    codecs.lookup('mbcs')
+except LookupError:
+    ascii = codecs.lookup('ascii')
+    func = lambda name, enc=ascii: {True: enc}.get(name=='mbcs')
+    codecs.register(func)
+
+from setuptools import find_packages, setup
+
+version = "0.3dev"
+
+setup(
+    name="grpc-django",
+    version=version,
+    packages=find_packages(exclude=["tests*", "manage.py", "test_pb2.py", "test_pb2_grpc.py"]),
+    include_package_data=True,
+    install_requires=[
+        "Django >= 1.9",
+        "grpcio",
+        "grpcio-tools", 'google', 'six'
+    ],
+    author="Sohel Tarir",
+    author_email="sohel.tarir@gmail.com",
+    description="GRPC integration with Django projects"
+)
