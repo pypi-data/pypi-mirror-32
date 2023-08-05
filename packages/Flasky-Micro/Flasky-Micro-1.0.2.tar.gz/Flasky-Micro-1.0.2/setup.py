001dfc64971c8c00d9105b7ec6fe3995760fddff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:08:23 +0000

    Add package Flasky-Micro/1.0.2/Flasky-Micro-1.0.2.tar.gz

---
+++ packages/Flasky-Micro/Flasky-Micro-1.0.2.tar.gz/Flasky-Micro-1.0.2/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+
+setup(
+    name="Flasky-Micro",
+    version="1.0.2",
+    description="Flask Micro Service Framework",
+    author="Mathieu Paul",
+    author_email="laup.mathieu@gmail.com",
+    license="MIT",
+    packages=["flasky"],
+    install_requires=[
+        "Flask",
+        "Flask-Classy",
+        "PyYAML"
+    ],
+    zip_safe=False
+)
