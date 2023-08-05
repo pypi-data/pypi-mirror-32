27bbcf8cee376fe4d654d9552d4eb119e01cc9fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:22 +0000

    Add package rq-scheduler-bcfg/0.8.4/rq-scheduler-bcfg-0.8.4.tar.gz

---
+++ packages/rq-scheduler-bcfg/rq-scheduler-bcfg-0.8.4.tar.gz/rq-scheduler-bcfg-0.8.4/setup.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+import sys
+
+from setuptools import setup
+
+tests_require = []
+if sys.version_info < (2, 7):
+    tests_require.append('discover==0.4.0')
+
+def get_dependencies():
+    deps = [
+        'croniter>=0.3.9',
+    ]
+    if (sys.version_info < (2, 7) or
+            (sys.version_info >= (3, 0) and sys.version_info < (3, 1))):
+        deps += ['rq~=0.8.0']
+    else:
+        deps += ['rq>=0.8.0']
+    return deps
+
+setup(
+    name='rq-scheduler-bcfg',
+    version='0.8.4',
+    author='Selwin Ong',
+    author_email='selwin.ong@gmail.com',
+    packages=['rq_scheduler'],
+    url='https://github.com/as3445/rq-scheduler-bcfg',
+    license='MIT',
+    description='Provides job scheduling capabilities to RQ (Redis Queue)',
+    long_description=open('README.rst').read(),
+    zip_safe=False,
+    include_package_data=True,
+    entry_points='''\
+    [console_scripts]
+    rqscheduler = rq_scheduler.scripts.rqscheduler:main
+    ''',
+    package_data={'': ['README.rst']},
+    tests_require=tests_require,
+    install_requires=get_dependencies() + tests_require,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ]
+)
