2abce54a226d8e3b3f6d47beffcc5f7d7f3f6a4a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:14 +0000

    Add package pylibcontainer/0.0.1/pylibcontainer-0.0.1.tar.gz

---
+++ packages/pylibcontainer/pylibcontainer-0.0.1.tar.gz/pylibcontainer-0.0.1/setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Install using distutils
+
+Run:
+    python setup.py install
+
+to install this package.
+"""
+from setuptools import setup, find_packages
+from os.path import join
+
+with  open('requirements.txt') as r_file:
+    requirements = r_file.read()
+
+name = "pylibcontainer"
+
+setup(
+    version=open(join(name, 'version')).readline().strip("\r\n"),
+    install_requires=[x for x in requirements.splitlines() if x],
+    packages=find_packages(),
+    entry_points='''
+        [console_scripts]
+        pylibcontainer=pylibcontainer.__main__:pylibcontainer
+        '''
+)
