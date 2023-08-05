0eec4906eb470be61f967478ac9772aea1f490b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:30 +0000

    Add package efes/0.0.1/efes-0.0.1.tar.gz

---
+++ packages/efes/efes-0.0.1.tar.gz/efes-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='efes',
+    version='0.0.1',
+    description='',
+    long_description='',
+    author='Omer Murat Yildirim',
+    author_email='omermuratyildirim@gmail.com',
+    packages=[],
+)
