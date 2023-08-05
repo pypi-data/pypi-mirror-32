276505362dde0ee80f58cde3fa7d715cb8b12e59
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:00 +0000

    Add package cache-simulator/2.0.0.post1/cache-simulator-2.0.0.post1.tar.gz

---
+++ packages/cache-simulator/cache-simulator-2.0.0.post1.tar.gz/cache-simulator-2.0.0.post1/setup.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+from setuptools import setup
+
+
+# Get long description (used on PyPI project page)
+def get_long_description():
+    try:
+        # Use pandoc to create reStructuredText README if possible
+        import pypandoc
+        return pypandoc.convert('README.md', 'rst')
+    except Exception:
+        return None
+
+
+setup(
+    name='cache-simulator',
+    version='2.0.0-rev.1',
+    description='A processor cache simulator for the MIPS ISA',
+    long_description=get_long_description(),
+    url='https://github.com/caleb531/cache-simulator',
+    author='Caleb Evans',
+    author_email='caleb@calebevans.me',
+    license='MIT',
+    keywords='mips processor cache simulator architecture',
+    packages=['cachesimulator'],
+    install_requires=[],
+    entry_points={
+        'console_scripts': [
+            'cache-simulator=cachesimulator.__main__:main'
+        ]
+    }
+)
