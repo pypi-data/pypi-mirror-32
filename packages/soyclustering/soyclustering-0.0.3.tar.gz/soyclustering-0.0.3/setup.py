58b445e8de165f30d80b9d50f69fb532ca092865
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:17:27 +0000

    Add package soyclustering/0.0.3/soyclustering-0.0.3.tar.gz

---
+++ packages/soyclustering/soyclustering-0.0.3.tar.gz/soyclustering-0.0.3/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="soyclustering",
+    version='0.0.3',
+    author='Lovit',
+    author_email='soy.lovit@gmail.com',
+    url='https://github.com/lovit/clustering4docs',
+    description="Python library for document clustering",
+    long_description="Python library for document clustering",
+    install_requires=[],
+    keywords = ['document clustering', 'clustering labeling'],
+    packages=find_packages()
+)
