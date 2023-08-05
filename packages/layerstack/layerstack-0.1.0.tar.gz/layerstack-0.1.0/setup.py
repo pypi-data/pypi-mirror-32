7916f1e9b8746d3d489aa2b1eb2d20c7c9e78fa4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:48 +0000

    Add package layerstack/0.1.0/layerstack-0.1.0.tar.gz

---
+++ packages/layerstack/layerstack-0.1.0.tar.gz/layerstack-0.1.0/setup.py
@@ -0,0 +1,29 @@
+
+from distutils.core import setup
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+# Get the long description from the README file
+with open(os.path.join(here, './README.txt'), encoding='utf-8') as f:
+    long_description = f.read()
+
+with open(os.path.join(here, 'layerstack', '_version.py'), encoding='utf-8') as f:
+    version = f.read()
+
+version = version.split()[2].strip('"').strip("'")
+
+setup(
+    name = 'layerstack',
+    version = version,
+    author = 'Elaine Hale, Michael Rossol',
+    author_email = 'elaine.hale@nrel.gov',
+    packages = ['layerstack'],
+    url = 'https://github.com/Smart-DS/layerstack',
+    description = 'Python package for assembling, sharing, and running workflows, especially those associated with modifying, running, and analyzing simulation models',
+    long_description = long_description,
+    package_data = {
+        'layerstack': ['*.template']
+    },
+    install_requires=open('requirements.txt').read()
+)
