03bbda07b9f728fc11dcf98f35a0b096e6b0c7a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:22 +0000

    Add package parseratorvariable/0.0.17/parseratorvariable-0.0.17.tar.gz

---
+++ packages/parseratorvariable/parseratorvariable-0.0.17.tar.gz/parseratorvariable-0.0.17/setup.py
@@ -0,0 +1,17 @@
+try:
+    from setuptools import setup, Extension
+except ImportError :
+    raise ImportError("setuptools module required, please go to https://pypi.python.org/pypi/setuptools and follow the instructions for installing setuptools")
+
+setup(
+    name='parseratorvariable',
+    url='https://github.com/datamade/parseratorvariables',
+    version='0.0.17',
+    description='Structured variable type for dedupe',
+    packages=['parseratorvariable'],
+    install_requires=['dedupe>=1.0.0',
+                      "backports.functools_lru_cache; python_version<'3.2'",
+                      'numpy',
+                      'probableparsing'],
+    license='The MIT License: http://www.opensource.org/licenses/mit-license.php'
+    )
