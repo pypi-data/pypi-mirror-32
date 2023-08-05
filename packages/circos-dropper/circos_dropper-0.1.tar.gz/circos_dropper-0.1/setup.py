006b6ad62eafc08bcac20d76365c00d4b335a63b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:57 +0000

    Add package circos-dropper/0.1/circos_dropper-0.1.tar.gz

---
+++ packages/circos-dropper/circos_dropper-0.1.tar.gz/circos_dropper-0.1/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+    name='circos_dropper',
+    version='0.1',
+    py_modules=['circos_dropper'],
+    install_requires=[
+        'numpy',
+        'pandas',
+        'pysam==0.10.0',
+        'pybedtools',
+        'pyfaidx',
+        'pathos',
+    ],
+    entry_points="""
+        [console_scripts]
+        circos_dropper=circos_dropper:circosdrop"""
+)
