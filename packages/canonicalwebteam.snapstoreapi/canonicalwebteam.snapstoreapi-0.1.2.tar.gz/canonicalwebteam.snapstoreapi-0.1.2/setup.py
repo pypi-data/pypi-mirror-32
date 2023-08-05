00164afe1e5ec7671b11fdf4a71728ff1e5a2e4b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:31:33 +0000

    Add package canonicalwebteam.snapstoreapi/0.1.2/canonicalwebteam.snapstoreapi-0.1.2.tar.gz

---
+++ packages/canonicalwebteam.snapstoreapi/canonicalwebteam.snapstoreapi-0.1.2.tar.gz/canonicalwebteam.snapstoreapi-0.1.2/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+setup(
+    name='canonicalwebteam.snapstoreapi',
+    version='0.1.2',
+    author='Canonical Webteam',
+    author_email='thomas.bille@canonical.com',
+    url='https://github.com/canonical-webteam/snapstore-api',
+    license='AGPLv3',
+    description='Snapstore API',
+    packages=[
+        'canonicalwebteam.snapstoreapi'
+    ],
+    install_requires=[
+        "requests",
+        "pymacaroons",
+        "python-openid",
+        "prometheus_client",
+    ],
+    # TODO Add proper test suite
+    test_suite='tests.py',
+)
