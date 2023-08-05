01242209591f9ebe9527519a5bd3d2718bf3e1c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:19:29 +0000

    Add package WebRequest/0.0.27/WebRequest-0.0.27.tar.gz

---
+++ packages/WebRequest/WebRequest-0.0.27.tar.gz/WebRequest-0.0.27/setup.py
@@ -0,0 +1,45 @@
+
+import setuptools
+from distutils.core import setup
+import sys
+import time
+setup(
+	# Application name:
+	name="WebRequest",
+
+	# Version number (initial):
+	version="0.0.27",
+
+	# Application author details:
+	author="Connor Wolf",
+	author_email="github@imaginaryindustries.com",
+
+	# Packages
+	packages=setuptools.find_packages(),
+	package_dir = {'WebRequest': 'WebRequest'},
+
+
+	# Details
+	url="https://github.com/fake-name/WebRequest",
+
+	#
+	# license="LICENSE.txt",
+	description="Like `requests`, but shittier.",
+
+	long_description              = open("README.md").read(),
+	long_description_content_type = "text/markdown",
+
+	dependency_links=[
+		'https://github.com/fake-name/ChromeController/tarball/master#egg=ChromeController-0.1.2',
+	],
+
+	# Dependent packages (distributions)
+	install_requires=[
+		'beautifulsoup4>=4.6.0',
+		'selenium>=3.8.1',
+		'PySocks>=1.6.8',
+		'cchardet>=2.1.1',
+		'lxml>=4.1.1',
+		"ChromeController>=0.1.2",
+	],
+)
