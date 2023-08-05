bae07ff56316090af3c60579be3cafa0ee8c2ca6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:50 +0000

    Add package abnum/0.1.7/abnum-0.1.7.tar.gz

---
+++ packages/abnum/abnum-0.1.7.tar.gz/abnum-0.1.7/setup.py
@@ -0,0 +1,47 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+import os
+
+#python setup.py sdist upload
+
+version = 'v0.1.7'
+
+name = 'abnum'
+
+def read(fname):
+    try:
+        return open(os.path.join(os.path.dirname(__file__), fname), encoding="utf8").read()
+    except:
+        print("Could not read/open file: %s" % os.path.join(os.path.dirname(__file__), fname))
+        return ''
+
+setup(
+  name = name,
+  packages = [name],
+  package_dir = {name: name},
+  package_data = {
+    name: ['*.py']
+  },
+  install_requires = ["remarkuple", "romanize3", "IPython", "pandas"],
+  version = version,
+  description = 'Abnum - Alphabetic numerals package including various letter value substituting systems from ancient times to modern artificial ones',
+  long_description = read('README'),
+  author = 'Marko Manninen',
+  author_email = 'elonmedia@gmail.com',
+
+  url = 'https://github.com/markomanninen/%s' % name,
+  download_url = 'https://github.com/markomanninen/%s/archive/%s.tar.gz' % (name, version),
+  keywords = ['python', 'jupyter notebook', 'ancient greek', 'isopsephy', 'text analysis'],
+  platforms = ['any'],
+
+  classifiers = [
+    "Development Status :: 3 - Alpha",
+    "Intended Audience :: Developers",
+    "License :: OSI Approved :: MIT License",
+    "Operating System :: OS Independent",
+    "Programming Language :: Python",
+    "Topic :: Software Development :: Libraries",
+  ]
+)
