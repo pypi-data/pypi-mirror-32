b6110ce0112ad8ebdb0ebb4c3a410167023506ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:23:12 +0000

    Add package Dans-Diffraction/1.0.1/Dans_Diffraction-1.0.1.zip

---
+++ packages/Dans-Diffraction/Dans_Diffraction-1.0.1.zip/Dans_Diffraction-1.0.1/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+setup(
+	name = 'Dans_Diffraction',
+	packages = ['Dans_Diffraction'],
+	version = '1.0.1',
+	description = 'Generate diffracted intensities from crystals',
+	author = 'Dan Porter',
+	author_email = 'd.g.porter@outlook.com',
+	url = 'https://github.com/DanPorter/Dans_Diffraction',
+	download_url = 'https://github.com/DanPorter/Dans_Diffraction/archive/0.1.tar.gz',
+	keywords = ['crystal','cif','diffraction','crystallography','science'],
+	classifiers = [
+		'Programming Language :: Python :: 2.7',
+		'Intended Audience :: Science/Research',
+		'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+		'Development Status :: 3 - Alpha', 
+		],
+	install_requires=['numpy','matplotlib','scipy'],
+	package_data={'':['data/*.txt','data/*.dat','Structures/*.cif','Structures/*.mcif']}
+	)
