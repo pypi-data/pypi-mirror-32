ae8c3d17c17c9529021cefd4c410b720f8ba03df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:57 +0000

    Add package wechat-requests/1.4.5/wechat-requests-1.4.5.tar.gz

---
+++ packages/wechat-requests/wechat-requests-1.4.5.tar.gz/wechat-requests-1.4.5/src/wechat/__version__.py
@@ -0,0 +1,2 @@
+__version__ = '1.4.5'
+__name__ = 'wechat-requests'
---
+++ packages/wechat-requests/wechat-requests-1.4.5.tar.gz/wechat-requests-1.4.5/src/wechat/settings.py
@@ -0,0 +1,29 @@
+# -*- encoding: utf-8
+
+from .utils import build_user_agent
+
+# common
+DEFAULT_HEADERS = {
+    'User-Agent': build_user_agent()
+}
+
+TIMEOUT = 1
+
+ENCODING = 'utf-8'
+
+RETRYS = 3
+RETRY_BACKOFF_FACTOR = 0.1
+RETRY_STATUS_FORCELIST = frozenset([500, 502, 504])
+
+
+# auth
+OAUTH_HOST = 'open.weixin.qq.com'
+AUTH_EXPIRED_CODES = frozenset([40001, 40014, 41001, 42001])
+
+# pay
+TRADE_TYPE_JSAPI = 'JSAPI'  # 公众号支付
+TRADE_TYPE_NATIVE = 'NATIVE'  # 扫码支付
+TRADE_TYPE_APP = 'APP'  # APP支付
+
+SIGN_TYPE = 'MD5'
+SIGN_NONCE_STR_LEN = 32
