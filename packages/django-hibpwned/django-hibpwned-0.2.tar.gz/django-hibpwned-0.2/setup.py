8363fdaeea5d9949fd7a05fc9fd0dfb553b05df4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:03 +0000

    Add package django-hibpwned/0.2/django-hibpwned-0.2.tar.gz

---
+++ packages/django-hibpwned/django-hibpwned-0.2.tar.gz/django-hibpwned-0.2/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+setup(
+    name='django-hibpwned',
+    version='0.2',
+    description='Django password validator based on haveibeenpwned.com API',
+    url='https://github.com/tombiasz/django-hibpwned',
+    download_url='https://github.com/tombiasz/django-hibpwned/archive/v0.1.zip',
+    author='tombiasz',
+    author_email='tomasz.m.nowacki@gmail.com',
+    license='MIT',
+    packages=['haveibeenpwned'],
+    zip_safe=False,
+    install_requires=[
+        'Django>=1.9',
+        'requests>=2'
+    ],
+    classifiers=[
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Framework :: Django',
+        'Programming Language :: Python :: 3 :: Only',
+    ]
+)
