1265d06a75e4f20b0ed43ad34c3be39d37a42963
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:15 +0000

    Add package star-builder/0.1.11/star-builder-0.1.11.tar.gz

---
+++ packages/star-builder/star-builder-0.1.11.tar.gz/star-builder-0.1.11/star_builder/app.py
@@ -0,0 +1,89 @@
+import logging
+
+from apistar import ASyncApp, App, exceptions
+from apistar.http import Response, JSONResponse
+
+from .bases.hooks import ErrorHook
+from .bases.service import Service
+from .bases.components import Component, SettingsComponent
+from .helper import load_packages, routing, print_routing
+
+__all__ = ["Application"]
+
+logger = logging.getLogger("star_builder.app")
+
+
+class FixedAsyncApp(ASyncApp):
+
+    def exception_handler(self, exc: Exception) -> Response:
+        if isinstance(exc, exceptions.HTTPException):
+            return JSONResponse(exc.detail, exc.status_code, exc.get_headers())
+        raise exc
+
+
+class FixedApp(App):
+
+    def exception_handler(self, exc: Exception) -> Response:
+        if isinstance(exc, exceptions.HTTPException):
+            return JSONResponse(exc.detail, exc.status_code, exc.get_headers())
+        raise exc
+
+
+def application(template_dir=None,
+                static_dir=None,
+                packages=None,
+                schema_url='/schema/',
+                docs_url='/docs/',
+                static_url='/static/',
+                components=None,
+                event_hooks=None,
+                settings_path="settings",
+                debug=True,
+                async=True):
+    """
+       参数指定选择使用异步app还是同步app
+       可以动态发现当前项目根目录下所有routes的service中的handler
+    """
+    load_packages(".")
+    include = routing(Service, None)
+    SettingsComponent.register_path(settings_path)
+    loaded, unloaded = [], []
+
+    def find_children(components):
+        children = []
+        for component in components:
+            children.append(component)
+            children.extend(find_children(component.children))
+        return children
+
+    for child in find_children(Component.children):
+        if child._instance:
+            loaded.append(child._instance)
+        else:
+            unloaded.append(child)
+
+    components = [c() for c in unloaded] + (components or []) + loaded
+
+    if include:
+        routes = [include]
+        print_routing(routes, callback=logger.debug)
+    else:
+        logger.info("Noting to route. ")
+        routes = []
+
+    app = (FixedAsyncApp if async else FixedApp)(
+        routes,
+        template_dir=template_dir,
+        static_dir=static_dir,
+        packages=packages,
+        schema_url=schema_url,
+        docs_url=docs_url,
+        static_url=static_url,
+        components=components,
+        event_hooks=[ErrorHook()] + event_hooks or [])
+
+    app.debug = debug
+    return app
+
+
+Application = application
