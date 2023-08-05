cc4b9ff62efaad77630cc02303bc75a3c954d90f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:36 +0000

    Add package pyqtlet/0.2.beta3/pyqtlet-0.2.beta3.tar.gz

---
+++ packages/pyqtlet/pyqtlet-0.2.beta3.tar.gz/pyqtlet-0.2.beta3/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+setup(
+  name = 'pyqtlet',
+  packages = ['pyqtlet'],
+  version = '0.2.beta3',
+  description = 'Using leaflet maps in PyQt',
+  author = 'Samarth Hattangady',
+  author_email = 'samhattangady@gmail.com',
+  url = 'https://github.com/skylarkdrones/pyqtlet',
+  keywords = ['leaflet', 'pyqt', 'maps'],
+  classifiers = [],
+)
