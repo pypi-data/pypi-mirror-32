26fbf190182cce6ef4a8e251f3cf3bd487cf250d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:09:52 +0000

    Add package pylint/2.0.0.dev1/pylint-2.0.0.dev1.tar.gz

---
+++ packages/pylint/pylint-2.0.0.dev1.tar.gz/pylint-2.0.0.dev1/pylint/test/functional/unused_typing_imports.py
@@ -0,0 +1,34 @@
+# pylint: disable=missing-docstring, bad-whitespace
+"""Regression test for https://github.com/PyCQA/pylint/issues/1168
+
+The problem was that we weren't handling keyword-only arguments annotations,
+which means we were never processing them.
+"""
+
+
+from typing import Optional, Callable, Iterable, Any, List, Tuple, Set
+
+
+def func1(arg: Optional[Callable]=None):
+    return arg
+
+
+def func2(*, arg: Optional[Iterable]=None):
+    return arg
+
+
+SOME_VALUE = [1] # type: List[Any]
+for VALUE in [[1], [2], [3]]:  # type: Tuple[Any]
+    print(VALUE)
+
+
+class ContextManager:
+    def __enter__(self):
+        return {1}
+
+    def __exit__(self, *_args):
+        pass
+
+
+with ContextManager() as SOME_DICT: # type: Set[int]
+    print(SOME_DICT)
