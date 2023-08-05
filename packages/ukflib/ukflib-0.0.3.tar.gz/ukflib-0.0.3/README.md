804198e4b8c826cc79c49bffb064caf50371012f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:27 +0000

    Add package ukflib/0.0.3/ukflib-0.0.3.tar.gz

---
+++ packages/ukflib/ukflib-0.0.3.tar.gz/ukflib-0.0.3/README.md
@@ -0,0 +1,12 @@
+[![Build Status](https://travis-ci.com/sciencectn/ukflib.svg?branch=master)](https://travis-ci.com/sciencectn/ukflib)
+
+# ukflib
+An Unscented Kalman Filter library for Python, created as a course project for ECE 722 (Kalman Filtering) at GMU. 
+
+There are a few UKF libraries available for Python, but this one is unique because it adds support for **non-additive** process noise and measurement noise. 
+
+All of the equations used are based on Rudolph Van der Merwe's PhD thesis: 
+```
+Van Der Merwe, Rudolph. "Sigma-point Kalman filters for 
+probabilistic inference in dynamic state-space models." (2004).
+```
---
+++ packages/ukflib/ukflib-0.0.3.tar.gz/ukflib-0.0.3/setup.py
@@ -0,0 +1,29 @@
+
+from setuptools import setup
+import os
+
+with open(os.path.join(os.path.abspath(os.path.dirname(__file__)), 'DESCRIPTION.rst')) as f:
+    long_description = f.read()
+
+setup(
+    name="ukflib",
+    author="Chris Taylor",
+    author_email="sciencectn@gmail.com",
+    description="An Unscented Kalman Filter library that allows for nonadditive process and measurement noise",
+    long_description=long_description,
+    keywords="ukf unscented kalman filter",
+    version="0.0.3",
+    py_modules=["ukflib"],
+    install_requires=["numpy>=1.14.0",
+                      "matplotlib>=2.2.0",
+                      "scipy>=1.0.0"],
+    url="https://github.com/sciencectn/ukflib",
+    classifiers=[
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 2.7",
+        "Topic :: Scientific/Engineering",
+        "Intended Audience :: Science/Research"
+    ]
+)
+
+
