450c2ef75c3d9f2751e125e40365f8666e08286a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:08:26 +0000

    Add package SFlock/0.3.3/SFlock-0.3.3.tar.gz

---
+++ packages/SFlock/SFlock-0.3.3.tar.gz/SFlock-0.3.3/sflock/unpack/tar.py
@@ -0,0 +1,82 @@
+# Copyright (C) 2015-2018 Jurriaan Bremer.
+# This file is part of SFlock - http://www.sflock.org/.
+# See the file 'docs/LICENSE.txt' for copying permission.
+
+import bz2
+import gzip
+import six
+import tarfile
+
+from sflock.abstracts import Unpacker, File
+
+class TarFile(Unpacker):
+    name = "tarfile"
+    mode = "r:"
+    exts = b".tar"
+    magic = "POSIX tar archive"
+
+    def supported(self):
+        return True
+
+    def unpack(self, password=None, duplicates=None):
+        try:
+            archive = tarfile.open(mode=self.mode, fileobj=self.f.stream)
+        except tarfile.ReadError as e:
+            self.f.mode = "failed"
+            self.f.error = e
+            return []
+
+        entries = []
+        for entry in archive:
+            # Ignore anything that's not a file for now.
+            if not entry.isfile():
+                continue
+
+            relapath = entry.path
+            if six.PY3:
+                relapath = relapath.encode()
+
+            entries.append(File(
+                relapath=relapath,
+                contents=archive.extractfile(entry).read()
+            ))
+
+        return self.process(entries, duplicates)
+
+class TargzFile(TarFile, Unpacker):
+    name = "targzfile"
+    mode = "r:gz"
+    exts = b".tar.gz"
+
+    def handles(self):
+        if self.f.filename and self.f.filename.lower().endswith(self.exts):
+            return True
+
+        if not self.f.filesize:
+            return False
+
+        try:
+            f = File(contents=gzip.GzipFile(fileobj=self.f.stream).read())
+        except IOError:
+            return False
+
+        return self.magic in f.magic
+
+class Tarbz2File(TarFile, Unpacker):
+    name = "tarbz2file"
+    mode = "r:bz2"
+    exts = b".tar.bz2"
+
+    def handles(self):
+        if self.f.filename and self.f.filename.lower().endswith(self.exts):
+            return True
+
+        if not self.f.filesize:
+            return False
+
+        try:
+            f = File(contents=bz2.decompress(self.f.contents))
+        except IOError:
+            return False
+
+        return self.magic in f.magic
---
+++ packages/SFlock/SFlock-0.3.3.tar.gz/SFlock-0.3.3/sflock/unpack/zip.py
@@ -0,0 +1,77 @@
+# Copyright (C) 2015-2018 Jurriaan Bremer.
+# This file is part of SFlock - http://www.sflock.org/.
+# See the file 'docs/LICENSE.txt' for copying permission.
+
+import six
+import zipfile
+import zlib
+
+from sflock.abstracts import File, Unpacker
+from sflock.exception import UnpackException
+
+class ZipFile(Unpacker):
+    name = "zipfile"
+    exts = b".zip"
+    magic = "Zip archive data"
+
+    def supported(self):
+        return True
+
+    def handles(self):
+        if super(ZipFile, self).handles():
+            return True
+        if self.f.stream.read(2) == b"PK":
+            return True
+        return False
+
+    def decrypt(self, password, archive, entry):
+        try:
+            archive.setpassword(password)
+
+            if six.PY3 and isinstance(entry.filename, str):
+                entry.filename = entry.filename.encode()
+
+            return File(
+                relapath=entry.filename,
+                contents=archive.read(entry),
+                password=password
+            )
+        except (RuntimeError, zipfile.BadZipfile, OverflowError,
+                zlib.error) as e:
+            msg = getattr(e, "message", None) or e.args[0]
+            if "Bad password" in msg:
+                return
+            if "Bad CRC-32" in msg:
+                return
+            if "password required" in msg:
+                return
+            if "Truncated file header" in msg:
+                return
+            if "invalid distance too far back" in msg:
+                return
+            if "cannot fit 'long' into" in msg:
+                return
+
+            raise UnpackException("Unknown zipfile error: %s" % e)
+
+    def unpack(self, password=None, duplicates=None):
+        try:
+            archive = zipfile.ZipFile(self.f.stream)
+        except (zipfile.BadZipfile, IOError) as e:
+            self.f.mode = "failed"
+            self.f.error = e
+            return []
+
+        entries = []
+        for entry in archive.infolist():
+            if entry.filename.endswith("/"):
+                continue
+
+            f = self.bruteforce(password, archive, entry)
+            entries.append(f or File(
+                filename=entry.filename,
+                mode="failed",
+                description="Error decrypting file"
+            ))
+
+        return self.process(entries, duplicates)
