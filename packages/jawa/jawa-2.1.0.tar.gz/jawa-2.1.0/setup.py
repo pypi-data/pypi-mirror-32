6b6c674841728ea3009f1a27c8d14847defc13cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:32:19 +0000

    Add package jawa/2.1.0/jawa-2.1.0.tar.gz

---
+++ packages/jawa/jawa-2.1.0.tar.gz/jawa-2.1.0/setup.py
@@ -0,0 +1,50 @@
+
+from setuptools import setup, find_packages
+
+setup(
+    name='jawa',
+    packages=find_packages(),
+    version='2.1.0',
+    description='Doing fun stuff with JVM ClassFiles.',
+    long_description=open('README.md', 'r').read(),
+    long_description_content_type='text/markdown',
+    author='Tyler Kennedy',
+    author_email='tk@tkte.ch',
+    url='http://github.com/TkTech/Jawa',
+    keywords=[
+        'java',
+        'disassembly',
+        'disassembler',
+        'assembly'
+    ],
+    include_package_data=True,
+    classifiers=[
+        'Programming Language :: Python',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Disassemblers'
+    ],
+    install_requires=[
+        'click>=5.0'
+    ],
+    tests_require=[
+        'pytest>=2.10',
+    ],
+    extras_require={
+        'dev': [
+            'pytest',
+            'sphinx',
+            'sphinxcontrib-googleanalytics',
+            'sphinx_rtd_theme',
+            'ghp-import',
+            'pyyaml',
+            'ipython'
+        ]
+    },
+    entry_points='''
+    [console_scripts]
+    jawa=jawa.cli:cli
+    '''
+)
