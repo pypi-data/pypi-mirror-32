9ae134c1d95fe3e00725289a54f440e68a2d2a5d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:43 +0000

    Add package terraform_validate/2.8.0/terraform_validate-2.8.0.tar.gz

---
+++ packages/terraform_validate/terraform_validate-2.8.0.tar.gz/terraform_validate-2.8.0/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="terraform_validate",
+    version="2.8.0",
+    author="Edmund Dipple",
+    author_email="elmundio1987@gmail.com",
+    description="A library that provides asserts for testing Terraform configuration",
+    url="https://github.com/elmundio87/terraform_validate",
+    download_url = 'https://github.com/elmundio87/terraform_validate/tarball/2.8.0',
+    keywords = ['terraform', 'assert', 'testing'],
+    packages = find_packages(),
+    install_requires=[
+        "pyhcl"
+    ],
+)
