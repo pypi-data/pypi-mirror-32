93c685d27190dcc4681987ff405770954e95a450
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:33 +0000

    Add package rlr/2.4.5/rlr-2.4.5.tar.gz

---
+++ packages/rlr/rlr-2.4.5.tar.gz/rlr-2.4.5/README.md
@@ -0,0 +1,21 @@
+# rlr
+L2 Regularized Logistic Regression With Case Weighting
+
+Minimal dependency logistic regression classifer with L2 Regularization and optional case weighting.
+
+[![Build Status](https://travis-ci.org/dedupeio/rlr.svg)](https://travis-ci.org/dedupeio/rlr)
+
+```python
+labels = numpy.array([1] * 6 + [0] * 6)
+examples = numpy.array([1, 0] * 6).reshape(12, 1)
+
+case_weights = numpy.arange(1, 13) * 1./12
+case_weights = numpy.array([0.5] * 12)
+
+classifier = rlr.RegularizedLogisticRegression(alpha = 0)
+classifier.fit(examples, labels, case_weights=case_weights)
+
+classifier.predict_proba(examples)
+[0.5, ... 0.5]
+```
+
---
+++ packages/rlr/rlr-2.4.5.tar.gz/rlr-2.4.5/setup.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+try:
+    from setuptools import setup, Extension
+except ImportError :
+    raise ImportError("setuptools module required, please go to https://pypi.python.org/pypi/setuptools and follow the instructions for installing setuptools")
+
+
+setup(
+    name='rlr',
+    url='https://github.com/dedupeio/rlr',
+    version='2.4.5',
+    description='Case weighted L2 regularized logistic regression',
+    packages=['rlr'],
+    install_requires=['numpy>=1.13.1', 'pylbfgs', 'future>=0.14'],
+    license='Apache 2.0 License: https://www.apache.org/licenses/LICENSE-2.0',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: Apache Software License',
+        'Natural Language :: English',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: POSIX',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Scientific/Engineering',
+        'Topic :: Scientific/Engineering :: Information Analysis']
+    )
+
