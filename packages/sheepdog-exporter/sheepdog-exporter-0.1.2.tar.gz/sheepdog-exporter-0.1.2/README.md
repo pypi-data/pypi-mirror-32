dc72cb1488a95cf98cc2aec5c3377bf192a1af26
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:30 +0000

    Add package sheepdog-exporter/0.1.2/sheepdog-exporter-0.1.2.tar.gz

---
+++ packages/sheepdog-exporter/sheepdog-exporter-0.1.2.tar.gz/sheepdog-exporter-0.1.2/README.md
@@ -0,0 +1,22 @@
+# sheepdog-exporter
+
+Export metadata from the DCP.
+
+```
+sheepdog-exporter program project
+
+sheepdog-exporter program project --dcp-url my-url --credentials path/to/credentials --output-path /path/to/write/output
+```
+
+This will write a `.json` file with the corresponding metadata
+with a filename corresponding to the program and project. `program-project.json`.
+
+## Development
+
+* A simple test demonstrate usage of the exporter class in `test`.
+
+## Issues
+
+* JSON data is created by translating from TSV to work around a sheepdog issue.
+* Provenance to the original JSON schemas are lost.
+* Some functions in the exporter are unused.
---
+++ packages/sheepdog-exporter/sheepdog-exporter-0.1.2.tar.gz/sheepdog-exporter-0.1.2/setup.py
@@ -0,0 +1,72 @@
+# Don't import __future__ packages here; they make setup fail
+import os
+
+# First, we try to use setuptools. If it's not available locally,
+# we fall back on ez_setup.
+try:
+    from setuptools import setup
+except ImportError:
+    from ez_setup import use_setuptools
+    use_setuptools()
+    from setuptools import setup
+
+with open("README.pypi.rst") as readmeFile:
+    long_description = readmeFile.read()
+
+install_requires = []
+with open("requirements.txt") as requirementsFile:
+    for line in requirementsFile:
+        line = line.strip()
+        if len(line) == 0:
+            continue
+        if line[0] == '#':
+            continue
+        pinnedVersion = line.split()[0]
+        install_requires.append(pinnedVersion)
+
+dependency_links = []
+try:
+    with open("constraints.txt") as constraintsFile:
+        for line in constraintsFile:
+            line = line.strip()
+            if len(line) == 0:
+                continue
+            if line[0] == '#':
+                continue
+            dependency_links.append(line)
+except EnvironmentError:
+    print('No constraints file found, proceeding without '
+          'creating dependency links.')
+
+
+setup(
+    name="sheepdog-exporter",
+    description="Export metadata from the DCP Sheepdog API.",
+    packages=["sheepdog_exporter"],
+    url="https://github.com/david4096/sheepdog-exporter",
+    entry_points={
+        'console_scripts': [
+            'sheepdog-exporter=sheepdog_exporter.sheepdog_exporter:main'
+        ]
+    },
+    long_description=long_description,
+    install_requires=install_requires,
+    dependency_links=dependency_links,
+    license='Apache License 2.0',
+    include_package_data=True,
+    zip_safe=True,
+    author="David Steinberg",
+    author_email="davidcs@ucsc.edu",
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: Apache Software License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Scientific/Engineering :: Bio-Informatics',
+    ],
+    version='0.1.2',
+    keywords=['genomics', 'metadata', 'NIHDataCommons'],
+    # Use setuptools_scm to set the version number automatically from Git
+)
+
