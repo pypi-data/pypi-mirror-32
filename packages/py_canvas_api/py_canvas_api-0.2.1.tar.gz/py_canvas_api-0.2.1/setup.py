528a119aa74fbb5e2a5ea37ae81131d7a719cf49
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:38 +0000

    Add package py_canvas_api/0.2.1/py_canvas_api-0.2.1.tar.gz

---
+++ packages/py_canvas_api/py_canvas_api-0.2.1.tar.gz/py_canvas_api-0.2.1/setup.py
@@ -0,0 +1,27 @@
+import os
+from setuptools import setup
+
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+def readme():
+  with open(os.path.join(BASE_DIR, 'README.rst')) as f:
+    return f.read()
+
+setup(
+    name='py_canvas_api',
+    version='0.2.1',
+    description = 'A very small library for accessing the API for the Canvas LMS.',
+    long_description = readme(),
+    #py_modules=['py_canvas_api', 'canvas_api'], 
+    packages=['py_canvas_api'],
+    #download_url = 'https://github.com/kajigga/py_canvas_api',
+    url = 'https://github.com/kajigga/py_canvas_api',
+    keywords = ['canvaslms', 'api'],
+    include_package_data=True,
+    install_requires=[
+				'requests'
+    ],
+    #test_suite='nose.collector',
+    #tests_require=['nose', 'mock']
+)
