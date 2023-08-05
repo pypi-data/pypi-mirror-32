cc75e5caac1aa25ed9736378f627f2c4bd4d59bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:10 +0000

    Add package pwclip/1.4.3/pwclip-1.4.3-py3-none-any.whl

---
+++ packages/pwclip/pwclip-1.4.3-py3-none-any.whl/pwclip/lib/system/__init__.py
@@ -0,0 +1,10 @@
+"""system lib init for pwclip"""
+from system.clips import clips, copy, paste
+
+from system.user import userfind, whoami
+
+from system.path import absrelpath, filetime, setfiletime, filerotate
+
+from system.xlib import xinput, xnotify, xyesno, xmsgok, xgetpass
+
+from system.which import which
