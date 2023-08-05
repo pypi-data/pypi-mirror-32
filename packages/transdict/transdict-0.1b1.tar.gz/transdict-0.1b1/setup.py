cab9df44eab6a173b74f48ecf8065558814a91ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:29 +0000

    Add package transdict/0.1b1/transdict-0.1b1.tar.gz

---
+++ packages/transdict/transdict-0.1b1.tar.gz/transdict-0.1b1/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+setup(name='transdict',
+    version='0.1b1',
+    description='Python dict mapping with transform functions',
+    long_description='Python dict mapping with transform functions',
+    url='https://github.com/golightlyb/transdict',
+    author='Ben Golightly',
+    author_email='golightly.ben@googlemail.com',
+    maintainer='Tawesoft Ltd',
+    maintainer_email='opensource@tawesoft.co.uk',
+    license='MIT',
+    packages=['transdict'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+    zip_safe=True)
