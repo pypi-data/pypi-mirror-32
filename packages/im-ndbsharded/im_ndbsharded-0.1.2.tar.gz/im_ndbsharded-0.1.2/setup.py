e2b09f2e4f9588964fd4a6e5a29e61922fc0ea73
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:33:31 +0000

    Add package im-ndbsharded/0.1.2/im_ndbsharded-0.1.2.tar.gz

---
+++ packages/im-ndbsharded/im_ndbsharded-0.1.2.tar.gz/im_ndbsharded-0.1.2/setup.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+dist = setup(
+    name='im_ndbsharded',
+    version='0.1.2',
+    description='ndb sharded function, for traversing ndb objects at any scale, for Google App Engine, Python standard environment',
+    author='Emlyn O\'Regan',
+    author_email='emlynoregan@gmail.com',
+    url='https://github.com/emlynoregan/im_ndbsharded',
+    license='../LICENSE.txt',
+    packages=['im_ndbsharded'],
+    install_requires=['im_task', 'im_util'],
+    long_description=open('../README.md').read(),
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: POSIX',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: MacOS :: MacOS X',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: System :: Distributed Computing',
+    ]
+)
