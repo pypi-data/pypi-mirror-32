069b6e68bce69a85f62a6ee02cfbf212f5196697
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:31:04 +0000

    Add package boxx/0.8.114/boxx-0.8.114.tar.gz

---
+++ packages/boxx/boxx-0.8.114.tar.gz/boxx-0.8.114/boxx/__init__.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import unicode_literals
+
+'''
+Box-X is a develop-time Toolbox for Python.
+Espacially for Scientific Computing and Computer Vision.
+'''
+__version__ = "0.8.114"
+__short_description__ = "Box-X is a tool-box for efficient build and debug in Python. Especially for Scientific Computing and Computer Vision."
+__license__ = "MIT"
+__author__ = "DIYer22"
+__author_email__ = "ylxx@live.com"
+__maintainer__ = "DIYer22"
+__maintainer_email__ = "ylxx@live.com"
+__github_username__ = "DIYer22"
+
+from . import ylsys
+from . import ylcompat
+from . import tool
+from . import ylimg
+from . import ylnp
+from . import ylml
+from . import yldb
+from . import undetermined
+
+
+
+from .tool import *
+from .ylsys import *
+from .ylcompat import *
+from .ylimg import *
+from .ylml import * 
+from .ylnp import *
+
+
+if __name__ == '__main__':
+    pass
+
---
+++ packages/boxx/boxx-0.8.114.tar.gz/boxx-0.8.114/boxx/ylcompat.py
@@ -0,0 +1,94 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import unicode_literals
+from __future__ import print_function
+
+from .ylsys import py2, pyi
+import os
+from functools import reduce, wraps
+
+printf = print
+
+
+if py2:
+    unicode = __builtins__['unicode']
+else:
+    unicode = str
+
+def isstr(s):
+    '''
+    `isinstance(s, str)` for compatibility both python 2/3
+    '''
+    return isinstance(s, (str, unicode))
+
+
+class Classobj:
+    ''' 兼容 python 2 的 classobj'''
+    pass
+classobj = type(Classobj)
+def istype(objOrType):
+    ''' 
+    `isinstance(objOrType, type)` for compatibility both python 2/3
+    '''
+    return isinstance(objOrType, (type, classobj))
+
+
+def setFuncation2ForAutoList():
+    '''
+    add funcation 'range2', 'map2', 'reduce2', 'zip2' that retrun a list like python2
+    '''
+    funForAutoList = ['range', 'map', 'reduce', 'filter', 'zip']
+    if py2:
+        addListAfter = lambda x:x
+    else:    
+        def addListAfter(f):
+            @wraps(f)
+            def innerF(*l, **kv):
+                r = f(*l, **kv)
+                return list(r)
+            return innerF
+    reduce
+    glob = globals()
+    for name in funForAutoList:
+        rawf = glob[name] if name in glob else  __builtins__[name]
+        glob[name+'2'] = addListAfter(rawf)
+
+setFuncation2ForAutoList()
+
+
+def setDisplayEnv():
+    msg = '''%s
+        os.environ["DISPLAY"] not found, may cuse error like this 
+        [QXcbConnection Error](https://github.com/ipython/ipython/issues/10627)
+        so, we auto set os.environ["QT_QPA_PLATFORM"] = "offscreen"    '''%'\x1b[36m%s\x1b[0m'% 'warning from boxx'
+    msg = '''os.environ["DISPLAY"] is not found
+    plt.show() are redirect to plt.savefig(tmp)
+    funcation: show, loga, crun, heatmap, plot will be affected'''
+    
+    from .tool import warn
+    warn(msg)
+    os.environ["QT_QPA_PLATFORM"] = "offscreen"
+    os.environ['DISPLAY'] = ':0'
+    
+if not pyi.plt:
+    setDisplayEnv()
+    import matplotlib.pyplot as plt
+    def savefig(*l, **kv):
+        from .ylsys import tmpboxx
+        from .tool import increase, warn, OffScreenWarning
+        showtmp = os.path.join(tmpboxx(), 'showtmp')
+        if not os.path.isdir(showtmp):
+            os.makedirs(showtmp)
+        png = os.path.join(showtmp,'%d_show.png'%increase('showtmp'))
+        warn('''os.environ["DISPLAY"] is not found
+    plt.show() are redirect to plt.savefig("%s")'''%png, OffScreenWarning)
+        plt.savefig(png)
+        plt.cla()
+        plt.clf()
+    pltshow = plt.show
+    plt.show = savefig
+
+if not py2 and 0:
+    __rawOpen__ = open
+    open = lambda *l:__rawOpen__(l[0],'r',-1,'utf8') if len(l) == 1 else __rawOpen__(l[0],l[1],-1,'utf8')
+
