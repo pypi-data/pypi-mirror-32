83c735a57caee7d67399917f84fd947337e44cfc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:07 +0000

    Add package testgokoro/0.0.1.dev1/testgokoro-0.0.1.dev1.tar.gz

---
+++ packages/testgokoro/testgokoro-0.0.1.dev1.tar.gz/testgokoro-0.0.1.dev1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='testgokoro',
+    version='0.0.1.dev1',
+    packages=['testgokoro'],
+    url='',
+    license='MIT',
+    author='gokoro',
+    author_email='',
+    description='testgokoro package',
+    long_description='#test',
+    long_description_content_type='text/markdown'
+)
