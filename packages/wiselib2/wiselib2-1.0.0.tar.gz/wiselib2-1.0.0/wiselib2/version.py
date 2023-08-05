d05711f2b31b4dd568123d0b36a09098794305ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:22:24 +0000

    Add package wiselib2/1.0.0/wiselib2-1.0.0.tar.gz

---
+++ packages/wiselib2/wiselib2-1.0.0.tar.gz/wiselib2-1.0.0/wiselib2/version.py
@@ -0,0 +1,11 @@
+
+# THIS FILE IS GENERATED FROM OASYS SETUP.PY
+short_version = '1.0.0'
+version = '1.0.0'
+full_version = '1.0.0'
+git_revision = 'ff6d9f314ea49e68a8a0684c816571be9070c73f'
+release = True
+
+if not release:
+    version = full_version
+    short_version += ".dev"
