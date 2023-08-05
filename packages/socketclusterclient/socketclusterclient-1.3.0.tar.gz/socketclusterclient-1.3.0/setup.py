4c3f163a6d3228894978ce01f086a0e92ace9391
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:16 +0000

    Add package socketclusterclient/1.3.0/socketclusterclient-1.3.0.tar.gz

---
+++ packages/socketclusterclient/socketclusterclient-1.3.0.tar.gz/socketclusterclient-1.3.0/setup.py
@@ -0,0 +1,30 @@
+from distutils.core import setup
+
+setup(
+    name='socketclusterclient',
+    packages=['socketclusterclient'],  # this must be the same as the name above
+    version='1.3.0',
+    description='Client library for socketcluster framework in nodejs',
+    author='Sachin Shinde',
+    author_email='sachinshinde7676@gmail.com',
+    license='MIT',
+    url='https://github.com/sacOO7/socketcluster-client-python',  # use the URL to the github repo
+    download_url='https://github.com/sacOO7/socketcluster-client-python/tarball/v1.3.0',  # I'll explain this in a second
+    keywords=['websocket', 'socketcluster', 'nodejs', 'client', 'socketclusterclient'],  # arbitrary keywords
+    install_requires=[
+          'websocket-client',
+      ],
+    classifiers=[],
+)
+
+
+# python setup.py register -r pypitest
+# python setup.py sdist upload -r pypitest
+# python setup.py register -r pypi
+# python setup.py sdist upload -r pypi
+# python3 -m twine upload dist/
+
+# git tag -a v0.2 -m "My more optimized version of socketcluster client"
+# git push --tags origin
+
+# https://upload.pypi.org/legacy/
