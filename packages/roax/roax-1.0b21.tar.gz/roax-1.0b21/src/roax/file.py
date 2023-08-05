abde0b0ea543a2308430a4b59c35209b424f4a60
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:56 +0000

    Add package roax/1.0b21/roax-1.0b21.tar.gz

---
+++ packages/roax/roax-1.0b21.tar.gz/roax-1.0b21/src/roax/file.py
@@ -0,0 +1,162 @@
+"""Module to store resource items in files."""
+
+# Copyright © 2017–2018 Paul Bryan.
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+import inspect
+import json
+import os
+import roax.schema as s
+
+from copy import copy
+from os.path import expanduser
+from roax.resource import Resource, Conflict, InternalServerError, NotFound, operation
+
+try:
+    import fcntl
+except ImportError:
+    fcntl = None
+
+
+_map = [(c, "%{:02X}".format(ord(c))) for c in "%/\\:*?\"<>|"]
+
+def _quote(s):
+    """_quote('abc/def') -> 'abc%2Fdef'"""
+    for m in _map:
+        s = s.replace(m[0], m[1])
+    return s
+
+def _unquote(s):
+    """_unquote('abc%2Fdef') -> 'abc/def'"""
+    if "%" in s:
+        for m in _map:
+            s = s.replace(m[1], m[0])
+    return s
+
+
+class FileResource(Resource):
+    """
+    Base class for a file-based resource; each item is a stored as a separate file
+    in a directory. This class is appropriate for up to hundreds of items; it is
+    probably not appropriate for thousands or more.
+    
+    The subclass must register the operations it wants to expose. An effective way
+    to do this is to override the function in the subclass, decorated with
+    @operator, and have it in turn call the superclass method.
+
+    The way the item file is encoded depends on the document schema:
+    - dict: each resource is stored as a text file containing a JSON object.
+    - str: each resource stored as a UTF-8 text file.
+    - bytes: each resource is a file containing binary data. 
+
+    The list method is suitable to expose as a query operation.
+    """
+
+    def _filename(self, id):
+        return "{}/{}{}".format(self.dir, _quote(self.id_schema.str_encode(id)), self.extension)
+
+    def _open(self, id, mode):
+        try:
+            encoding = None if isinstance(self.schema, s.bytes) else "utf-8"
+            mode = (mode + "b") if isinstance(self.schema, s.bytes) else mode
+            file = open(self._filename(id), mode, encoding=encoding)
+            if fcntl:
+                fcntl.flock(file.fileno(), fcntl.LOCK_EX)
+            return file
+        except FileNotFoundError:
+            raise NotFound("{} item not found".format(self.name))
+        except FileExistsError:
+            raise Conflict("{} item already exists".format(self.name))
+
+    def _read(self, file):
+        file.seek(0)
+        try:
+            if isinstance(self.schema, s.dict):
+                return self.schema.json_decode(json.load(file))
+            else:
+                result = file.read()
+                self.schema.validate(result)
+                return result
+        except ValueError:
+            raise InternalServerError("malformed document")
+        except s.SchemaError:
+            raise InternalServerError("document failed schema validation")
+
+    def _write(self, file, body, id):
+        file.seek(0)
+        file.truncate()
+        if isinstance(self.schema, s.dict) and self.id_property in self.schema.properties:
+            if self.id_property and self.id_property in self.schema.properties:
+                body = {**body, self.id_property: id}
+            json.dump(self.schema.json_encode(body), file, separators=(",",":"), ensure_ascii=False)
+        else:
+            file.write(body)    
+
+    def __init__(self, dir, name=None, description=None, *, schema=None, extension=None, id_property=None):
+        """
+        Initialize file resource.
+
+        :param name: Short name of the resource. (default: class name in lower case)
+        :param description: Short description of the resource. (default: resource docstring)
+        :param dir: Directory to store resource items in.
+        :param schema: Item schema. Can declare as class or instance variable.
+        :param extenson: Filename extension to use for each file (including dot).
+        :param id_property: Name of resource identifier property in document schema. (default: "id")
+        """
+        super().__init__()
+        self.schema = schema or self.schema
+        self.id_property = id_property or getattr(self, "id_property", "id")
+        if not isinstance(self.schema, s.dict) or self.id_property not in self.schema.properties:
+            self.id_property = None
+        self.id_schema = self.schema.properties[self.id_property] if self.id_property else s.str()
+        self.dir = expanduser(dir.rstrip("/"))
+        os.makedirs(self.dir, exist_ok=True)
+        self.extension = extension or getattr(self, "extension", "")
+        self.__doc__ = self.schema.description
+
+    def create(self, id, _body):
+        """Create a resource item."""
+        try:
+            with self._open(id, "x") as file:
+                self._write(file, _body, id)
+        except NotFound:
+            raise InternalServerError("file resource directory not found")
+        return {"id": id}
+
+    def read(self, id):
+        """Read a resource item."""
+        with self._open(id, "r") as file:
+            body = self._read(file)
+        if isinstance(self.schema, s.dict) and self.id_property in self.schema.properties:
+            body[self.id_property] = id  # filename is canonical identifier
+        return body
+
+    def update(self, id, _body):
+        """Update a resource item."""
+        with self._open(id, "r+") as file:
+            self._write(file, _body, id)
+
+    def delete(self, id):
+        """Delete a resource item."""
+        try:
+            os.remove(self._filename(id))
+        except FileNotFoundError:
+            raise NotFound("{} item not found".format(self.name))
+
+    def list(self):
+        """Return a list of all resource item identifiers."""
+        result = []
+        for name in os.listdir(self.dir):
+            if name.endswith(self.extension):
+                name = name[:len(name) - len(self.extension)]
+                str_id = _unquote(name)
+                if name != _quote(str_id):  # ignore improperly encoded names
+                    continue
+                try:
+                    result.append(self.id_schema.str_decode(str_id))
+                except s.SchemaError:
+                    pass  # ignore filenames that can't be parsed
+        return result
---
+++ packages/roax/roax-1.0b21.tar.gz/roax-1.0b21/src/roax/wsgi.py
@@ -0,0 +1,194 @@
+"""Module to expose resources through a WSGI interface."""
+
+# Copyright © 2015–2018 Paul Bryan.
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+
+import logging
+import re
+import roax.resource as resource
+import roax.context as context
+
+from copy import copy
+from roax.schema import SchemaError
+from urllib.parse import urlparse
+from webob import Request, Response, exc
+
+
+class _ErrorResponse(Response):
+
+    def __init__(self, code, message):
+        super().__init__()
+        self.status_code = code
+        self.content_type = "application/json"
+        self.json = {"error": code, "message": message}
+
+
+def _response(operation, result):
+    returns = operation.returns
+    response = Response()
+    if returns and result is not None:
+        response.content_type = returns.content_type
+        if returns.json_type == "string" and returns.format == "binary":
+            response.body = result
+        elif returns.json_type == "string" and returns.format == "raw":
+            response.text = result
+        else:
+            response.json = returns.json_encode(result)
+    else:
+        response.status_code = exc.HTTPNoContent.code
+        response.content_type = None
+    return response
+
+
+def _params(request, operation):
+    result = {}
+    params = operation.params
+    for name, param in params.items() if params else []:
+        try:
+            if name == "_body":
+                if param.required and not request.is_body_readable:
+                    raise SchemaError("missing request entity-body")
+                if param.json_type == "string" and param.format == "binary":
+                    result["_body"] = param.bin_decode(request.body)
+                elif param.json_type == "string" and param.format == "raw":
+                    result["_body"] = param.str_decode(request.text)
+                else:
+                    try:
+                        json = request.json
+                    except:
+                        raise SchemaError("invalid JSON in entity-body")
+                    result["_body"] = param.json_decode(json)
+            else:
+                result[name] = param.str_decode(request.params[name])
+        except KeyError:
+            pass
+    return result
+
+def _filters(requirements):
+    """Produce set of filters associated with the security requirements."""
+    result = set()
+    for requirement in requirements or []:
+        scheme = getattr(requirement, "scheme", None)
+        if scheme:
+            filter = getattr(scheme, "filter", None)
+            if filter:
+                result.add(filter)
+    return result
+
+
+_context_patterns = [re.compile(p) for p in (
+    "^REQUEST_METHOD$", "^SCRIPT_NAME$", "^PATH_INFO$", "^QUERY_STRING$",
+    "^CONTENT_TYPE$", "^CONTENT_LENGTH$", "^SERVER_NAME$", "^SERVER_PORT$",
+    "^SERVER_PROTOCOL$", "^HTTP_.*", "^wsgi.version$", "^wsgi.multithread$",
+    "^wsgi.multiprocess$", "^wsgi.run_once$")]
+
+def _environ(environ):
+    result = {}
+    for key, value in environ.items():
+        for pattern in _context_patterns:
+            if pattern.match(key):
+                result[key] = value
+    return result
+
+
+
+class App:
+    """Roax WSGI application."""
+
+    def __init__(self, url, title, version, description=None):
+        """
+        Initialize WSGI application.
+        
+        :param url: The URL to access the application.
+        :param title: The title of the application.
+        :param version: The API implementation version.
+        :param description: A short description of the application.
+        """
+        self.url = url
+        self.base = urlparse(self.url).path.rstrip("/")
+        self.title = title
+        self.description = description
+        self.version = version
+        self.operations = {}
+
+    def register(self, path, resource, publish=True):
+        """
+        Register a resource to the application.
+
+        :param path: Path of resource relative to application URL.
+        :param resource: Resource to be served when path is requested.
+        :param publish: Publish resource in online documentation.
+        """
+        resource_path = self.base + "/" + path.lstrip("/").rstrip("/")
+        for op in resource.operations.values():
+            if op.security is None:
+                raise ValueError("operation {} must define security requirements".format(op.name))
+            if op.type == "create":
+                op_method, op_path = "POST", resource_path
+            elif op.type == "read":
+                op_method, op_path = "GET", resource_path
+            elif op.type == "update":
+                op_method, op_path = "PUT", resource_path
+            elif op.type == "delete":
+                op_method, op_path = "DELETE", resource_path
+            elif op.type == "action":
+                op_method, op_path = "POST", resource_path + "/" + op.name
+            elif op.type == "query":
+                op_method, op_path = "GET", resource_path + "/" + op.name
+            else:
+                raise ValueError("operation {} has unknown operation type: {}".format(op.name, op.type))
+            if not publish:
+                op = copy(op)
+                op.publish = False
+            if (op_method, op_path) in self.operations:
+                raise ValueError("operation already defined for {} {}".format(op_method, op_path))
+            self.operations[(op_method, op_path)] = op
+
+    def __call__(self, environ, start_response):
+        """Handle WSGI request."""
+        request = Request(environ)
+        try:
+            with context.push(context_type="http", http_environ=_environ(environ)):
+                operation = self._get_operation(request)
+                def handle(request):
+                    try:
+                        params = _params(request, operation)
+                    except Exception:  # authorization trumps input validation
+                        resource.authorize(operation.security)
+                        raise
+                    return _response(operation, operation.function(**params))
+                response = Chain(_filters(operation.security), handle).next(request)
+        except exc.HTTPException as he:
+            response = _ErrorResponse(he.code, he.detail)
+        except resource.ResourceError as re:
+            response = _ErrorResponse(re.code, re.detail)
+        except SchemaError as se:
+            response = _ErrorResponse(exc.HTTPBadRequest.code, str(se))
+        except Exception as e:
+            logging.exception(str(e))
+            response = _ErrorResponse(exc.HTTPInternalServerError.code, str(e))
+        return response(environ, start_response)
+
+    def _get_operation(self, request):
+        operation = self.operations.get((request.method, request.path_info))
+        if operation:
+            return operation
+        for _, op_path in self.operations:
+            if op_path == request.path_info:  # path is defined, but not method
+                raise exc.MethodNotAllowed()
+        raise exc.HTTPNotFound("resource or operation not found")
+
+
+class Chain:
+    """A chain of filters, terminated by a handler."""
+
+    def __init__(self, filters=[], terminus=None):
+        """Initialize a filter chain."""
+        self.filters = list(filters)
+        self.terminus = terminus
+
+    def next(self, request):
+        """Calls the next filter in the chain, or the terminus."""
+        return self.filters.pop(0)(request, self) if self.filters else self.terminus(request)
