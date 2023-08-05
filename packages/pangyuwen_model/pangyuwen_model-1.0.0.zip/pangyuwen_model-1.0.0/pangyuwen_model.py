09ac27d401e3b09db0ec9523e65e077024ef6c67
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:09:13 +0000

    Add package pangyuwen_model/1.0.0/pangyuwen_model-1.0.0.zip

---
+++ packages/pangyuwen_model/pangyuwen_model-1.0.0.zip/pangyuwen_model-1.0.0/pangyuwen_model.py
@@ -0,0 +1,11 @@
+'''This is a mopdel.py model,provide a function,named as print_lol(the_list),used to print list,which may very complex.'''
+
+def model(the_list):
+
+    '''This is a funciton needs a input,the type is python list.
+The use of this function is to print each itmes in the iuput list.'''
+    for each_item in the_list:
+        if isinstance(each_item,list):
+            print_lol(each_item)
+        else:
+            print(each_item)
---
+++ packages/pangyuwen_model/pangyuwen_model-1.0.0.zip/pangyuwen_model-1.0.0/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+setup(
+    name='pangyuwen_model',
+    version='1.0.0',
+    py_modules=['pangyuwen_model'],
+    author='pangyuwen',
+    author_email='tzpangyuwen@gmail.com',
+    description='A smiple printer of nested lists',
+    )
