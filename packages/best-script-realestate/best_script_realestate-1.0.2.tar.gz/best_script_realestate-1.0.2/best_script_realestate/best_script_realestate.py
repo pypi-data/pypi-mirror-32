23b49b5245fa082a289bbec017352b2757640d6d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:11:49 +0000

    Add package best-script-realestate/1.0.2/best_script_realestate-1.0.2.tar.gz

---
+++ packages/best-script-realestate/best_script_realestate-1.0.2.tar.gz/best_script_realestate-1.0.2/best_script_realestate/best_script_realestate.py
@@ -0,0 +1,23 @@
+# the inclusion of the tests module is not meant to offer best practices for
+# testing in general, but rather to support the `find_packages` example in
+# setup.py that excludes installing the "tests" package
+
+
+class BESTSCRIPT:
+
+    def __init__(self):
+        pass
+
+    class Start:
+
+        def __init__(self, text):
+            print('WARNING!!!')
+            print('Your request: {}'.format(text))
+            print('Download full version from https://github.com/reactpython/best_script_for_agencies')
+
+
+
+if __name__ == "__main__":
+    pass
+
+    # BESTSCRIPT.Start('Test')
