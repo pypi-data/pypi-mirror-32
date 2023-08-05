6463d8675b44b25597acac034240f23cd77fc886
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:04 +0000

    Add package freya/0.0.1/freya-0.0.1.tar.gz

---
+++ packages/freya/freya-0.0.1.tar.gz/freya-0.0.1/setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+setup(
+    name='freya',
+    version='0.0.1',
+    packages=find_packages(),
+    requires=['python (>= 3.6)'],
+    description='',
+    long_description=open('README.md').read(),
+    author='Ingvar Vilkman',
+    author_email='zeegin@zeegin.com',
+    url='https://github.com/zeegin/freya',
+    download_url='https://github.com/zeegin/freya',
+    license='MIT License',
+    keywords='',
+    classifiers=[
+        'Programming Language :: Python',
+    ],
+)
