ffbf9e226a990016e02ad3d53b64d71fcb175f94
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:04 +0000

    Add package simtables/0.0.1/simtables-0.0.1.tar.gz

---
+++ packages/simtables/simtables-0.0.1.tar.gz/simtables-0.0.1/README.md
@@ -0,0 +1,1 @@
+# Simple Tables
---
+++ packages/simtables/simtables-0.0.1.tar.gz/simtables-0.0.1/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='simtables',
+    version='0.0.1',
+    author='Alexander Chambers',
+    author_email='alexander.chambers8472@gmail.com',
+    description='Simple tables',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/achambers8472/simtables',
+    packages=setuptools.find_packages(),
+    classifiers=(
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ),
+    # setup_requires=['pytest-runner'],
+    # tests_require=['pytest'],
+    # test_suite='tests',
+)
