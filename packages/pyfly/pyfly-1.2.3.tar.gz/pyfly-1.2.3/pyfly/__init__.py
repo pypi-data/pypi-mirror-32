3d7136e13222f495c5a7bf238e4827e9b9da720a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:33:29 +0000

    Add package pyfly/1.2.3/pyfly-1.2.3.tar.gz

---
+++ packages/pyfly/pyfly-1.2.3.tar.gz/pyfly-1.2.3/pyfly/__init__.py
@@ -0,0 +1,11 @@
+from .core import task, TaskDataQueue, TaskSet, FlyQueen, async_task, sleep
+from .runners import FlyMaster, FlySlave
+from .events import task_success, task_fail, auto_submit_task
+import traceback
+try:
+    from .web import WebApp
+except:
+    print(traceback.format_exc())
+    print("You should to install dash, if you want to run master on this pc!")
+
+__version__ = "1.2.3"
