c0f4e7dc4eb4a490ffeca4aed187865608949e4f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:32 +0000

    Add package mammoth-analytics-sdk/1.0.2b2/mammoth_analytics_sdk-1.0.2b2-py2-none-any.whl

---
+++ packages/mammoth-analytics-sdk/mammoth_analytics_sdk-1.0.2b2-py2-none-any.whl/MammothAnalytics/const.py
@@ -0,0 +1,13 @@
+from __future__ import unicode_literals
+import logging
+import os
+
+log = logging
+
+
+if os.environ.get('MAMMOTH_API_URL', None):
+    API = os.environ['MAMMOTH_API_URL']
+else:
+    API = 'https://app.mammoth.io/api/v1'
+
+log.warning('{0} is the api server URL'.format(API))
