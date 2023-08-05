7b9bcb2607a7c7f53224c9655d81ddc7d30bd3e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:08 +0000

    Add package microphone/0.3.1/microphone-0.3.1.tar.gz

---
+++ packages/microphone/microphone-0.3.1.tar.gz/microphone-0.3.1/README.rst
@@ -0,0 +1,21 @@
+microphone
+==========
+
+python audio recorder using pyaudio for audio capture and transmission using zeromq
+
+NOTE: Program is very much in an alpha stage! You've been warned!
+
+Installation
+------------
+
+::
+
+    pip install microphone
+
+-or- ::
+
+    pip install git+https://github.com/benhoff/microphone.git
+
+if running from source need to run the command ::
+
+   python setup.py develop
---
+++ packages/microphone/microphone-0.3.1.tar.gz/microphone-0.3.1/setup.py
@@ -0,0 +1,49 @@
+import os
+from setuptools import find_packages, setup
+
+
+# directory = os.path.abspath(os.path.dirname(__file__))
+"""
+with open(os.path.join(directory, 'README.rst')) as f:
+    long_description = f.read()
+"""
+
+setup(
+    name="microphone",
+    version='0.3.1',
+    description='Sending microphone data using ZeroMQ',
+    # long_description=long_description,
+    url='https://github.com/benhoff/microphone',
+    license='GPL3',
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Topic :: Multimedia :: Sound/Audio :: Capture/Recording',
+        'Topic :: Utilities',
+        'Operating System :: OS Independent'],
+    keywords='sound capture to text',
+    author='Ben Hoff',
+    author_email='beohoff@gmail.com',
+    package_data={'microphone':['resources/congo.wav',
+                                'resources/done.wav',
+                                'default_settings.ini',]},
+
+    entry_points={'microphone.audioengines': ['pyaudio = microphone.pyaudio_:PyAudio',],
+                  'console_scripts': ['microphone-python = microphone.__main__:main'],
+                  'vexbot.adapters': ['microphone = microphone.__main__']},
+
+    packages= find_packages(), # exclude=['docs', 'tests']
+    install_requires=[
+        'pluginmanager>=0.4.1',
+        'PyAudio',
+        'pyzmq',
+        'vexmessage',
+        ],
+
+    extras_require={
+        'dev': ['flake8']
+        },
+)
