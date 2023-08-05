6c3bf20e8c595a28d34f64af8910a7f61f90ee55
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:09:31 +0000

    Add package fc/0.0.8/fc-0.0.8.tar.gz

---
+++ packages/fc/fc-0.0.8.tar.gz/fc-0.0.8/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='fc',
+    version='0.0.8',
+    description='Enables Python to be functionally chain-styled.',
+
+    url='https://github.com/Thoxvi/Fuck-Py-Functional',
+    author='Thoxvi',
+    author_email='A@Thoxvi.com',
+
+    classifiers=[
+        'Programming Language :: Python',
+        'Programming Language :: Python :: Implementation',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Software Development :: Libraries'
+    ],
+    keywords='lambda function',
+
+    packages=find_packages(),
+)
