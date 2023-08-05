1a8871636d1cdefb69ab097cf9860b85c6d02c23
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:28 +0000

    Add package pykorbit/0.1.0/pykorbit-0.1.0.tar.gz

---
+++ packages/pykorbit/pykorbit-0.1.0.tar.gz/pykorbit-0.1.0/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name            = 'pykorbit',
+    version         = '0.1.0',
+    description     = 'python wrapper for Korbit API',
+    url             = 'https://github.com/sharebook/pykorbit',
+    author          = 'brayden.jo',
+    author_email    = 'brayden.jo@outlook.com',
+    license         = 'MIT',
+    packages        = ['pykorbit'],
+    zip_safe        = False
+)
