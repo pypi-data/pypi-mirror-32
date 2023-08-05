3e2327f7d79ceb07da7979efdccf798779a42036
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:28:38 +0000

    Add package cr8/0.12.0/cr8-0.12.0.tar.gz

---
+++ packages/cr8/cr8-0.12.0.tar.gz/cr8-0.12.0/cr8/run_track.py
@@ -0,0 +1,119 @@
+import argh
+import os
+import toml
+from glob import glob
+from .log import Logger
+from .run_spec import do_run_spec
+from .run_crate import CrateNode, get_crate
+from .clients import client_errors
+
+
+class Executor:
+    def __init__(self,
+                 *,
+                 track_dir,
+                 log,
+                 sample_mode,
+                 result_hosts=None,
+                 crate_root=None,
+                 fail_fast=None):
+        self.track_dir = track_dir
+        self.sample_mode = sample_mode
+        self.result_hosts = result_hosts
+        self.crate_root = crate_root
+        self.log = log
+        self.fail_fast = fail_fast
+
+    def _expand_paths(self, paths):
+        paths = (os.path.join(self.track_dir, path) for path in paths)
+        paths = (os.path.abspath(path) for path in paths)
+        return (p for path in paths for p in glob(path))
+
+    def _run_specs(self, specs, benchmark_host, action=None):
+        specs = self._expand_paths(specs)
+        for spec in specs:
+            self.log.info('### Running spec file: ', os.path.basename(spec))
+            try:
+                do_run_spec(
+                    spec=spec,
+                    benchmark_hosts=benchmark_host,
+                    log=self.log,
+                    result_hosts=self.result_hosts,
+                    sample_mode=self.sample_mode,
+                    action=action)
+            except Exception:
+                if self.fail_fast:
+                    raise
+                else:
+                    self.log.info('WARNING: Spec file failed due to the following exception:')
+                    import traceback
+                    traceback.print_exc()
+
+    def _execute_specs(self, specs, benchmark_host):
+        if isinstance(specs, list):
+            self._run_specs(specs, benchmark_host)
+        else:
+            fixtures = specs.get('fixtures', [])
+            queries = specs.get('queries', [])
+            full = specs.get('full', [])
+            self._run_specs(fixtures, benchmark_host, action=['setup'])
+            self._run_specs(queries, benchmark_host, action=['queries'])
+            self._run_specs(fixtures, benchmark_host, action=['teardown'])
+            self._run_specs(full, benchmark_host)
+
+    def execute(self, track):
+        configurations = list(self._expand_paths(track['configurations']))
+        versions = track['versions']
+        for version in versions:
+            self.log.info('# Version: ', version)
+            for c, configuration in enumerate(configurations):
+                self.log.info('## Starting Crate {0}, configuration: {1}'.format(
+                    os.path.basename(version),
+                    os.path.basename(configuration)
+                ))
+                configuration = toml.load(os.path.join(self.track_dir, configuration))
+                crate_dir = get_crate(version, self.crate_root)
+                with CrateNode(crate_dir=crate_dir,
+                               env=configuration.get('env'),
+                               settings=configuration.get('settings')) as node:
+                    node.start()
+                    self._execute_specs(track['specs'], node.http_url)
+
+
+@argh.arg('-r', '--result_hosts', type=str)
+@argh.arg('-of', '--output-fmt', choices=['json', 'text'], default='text')
+@argh.arg('--failfast', action='store_true')
+@argh.arg('--logfile-info', help='Redirect info messages to a file')
+@argh.arg('--logfile-result', help='Redirect benchmark results to a file')
+@argh.arg('--sample-mode', choices=('all', 'reservoir'),
+          help='Method used for sampling', default='reservoir')
+@argh.wrap_errors([KeyboardInterrupt] + client_errors)
+def run_track(track,
+              result_hosts=None,
+              crate_root=None,
+              output_fmt=None,
+              logfile_info=None,
+              logfile_result=None,
+              failfast=False,
+              sample_mode='reservoir'):
+    """Execute a track file"""
+    with Logger(output_fmt=output_fmt,
+                logfile_info=logfile_info,
+                logfile_result=logfile_result) as log:
+        executor = Executor(
+            track_dir=os.path.dirname(track),
+            log=log,
+            result_hosts=result_hosts,
+            crate_root=crate_root,
+            fail_fast=failfast,
+            sample_mode=sample_mode
+        )
+        executor.execute(toml.load(track))
+
+
+def main():
+    argh.dispatch_command(run_track)
+
+
+if __name__ == "__main__":
+    main()
---
+++ packages/cr8/cr8-0.12.0.tar.gz/cr8-0.12.0/setup.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+try:
+    with open('README.rst', 'r', encoding='utf-8') as f:
+        readme = f.read()
+except IOError:
+    readme = ''
+
+
+setup(
+    name='cr8',
+    author='Mathias Fußenegger',
+    author_email='pip@zignar.net',
+    url='https://github.com/mfussenegger/cr8',
+    description='A collection of command line tools for crate devs',
+    long_description=readme,
+    entry_points={
+        'console_scripts': [
+            'cr8 = cr8.__main__:main',
+        ]
+    },
+    packages=['cr8'],
+    install_requires=[
+        'crate>=0.16',
+        'argh',
+        'tqdm',
+        'Faker',
+        'aiohttp>=2.0,<3',
+        'toml'
+    ],
+    extras_require={
+        'uvloop': ['uvloop'],
+    },
+    python_requires='>=3.6',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+    ],
+    use_scm_version=True,
+    setup_requires=['setuptools_scm']
+)
