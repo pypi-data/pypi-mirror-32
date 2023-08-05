a0f1c3f37d5e69a3e84d6518a8c2ce2f18405989
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:31:31 +0000

    Add package anitomy.py/1.0.0/anitomy.py-1.0.0.tar.gz

---
+++ packages/anitomy.py/anitomy.py-1.0.0.tar.gz/anitomy.py-1.0.0/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+setuptools.setup(
+    name='anitomy.py',
+    packages=['anitopy'],
+    version='1.0.0',
+    description='An anime video filename parser',
+    author='Igor Cescon de Moura',
+    author_email='igorcesconm@gmail.com',
+    url='https://git.widmer.me/project/multimedia/anitopy',
+    python_requires='>=2.7',
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ]
+)
