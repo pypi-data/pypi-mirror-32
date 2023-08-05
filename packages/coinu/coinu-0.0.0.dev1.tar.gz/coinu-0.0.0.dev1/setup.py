2a6ba46d6e2ae27dd2fc9fd157dc54b23fc0dd83
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:43 +0000

    Add package coinu/0.0.0.dev1/coinu-0.0.0.dev1.tar.gz

---
+++ packages/coinu/coinu-0.0.0.dev1.tar.gz/coinu-0.0.0.dev1/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='coinu',
+    version='0.0.0.dev1',
+    packages=[
+        'coinu',
+    ],
+    url='https://github.com/coinu',
+    license='MIT',
+    author='gokoro',
+    author_email='',
+    description='COIN Utility basic package',
+    long_description='https://github.com/coinu',
+)
