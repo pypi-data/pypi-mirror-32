2c474e81261c61edbe28bc3e582891a67a3cd7f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:08:21 +0000

    Add package helloworldpackage/1.2/helloworldpackage-1.2.tar.gz

---
+++ packages/helloworldpackage/helloworldpackage-1.2.tar.gz/helloworldpackage-1.2/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+ 
+setup(
+     name='helloworldpackage',      # This is the name of your PyPI-package.    
+     version='1.2',           # Update the version number for new releases
+     scripts=['helloworld'] # The name of your package, and also the command you'll be using for calling it
+ )
