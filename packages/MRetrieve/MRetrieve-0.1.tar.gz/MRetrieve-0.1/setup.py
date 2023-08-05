84ea13b7c1bf4031d7b1db9774c44f8370089e06
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:10:38 +0000

    Add package MRetrieve/0.1/MRetrieve-0.1.tar.gz

---
+++ packages/MRetrieve/MRetrieve-0.1.tar.gz/MRetrieve-0.1/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+
+setup(
+    name="MRetrieve",
+    version="0.1",
+    description="Retrieval library for manga websites",
+    author="Zedoax",
+    author_email="zedoax@anagrio.net",
+    url="https://www.github.com/MRetrieve",
+    install_requires=[
+        'bs4',
+        'datetime'
+    ]
+)
