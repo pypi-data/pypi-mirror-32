7b853783399a9dc13cab6d7b75a847b3d85347f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:08:26 +0000

    Add package gluoncv/0.2.0b20180529/gluoncv-0.2.0b20180529.tar.gz

---
+++ packages/gluoncv/gluoncv-0.2.0b20180529.tar.gz/gluoncv-0.2.0b20180529/gluoncv/data/__init__.py
@@ -0,0 +1,13 @@
+"""
+This module provides data loaders and transfomers for popular vision datasets.
+"""
+from . import transforms
+from .imagenet.classification import ImageNet
+from .dataloader import DetectionDataLoader
+from .pascal_voc.detection import VOCDetection
+from .mscoco.detection import COCODetection
+from .pascal_voc.segmentation import VOCSegmentation
+from .pascal_aug.segmentation import VOCAugSegmentation
+from .ade20k.segmentation import ADE20KSegmentation
+from .segbase import get_segmentation_dataset, test_batchify_fn
+from .recordio.detection import RecordFileDetection
