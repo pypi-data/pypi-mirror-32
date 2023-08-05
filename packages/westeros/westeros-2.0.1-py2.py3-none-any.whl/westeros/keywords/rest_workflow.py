911bb2e417c1519ed9811b25aec32dc1a6d49244
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:01 +0000

    Add package westeros/2.0.1/westeros-2.0.1-py2.py3-none-any.whl

---
+++ packages/westeros/westeros-2.0.1-py2.py3-none-any.whl/westeros/keywords/rest_workflow.py
@@ -0,0 +1,10 @@
+import os
+
+from robot.api import logger
+
+from westeros.auc.rest.manager import RestManager
+from .base_workflow import BaseWorkflow
+
+
+class RestWorkflow(BaseWorkflow):
+    pass
