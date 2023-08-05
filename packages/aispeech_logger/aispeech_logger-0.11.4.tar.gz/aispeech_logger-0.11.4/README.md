9d024f8f993dd2500a7099421ed9480d68f93e0f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:08 +0000

    Add package aispeech_logger/0.11.4/aispeech_logger-0.11.4.tar.gz

---
+++ packages/aispeech_logger/aispeech_logger-0.11.4.tar.gz/aispeech_logger-0.11.4/README.md
@@ -0,0 +1,5 @@
+运行  sh run.sh
+
+环境依赖： pytz(处理时区)
+环境依赖： py_zipkin(链路追踪)
+环境依赖： kafka(写入日志到kafka)
---
+++ packages/aispeech_logger/aispeech_logger-0.11.4.tar.gz/aispeech_logger-0.11.4/setup.py
@@ -0,0 +1,33 @@
+# -*- coding: UTF-8 -*-
+"""
+File Name :    setup.py
+Author :       unasm
+mail :         unasm@sina.cn
+Last_Modified: 2018-05-16 20:43:11
+"""
+
+
+from distutils.core import setup
+from setuptools import setup, find_packages
+
+__version__ = '0.11.4'
+
+setup(
+    name='aispeech_logger',
+    version=__version__,
+    keywords=('aispeech'),
+    description='test for balogger',
+    license='MIT License',
+    author='jiamin.dou',
+    author_email='jiamin.dou@aispeech.com',
+    packages=["aispeech_logger"],
+    #packages=find_packages(),
+    platforms='any',
+    url='http://www.unasm.com',      # 包的主页
+    install_requires=[
+        "kafka",
+        "py_zipkin",
+        "pytz",
+        "uuid",
+    ]
+)
