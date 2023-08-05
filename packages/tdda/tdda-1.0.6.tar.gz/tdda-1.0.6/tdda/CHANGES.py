924620b2e60e1f6cea01cb55a602b79370a940fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:11:06 +0000

    Add package tdda/1.0.6/tdda-1.0.6.tar.gz

---
+++ packages/tdda/tdda-1.0.6.tar.gz/tdda-1.0.6/tdda/CHANGES.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+from __future__ import division
+from __future__ import print_function
+from __future__ import absolute_import
+"""1.0.02
+Improved documentation to use shorter import forms.
+
+Also bumped version number.
+
+Also planning to tag this with a version to see whether that encourages
+readthedocs to add the version properly.
+
+29.05.2018 1.0.03
+Add pushv (for maintainer use only) and this CHANGES.py file.
+
+29.05.2018 1.0.04
+Correctly cast strings to native strings in check_shell_output.
+
+29.05.2018 1.0.05
+Tests for feather are now ignored if pmmif/feather are not installed.
+
+29.05.2018 1.0.06
+Windows fixes.
+"""
