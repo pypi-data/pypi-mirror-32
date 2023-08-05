6efd457115294cfa314af6dc46ec81dc3059e715
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:10 +0000

    Add package romanize3/0.1.14/romanize3-0.1.14.tar.gz

---
+++ packages/romanize3/romanize3-0.1.14.tar.gz/romanize3-0.1.14/romanize/main.py
@@ -0,0 +1,19 @@
+#!/usr/local/bin/python
+# -*- coding: utf-8 -*-
+# file: main.py
+
+"""
+ara, arabic
+cop, coptic
+eng, english
+grc, greek
+heb, hebrew
+san, sanskrit
+arm, aramaic
+fin, finnish
+brh, brahmi
+phn, phoenician
+syc, syriaic
+"""
+
+from . import ara, cop, eng, grc, heb, san, arm, fin, brh, phn, syc
