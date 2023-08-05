2f32e64676f28316b7e8dba7bd4218354cf8bdc9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:35 +0000

    Add package msm/0.5.12/msm-0.5.12.tar.gz

---
+++ packages/msm/msm-0.5.12.tar.gz/msm-0.5.12/msm/__init__.py
@@ -0,0 +1,4 @@
+from .exceptions import *
+from .mycroft_skills_manager import MycroftSkillsManager
+from .skill_entry import SkillEntry
+from .skill_repo import SkillRepo
---
+++ packages/msm/msm-0.5.12.tar.gz/msm-0.5.12/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+    name='msm',
+    version='0.5.12',
+    packages=['msm'],
+    install_requires=['GitPython', 'typing'],
+    url='https://github.com/MycroftAI/mycroft-skills-manager',
+    license='MIT',
+    author='jarbasAI, Matthew Scholefield',
+    author_email='jarbasai@mailfence.com, matthew331199@gmail.com',
+    description='Mycroft Skills Manager',
+    entry_points={
+        'console_scripts': {
+            'msm=msm.__main__:main'
+        }
+    }
+)
