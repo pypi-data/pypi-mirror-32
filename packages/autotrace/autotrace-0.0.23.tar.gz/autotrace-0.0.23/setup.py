e1c873299cdd0d922550f3c129146762c8a2a6d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:08 +0000

    Add package autotrace/0.0.23/autotrace-0.0.23.tar.gz

---
+++ packages/autotrace/autotrace-0.0.23.tar.gz/autotrace-0.0.23/setup.py
@@ -0,0 +1,21 @@
+# SEE https://raw.githubusercontent.com/pypa/sampleproject/master/setup.py
+from setuptools import setup, find_packages
+from codecs import open
+from os import path
+here = path.abspath(path.dirname(__file__))
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+setup(
+    name='autotrace',
+ version='0.0.23',
+    description='Telemetry on any command',
+    author='Ian Miell',
+    author_email='ian.miell@gmail.com',
+    packages=find_packages(exclude=[]),
+    install_requires=['curtsies','pexpect','dateparser'],
+    entry_points={
+        'console_scripts': [
+            'autotrace=autotrace.autotrace:main',
+        ],
+    },
+)
