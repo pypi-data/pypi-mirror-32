7065cc815e5c519882a23115289dc8b7480dd2c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:11:26 +0000

    Add package muacrypt/0.8.2/muacrypt-0.8.2.tar.gz

---
+++ packages/muacrypt/muacrypt-0.8.2.tar.gz/muacrypt-0.8.2/muacrypt/myattr.py
@@ -0,0 +1,32 @@
+"""
+
+helper which exposes the https://attrs.org library and some
+type validation helpers.
+
+"""
+import attr  # noqa
+import six
+from attr import attrs, attrib  # noqa
+from attr import validators as v
+
+
+def attrib_float():
+    return attrib(validator=v.instance_of(float))
+
+
+def attrib_text():
+    return attrib(validator=v.instance_of(six.text_type))
+
+
+def attrib_bytes():
+    return attrib(validator=v.instance_of(bytes))
+
+
+def attrib_text_or_none():
+    return attrib(validator=v.optional(v.instance_of(six.text_type)),
+                  default=None)
+
+
+def attrib_bytes_or_none():
+    return attrib(validator=v.optional(v.instance_of(bytes)),
+                  default=None)
