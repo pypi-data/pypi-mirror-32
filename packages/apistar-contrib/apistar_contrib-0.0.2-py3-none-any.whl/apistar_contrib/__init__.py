39850b2964521f28d217b3294baf5a78423b48ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:36 +0000

    Add package apistar-contrib/0.0.2/apistar_contrib-0.0.2-py3-none-any.whl

---
+++ packages/apistar-contrib/apistar_contrib-0.0.2-py3-none-any.whl/apistar_contrib/__init__.py
@@ -0,0 +1,17 @@
+"""API Star Contrib."""
+
+__author__ = """Ryan Anguiano"""
+__email__ = 'ryan.anguiano@gmail.com'
+__version__ = '0.0.2'
+
+
+from apistar_contrib.sessions import SessionComponent
+
+commands = [
+
+]
+
+components = [
+    SessionComponent(),
+]
+
