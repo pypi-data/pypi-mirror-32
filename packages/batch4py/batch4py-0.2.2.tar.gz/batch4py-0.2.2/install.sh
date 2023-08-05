2062ee45f10b0ab1386ab767a1f0d38d1ee92d77
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:12:05 +0000

    Add package batch4py/0.2.2/batch4py-0.2.2.tar.gz

---
+++ packages/batch4py/batch4py-0.2.2.tar.gz/batch4py-0.2.2/install.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+
+echo "testing batch4py importability..."
+python -c "import batch4py"
+retval=$?
+echo "done"
+if [ $retval -ne 0 ]; then
+    exit $retval
+fi
+
+echo "Performing batch4py test suite..."
+sudo python tests/simple.py
+retval=$?
+echo "Done"
+
+if [ $retval -ne 0 ]; then
+    exit $retval
+fi
