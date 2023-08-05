c7e768610b355a32d9ed763f376a965130813300
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:09:17 +0000

    Add package djangorestframework-sso/0.0.10/djangorestframework-sso-0.0.10.tar.gz

---
+++ packages/djangorestframework-sso/djangorestframework-sso-0.0.10.tar.gz/djangorestframework-sso-0.0.10/setup.py
@@ -0,0 +1,57 @@
+# coding: utf-8
+import os
+from setuptools import find_packages, setup
+
+
+def get_install_requires():
+    """
+    parse requirements.txt, ignore links, exclude comments
+    """
+    requirements = []
+    for line in open('requirements.txt').readlines():
+        # skip to next iteration if comment or empty line
+        if line.startswith('#') or line == '' or line.startswith('http') or line.startswith('git'):
+            continue
+        # add line to requirements
+        requirements.append(line)
+    return requirements
+
+
+with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
+    README = readme.read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+
+setup(
+    name='djangorestframework-sso',
+    version='0.0.10',
+    packages=find_packages(exclude=['tests']),
+    include_package_data=True,
+    license='MIT License',
+    description='Single sign-on extension to the Django REST Framework.',
+    long_description=README,
+    url='https://github.com/namespace-ee/django-rest-framework-sso',
+    author='Lenno Nagel',
+    author_email='lenno@namespace.ee',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Framework :: Django :: 1.9',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Session',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+    install_requires=get_install_requires(),
+)
