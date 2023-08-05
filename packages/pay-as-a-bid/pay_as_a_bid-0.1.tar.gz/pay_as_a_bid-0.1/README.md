5bbc99ebe4d2b8ca3ba640a0e654a4f0537bde39
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:20 +0000

    Add package pay-as-a-bid/0.1/pay_as_a_bid-0.1.tar.gz

---
+++ packages/pay-as-a-bid/pay_as_a_bid-0.1.tar.gz/pay_as_a_bid-0.1/README.md
@@ -0,0 +1,3 @@
+# Example Package
+
+This is a package about simulation pay as a bid electricity auction.
---
+++ packages/pay-as-a-bid/pay_as_a_bid-0.1.tar.gz/pay_as_a_bid-0.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(name='pay_as_a_bid',
+      version='0.1',
+      description='pay as a bid for electricity auction model',
+      url='',
+      author='Amirhosein Shirani',
+      author_email='amirhhh.shirani@yahoo.com',
+      license='Shatif University of Technology',
+      packages=['pay_as_a_bid'],
+      zip_safe=False)
