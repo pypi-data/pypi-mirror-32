1f8bcf77736bb5f73c0d3477f1773523cc926cc0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:12 +0000

    Add package ltst-midge/0.0.3/ltst_midge-0.0.3.tar.gz

---
+++ packages/ltst-midge/ltst_midge-0.0.3.tar.gz/ltst_midge-0.0.3/setup.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+
+import os
+import sys
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+if sys.argv[-1] == 'publish':
+    os.system('python setup.py sdist upload')
+    sys.exit()
+
+packages = [
+    'midge'
+]
+
+with open('README.md') as f:
+    readme = f.read()
+# with open('LICENSE.txt') as f:
+#     license = f.read()
+
+setup(
+    name='ltst_midge',
+    version='0.0.3',
+    author='midge',
+    description='midge',
+    long_description=readme,
+    license='MIT',
+    packages=packages,
+    include_package_data=True,
+    zip_safe=False,
+    classifiers=[
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 3 - Alpha',
+
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+    ],
+)
