60a0eb412256865811fc9004b6dc77fa0f9fedc4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:10 +0000

    Add package TroxPackage/0.5/TroxPackage-0.5-py3-none-any.whl

---
+++ packages/TroxPackage/TroxPackage-0.5-py3-none-any.whl/TroxPackage/__init__.py
@@ -0,0 +1,38 @@
+import sqlite3
+name = "TroxPackage"
+
+def showParkings():
+    conn = sqlite3.connect("ParkingDB.sqlite")
+    c = conn.cursor()
+    c.execute('SELECT * FROM Parking')
+    for row in c:
+        print(row)
+    conn.close()
+
+def addParking(cap, ava, restrictions):
+    conn = sqlite3.connect("ParkingDB.sqlite")
+    c = conn.cursor()
+    newParking = (cap, ava, restrictions)
+    with conn:
+        c.execute('''INSERT INTO Parking(capacity, availability, restrictions) VALUES(?,?,?)''', newParking)
+    conn.close()
+
+def isUserOnParking(userID):
+    conn = sqlite3.connect("ParkingDB.sqlite")
+    c = conn.cursor()
+    with conn:
+        c.execute('''SELECT ParkingID FROM User WHERE iD = ?''', str(userID))
+    id = c.fetchone()[0]
+    if id == -1:
+        print("User outside of parking")
+    else:
+        print("User on parking number :", id)
+
+    conn.close()
+
+
+isUserOnParking(6)
+
+
+
+
