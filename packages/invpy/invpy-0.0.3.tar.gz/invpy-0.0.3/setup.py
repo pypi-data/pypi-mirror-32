cb4fda552023d8169b8ba17803715f7a0286a63b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:32 +0000

    Add package invpy/0.0.3/invpy-0.0.3.tar.gz

---
+++ packages/invpy/invpy-0.0.3.tar.gz/invpy-0.0.3/setup.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May 16 13:02:16 2018
+
+@author: mai1346
+"""
+
+from setuptools import setup
+
+setup(
+  name = 'invpy',
+  packages = ['invpy'], 
+  version = '0.0.3',
+  description = 'An experimental financial data scraper.',
+  author = 'Haoyuan Mai',
+  author_email = 'maihaoyuan@gmail.com',
+  url = 'https://github.com/mai1346/invpy',
+  install_requires= ['requests','pandas','beautifulsoup4','numpy']
+)
+
