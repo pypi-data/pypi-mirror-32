2124f60f8fda9057d87c14a777e3f2cfefb19bb2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:10:11 +0000

    Add package mellori/0.0.1.dev1/mellori-0.0.1.dev1.tar.gz

---
+++ packages/mellori/mellori-0.0.1.dev1.tar.gz/mellori-0.0.1.dev1/setup.py
@@ -0,0 +1,13 @@
+import setuptools
+
+setuptools.setup(
+    name="mellori",
+    version="0.0.1dev1",
+    author="Lodek",
+    author_email="lodek@protonmail.com",
+    description="Mellori",
+    long_description='',
+    long_description_content_type="text/markdown",
+    url="https://github.com/lodek/mellori",
+    packages=setuptools.find_packages(),
+)
