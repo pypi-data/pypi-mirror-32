ae36eae9ed5a52afec09aba553ec5e1a8d48d755
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:10:42 +0000

    Add package aquitania/0.0.4.12/aquitania-0.0.4.12-py2.py3-none-any.whl

---
+++ packages/aquitania/aquitania-0.0.4.12-py2.py3-none-any.whl/aquitania/brains/is_oos_split/abstract_is_oos_split.py
@@ -0,0 +1,36 @@
+########################################################################################################################
+# |||||||||||||||||||||||||||||||||||||||||||||||||| AQUITANIA ||||||||||||||||||||||||||||||||||||||||||||||||||||||| #
+# |||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||| #
+# |||| To be a thinker means to go by the factual evidence of a case, not by the judgment of others |||||||||||||||||| #
+# |||| As there is no group stomach to digest collectively, there is no group mind to think collectively. |||||||||||| #
+# |||| Each man must accept responsibility for his own life, each must be sovereign by his own judgment. ||||||||||||| #
+# |||| If a man believes a claim to be true, then he must hold to this belief even though society opposes him. ||||||| #
+# |||| Not only know what you want, but be willing to break all established conventions to accomplish it. |||||||||||| #
+# |||| The merit of a design is the only credential that you require. |||||||||||||||||||||||||||||||||||||||||||||||| #
+# |||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||| #
+########################################################################################################################
+
+"""
+.. moduleauthor:: H Roark
+
+28/05/2018 - Abstract class created around 17/02/2018, I need to check if it is useful.
+"""
+
+
+import abc
+
+
+class SplitABC:
+    """
+    Abstract class to split between train and test sets (maybe validation in the future as well).
+
+    Only current functionality is to force the implementation of output method.
+    """
+    __metaclass__ = abc.ABCMeta
+
+    def __init__(self):
+        pass
+
+    @abc.abstractmethod
+    def output(self, X, y):
+        pass
