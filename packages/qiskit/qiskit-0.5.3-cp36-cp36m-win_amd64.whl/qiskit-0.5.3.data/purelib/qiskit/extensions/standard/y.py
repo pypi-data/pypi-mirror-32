41f0afaaa8e8023f4acfd8535c145ebdd6d8f4b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:08:52 +0000

    Add package qiskit/0.5.3/qiskit-0.5.3-cp36-cp36m-win_amd64.whl

---
+++ packages/qiskit/qiskit-0.5.3-cp36-cp36m-win_amd64.whl/qiskit-0.5.3.data/purelib/qiskit/extensions/standard/y.py
@@ -0,0 +1,64 @@
+# -*- coding: utf-8 -*-
+# pylint: disable=invalid-name
+
+# Copyright 2017 IBM RESEARCH. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# =============================================================================
+
+"""
+Pauli Y (bit-phase-flip) gate.
+"""
+from qiskit import CompositeGate
+from qiskit import Gate
+from qiskit import InstructionSet
+from qiskit import QuantumCircuit
+from qiskit import QuantumRegister
+from qiskit.extensions.standard import header  # pylint: disable=unused-import
+
+
+class YGate(Gate):
+    """Pauli Y (bit-phase-flip) gate."""
+
+    def __init__(self, qubit, circ=None):
+        """Create new Y gate."""
+        super().__init__("y", [], [qubit], circ)
+
+    def qasm(self):
+        """Return OPENQASM string."""
+        qubit = self.arg[0]
+        return self._qasmif("y %s[%d];" % (qubit[0].name, qubit[1]))
+
+    def inverse(self):
+        """Invert this gate."""
+        return self  # self-inverse
+
+    def reapply(self, circuit):
+        """Reapply this gate to corresponding qubits in circ."""
+        self._modifiers(circuit.y(self.arg[0]))
+
+
+def y(self, q):
+    """Apply Y to q."""
+    if isinstance(q, QuantumRegister):
+        instructions = InstructionSet()
+        for j in range(q.size):
+            instructions.add(self.y((q, j)))
+        return instructions
+
+    self._check_qubit(q)
+    return self._attach(YGate(q, self))
+
+
+QuantumCircuit.y = y
+CompositeGate.y = y
---
+++ packages/qiskit/qiskit-0.5.3-cp36-cp36m-win_amd64.whl/qiskit-0.5.3.data/purelib/qiskit/wrapper/_wrapper.py
@@ -0,0 +1,261 @@
+# -*- coding: utf-8 -*-
+
+# Copyright 2018 IBM RESEARCH. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# =============================================================================
+"""Helper module for simplified QISKit usage."""
+
+import os
+import qiskit._compiler
+from qiskit import QISKitError
+from qiskit.backends.ibmq.ibmqprovider import IBMQProvider
+from qiskit.wrapper.defaultqiskitprovider import DefaultQISKitProvider
+from qiskit import QuantumJob
+from qiskit.qasm import Qasm
+from qiskit.unroll import Unroller, CircuitBackend
+
+
+# Default provider used by the rest of the functions on this module. Please
+# note that this is a global object.
+_DEFAULT_PROVIDER = DefaultQISKitProvider()
+
+
+def register(token, url='https://quantumexperience.ng.bluemix.net/api',
+             hub=None, group=None, project=None, proxies=None, verify=True,
+             provider_name='ibmq'):
+    """
+    Authenticate against an online backend provider.
+
+    Args:
+            token (str): The token used to register on the online backend such
+                as the quantum experience.
+            url (str): The url used for online backend such as the quantum
+                experience.
+            hub (str): The hub used for online backend.
+            group (str): The group used for online backend.
+            project (str): The project used for online backend.
+            proxies (dict): Proxy configuration for the API, as a dict with
+                'urls' and credential keys.
+            verify (bool): If False, ignores SSL certificates errors.
+            provider_name (str): the unique name for the online backend
+                provider (for example, 'ibmq' for the IBM Quantum Experience).
+    Raises:
+        QISKitError: if the provider name is not recognized.
+    """
+    if provider_name == 'ibmq':
+        provider = IBMQProvider(token, url,
+                                hub, group, project, proxies, verify)
+        _DEFAULT_PROVIDER.add_provider(provider)
+    else:
+        raise QISKitError('provider name %s is not recognized' % provider_name)
+
+
+# Functions for inspecting and retrieving backends.
+
+
+def available_backends(filters=None, compact=True):
+    """
+    Return the backends that are available in the SDK, optionally filtering
+    them based on their capabilities.
+
+    Note:
+        In order for this function to return online backends, a connection with
+        an online backend provider needs to be established by calling the
+        `register()` function.
+    Args:
+        filters (dict): dictionary of filtering conditions.
+        compact (bool): group backend names based on aliases
+
+    Returns:
+        list[str]: the names of the available backends.
+    """
+    backend_names = [str(backend)
+                     for backend in _DEFAULT_PROVIDER.available_backends(filters)]
+
+    if compact:
+        alias_dict = _DEFAULT_PROVIDER.aliased_backend_names()
+        aliases = set()
+        for name in backend_names:
+            backend_alias = set(k for k, v in alias_dict.items() if name in v)
+            if not backend_alias:
+                aliases.add(name)
+            elif len(backend_alias) == 1:
+                (alias,) = backend_alias
+                aliases.add(alias)
+        backend_names = list(aliases)
+
+    return backend_names
+
+
+def local_backends(compact=True):
+    """
+    Return the available local backends.
+
+    Args:
+        compact (bool): only report alias names. this is usually shorter, any several
+        backends usually share the same alias.
+
+    Returns:
+        list[str]: the names of the available remote backends.
+    """
+    return available_backends({'local': True}, compact=compact)
+
+
+def remote_backends(compact=True):
+    """
+    Return the available remote backends.
+
+    Args:
+        compact (bool): only report alias names. this is usually shorter, any several
+        backends usually share the same alias.
+
+    Returns:
+        list[str]: the names of the available remote backends.
+    """
+    return available_backends({'local': False}, compact=compact)
+
+
+def get_backend(name):
+    """
+    Return an instance of a `Backend` object from its name identifier.
+
+    Args:
+        name(str): unique name of the backend.
+    Returns:
+        BaseBackend: a Backend instance.
+    """
+    return _DEFAULT_PROVIDER.get_backend(name)
+
+
+# Functions for compiling and executing.
+
+
+def compile(circuits, backend,
+            config=None, basis_gates=None, coupling_map=None, initial_layout=None,
+            shots=1024, max_credits=10, seed=None, qobj_id=None, hpc=None,
+            skip_translation=False):
+    """Compile a list of circuits into a qobj.
+
+    Args:
+        circuits (QuantumCircuit or list[QuantumCircuit]): circuits to compile
+        backend (BaseBackend or str): a backend to compile for
+        config (dict): dictionary of parameters (e.g. noise) used by runner
+        basis_gates (str): comma-separated basis gate set to compile to
+        coupling_map (list): coupling map (perhaps custom) to target in mapping
+        initial_layout (list): initial layout of qubits in mapping
+        shots (int): number of repetitions of each circuit, for sampling
+        max_credits (int): maximum credits to use
+        seed (int): random seed for simulators
+        qobj_id (int): identifier for the generated qobj
+        hpc (dict): HPC simulator parameters
+        skip_translation (bool): If True, bypass most of the compilation process and
+            creates a qobj with minimal check nor translation
+    Returns:
+        obj: the qobj to be run on the backends
+    """
+    # pylint: disable=redefined-builtin
+    if isinstance(backend, str):
+        backend = _DEFAULT_PROVIDER.get_backend(backend)
+    return qiskit._compiler.compile(circuits, backend,
+                                    config, basis_gates, coupling_map, initial_layout,
+                                    shots, max_credits, seed, qobj_id, hpc,
+                                    skip_translation)
+
+
+def execute(circuits, backend,
+            config=None, basis_gates=None, coupling_map=None, initial_layout=None,
+            shots=1024, max_credits=10, seed=None, qobj_id=None, hpc=None,
+            skip_translation=False):
+    """Executes a set of circuits.
+
+    Args:
+        circuits (QuantumCircuit or list[QuantumCircuit]): circuits to execute
+        backend (BaseBackend or str): a backend to execute the circuits on
+        config (dict): dictionary of parameters (e.g. noise) used by runner
+        basis_gates (str): comma-separated basis gate set to compile to
+        coupling_map (list): coupling map (perhaps custom) to target in mapping
+        initial_layout (list): initial layout of qubits in mapping
+        shots (int): number of repetitions of each circuit, for sampling
+        max_credits (int): maximum credits to use
+        seed (int): random seed for simulators
+        qobj_id (int): identifier for the generated qobj
+        hpc (dict): HPC simulator parameters
+        skip_translation (bool): skip most of the compile steps and produce qobj directly
+
+    Returns:
+        BaseJob: returns job instance derived from BaseJob
+    """
+    if isinstance(backend, str):
+        backend = _DEFAULT_PROVIDER.get_backend(backend)
+    qobj = compile(circuits, backend,
+                   config, basis_gates, coupling_map, initial_layout,
+                   shots, max_credits, seed, qobj_id, hpc,
+                   skip_translation)
+    # XXX When qobj is done this should replace q_job
+    q_job = QuantumJob(qobj, backend=backend, preformatted=True, resources={
+        'max_credits': qobj['config']['max_credits']})
+    return backend.run(q_job)
+
+
+# Functions for importing qasm
+
+
+def load_qasm_string(qasm_string, name=None,
+                     basis_gates="id,u0,u1,u2,u3,x,y,z,h,s,sdg,t,tdg,rx,ry,rz,"
+                                 "cx,cy,cz,ch,crz,cu1,cu3,swap,ccx,cswap"):
+    """Construct a quantum circuit from a qasm representation (string).
+
+    Args:
+        qasm_string (str): a string of qasm, or a filename containing qasm.
+        basis_gates (str): basis gates for the quantum circuit.
+        name (str or None): the name of the quantum circuit after loading qasm
+            text into it. If no name given, assign automatically.
+    Returns:
+        QuantumCircuit: circuit constructed from qasm.
+    Raises:
+        QISKitError: if the string is not valid QASM
+    """
+    node_circuit = Qasm(data=qasm_string).parse()
+    unrolled_circuit = Unroller(node_circuit, CircuitBackend(basis_gates.split(",")))
+    circuit_unrolled = unrolled_circuit.execute()
+    if name:
+        circuit_unrolled.name = name
+    return circuit_unrolled
+
+
+def load_qasm_file(qasm_file, name=None,
+                   basis_gates="id,u0,u1,u2,u3,x,y,z,h,s,sdg,t,tdg,rx,ry,rz,"
+                               "cx,cy,cz,ch,crz,cu1,cu3,swap,ccx,cswap"):
+    """Construct a quantum circuit from a qasm representation (file).
+
+    Args:
+        qasm_file (str): a string for the filename including its location.
+        name (str or None): the name of the quantum circuit after
+            loading qasm text into it. If no name is give the name is of
+            the text file.
+        basis_gates (str): basis gates for the quantum circuit.
+    Returns:
+         QuantumCircuit: circuit constructed from qasm.
+    Raises:
+        QISKitError: if the file cannot be read.
+    """
+    if not os.path.exists(qasm_file):
+        raise QISKitError('qasm file "{0}" not found'.format(qasm_file))
+    if not name:
+        name = os.path.splitext(os.path.basename(qasm_file))[0]
+
+    with open(qasm_file) as file:
+        qasm_data = file.read()
+
+    return load_qasm_string(qasm_data, name=name, basis_gates=basis_gates)
