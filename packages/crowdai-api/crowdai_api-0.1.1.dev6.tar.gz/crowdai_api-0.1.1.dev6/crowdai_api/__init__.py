7aecb266cb9859eeba41da6adcd52a62678d9490
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:58 +0000

    Add package crowdai-api/0.1.1.dev6/crowdai_api-0.1.1.dev6.tar.gz

---
+++ packages/crowdai-api/crowdai_api-0.1.1.dev6.tar.gz/crowdai_api-0.1.1.dev6/crowdai_api/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for crowdAI API."""
+
+__author__ = """S.P. Mohanty"""
+__email__ = 'spmohanty91@gmail.com'
+__version__ = '0.1.0'
+
+from .crowdai_api import API
+from .submission import CrowdAISubmission
+from .gitlab_submission import GitlabSubmission
+from .exceptions import *
