93bcad457902e63bdbce373150f797d8de75470f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:21 +0000

    Add package williaml-test-nester/1.0.5/williaml-test-nester-1.0.5.tar.gz

---
+++ packages/williaml-test-nester/williaml-test-nester-1.0.5.tar.gz/williaml-test-nester-1.0.5/nester.py
@@ -0,0 +1,12 @@
+"""Comment 1"""
+
+
+def print_lol(the_list, level=0):
+    """Comment 2"""
+    for item in the_list:
+        if isinstance(item, list):
+            print_lol(item, level)
+        else:
+            for t in range(level):
+                print("\t", end=' ')
+                print(item)
