1fc06dc23e245ebc3d8b540b8045d082c6c7f143
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:03 +0000

    Add package illumon-jpy/1.20180430.23/illumon_jpy-1.20180430.23-cp27-cp27m-macosx_10_13_x86_64.whl

---
+++ packages/illumon-jpy/illumon_jpy-1.20180430.23-cp27-cp27m-macosx_10_13_x86_64.whl/jpyconfig.py
@@ -0,0 +1,8 @@
+# Created by 'jpyutil.py' tool on 2018-05-18 09:01:27.829349
+# This file is read by the 'jpyutil' module in order to load and configure the JVM from Python
+java_home = '/Library/Java/JavaVirtualMachines/jdk1.8.0_162.jdk/Contents/Home'
+jvm_dll = '/Library/Java/JavaVirtualMachines/jdk1.8.0_162.jdk/Contents/Home/jre/lib/server/libjvm.dylib'
+jvm_maxmem = None
+jvm_classpath = []
+jvm_properties = {}
+jvm_options = []
