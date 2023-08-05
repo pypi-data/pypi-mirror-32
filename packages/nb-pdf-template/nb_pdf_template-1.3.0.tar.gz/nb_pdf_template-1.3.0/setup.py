b17a1235085ab23015fb424ab3d794637160ca2b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:03 +0000

    Add package nb-pdf-template/1.3.0/nb_pdf_template-1.3.0.tar.gz

---
+++ packages/nb-pdf-template/nb_pdf_template-1.3.0.tar.gz/nb_pdf_template-1.3.0/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(name='nb_pdf_template',
+      version='1.3.0',
+      description='LaTeX templates for jupyter notebook conversion to pdf',
+      url='https://github.com/t-makaro/nb_pdf_template',
+      author='Tyler Makaro',
+      author_email='',
+      license='MIT',
+      packages=['nb_pdf_template'],
+      include_package_data = True,
+      classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.6',
+      ],
+      zip_safe=False)
