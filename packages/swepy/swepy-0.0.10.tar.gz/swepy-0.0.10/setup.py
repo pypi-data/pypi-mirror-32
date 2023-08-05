10c222fab0b7c27b7513f87173990d73d1ba3f04
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:12:37 +0000

    Add package swepy/0.0.10/swepy-0.0.10.tar.gz

---
+++ packages/swepy/swepy-0.0.10.tar.gz/swepy-0.0.10/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="swepy",
+    version="0.0.10",
+    author="William Norris",
+    author_email="wino6687@colorado.edu",
+    description="A python package for obtaining and cleaning Tb files",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/wino6687/SWEpy",
+    packages=setuptools.find_packages(),
+    install_requires=[
+    'datetime', 'numpy', 'netCDF4', 'scikit-image',
+    'pandas', 'nco', 'os', 'tqdm', 'affine', 'requests',
+    'gdal', 'sys', 'mapboxgl'
+    ],
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ),
+)
