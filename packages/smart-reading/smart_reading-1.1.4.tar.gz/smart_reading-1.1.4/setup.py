46bb71a5ea3a0feb07de72bb02531a6127500163
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:02 +0000

    Add package smart-reading/1.1.4/smart_reading-1.1.4.tar.gz

---
+++ packages/smart-reading/smart_reading-1.1.4.tar.gz/smart_reading-1.1.4/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+    
+setup(
+    name = 'smart_reading',
+    version = '1.1.4',
+    description = 'An NLTK-based toolkit aimed at increasing the understanding of various texts.',
+    url = 'https://github.com/andredelft/smart_reading',
+    packages = find_packages(),
+    install_requires = ['nltk','networkx','numpy','matplotlib'],
+    keywords = 'ebook understanding nltk reading toolkit',
+    long_description = long_description,
+    long_description_content_type = 'text/markdown',
+    include_package_data = True,
+    python_requires = '>=2.7',
+    author = 'Andre van Delft',
+    author_email = 'andrevandelft@outlook.com',
+    classifiers = [
+        'Development Status :: 4 - Beta',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3'
+    ]
+)
