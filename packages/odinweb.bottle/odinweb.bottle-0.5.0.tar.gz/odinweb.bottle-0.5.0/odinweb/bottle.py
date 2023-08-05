2f1ef70801f1abf344153de6b521282b38494cde
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:19 +0000

    Add package odinweb.bottle/0.5.0/odinweb.bottle-0.5.0.tar.gz

---
+++ packages/odinweb.bottle/odinweb.bottle-0.5.0.tar.gz/odinweb.bottle-0.5.0/odinweb/bottle.py
@@ -0,0 +1,152 @@
+"""
+OdinWeb.Bottle API
+~~~~~~~~~~~~~~~~~
+
+Bottle implementation of the OdinWeb API interface.
+
+Usage::
+    
+    import bottle
+    from odinweb.bottle import Api
+        
+    app = bottle.Bottle()
+    app.merge(
+        Api(
+            ApiVersion(
+                UserApi(),
+                version='v1
+            )
+        )
+    )    
+
+"""
+from __future__ import absolute_import
+
+from bottle import Route, response, request, BaseRequest
+
+from odin.utils import lazy_property
+from odinweb.containers import ApiInterfaceBase
+from odinweb.constants import Type, Method, PATH_STRING_RE
+from odinweb.data_structures import PathParam, MultiValueDict, BaseHttpRequest
+
+# Imports for typing support
+from typing import Iterator, List  # noqa
+
+
+TYPE_MAP = {
+    Type.Integer: 'int',
+    Type.Long: 'int',
+    Type.Float: 'float',
+    Type.Double: 'float',
+    Type.String: 're:' + PATH_STRING_RE,
+    Type.Byte: '',
+    Type.Binary: '',
+    Type.Boolean: 'bool',
+    Type.Date: 're:' + PATH_STRING_RE,
+    Type.Time: 're:' + PATH_STRING_RE,
+    Type.DateTime: 're:' + PATH_STRING_RE,
+    Type.Password: 're:' + PATH_STRING_RE,
+}
+
+
+class RequestProxy(BaseHttpRequest):
+    def __init__(self, r):
+        # type: (BaseRequest) -> None
+        self.request = r
+
+    @lazy_property
+    def environ(self):
+        return self.request.environ
+
+    @lazy_property
+    def method(self):
+        try:
+            return Method(self.request.method)
+        except KeyError:
+            pass
+
+    @lazy_property
+    def scheme(self):
+        return self.request.urlparts.scheme
+
+    @lazy_property
+    def host(self):
+        return self.request.urlparts.netloc
+
+    @lazy_property
+    def path(self):
+        return self.request.urlparts.path
+
+    @lazy_property
+    def query(self):
+        return MultiValueDict(self.request.GET.allitems())
+
+    @lazy_property
+    def headers(self):
+        return self.request.headers
+
+    @lazy_property
+    def cookies(self):
+        return self.request.cookies
+
+    @lazy_property
+    def session(self):
+        raise NotImplemented("Not supplied by Bottle")
+
+    @lazy_property
+    def body(self):
+        return self.request.body.read()
+    
+    @lazy_property
+    def form(self):
+        return MultiValueDict(self.request.POST.allitems())
+
+
+class Api(ApiInterfaceBase):
+    def __iter__(self):
+        # type: () -> Iterator[Route]
+        """
+        Iterator to simplify registration with Bottle using :func:`bottle.Bottle.merge`.
+        """
+        for url_path, operation in self.op_paths():
+            path = url_path.format(self.node_formatter)
+            for method in operation.methods:
+                yield Route(self, path, method.value, self._bound_callback(operation))
+
+    @staticmethod
+    def node_formatter(path_node):
+        # type: (PathParam) -> str
+        """
+        Format a node to be consumable by the `UrlPath.parse`.
+        """
+        if path_node.type:
+            node_type = TYPE_MAP.get(path_node.type, 'str')
+            return "<{}:{}>".format(path_node.name, node_type)
+        return "<{}>".format(path_node.name)
+
+    @property
+    def plugins(self):
+        # type: () -> list
+        # Placeholder to match the Bottle App API.
+        return []
+
+    @property
+    def routes(self):
+        # type: () -> List[Route]
+        return list(self)
+
+    def _bound_callback(self, operation):
+        """
+        Bind operation into method for translating between OdinWeb and Bottle.
+        """
+        def callback(**path_args):
+            # Dispatch incoming request after applying proxy to request object
+            resp = self.dispatch(operation, RequestProxy(request), **path_args)
+
+            # Translate standard OdinWeb response into Bottle response.
+            response.status = resp.status
+            for k, v in resp.headers.items():
+                response[k] = v
+
+            return resp.body
+        return callback
---
+++ packages/odinweb.bottle/odinweb.bottle-0.5.0.tar.gz/odinweb.bottle-0.5.0/tests/test_bottle.py
@@ -0,0 +1,60 @@
+from __future__ import absolute_import
+
+import pytest
+import webtest
+import six
+
+from bottle import Bottle, request
+
+from odinweb import bottle
+from odinweb.constants import Method, Type
+from odinweb.data_structures import PathParam
+
+
+def test_request_proxy():
+    test_app = Bottle(catchall=False)
+    app = webtest.TestApp(test_app)
+
+    @test_app.route('/', method='POST')
+    def test_method():
+        target = bottle.RequestProxy(request)
+        assert target.method == Method.POST
+        assert set(target.query.getlist('a')) == {'1', '3'}
+        assert set(target.query.getlist('b')) == {'2'}
+        assert target.body == "123" if six.PY2 else b'123'
+        assert target.content_type == 'text/html'
+        assert target.origin == "http://localhost:9000"
+        return 'OK'
+
+    result = app.post('/?a=1&b=2&a=3', expect_errors=False, content_type="text/html",
+                      headers={"Origin": "http://localhost:9000"}, params='123')
+    assert result.body == 'OK' if six.PY2 else b'OK'
+
+
+@pytest.mark.parametrize('path_node, expected', (
+    (PathParam('foo'), "<foo:int>"),
+    (PathParam('foo', Type.String), "<foo:re:[-\w.~,!%]+>"),
+    (PathParam('foo', Type.Boolean), "<foo:bool>"),
+    (PathParam('foo', None), "<foo>"),
+))
+def test_node_formatter(path_node, expected):
+    actual = bottle.Api.node_formatter(path_node)
+    assert actual == expected
+
+
+class TestApi(object):
+    def test_merge_api_and_request(self):
+        test_app = Bottle(catchall=False)
+        app = webtest.TestApp(test_app)
+        target = bottle.Api()
+
+        @target.operation('{foo:String}')
+        def sample_operation(request, foo):
+            assert isinstance(request, bottle.RequestProxy)
+            assert foo == 'eek'
+            return 'OK'
+
+        test_app.merge(target)
+
+        result = app.get('/api/eek', expect_errors=False)
+        assert result.body == '"OK"' if six.PY2 else b'"OK"'
