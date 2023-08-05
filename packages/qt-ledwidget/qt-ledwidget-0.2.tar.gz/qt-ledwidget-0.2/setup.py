f1ae77a1189dfd42646c80550658e02b63799883
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:31:01 +0000

    Add package qt-ledwidget/0.2/qt-ledwidget-0.2.tar.gz

---
+++ packages/qt-ledwidget/qt-ledwidget-0.2.tar.gz/qt-ledwidget-0.2/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup(name='qt-ledwidget',
+      version='0.2',
+      packages=['qt_ledwidget'],
+      install_requires=[
+          'PyQt5',
+      ],
+      include_package_data=True,
+      url='https://github.com/tappi287/qt_ledwidget', license='MIT',
+      author='Stefan Tapper',
+      author_email='tapper.stefan@gmail.com',
+      description='PyQt5 widget with animated LEDs')
+
+"""
+Build PyPi package:
+python setup.py sdist bdist_wheel
+
+Upload to TestPyPi
+twine upload --repository-url https://test.pypi.org/legacy/ dist/*
+"""
