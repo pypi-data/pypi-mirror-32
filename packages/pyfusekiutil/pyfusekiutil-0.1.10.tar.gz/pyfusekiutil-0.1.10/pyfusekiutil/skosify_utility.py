859c40952460e60fe2bdd2b0859f20ee8c6e9516
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:27:39 +0000

    Add package pyfusekiutil/0.1.10/pyfusekiutil-0.1.10.tar.gz

---
+++ packages/pyfusekiutil/pyfusekiutil-0.1.10.tar.gz/pyfusekiutil-0.1.10/pyfusekiutil/skosify_utility.py
@@ -0,0 +1,23 @@
+import skosify
+import requests
+
+
+def skosfiy(url, config, name, file_name, default_language=None, namespace=None):
+    response = requests.get(url)
+    path = config['data']['base'] + config['data']['temporary'] + file_name
+    if response.ok:
+        with open(path, 'w+') as file:
+            file.write(response.text)
+        voc = skosify.skosify(path,
+                              label=name,
+                              namespace=namespace,
+                              default_language=default_language,
+                              mark_top_concepts=True,
+                              eliminate_redundancy=True,
+                              break_cycles=True,
+                              keep_related=False,
+                              cleanup_classes=True,
+                              cleanup_properties=True,
+                              cleanup_unreachable=True
+                              )
+        voc.serialize(path.split('.')[0] + '.ttl', format='ttl')
