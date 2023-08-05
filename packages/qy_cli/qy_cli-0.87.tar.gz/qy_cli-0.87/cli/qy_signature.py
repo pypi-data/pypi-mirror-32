53319e07c0fa428c0fb7b38ff0d7a9cecbd3871c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:00 +0000

    Add package qy_cli/0.87/qy_cli-0.87.tar.gz

---
+++ packages/qy_cli/qy_cli-0.87.tar.gz/qy_cli-0.87/cli/qy_signature.py
@@ -0,0 +1,107 @@
+
+from urllib.parse import quote
+import urllib;
+import base64
+import hmac
+from hashlib import sha256
+
+"""
+使用getSignature 获取签名
+"""
+
+
+#构造URL请求参数部分
+def getSignatureArgs(str_map):
+    # 字典排序
+    str_url = sort_dict(str_map);
+    # print(str(str_url));
+
+    #构造url
+    str_url = urllib.parse.urlencode(str_url);
+
+    #对参数名称和参数值进行URL编码
+    # str_url = percentEncode(str_url)
+    return  str_url;
+
+
+def getSignatureStr(method,uri,url):
+    """
+    #构造被签名的串，被签名串 = HTTP请求方式 + “\n” + uri + “\n” + url 请求串
+    :param method:
+    :param uri:
+    :param url:
+    :return:
+    """
+    str = method + "\n" + uri + "\n"+ url;
+    return  str;
+
+def percentEncode(str):
+    res = urllib.parse.quote(str, '')
+    res = res.replace('+', '%20')
+    res = res.replace('*', '%2A')
+    res = res.replace('%7E', '~')
+    return res
+
+
+def sort_dict(dict_words):
+    """
+    字典排序
+    :param dict_words:
+    :return:
+    """
+    return dict(sorted(dict_words.items(), key=lambda d: d[0]))
+
+
+
+
+def getSignature(method,uri,url_map,secret_access_key):
+    """
+    获取签名
+    :param method:请求方法
+    :param uri:请求的uri路径
+    :param url_map:请求参数字典
+    :param secret_access_key:私钥
+    :return:签名
+    """
+
+    url =  getSignatureArgs(url_map);
+    string_to_sign = getSignatureStr(method,uri,url);
+
+    sb = bytes(secret_access_key, encoding="utf8")
+
+    h = hmac.new(sb, digestmod=sha256)
+    h.update(string_to_sign.encode("utf8"))
+    sign = base64.b64encode(h.digest()).strip()
+    signature = urllib.parse.quote_plus(sign)
+    return  signature;
+
+
+
+def test():
+    """
+    测试的
+    :return:
+    """
+    url_map = {
+      "count":1,
+      "vxnets.1":"vxnet-0",
+      "zone":"pek1",
+      "instance_type":"small_b",
+      "signature_version":1,
+      "signature_method":"HmacSHA256",
+      "instance_name":"demo",
+      "image_id":"centos64x86a",
+      "login_mode":"passwd",
+      "login_passwd":"QingCloud20130712",
+      "version":1,
+      "access_key_id":"QYACCESSKEYIDEXAMPLE",
+      "action":"RunInstances",
+      "time_stamp":"2013-08-27T14:30:10Z"
+    };
+    s_url = 'GET\n/iaas/\naccess_key_id=QYACCESSKEYIDEXAMPLE&action=RunInstances&count=1&image_id=centos64x86a&instance_name=demo&instance_type=small_b&login_mode=passwd&login_passwd=QingCloud20130712&signature_method=HmacSHA256&signature_version=1&time_stamp=2013-08-27T14%3A30%3A10Z&version=1&vxnets.1=vxnet-0&zone=pek1'
+
+    s_s = '32bseYy39DOlatuewpeuW5vpmW51sD1A%2FJdGynqSpP8%3D';
+    method = 'GET';
+    uri = "/iaas/";
+    secret_access_key = 'SECRETACCESSKEY'
+    print(getSignature(method,uri,url_map,secret_access_key) == s_s)
---
+++ packages/qy_cli/qy_cli-0.87.tar.gz/qy_cli-0.87/cli/qy_url.py
@@ -0,0 +1,72 @@
+import datetime
+from cli.qy_signature import getSignature
+method = 'GET';
+uri = 'https://api.qingcloud.com/iaas/';
+
+"""
+构建请求的url
+"""
+class httpUrl:
+     def __init__(self,url_map,config_dict):
+         """
+
+         :param url_map: 可选参数字典
+         :param kwargs: 配置参数字典
+         """
+         self.urldict = url_map;
+         self.zone = config_dict['zone'];
+         self.access_key_id = config_dict['access_key_id'];
+         self.secret_access_key = config_dict['secret_access_key']
+         self.getRequestArgs();
+
+         #获取请求url
+         self.url = uri + httpUrl.parse_url(self.urldict)
+
+
+     def getRequestArgs(self):
+         """
+         公共参数处理
+         :return:
+         """
+
+         #如何没有指定zone，那么取配置文件的zone
+         if not 'zone' in self.urldict:
+             self.urldict['zone'] = self.zone;
+
+         time_stamp = datetime.datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%SZ");
+         self.urldict['time_stamp'] = time_stamp
+
+         access_key_id = self.access_key_id;
+         self.urldict['access_key_id'] = access_key_id
+
+         version = 1;
+         self.urldict['version'] = version
+
+         signature_method = 'HmacSHA256';
+         self.urldict['signature_method'] = signature_method;
+
+         signature_version = 1;
+         self.urldict['signature_version'] = signature_version
+
+         signature = getSignature(method,uri,self.urldict,self.secret_access_key);
+         self.urldict['signature'] = signature
+
+
+
+     @staticmethod
+     def parse_url(data={}):
+         """
+         构造URL 中参数部分
+         :param data: 请求参数字典
+         :return:url中参数部分
+         """
+         item = data.items()
+         urls = "?"
+         for i in item:
+             (key, value) = i
+             temp_str = key + "=" + value
+             urls = urls + temp_str + "&"
+         urls = urls[:len(urls) - 1]
+         return urls
+
+
