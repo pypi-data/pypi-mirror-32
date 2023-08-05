91f0f380ce4c053bf983ae25ef21fafeeaae2a38
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:57 +0000

    Add package NiftyNet/0.3.0/NiftyNet-0.3.0-py2.py3-none-any.whl

---
+++ packages/NiftyNet/NiftyNet-0.3.0-py2.py3-none-any.whl/niftynet/info.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""NiftyNet version tracker.
+
+This module only holds the NiftyNet version, generated using the 
+``git describe --match v[0-9]*`` command.
+
+"""
+
+
+VERSION_DESCRIPTOR = "0.3.0"
