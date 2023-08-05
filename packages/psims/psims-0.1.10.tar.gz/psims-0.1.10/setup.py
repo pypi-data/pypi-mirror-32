c850ea1ecccbbbaa5a0225e1d1f42af342c19cec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:24 +0000

    Add package psims/0.1.10/psims-0.1.10.tar.gz

---
+++ packages/psims/psims-0.1.10.tar.gz/psims-0.1.10/setup.py
@@ -0,0 +1,34 @@
+from setuptools import find_packages, setup
+
+
+with open("psims/version.py") as version_file:
+    version = None
+    for line in version_file.readlines():
+        if "version = " in line:
+            version = line.split(" = ")[1].replace("\"", "").replace("'", "").strip()
+            print("Version is: %r" % (version,))
+            break
+    else:
+        print("Cannot determine version")
+
+setup(
+    name='psims',
+    version=version,
+    description="Writers and controlled vocabulary manager for PSI-MS's mzML and mzIdentML standards",
+    maintainer='Joshua Klein',
+    maintainer_email="jaklein@bu.edu",
+    zip_safe=False,
+    packages=find_packages(),
+    url="https://github.com/mobiusklein/psims",
+    include_package_data=True,
+    package_data={
+        "psims.controlled_vocabulary": ["psims/controlled_vocabulary/vendor/*"],
+        "psims.validation": ["psims/validation/xsd/*"]
+    },
+    install_requires=[
+        "lxml",
+        "six",
+        "sqlalchemy",
+        "numpy"
+    ]
+)
