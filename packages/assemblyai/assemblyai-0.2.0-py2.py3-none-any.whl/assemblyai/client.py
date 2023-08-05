bb654bc42c4c62dc12a632dad00fad8e7dfeebee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:32:11 +0000

    Add package assemblyai/0.2.0/assemblyai-0.2.0-py2.py3-none-any.whl

---
+++ packages/assemblyai/assemblyai-0.2.0-py2.py3-none-any.whl/assemblyai/client.py
@@ -0,0 +1,39 @@
+"""Client module."""
+
+from assemblyai.config import ASSEMBLYAI_URL, ASSEMBLYAI_TOKEN
+# from assemblyai.exceptions import handle_warnings
+from assemblyai.model import Model
+from assemblyai.transcript import Transcript
+# from assemblyai.token import trial_token, validate_token
+
+
+class Client(object):
+    """Client for the AssemblyAI API."""
+
+    def __init__(self, token=None, email=None):
+        """Initialize client."""
+        self.token = token or ASSEMBLYAI_TOKEN  # or trial_token()
+        # validate_token(self.token)
+        self.headers = {'authorization': self.token}
+        self.api = ASSEMBLYAI_URL
+
+    def __repr__(self):
+        concat_token = self.token[0:8] + '..'
+        return 'Client(token=%s)' % concat_token
+
+    def train(self, phrases, closed_domain=None, name=None):
+        """Create a custom language model."""
+        client = self
+        self.model = Model(client, phrases=phrases,
+                           closed_domain=closed_domain, name=name)
+        self.model = self.model.create()
+        return self.model
+
+    def transcribe(self, filename=None, audio_url=None, model=None):
+        """Create a transcript request. If the transcript depends on a
+        custom language model, defer creation until model is trained."""
+        client = self
+        self.transcript = Transcript(client, filename=filename,
+                                     audio_url=audio_url, model=model)
+        self.transcript = self.transcript.create()
+        return self.transcript
