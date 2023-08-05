11b4bffaebf35d345fe40a710bf0e7b1ab261878
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:15:39 +0000

    Add package mozilla-taarlite/0.2.0/mozilla-taarlite-0.2.0.tar.gz

---
+++ packages/mozilla-taarlite/mozilla-taarlite-0.2.0.tar.gz/mozilla-taarlite-0.2.0/tests/test_logging.py
@@ -0,0 +1,37 @@
+from moto import mock_s3
+from taar_lite.recommenders import GuidBasedRecommender
+from taar_lite.recommenders.guid_based_recommender import ADDON_LIST_BUCKET
+from taar_lite.recommenders.guid_based_recommender import ADDON_LIST_KEY
+from taar_lite.recommenders.guid_based_recommender import GUID_RANKING_KEY
+import boto3
+import json
+from taar_lite.recommenders.cache import LazyJSONLoader
+from .conftest import mock_cold_redis_cache
+
+
+@mock_s3
+def test_logging(default_ctx, MOCK_DATA, MOCK_GUID_RANKING):
+    conn = boto3.resource('s3', region_name='us-west-2')
+    conn.create_bucket(Bucket=ADDON_LIST_BUCKET)
+    conn.Object(ADDON_LIST_BUCKET, ADDON_LIST_KEY)\
+        .put(Body=json.dumps(MOCK_DATA))
+    conn.Object(ADDON_LIST_BUCKET, GUID_RANKING_KEY)\
+        .put(Body=json.dumps(MOCK_GUID_RANKING))
+    coinstall_loader = LazyJSONLoader(default_ctx,
+                                      ADDON_LIST_BUCKET,
+                                      ADDON_LIST_KEY)
+
+    ranking_loader = LazyJSONLoader(default_ctx,
+                                    ADDON_LIST_BUCKET,
+                                    GUID_RANKING_KEY)
+
+    default_ctx['coinstall_loader'] = mock_cold_redis_cache(coinstall_loader)
+    default_ctx['ranking_loader'] = mock_cold_redis_cache(ranking_loader)
+
+    recommender = GuidBasedRecommender(default_ctx)
+
+    # These would error out if the object type was incorrect
+    recommender.logger.error('foo')
+    recommender.logger.warn('bar')
+    recommender.logger.info('foo')
+    recommender.logger.debug('bar')
