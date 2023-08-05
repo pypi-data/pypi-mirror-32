245c6f29c25e2dbecee9fcb1ad4b58f5e5a77956
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:45 +0000

    Add package stacklift/0.0.2/stacklift-0.0.2.tar.gz

---
+++ packages/stacklift/stacklift-0.0.2.tar.gz/stacklift-0.0.2/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup
+import os
+
+with open('README.md') as file:
+    long_description = file.read()
+
+tag = os.getenv("CIRCLE_TAG")
+if tag:
+    version = tag[1:]
+else:
+    version = "0.0.0development"
+
+setup(
+    name='stacklift',
+    version=version,
+    description='CloudFormation stack management tool',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/syginc/stacklift',
+    scripts=['bin/stacklift'],
+    author='syginc',
+    license='MIT',
+    keywords='aws cloudformation',
+    packages=[
+        "stacklift"
+    ],
+    install_requires= [
+        "pyyaml",
+        "boto3",
+        "click"
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Software Development :: Build Tools'
+    ],
+)
