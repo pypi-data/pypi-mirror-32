3672e08c132d2938f99880d9e6f703ea9948d92a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:32:16 +0000

    Add package nwapp/1.5/nwapp-1.5.tar.gz

---
+++ packages/nwapp/nwapp-1.5.tar.gz/nwapp-1.5/setup.py
@@ -0,0 +1,34 @@
+
+
+
+from setuptools import setup, find_packages
+
+
+setup(
+        name = 'nwapp',
+        version = '1.5',
+        description='This CLI tool is to manage freelance works',
+        url='http://www.example.com/~cschultz/bvote/',
+        author='Naseef Ummer',
+        author_email = 'naseefo@gmail.com',
+        license = 'MIT',
+        zip_safe = False,
+        #py_modules = ['hello'],
+        packages = find_packages(),
+        include_package_data=True,
+        install_requires = [
+            'Click',
+            'pymongo',
+            'pandas',
+            'forex-python',
+            'colorama',
+            'datetime',
+            'requests'
+            ],
+        entry_points = {
+            'console_scripts':[
+                'nw = nwapp.scripts.nw:cli',
+
+            ],
+        }
+    )
