5ce47ee5d2285a53ee5c18af6b619facd25159cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:49 +0000

    Add package GKextCPy/0.3.3/GKextCPy-0.3.3.tar.gz

---
+++ packages/GKextCPy/GKextCPy-0.3.3.tar.gz/GKextCPy-0.3.3/setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+"""
+setup.py file for SWIG 
+"""
+
+#from distutils.core import setup, Extension
+from setuptools import setup, Extension
+import numpy as np
+
+GKextCPy_module = Extension('_GKextCPy', sources=['GKextCPy_wrap.cxx', 'GKextCPy.cpp'],swig_opts=['-c++'], extra_compile_args = ["-std=c++11"])
+                           
+
+setup (name = 'GKextCPy',
+       version = '0.3.3',
+       author      = "Elisabetta Ghisu",
+       description = """Graph Kernels: building the extension Python module. This is a wrapper package from C++ to Python.""",
+       include_dirs = [np.get_include(), '/usr/include/igraph','/usr/local/include/igraph','-I/usr/include/igraph', '-I/usr/local/include/igraph'],
+       ext_modules = [GKextCPy_module],
+       py_modules = ["GKextCPy"],
+       install_requires = ['python-igraph', 'numpy'],
+       license = 'ETH Zurich',
+       )
