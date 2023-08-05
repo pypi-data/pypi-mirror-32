82ca793420df4b665ff432a12640de4cc16a55a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:25:10 +0000

    Add package citellus/0.1.9.383095526.20180524092746/citellus-0.1.9.383095526.20180524092746.tar.gz

---
+++ packages/citellus/citellus-0.1.9.383095526.20180524092746.tar.gz/citellus-0.1.9.383095526.20180524092746/citellusclient/extensions/metadata.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python
+# encoding: utf-8
+#
+# Description: Extension for running and reporting metadata
+# Author: Pablo Iranzo Gomez (Pablo.Iranzo@gmail.com)
+
+from __future__ import print_function
+
+import os
+
+try:
+    import citellusclient.shell as citellus
+except:
+    import shell as citellus
+
+# Load i18n settings from citellus
+_ = citellus._
+
+extension = "metadata"
+pluginsdir = os.path.join(citellus.citellusdir, 'plugins', extension)
+
+
+def init():
+    """
+    Initializes module
+    :return: List of triggers for extension
+    """
+    triggers = ['metadata']
+    return triggers
+
+
+def listplugins(options=None):
+    """
+    List available plugins
+    :param options: argparse options provided
+    :return: plugin object generator
+    """
+
+    prio = 0
+    if options:
+        try:
+            prio = options.prio
+        except:
+            pass
+
+    yield citellus.findplugins(folders=[pluginsdir], extension='metadata', prio=prio)
+
+
+def get_metadata(plugin):
+    """
+    Gets metadata for plugin
+    :param plugin: plugin object
+    :return: metadata dict for that plugin
+    """
+
+    metadata = {'description': citellus.regexpfile(filename=plugin['plugin'], regexp='\A# description:')[14:].strip(),
+                'long_name': citellus.regexpfile(filename=plugin['plugin'], regexp='\A# long_name:')[12:].strip(),
+                'priority': int(citellus.regexpfile(filename=plugin['plugin'], regexp='\A# priority:')[11:].strip() or 0)}
+    return metadata
+
+
+def run(plugin):  # do not edit this line
+    """
+    Executes plugin
+    :return: returncode, out, err
+    """
+    return citellus.execonshell(filename=plugin['plugin'])
+
+
+def help():  # do not edit this line
+    """
+    Returns help for plugin
+    :return: help text
+    """
+
+    commandtext = _("This extension proceses Citellus metadata plugins to fill details about system")
+    return commandtext
