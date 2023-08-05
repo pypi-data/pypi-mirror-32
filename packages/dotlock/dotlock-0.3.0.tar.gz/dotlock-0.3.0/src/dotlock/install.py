2d6bfa0b1ef7aa341f4f1fc69244112bcd8458ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:01 +0000

    Add package dotlock/0.3.0/dotlock-0.3.0.tar.gz

---
+++ packages/dotlock/dotlock-0.3.0.tar.gz/dotlock-0.3.0/src/dotlock/install.py
@@ -0,0 +1,51 @@
+import asyncio
+import asyncio.subprocess
+import os
+import os.path
+import logging
+from tempfile import TemporaryDirectory
+from typing import List
+
+import distlib.index
+
+
+logger = logging.getLogger(__name__)
+
+
+async def download(dir: str, requirement: dict):
+    logger.info('Downloading %s from %s', requirement['name'], requirement['url'])
+    index = distlib.index.PackageIndex(requirement['source'])
+    package_filename = requirement['url'].split('/')[-1]
+    destfile = os.path.join(dir, package_filename)
+    index.download_file(
+        requirement['url'],
+        destfile=destfile,
+        digest=('sha256', requirement['sha256']),
+    )
+
+
+async def download_all(dir: str, requirements: List[dict]):
+    return await asyncio.gather(*[
+        download(dir, requirement) for requirement in requirements
+    ])
+
+
+async def install(requirements: List[dict]):
+    venv_path = os.path.join(os.getcwd(), 'venv')
+
+    with TemporaryDirectory(prefix='python-package-') as dir:
+        await download_all(dir, requirements)
+        for requirement in requirements:
+            # We can't just use requirement['name'] as the package name because
+            # pip won't find the file if its (potentially non-canonical) name
+            # does not match the package name.
+            filename = requirement['url'].split('/')[-1]
+            process = await asyncio.subprocess.create_subprocess_exec(
+                'pip', 'install', '--no-index',
+                '--target', os.path.join(venv_path, 'lib/python3.6/site-packages/'),
+                '--find-links', dir,
+                '--upgrade',
+                os.path.join(dir, filename),
+            )
+
+            await process.wait()
---
+++ packages/dotlock/dotlock-0.3.0.tar.gz/dotlock-0.3.0/src/dotlock/sdist_handling.py
@@ -0,0 +1,116 @@
+import asyncio
+import logging
+import os
+import sys
+import distutils.core
+from typing import List
+from tempfile import TemporaryDirectory
+
+from aiohttp import ClientSession
+from packaging.utils import canonicalize_name
+
+from dotlock.dist_info_parsing import RequirementInfo, CandidateInfo, PackageType, parse_requires_dist
+from dotlock.tempdir import temp_working_dir
+
+
+logger = logging.getLogger(__name__)
+
+
+def run_setup():
+    """
+    Like distutils.core.run_setup('setup.py', stop_after='config')
+    except sets __name__ = '__main__' which some packages require.
+
+    Returns:
+        A distutils.core.Distribution instance.
+    """
+    saved_argv = sys.argv[:]
+    sys.argv[0] = 'setup.py'
+    distutils.core._setup_stop_after = 'config'
+
+    try:
+        with open('setup.py') as fp:
+            exec(fp.read(), {
+                '__file__': 'setup.py',
+                '__name__': '__main__',
+            })
+    finally:
+        sys.argv = saved_argv
+        distutils.core._setup_stop_after = None
+
+    return distutils.core._setup_distribution
+
+
+async def get_sdist_requirements(session: ClientSession, candidate_info: CandidateInfo) -> List[RequirementInfo]:
+    """
+    Getting the requirements for an sdist package is waaaaay more work than it should be.
+
+    Args:
+        session: an open aiohttp.ClientSession for making repeated queries to PyPI
+        candidate_info: specifies the candidate that we want requirements for
+
+    Returns:
+        A list of RequirementInfo for the candidate.
+    """
+    assert candidate_info.package_type == PackageType.sdist
+    logger.debug('%s is an sdist, doing the sdist dance to get requirements', candidate_info.name)
+
+    url = candidate_info.url
+    filename = url.split('/')[-1]
+
+    with temp_working_dir():
+        # Download the tarball.
+        logger.debug('downloading archive %s', candidate_info.url)
+        async with session.get(candidate_info.url) as response:
+            with open(filename, 'wb') as fp:
+                async for chunk in response.content.iter_any():
+                    fp.write(chunk)
+
+        return await get_sdist_file_requirements(candidate_info.name, filename)
+
+
+async def get_sdist_file_requirements(candidate_name: str, filename: str) -> List[RequirementInfo]:
+    # Extract the file.
+    if filename.endswith('.tar.gz'):
+        ext = '.tar.gz'
+        subprocess = await asyncio.create_subprocess_exec(
+            'tar', '-xf', filename,
+        )
+    elif filename.endswith('.tar.bz2'):
+        ext = '.tar.bz2'
+        subprocess = await asyncio.create_subprocess_exec(
+            'tar', '-xf', filename,
+        )
+    elif filename.endswith('.zip'):
+        ext = '.zip'
+        subprocess = await asyncio.create_subprocess_exec(
+            'unzip', filename,
+        )
+    else:
+        raise ValueError('Unrecognized archive format: %s', filename)
+
+    await subprocess.wait()
+    extracted_dir = filename[:-len(ext)]
+
+    # CD into the extracted directory.
+    # Necessary since some setup.py files expect to run from this directory.
+    os.chdir(extracted_dir)
+    # Some setup.py files also expect the current directory to be in the path.
+    sys.path.append(os.getcwd())
+
+    try:
+        # Parse setup.py and partially execute it.
+        distribution = run_setup()
+    finally:
+        sys.path.pop()
+
+    canonical_name = canonicalize_name(distribution.get_name())
+    assert canonical_name == candidate_name, canonical_name
+
+    if distribution.setup_requires:
+        logger.warning('Package %s uses setup_requires; we cannot guarantee integrity.',
+                       distribution.get_fullname())
+
+    requires = distribution.install_requires
+    logger.debug('%s sdist requires: %r', candidate_name, requires)
+    return parse_requires_dist(requires)
