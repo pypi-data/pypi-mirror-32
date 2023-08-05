a42c58210a4c069c61b16e659c7da5a2cd037048
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:20 +0000

    Add package pyims/0.1.1/pyims-0.1.1.tar.gz

---
+++ packages/pyims/pyims-0.1.1.tar.gz/pyims-0.1.1/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(name="pyims",
+    version='0.1.1',
+    description='A python wrapper for the IMS Word Sense Disambiguation tool (Zhong and Ng, 2010)',
+    url='http://github.com/vishnumenon/pyims',
+    author="Vishnu Menon",
+    author_email="me@vishnumenon.com",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license='MIT',
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'nltk',
+    ],
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+    zip_safe=False)
