b56a97a6cec7b6eacb914aa8818aeea35d4415f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:49 +0000

    Add package onegov.ballot/3.6.1/onegov.ballot-3.6.1.tar.gz

---
+++ packages/onegov.ballot/onegov.ballot-3.6.1.tar.gz/onegov.ballot-3.6.1/onegov/ballot/collections/__init__.py
@@ -0,0 +1,12 @@
+from onegov.ballot.collections.ballots import BallotCollection
+from onegov.ballot.collections.elections import ElectionCollection
+from onegov.ballot.collections.election_compounds import \
+    ElectionCompoundCollection
+from onegov.ballot.collections.votes import VoteCollection
+
+__all__ = [
+    'BallotCollection',
+    'ElectionCollection',
+    'ElectionCompoundCollection',
+    'VoteCollection'
+]
