eba283c5638b90f30ec077bd3de3691a3c767f92
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:34 +0000

    Add package pybea/0.3.0a0/pybea-0.3.0a0.tar.gz

---
+++ packages/pybea/pybea-0.3.0a0.tar.gz/pybea-0.3.0a0/setup.py
@@ -0,0 +1,35 @@
+import setuptools
+from distutils.core import setup
+
+
+CLASSIFIERS = ['Development Status :: 3 - Alpha',
+               'Intended Audience :: Education',
+               'Intended Audience :: Science/Research',
+               'License :: OSI Approved :: MIT License',
+               'Operating System :: OS Independent',
+               'Programming Language :: Python',
+               'Programming Language :: Python :: 2',
+               'Programming Language :: Python :: 3',
+               'Programming Language :: Python :: 2.7',
+               'Programming Language :: Python :: 3.3',
+               'Programming Language :: Python :: 3.4',
+               'Programming Language :: Python :: 3.5',
+               'Topic :: Scientific/Engineering',
+               ]
+
+DESCRIPTION = "Python package for accessing data from the BEA data API"
+
+PACKAGES = ['pybea']
+
+VERSION = "0.3.0-alpha"
+
+setup(name='pybea',
+      packages=PACKAGES,
+      version=VERSION,
+      description=DESCRIPTION,
+      author='David R. Pugh',
+      author_email='drpugh@protonmail.com',
+      url='https://github.com/davidrpugh/pyBEA',
+      license='LICENSE.rst',
+      classifiers=CLASSIFIERS
+      )
