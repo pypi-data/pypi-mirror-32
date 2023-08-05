6b0275e6827e63fadc5647cf9a082bea5aaff955
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:11 +0000

    Add package phonetic-algorithmRu/0.8.2/phonetic_algorithmRu-0.8.2.tar.gz

---
+++ packages/phonetic-algorithmRu/phonetic_algorithmRu-0.8.2.tar.gz/phonetic_algorithmRu-0.8.2/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+from setuptools import find_packages
+
+
+setup(name='phonetic_algorithmRu',
+     version='0.8.2',
+     description='Phonenetic algorithm for Russian language',
+     author='Anastasiya Kostyanitsyna, George Moroz',
+     packages=find_packages(),
+     include_package_data=True,
+     install_requires=['numpy==1.14.2', 'pandas==0.22.0'],
+     zip_safe=False
+)
