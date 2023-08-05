97d9bb4a781634086781c706887b03cffa74ed48
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:09:41 +0000

    Add package fileop/0.0.2/fileop-0.0.2.tar.gz

---
+++ packages/fileop/fileop-0.0.2.tar.gz/fileop-0.0.2/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="fileop",
+    version="0.0.2",
+    author="Saurabh Londhe",
+    author_email="saurabhlondhe1111@gmail.com",
+    description="All file operations without writing code for it",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/saurabhlondhe/fileop",
+    packages=setuptools.find_packages(),
+    classifiers=(
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 2",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+)
