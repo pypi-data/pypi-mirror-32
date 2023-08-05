0909ff9d6f89ab86da8a42739d276b453a401e3b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:56 +0000

    Add package tif-stats/0.6/tif_stats-0.6.tar.gz

---
+++ packages/tif-stats/tif_stats-0.6.tar.gz/tif_stats-0.6/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='tif_stats',
+      version='0.6',
+      description='Generates basic statistics on a geojson clipped tif image',
+      url='',
+      author='Development Seed',
+      author_email='aimee@developmentseed.org,nana@developmentseed.org',
+      license='MIT',
+      py_modules=['tif_stats'],
+      install_requires=['gippy==1.0.0.post3'])
