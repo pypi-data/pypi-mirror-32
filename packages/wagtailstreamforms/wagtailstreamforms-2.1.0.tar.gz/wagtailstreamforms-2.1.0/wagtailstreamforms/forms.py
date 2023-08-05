d585bc2d4c1b1840e2def2904f7e0b24926b44a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:31:28 +0000

    Add package wagtailstreamforms/2.1.0/wagtailstreamforms-2.1.0.tar.gz

---
+++ packages/wagtailstreamforms/wagtailstreamforms-2.1.0.tar.gz/wagtailstreamforms-2.1.0/wagtailstreamforms/forms.py
@@ -0,0 +1,71 @@
+from django import forms
+from django.core.exceptions import ValidationError
+from django.core.validators import validate_email
+from django.utils.translation import ugettext_lazy as _
+
+from captcha.fields import ReCaptchaField
+from wagtail.contrib.forms.forms import FormBuilder as OrigFormBuilder
+from wagtailstreamforms.utils import recaptcha_enabled
+from wagtailstreamforms.widgets import MultiEmailWidget
+
+
+class FormBuilder(OrigFormBuilder):
+
+    def __init__(self, fields, **kwargs):
+        super().__init__(fields)
+
+        self.add_recaptcha = kwargs.pop('add_recaptcha')
+
+    def create_regexfield_field(self, field, options):
+        """ The regex field """
+
+        if field.regex_validator:
+            # there is a selected validator so use it
+            options.update({
+                'regex': field.regex_validator.regex,
+                'error_messages': {'invalid': field.regex_validator.error_message}
+            })
+        else:
+            # otherwise allow anything
+            options.update({'regex': '(.*?)'})
+
+        return forms.RegexField(**options)
+
+    @property
+    def formfields(self):
+        """ Add additional fields to the already defined ones """
+
+        fields = super().formfields
+
+        # add fields to uniquely identify the form
+        fields['form_id'] = forms.CharField(widget=forms.HiddenInput)
+        fields['form_reference'] = forms.CharField(widget=forms.HiddenInput)
+
+        # add recaptcha field if enabled
+        if self.add_recaptcha and recaptcha_enabled():
+            fields['recaptcha'] = ReCaptchaField(label='')
+
+        return fields
+
+
+class MultiEmailField(forms.Field):
+    message = _('Enter valid email addresses.')
+    code = 'invalid'
+    widget = MultiEmailWidget
+
+    def to_python(self, value):
+        """ Normalize data to a list of strings. """
+
+        if not value:
+            return []
+        return [v.strip() for v in value.splitlines() if v != '']
+
+    def validate(self, value):
+        """ Check if value consists only of valid emails. """
+
+        super().validate(value)
+        try:
+            for email in value:
+                validate_email(email)
+        except ValidationError:
+            raise ValidationError(self.message, code=self.code)
---
+++ packages/wagtailstreamforms/wagtailstreamforms-2.1.0.tar.gz/wagtailstreamforms-2.1.0/wagtailstreamforms/models/submission.py
@@ -0,0 +1,52 @@
+import json
+
+from django.db import models
+from django.utils.translation import ugettext_lazy as _
+
+
+class AbstractFormSubmission(models.Model):
+    """
+    Data for a form submission.
+
+    You can create custom submission model based on this abstract model.
+    For example, if you need to save additional data or a reference to a user.
+    """
+
+    form_data = models.TextField(
+        _('Form data'),
+    )
+    form = models.ForeignKey(
+        'wagtailstreamforms.BaseForm',
+        verbose_name=_('Form'),
+        on_delete=models.CASCADE
+    )
+    submit_time = models.DateTimeField(
+        _('Submit time'),
+        auto_now_add=True
+    )
+
+    def get_data(self):
+        """
+        Returns dict with form data.
+
+        You can override this method to add additional data.
+        """
+        form_data = json.loads(self.form_data)
+
+        form_data.update(
+            {'submit_time': self.submit_time, }
+        )
+
+        return form_data
+
+    def __str__(self):
+        return self.form_data
+
+    class Meta:
+        abstract = True
+        ordering = ['-submit_time', ]
+        verbose_name = _('Form submission')
+
+
+class FormSubmission(AbstractFormSubmission):
+    """ Data for a Form submission. """
