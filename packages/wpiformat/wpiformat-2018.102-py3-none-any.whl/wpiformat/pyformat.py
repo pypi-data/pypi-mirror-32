79b823d11d7e49dbc179f8d6b4df87251ee862b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:09:28 +0000

    Add package wpiformat/2018.102/wpiformat-2018.102-py3-none-any.whl

---
+++ packages/wpiformat/wpiformat-2018.102-py3-none-any.whl/wpiformat/pyformat.py
@@ -0,0 +1,27 @@
+"""This task runs yapf on files with Python extension."""
+
+import subprocess
+import sys
+
+from wpiformat.task import Task
+
+
+class PyFormat(Task):
+
+    def should_process_file(self, config_file, name):
+        return name.endswith(".py")
+
+    def run_batch(self, config_file, names):
+        try:
+            args = ["python3", "-m", "yapf", "--style", "google", "-i"]
+            returncode = subprocess.run(args + names).returncode
+        except FileNotFoundError:
+            try:
+                args = ["py", "-3", "-m", "yapf", "--style", "google", "-i"]
+                returncode = subprocess.run(args + names).returncode
+            except FileNotFoundError:
+                print(
+                    "Error: yapf not found in PATH. Is it installed?",
+                    file=sys.stderr)
+                return False
+        return True
