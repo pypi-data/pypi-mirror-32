1191d517d0d91a26c8d4a1ee90d8cc887104b59b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:20 +0000

    Add package radar-resolver/0.2.7/radar-resolver-0.2.7.tar.gz

---
+++ packages/radar-resolver/radar-resolver-0.2.7.tar.gz/radar-resolver-0.2.7/radar/action.py
@@ -0,0 +1,5 @@
+from radar.query import Query
+
+
+class Action(Query):
+    __slots__ = Query.__slots__
---
+++ packages/radar-resolver/radar-resolver-0.2.7.tar.gz/radar-resolver-0.2.7/setup.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3 -S
+import os
+import uuid
+from setuptools import setup
+from pkgutil import walk_packages
+
+
+PKG = 'radar'
+PKG_NAME = 'radar-resolver'
+PKG_VERSION = '0.2.7'
+
+pathname = os.path.dirname(os.path.realpath(__file__))
+
+def parse_requirements(filename):
+    """ load requirements from a pip requirements file """
+    lineiter = (line.strip() for line in open(filename))
+    return (line for line in lineiter if line and not line.startswith("#"))
+
+install_reqs = parse_requirements(pathname + "/requirements.txt")
+
+
+def find_packages(prefix=""):
+    path = [prefix]
+    yield prefix
+    prefix = prefix + "."
+    for _, name, ispkg in walk_packages(path, prefix):
+        if ispkg:
+            yield name
+
+
+setup(
+    name=PKG_NAME,
+    version=PKG_VERSION,
+    description='A python resolver for react-radar Queries and Actions',
+    author='Jared Lunde',
+    author_email='jared.lunde@gmail.com',
+    url='https://github.com/jaredlunde/radar-resolver',
+    license="MIT",
+    install_requires=list(install_reqs),
+    packages=list(find_packages(PKG))
+)
