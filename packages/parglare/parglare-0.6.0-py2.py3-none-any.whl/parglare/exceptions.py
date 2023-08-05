9aa546fe396bdad548d34de309a4ee7de93d605c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:12:06 +0000

    Add package parglare/0.6.0/parglare-0.6.0-py2.py3-none-any.whl

---
+++ packages/parglare/parglare-0.6.0-py2.py3-none-any.whl/parglare/exceptions.py
@@ -0,0 +1,119 @@
+from __future__ import unicode_literals
+from parglare.termui import s_header as _
+
+
+class LocationError(Exception):
+    def __init__(self, location, message):
+        self.location = location
+        super(LocationError, self).__init__(str(location) + message)
+
+
+class GrammarError(LocationError):
+    def __init__(self, location, message):
+        super(GrammarError, self).__init__(location, message)
+
+
+class ParseError(LocationError):
+    def __init__(self, location, message):
+        super(ParseError, self).__init__(location, message)
+
+
+def expected_message(symbols):
+    return (_('Expected: ') + '{}').format(
+        _(' or ').join(sorted([s.name for s in symbols])))
+
+
+def disambiguation_error(tokens):
+    return 'Can\'t disambiguate between: {}'.format(
+                _(' or ').join([str(t) for t in tokens]))
+
+
+class ParserInitError(Exception):
+    pass
+
+
+class DisambiguationError(Exception):
+    def __init__(self, tokens):
+        self.tokens = tokens
+
+
+class DynamicDisambiguationConflict(Exception):
+    def __init__(self, state, token, actions):
+        self.state = state
+        self.token = token
+        self.actions = actions
+
+        from parglare.parser import SHIFT
+        message = "{}\nIn state {}:{} and input symbol '{}' after calling"\
+                  " dynamic disambiguation still can't decide "\
+                  .format(str(state), state.state_id, state.symbol, token)
+        if actions[0].action == SHIFT:
+            prod_str = " or ".join(["'{}'".format(str(a.prod))
+                                    for a in actions[1:]])
+            message += "whether to shift or reduce by "\
+                       "production(s) {}.".format(prod_str)
+        else:
+            prod_str = " or ".join(["'{}'".format(str(a.prod))
+                                    for a in actions])
+            message += "which reduction to perform: {}".format(prod_str)
+
+        self.message = message
+
+    def __str__(self):
+        return self.message
+
+
+class LRConflict(object):
+    def __init__(self, state, term, productions):
+        self.message = ""
+        self.state = state
+        self.term = term
+        self.productions = productions
+
+    @property
+    def dynamic(self):
+        return self.term in self.state.dynamic
+
+
+class SRConflict(LRConflict):
+    def __init__(self, state, term, productions):
+        super(SRConflict, self).__init__(state, term, productions)
+        prod_str = " or ".join(["'{}'".format(str(p))
+                                for p in productions])
+        message = "{}\nIn state {}:{} and input symbol '{}' can't " \
+                  "decide whether to shift or reduce by production(s) {}." \
+            .format(str(state), state.state_id, state.symbol, term, prod_str)
+        if self.dynamic:
+            message += " Dynamic disambiguation strategy will be called."
+        self.message = message
+
+
+class RRConflict(LRConflict):
+    def __init__(self, state, term, productions):
+        super(RRConflict, self).__init__(state, term, productions)
+        prod_str = " or ".join(["'{}'".format(str(p))
+                                for p in productions])
+        message = "{}\nIn state {}:{} and input symbol '{}' can't " \
+                  "decide which reduction to perform: {}." \
+                  .format(str(state), state.state_id, state.symbol, term,
+                          prod_str)
+        if self.dynamic:
+            message += " Dynamic disambiguation strategy will be called."
+        self.message = message
+
+
+class LRConflicts(Exception):
+    def __init__(self, conflicts):
+        self.conflicts = conflicts
+        message = "\n{} conflicts in following states: {}"\
+                  .format(self.kind,
+                          set([c.state.state_id for c in conflicts]))
+        super(LRConflicts, self).__init__(message)
+
+
+class SRConflicts(LRConflicts):
+    kind = "Shift/Reduce"
+
+
+class RRConflicts(LRConflicts):
+    kind = "Reduce/Reduce"
