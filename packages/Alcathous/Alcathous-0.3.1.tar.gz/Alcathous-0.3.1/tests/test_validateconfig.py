1618e4d4a983e6278fd82a924e35161ac7171927
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:31:27 +0000

    Add package Alcathous/0.3.1/Alcathous-0.3.1.tar.gz

---
+++ packages/Alcathous/Alcathous-0.3.1.tar.gz/Alcathous-0.3.1/tests/test_validateconfig.py
@@ -0,0 +1,17 @@
+import unittest
+import os
+import sys
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+from pelops.read_config import read_config, validate_config
+from alcathous.datapointmanager import DataPointManager
+
+
+class TestValidateConfig(unittest.TestCase):
+    def test_validate_config(self):
+        config = read_config("config.yaml")
+        validation_result = validate_config(config, DataPointManager._get_schema())
+        self.assertIsNone(validation_result)
+
+
+if __name__ == '__main__':
+    unittest.main()
