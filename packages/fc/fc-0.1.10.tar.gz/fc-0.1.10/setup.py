c81ea4a6013a5f69fa6e13385f65f67c7a173081
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:56 +0000

    Add package fc/0.1.10/fc-0.1.10.tar.gz

---
+++ packages/fc/fc-0.1.10.tar.gz/fc-0.1.10/setup.py
@@ -0,0 +1,26 @@
+import os
+from setuptools import setup, find_packages
+
+setup(
+    name='fc',
+    version='0.1.10',
+    description='Enables Python to be functionally chain-styled.',
+    long_description=open(os.path.join(os.path.dirname(__file__),
+                                       'README.rst')).read(),
+    python_requires='>=3.4',
+    url='https://github.com/Thoxvi/Function-Chain-Python',
+    author='Thoxvi',
+    author_email='A@Thoxvi.com',
+
+    classifiers=[
+        'Programming Language :: Python',
+        'Programming Language :: Python :: Implementation',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Software Development :: Libraries'
+    ],
+    keywords='lambda function',
+
+    packages=["fc"],
+)
