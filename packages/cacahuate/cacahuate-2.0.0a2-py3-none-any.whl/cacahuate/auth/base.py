0a0cae93f793755148105538b3737ca0be837305
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:30 +0000

    Add package cacahuate/2.0.0a2/cacahuate-2.0.0a2-py3-none-any.whl

---
+++ packages/cacahuate/cacahuate-2.0.0a2-py3-none-any.whl/cacahuate/auth/base.py
@@ -0,0 +1,26 @@
+from cacahuate.models import User
+
+
+class BaseAuthProvider:
+
+    def __init__(self, config):
+        self.config = config
+
+    def authenticate(self, **credentials) -> User:
+        raise NotImplementedError('Must be implemented in subclasses')
+
+
+class BaseHierarchyProvider:
+
+    def __init__(self, config):
+        self.config = config
+
+    def validate_user(self, user, **params):
+        ''' given a user, should rise an exception if the user does not match
+        the hierarchy conditions required via params '''
+        raise NotImplementedError('Must be implemented in subclasses')
+
+    def find_users(self, **params) -> [User]:
+        ''' given the params, retrieves the
+        user identifiers that match them '''
+        raise NotImplementedError('Must be implemented in subclasses')
