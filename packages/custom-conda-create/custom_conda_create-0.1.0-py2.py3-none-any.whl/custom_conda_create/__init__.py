451e8c305ebccf0f096bd984ab0cb9f4b6c12d9e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:30 +0000

    Add package custom-conda-create/0.1.0/custom_conda_create-0.1.0-py2.py3-none-any.whl

---
+++ packages/custom-conda-create/custom_conda_create-0.1.0-py2.py3-none-any.whl/custom_conda_create/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+"""
+Top-level package for Custom conda create.
+"""
+
+__author__ = """Bjoern Kolja Mielsch"""
+__email__ = 'kolja.mielsch@gmail.com'
+__version__ = '0.1.0'
