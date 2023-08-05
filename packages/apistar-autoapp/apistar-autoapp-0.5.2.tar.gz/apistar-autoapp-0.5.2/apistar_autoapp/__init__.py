ec5450b033172f9439770a0423e3d41cb61a0a1c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:32:47 +0000

    Add package apistar-autoapp/0.5.2/apistar-autoapp-0.5.2.tar.gz

---
+++ packages/apistar-autoapp/apistar-autoapp-0.5.2.tar.gz/apistar-autoapp-0.5.2/apistar_autoapp/__init__.py
@@ -0,0 +1,9 @@
+from apistar_autoapp.autoapp import AutoApp, AutoASyncApp, app_args
+from apistar_autoapp.printer import Printer
+
+__version__ = '0.5.2'
+
+
+__all__ = [
+    'app_args', 'AutoApp', 'AutoASyncApp', 'Printer'
+]
