cae67cfda7af0ba7b392be8263c67433d796c51e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:09 +0000

    Add package pytest-play/1.4.2/pytest-play-1.4.2.tar.gz

---
+++ packages/pytest-play/pytest-play-1.4.2.tar.gz/pytest-play-1.4.2/pytest_play/providers/include.py
@@ -0,0 +1,14 @@
+import os
+from . import BaseProvider
+
+
+class IncludeProvider(BaseProvider):
+    """ PlayEngine wrapper """
+
+    def command_include(self, command, **kwargs):
+        """ Include scenario """
+        file_path = os.path.normcase(command['path'])
+        data = self.engine.get_file_contents(file_path)
+        self.engine.execute(
+            self.engine.parametrizer.parametrize(data)
+        )
