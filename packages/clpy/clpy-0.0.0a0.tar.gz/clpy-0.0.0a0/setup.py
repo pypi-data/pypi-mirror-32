96c510a66a700b2cb561b81b8f4f8a972c0e29c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:10:08 +0000

    Add package clpy/0.0.0a0/clpy-0.0.0a0.tar.gz

---
+++ packages/clpy/clpy-0.0.0a0.tar.gz/clpy-0.0.0a0/setup.py
@@ -0,0 +1,13 @@
+import setuptools
+
+setuptools.setup(
+    name='clpy',
+    version="0.0.0a0",
+    description='ClPy: OpenCL backend for CuPy',
+    url='https://github.com/fixstars/clpy',
+    author='The University of Tokyo, National Institute of Advanced Industrial Science and Technology, Fixstars Corporation',
+    maintainer='Fixstars Corporation',
+    maintainer_email='clpy@fixstars.com',
+    license='MIT License',
+	packages=setuptools.find_packages(),
+)
