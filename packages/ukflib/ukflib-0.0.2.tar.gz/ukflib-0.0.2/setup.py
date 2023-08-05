05f02b6880286ec5fdff9ac1efdac6b0ad26cb3f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:06 +0000

    Add package ukflib/0.0.2/ukflib-0.0.2.tar.gz

---
+++ packages/ukflib/ukflib-0.0.2.tar.gz/ukflib-0.0.2/setup.py
@@ -0,0 +1,25 @@
+
+from setuptools import setup
+
+
+setup(
+    name="ukflib",
+    author="Chris Taylor",
+    author_email="sciencectn@gmail.com",
+    description="An Unscented Kalman Filter library that allows for nonadditive process and measurement noise",
+    keywords="ukf unscented kalman filter",
+    version="0.0.2",
+    py_modules=["ukflib"],
+    install_requires=["numpy>=1.14.0",
+                      "matplotlib>=2.2.0",
+                      "scipy>=1.0.0"],
+    url="https://github.com/sciencectn/ukflib",
+    classifiers=[
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 2.7",
+        "Topic :: Scientific/Engineering",
+        "Intended Audience :: Science/Research"
+    ]
+)
+
+
