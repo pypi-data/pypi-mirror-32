4f579d9fd5c23687c2f17e5ea96fa3d49186949c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:16:12 +0000

    Add package mypackage12930123912903/0.1.3/mypackage12930123912903-0.1.3.tar.gz

---
+++ packages/mypackage12930123912903/mypackage12930123912903-0.1.3.tar.gz/mypackage12930123912903-0.1.3/setup.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+packages = \
+['mypackage12930123912903']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'mypackage12930123912903',
+    'version': '0.1.3',
+    'description': '',
+    'long_description': None,
+    'author': 'Your Name',
+    'author_email': 'you@example.com',
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>= 2.7.0.0, < 2.8.0.0',
+}
+
+
+setup(**setup_kwargs)
