40ee8cd179112579ed3c4745632b51ae6442b9df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:53 +0000

    Add package statusurl/1.0.1/statusurl-1.0.1.tar.gz

---
+++ packages/statusurl/statusurl-1.0.1.tar.gz/statusurl-1.0.1/statusurl.py
@@ -0,0 +1,17 @@
+import requests
+
+'''test url in list'''
+def print_list(the_list):
+    try:
+        for each_item in the_list:
+            if isinstance (each_item, list):
+                print_list(each_item, end=" status: ")
+                r = requests.get('{}'.format(each_item))
+                print(r.status_code)
+            else:
+                print(each_item, end=" status: ")
+                r = requests.get('{}'.format(each_item))
+                print(r.status_code)
+    except:
+        print("url not exist :(")    
+
