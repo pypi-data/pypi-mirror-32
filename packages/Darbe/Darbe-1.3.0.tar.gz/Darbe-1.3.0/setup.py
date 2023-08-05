1cd02327bdf9a1c912240b9ee503f122719606eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:14 +0000

    Add package Darbe/1.3.0/Darbe-1.3.0.tar.gz

---
+++ packages/Darbe/Darbe-1.3.0.tar.gz/Darbe-1.3.0/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup(
+    name='Darbe',
+    version='1.3.0',
+    author='Cenk Altı',
+    author_email='cenkalti@gmail.com',
+    keywords='mysql rds migration database replication slave',
+    url='https://github.com/cenk/darbe',
+    py_modules=['darbe'],
+    install_requires=[
+        'boto3',
+        'pymysql',
+    ],
+    description='RDS MySQL replication setup tool',
+    entry_points={
+        'console_scripts': [
+            'darbe = darbe:main',
+        ],
+    },
+)
