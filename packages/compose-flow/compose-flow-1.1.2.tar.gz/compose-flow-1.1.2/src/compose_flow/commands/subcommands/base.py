6663e8221baae20a6edd71f932fb3637fdc278a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:27:00 +0000

    Add package compose-flow/1.1.2/compose-flow-1.1.2.tar.gz

---
+++ packages/compose-flow/compose-flow-1.1.2.tar.gz/compose-flow-1.1.2/src/compose_flow/commands/subcommands/base.py
@@ -0,0 +1,119 @@
+from abc import ABC, abstractclassmethod
+
+from compose_flow.errors import CommandError, EnvError, NotConnected, ProfileError
+
+class BaseSubcommand(ABC):
+    """
+    Parent class for any subcommand class
+    """
+    dirty_working_copy_okay = False
+
+    def __init__(self, workflow):
+        self.profile = None  # populated in run()
+        self.workflow = workflow
+
+    @property
+    def args(self):
+        return self.workflow.args
+
+    def _check_args(self):
+        """
+        Checks and transforms the command line arguments
+        """
+        args = self.workflow.args
+
+        if None in (args.environment,):
+            raise CommandError('Error: environment is required')
+
+        args.profile = args.profile or args.environment
+
+    def get_subcommand(self, name:str) -> object:
+        """
+        Returns the requested subcommand class by name
+        """
+        from . import get_subcommand_class
+
+        subcommand_cls = get_subcommand_class(name)
+
+        return subcommand_cls(self.workflow)
+
+    @property
+    def env(self):
+        """
+        Returns an Env instance
+        """
+        # avoid circular import
+        from .env import Env
+
+        return Env(self.workflow)
+
+    @property
+    def env_name(self):
+        args = self.workflow.args
+
+        return f'{args.environment}-{args.project_name}'
+
+    @abstractclassmethod
+    def fill_subparser(cls, parser, subparser):
+        """
+        Stub for adding arguments to this subcommand's subparser
+        """
+
+    def handle(self):
+        return self.handle_action()
+
+    def handle_action(self):
+        action = self.workflow.args.action
+
+        action_fn = getattr(self, action, None)
+        if action_fn:
+            return action_fn()
+        else:
+            self.print_subcommand_help(self.__doc__, error=f'unknown action={action}')
+
+    def is_dirty_working_copy_okay(self, exc):
+        return self.dirty_working_copy_okay
+
+    def is_env_error_okay(self, exc):
+        return False
+
+    def is_missing_profile_okay(self, exc):
+        return False
+
+    def is_write_profile_error_okay(self, exc):
+        return False
+
+    def print_subcommand_help(self, doc, error=None):
+        print(doc.lstrip())
+
+        self.workflow.parser.print_help()
+
+        if error:
+            return f'\nError: {error}'
+
+    def run(self, *args, **kwargs):
+        self._check_args()
+
+        try:
+            self._write_profile()
+        except (EnvError, NotConnected, ProfileError) as exc:
+            if not self.is_write_profile_error_okay(exc):
+                raise
+
+        return self.handle(*args, **kwargs)
+
+    @classmethod
+    def setup_subparser(cls, parser, subparsers):
+        name = cls.__name__.lower()
+        aliases = getattr(cls, 'aliases', [])
+
+        subparser = subparsers.add_parser(name, aliases=aliases)
+        subparser.set_defaults(subcommand_cls=cls)
+
+        cls.fill_subparser(parser, subparser)
+
+    def _write_profile(self):
+        from .profile import Profile
+
+        self.profile = Profile(self.workflow)
+        self.profile.write()
---
+++ packages/compose-flow/compose-flow-1.1.2.tar.gz/compose-flow-1.1.2/src/compose_flow/commands/subcommands/env.py
@@ -0,0 +1,186 @@
+"""
+Env subcommand
+"""
+import io
+import logging
+import os
+import shlex
+import sys
+import tempfile
+
+import sh
+
+from .base import BaseSubcommand
+
+from compose_flow import docker, errors
+
+
+class Env(BaseSubcommand):
+    """
+    Subcommand for managing environment
+    """
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, *kwargs)
+
+        self._config = None
+
+    @classmethod
+    def fill_subparser(cls, parser, subparser):
+        subparser.add_argument('action')
+        subparser.add_argument('path', nargs='*')
+
+    def cat(self) -> str:
+        """
+        Prints the loaded config to stdout
+        """
+        if self.env_name not in docker.get_configs():
+            return f'docker config named {self.env_name} not in swarm'
+
+        print(self.render())
+
+    @property
+    def data(self) -> dict:
+        """
+        Returns the loaded config as a dictionary
+        """
+        data = {}
+
+        env = self.load()
+        for line in env.splitlines():
+            # skip empty lines
+            if line.strip() == '':
+                continue
+
+            # skip commented lines
+            if line.startswith('#'):
+                continue
+
+            try:
+                key, value = line.split('=', 1)
+            except ValueError as exc:
+                print(f'unable to split line={line}')
+                raise
+
+            data[key] = value
+
+        # TODO: this is hacky because of the back-and-forth relationship
+        # between data() and load() ... gotta fix this.
+        docker_image = data.get('DOCKER_IMAGE')
+        if 'VERSION' in data and docker_image and ':' in docker_image:
+            data['DOCKER_IMAGE'] = f'{docker_image.split(":", 1)[0]}:{data["VERSION"]}'
+
+        data['CF_ENV_NAME'] = self.env_name
+
+        return data
+
+    def edit(self) -> None:
+        with tempfile.NamedTemporaryFile('w') as fh:
+            path = fh.name
+
+            self.render_buf(fh)
+
+            fh.flush()
+
+            editor = os.environ.get('EDITOR', os.environ.get('VISUAL', 'vi'))
+
+            command = shlex.split(f'{editor} {path}')
+
+            # os.execve(command[0], command, os.environ)
+            proc = getattr(sh, command[0])
+            proc(*command[1:], _env=os.environ, _fg=True)
+
+            self.push(path)
+
+    def is_dirty_working_copy_okay(self, exc):
+        return self.workflow.args.action in ('cat', 'push')
+
+    def is_env_error_okay(self, exc):
+        return self.workflow.args.action in ('push',)
+
+    def is_write_profile_error_okay(self, exc):
+        return self.workflow.args.action in ('push',)
+
+    def load(self) -> str:
+        """
+        Loads an environment from the docker swarm config
+        """
+        if self._config:
+            return self._config
+
+        self._config = docker.get_config(self.env_name)
+
+        # inject the version from tag-version command into the loaded environment
+        tag_version = 'unknown'
+        try:
+            tag_version_command = getattr(sh, 'tag-version')
+        except Exception as exc:
+            print(f'Warning: unable to find tag-version ({exc})\n', file=sys.stderr)
+        else:
+            try:
+                tag_version = tag_version_command().stdout.decode('utf8').strip()
+            except Exception as exc:
+                # check if the subcommand is okay with a dirty working copy
+                if not self.workflow.subcommand.is_dirty_working_copy_okay(exc):
+                    raise errors.TagVersionError(f'Warning: unable to run tag-version ({exc})\n')
+
+        data = self.data
+
+        version_var = 'VERSION'
+        data[version_var] = tag_version
+        self._config = self.render(data)
+
+        return self._config
+
+    @property
+    def logger(self):
+        return logging.getLogger(f'{__name__}.{self.__class__.__name__}')
+
+    def push(self, path:str=None) -> None:
+        """
+        Saves an environment into the swarm
+        """
+        path = path or self.args.path
+        if not path:
+            return self.print_subcommand_help(__doc__, error='path needed to load')
+
+        docker.load_config(self.env_name, path)
+
+    def render(self, data:dict=None) -> str:
+        """
+        Returns a rendered file in .env file format
+        """
+        buf = io.StringIO()
+
+        self.render_buf(buf, data=data)
+
+        return buf.getvalue()
+
+    def render_buf(self, buf, data: dict=None):
+        data = data or self.data
+        for k, v in data.items():
+            buf.write(f'{k}={v}\n')
+
+    def rm(self) -> None:
+        """
+        Removes an environment from the swarm
+        """
+        docker.remove_config(self.env_name)
+
+    def write_tag(self) -> None:
+        """
+        Writes the projects tag version into the environment
+
+        This currently writes the tag to the `DOCKER_IMAGE` variable
+        """
+        data = self.data
+
+        image_base = data['DOCKER_IMAGE'].rsplit(':', 1)[0]
+        data['DOCKER_IMAGE'] = f'{image_base}:{data["VERSION"]}'
+
+        with tempfile.NamedTemporaryFile('w+') as fh:
+            fh.write(self.render(data))
+            fh.flush()
+
+            fh.seek(0, 0)
+
+            self.push(path=fh.name)
