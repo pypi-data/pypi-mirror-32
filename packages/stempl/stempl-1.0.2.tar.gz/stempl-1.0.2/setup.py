577d2885c96f5df351df5dc4f0fd5cef97468528
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:36:58 +0000

    Add package stempl/1.0.2/stempl-1.0.2.tar.gz

---
+++ packages/stempl/stempl-1.0.2.tar.gz/stempl-1.0.2/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+
+with open('README.md', 'rt', encoding='utf8') as f:
+    readme = f.read()
+
+
+setup(
+    name='stempl',
+    version='1.0.2',
+    description='HTML templates with just Python',
+    long_description=readme,
+    url='http://www.github.com/gabrielhora/stempl',
+    author='Gabriel Hora',
+    author_email='gabriel@switchpayments.com',
+    license='MIT',
+    packages=['stempl'],
+    include_package_data=True,
+    zip_safe=False,
+    tests_require=['tox==3.0.0'],
+    install_requires=[],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+    ],
+    keywords='html template'
+)
---
+++ packages/stempl/stempl-1.0.2.tar.gz/stempl-1.0.2/stempl/__init__.py
@@ -0,0 +1,1 @@
+from stempl.stempl import *
