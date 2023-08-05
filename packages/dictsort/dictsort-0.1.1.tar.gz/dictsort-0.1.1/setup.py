a649a5c139934a38174d8584a0ed17ca67d770ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:16 +0000

    Add package dictsort/0.1.1/dictsort-0.1.1.tar.gz

---
+++ packages/dictsort/dictsort-0.1.1.tar.gz/dictsort-0.1.1/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+setup(
+    name='dictsort',
+    version='0.1.1',
+    keywords=("pip"),
+    description='sort dict data',
+    url='https://github.com/danyubaoqi/dictsort',
+    author='danyubaoqi',
+    author_email='1600093003@pku.edu.cn',
+    license='MIT',
+    zip_safe=False,
+    classifiers=['Development Status :: 4 - Beta',
+                 'Operating System :: OS Independent',
+                 'Intended Audience :: Developers',
+                 'License :: OSI Approved :: BSD License',
+                 'Programming Language :: Python',
+                 'Programming Language :: Python :: Implementation',
+                 'Programming Language :: Python :: 2',
+                 'Programming Language :: Python :: 2.7',
+                 'Programming Language :: Python :: 3',
+                 'Programming Language :: Python :: 3.4',
+                 'Programming Language :: Python :: 3.5',
+                 'Programming Language :: Python :: 3.6',
+                 'Topic :: Software Development :: Libraries'
+                 ]
+)
