74da85cd27392106e6ae68de26422c6724bf7c0c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:17:25 +0000

    Add package karaage/5.0.6/karaage-5.0.6.tar.gz

---
+++ packages/karaage/karaage-5.0.6.tar.gz/karaage-5.0.6/scripts/docker.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+set -e
+
+start_slurm
+
+chmod go+rX -R /opt/karaage/
+if test "$1" = "apache"
+then
+    chmod 644 /etc/apache2/conf-available/karaage3-wsgi.conf
+fi
+
+install -d -o www-data -g www-data /var/cache/karaage3
+install -d -o www-data -g www-data /var/log/karaage3
+install -d -o www-data -g www-data /var/lib/karaage3/files
+install -d -o root -g root /var/lib/karaage3/static
+
+python3 manage.py collectstatic --noinput
+
+if test "$1" = "apache"
+then
+    install -d -o www-data -g www-data /var/log/apache2
+    ./scripts/start.sh "$@"
+else
+    sudo -u www-data -E ./scripts/start.sh "$@"
+fi
