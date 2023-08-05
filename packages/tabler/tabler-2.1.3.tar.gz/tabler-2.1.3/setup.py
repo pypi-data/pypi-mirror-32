195c582222853d7d426412dc83be2995d5a851b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:32:37 +0000

    Add package tabler/2.1.3/tabler-2.1.3.tar.gz

---
+++ packages/tabler/tabler-2.1.3.tar.gz/tabler-2.1.3/setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+"""Setup for tabler package."""
+
+import setuptools
+
+with open("README.rst", "r") as readme:
+    long_description = readme.read()
+
+setuptools.setup(
+    name="tabler",
+    version="2.1.3",
+    description="Simple interface for tabulated data and .csv files",
+    long_description=long_description,
+    url="https://github.com/axevalley/tabler.git",
+    author="Luke Shiner",
+    author_email="luke@lukeshiner.com",
+    keywords=["table", "csv", "simple"],
+    install_requires=[
+        "requests", "ezodf", "lxml", "openpyxl", "pyexcel_ods3", "jinja2"],
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    python_requires='>=3.6.0',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Development Status :: 4 - Beta"],
+    )
---
+++ packages/tabler/tabler-2.1.3.tar.gz/tabler-2.1.3/tabler/__init__.py
@@ -0,0 +1,32 @@
+"""
+Tabler package.
+
+The tabler package provides the :class:`tabler.Table` class for simple and
+intutive accessing, manipulation and writing of tablulated data.
+
+    Basic Usage::
+
+        >>> from tabler import Table
+        >>> table = Table('somefile.csv')
+        >>> table.open('Path/To/Input_File.csv')
+        >>> table[0]['Price']
+        '29.99'
+        >>> table[0]['Price'] = 15.50
+        >>> table[0]['Price']
+        '15.5'
+        >>> table.write('Path/To/Output_File')
+        Writen 3 lines to file Path/To/Output_File.csv
+
+"""
+
+__title__ = 'tabler'
+__version__ = '2.0'
+__author__ = 'Luke Shiner'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2018 Luke Shiner'
+
+from . table import Table  # NOQA
+from . tabletypes import *  # NOQA
+from . tohtml import ToHTML  # NOQA
+
+Tabler = Table  # Backwards compatibility with v1.*
