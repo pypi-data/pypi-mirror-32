7e6275eb6cba11447db18b10371152c2af596a1a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:09:29 +0000

    Add package star-builder/0.1.9/star-builder-0.1.9.tar.gz

---
+++ packages/star-builder/star-builder-0.1.9.tar.gz/star-builder-0.1.9/star_builder/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding:utf-8 -*-
+from .build import main
+from .app import Application
+from .bases.service import Service
+from .bases.components import Component
+from .route import route, get, post, delete, put, options
+
+
+__version__ = "0.1.9"
