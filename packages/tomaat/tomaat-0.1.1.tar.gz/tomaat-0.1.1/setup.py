a4bee79e39c9b3deae126bd5e7fc81a2ca50c22f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:37 +0000

    Add package tomaat/0.1.1/tomaat-0.1.1.tar.gz

---
+++ packages/tomaat/tomaat-0.1.1.tar.gz/tomaat-0.1.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='tomaat',
+    version="0.1.1",
+    description='TOMAAT SERVER SIDE',
+    url='https://github.com/fmilletari/tomaat',
+    author='Fausto Milletari',
+    author_email='tomaat.segmentation@gmail.com',
+    packages=find_packages(),
+)
