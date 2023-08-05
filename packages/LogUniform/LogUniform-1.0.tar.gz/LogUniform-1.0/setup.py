be7b4eeba9ef28cfc2629275d644be41f69e18f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:27:40 +0000

    Add package LogUniform/1.0/LogUniform-1.0.tar.gz

---
+++ packages/LogUniform/LogUniform-1.0.tar.gz/LogUniform-1.0/setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+setup(name='LogUniform',
+      version='1.0',
+      description='Implementation of the log-uniform and modified log-uniform distributions',
+      author='João Faria',
+      author_email='joao.faria@astro.up.pt',
+      url='https://github.com/j-faria/LogUniform',
+      packages=['loguniform'],
+      setup_requires=['pytest-runner',],
+      tests_require=['pytest',],
+      classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+     )
