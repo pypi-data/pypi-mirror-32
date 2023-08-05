f2de09910a2077f6e7de7c74cae6a99b7754a20e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:36:51 +0000

    Add package nwapp/1.1/nwapp-1.1.zip

---
+++ packages/nwapp/nwapp-1.1.zip/nwapp-1.1/nwapp/scripts/classfiles.py
@@ -0,0 +1,17 @@
+
+
+
+import os
+import sys
+
+class Contract_Under_Discussion:
+
+    # Adding a new contract under discussion
+
+    # Editing a new contract under discussion
+
+    # Delete a new contract under discussion
+
+    # Displaying a new contract under discussion
+
+    # Printing to a file all or partial contracts under discussion to a JSON file
---
+++ packages/nwapp/nwapp-1.1.zip/nwapp-1.1/nwapp/scripts/nw.py
@@ -0,0 +1,43 @@
+
+
+import os
+import sys
+import argparse
+#from contract_process import *
+from pymongo import MongoClient
+from pprint import pprint
+import datetime
+import click
+
+class Config(object):
+
+    def __init__(self):
+        self.verbose = False
+
+
+pass_config = click.make_pass_decorator(Config, ensure=True)
+
+@click.group()
+@click.option('--verbose', is_flag=True)
+@pass_config
+def cli(config, verbose):
+    config.verbose = verbose
+    if config.verbose:
+        click.echo('We are in verbose mode.')
+
+@cli.command()
+@click.option('-s','--string', default='World', help='This is the thing that is greeted.')
+@click.option('-r','--repeat', default=1, help='How many types you wish to repeat the greetings?')
+@click.argument('out', type=click.File('w'), default='-', required=False)
+@pass_config
+def cud(config, string, repeat, out):
+    """This function greets the user"""
+    '''
+    if config.verbose:
+        click.echo('We are in say verbose mode')
+    for x in range(repeat):
+        click.echo('Hello {}!'.format(string), file=out)
+    '''
+@cud.command()
+def edit():
+    pass
