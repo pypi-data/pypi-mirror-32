3acc24757e09a9fc0be861f1721185c991fec125
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:46 +0000

    Add package FiddleOptions/1.0.3/FiddleOptions-1.0.3.tar.gz

---
+++ packages/FiddleOptions/FiddleOptions-1.0.3.tar.gz/FiddleOptions-1.0.3/swagger_client/utils.py
@@ -0,0 +1,19 @@
+from datetime import date
+# from swagger_client.mercury.client.api.marketdata_api import MarketdataApi
+
+
+class Utils:
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def create_option_leg_with_option_contract(option_contract,
+                                               direction,
+                                               quantity):
+        from swagger_client.mercury.client.model import OptionLeg
+
+        return OptionLeg(expiration_date=option_contract.expiration_date,
+                         direction=direction,
+                         type='CALL' if option_contract.call else 'PUT',
+                         quantity=quantity,
+                         strike=option_contract.strike)
