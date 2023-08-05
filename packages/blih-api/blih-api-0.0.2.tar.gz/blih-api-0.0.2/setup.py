49de8ac729a1c758b1b3e952a317f30b93d35ec7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:09 +0000

    Add package blih-api/0.0.2/blih-api-0.0.2.tar.gz

---
+++ packages/blih-api/blih-api-0.0.2.tar.gz/blih-api-0.0.2/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+    name="blih-api",
+    packages=["blih_api"],
+    entry_points={
+        'console_scripts': [
+            'blih-api = blih_api.__main__:main',
+        ]
+    },
+    version='0.0.2',
+    description="Blih API - Epitech Blih Library",
+    long_description="Interact easily with the blih API",
+    author="SakiiR SakiiR (@SakiiR)",
+    url="https://github.com/SakiiR/blih-api",
+    install_requires=[
+        'requests',
+    ],
+)
