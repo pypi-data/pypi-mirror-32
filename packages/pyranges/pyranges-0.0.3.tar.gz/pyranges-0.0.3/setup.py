5aa0afb0ced8deaf0f281f36a0e27b2540c1a92b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:13:02 +0000

    Add package pyranges/0.0.3/pyranges-0.0.3.tar.gz

---
+++ packages/pyranges/pyranges-0.0.3.tar.gz/pyranges-0.0.3/setup.py
@@ -0,0 +1,36 @@
+import os
+import sys
+
+from distutils.core import setup
+from setuptools import find_packages
+
+install_requires = ["cython", "pandas", "ncls", "clustertree", "tabulate", "pysam", "sorted_nearest"]
+
+from pyranges.version import __version__
+
+setup(
+    name="pyranges",
+    packages=find_packages(),
+    package_data={'pyranges': ['example_data/*.bed']},
+    include_dirs=["."],
+    version="0.0.3",
+    description="PyRanges for Python.",
+    author="Endre Bakken Stovner",
+    author_email="endrebak85@gmail.com",
+    url="http://github.com/endrebak/pyranges",
+    keywords=["Bioinformatics"],
+    license=["MIT"],
+    install_requires=install_requires,
+    classifiers=[
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Development Status :: 4 - Beta",
+        "Environment :: Other Environment", "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: POSIX :: Linux",
+        "Operating System :: MacOS :: MacOS X",
+        "Topic :: Scientific/Engineering"
+    ],
+    long_description=
+    ("Performant Pythonic Genomic Ranges."))
