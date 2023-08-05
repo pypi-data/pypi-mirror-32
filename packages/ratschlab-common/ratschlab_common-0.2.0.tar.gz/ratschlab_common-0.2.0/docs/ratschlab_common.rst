1a6efdaac7299dd51f181ea0e795a0afbe411310
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:25:05 +0000

    Add package ratschlab-common/0.2.0/ratschlab_common-0.2.0.tar.gz

---
+++ packages/ratschlab-common/ratschlab_common-0.2.0.tar.gz/ratschlab_common-0.2.0/docs/ratschlab_common.rst
@@ -0,0 +1,18 @@
+ratschlab\_common package
+=========================
+
+Subpackages
+-----------
+
+.. toctree::
+
+    ratschlab_common.collections
+    ratschlab_common.io
+
+Module contents
+---------------
+
+.. automodule:: ratschlab_common
+    :members:
+    :undoc-members:
+    :show-inheritance:
---
+++ packages/ratschlab-common/ratschlab_common-0.2.0.tar.gz/ratschlab_common-0.2.0/setup.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""The setup script."""
+
+from setuptools import setup, find_packages
+
+with open('README.rst') as readme_file:
+    readme = readme_file.read()
+
+setup_requirements = ['pytest-runner']
+
+with open('requirements.txt') as f:
+    requirements = list(f.readlines())
+
+test_requirements = ['pytest']
+
+setup(
+    author="ETH Zurich, Biomedical Informatics Group",
+    author_email='marc.zimmermann@inf.ethz.ch',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6'
+    ],
+    description="Small library of common functionalities used in various projects in the ratschlab",
+    entry_points={
+        'console_scripts': [
+            'bigmatrix_repack=ratschlab_common.scripts.bigmatrix_repack:main',
+        ],
+    },
+    install_requires=requirements,
+    license="MIT license",
+    long_description=readme,
+    include_package_data=True,
+    keywords='ratschlab_common',
+    name='ratschlab_common',
+    packages=find_packages(),
+    setup_requires=setup_requirements,
+    test_suite='tests',
+    tests_require=test_requirements,
+    url='https://github.com/ratschlab/ratschlab-common',
+    version='0.2.0',
+    zip_safe=False,
+)
