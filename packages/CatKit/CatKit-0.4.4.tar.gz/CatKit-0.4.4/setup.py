278dd8405818062352e429465b2321616b141596
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:43 +0000

    Add package CatKit/0.4.4/CatKit-0.4.4.tar.gz

---
+++ packages/CatKit/CatKit-0.4.4.tar.gz/CatKit-0.4.4/setup.py
@@ -0,0 +1,41 @@
+import setuptools
+
+with open('requirements.txt', 'r') as f:
+    requirements = f.readlines()
+
+setuptools.setup(
+    name="CatKit",
+    version="0.4.4",
+    url="https://github.com/SUNCAT-Center/CatKit",
+
+    author="Jacob Boes",
+    author_email="jrboes@stanford.edu",
+
+    description="General purpose tools for high-throughput catalysis.",
+    license='GPL-3.0',
+
+    packages=[
+        'catkit',
+        'catkit.pawprint',
+        'catkit.gen',
+        'catkit.gen.api',
+        'catkit.flow'
+    ],
+    package_dir={'catkit': 'catkit'},
+
+    install_requires=requirements,
+    python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*, <4',
+
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Topic :: Scientific/Engineering :: Chemistry',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+)
