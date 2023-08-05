3b7a3df4adbabb5ff8c62c6193b027dea5cab7f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:21 +0000

    Add package mama/0.1.0a1/mama-0.1.0a1.tar.gz

---
+++ packages/mama/mama-0.1.0a1.tar.gz/mama-0.1.0a1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(name='mama',
+      version='0.1.0a1',
+      description='A C++ build tool even your mama can use',
+      url='https://github.com/RedFox20/Mama',
+      author='Jorma Rebane',
+      author_email='jorma.rebane@gmail.com',
+      license='MIT',
+      packages=['mama'],
+      entry_points = { 'console_scripts': ['mama=mama.main:main'], },
+      zip_safe=False,
+      python_requires='>=3.6',
+      classifiers=[],
+      keywords=['mama', 'build', 'mamabuild', 'c', 'c++', 'tool', 'cmake', 'simple', 'easy', 'cross-platform'],
+)
