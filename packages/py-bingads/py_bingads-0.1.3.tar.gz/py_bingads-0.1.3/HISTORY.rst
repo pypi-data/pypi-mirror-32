6f33a2dad8884af5f010e1d09c5865e5f773059b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:09:49 +0000

    Add package py-bingads/0.1.3/py_bingads-0.1.3.tar.gz

---
+++ packages/py-bingads/py_bingads-0.1.3.tar.gz/py_bingads-0.1.3/HISTORY.rst
@@ -0,0 +1,27 @@
+.. :changelog:
+
+Release History
+---------------
+
+0.1.3 (2018-05-28)
+++++++++++++++++++
+
+- Fix negative keywords
+
+
+0.1.2 (2018-05-28)
+++++++++++++++++++
+
+- Fix encoding issues
+
+
+0.1.1 (2018-05-28)
+++++++++++++++++++
+
+- Allow tuples to be passed as negative keywords
+
+
+0.1.0 (2018-02-23)
+++++++++++++++++++
+
+- Birth!
---
+++ packages/py-bingads/py_bingads-0.1.3.tar.gz/py_bingads-0.1.3/setup.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+import os
+import sys
+
+from codecs import open
+
+import setuptools
+
+
+if sys.argv[-1] == 'publish':
+    os.system('python setup.py sdist upload')
+    sys.exit()
+
+with open('README.rst', 'r', 'utf-8') as f:
+    readme = f.read()
+with open('HISTORY.rst', 'r', 'utf-8') as f:
+    history = f.read()
+
+package_name = 'py_bingads'
+
+packages = [package_name] + [
+    '%s.%s' % (package_name, item)
+    for item in
+    setuptools.find_packages('py_bingads')
+]
+
+setuptools.setup(
+    name=package_name,
+    version='0.1.3',
+    description='Wraps BingAds Python SDK to make it more Pythonic',
+    long_description=readme + '\n\n' + history,
+    author='Julie MacDonell',
+    author_email='julie.macdonell@stylight.com',
+    url='https://github.com/stylight/python-bingads',
+    download_url='https://github.com/stylight/python-bingads/archive/0.2.tar.gz',
+    license='MIT License',
+    packages=packages,
+    package_data={'': ['*.rst']},
+    include_package_data=True,
+    install_requires=[
+        'bingads==11.5.8',
+        'six',
+    ],
+    keywords='python bingads bing ads api'
+)
