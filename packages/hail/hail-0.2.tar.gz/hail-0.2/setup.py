8421d1fc08fab9663c34c6b605d96b0df152862b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:53 +0000

    Add package hail/0.2/hail-0.2.tar.gz

---
+++ packages/hail/hail-0.2.tar.gz/hail-0.2/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='hail',
+      version='0.2',
+      description='A library for scalable biological data analysis.',
+      url='https://www.hail.is',
+      author='Hail Team',
+      author_email='hail@broadinstitute.org',
+      license='MIT')
