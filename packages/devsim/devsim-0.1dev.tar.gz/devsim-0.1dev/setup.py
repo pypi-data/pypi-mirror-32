e159e7a1c7c2cee30056fa3531a3b22ca992a5b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:33 +0000

    Add package devsim/0.1dev/devsim-0.1dev.tar.gz

---
+++ packages/devsim/devsim-0.1dev.tar.gz/devsim-0.1dev/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='devsim',
+    packages=['devsim',],
+    version='0.1dev',
+    description='DEVSIM TCAD Simulator',
+    url = 'https://devsim.org',
+    license='Apache 2.0',
+    long_description=open('README.txt').read(),
+    author='Juan Sanchez',
+    author_email='info@devsim.com',
+)
