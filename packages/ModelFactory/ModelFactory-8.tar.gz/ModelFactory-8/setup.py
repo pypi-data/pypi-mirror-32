30ad8d64e592915ccd6fd3152d17d6e8799064b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:52 +0000

    Add package ModelFactory/8/ModelFactory-8.tar.gz

---
+++ packages/ModelFactory/ModelFactory-8.tar.gz/ModelFactory-8/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+setup(
+  name = 'ModelFactory',
+  packages = ['ModelFactory'], # this must be the same as the name above
+  version = '8',
+  description = 'ModelFactory is easy to use package that allows you to float around data till building a model on it.',
+  author = 'Sudarson',
+  author_email = 'write2sudarson@gmail.com',
+  url = 'https://github.com/sudarsontm/Python',
+  download_url = 'https://github.com/sudarsontm/Python/archive/MF_8.tar.gz',
+  keywords = ['tag1', 'tag2'],
+  classifiers = [],
+)
+
