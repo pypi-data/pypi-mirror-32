6a73635d00a9dc0d71a69345212dd35020fd482d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:29:54 +0000

    Add package branch-bound/0.1.3/Branch-Bound-0.1.3.tar.gz

---
+++ packages/branch-bound/Branch-Bound-0.1.3.tar.gz/Branch-Bound-0.1.3/Branch-Bound/Heuristic.py
@@ -0,0 +1,10 @@
+def compute_initial_heuristic(tree_seq, init_alg, edge_weight_func, cpu_affinity=1, 
+                                develop_tree=False, init_branching_factor=1,
+                                dynamic_heuristic=False, recursive_decisions=False):
+    return 0
+    
+def compute_recursive_heuristic(init_heuristic_object):
+    return 0
+
+def update_dynamic_bound(heuristic_object, bound):
+    return 0
