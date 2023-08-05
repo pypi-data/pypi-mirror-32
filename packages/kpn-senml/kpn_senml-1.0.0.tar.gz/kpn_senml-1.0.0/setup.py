34e404c31f78f45f7f010a97f3c4a5c91f512ad7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:36:01 +0000

    Add package kpn-senml/1.0.0/kpn_senml-1.0.0.tar.gz

---
+++ packages/kpn-senml/kpn_senml-1.0.0.tar.gz/kpn_senml-1.0.0/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name='kpn_senml',
+    version='1.0.0',
+    packages=['kpn_senml'],
+    url='https://github.com/jan-bogaerts/slate',
+    license='APACHE',
+    author='Jan Bogaerts',
+    author_email='jb@elastetic.com',
+    description='generate and parse senml json and cbor data',
+    long_description='With this library you can generate senml packs containing sensor data in both json and cbor format.  It can also parse senml data in both json and cbor format in order to support actuators.',
+    keywords='senml kpn cbor json',
+    install_requires=[
+          'cbor2',
+    ]
+)
