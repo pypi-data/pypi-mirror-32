9772d21032b3473caab556aced9cc575fecfc3b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:41 +0000

    Add package django_matialvarezs_grafana_customers/0.1.81/django_matialvarezs_grafana_customers-0.1.81.tar.gz

---
+++ packages/django_matialvarezs_grafana_customers/django_matialvarezs_grafana_customers-0.1.81.tar.gz/django_matialvarezs_grafana_customers-0.1.81/matialvarezs_grafana_customers/panels.py
@@ -0,0 +1,10 @@
+from . import models
+from . import utils
+from . import manage_dashboard
+def create_and_save_panel_database(object_project_dashboard_id,object_project_panel_id,panel_id,panel_content):
+    dashboard = models.DashboardGrafana.objects.get(object_project=object_project_dashboard_id)
+    panel = utils.get_or_none_panel_grafana(object_project=object_project_panel_id)
+    if not panel:
+        panel = models.PanelGrafana(object_project=object_project_panel_id,dashboard=dashboard,panel_id=panel_id,panel_content=panel_content)
+        panel.save()
+        manage_dashboard.update_dashboard(dashboard,panel)
