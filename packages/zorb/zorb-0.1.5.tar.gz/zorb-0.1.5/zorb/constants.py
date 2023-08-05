840260c59617324a3ba489d063b901fb591f2dc3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:51 +0000

    Add package zorb/0.1.5/zorb-0.1.5.tar.gz

---
+++ packages/zorb/zorb-0.1.5.tar.gz/zorb-0.1.5/zorb/constants.py
@@ -0,0 +1,8 @@
+# Copyright Somatic Technologies, Inc. 2018
+
+#################################################################################
+# Zorb Constants
+#################################################################################
+
+# Name used to recognize a Zorb device
+DEVICE_NAME = "Moment"
