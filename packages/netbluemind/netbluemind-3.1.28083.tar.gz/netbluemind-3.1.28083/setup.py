dd27b017a2753b76a95fb292e4ecdb96e83fb4aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:04 +0000

    Add package netbluemind/3.1.28083/netbluemind-3.1.28083.tar.gz

---
+++ packages/netbluemind/netbluemind-3.1.28083.tar.gz/netbluemind-3.1.28083/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'netbluemind',
+    packages = find_packages(),
+    version = '3.1.28083',
+    description = 'Automatically generated client for BlueMind REST API',
+    author = 'BlueMind team',
+    author_email = 'contact@bluemind.net',
+    url = 'http://git.blue-mind.net/gitlist/bluemind/',
+    keywords = ['bluemind', 'rest', 'api', 'mail', 'groupware'],
+    classifiers = [
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+    ],
+    install_requires=["enum34; python_version < '3.4'", 'requests'],
+)
