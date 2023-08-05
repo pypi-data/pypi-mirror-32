e27d5a450a13aa37433656631bfca6e521015e8b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:08 +0000

    Add package smart-router-api/1.1.0/smart_router_api-1.1.0-py2-none-any.whl

---
+++ packages/smart-router-api/smart_router_api-1.1.0-py2-none-any.whl/smart_router_api/router_status.py
@@ -0,0 +1,44 @@
+# coding=utf-8
+
+import netifaces as nf
+
+print dir(nf)
+
+def getRouter4G():
+    gateway = nf.gateways()
+    if nf.AF_INET in gateway:
+        return gateway[nf.AF_INET][0][1] == '3g-4g'
+    elif nf.AF_INET6 in gateway:
+        return gateway[AF_INET6][0][1] == '3g-4g'
+    else:
+        return False
+    pass
+def getRouterWired():
+    gateway = nf.gateways()
+    if nf.AF_INET in gateway:
+        return gateway[nf.AF_INET][0][1] == 'eth0.2'
+    elif nf.AF_INET6 in gateway:
+        return gateway[AF_INET6][0][1] == 'eth0.2'
+    else:
+        return False
+    pass
+def getRouterWireless():
+    gateway = nf.gateways()
+    if nf.AF_INET in gateway:
+        return gateway[nf.AF_INET][0][1] == 'wlan0'
+    elif nf.AF_INET6 in gateway:
+        return gateway[AF_INET6][0][1] == 'wlan0'
+    else:
+        return False
+    pass
+def getRouterGateway():
+    gateway = nf.gateways()
+    if gateway['default'] == {}:
+        return False
+    else:
+        return True
+    pass
+
+if __name__ == '__main__':
+    print('test:')
+
