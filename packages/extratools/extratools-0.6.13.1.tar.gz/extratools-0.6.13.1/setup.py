fda358e0b445bd27f5b80e509b12682909208b84
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:55 +0000

    Add package extratools/0.6.13.1/extratools-0.6.13.1.tar.gz

---
+++ packages/extratools/extratools-0.6.13.1.tar.gz/extratools-0.6.13.1/setup.py
@@ -0,0 +1,44 @@
+#! /usr/bin/env python3
+
+from setuptools import setup
+from glob import glob
+
+url = "https://github.com/chuanconggao/extratools"
+version = "0.6.13.1"
+
+setup(
+    name="extratools",
+
+    packages=["extratools"],
+    scripts=glob("bin/extratools-*"),
+    include_package_data=True,
+
+    url=url,
+
+    version=version,
+    download_url=f"{url}/tarball/{version}",
+
+    license="MIT",
+
+    author="Chuancong Gao",
+    author_email="chuancong@gmail.com",
+
+    description="Extra Functional Tools beyond Standard and Third-Party Libraries",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+
+    keywords=[
+        "functional",
+        "tools"
+    ],
+    classifiers=[
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3"
+    ],
+
+    python_requires=">= 3",
+    install_requires=[
+        line.strip() for line in open("requirements.txt")
+    ]
+)
