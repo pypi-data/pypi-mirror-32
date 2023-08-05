e977c1b2d92f51d5898ca442b5b9714b32d9f93e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:51 +0000

    Add package pyapputil/1.0.23/pyapputil-1.0.23.tar.gz

---
+++ packages/pyapputil/pyapputil-1.0.23.tar.gz/pyapputil-1.0.23/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+import os
+
+"""
+Publish by building the package:
+    python setup.py sdist
+and then uploading:
+    twine upload dist/<NAME>-<version>.tar.gz
+"""
+
+NAME = "pyapputil"
+setup(
+    name = NAME,
+    version = "1.0.23",
+    author = "Carl Seelye",
+    author_email = "cseelye@gmail.com",
+    description = "Tools for building CLI applications",
+    license = "MIT",
+    keywords = "cli arguments configuration logging signals threads time",
+    packages = [NAME],
+    url = "https://github.com/cseelye/{}".format(NAME),
+    long_description = open(os.path.join(os.path.dirname(__file__), "README.rst")).read(),
+    install_requires = open(os.path.join(os.path.dirname(__file__), "requirements.txt")).readlines()
+)
