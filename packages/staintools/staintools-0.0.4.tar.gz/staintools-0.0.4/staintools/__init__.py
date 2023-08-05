41804c18ca071c1e16cc464f6d8ddd5327d2fe3c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:08 +0000

    Add package staintools/0.0.4/staintools-0.0.4.tar.gz

---
+++ packages/staintools/staintools-0.0.4.tar.gz/staintools-0.0.4/staintools/__init__.py
@@ -0,0 +1,4 @@
+from .normalization.reinhard import ReinhardNormalizer
+from .normalization.macenko import MacenkoNormalizer
+from .normalization.vahadane import VahadaneNormalizer
+from .utils.misc import standardize_brightness
