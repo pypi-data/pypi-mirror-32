a4f2d8edd67ceaf858c71d819fde438714242253
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:11:52 +0000

    Add package jsondispatch/0.1.1/jsondispatch-0.1.1.tar.gz

---
+++ packages/jsondispatch/jsondispatch-0.1.1.tar.gz/jsondispatch-0.1.1/setup.py
@@ -0,0 +1,45 @@
+from codecs import open as _open
+from os import path
+
+from setuptools import setup, find_packages
+
+HERE = path.abspath(path.dirname(__file__))
+
+with _open(path.join(HERE, 'README.md'), encoding='utf-8') as f:
+    LONG_DESCRIPTION = f.read()
+
+    setup(
+        name='jsondispatch',
+        version='0.1.1',
+        description='Dispatch commands with JSON HTTP requests',
+        long_description=LONG_DESCRIPTION,
+        author='Pierre Penninckx',
+        author_email='ibizapeanut@gmail.com',
+        license='GPLv3',
+        packages=find_packages(),
+        url='https://github.com/ibizaman/jsondispatch',
+        download_url='https://github.com/ibizaman/jsondispatch/archive/0.1.1.tar.gz',
+        keywords=['json', 'aiohttp'],
+        entry_points = {
+            'console_scripts': ['jsondispatch=jsondispatch.__main__:main'],
+        },
+        install_requires=[
+            'aiohttp == 2.3.7',
+            'aiohttp_cors == 0.6.0',
+            'yarl == 0.18.0',
+            'PyYAML == 3.12',
+        ],
+        extras_require={
+            'youtube-dl': [
+                'youtube-dl',
+            ],
+            'dev': [
+                'pylint == 1.7.2',
+            ],
+            'test': [
+                'coverage == 4.4.1',
+                'pytest == 3.1.3',
+                'pytest-cov == 2.5.1',
+            ],
+        }
+    )
