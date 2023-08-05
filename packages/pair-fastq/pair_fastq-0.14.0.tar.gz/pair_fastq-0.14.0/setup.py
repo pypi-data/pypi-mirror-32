7a6132e6fd39a924f604f9061a35b60b3de20956
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:20 +0000

    Add package pair-fastq/0.14.0/pair_fastq-0.14.0.tar.gz

---
+++ packages/pair-fastq/pair_fastq-0.14.0.tar.gz/pair_fastq-0.14.0/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name='pair_fastq',
+    author='jlevy44',
+    version='0.14.0',
+    py_modules=['pair_fastq'],
+    install_requires=[
+        'pandas',
+	'numpy',
+	'click'
+    ],
+    entry_points="""
+        [console_scripts]
+        pairfastq=pair_fastq:pairfastq"""
+)
