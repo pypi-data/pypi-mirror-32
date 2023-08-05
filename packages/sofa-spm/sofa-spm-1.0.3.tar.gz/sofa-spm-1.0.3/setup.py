ccd7e9e3b204da81a6dc494909fe8349dd4dacdf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:16:29 +0000

    Add package sofa-spm/1.0.3/sofa-spm-1.0.3.tar.gz

---
+++ packages/sofa-spm/sofa-spm-1.0.3.tar.gz/sofa-spm-1.0.3/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(name='sofa-spm',
+      version='1.0.3',
+      description='A package manager for the simulation framework called Sofa (https://www.sofa-framework.org)',
+      url='https://github.com/SofaDefrost/SPM',
+      author='Damien Marchal',
+      author_email='damien.marchal@univ-lille1.fr',
+      license='GPL',
+      scripts=['sofa-spm.py'],
+      packages=["spm"],
+      package_data={'spm' : ['spm/recipes/*']},
+      install_requires=['mu-repo', 'GitPython', 'requests'],
+      include_package_data=True,
+      zip_safe=True)
