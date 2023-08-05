a118826753f692a35467043dab0d9a923b69eb93
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:30 +0000

    Add package dbgdb/0.0.2/dbgdb-0.0.2.tar.gz

---
+++ packages/dbgdb/dbgdb-0.0.2.tar.gz/dbgdb-0.0.2/dbgdb/tasks/postgres/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Created by pat on 5/21/18
+"""
+.. currentmodule:: __init__.py
+.. moduleauthor:: Pat Daburu <pat@daburu.net>
+
+Say something descriptive about the '__init__.py' module.
+"""
