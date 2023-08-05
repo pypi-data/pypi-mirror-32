0eff850a968fcb40fa6222dee380b5301c65b9e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:23:38 +0000

    Add package fmrest/1.0.5/fmrest-1.0.5.tar.gz

---
+++ packages/fmrest/fmrest-1.0.5.tar.gz/fmrest-1.0.5/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='fmrest',
+    version='1.0.5',
+    description='a Python library for interacting with the FileMaker REST APIs',
+    url='https://github.com/eluce2/fmrest',
+    author='Eric Luce',
+    author_email='eluce@huntington.edu',
+    install_requires=['requests'],
+    license='MIT',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3',
+        'Topic :: Utilities'
+    ],
+    keywords='filemaker helper rest api fmrest fmapi',
+    packages=find_packages()
+)
