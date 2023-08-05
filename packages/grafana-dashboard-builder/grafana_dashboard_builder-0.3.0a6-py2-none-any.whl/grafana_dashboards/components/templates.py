ffa9919a9c5030f28eef0f67193753797ff35fd8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:01 +0000

    Add package grafana-dashboard-builder/0.3.0a6/grafana_dashboard_builder-0.3.0a6-py2-none-any.whl

---
+++ packages/grafana-dashboard-builder/grafana_dashboard_builder-0.3.0a6-py2-none-any.whl/grafana_dashboards/components/templates.py
@@ -0,0 +1,106 @@
+# -*- coding: utf-8 -*-
+# Copyright 2015-2018 grafana-dashboard-builder contributors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+from grafana_dashboards.components.base import JsonListGenerator, JsonGenerator
+
+__author__ = 'Jakub Plichta <jakub.plichta@gmail.com>'
+
+
+class Templates(JsonListGenerator):
+    def __init__(self, data, registry):
+        super(Templates, self).__init__(data, registry, TemplatesItemBase)
+
+
+class TemplatesItemBase(JsonGenerator):
+    pass
+
+
+class Query(TemplatesItemBase):
+
+    def gen_json_from_data(self, data, context):
+        super(Query, self).gen_json_from_data(data, context)
+        processed_parts = []
+        queries = []
+        if not data.get('query'):
+            return queries
+        refresh_only_first = data.get('refresh-only-first', False)
+        for query_part in data['query'].split('.'):
+            if query_part.startswith('$'):
+                is_first = False if queries else True
+                query = query_part[1:]
+                metric = '*'
+                template_json = {
+                    'type': 'query',
+                    'refresh_on_load': not refresh_only_first or is_first,
+                    'name': query,
+                    'refresh': not refresh_only_first or is_first
+                }
+                if query in data:
+                    query_config = data[query]
+                    metric = query_config.get('metric', metric)
+                    if 'current' in query_config:
+                        current = query_config['current']
+                        template_json['current'] = {
+                            'text': current,
+                            'value': current
+                        }
+                    if 'options' in query_config:
+                        template_json['options'] = [{'text': option, 'value': option} for option in
+                                                    (query_config['options'])]
+
+                    for key in ['regex', 'multi', 'includeAll', 'hide', 'allFormat', 'allValue']:
+                        if key in query_config:
+                            template_json[key] = query_config[key]
+
+                template_json['query'] = '.'.join(processed_parts + [metric])
+                queries.append(template_json)
+            processed_parts.append(query_part)
+        return queries
+
+
+class EnumeratedTemplateBase(TemplatesItemBase):
+
+    def __init__(self, data, registry, template_type):
+        super(EnumeratedTemplateBase, self).__init__(data, registry)
+        self._template_type = template_type
+
+    def gen_json_from_data(self, data, context):
+        template_json = super(EnumeratedTemplateBase, self).gen_json_from_data(data, context)
+        template_json.update({
+            'type': self._template_type,
+            'refresh_on_load': False,
+            'datasource': None,
+            'name': data['name'],
+            'query': ','.join([str(options) for options in data['options']])
+        })
+        if 'current' in data:
+            current = data['current']
+            template_json['current'] = {
+                'text': current,
+                'value': current
+            }
+        if 'options' in data:
+            template_json['options'] = [{'text': option, 'value': option} for option in
+                                        (data['options'])]
+        return [template_json]
+
+
+class CustomTemplate(EnumeratedTemplateBase):
+    def __init__(self, data, registry):
+        super(CustomTemplate, self).__init__(data, registry, 'custom')
+
+
+class IntervalTemplate(EnumeratedTemplateBase):
+    def __init__(self, data, registry):
+        super(IntervalTemplate, self).__init__(data, registry, 'interval')
