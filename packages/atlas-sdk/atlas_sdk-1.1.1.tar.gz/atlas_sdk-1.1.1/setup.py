39cff91d6b016c081d75192aa375d6ad590bb168
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:25:08 +0000

    Add package atlas-sdk/1.1.1/atlas_sdk-1.1.1.tar.gz

---
+++ packages/atlas-sdk/atlas_sdk-1.1.1.tar.gz/atlas_sdk-1.1.1/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+import os
+
+ROOT_PATH = os.path.dirname(os.path.abspath(__file__))
+README = os.path.join(ROOT_PATH, 'README.md')
+
+with open(README, encoding='utf8') as f:
+  readme = f.read()
+
+setup(
+  name='atlas_sdk',
+  description='Python SDK to interact with atlas',
+  long_description=readme,
+  long_description_content_type="text/markdown",
+  author='Julien LEICHER',
+  license='GPL-3.0',
+  version='1.1.1',
+  packages=['atlas_sdk'],
+  install_requires=[
+    'paho-mqtt==1.3.1',
+    'python-dateutil==2.7.2',
+    'semantic-version==2.6.0',
+  ],
+)
