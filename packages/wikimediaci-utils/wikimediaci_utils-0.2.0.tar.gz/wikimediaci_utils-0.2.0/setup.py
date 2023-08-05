334a178f46b900f82e2ab4dd6b3779dbb6d2881e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:15:28 +0000

    Add package wikimediaci-utils/0.2.0/wikimediaci_utils-0.2.0.tar.gz

---
+++ packages/wikimediaci-utils/wikimediaci_utils-0.2.0.tar.gz/wikimediaci_utils-0.2.0/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+    name='wikimediaci_utils',
+    version='0.2.0',
+    packages=['wikimediaci_utils'],
+    url='https://gerrit.wikimedia.org/g/integration/utils',
+    license='GPL-3.0-or-later',
+    author='Kunal Mehta',
+    author_email='legoktm@member.fsf.org',
+    description='Common utility functions for tools related to Wikimedia CI',
+    long_description=open('README.rst').read(),
+    long_description_content_type='text/x-rst',
+    install_requires=[
+        'pyyaml',
+        'requests',
+    ],
+)
