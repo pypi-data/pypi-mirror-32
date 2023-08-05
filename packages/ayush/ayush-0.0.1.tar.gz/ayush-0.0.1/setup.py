de4cc304302cca8bde5df4b02f80b174edfe4760
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:05 +0000

    Add package ayush/0.0.1/ayush-0.0.1.tar.gz

---
+++ packages/ayush/ayush-0.0.1.tar.gz/ayush-0.0.1/setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+
+DESCRIPTION = 'Contains utils that I frequently use'
+
+
+setuptools.setup(
+    name="ayush",
+    version="0.0.1",
+    author="Ayush Shanker",
+    author_email="shankerayush@gmail.com",
+    description=DESCRIPTION,
+    long_description='',
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    classifiers=(
+        "Programming Language :: Python :: 3",
+    ),
+)
