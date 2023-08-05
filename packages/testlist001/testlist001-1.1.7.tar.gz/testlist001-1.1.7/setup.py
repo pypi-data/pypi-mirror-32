cddc40ff900d5fb4c640b7c134241b07b6da52b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:10:25 +0000

    Add package testlist001/1.1.7/testlist001-1.1.7.tar.gz

---
+++ packages/testlist001/testlist001-1.1.7.tar.gz/testlist001-1.1.7/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+	name='testlist001',
+	version='1.1.7',
+	description='pypi testing',
+	packages=['potatofield'],
+	entry_points={
+	    'console_scripts': [
+	        'potato=potatofield.potato:run',
+	        'potatofield=potatofield.potato:run'
+	    ]
+	}
+)
