50865bc7983c0e881d91c656a61b438113be9088
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:18:47 +0000

    Add package alfonsiot/0.0.1/AlfonsIoT-0.0.1.tar.gz

---
+++ packages/alfonsiot/AlfonsIoT-0.0.1.tar.gz/AlfonsIoT-0.0.1/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="AlfonsIoT",
+    version="0.0.1",
+    author="Anton Lindroth",
+    author_email="ntoonio@gmail.com",
+    description="A package for sensors to interact with Alfons",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ntoonio/AlfonsSensor.git",
+    packages=setuptools.find_packages(),
+	install_requires=[
+		"AlfonsIoT"
+	],
+	entry_points = {
+		"console_scripts": [
+			"alfsens = main:main",
+		],
+	}
+)
