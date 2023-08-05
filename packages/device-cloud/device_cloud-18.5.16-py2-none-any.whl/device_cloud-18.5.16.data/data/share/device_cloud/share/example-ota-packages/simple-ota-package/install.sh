529d4fab00196b9c8f213c6bb3a94cc5f69d98e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:09:48 +0000

    Add package device-cloud/18.5.16/device_cloud-18.5.16-py2-none-any.whl

---
+++ packages/device-cloud/device_cloud-18.5.16-py2-none-any.whl/device_cloud-18.5.16.data/data/share/device_cloud/share/example-ota-packages/simple-ota-package/install.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# ---------------------------------------------------------------------
+# This is an example install script.  Typically package install
+# actions go in here apt-get, dnf, yum, pip install
+# etc., but anything can be run here.  Return a non zero value and the
+# err_install.sh script will be invoked.
+# ---------------------------------------------------------------------
+echo "install running...."
+ping -c 1 google.ca
+exit 0
