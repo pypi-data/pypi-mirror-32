de5807d96c82d5a248157789b29ede51d1574a7a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:10:20 +0000

    Add package evernode/1.2.2/evernode-1.2.2.tar.gz

---
+++ packages/evernode/evernode-1.2.2.tar.gz/evernode-1.2.2/evernode/models/password_reset_model.py
@@ -0,0 +1,38 @@
+"""
+    User Model
+"""
+from .base_model import BaseModel
+from sqlalchemy import Column, String, Integer, ForeignKey
+
+
+class PasswordResetModel(BaseModel):
+    """ Password Reset db Model """
+
+    __tablename__ = 'user_password_resets'
+    token = Column(String(1000))
+    uuid = Column(String(70))
+    user_id = Column(Integer, ForeignKey('users.id'))
+
+    @classmethod
+    def where_token(cls, token):
+        """ get by token """
+        return cls.query.filter_by(token=token).first()
+
+    @classmethod
+    def where_uuid(cls, uuid):
+        """ get by uuid """
+        return cls.query.filter_by(uuid=uuid).first()
+
+    @classmethod
+    def where_user_id(cls, user_id):
+        """ get by user_id """
+        return cls.query.filter_by(user_id=user_id).first()
+
+    @classmethod
+    def delete_where_user_id(cls, user_id):
+        """ delete by email """
+        result = cls.where_user_id(user_id)
+        if result is None:
+            return None
+        result.delete()
+        return True
