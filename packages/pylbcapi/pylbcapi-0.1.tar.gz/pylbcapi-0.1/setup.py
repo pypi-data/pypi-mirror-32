0b5f1613f6eafbf6f560a86f7fe8278cdb529a8a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:10:37 +0000

    Add package pylbcapi/0.1/pylbcapi-0.1.tar.gz

---
+++ packages/pylbcapi/pylbcapi-0.1.tar.gz/pylbcapi-0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='pylbcapi',
+      version='0.1',
+      description='LocalBitcoin API Wrapper for Python 3.X',
+      author='J. Fernando Gonzalez',
+      author_email='jfgonzalem@gmail.com',
+      license='MIT',
+      packages=['pylbcapi'],
+      keywords=['API', 'LocalBitcoin'],
+      python_requires='>3.*',
+      )
