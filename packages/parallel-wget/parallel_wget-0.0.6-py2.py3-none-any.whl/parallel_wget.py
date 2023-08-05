f02715588129caa77ed27a033601487c02741a4f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:58 +0000

    Add package parallel-wget/0.0.6/parallel_wget-0.0.6-py2.py3-none-any.whl

---
+++ packages/parallel-wget/parallel_wget-0.0.6-py2.py3-none-any.whl/parallel_wget.py
@@ -0,0 +1,32 @@
+import sys, os
+from os import path as op
+import os, sys
+op.dirname(sys.executable)
+import wget
+import tarfile
+from multiprocessing import Pool
+
+def wget_file(file_url):
+    try:
+      wget.download(file_url)
+      print("File {} downloaded!".format(file_url))
+    except Exception as e:
+      print('Got exception: {0}'.format(e))
+      raise e
+
+def parallel_wget(host, path, files):
+    file_urls = ['{0}{1}{2}'.format(host, path, f) for f in files]
+    print('\n'.join(file_urls))
+    print("Downloading files~")
+    p = Pool(len(file_urls))
+    p.map(wget_file, file_urls)
+    downloaded_files = [op.join(os.getcwd(), f) for f in os.listdir(".") if op.isfile(f)]
+    for file in downloaded_files:
+        if file.endswith(".tgz"):
+            print("unziping {}".format(file))
+            tar = tarfile.open(file, "r:gz")
+            tar.extractall()
+            tar.close()
+            os.remove(file)
+            print("{} has remove".format(file))
+    print('Completed download of {0} files'.format(len(files)))
