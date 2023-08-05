2a602d717d9cddfe5881e3483cb59083ffeaaa4f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:10:29 +0000

    Add package spamme/0.0.1/spamme-0.0.1.tar.gz

---
+++ packages/spamme/spamme-0.0.1.tar.gz/spamme-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+setup(name = 'spamme',
+    version = '0.0.1',
+    description = 'The trash clean utilities',
+    url = 'http://fake.usr.com',
+    author = 'Z.G.',
+    author_email = 'zg@fake.usr.cim',
+    license ='MIT',
+    packages = ['spamme'],
+    zip_safe = False)
+
---
+++ packages/spamme/spamme-0.0.1.tar.gz/spamme-0.0.1/spamme/trash.py
@@ -0,0 +1,2 @@
+def clean_trash():
+    print "haha, I am joking"
