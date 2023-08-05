3087ab1c37202f5f3df4f9897927f6cb47affcac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:11:35 +0000

    Add package single-factor-model/0.0.4/single_factor_model-0.0.4.tar.gz

---
+++ packages/single-factor-model/single_factor_model-0.0.4.tar.gz/single_factor_model-0.0.4/setup.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May  8 13:21:09 2018
+
+@author: yili.peng
+"""
+
+from setuptools import setup
+
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
+setup(name='single_factor_model'
+      ,version='0.0.4'
+      ,description='factor model'
+      ,long_description=readme()
+      ,keywords='factor model quant'
+      ,lisence='MIT'
+      ,author='Yili Peng'
+      ,author_email='yili.peng@outlook.com'
+      ,packages=['single_factor_model']
+      ,zip_safe=False)
