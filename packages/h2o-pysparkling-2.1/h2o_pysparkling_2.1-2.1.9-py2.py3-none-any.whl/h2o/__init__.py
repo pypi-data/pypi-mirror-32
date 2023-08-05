4156c6c11039d10cb59811fd6ff735dab29d5cd7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:29:41 +0000

    Add package h2o-pysparkling-2.1/2.1.9/h2o_pysparkling_2.1-2.1.9-py2.py3-none-any.whl

---
+++ packages/h2o-pysparkling-2.1/h2o_pysparkling_2.1-2.1.9-py2.py3-none-any.whl/h2o/__init__.py
@@ -0,0 +1,37 @@
+# -*- encoding: utf-8 -*-
+# Copyright: (c) 2016 H2O.ai
+# License:   Apache License Version 2.0 (see LICENSE for details)
+"""
+:mod:`h2o` -- module for using H2O services.
+
+(please add description).
+"""
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+from h2o.h2o import (connect, init, api, connection,
+                     lazy_import, upload_file, import_file, import_sql_table, import_sql_select,
+                     parse_setup, parse_raw, assign, deep_copy, get_model, get_grid, get_frame,
+                     show_progress, no_progress, log_and_echo, remove, remove_all, rapids,
+                     ls, frame, frames, create_frame,
+                     download_pojo, download_csv, download_all_logs, save_model, load_model, export_file,
+                     cluster_status, cluster_info, shutdown, network_test, cluster,
+                     interaction, as_list,
+                     get_timezone, set_timezone, list_timezones,
+                     load_dataset, demo, make_metrics,flow)
+# We have substantial amount of code relying on h2o.H2OFrame to exist. Thus, we make this class available from
+# root h2o module, without exporting it explicitly. In the future this import may be removed entirely, so that
+# one would have to import it from h2o.frames.
+from h2o.frame import H2OFrame  # NOQA
+
+__version__ = "3.10.5.1"
+__buildinfo__ = "versionFromGradle='3.10.5',projectVersion='3.10.5.1',branch='rel-vajda',lastCommitHash='e322f0d8ae1bbc8072ec026021aef9d3c86e4add',gitDescribe='jenkins-michalk-bueno-1',compiledOn='2017-06-09 21:48:52',compiledBy='jenkins'"
+
+if (__version__.endswith("99999")):
+    print(__buildinfo__)
+
+__all__ = ("connect", "init", "api", "connection", "upload_file", "lazy_import", "import_file", "import_sql_table",
+           "import_sql_select", "parse_setup", "parse_raw", "assign", "deep_copy", "get_model", "get_grid", "get_frame",
+           "show_progress", "no_progress", "log_and_echo", "remove", "remove_all", "rapids", "ls", "frame",
+           "frames", "download_pojo", "download_csv", "download_all_logs", "save_model", "load_model", "export_file",
+           "cluster_status", "cluster_info", "shutdown", "create_frame", "interaction", "as_list", "network_test",
+           "set_timezone", "get_timezone", "list_timezones", "demo", "make_metrics", "cluster", "load_dataset","flow")
