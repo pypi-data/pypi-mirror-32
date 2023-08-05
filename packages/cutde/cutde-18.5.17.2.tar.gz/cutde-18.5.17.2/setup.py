8e8dbc1ddb91a2245a9179230b00d38b435e8893
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:28 +0000

    Add package cutde/18.5.17.2/cutde-18.5.17.2.tar.gz

---
+++ packages/cutde/cutde-18.5.17.2.tar.gz/cutde-18.5.17.2/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+
+version = open('VERSION').read()
+
+try:
+    import pypandoc
+    description = pypandoc.convert('README.md', 'rst')
+except (IOError, ImportError):
+    description = open('README.md').read()
+
+setup(
+    packages = ['cutde'],
+
+    install_requires = ['cluda'],
+    zip_safe = False,
+    include_package_data = True,
+
+    name = 'cutde',
+    version = version,
+    description = '',
+    long_description = description,
+
+    url = 'https://github.com/tbenthompson/cutde',
+    author = 'T. Ben Thompson',
+    author_email = 't.ben.thompson@gmail.com',
+    license = 'MIT',
+    platforms = ['any']
+)
