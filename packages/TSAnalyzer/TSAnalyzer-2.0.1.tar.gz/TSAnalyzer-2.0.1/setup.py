9bdf504498a9866cd61f19c9d1ae29ca6039a4f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:23:40 +0000

    Add package TSAnalyzer/2.0.1/TSAnalyzer-2.0.1.tar.gz

---
+++ packages/TSAnalyzer/TSAnalyzer-2.0.1.tar.gz/TSAnalyzer-2.0.1/setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# author: WU Dingcheng
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+setup(
+    name='TSAnalyzer',
+    version='2.0.1',
+    description='Python GNSS Time Series Analysis and Visualization',
+    license='GPL v3',
+    author='WU Dingcheng',
+    author_email='wudingcheng14@mails.ucas.ac.cn',
+    url='https://github.com/wudingcheng/tsanalyzer',
+    packages=find_packages(),
+    package_data={'TSAnalyzer': ['resources/style.qss',
+                                 'resources/images/*',
+                                 'resources/ui/*.ui']},
+    install_requires=[
+        'matplotlib',
+        'pandas',
+        'numpy',
+        'qtpy',
+        'cvxpy',
+        'cvxopt'],
+    entry_points={'gui_scripts': ['TSAnalyzer = TSAnalyzer:main']}
+)
