1e7b5d0807840a505ebbd12152d8da4c2a3547b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:11:57 +0000

    Add package chuda/0.0.15/chuda-0.0.15.tar.gz

---
+++ packages/chuda/chuda-0.0.15.tar.gz/chuda-0.0.15/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup
+
+try:
+    with open('README.MD') as readme_file:
+        README = readme_file.read()
+except FileNotFoundError:
+    README = ""
+
+VERSION = '0.0.15'
+
+setup(
+    name='chuda',
+    packages=['chuda'],
+    version=VERSION,
+    description='A simple framework to create CLI tools',
+    long_description=README,
+    license='MIT',
+    install_requires=[
+        "delegator.py>=0.1.0",
+        "argcomplete>=1.9.4",
+        "crayons>=0.1.2"
+    ],
+    author='Romain Moreau',
+    author_email='moreau.romain83@gmail.com',
+    url='https://github.com/Varkal/chuda',
+    download_url='https://github.com/Varkal/chuda/archive/{}.tar.gz'.format(VERSION),
+    keywords=['cli', 'chuda'],
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Environment :: Console",
+        "Programming Language :: Python :: 3"
+    ],
+)
