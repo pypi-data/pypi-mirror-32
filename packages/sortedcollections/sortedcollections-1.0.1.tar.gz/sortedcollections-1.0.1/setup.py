37334fa72ab18165d6978c26274d8f6639f0863e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:15 +0000

    Add package sortedcollections/1.0.1/sortedcollections-1.0.1.tar.gz

---
+++ packages/sortedcollections/sortedcollections-1.0.1.tar.gz/sortedcollections-1.0.1/setup.py
@@ -0,0 +1,51 @@
+from setuptools import setup, find_packages
+from setuptools.command.test import test as TestCommand
+import sys
+
+
+class Tox(TestCommand):
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+    def run_tests(self):
+        import tox
+        errno = tox.cmdline(self.test_args)
+        sys.exit(errno)
+
+
+with open('README.rst') as fptr:
+    readme = fptr.read()
+
+setup(
+    name='sortedcollections',
+    version='1.0.1',
+    description='Python Sorted Collections',
+    long_description=readme,
+    author='Grant Jenks',
+    author_email='contact@grantjenks.com',
+    url='http://www.grantjenks.com/docs/sortedcollections/',
+    license='Apache 2.0',
+    packages=find_packages(exclude=('tests', 'docs')),
+    install_requires=['sortedcontainers'],
+    tests_require=['tox'],
+    cmdclass={'test': Tox},
+    classifiers=(
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Natural Language :: English',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+    ),
+)
