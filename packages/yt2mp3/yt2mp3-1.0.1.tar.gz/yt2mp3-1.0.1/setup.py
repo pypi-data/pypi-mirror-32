0d8665fb0265262f88e267bff7b11fd20a20a843
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:18:48 +0000

    Add package yt2mp3/1.0.1/yt2mp3-1.0.1.tar.gz

---
+++ packages/yt2mp3/yt2mp3-1.0.1.tar.gz/yt2mp3-1.0.1/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+  name = 'yt2mp3',
+  version = '1.0.1',
+  description = 'Simplifies the process of downloading and converting Youtube videos to MP3 files',
+  long_description=long_description,
+  long_description_content_type="text/markdown",
+  author = 'Brett Stevenson',
+  author_email = 'bstevensondev@gmail.com',
+  url = 'https://github.com/tterb/yt2mp3',
+  download_url = 'https://github.com/tterb/yt2mp3/archive/1.0.1.tar.gz',
+  keywords = ['youtube', 'mp3', 'download', 'itunes', 'music'],
+  packages = setuptools.find_packages(),
+  classifiers = [
+    "Programming Language :: Python :: 3",
+    "License :: OSI Approved :: MIT License",
+    "Operating System :: OS Independent",
+  ],
+)
