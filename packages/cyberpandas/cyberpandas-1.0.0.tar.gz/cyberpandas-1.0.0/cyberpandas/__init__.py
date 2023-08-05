d1380d6d03ceff4aa528a5ffeb019cb5b109d53b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:09:18 +0000

    Add package cyberpandas/1.0.0/cyberpandas-1.0.0.tar.gz

---
+++ packages/cyberpandas/cyberpandas-1.0.0.tar.gz/cyberpandas-1.0.0/cyberpandas/__init__.py
@@ -0,0 +1,30 @@
+"""Custom IP address dtype / block for pandas"""
+
+from .ip_array import (
+    IPType,
+    IPArray,
+    IPAccessor,
+)
+from .parser import to_ipaddress
+from .mac_array import MACType, MACArray
+
+from pkg_resources import get_distribution, DistributionNotFound
+try:
+    __version__ = get_distribution(__name__).version
+except DistributionNotFound:
+    # package is not installed
+    pass
+
+del get_distribution
+del DistributionNotFound
+
+
+__all__ = [
+    '__version__',
+    'IPAccessor',
+    'IPArray',
+    'IPType',
+    'MACArray',
+    'MACType',
+    'to_ipaddress',
+]
