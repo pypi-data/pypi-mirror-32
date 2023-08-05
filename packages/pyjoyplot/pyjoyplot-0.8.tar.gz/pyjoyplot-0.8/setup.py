a6888d73e80285104ec3c5a61e428674e9b14773
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:34 +0000

    Add package pyjoyplot/0.8/pyjoyplot-0.8.tar.gz

---
+++ packages/pyjoyplot/pyjoyplot-0.8.tar.gz/pyjoyplot-0.8/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'pyjoyplot',
+  packages = ['pyjoyplot'], # this must be the same as the name above
+  version = '0.8',
+  description = 'Joy Plots in Python',
+  author = 'Neal Ó Riain',
+  author_email = 'neal@n-o-r.xyz',
+  url = 'https://github.com/neal-o-r/pyjoyplot', # use the URL to the github repo
+  download_url = 'https://github.com/neal-o-r/pyjoyplot/archive/0.8.tar.gz',
+  keywords = ['plotting'], # arbitrary keywords
+  classifiers = [],
+)
