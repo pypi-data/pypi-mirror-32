1e681fa45d92c932f5e5a4f6244c06dde9e7cb5f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:03 +0000

    Add package statusurl/1.0.3/statusurl-1.0.3.tar.gz

---
+++ packages/statusurl/statusurl-1.0.3.tar.gz/statusurl-1.0.3/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+setup(
+  name = 'statusurl',
+  py_modules = ['statusurl'],	
+  install_requires = ['requests'],
+  version = '1.0.3',
+  description = 'A test with lists of urls',
+  author = 'Alisson Castro',
+  author_email = 'alissoncastroskt@gmail.com',
+  url = 'https://github.com/alicastrogit/', # use the URL to the github repo
+  #download_url = 'https://github.com/0.1.tar.gz', # I'll explain this in a second
+  keywords = ['testing', 'list', 'example'], # arbitrary keywords
+  classifiers = [],
+)
