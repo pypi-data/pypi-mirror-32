fa6f44b1fe6bca63c7de9a7355d7033562303e3a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:08 +0000

    Add package ontospy/1.9.1/ontospy-1.9.1-py2.py3-none-any.whl

---
+++ packages/ontospy/ontospy-1.9.1-py2.py3-none-any.whl/ontospy/VERSION.py
@@ -0,0 +1,24 @@
+# !/usr/bin/env python
+#  -*- coding: UTF-8 -*-
+
+
+##################
+#
+#  metadata
+#
+#  note: 1.8 major update with ontodocs split
+##################
+
+
+
+__version__ = "1.9.1"  # Pypi latest: 1.9 - 2018-05-08
+
+__copyright__ = "CopyRight (C) 2015-2018 by Michele Pasin"
+__license__ = "GNU"
+__author__ = "Michele Pasin"
+__author_email__ = "michele dot pasin at gmail dot com"
+
+USAGE = "ontospy [options] [uri]"
+VERSION = "v" + __version__
+
+AGENT = "%s/%s" % (__name__, __version__)
