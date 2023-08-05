1d7d505c52d385a8a6ff081e91ad0911c161ec29
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:02 +0000

    Add package bee-django-user/0.0.3/bee-django-user-0.0.3.tar.gz

---
+++ packages/bee-django-user/bee-django-user-0.0.3.tar.gz/bee-django-user-0.0.3/bee_django_user/urls.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+__author__ = 'bee'
+from django.conf.urls import include, url
+from django.contrib.auth import views as auth_views
+
+from . import views
+
+app_name = "bee_django_user"
+
+urlpatterns = [
+    # url(r'^', views.home_page, name='homepage'),
+    url(r'^test/$', views.test, name='test'),
+    url(r'^$', views.UserList.as_view(), name='index'),
+
+    url(r'^login/$', auth_views.LoginView.as_view(template_name='bee_django_user/user/login.html'), name='user_login'),
+    url(r'^logout/$', auth_views.logout, {'next_page': '/user/login'}, name='user_logout'),
+
+    # 学生
+    url(r'^list/$', views.UserList.as_view(), name='user_list'),
+    url(r'^detail/(?P<pk>[0-9]+)/$', views.UserDetail.as_view(), name='user_detail'),
+    url(r'^create/$', views.UserCreate.as_view(), name='user_create'),
+    url(r'^update/(?P<pk>[0-9]+)/$', views.UserUpdate.as_view(), name='user_update'),
+    url(r'^group/update/(?P<pk>[0-9]+)/$', views.UserGroupUpdate.as_view(), name='user_group_update'),
+    # 组及权限
+    url(r'^group/list/$', views.GroupList.as_view(), name='group_list'),
+
+    # 班级
+    url(r'^class/list/$', views.ClassList.as_view(), name='class_list'),
+    url(r'^class/create/$', views.ClassCreate.as_view(), name='class_create'),
+    url(r'^class/detail/(?P<pk>[0-9]+)/$', views.ClassDetail.as_view(), name='class_detail'),
+    url(r'^class/update/(?P<pk>[0-9]+)/$', views.ClassUpdate.as_view(), name='class_update'),
+
+]
