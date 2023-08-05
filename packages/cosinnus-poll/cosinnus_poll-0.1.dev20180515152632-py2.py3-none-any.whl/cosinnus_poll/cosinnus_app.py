255f3bf4ef4c334ae8d10ee7f365d304073c842d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:01 +0000

    Add package cosinnus-poll/0.1.dev20180515152632/cosinnus_poll-0.1.dev20180515152632-py2.py3-none-any.whl

---
+++ packages/cosinnus-poll/cosinnus_poll-0.1.dev20180515152632-py2.py3-none-any.whl/cosinnus_poll/cosinnus_app.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from cosinnus.conf import settings
+
+def register():
+    if 'cosinnus_poll' in getattr(settings, 'COSINNUS_DISABLED_COSINNUS_APPS', []):
+        return
+    
+    # Import here to prpoll import side effects
+    from django.utils.translation import ugettext_lazy as _
+    from django.utils.translation import pgettext_lazy
+
+    from cosinnus.core.registries import (app_registry,
+        attached_object_registry, url_registry, widget_registry)
+
+    app_registry.register('cosinnus_poll', 'poll', _('Polls'), deactivatable=True)
+    attached_object_registry.register('cosinnus_poll.Poll',
+                             'cosinnus_poll.utils.renderer.PollRenderer')
+    url_registry.register_urlconf('cosinnus_poll', 'cosinnus_poll.urls')
+    widget_registry.register('poll', 'cosinnus_poll.dashboard.CurrentPolls')
+    
+    # makemessages replacement protection
+    name = pgettext_lazy("the_app", "poll")
