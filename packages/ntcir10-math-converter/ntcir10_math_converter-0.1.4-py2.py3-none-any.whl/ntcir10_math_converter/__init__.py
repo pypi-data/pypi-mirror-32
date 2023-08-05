1aa08c24eb9135e8a3d786619459f45a6f132297
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:12 +0000

    Add package ntcir10-math-converter/0.1.4/ntcir10_math_converter-0.1.4-py2.py3-none-any.whl

---
+++ packages/ntcir10-math-converter/ntcir10_math_converter-0.1.4-py2.py3-none-any.whl/ntcir10_math_converter/__init__.py
@@ -0,0 +1,12 @@
+"""
+The NTCIR-10 Math Converter package converts NTCIR-10 Math dataset and
+relevance judgements to the NTCIR-11 Math-2, and NTCIR-12 MathIR format.
+"""
+
+from .converter import convert_judgements, get_judged_identifiers, process_dataset
+from .__main__ import main
+
+
+__author__ = "Vit Novotny"
+__version__ = "0.1.4"
+__license__ = "MIT"
