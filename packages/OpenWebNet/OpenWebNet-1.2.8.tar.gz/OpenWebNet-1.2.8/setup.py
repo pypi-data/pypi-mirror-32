ae42ce914251b562ffa9a1351125698b4488575f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:31 +0000

    Add package OpenWebNet/1.2.8/OpenWebNet-1.2.8.tar.gz

---
+++ packages/OpenWebNet/OpenWebNet-1.2.8.tar.gz/OpenWebNet-1.2.8/setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+setup(
+    name = 'OpenWebNet',
+    version = '1.2.8',
+    py_modules=['OpenWebNet'],
+    author='clau67',
+    author_email='claudio.carletti.67@gmail.com',
+    description = 'OpenWebNet reading/writing class',
+    url='https://github.com/pippocla/OpenWebNet',
+    classifiers = [
+        "Development Status :: 4 - Beta",
+        "Programming Language :: Python :: 3",
+        "Topic :: Software Development",
+    ]
+)
