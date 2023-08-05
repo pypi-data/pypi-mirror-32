8cd9b7d9cd969212bb9aa446218677d063af894a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:49 +0000

    Add package PySychonaut/0.5/PySychonaut-0.5.tar.gz

---
+++ packages/PySychonaut/PySychonaut-0.5.tar.gz/PySychonaut-0.5/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='PySychonaut',
+    version='0.5',
+    packages=['pysychonaut'],
+    url='https://github.com/JarbasAl/PySychonaut',
+    license='MIT',
+    author='jarbasAI',
+    author_email='jarbasai@mailfence.com',
+    description='unnoficial erowid, psychonaut wiki and ask_the_caterpillar apis',
+    install_requires=["lxml", "bs4"]
+)
