77fec27672201bd1c41df48282531cffd073847c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:46 +0000

    Add package cwltool/1.0.20180521150620/cwltool-1.0.20180521150620-py2.py3-none-any.whl

---
+++ packages/cwltool/cwltool-1.0.20180521150620-py2.py3-none-any.whl/cwltool/expression.py
@@ -0,0 +1,267 @@
+from __future__ import absolute_import
+import copy
+import json
+import logging
+import re
+from typing import Any, AnyStr, Dict, List, Text, Union
+from .utils import docker_windows_path_adjust
+import six
+from six import u
+
+from . import sandboxjs
+from .errors import WorkflowException
+from .utils import bytes2str_in_dicts
+
+_logger = logging.getLogger("cwltool")
+
+
+def jshead(engineConfig, rootvars):
+    # type: (List[Text], Dict[Text, Any]) -> Text
+
+    # make sure all the byte strings are converted
+    # to str in `rootvars` dict.
+    # TODO: need to make sure the `rootvars dict`
+    # contains no bytes type in the first place.
+    if six.PY3:
+        rootvars = bytes2str_in_dicts(rootvars)  # type: ignore
+
+    return u"\n".join(engineConfig + [u"var %s = %s;" % (k, json.dumps(v, indent=4)) for k, v in rootvars.items()])
+
+
+# decode all raw strings to unicode
+seg_symbol = r"""\w+"""
+seg_single = r"""\['([^']|\\')+'\]"""
+seg_double = r"""\["([^"]|\\")+"\]"""
+seg_index = r"""\[[0-9]+\]"""
+segments = r"(\.%s|%s|%s|%s)" % (seg_symbol, seg_single, seg_double, seg_index)
+segment_re = re.compile(u(segments), flags=re.UNICODE)
+param_str = r"\((%s)%s*\)$" % (seg_symbol, segments)
+param_re = re.compile(u(param_str), flags=re.UNICODE)
+
+JSON = Union[Dict[Any, Any], List[Any], Text, int, float, bool, None]
+
+
+class SubstitutionError(Exception):
+    pass
+
+
+def scanner(scan):  # type: (Text) -> List[int]
+    DEFAULT = 0
+    DOLLAR = 1
+    PAREN = 2
+    BRACE = 3
+    SINGLE_QUOTE = 4
+    DOUBLE_QUOTE = 5
+    BACKSLASH = 6
+
+    i = 0
+    stack = [DEFAULT]
+    start = 0
+    while i < len(scan):
+        state = stack[-1]
+        c = scan[i]
+
+        if state == DEFAULT:
+            if c == '$':
+                stack.append(DOLLAR)
+            elif c == '\\':
+                stack.append(BACKSLASH)
+        elif state == BACKSLASH:
+            stack.pop()
+            if stack[-1] == DEFAULT:
+                return [i - 1, i + 1]
+        elif state == DOLLAR:
+            if c == '(':
+                start = i - 1
+                stack.append(PAREN)
+            elif c == '{':
+                start = i - 1
+                stack.append(BRACE)
+            else:
+                stack.pop()
+        elif state == PAREN:
+            if c == '(':
+                stack.append(PAREN)
+            elif c == ')':
+                stack.pop()
+                if stack[-1] == DOLLAR:
+                    return [start, i + 1]
+            elif c == "'":
+                stack.append(SINGLE_QUOTE)
+            elif c == '"':
+                stack.append(DOUBLE_QUOTE)
+        elif state == BRACE:
+            if c == '{':
+                stack.append(BRACE)
+            elif c == '}':
+                stack.pop()
+                if stack[-1] == DOLLAR:
+                    return [start, i + 1]
+            elif c == "'":
+                stack.append(SINGLE_QUOTE)
+            elif c == '"':
+                stack.append(DOUBLE_QUOTE)
+        elif state == SINGLE_QUOTE:
+            if c == "'":
+                stack.pop()
+            elif c == '\\':
+                stack.append(BACKSLASH)
+        elif state == DOUBLE_QUOTE:
+            if c == '"':
+                stack.pop()
+            elif c == '\\':
+                stack.append(BACKSLASH)
+        i += 1
+
+    if len(stack) > 1:
+        raise SubstitutionError(
+            "Substitution error, unfinished block starting at position {}: {}".format(start, scan[start:]))
+    else:
+        return None
+
+
+def next_seg(parsed_string, remaining_string, current_value):  # type: (Text, Text, Any) -> Any
+    if remaining_string:
+        m = segment_re.match(remaining_string)
+        next_segment_str = m.group(0)
+
+        key = None  # type: Union[Text, int]
+        if next_segment_str[0] == '.':
+            key = next_segment_str[1:]
+        elif next_segment_str[1] in ("'", '"'):
+            key = next_segment_str[2:-2].replace("\\'", "'").replace('\\"', '"')
+
+        if key:
+            if isinstance(current_value, list) and key == "length" and not remaining_string[m.end(0):]:
+                return len(current_value)
+            if not isinstance(current_value, dict):
+                raise WorkflowException("%s is a %s, cannot index on string '%s'" % (parsed_string, type(current_value).__name__, key))
+            if key not in current_value:
+                raise WorkflowException("%s does not contain key '%s'" % (parsed_string, key))
+        else:
+            try:
+                key = int(next_segment_str[1:-1])
+            except ValueError as v:
+                raise WorkflowException(u(str(v)))
+            if not isinstance(current_value, list):
+                raise WorkflowException("%s is a %s, cannot index on int '%s'" % (parsed_string, type(current_value).__name__, key))
+            if key >= len(current_value):
+                raise WorkflowException("%s list index %i out of range" % (parsed_string, key))
+
+        try:
+            return next_seg(parsed_string + remaining_string, remaining_string[m.end(0):], current_value[key])
+        except KeyError:
+            raise WorkflowException("%s doesn't have property %s" % (parsed_string, key))
+    else:
+        return current_value
+
+
+def evaluator(ex, jslib, obj, fullJS=False, timeout=None, force_docker_pull=False, debug=False, js_console=False):
+    # type: (Text, Text, Dict[Text, Any], bool, int, bool, bool, bool) -> JSON
+    m = param_re.match(ex)
+
+    expression_parse_exception = None
+    expression_parse_succeeded = False
+
+    if m:
+        first_symbol = m.group(1)
+        first_symbol_end = m.end(1)
+
+        if first_symbol_end + 1 == len(ex) and first_symbol == "null":
+            return None
+        try:
+            if obj.get(first_symbol) is None:
+                raise WorkflowException("%s is not defined" % first_symbol)
+
+            return next_seg(first_symbol, ex[first_symbol_end:-1], obj[first_symbol])
+        except WorkflowException as w:
+            expression_parse_exception = w
+        else:
+            expression_parse_succeeded = True
+
+    if fullJS and not expression_parse_succeeded:
+        return sandboxjs.execjs(ex, jslib, timeout=timeout, force_docker_pull=force_docker_pull, debug=debug, js_console=js_console)
+    else:
+        if expression_parse_exception is not None:
+            raise sandboxjs.JavascriptException(
+                "Syntax error in parameter reference '%s': %s. This could be due to using Javascript code without specifying InlineJavascriptRequirement." % \
+                    (ex[1:-1], expression_parse_exception))
+        else:
+            raise sandboxjs.JavascriptException(
+                "Syntax error in parameter reference '%s'. This could be due to using Javascript code without specifying InlineJavascriptRequirement." % \
+                    ex)
+
+
+def interpolate(scan, rootvars,
+                timeout=None, fullJS=None, jslib="", force_docker_pull=False,
+                debug=False, js_console=False, strip_whitespace=True):
+    # type: (Text, Dict[Text, Any], int, bool, Union[str, Text], bool, bool, bool, bool) -> JSON
+    if strip_whitespace:
+        scan = scan.strip()
+    parts = []
+    w = scanner(scan)
+    while w:
+        parts.append(scan[0:w[0]])
+
+        if scan[w[0]] == '$':
+            e = evaluator(scan[w[0] + 1:w[1]], jslib, rootvars, fullJS=fullJS,
+                          timeout=timeout, force_docker_pull=force_docker_pull,
+                          debug=debug, js_console=js_console)
+            if w[0] == 0 and w[1] == len(scan) and len(parts) <= 1:
+                return e
+            leaf = json.dumps(e, sort_keys=True)
+            if leaf[0] == '"':
+                leaf = leaf[1:-1]
+            parts.append(leaf)
+        elif scan[w[0]] == '\\':
+            e = scan[w[1] - 1]
+            parts.append(e)
+
+        scan = scan[w[1]:]
+        w = scanner(scan)
+    parts.append(scan)
+    return ''.join(parts)
+
+def needs_parsing(snippet):  # type: (Any) -> bool
+    return isinstance(snippet, (str, Text)) \
+        and ("$(" in snippet or "${" in snippet)
+
+def do_eval(ex, jobinput, requirements, outdir, tmpdir, resources,
+            context=None, pull_image=True, timeout=None, force_docker_pull=False,
+            debug=False, js_console=False, strip_whitespace=True):
+    # type: (Union[dict, AnyStr], Dict[Text, Union[Dict, List, Text]], List[Dict[Text, Any]], Text, Text, Dict[Text, Union[int, Text]], Any, bool, int, bool, bool, bool, bool) -> Any
+
+    runtime = copy.copy(resources)
+    runtime["tmpdir"] = docker_windows_path_adjust(tmpdir)
+    runtime["outdir"] = docker_windows_path_adjust(outdir)
+
+    rootvars = {
+        u"inputs": jobinput,
+        u"self": context,
+        u"runtime": runtime}
+
+    if needs_parsing(ex):
+        assert isinstance(ex, (str, Text))
+        fullJS = False
+        jslib = u""
+        for r in reversed(requirements):
+            if r["class"] == "InlineJavascriptRequirement":
+                fullJS = True
+                jslib = jshead(r.get("expressionLib", []), rootvars)
+                break
+
+        try:
+            return interpolate(ex,
+                               rootvars,
+                               timeout=timeout,
+                               fullJS=fullJS,
+                               jslib=jslib,
+                               force_docker_pull=force_docker_pull,
+                               debug=debug,
+                               js_console=js_console,
+                               strip_whitespace=strip_whitespace)
+
+        except Exception as e:
+            raise WorkflowException("Expression evaluation error:\n%s" % e)
+    else:
+        return ex
---
+++ packages/cwltool/cwltool-1.0.20180521150620-py2.py3-none-any.whl/cwltool/pack.py
@@ -0,0 +1,200 @@
+from __future__ import absolute_import
+import copy
+import re
+from typing import Any, Callable, Dict, List, Set, Text, Union, cast
+
+from schema_salad.ref_resolver import Loader, SubLoader
+from six.moves import urllib
+from ruamel.yaml.comments import CommentedSeq, CommentedMap
+
+from .process import shortname, uniquename
+import six
+
+
+def flatten_deps(d, files):  # type: (Any, Set[Text]) -> None
+    if isinstance(d, list):
+        for s in d:
+            flatten_deps(s, files)
+    elif isinstance(d, dict):
+        if d["class"] == "File":
+            files.add(d["location"])
+        if "secondaryFiles" in d:
+            flatten_deps(d["secondaryFiles"], files)
+        if "listing" in d:
+            flatten_deps(d["listing"], files)
+
+
+def find_run(d, loadref, runs):  # type: (Any, Callable[[Text, Text], Union[Dict, List, Text]], Set[Text]) -> None
+    if isinstance(d, list):
+        for s in d:
+            find_run(s, loadref, runs)
+    elif isinstance(d, dict):
+        if "run" in d and isinstance(d["run"], six.string_types):
+            if d["run"] not in runs:
+                runs.add(d["run"])
+                find_run(loadref(None, d["run"]), loadref, runs)
+        for s in d.values():
+            find_run(s, loadref, runs)
+
+
+def find_ids(d, ids):  # type: (Any, Set[Text]) -> None
+    if isinstance(d, list):
+        for s in d:
+            find_ids(s, ids)
+    elif isinstance(d, dict):
+        for i in ("id", "name"):
+            if i in d and isinstance(d[i], six.string_types):
+                ids.add(d[i])
+        for s in d.values():
+            find_ids(s, ids)
+
+
+def replace_refs(d, rewrite, stem, newstem):
+    # type: (Any, Dict[Text, Text], Text, Text) -> None
+    if isinstance(d, list):
+        for s, v in enumerate(d):
+            if isinstance(v, six.string_types):
+                if v in rewrite:
+                    d[s] = rewrite[v]
+                elif v.startswith(stem):
+                    d[s] = newstem + v[len(stem):]
+                    rewrite[v] = d[s]
+            else:
+                replace_refs(v, rewrite, stem, newstem)
+    elif isinstance(d, dict):
+        for s, v in d.items():
+            if isinstance(v, six.string_types):
+                if v in rewrite:
+                    d[s] = rewrite[v]
+                elif v.startswith(stem):
+                    id_ = v[len(stem):]
+                    # prevent appending newstems if tool is already packed
+                    if id_.startswith(newstem.strip("#")):
+                        d[s] = "#" + id_
+                    else:
+                        d[s] = newstem + id_
+                    rewrite[v] = d[s]
+            replace_refs(v, rewrite, stem, newstem)
+
+def import_embed(d, seen):
+    # type: (Any, Set[Text]) -> None
+    if isinstance(d, list):
+        for v in d:
+            import_embed(v, seen)
+    elif isinstance(d, dict):
+        for n in ("id", "name"):
+            if n in d:
+                if d[n] in seen:
+                    this = d[n]
+                    d.clear()
+                    d["$import"] = this
+                else:
+                    this = d[n]
+                    seen.add(this)
+                    break
+
+        for k in sorted(d.keys()):
+            import_embed(d[k], seen)
+
+
+def pack(document_loader, processobj, uri, metadata, rewrite_out=None):
+    # type: (Loader, Union[Dict[Text, Any], List[Dict[Text, Any]]], Text, Dict[Text, Text], Dict[Text, Text]) -> Dict[Text, Any]
+
+    document_loader = SubLoader(document_loader)
+    document_loader.idx = {}
+    if isinstance(processobj, dict):
+        document_loader.idx[processobj["id"]] = CommentedMap(six.iteritems(processobj))
+    elif isinstance(processobj, list):
+        path, frag = urllib.parse.urldefrag(uri)
+        for po in processobj:
+            if not frag:
+                if po["id"].endswith("#main"):
+                    uri = po["id"]
+            document_loader.idx[po["id"]] = CommentedMap(six.iteritems(po))
+
+    def loadref(b, u):
+        # type: (Text, Text) -> Union[Dict, List, Text]
+        return document_loader.resolve_ref(u, base_url=b)[0]
+
+    ids = set()  # type: Set[Text]
+    find_ids(processobj, ids)
+
+    runs = {uri}
+    find_run(processobj, loadref, runs)
+
+    for f in runs:
+        find_ids(document_loader.resolve_ref(f)[0], ids)
+
+    names = set()  # type: Set[Text]
+    if rewrite_out is None:
+        rewrite = {}  # type: Dict[Text, Text]
+    else:
+        rewrite = rewrite_out
+
+    mainpath, _ = urllib.parse.urldefrag(uri)
+
+    def rewrite_id(r, mainuri):
+        # type: (Text, Text) -> None
+        if r == mainuri:
+            rewrite[r] = "#main"
+        elif r.startswith(mainuri) and r[len(mainuri)] in ("#", "/"):
+            if r[len(mainuri):].startswith("#main/"):
+                rewrite[r] = "#" + uniquename(r[len(mainuri)+1:], names)
+            else:
+                rewrite[r] = "#" + uniquename("main/"+r[len(mainuri)+1:], names)
+        else:
+            path, frag = urllib.parse.urldefrag(r)
+            if path == mainpath:
+                rewrite[r] = "#" + uniquename(frag, names)
+            else:
+                if path not in rewrite:
+                    rewrite[path] = "#" + uniquename(shortname(path), names)
+
+    sortedids = sorted(ids)
+
+    for r in sortedids:
+        rewrite_id(r, uri)
+
+    packed = {"$graph": [], "cwlVersion": metadata["cwlVersion"]
+              }  # type: Dict[Text, Any]
+    namespaces = metadata.get('$namespaces', None)
+
+    schemas = set()  # type: Set[Text]
+    for r in sorted(runs):
+        dcr, metadata = document_loader.resolve_ref(r)
+        if isinstance(dcr, CommentedSeq):
+            dcr = dcr[0]
+            dcr = cast(CommentedMap, dcr)
+        if not isinstance(dcr, dict):
+            continue
+        for doc in (dcr, metadata):
+            if "$schemas" in doc:
+                for s in doc["$schemas"]:
+                    schemas.add(s)
+        if dcr.get("class") not in ("Workflow", "CommandLineTool", "ExpressionTool"):
+            continue
+        dc = cast(Dict[Text, Any], copy.deepcopy(dcr))
+        v = rewrite[r]
+        dc["id"] = v
+        for n in ("name", "cwlVersion", "$namespaces", "$schemas"):
+            if n in dc:
+                del dc[n]
+        packed["$graph"].append(dc)
+
+    if schemas:
+        packed["$schemas"] = list(schemas)
+
+    for r in list(rewrite.keys()):
+        v = rewrite[r]
+        replace_refs(packed, rewrite, r + "/" if "#" in r else r + "#", v + "/")
+
+    import_embed(packed, set())
+
+    if len(packed["$graph"]) == 1:
+        # duplicate 'cwlVersion' inside $graph when there is a single item
+        # because we're printing contents inside '$graph' rather than whole dict
+        packed["$graph"][0]["cwlVersion"] = packed["cwlVersion"]
+    if namespaces:
+        packed["$graph"][0]["$namespaces"] = dict(cast(Dict, namespaces))
+
+    return packed
