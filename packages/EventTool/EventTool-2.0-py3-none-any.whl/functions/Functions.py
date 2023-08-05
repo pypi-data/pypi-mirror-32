61eecc87c54635e2564ab7326b1a2f69b86f25d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:08 +0000

    Add package EventTool/2.0/EventTool-2.0-py3-none-any.whl

---
+++ packages/EventTool/EventTool-2.0-py3-none-any.whl/functions/Functions.py
@@ -0,0 +1,39 @@
+from datetime import date, datetime
+from dateutil.relativedelta import relativedelta
+from event import Event
+from tkinter import messagebox
+import time
+
+
+def printAll(events):
+    counter = 1
+    for event in events:
+        print("%s.  Nazwa: %s \n"
+              "    Opis: %s \n"
+              "    Data: %s \n"
+              % (counter, event.name, event.description, event.startDate))
+        counter = counter + 1
+
+
+def check():
+    while 1:
+        events = Event.getAll(1)
+        for event in events:
+            if event.startDate <= datetime.combine(date.today(),
+                                                   datetime.today().time()):
+                event.isFinished = 1
+                Event.update(event)
+
+        events = Event.getAll(1)
+        for event in events:
+            if event.startDate + relativedelta(days=-2) \
+                    <= datetime.combine(date.today(), datetime.today().time()):
+                messagebox.showinfo("Przypomnienie",
+                                    "W przeciągu 2 dni"
+                                    " masz następujące wydarzenie: \n"
+                                    " Nazwa: %s \n"
+                                    " Opis: %s \n"
+                                    " Data: %s \n"
+                                    % (event.name, event.description,
+                                       event.startDate))
+        time.sleep(10)
