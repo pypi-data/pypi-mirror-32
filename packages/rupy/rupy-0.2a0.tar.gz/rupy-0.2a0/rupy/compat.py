d9ac24b816d2d0fccd042db234ae5765bf1f781f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:23:18 +0000

    Add package rupy/0.2a0/rupy-0.2a0.tar.gz

---
+++ packages/rupy/rupy-0.2a0.tar.gz/rupy-0.2a0/rupy/compat.py
@@ -0,0 +1,31 @@
+import sys
+
+def metabase(metaclass, *bases):
+    if len(bases) == 0:
+        bases = object,
+    base = metaclass('%s meta-base' % metaclass, bases, {})
+    return base
+
+if sys.version_info.major == 2:
+    # Python 2.x compatibility
+    from future_builtins import *
+    range = xrange  # No real need for range() over xrange()
+    def compatible(object):
+        if hasattr(object, "__bytes__"):
+            object.__str__ = object.__bytes__
+        if hasattr(object, "__next__"):
+            object.next = object.__next__
+
+        return object
+
+else:
+    # Python 3.x compatibility
+    long = int
+    buffer = memoryview  # in 2.x these are not compatible in some builtins for some stupid reason
+    file = type("file", (), {})
+    def compatible(object):
+        if hasattr(object, "__unicode__"):
+            setattr(object, "__str__", object.__unicode__)
+        return object
+
+del sys
