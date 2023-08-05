e4ee96371cf7a9c4ed7d4642bb24ae4e0a7882e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:11 +0000

    Add package redash-client/0.2.3/redash_client-0.2.3.tar.gz

---
+++ packages/redash-client/redash_client-0.2.3.tar.gz/redash_client-0.2.3/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+setup(
+  name = 'redash_client',
+  packages = ['redash_client'],
+  version = '0.2.3',
+  description = 'A client for the re:dash API for stmo (https://sql.telemetry.mozilla.org)',
+  author = 'Marina Samuel',
+  author_email = 'msamuel@mozilla.com',
+  url = 'https://github.com/mozilla/redash_client',
+  keywords = ['redash', 'experiments', 'a/b tests'],
+  classifiers = [],
+  install_requires=[
+    "requests == 2.12.1",
+    "python-slugify == 1.2.4",
+    "urllib3 == 1.21.1"
+  ]
+)
