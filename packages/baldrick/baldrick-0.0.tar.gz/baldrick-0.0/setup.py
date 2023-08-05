a0646b57ec495e90f66dbf58c673805e7ffb246f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:20 +0000

    Add package baldrick/0.0/baldrick-0.0.tar.gz

---
+++ packages/baldrick/baldrick-0.0.tar.gz/baldrick-0.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(version='0.0',
+      name="baldrick",
+      description="Helpers for GitHub bots",
+      url='https://github.com/astrofrog/baldrick',
+      packages=['baldrick'],
+      author='Stuart Mumford and Thomas Robitaille')
