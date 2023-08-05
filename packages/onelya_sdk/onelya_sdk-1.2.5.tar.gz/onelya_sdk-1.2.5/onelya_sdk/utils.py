18407bc24b85bd83194561c8ef792303dd49c620
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:51 +0000

    Add package onelya_sdk/1.2.5/onelya_sdk-1.2.5.tar.gz

---
+++ packages/onelya_sdk/onelya_sdk-1.2.5.tar.gz/onelya_sdk-1.2.5/onelya_sdk/utils.py
@@ -0,0 +1,37 @@
+from datetime import datetime
+
+
+def get_array(items, item_class):
+    if items is not None:
+        return [item_class(item) for item in items]
+    return None
+
+
+def get_item(item, item_class):
+    if item is not None:
+        return item_class(item)
+    return None
+
+
+def get_datetime(item):
+    if item is not None:
+        return datetime.strptime(item, '%Y-%m-%dT%X')
+    return None
+
+
+def get_datetime_array(items):
+    if items is not None:
+        return [datetime.strptime(item, '%Y-%m-%dT%X') for item in items]
+    return None
+
+
+def str_datetime(item):
+    if item is not None:
+        return item.strftime('%Y-%m-%dT%X')
+    return None
+
+
+def get_bool_item(item):
+    if item is not None:
+        return bool(item)
+    return False
