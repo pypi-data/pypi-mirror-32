517434cc91e06547523e68520fa102eac6e58689
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:29 +0000

    Add package scriptedforms/0.9.3.dev0/scriptedforms-0.9.3.dev0.tar.gz

---
+++ packages/scriptedforms/scriptedforms-0.9.3.dev0.tar.gz/scriptedforms-0.9.3.dev0/scriptedforms/jlab-build/main.d.ts
@@ -0,0 +1,6 @@
+import './polyfills';
+import './vendors/jupyterlab-styles';
+import './vendors/angular-styles';
+import 'hammerjs';
+import { plugin } from './jupyterlab-extension/jupyterlab-plugin';
+export default plugin;
