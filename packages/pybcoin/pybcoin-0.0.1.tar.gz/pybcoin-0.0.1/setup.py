fbdd735f853ab7f481c9af3315001f111860d92e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:08:35 +0000

    Add package pybcoin/0.0.1/pybcoin-0.0.1.tar.gz

---
+++ packages/pybcoin/pybcoin-0.0.1.tar.gz/pybcoin-0.0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name = 'pybcoin',
+    packages = ['pybcoin'],
+    version = '0.0.1',
+    description = 'Bitcoin Trend Forecast',
+    author='Pushottam Shivraj',
+    author_email='pshivraj@uw.edu',
+    url='https://github.com/rguptauw/pybcoin',
+    classifiers=[],
+)
