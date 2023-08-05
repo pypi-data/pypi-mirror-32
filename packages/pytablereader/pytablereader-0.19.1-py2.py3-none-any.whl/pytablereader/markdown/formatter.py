075ac0bf8c19b1e1baccc5614e3b2ccd1c1fd305
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:09:22 +0000

    Add package pytablereader/0.19.1/pytablereader-0.19.1-py2.py3-none-any.whl

---
+++ packages/pytablereader/pytablereader-0.19.1-py2.py3-none-any.whl/pytablereader/markdown/formatter.py
@@ -0,0 +1,24 @@
+# encoding: utf-8
+
+"""
+.. codeauthor:: Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
+"""
+
+from __future__ import absolute_import, unicode_literals
+
+import typepy
+from pytablereader import InvalidDataError
+
+from ..html.formatter import HtmlTableFormatter
+
+
+class MarkdownTableFormatter(HtmlTableFormatter):
+
+    def __init__(self, source_data):
+        import markdown2
+
+        if typepy.is_null_string(source_data):
+            raise InvalidDataError
+
+        super(MarkdownTableFormatter, self).__init__(
+            markdown2.markdown(source_data, extras=["tables"]))
