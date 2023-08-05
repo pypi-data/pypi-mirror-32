4611e01da34dbc63aa3c894cbe1c31cd56e84a86
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:02 +0000

    Add package EventProcces/0.9/EventProcces-0.9.tar.gz

---
+++ packages/EventProcces/EventProcces-0.9.tar.gz/EventProcces-0.9/setup.py
@@ -0,0 +1,10 @@
+import setuptools
+
+setuptools.setup(
+    name="EventProcces",
+    version="0.9",
+    author="Artur",
+    author_email="artur@wp.com",
+    description="Package to process events in Calendar",
+    packages=['event','functions'],
+)
