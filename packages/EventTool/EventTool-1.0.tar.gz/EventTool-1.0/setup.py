23fdbed2f2c4f91436e6447222370ed692bc1879
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:08 +0000

    Add package EventTool/1.0/EventTool-1.0.tar.gz

---
+++ packages/EventTool/EventTool-1.0.tar.gz/EventTool-1.0/setup.py
@@ -0,0 +1,10 @@
+import setuptools
+
+setuptools.setup(
+    name="EventTool",
+    version="1.0",
+    author="Artur",
+    author_email="artur@wp.com",
+    description="Package to process events in Calendar",
+    packages=['event','functions'],
+)
