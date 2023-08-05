e56e27111307d480c5a6dc5b45bd8f4d2a7e8e2f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:28 +0000

    Add package byudml/0.5.0/byudml-0.5.0.tar.gz

---
+++ packages/byudml/byudml-0.5.0.tar.gz/byudml-0.5.0/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+	name="byudml",
+	packages = find_packages(),
+	version = "0.5.0",
+	description = "A collection of DARPA D3M primitives developed by BYU",
+	author = "Roland Laboulaye, Brandom Schoenfeld, Jarom Christensen",
+	url = "https://github.com/byu-dml/d3m-primitives",
+	keywords = ["metalearning", "metafeature", "machine learning", "metalearn", "d3m_primitive"],
+	install_requires = [
+		"metalearn==0.4.5",
+		"numpy",
+		"pandas"
+	],
+	entry_points = {
+    	'd3m.primitives': [
+        	'byudml.imputer.RandomSamplingImputer = byudml.imputer:RandomSamplingImputer',
+        	'byudml.metafeature_extraction.MetafeatureExtractor = byudml.metafeature_extraction:MetafeatureExtractor'
+    	]
+	}
+)
