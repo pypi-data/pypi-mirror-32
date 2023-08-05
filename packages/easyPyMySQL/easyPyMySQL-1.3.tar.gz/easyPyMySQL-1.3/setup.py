fce36a08d92a05687ed2370be48b06dfc6b85646
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:09 +0000

    Add package easyPyMySQL/1.3/easyPyMySQL-1.3.tar.gz

---
+++ packages/easyPyMySQL/easyPyMySQL-1.3.tar.gz/easyPyMySQL-1.3/setup.py
@@ -0,0 +1,20 @@
+﻿from setuptools import setup, find_packages
+
+setup(
+    name='easyPyMySQL',  # 名称
+    version='1.3',  # 版本
+    description="简单易用的数据库ORM模块",
+    keywords='mysql orm',
+    author='akiyamaryou',  # 作者
+    author_email='crh51306@gmail.com',  # 作者邮箱
+    url='https://github.com/AkiYama-Ryou',  # 作者链接
+    packages=find_packages(exclude=['easyPyMySQL']),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[  # 需求的第三方模块
+        'pymysql',
+    ],
+)
+
+
+
