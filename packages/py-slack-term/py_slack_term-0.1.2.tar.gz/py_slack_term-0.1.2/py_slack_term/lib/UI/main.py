d69181250f66bc52d6f7885b1840c4de86243bd9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:10:40 +0000

    Add package py-slack-term/0.1.2/py_slack_term-0.1.2.tar.gz

---
+++ packages/py-slack-term/py_slack_term-0.1.2.tar.gz/py_slack_term-0.1.2/py_slack_term/lib/UI/main.py
@@ -0,0 +1,78 @@
+import npyscreen
+from ...lib import Logger
+from ...lib.UI.widgets import BoxedChannelMessages
+from ...lib.UI.widgets import BoxedChannelSelector
+from ...lib.UI.widgets import BoxedMessageComposer
+from ...lib.slack_client.API import Message
+
+from py_slack_term.lib.slack_client.RTM import SlackRTMClient
+
+
+class SlackWindowForm(npyscreen.FormBaseNew):
+    def __init__(self, *args, slack_client=None, **kwargs):
+        self.logger = Logger('SlackWindowForm')
+        self.slack_client = slack_client
+        rtm_url = self.slack_client.rtm_connect()
+        super(SlackWindowForm, self).__init__(*args, **kwargs)
+        self.channel_selector = None
+        self.current_channel = None
+
+        self.rtm_client = SlackRTMClient(rtm_url, self.new_RTM_event)
+        self.rtm_client.start()
+
+    def create(self):
+        y, x = self.useable_space()
+
+        self.channel_selector = self.add_widget(BoxedChannelSelector, max_width=x // 5)
+        self.channel_messages = self.add_widget(BoxedChannelMessages,
+                                                relx=self.channel_selector.width + 3,
+                                                rely=self.channel_selector.rely,
+                                                max_height=y-8)  # type: BoxedChannelMessages
+        self.message_composer = self.add_widget(BoxedMessageComposer, relx=self.channel_messages.relx, rely=y-6, max_height=4)
+
+        self.refresh_channels()
+
+    def select_channel(self, ch):
+        self.current_channel = ch
+        self.channel_messages.clear_buffer()
+        self.channel_messages.buffer(list(reversed(ch.fetch_messages())))
+        self.channel_messages.set_channel(ch)
+        self.current_channel.has_unread = False
+        self.channel_messages.display()
+
+    def refresh_channels(self):
+        self.channel_selector.update_channels(self.slack_client.get_active_channels_im_in())
+        self.channel_selector.display()
+
+    def send_message(self):
+        message = self.message_composer.value
+        self.message_composer.clear_message()
+        self.current_channel.post_message(msg=message)
+
+    def new_RTM_event(self, event: dict):
+
+        event_type = event.get('type')
+        if event_type == 'message':
+            message = Message(self.slack_client, **event)
+            if self.current_channel:
+                if event.get('channel') == str(self.current_channel.id):
+                    self.channel_messages.buffer(message)
+                    self.current_channel.mark(message.ts)
+                self.current_channel.has_unread = False
+        self.display()
+        self.channel_messages.display()
+
+    def stop(self):
+        self.rtm_client.stop()
+
+
+class SlackApplication(npyscreen.NPSAppManaged):
+    def __init__(self, *args, slack_client=None, **kwargs):
+        self.slack_client = slack_client
+        super(SlackApplication, self).__init__(*args, **kwargs)
+
+    def onStart(self):
+        self.main_window = self.addForm('MAIN', SlackWindowForm, name='Slack Terminal', slack_client=self.slack_client)
+
+    def stop(self):
+        self.main_window.stop()
---
+++ packages/py-slack-term/py_slack_term-0.1.2.tar.gz/py_slack_term-0.1.2/py_slack_term/lib/UI/widgets/channel_messages.py
@@ -0,0 +1,111 @@
+import re
+
+import npyscreen
+
+from ....lib.slack_client.API import Channel, Message
+
+
+class ChannelMessages(npyscreen.BufferPager):
+    message_format = "{user}: {text}"
+    mention_regex = re.compile("<@[A-Z0-9]+>")
+
+    def __init__(self, *args, **kwargs):
+        super(ChannelMessages, self).__init__(*args, **kwargs)
+        self.editable = False
+        self.autowrap = True
+
+    def display_value(self, vl: Message) -> str:
+        if isinstance(vl, Message):
+            message_dict = vl.to_format_dict()
+            match = re.search(self.mention_regex, message_dict.get('text'))
+            if match:
+                user_id = match.group().replace('<', '').replace('@', '').replace('>', '')
+                message_dict['text'] = message_dict.get('text').replace(match.group(),
+                                                                    '@' + vl.client.users.get(user_id).get_name())
+            return self.message_format.format(**message_dict)
+        else:
+            return vl
+
+    def display(self, *args, **kwargs):
+        super(ChannelMessages, self).display(*args, **kwargs)
+
+    def clear_buffer(self, *args, **kwargs):
+        """
+        compatibility with non pythonic code in library
+        """
+        self.clearBuffer(*args, **kwargs)
+
+    # TODO: this is a monkey patch of the base class Pager
+    # TODO: this method can be removed when https://github.com/npcole/npyscreen/pull/60 is merged
+    def update(self, clear=True):
+        #we look this up a lot. Let's have it here.
+        if self.autowrap:
+            self.setValuesWrap(list(self.display_value(l) for l in self.values))
+
+        if self.center:
+            self.centerValues()
+
+        display_length = len(self._my_widgets)
+        values_len = len(self.values)
+
+        if self.start_display_at > values_len - display_length:
+            self.start_display_at = values_len - display_length
+        if self.start_display_at < 0: self.start_display_at = 0
+
+        indexer = 0 + self.start_display_at
+        for line in self._my_widgets[:-1]:
+            self._print_line(line, indexer)
+            indexer += 1
+
+        # Now do the final line
+        line = self._my_widgets[-1]
+
+        if values_len <= indexer+1:
+            self._print_line(line, indexer)
+        else:
+            line.value = MORE_LABEL
+            line.highlight = False
+            line.show_bold = False
+
+        for w in self._my_widgets:
+            # call update to avoid needless refreshes
+            w.update(clear=True)
+        # There is a bug somewhere that affects the first line.  This cures it.
+        # Without this line, the first line inherits the color of the form when not editing. Not clear why.
+        self._my_widgets[0].update()
+
+
+class BoxedChannelMessages(npyscreen.BoxTitle):
+    _contained_widget = ChannelMessages
+
+    def __init__(self, *args, **kwargs):
+        self.name = 'Messages'
+        super(BoxedChannelMessages, self).__init__(*args, **kwargs)
+
+    def buffer(self, *args, **kwargs) -> None:
+        self.entry_widget.buffer(*args, **kwargs)
+
+    def clear_buffer(self, *args, **kwargs) -> None:
+        self.entry_widget.clear_buffer(*args, **kwargs)
+
+    def set_channel(self, ch: Channel) -> None:
+        """
+        function to set title of box to channel name and display associated information
+        """
+        new_name = "Messages | {name}".format(name=ch.name)
+
+        if ch.topic:
+            topic = ch.topic.get('value')
+        elif ch.purpose:
+            topic = ch.purpose.get('value')
+        else:
+            topic = None
+        if topic:
+            new_name += " ({})".format(topic)
+
+        if ch.is_private:
+            new_name += " [PRIVATE]"
+
+        self.name = new_name
+
+
