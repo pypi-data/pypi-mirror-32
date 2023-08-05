29edf86adeedef05f7c60f26e7b2be4e049a1868
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:09:56 +0000

    Add package ModelFactory/7/ModelFactory-7.tar.gz

---
+++ packages/ModelFactory/ModelFactory-7.tar.gz/ModelFactory-7/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+setup(
+  name = 'ModelFactory',
+  packages = ['ModelFactory'], # this must be the same as the name above
+  version = '7',
+  description = 'Simple String library',
+  author = 'Sudarson',
+  author_email = 'write2sudarson@gmail.com',
+  url = 'https://github.com/sudarsontm/Python',
+  download_url = 'https://github.com/sudarsontm/Python/archive/MF_7.tar.gz',
+  keywords = ['tag1', 'tag2'],
+  classifiers = [],
+)
+
