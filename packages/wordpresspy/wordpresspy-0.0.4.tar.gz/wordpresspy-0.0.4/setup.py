5392709f28effa96ace054d35da926ee05561034
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:37 +0000

    Add package wordpresspy/0.0.4/wordpresspy-0.0.4.tar.gz

---
+++ packages/wordpresspy/wordpresspy-0.0.4.tar.gz/wordpresspy-0.0.4/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(name='wordpresspy',
+      version='0.0.4',
+      description='Library for management WordPress and its plugins',
+      url='https://github.com/weecan-group/wordpresspy',
+      author='Weecan',
+      author_email='contato@moacirbrg.org',
+      license='MIT',
+      packages=['wordpresspy'],
+      zip_safe=False,
+      install_requires=[
+        'six'
+      ],
+      classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+      ))
