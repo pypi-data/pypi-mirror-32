a42f54ed5963526462f880c1d057226e395edaaf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:12:57 +0000

    Add package star-builder/0.0.8/star-builder-0.0.8.tar.gz

---
+++ packages/star-builder/star-builder-0.0.8.tar.gz/star-builder-0.0.8/star_builder/app.py
@@ -0,0 +1,42 @@
+import logging
+
+from apistar import ASyncApp
+
+from .helper import load_packages, routing, print_routing
+from .service import Service
+
+__all__ = ["Application"]
+
+logger = logging.getLogger("app")
+
+
+class Application(ASyncApp):
+    """
+    可以动态发现当前项目根目录下所有routes的service中的handler
+    """
+    def __init__(self,
+                 template_dir=None,
+                 static_dir=None,
+                 packages=None,
+                 schema_url='/schema/',
+                 docs_url='/docs/',
+                 static_url='/static/',
+                 components=None,
+                 event_hooks=None):
+        load_packages(".")
+        include = routing(Service, None)
+        if include:
+            routes = [include]
+            print_routing(routes, callback=logger.debug)
+        else:
+            logger.info("Noting to route. ")
+            routes = []
+        super(Application, self).__init__(
+            routes,
+            template_dir=template_dir,
+            static_dir=static_dir,
+            schema_url=schema_url,
+            docs_url=docs_url,
+            static_url=static_url,
+            components=components,
+            event_hooks=event_hooks)
