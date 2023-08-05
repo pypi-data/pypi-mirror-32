6823a201d9e446135b542794a6ba3b98e6d28376
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:12:39 +0000

    Add package bw-plex/0.0.7/bw_plex-0.0.7.tar.gz

---
+++ packages/bw-plex/bw_plex-0.0.7.tar.gz/bw_plex-0.0.7/tests/conftest.py
@@ -0,0 +1,122 @@
+import os
+import shutil
+import sys
+import tempfile
+from datetime import datetime as DT
+
+
+from plexapi.video import Episode, Show
+from plexapi.compat import makedirs
+from sqlalchemy.orm.exc import NoResultFound
+import pytest
+
+fp = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'bw_plex')
+
+# I dont like it..
+sys.path.insert(1, fp)
+
+
+import bw_plex
+
+old_def = bw_plex.DEFAULT_FOLDER
+# Change default folder so we dont mess up the users normal things..
+# This needs to deleted after all the tests are done.
+bw_plex.DEFAULT_FOLDER = os.path.join(tempfile.gettempdir(), 'bw_plex_test_root')
+
+# Delete any old stuff in the test dir..
+if os.path.exists(bw_plex.DEFAULT_FOLDER):
+    shutil.rmtree(bw_plex.DEFAULT_FOLDER)
+
+# Copy the stuff over to the new folder.
+shutil.copytree(old_def, bw_plex.DEFAULT_FOLDER)
+
+#if not os.path.exists(bw_plex.DEFAULT_FOLDER):
+#    os.makedirs(bw_plex.DEFAULT_FOLDER)
+
+bw_plex.THEMES = os.path.join(bw_plex.DEFAULT_FOLDER, 'themes')
+bw_plex.TEMP_THEMES = os.path.join(bw_plex.DEFAULT_FOLDER, 'temp_themes')
+bw_plex.FP_HASHES = os.path.join(bw_plex.DEFAULT_FOLDER, 'hashes.pklz')
+bw_plex.LOG_FILE = os.path.join(bw_plex.DEFAULT_FOLDER, 'log.txt')
+bw_plex.INI_FILE = os.path.join(bw_plex.DEFAULT_FOLDER, 'config.ini')
+
+import bw_plex.plex as plex
+import bw_plex.misc as misc
+import bw_plex.credits as credits
+from bw_plex.db import session_scope, Preprocessed
+
+TEST_DATA = os.path.join(os.path.dirname(__file__), 'test_data')
+
+
+@pytest.fixture()
+def outro_file():
+    fp = os.path.join(TEST_DATA, 'out.mkv')
+    return fp
+
+
+@pytest.fixture()
+def intro_file():
+    fp = os.path.join(TEST_DATA, 'dexter_s03e01_intro.mkv')
+    return fp
+
+
+@pytest.fixture(scope='session')
+def HT():
+    return misc.get_hashtable()
+
+
+@pytest.fixture()
+def in_db(ratingkey):
+    rk = int(ratingkey)
+    with session_scope() as se:
+        try:
+            item = se.query(Preprocessed).filter_by(ratingKey=rk).one()
+            return item
+        except NoResultFound:
+            return
+
+
+@pytest.fixture()
+def episode(mocker):
+
+    ep = mocker.MagicMock(spec=Episode)
+    ep.TYPE = 'episode'
+    ep.name = ''
+    ep.title = ''
+    ep.grandparentTitle = 'Dexter'
+    ep.ratingKey = 1337
+    ep._server = ''
+    ep.title = 'Dexter'
+    ep.index = 1
+    ep.parentIndex = 1
+    ep.grandparentRatingKey = 1337
+    ep.grandparentTheme = ''
+    ep.duration = 60 * 60 * 1000  # 1h in ms
+    ep.updatedAt = DT(1970, 1, 1)
+
+    def _prettyfilename():
+        return 'Dexter.s01.e01'
+
+    def iterParts():
+        yield os.path.join(TEST_DATA, 'dexter_s03e01_intro.mkv')
+
+    ep._prettyfilename = _prettyfilename
+
+    return ep
+
+
+@pytest.fixture()
+def media(mocker, episode):
+    media = mocker.Mock(spec=Show)
+    media.TYPE = 'show'
+    media.name = 'dexter'
+    media.ratingKey = 1337
+    media.theme = ''
+    media._server = ''
+    media.title = 'dexter'
+
+    def _episodes():
+        return [episode]
+
+    media.episodes = _episodes
+
+    return media
---
+++ packages/bw-plex/bw_plex-0.0.7.tar.gz/bw_plex-0.0.7/tests/test_cli.py
@@ -0,0 +1,184 @@
+import os
+import json
+
+from conftest import plex
+import click
+
+
+def test_cli():
+    pass
+
+
+def test_create_config(monkeypatch, cli_runner, tmpdir):
+    fullpath = os.path.join(str(tmpdir), 'some_config.ini')
+    res = cli_runner.invoke(plex.create_config, ['-fp', fullpath])
+    click.echo(res.output)
+
+    assert os.path.exists(fullpath)
+
+
+def test_check(episode, intro_file, cli_runner, tmpdir, monkeypatch, HT, mocker):
+    def fetchItem(i):
+        return episode
+    m = mocker.Mock()
+    m.fetchItem = fetchItem
+
+    def zomg(*args, **kwargs):
+        pass
+
+    monkeypatch.setitem(plex.CONFIG, 'theme_source', 'tvtunes')
+    monkeypatch.setattr(plex, 'check_file_access', lambda k: intro_file)
+    monkeypatch.setattr(plex, 'HT', HT)
+    monkeypatch.setattr(plex, 'PMS', m)
+    monkeypatch.setattr(plex, 'find_next', lambda k: None)
+    monkeypatch.setattr(plex, 'client_action', zomg)
+
+    data = {"PlaySessionStateNotification": [{"guid": "",
+                                              "key": "/library/metadata/1337",
+                                              "playQueueItemID": 22631,
+                                              "ratingKey": "1337",
+                                              "sessionKey": "84",
+                                              "state": "playing",
+                                              "transcodeSession": "4avh8p7h64n4e9a16xsqvr9e",
+                                              "url": "",
+                                              "viewOffset": 1000}],
+            "size": 1,
+            "type": "playing"
+            }
+
+    # This should add the shit to the db. Lets check it.
+    plex.check(data)
+    plex.POOL.close()
+    plex.POOL.join()
+
+    with plex.session_scope() as se:
+        assert se.query(plex.Preprocessed).filter_by(ratingKey=episode.ratingKey).one()
+
+        # lets check that we can export db shit too.
+
+        res = cli_runner.invoke(plex.export_db, ['-f', 'json', '-fp', str(tmpdir), '-wf'])
+        print(res.output)
+
+        fp = os.path.join(str(tmpdir), 'Preprocessed.json')
+        assert os.path.exists(fp)
+
+        with open(fp, 'r') as f:
+            assert json.load(f)
+
+
+
+def _test_process_to_db(episode, intro_file, cli_runner, tmpdir, monkeypatch, HT, mocker):
+    # This is tested in check
+    def fetchItem(i):
+        return episode
+    m = mocker.Mock()
+    m.fetchItem = fetchItem
+
+    monkeypatch.setitem(plex.CONFIG, 'theme_source', 'tvtunes')
+    monkeypatch.setattr(plex, 'check_file_access', lambda k: intro_file)
+    monkeypatch.setattr(plex, 'HT', HT)
+    monkeypatch.setattr(plex, 'PMS', m)
+    monkeypatch.setattr(plex, 'find_next', lambda k: None)
+
+    plex.task(1337, 1)
+
+    with plex.session_scope() as se:
+        assert se.query(plex.Preprocessed).filter_by(ratingKey=episode.ratingKey).one()
+
+        # lets check that we can export db shit too.
+        res = cli_runner.invoke(plex.export_db, ['-f', 'json', '-fp', str(tmpdir), '-wf'])
+        # print(res.output)
+
+        fp = os.path.join(str(tmpdir), 'Preprocessed.json')
+        assert os.path.exists(fp)
+
+        with open(fp, 'r') as f:
+            assert json.load(f)
+
+
+def test_process(cli_runner, monkeypatch, episode, media, HT, intro_file, mocker):
+    # Let the mock begin..
+    mocker.patch.object(plex, 'find_all_shows', side_effect=[[media], [episode]])
+    mocker.patch('click.prompt', side_effect=['0', '0'])
+
+    def fetchItem(i):
+        return episode
+
+    m = mocker.Mock()
+    m.fetchItem = fetchItem
+
+    def zomg(*args, **kwargs):
+        pass
+
+    monkeypatch.setitem(plex.CONFIG, 'theme_source', 'tvtunes')
+    monkeypatch.setattr(plex, 'check_file_access', lambda k: intro_file)
+    monkeypatch.setattr(plex, 'HT', HT)
+    monkeypatch.setattr(plex, 'PMS', m)
+    monkeypatch.setattr(plex, 'find_next', lambda k: None)
+
+    res = cli_runner.invoke(plex.process, ['-n', 'dexter', '-s', '1', '-t', '2', '-sd'])
+    #print(res.output)
+
+
+def test_add_theme_to_hashtable(cli_runner, monkeypatch, HT):
+    # We just want to check that this doesnt blow up..
+    monkeypatch.setattr(plex, 'get_hashtable', HT)
+    cli_runner.invoke(plex.add_theme_to_hashtable, [2, None])
+
+
+def test_ffmpeg_process(cli_runner, intro_file):
+    res = cli_runner.invoke(plex.ffmpeg_process, [intro_file])
+    assert len(res.output)
+    # In short we miss on this episode as we find the start of the theme, not the end.
+    # correct value should be sec ~217.
+    # we dont care about the result. This file is already test other places.
+
+
+def test_manually_correct_theme():
+    pass
+
+
+# Disable for now. Add this to test_check.
+def _test_timeline(intro_file, HT, monkeypatch, mocker, episode):
+
+    def fetchItem(i):
+        return episode
+    m = mocker.Mock()
+    m.fetchItem = fetchItem
+
+    monkeypatch.setitem(plex.CONFIG, 'theme_source', 'tvtunes')
+    monkeypatch.setattr(plex, 'check_file_access', lambda k: intro_file)
+    monkeypatch.setattr(plex, 'HT', HT)
+    monkeypatch.setattr(plex, 'PMS', m)
+    monkeypatch.setattr(plex, 'find_next', lambda k: None)
+
+    data = {"type": "timeline",
+            "size": 1,
+            "TimelineEntry": [{"identifier": "com.plexapp.plugins.library",
+                               "sectionID": 2,
+                               "itemID": 1337,
+                               "type": 4,
+                               "title": "Dexter S01 E01",
+                               "state": 0,
+                               "mediaState": "created",
+                               "queueSize": 8,
+                               "updatedAt": 1526744644}]
+    }
+
+    deleted_dict = {'type': 'timeline',
+                    'size': 1,
+                    'TimelineEntry': [{'identifier': 'com.plexapp.plugins.library',
+                                       'sectionID': 2,
+                                       'itemID': 2041,
+                                       'parentItemID': 2035,
+                                       'rootItemID': 2032,
+                                       'type': 4,
+                                       'title': '2 Broke Girls S01 E06',
+                                       'state': 9,
+                                       'metadataState': 'deleted',
+                                       'updatedAt': 1526859691}]}
+
+    plex.timeline(data)
+    plex.POOL.close()
+    plex.POOL.join()
+    assert len(HT.get_theme(episode))
