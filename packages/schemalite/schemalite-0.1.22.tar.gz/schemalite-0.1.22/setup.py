85d07ba94a9a3614dce3bae4b099a59e329366f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:20 +0000

    Add package schemalite/0.1.22/schemalite-0.1.22.tar.gz

---
+++ packages/schemalite/schemalite-0.1.22.tar.gz/schemalite-0.1.22/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='schemalite',
+    version='0.1.22',
+    description='A minimalistic Schema validation library',
+    long_description='A minimalistic Schema validation library',
+    packages=['schemalite'],
+    include_package_data=True,
+    license='MIT',
+    author='SuryaSankar',
+    author_email='suryashankar.m@gmail.com')
