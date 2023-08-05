1aa8454a9802d95712850d2767d9d85b1c97d45a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:09:44 +0000

    Add package lidar/0.1.5/lidar-0.1.5.tar.gz

---
+++ packages/lidar/lidar-0.1.5.tar.gz/lidar-0.1.5/lidar/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for lidar."""
+
+__author__ = """Qiusheng Wu"""
+__email__ = 'giswqs@gmail.com'
+__version__ = '0.1.5'
+
+from .filling import ExtractSinks
+from .slicing import DelineateDepressions
