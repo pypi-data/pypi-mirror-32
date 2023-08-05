c5eb2f764d649cdfa9bc55d87b71b5250e789b67
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:08:49 +0000

    Add package Cortex-Runner/1.0.1/Cortex%20Runner-1.0.1.tar.gz

---
+++ packages/Cortex-Runner/Cortex%20Runner-1.0.1.tar.gz/Cortex Runner-1.0.1/setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+setup(
+    name='Cortex Runner',
+    version='1.0.1',
+    description='Cortex jobs automation for TheHive.',
+    long_description='',
+    author='Rémi ALLAIN',
+    author_email='rallain@cyberprotect.fr',
+    maintainer='Rémi ALLAIN',
+    url='https://github.com/Cyberprotect/CortexRunner',
+    license='Apache 2.0',
+    packages=['cortexrunner'],
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Intended Audience :: Information Technology',
+        'Intended Audience :: Science/Research',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Security',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ],
+    include_package_data=True,
+    install_requires=['requests', 'thehive4py']
+)
