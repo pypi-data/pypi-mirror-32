afa195b3f0771b021091016911cfd8c3be1c7f79
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:12:54 +0000

    Add package mooquant_mercadobitcoin/0.1.0/mooquant_mercadobitcoin-0.1.0.tar.gz

---
+++ packages/mooquant_mercadobitcoin/mooquant_mercadobitcoin-0.1.0.tar.gz/mooquant_mercadobitcoin-0.1.0/setup.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""The setup script."""
+
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    from distutils.core import setup, find_packages
+
+from mooquant_mercadobitcoin import __version__
+
+with open('README.md') as readme_file:
+    readme = readme_file.read()
+
+requirements = ['mooquant', ]
+test_requirements = ['mooquant', 'pytest', ]
+setup_requirements = ['mooquant', ]
+
+setup(
+    name='mooquant_mercadobitcoin',
+    version=__version__,
+    description="mooquant bitfinex module",
+    long_description=readme + '\n\n',
+    author="bopo.wang",
+    author_email='ibopo@126.com',
+    url='https://github.com/bopo/mooquant_mercadobitcoin',
+    packages=find_packages(
+        include=[
+            'mooquant_mercadobitcoin',
+            'mooquant_mercadobitcoin.*']),
+    include_package_data=True,
+    install_requires=requirements,
+    license="MIT license",
+    zip_safe=False,
+    keywords='mooquant_mercadobitcoin',
+    entry_points={
+        'console_scripts': [
+            'mooquant_mercadobitcoin = mooquant_mercadobitcoin.cli:main',
+        ]},
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+    test_suite='tests',
+    tests_require=test_requirements,
+    setup_requires=setup_requirements,
+)
