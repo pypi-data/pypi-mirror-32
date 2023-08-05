a555f77ba6f4e988aa73d73de007daaa18a3b405
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:43 +0000

    Add package dkamotsky.logr/1.0.7/dkamotsky.logr-1.0.7.tar.gz

---
+++ packages/dkamotsky.logr/dkamotsky.logr-1.0.7.tar.gz/dkamotsky.logr-1.0.7/src/logr/config.py
@@ -0,0 +1,8 @@
+APP_NAME='logr'
+APP_LOG_FILE='./application.log'
+APP_LOG_LEVEL="INFO"
+APP_LOG_SIZE=5*1024*1024
+APP_LOG_BACKUPS=100
+APP_LOG_FORMAT='%(asctime)s |%(processName)s|%(threadName)s|%(name)s| %(levelname)s | %(funcName)s(%(lineno)d) %(message)s'
+APP_LOG_QUEUE_SIZE=1000000
+
