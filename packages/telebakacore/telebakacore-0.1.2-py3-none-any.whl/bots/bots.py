1e2b7a9922e024b1ae841fa7ce8ecdd7106addac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:53 +0000

    Add package telebakacore/0.1.2/telebakacore-0.1.2-py3-none-any.whl

---
+++ packages/telebakacore/telebakacore-0.1.2-py3-none-any.whl/bots/bots.py
@@ -0,0 +1,32 @@
+import logging
+from django.urls import path, include
+
+from telegram import Bot
+from telegram.ext import Dispatcher
+import lazy_object_proxy
+
+from bots.models import TelegramBot
+from bots.utils import get_plugins
+from bots.urls import urlpatterns
+
+
+def collect_bots():
+    dispatchers = {}
+
+    plugins = get_plugins()
+
+    for telegram_bot in TelegramBot.objects.all():
+        bot = Bot(telegram_bot.token)
+        dispatcher = Dispatcher(bot, None, workers=0)
+        try:
+            module = plugins[telegram_bot.plugin_name]
+
+            module.bot.setup(dispatcher)
+            dispatchers[str(telegram_bot.pk)] = dispatcher
+        except KeyError:
+            logging.warning(f'Module {telegram_bot.plugin_name} was not found')
+
+    return dispatchers
+
+
+dispatchers = lazy_object_proxy.Proxy(collect_bots)
