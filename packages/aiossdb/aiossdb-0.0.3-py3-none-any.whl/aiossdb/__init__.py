92a66ff9d3e1cfbaf0f6c46d581b021d74b91cca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:30 +0000

    Add package aiossdb/0.0.3/aiossdb-0.0.3-py3-none-any.whl

---
+++ packages/aiossdb/aiossdb-0.0.3-py3-none-any.whl/aiossdb/__init__.py
@@ -0,0 +1,10 @@
+from .connection import create_connection, SSDBConnection
+from .errors import SSDBError, ReplyError, ConnectionClosedError, ProtocolError, PoolClosedError
+from .parser import SSDBParser
+from .pool import create_pool, SSDBConnectionPool
+from .client import Client
+
+__version__ = '0.0.3'
+__author__ = 'Kevin Du'
+__email__ = 'dgt_x@foxmail.com'
+__licence__ = 'MIT'
