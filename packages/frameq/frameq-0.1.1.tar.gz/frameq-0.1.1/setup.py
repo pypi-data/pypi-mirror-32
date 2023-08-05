64da6be83b32fc85ae207baa213459337ab0e35b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:57 +0000

    Add package frameq/0.1.1/frameq-0.1.1.tar.gz

---
+++ packages/frameq/frameq-0.1.1.tar.gz/frameq-0.1.1/setup.py
@@ -0,0 +1,55 @@
+# Always prefer setuptools over distutils
+from setuptools import setup, find_packages
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='frameq',
+    version='0.1.1',
+    description='FrameWorks For Building Bioinformatics Platform',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://gene.pku.edu.cn',
+    author='Xiannian Zhang',
+    author_email='friedpine@gmail.com',
+
+    classifiers = [
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.6',
+    ],
+
+    keywords = 'sample setuptools development',
+
+    packages = find_packages(include=['base*']),
+
+    install_requires=[
+        'pandas',
+        'numpy',
+        'matplotlib',
+        'jinja2',
+        'click',
+        'sqlalchemy',
+        'configparser'
+    ],
+
+    extras_require={
+        'dev': ['check-manifest'],
+        'test': ['coverage'],
+    },
+
+    entry_points={
+        'console_scripts': [
+            'frameq-project = frameq.Django.cmd:cli',
+            'frameq-html = frameq.HTML.cmd:cli'
+        ]
+    }
+)
