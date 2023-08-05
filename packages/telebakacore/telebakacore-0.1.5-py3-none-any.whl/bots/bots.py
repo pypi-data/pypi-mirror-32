fe25cecaf5a97672a259fa750bdc567ee8f806c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:24 +0000

    Add package telebakacore/0.1.5/telebakacore-0.1.5-py3-none-any.whl

---
+++ packages/telebakacore/telebakacore-0.1.5-py3-none-any.whl/bots/bots.py
@@ -0,0 +1,40 @@
+import logging
+
+from django.conf import settings
+from raven import Client
+from telegram import Bot
+from telegram.ext import Dispatcher
+import lazy_object_proxy
+
+from bots.models import TelegramBot
+from bots.utils import get_plugins
+
+
+client = Client(settings.SENTRY_DSN)
+
+
+def error_handler(bot, update, error):
+    client.captureException()
+
+
+def collect_bots():
+    dispatchers = {}
+
+    plugins = get_plugins()
+
+    for telegram_bot in TelegramBot.objects.all():
+        bot = Bot(telegram_bot.token)
+        dispatcher = Dispatcher(bot, None, workers=0)
+        dispatcher.add_error_handler(error_handler)
+        try:
+            module = plugins[telegram_bot.plugin_name]
+
+            module.bot.setup(dispatcher)
+            dispatchers[str(telegram_bot.pk)] = dispatcher
+        except KeyError:
+            logging.warning(f'Module {telegram_bot.plugin_name} was not found')
+
+    return dispatchers
+
+
+dispatchers = lazy_object_proxy.Proxy(collect_bots)
