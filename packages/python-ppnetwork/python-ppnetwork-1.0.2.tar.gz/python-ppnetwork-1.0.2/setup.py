9511acb6e83176a4a88b4c3c2b02f190fb6fba82
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:11 +0000

    Add package python-ppnetwork/1.0.2/python-ppnetwork-1.0.2.tar.gz

---
+++ packages/python-ppnetwork/python-ppnetwork-1.0.2.tar.gz/python-ppnetwork-1.0.2/setup.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+# coding=utf-8
+
+from setuptools import setup, find_packages
+
+setup(name='python-ppnetwork',
+#       py_modules = ["ppnetwork"],
+      packages = find_packages(),
+#       package_data = {"":["*.yaml'"]},
+#       data_files=[('config', ['*.yaml']),],
+#       exclude_package_data = { 'test': ['test*.py'] },
+      entry_points = {
+        'console_scripts': [
+            'ppnetwork = ppnetwork:main',
+        ],},
+      author='gonewind.he',
+      author_email='gonewind.he@gmail.com',
+      maintainer='gonewind',
+      maintainer_email='gonewind.he@gmail.com',
+      url='https://github.com/gonewind73/ppnetwork',
+      description='Linux/Windows  Virtual LAN application written in  python',
+      long_description=open('README.rst',encoding='utf-8').read(),
+      version='1.0.2',
+      install_requires=['python-pytuntap>=1.0.5',        ],
+      python_requires='>=3',
+      platforms=["Linux","Windows"],
+      classifiers=[
+          'Development Status :: 5 - Production/Stable',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: MIT License',
+          'Programming Language :: Python :: 3',
+          'Topic :: Software Development :: Libraries :: Python Modules',
+          'Topic :: System :: Networking'])
