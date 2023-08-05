fda214db0c363782cc97389f5f65f5f60d10d9cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:08:48 +0000

    Add package list-nester-ch/1.3.0/list_nester_ch-1.3.0.tar.gz

---
+++ packages/list-nester-ch/list_nester_ch-1.3.0.tar.gz/list_nester_ch-1.3.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+        name              = 'list_nester_ch',
+        version           = '1.3.0',
+        py_modules        = ['list_nester_ch'],
+        author            = 'sleekcharly',
+        author_email      = 'c_ukasoanya@yahoo.com',
+        url               = 'http://github.com/sleekcharly',
+        description       = 'A simple printer of nested lists',
+        )
