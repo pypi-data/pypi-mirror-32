fba48d8e85bb92b2ea9a6a67306b73ea56988fdc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:15:41 +0000

    Add package comiccrawler/2018.5.24/comiccrawler-2018.5.24-py3-none-any.whl

---
+++ packages/comiccrawler/comiccrawler-2018.5.24-py3-none-any.whl/comiccrawler/mission_manager.py
@@ -0,0 +1,297 @@
+#! python3
+
+"""Mission Manager"""
+
+import json
+import hashlib
+
+from collections import OrderedDict
+from threading import Lock
+from contextlib import suppress, contextmanager
+
+from worker import current
+
+from .safeprint import print
+from .core import Mission, Episode, MissionProxy, safefilepath, mission_lock
+from .io import backup, open, remove, move
+from .profile import get as profile
+from .channel import mission_ch
+
+def get_mission_id(mission):
+	"""Use title and sha1 of URL as mission id"""
+	return "{title} [{sha1}]".format(
+		title=mission.title,
+		sha1=hashlib.sha1(mission.url.encode("utf-8")).hexdigest()[:6]
+	)
+	
+@contextmanager
+def edit_mission_id(mission):
+	"""A contextmanager for changing mission title."""
+	old_id = get_mission_id(mission)
+	yield
+	new_id = get_mission_id(mission)
+	
+	if old_id == new_id:
+		return
+	
+	old_path = make_ep_path(old_id)
+	new_path = make_ep_path(new_id)
+	
+	move(old_path, new_path)
+	
+def make_ep_path(id):
+	"""Construct ep path with id"""
+	return profile("pool/" + safefilepath(id + ".json"))
+	
+def get_ep_path(mission):
+	"""Return episode save file path"""
+	return make_ep_path(get_mission_id(mission))
+	
+episode_loader_cache = {}
+class EpisodeLoader:
+	"""Context manager. Load episode info."""
+	def __init__(self, mission):
+		self.mission = mission
+		
+	def __enter__(self):
+		self.load()
+		
+	def __exit__(self, _type, _value, _traceback):
+		self.unload()
+		
+	def load(self):
+		if self.mission not in episode_loader_cache:
+			episode_loader_cache[self.mission] = 0
+		# self.lock = episode_loader_cache.setdefault(self.mission, 0)
+		if not episode_loader_cache[self.mission]:
+			init_episode(self.mission)
+		episode_loader_cache[self.mission] += 1
+		
+	def unload(self):
+		episode_loader_cache[self.mission] -= 1
+		if not episode_loader_cache[self.mission]:
+			uninit_episode(self.mission)
+			del episode_loader_cache[self.mission]
+
+def init_episode(mission):
+	"""Construct mission.episodes"""
+	if not mission.episodes:
+		eps = load(get_ep_path(mission))
+		if eps:
+			mission.episodes = [Episode(**e) for e in eps]
+
+def uninit_episode(mission):
+	"""Destruct mission.episodes to save memory"""
+	if mission.episodes:
+		file = get_ep_path(mission)
+		dump(mission.episodes, file)
+		mission.episodes = None
+		
+def cleanup_episode(mission):
+	"""Remove episode save file. (probably because the mission is removed from
+	the mission manager)
+	"""
+	remove(get_ep_path(mission))
+		
+def load(file):
+	"""My json.load"""
+	with suppress(OSError):
+		with open(file) as fp:
+			return json.load(fp)
+				
+def dump(data, file):
+	"""My json.dump"""
+	
+	def encoder(object):
+		"""Encode any object to json."""
+		if hasattr(object, "tojson"):
+			return object.tojson()
+		return vars(object)
+		
+	with open(file, "w") as fp:
+		json.dump(
+			data,
+			fp,
+			indent=4,
+			ensure_ascii=False,
+			default=encoder
+		)
+
+class MissionManager:
+	"""Since check_update thread might grab mission from mission_manager, we
+	have to make it thread safe.
+	"""
+	def __init__(self):
+		"""Construct."""
+		self.pool = {}
+		self.view = OrderedDict()
+		self.library = OrderedDict()
+		self.edit = False
+		self.lock = Lock()
+		
+		self.load()
+
+		thread = current()
+		mission_ch.sub(thread)
+		@thread.listen("MISSION_PROPERTY_CHANGED")
+		def _(event):
+			"""Set the edit flag after mission changed."""
+			self.edit = True
+
+	def cleanup(self):
+		"""Cleanup unused missions"""
+		main_pool = set(self.pool)
+		view_pool = set(self.view)
+		library_pool = set(self.library)
+
+		for url in main_pool - (view_pool | library_pool):
+			cleanup_episode(self.pool[url])
+			del self.pool[url]
+
+	def save(self):
+		"""Save missions to json."""
+		if not self.edit:
+			return
+
+		with mission_lock:
+			dump(list(self.pool.values()), profile("pool.json"))
+			dump(list(self.view), profile("view.json"))
+			dump(list(self.library), profile("library.json"))
+			
+		self.edit = False
+		print("Session saved")
+
+	def load(self):
+		"""Load mission from json.
+
+		If failing to load missions, create json backup .
+		"""
+		try:
+			self._load()
+		except Exception:
+			print("Failed to load session!")
+			backup(profile("*.json"))
+			raise
+		self.cleanup()
+
+	def _load(self):
+		"""Load missions from json. Called by MissionManager.load."""
+		pool = load(profile("pool.json")) or []
+		view = load(profile("view.json")) or []
+		library = load(profile("library.json")) or []
+
+		for m_data in pool:
+			# reset state
+			if m_data["state"] in ("DOWNLOADING", "ANALYZING"):
+				m_data["state"] = "ERROR"
+			# build episodes
+			# compatible 2016.6.4
+			if m_data["episodes"]:
+				episodes = []
+				for ep_data in m_data["episodes"]:
+					# compatible 2016.4.3
+					if "total" not in ep_data:
+						if not ep_data["current_url"]:
+							ep_data["total"] = 0
+							
+						elif ep_data["url"] == ep_data["current_url"]:
+							# first page crawler
+							ep_data["total"] = ep_data["current_page"] - 1
+							
+						else:
+							# per page crawler
+							ep_data["total"] = ep_data["current_page"] - 1
+							ep_data["current_page"] = 1
+						
+						if ep_data["complete"]:
+							ep_data["total"] += 1
+							
+					episodes.append(Episode(**ep_data))
+				m_data["episodes"] = episodes
+			mission = MissionProxy(Mission(**m_data))
+			
+			if mission.episodes:
+				uninit_episode(mission)
+				
+			self.pool[mission.url] = mission
+
+		for url in view:
+			self.view[url] = self.pool[url]
+
+		for url in library:
+			self.library[url] = self.pool[url]
+
+		mission_ch.pub("MISSION_LIST_REARRANGED", self.view)
+		mission_ch.pub("MISSION_LIST_REARRANGED", self.library)
+
+	def add(self, pool_name, *missions):
+		"""Add missions to pool."""
+		pool = getattr(self, pool_name)
+
+		with self.lock:
+			for mission in missions:
+				if mission.url not in self.pool:
+					mission_ch.pub("MISSION_ADDED", mission)
+				self.pool[mission.url] = mission					
+				pool[mission.url] = mission
+		
+		mission_ch.pub("MISSION_LIST_REARRANGED", pool)
+		self.edit = True
+
+	def remove(self, pool_name, *missions):
+		"""Remove missions from pool."""
+		pool = getattr(self, pool_name)
+
+		# check mission state
+		missions = [m for m in missions if m.state not in ("ANALYZING", "DOWNLOADING")]
+
+		with self.lock:
+			for mission in missions:
+				del pool[mission.url]
+			self.cleanup()
+			
+		mission_ch.pub("MISSION_LIST_REARRANGED", pool)
+		self.edit = True
+
+	def lift(self, pool_name, *missions):
+		"""Lift missions to the top."""
+		pool = getattr(self, pool_name)
+		with self.lock:
+			for mission in reversed(missions):
+				pool.move_to_end(mission.url, last=False)
+		mission_ch.pub("MISSION_LIST_REARRANGED", pool)
+		self.edit = True
+
+	def drop(self, pool_name, *missions):
+		"""Drop missions to the bottom."""
+		pool = getattr(self, pool_name)
+		with self.lock:
+			for mission in missions:
+				pool.move_to_end(mission.url)
+		mission_ch.pub("MISSION_LIST_REARRANGED", pool)
+		self.edit = True
+
+	def get_by_state(self, pool_name, states):
+		"""Get first mission matching states."""
+		with self.lock:
+			for mission in getattr(self, pool_name).values():
+				if mission.state in states:
+					return mission
+			return None
+			
+	def get_all_by_state(self, pool_name, states):
+		"""Get all missions matching states"""
+		with self.lock:
+			output = []
+			for mission in getattr(self, pool_name).values():
+				if mission.state in states:
+					output.append(mission)
+			return output
+
+	def get_by_url(self, url, pool_name=None):
+		"""Get mission by url."""
+		if not pool_name:
+			return self.pool[url]
+		return getattr(self, pool_name)[url]
+
+mission_manager = MissionManager()
---
+++ packages/comiccrawler/comiccrawler-2018.5.24-py3-none-any.whl/comiccrawler/mods/pixiv.py
@@ -0,0 +1,168 @@
+#! python3
+
+"""this is pixiv module for comiccrawler
+
+Ex:
+	http://www.pixiv.net/member_illust.php?id=2211832
+
+"""
+
+import re
+import json
+from html import unescape
+from io import BytesIO
+from zipfile import ZipFile
+
+from node_vm2 import eval
+
+from ..core import Episode, grabhtml
+from ..error import SkipEpisodeError, PauseDownloadError, is_403
+from ..url import urljoin
+
+domain = ["www.pixiv.net"]
+name = "Pixiv"
+noepfolder = True
+config = {
+	"cookie_PHPSESSID": "請輸入Cookie中的PHPSESSID"
+}
+
+def get_title(html, url):
+	if ("js-mount-point-search-result-list" not in html and
+		"illust_id=" not in url):
+		try:
+			user = unescape(re.search("class=\"user-name\"[^>]*>([^<]+)", html).group(1))
+			id = re.search(r"pixiv.context.userId = \"(\d+)\"", html).group(1)
+			return "{} - {}".format(id, user)
+		except AttributeError:
+			pass
+	return "[pixiv] " + unescape(re.search("<title>([^<]+)", html).group(1))
+
+def get_episodes(html, url):
+	if "pixiv.user.loggedIn = true" not in html:
+		raise PauseDownloadError("you didn't login!")
+	s = []
+	for m in re.finditer(r'<a href="([^"]+)"><h1 class="title" title="([^"]+)">', html):
+		ep_url, title = m.groups()
+		uid = re.search("id=(\d+)", ep_url).group(1)
+		e = Episode("{} - {}".format(uid, unescape(title)), urljoin(url, ep_url))
+		s.append(e)
+	# search result?
+	match = re.search('id="js-mount-point-search-result-list"data-items="([^"]+)', html)
+	if match:
+		data = unescape(match.group(1))
+		for illust in json.loads(data):
+			s.append(Episode(
+				"{illustId} - {illustTitle}".format_map(illust),
+				urljoin(url, "/member_illust.php?mode=medium&illust_id={illustId}".format_map(illust))
+			))
+			
+	# single image
+	match = re.search(r'pixiv\.context\.illustId\s*=\s*"(\d+)', html)
+	if match:
+		s.append(Episode("image", url))
+		
+	return s[::-1]
+	
+cache = {}
+
+def get_images_old(html, url):
+	match = re.search(r'"works_display"><a (?:class="[^"]*" )?href="([^"]+)"', html)
+	
+	if not match:
+		return
+		
+	inner_url = match.group(1)
+	html = grabhtml(urljoin(url, inner_url), referer=url)
+
+	if "mode=big" in inner_url:
+		# single image
+		img = re.search(r'src="([^"]+)"', html).group(1)
+		return [img]
+
+	if "mode=manga" in inner_url:
+		# multiple image
+		imgs = []
+		
+		def create_grabber(url):
+			def grabber():
+				html = grabhtml(url)
+				return re.search(r'img src="([^"]+)"', html).group(1)
+			return grabber
+
+		for match in re.finditer(r'a href="(/member_illust\.php\?mode=manga_big[^"]+)"', html):
+			imgs.append(create_grabber(urljoin(url, match.group(1))))
+
+		# New manga reader (2015/3/18)
+		# http://www.pixiv.net/member_illust.php?mode=manga&illust_id=19254298
+		if not imgs:
+			for match in re.finditer(r'originalImages\[\d+\] = ("[^"]+")', html):
+				img = eval(match.group(1))
+				imgs.append(img)
+
+		return imgs
+
+def get_images(html, url):
+	if "pixiv.user.loggedIn = true" not in html:
+		raise PauseDownloadError("you didn't login!")
+
+	# ugoku
+	rs = re.search(r"pixiv\.context\.ugokuIllustFullscreenData\s+= ([^;]+)", html)
+	if rs:
+		json = rs.group(1)
+		o = eval("(" + json + ")")
+		cache["frames"] = o["frames"]
+		return [o["src"]]
+
+	# new image layout (2014/12/14)
+	rs = re.search(r'class="big" data-src="([^"]+)"', html)
+	if rs:
+		return [rs.group(1)]
+
+	rs = re.search(r'data-src="([^"]+)" class="original-image"', html)
+	if rs:
+		return [rs.group(1)]
+
+	# old image layout
+	imgs = get_images_old(html, url)
+	if imgs:
+		return imgs
+	
+	# restricted
+	rs = re.search('<section class="restricted-content">', html)
+	if rs:
+		raise SkipEpisodeError
+
+	# error page
+	rs = re.search('class="error"', html)
+	if rs:
+		raise SkipEpisodeError
+
+	# id doesn't exist
+	rs = re.search("pixiv.context.illustId", html)
+	if not rs:
+		raise SkipEpisodeError
+
+def errorhandler(er, crawler):
+	# http://i1.pixiv.net/img21/img/raven1109/10841650_big_p0.jpg
+	# Private page?
+	if is_403(er):
+		raise SkipEpisodeError
+			
+def imagehandler(ext, bin):
+	"""Append index info to ugoku zip"""
+	if ext == ".zip":
+		bin = pack_ugoira(bin, cache["frames"])
+		ext = ".ugoira"
+	return ext, bin
+	
+def pack_ugoira(bin, frames):
+	with BytesIO(bin) as imbin:
+		with ZipFile(imbin, "a") as zip:
+			data = json.dumps({"frames": frames}, separators=(',', ':'))
+			zip.writestr("animation.json", data.encode("utf-8"))
+		return imbin.getvalue()
+
+def get_next_page(html, url):
+	match = re.search("href=\"([^\"]+)\" rel=\"next\"", html)
+	if match:
+		return urljoin(url, unescape(match.group(1)))
