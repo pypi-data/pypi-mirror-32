644124baeb0b69c08f6d46b5857d3e218f248991
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:51 +0000

    Add package zorb/0.1.4/zorb-0.1.4.tar.gz

---
+++ packages/zorb/zorb-0.1.4.tar.gz/zorb-0.1.4/setup.py
@@ -0,0 +1,25 @@
+from setuptools import Command
+from setuptools import find_packages
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="zorb",
+    version="0.1.4",
+    author="Somatic Labs",
+    author_email="developers@somaticlabs.io",
+    description="Python SDK for the Zorb Engine",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/SomaticLabs/ZorbPy",
+    packages=find_packages(),
+    classifiers=(
+        "Programming Language :: Python :: 2.7",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: MacOS",
+        "Operating System :: POSIX :: Linux",
+        "Topic :: Scientific/Engineering :: Human Machine Interfaces",
+    ),
+)
