b383e580c8b24019e8f37cf638be677c357a93b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:08 +0000

    Add package EventProcces/1.5/EventProcces-1.5-py3-none-any.whl

---
+++ packages/EventProcces/EventProcces-1.5-py3-none-any.whl/event/EventClass.py
@@ -0,0 +1,9 @@
+from datetime import datetime
+class Event:
+    id = 0
+    name = ""
+    description = ""
+    startDate = datetime(1, 2, 3)
+    periodType = ""
+    periodFrequency = ""
+    isFinished = 0
