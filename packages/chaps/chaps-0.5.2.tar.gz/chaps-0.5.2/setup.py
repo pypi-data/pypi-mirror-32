76a5fe78c6f18925ec09b50a3a4bd6d301b959bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:59 +0000

    Add package chaps/0.5.2/chaps-0.5.2.tar.gz

---
+++ packages/chaps/chaps-0.5.2.tar.gz/chaps-0.5.2/setup.py
@@ -0,0 +1,21 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='chaps',
+    version='0.5.2',
+    packages=find_packages(),
+    url='https://github.com/ekiro/chaps',
+    license='MIT License',
+    author='Piotr Karkut',
+    author_email='karkucik@gmail.com',
+    description='Simple DI Library',
+    platforms='any',
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.6",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development",
+        "Intended Audience :: Developers",
+        "Development Status :: 4 - Beta"]
+)
