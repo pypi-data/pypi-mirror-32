a3d52f44eb8bd559229a944c1f8a9eb6d5c47b24
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:13 +0000

    Add package gsum/0.1/gsum-0.1.tar.gz

---
+++ packages/gsum/gsum-0.1.tar.gz/gsum-0.1/gsum/__init__.py
@@ -0,0 +1,14 @@
+from .models import SGP
+from .models import PowerSeries
+from .models import PowerProcess
+
+from .helpers import cartesian
+from .helpers import toy_data
+from .helpers import generate_coefficients
+from .helpers import coefficients
+from .helpers import partials
+from .helpers import predictions
+from .helpers import stabilize
+from .helpers import gaussian
+from .helpers import HPD
+from .helpers import HPD_pdf
---
+++ packages/gsum/gsum-0.1.tar.gz/gsum-0.1/setup.py
@@ -0,0 +1,26 @@
+from distutils.core import setup
+
+setup(
+    name='gsum',
+    packages=['gsum'],
+    # py_modules=[''],
+    version='0.1',
+    description='A Bayesian model of series convergence using Gaussian sums',
+    author='Jordan Melendez',
+    author_email='jmelendez1992@gmail.com',
+    license='MIT',
+    url='https://github.com/jordan-melendez/gsum.git',
+    download_url='',
+    keywords='EFT nuclear model gaussian process uncertainty quantification buqeyemodel buqeye',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Intended Audience :: Science/Research',
+        'Topic :: Scientific/Engineering',
+        'Topic :: Scientific/Engineering :: Mathematics',
+        'Topic :: Scientific/Engineering :: Physics'
+        ]
+)
