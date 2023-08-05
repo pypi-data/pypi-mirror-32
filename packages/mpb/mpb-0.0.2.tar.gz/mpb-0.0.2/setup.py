0e2963467a75de049bf1af932bde1e738273453e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:49 +0000

    Add package mpb/0.0.2/mpb-0.0.2.tar.gz

---
+++ packages/mpb/mpb-0.0.2.tar.gz/mpb-0.0.2/setup.py
@@ -0,0 +1,15 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="mpb",
+    version="0.0.2",
+    author = 'Abin Simon',
+    author_email = 'abinsimon10@gmail.com',
+    description="A prettier progressbar library",
+    url="https://github.com/meain/mpb",
+    packages=setuptools.find_packages(),
+    keywords = ['progressbar', 'spinner', 'loader'],
+)
