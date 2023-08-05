2ec255c2a47a9b3666b0fe2ab4b8690e852017ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:06 +0000

    Add package EventProcces/1.2/EventProcces-1.2-py3-none-any.whl

---
+++ packages/EventProcces/EventProcces-1.2-py3-none-any.whl/functions/DisplayFunctions.py
@@ -0,0 +1,6 @@
+def displayMenu():
+    print("1. Dodaj nowe wydarzenie")
+    print("2. Pokaż zaplanowane wydarzenia")
+    print("3. Pokaż wydarzenia, które się już odbyły")
+    print("4. Usuń wydarzenie")
+    print("5. Edytuj wydarzenie")
