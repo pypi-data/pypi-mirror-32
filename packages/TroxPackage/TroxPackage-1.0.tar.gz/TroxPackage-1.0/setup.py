c0e1c6ebfd5f4da067e6c666c84f82fa9bd6b8cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:11 +0000

    Add package TroxPackage/1.0/TroxPackage-1.0.tar.gz

---
+++ packages/TroxPackage/TroxPackage-1.0.tar.gz/TroxPackage-1.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='TroxPackage',
+    version='1.0',
+    license='MIT',
+    description='Trox python package',
+    url='https://gitlab.com/ja_Quben/TroxPackage.git',
+    author='Jakub Zep',
+    author_email='myemail@example.com',
+    packages=find_packages(),
+)
