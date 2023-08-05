0c6c0a3931e605a11184c5d73eaef069f402f22c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:09:06 +0000

    Add package JSPy/0.5/JSPy-0.5.tar.gz

---
+++ packages/JSPy/JSPy-0.5.tar.gz/JSPy-0.5/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+setup(name='JSPy',
+      description='Python Library that runs Javascript',
+      version='0.5',
+      url='https://github.com/rithik/JSPy',
+      author='Rithik Yelisetty',
+      author_email='rithik@gmail.com',
+      license='MIT',
+      classifiers=[
+          'License :: OSI Approved :: MIT License',
+          'Programming Language :: Python :: 3'
+      ],
+      packages=['JSPy'],
+      install_requires=[
+      ],
+      entry_points={
+          'console_scripts': [
+              'JSPy=JSPy.main:run'
+          ]
+      }
+)
