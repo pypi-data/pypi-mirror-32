56bc9ec219634ee0826bf876667b203efccacad5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:32:59 +0000

    Add package pmxbot.webhooks/1.2.1/pmxbot.webhooks-1.2.1.tar.gz

---
+++ packages/pmxbot.webhooks/pmxbot.webhooks-1.2.1.tar.gz/pmxbot.webhooks-1.2.1/setup.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python
+
+# Project skeleton maintained at https://github.com/jaraco/skeleton
+
+import io
+
+import setuptools
+
+with io.open('README.rst', encoding='utf-8') as readme:
+	long_description = readme.read()
+
+name = 'pmxbot.webhooks'
+description = 'An HTTP-based webhook service for pmxbot'
+nspkg_technique = 'managed'
+"""
+Does this package use "native" namespace packages or
+pkg_resources "managed" namespace packages?
+"""
+
+params = dict(
+	name=name,
+	use_scm_version=True,
+	author="Jason R. Coombs",
+	author_email="jaraco@jaraco.com",
+	description=description or name,
+	long_description=long_description,
+	url="https://github.com/jaraco/" + name,
+	packages=setuptools.find_packages(exclude='tests'),
+	include_package_data=True,
+	namespace_packages=(
+		name.split('.')[:-1] if nspkg_technique == 'managed'
+		else []
+	),
+	python_requires='>=3.5',
+	install_requires=[
+		'twilio>=6',
+		'cherrypy_cors',
+		'jaraco.itertools',
+	],
+	extras_require={
+		'testing': [
+			# upstream
+			'pytest>=3.5',
+			'pytest-sugar>=0.9.1',
+			'collective.checkdocs',
+			'pytest-flake8',
+
+			# local
+			'pmxbot',
+			'cherrypy[testing]',
+		],
+		'docs': [
+			# upstream
+			'sphinx',
+			'jaraco.packaging>=3.2',
+			'rst.linker>=1.9',
+
+			# local
+		],
+	},
+	setup_requires=[
+		'setuptools_scm>=1.15.0',
+	],
+	classifiers=[
+		"Development Status :: 5 - Production/Stable",
+		"Intended Audience :: Developers",
+		"License :: OSI Approved :: MIT License",
+		"Programming Language :: Python :: 3",
+	],
+	entry_points={
+		'pmxbot_handlers': [
+			'webhooks = pmxbot.webhooks',
+		],
+	},
+)
+if __name__ == '__main__':
+	setuptools.setup(**params)
