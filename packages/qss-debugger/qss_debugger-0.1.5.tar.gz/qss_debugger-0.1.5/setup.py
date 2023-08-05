b20bc8980eafd4f0380682e4dde20b0e1fa9ff5d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:41 +0000

    Add package qss-debugger/0.1.5/qss_debugger-0.1.5.tar.gz

---
+++ packages/qss-debugger/qss_debugger-0.1.5.tar.gz/qss_debugger-0.1.5/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='qss_debugger',
+    version='0.1.5',
+    packages=['qss_debugger'],
+    install_requires=['future', 'Qt.py'],
+    url='https://github.com/rubenhenares/qss_debugger',
+    license='Apache License 2.0',
+    author='Ruben Henares',
+    author_email='ruben@404fs.com',
+    description='Improves the process of styling Qt applications with qss.',
+    keywords=['qt', 'styling', 'qss']
+)
+
