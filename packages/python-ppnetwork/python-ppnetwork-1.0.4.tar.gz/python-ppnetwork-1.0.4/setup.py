806c4292166231cfbe20b15de71310e261443d26
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:20 +0000

    Add package python-ppnetwork/1.0.4/python-ppnetwork-1.0.4.tar.gz

---
+++ packages/python-ppnetwork/python-ppnetwork-1.0.4.tar.gz/python-ppnetwork-1.0.4/setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# coding=utf-8
+
+from setuptools import setup
+
+setup(name='python-ppnetwork',
+    py_modules = ["pp_link","pp_flow","pp_control"],
+    author='gonewind.he',
+      author_email='gonewind.he@gmail.com',
+      maintainer='gonewind',
+      maintainer_email='gonewind.he@gmail.com',
+      url='https://github.com/gonewind73/ppnetwork',
+      description='Linux/Windows  ppnetwork driver  written in  python',
+      long_description=open('README.rst',encoding="utf-8").read(),
+#       long_description_content_type="text/markdown",
+      version='1.0.4',
+      python_requires='>=3',
+      platforms=["Linux","Windows"],
+      classifiers=[
+          'Development Status :: 5 - Production/Stable',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: MIT License',
+          'Programming Language :: Python :: 3',
+          'Topic :: Software Development :: Libraries :: Python Modules',
+          'Topic :: System :: Networking'])
