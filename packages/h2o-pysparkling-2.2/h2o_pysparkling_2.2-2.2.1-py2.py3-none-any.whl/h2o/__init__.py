af434f40365fdc59de4ee687c7743e54485b275b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:25:59 +0000

    Add package h2o-pysparkling-2.2/2.2.1/h2o_pysparkling_2.2-2.2.1-py2.py3-none-any.whl

---
+++ packages/h2o-pysparkling-2.2/h2o_pysparkling_2.2-2.2.1-py2.py3-none-any.whl/h2o/__init__.py
@@ -0,0 +1,39 @@
+# -*- encoding: utf-8 -*-
+# Copyright: (c) 2016 H2O.ai
+# License:   Apache License Version 2.0 (see LICENSE for details)
+"""
+:mod:`h2o` -- module for using H2O services.
+
+(please add description).
+"""
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+from h2o.h2o import (connect, init, api, connection,
+                     lazy_import, upload_file, import_file, import_sql_table, import_sql_select,
+                     parse_setup, parse_raw, assign, deep_copy, get_model, get_grid, get_frame,
+                     show_progress, no_progress, enable_expr_optimizations, is_expr_optimizations_enabled,
+                     log_and_echo, remove, remove_all, rapids,
+                     ls, frame, frames, create_frame,
+                     download_pojo, download_csv, download_all_logs, save_model, load_model, export_file,
+                     cluster_status, cluster_info, shutdown, network_test, cluster,
+                     interaction, as_list,
+                     get_timezone, set_timezone, list_timezones,
+                     load_dataset, demo, make_metrics,flow)
+# We have substantial amount of code relying on h2o.H2OFrame to exist. Thus, we make this class available from
+# root h2o module, without exporting it explicitly. In the future this import may be removed entirely, so that
+# one would have to import it from h2o.frames.
+from h2o.frame import H2OFrame  # NOQA
+
+__version__ = "3.14.0.6"
+__buildinfo__ = "versionFromGradle='3.14.0',projectVersion='3.14.0.6',branch='rel-weierstrass',lastCommitHash='c739ff1c07c5bfb6b0c4f307852dbc3f6cf5ef75',gitDescribe='jenkins-3.14.0.5-2-gc739ff1c0',compiledOn='2017-10-09 20:23:09',compiledBy='jenkins'"
+
+if (__version__.endswith("99999")):
+    print(__buildinfo__)
+
+__all__ = ("connect", "init", "api", "connection", "upload_file", "lazy_import", "import_file", "import_sql_table",
+           "import_sql_select", "parse_setup", "parse_raw", "assign", "deep_copy", "get_model", "get_grid", "get_frame",
+           "show_progress", "no_progress", "enable_expr_optimizations", "is_expr_optimizations_enabled", "log_and_echo",
+           "remove", "remove_all", "rapids", "ls", "frame",
+           "frames", "download_pojo", "download_csv", "download_all_logs", "save_model", "load_model", "export_file",
+           "cluster_status", "cluster_info", "shutdown", "create_frame", "interaction", "as_list", "network_test",
+           "set_timezone", "get_timezone", "list_timezones", "demo", "make_metrics", "cluster", "load_dataset","flow")
