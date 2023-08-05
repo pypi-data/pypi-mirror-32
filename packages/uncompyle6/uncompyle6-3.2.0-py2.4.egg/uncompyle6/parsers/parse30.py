9f9a35c70c1843315bccf4cca63d09ed23776d25
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:37 +0000

    Add package uncompyle6/3.2.0/uncompyle6-3.2.0-py2.4.egg

---
+++ packages/uncompyle6/uncompyle6-3.2.0-py2.4.egg/uncompyle6/parsers/parse30.py
@@ -0,0 +1,49 @@
+#  Copyright (c) 2016-2017 Rocky Bernstein
+"""
+spark grammar differences over Python 3.1 for Python 3.0.
+"""
+
+from uncompyle6.parser import PythonParserSingle
+from uncompyle6.parsers.parse31 import Python31Parser
+
+class Python30Parser(Python31Parser):
+
+    def p_30(self, args):
+        """
+
+        # FIXME: combine with parse3.2
+        whileTruestmt     ::= SETUP_LOOP l_stmts_opt JUMP_BACK
+                              COME_FROM_LOOP
+        whileTruestmt     ::= SETUP_LOOP returns
+                              COME_FROM_LOOP
+
+        # In many ways Python 3.0 code generation is more like Python 2.6 than
+        # it is 2.7 or 3.1. So we have a number of 2.6ish (and before) rules below
+
+        _ifstmts_jump  ::= c_stmts_opt JUMP_FORWARD _come_froms POP_TOP COME_FROM
+        jmp_true       ::= JUMP_IF_TRUE POP_TOP
+        jmp_false      ::= JUMP_IF_FALSE POP_TOP
+
+        # Used to keep index order the same in semantic actions
+        jb_pop_top     ::= JUMP_BACK POP_TOP
+
+        while1stmt ::= SETUP_LOOP l_stmts COME_FROM_LOOP
+
+        else_suitel ::= l_stmts COME_FROM_LOOP JUMP_BACK
+
+        ifelsestmtl ::= testexpr c_stmts_opt jb_pop_top else_suitel
+
+        withasstmt    ::= expr setupwithas store suite_stmts_opt
+                          POP_BLOCK LOAD_CONST COME_FROM_FINALLY
+                          LOAD_FAST DELETE_FAST WITH_CLEANUP END_FINALLY
+        setupwithas   ::= DUP_TOP LOAD_ATTR STORE_FAST LOAD_ATTR CALL_FUNCTION_0 setup_finally
+        setup_finally ::= STORE_FAST SETUP_FINALLY LOAD_FAST DELETE_FAST
+        """
+
+    def customize_grammar_rules(self, tokens, customize):
+        super(Python30Parser, self).customize_grammar_rules(tokens, customize)
+        return
+    pass
+
+class Python30ParserSingle(Python30Parser, PythonParserSingle):
+    pass
