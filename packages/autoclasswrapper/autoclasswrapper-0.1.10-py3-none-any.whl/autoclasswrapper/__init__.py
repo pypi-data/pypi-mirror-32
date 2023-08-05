4b1406612762bac0d49d69ec83a278a0d36232d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:27 +0000

    Add package autoclasswrapper/0.1.10/autoclasswrapper-0.1.10-py3-none-any.whl

---
+++ packages/autoclasswrapper/autoclasswrapper-0.1.10-py3-none-any.whl/autoclasswrapper/__init__.py
@@ -0,0 +1,16 @@
+__version__ = "0.1.10"
+__copyright__ = "Copyright 2018 Pierre Poulain"
+
+import logging
+log = logging.getLogger(__name__)
+log_handler = logging.StreamHandler()
+log_formatter = logging.Formatter("%(asctime)s %(levelname)-8s %(message)s",
+                                  "%Y-%m-%d %H:%M:%S")
+
+log_handler.setFormatter(log_formatter)
+log.addHandler(log_handler)
+log.setLevel(logging.DEBUG)
+
+from .input import Input, Dataset, DuplicateColumnNameError
+from .output import Output
+from .run import search_autoclass_in_path, Run
