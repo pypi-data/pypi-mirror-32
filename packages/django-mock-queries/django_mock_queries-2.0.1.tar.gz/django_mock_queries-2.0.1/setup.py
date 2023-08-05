4a51bde16735eb9b3d01e9bd8f1725edb93b9fef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:23 +0000

    Add package django-mock-queries/2.0.1/django_mock_queries-2.0.1.tar.gz

---
+++ packages/django-mock-queries/django_mock_queries-2.0.1.tar.gz/django_mock_queries-2.0.1/setup.py
@@ -0,0 +1,32 @@
+from distutils.core import setup
+try:
+    from pip._internal.req import parse_requirements
+except ImportError:
+    from pip.req import parse_requirements
+
+
+install_req = parse_requirements('requirements/core.txt', session='skip')
+req = [str(ir.req) for ir in install_req]
+
+
+def read_md(filename):
+    try:
+        from pypandoc import convert_file
+        return convert_file(filename, 'rst')
+    except (ImportError, OSError):
+        return open(filename).read()
+
+
+setup(
+    name='django_mock_queries',
+    packages=['django_mock_queries'],
+    version='2.0.1',
+    description='A django library for mocking queryset functions in memory for testing',
+    long_description=read_md('README.md'),
+    author='Phivos Stylianides',
+    author_email='stphivos@gmail.com',
+    url='https://github.com/stphivos/django-mock-queries',
+    keywords='django orm mocking unit-testing tdd',
+    classifiers=[],
+    install_requires=req
+)
