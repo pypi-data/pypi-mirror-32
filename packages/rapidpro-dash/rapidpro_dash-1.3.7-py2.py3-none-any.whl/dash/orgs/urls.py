497a4873da8ac4ba49567dab03cbbf7e796a5552
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:10:19 +0000

    Add package rapidpro-dash/1.3.7/rapidpro_dash-1.3.7-py2.py3-none-any.whl

---
+++ packages/rapidpro-dash/rapidpro_dash-1.3.7-py2.py3-none-any.whl/dash/orgs/urls.py
@@ -0,0 +1,9 @@
+from __future__ import unicode_literals
+
+from . import views
+
+
+urlpatterns = views.OrgCRUDL().as_urlpatterns()
+urlpatterns += views.OrgBackgroundCRUDL().as_urlpatterns()
+urlpatterns += views.TaskCRUDL().as_urlpatterns()
+urlpatterns += views.OrgBackendCRUDL().as_urlpatterns()
