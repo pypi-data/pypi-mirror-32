8637f2bd3b23ae9928e435ab73cc61d78996df1b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:16 +0000

    Add package dw-yahoo-earnings-calendar/0.0.2/dw_yahoo_earnings_calendar-0.0.2.tar.gz

---
+++ packages/dw-yahoo-earnings-calendar/dw_yahoo_earnings_calendar-0.0.2.tar.gz/dw_yahoo_earnings_calendar-0.0.2/setup.py
@@ -0,0 +1,18 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+setup(
+    name='dw_yahoo_earnings_calendar',
+    packages=['yahoo_earnings_calendar'],
+    install_requires=[
+        'requests'
+    ],
+    version='0.0.2',
+    description='Scrapes data from Yahoo! Finance earnings calendar',
+    author='Daniel Wang',
+    author_email='danielwpz@gmail.com',
+    keywords=['stock', 'earnings', 'yahoo', 'scrape', 'finance'],
+    classifiers=[],
+)
