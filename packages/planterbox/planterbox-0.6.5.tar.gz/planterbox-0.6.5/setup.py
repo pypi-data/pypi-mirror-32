e0a7cb3db348eb48d5db1d40866bee713317746a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:52 +0000

    Add package planterbox/0.6.5/planterbox-0.6.5.tar.gz

---
+++ packages/planterbox/planterbox-0.6.5.tar.gz/planterbox-0.6.5/setup.py
@@ -0,0 +1,33 @@
+import os
+
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+requires = [
+    "nose2>=0.6.0",
+    "mock",
+    "six",
+]
+
+description = 'A plugin for nose2 implementing behavior-driven testing.'
+
+setup(name='planterbox',
+      version='0.6.5',
+      description=description,
+      license='MIT',
+      classifiers=[
+          "Intended Audience :: Developers",
+          'Topic :: Software Development :: Testing',
+          "Programming Language :: Python",
+      ],
+      author='Nick Pilon',
+      author_email='npilon@gmail.com',
+      url='https://github.com/npilon/planterbox',
+      keywords='testing test bdd lettuce cucumber gherkin nosetests nose2',
+      packages=find_packages(),
+      include_package_data=True,
+      zip_safe=True,
+      test_suite='planterbox',
+      install_requires=requires,
+      )
