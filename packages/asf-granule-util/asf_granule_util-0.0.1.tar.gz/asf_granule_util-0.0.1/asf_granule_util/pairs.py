936e017e8c3f20d2a54fa777612d76470aa5d875
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:24 +0000

    Add package asf-granule-util/0.0.1/asf_granule_util-0.0.1.tar.gz

---
+++ packages/asf-granule-util/asf_granule_util-0.0.1.tar.gz/asf_granule_util-0.0.1/asf_granule_util/pairs.py
@@ -0,0 +1,36 @@
+class GranulePair(object):
+    def __init__(self, g1, g2):
+        self.master = g1
+        self.slave = g2
+
+    def to_obj(self):
+        return [self.master.to_obj(), self.slave.to_obj()]
+
+    def __str__(self):
+        return "GranulePair(" + str(self.to_obj()) + ")"
+
+    def __repr__(self):
+        return self.__str__()
+
+
+class SentinelGranulePair(GranulePair):
+    def get_dates(self):
+        return self.master.start_date + "_" + self.slave.start_date
+
+    def checksum_str(pair):
+        return "{}-{}".format(pair.master.unique_id, pair.slave.unique_id)
+
+
+def get_pairs(granule_list, gap=1, pair_type=GranulePair):
+    """pairs up granule, takes a list of granules sorted by time"""
+    granule_list.sort(key=lambda g: g.start_date)
+    pair_list = []
+
+    for i, granule in enumerate(granule_list):
+        try:
+            pair = pair_type(granule, granule_list[i + gap])
+            pair_list.append(pair)
+        except:
+            break
+
+    return pair_list
