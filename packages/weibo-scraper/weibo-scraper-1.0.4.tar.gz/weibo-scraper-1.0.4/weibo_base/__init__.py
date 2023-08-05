506c4cc78864b6b1a3e245fe09448cd9d90537bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:04 +0000

    Add package weibo-scraper/1.0.4/weibo-scraper-1.0.4.tar.gz

---
+++ packages/weibo-scraper/weibo-scraper-1.0.4.tar.gz/weibo-scraper-1.0.4/weibo_base/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding:utf-8 -*-
+
+"""
+ Author: Helixcs
+ Site: https://iliangqunru.bitcron.com/
+ File: __init__.py.py
+ Time: 5/19/18
+"""
+from .weibo_api import exist_get_uid, get_weibo_containerid, weibo_tweets
