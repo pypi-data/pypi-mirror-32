3056d3b9454ac7caf0c2af57b59ce6ab447713d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:03 +0000

    Add package illumon-jpy/1.20180430.23/illumon_jpy-1.20180430.23-cp27-cp27m-win_amd64.whl

---
+++ packages/illumon-jpy/illumon_jpy-1.20180430.23-cp27-cp27m-win_amd64.whl/jpyconfig.py
@@ -0,0 +1,8 @@
+# Created by 'jpyutil.py' tool on 2018-05-18 15:58:16.937000
+# This file is read by the 'jpyutil' module in order to load and configure the JVM from Python
+java_home = 'C:\\Program Files\\Java\\jdk1.8.0_162'
+jvm_dll = 'C:\\Program Files\\Java\\jdk1.8.0_162\\jre\\bin\\server\\jvm.dll'
+jvm_maxmem = None
+jvm_classpath = []
+jvm_properties = {}
+jvm_options = []
