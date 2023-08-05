6392306caa463365bd4ab8700ef008aa0ddf59c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:51 +0000

    Add package email-scraper-from-html/1.1/email_scraper_from_html-1.1.tar.gz

---
+++ packages/email-scraper-from-html/email_scraper_from_html-1.1.tar.gz/email_scraper_from_html-1.1/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(name='email_scraper_from_html',
+      version='1.1',
+      description='Generates list of emails scraped from a given HTML file',
+      url='https://github.com/jonjonrankin/email_scraper_from_html',
+      author='jonjonrankin',
+      author_email='jonathan@skoller.co',
+      license='MIT',
+      packages=['email_scraper_from_html'],
+      zip_safe=False,
+      install_requires=[
+            'bs4',
+            'pandas',
+            'argparse',
+            'lxml'
+      ],
+      python_requires='>=3',
+      scripts=['bin/scrape'])
