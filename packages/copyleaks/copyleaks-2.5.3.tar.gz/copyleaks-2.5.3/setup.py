680adc8cb44b0bfb87d3303327c2fe9ed10f9c60
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:55 +0000

    Add package copyleaks/2.5.3/copyleaks-2.5.3.tar.gz

---
+++ packages/copyleaks/copyleaks-2.5.3.tar.gz/copyleaks-2.5.3/setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+setup(
+    name = 'copyleaks',
+    packages = ['copyleaks'],
+    version = '2.5.3',
+    description = 'Copyleaks API gives you access to a variety of plagiarism detection technologies to protect your online content. Get the most comprehensive plagiarism report for your content that is easy to use and integrate.',
+    author = 'Copyleaks ltd',
+    author_email = 'sales@copyleaks.com',
+    url = 'https://api.copyleaks.com',
+    download_url = 'https://github.com/Copyleaks/Python-Plagiarism-Checker', 
+    keywords = ['copyleaks', 'api', 'plagiarism', 'content', 'checker', 'online', 'academic', 'publishers', 'websites'],
+    install_requires=[
+        'python-dateutil', 'enum34', 'requests'
+    ],
+    classifiers = [],
+)
