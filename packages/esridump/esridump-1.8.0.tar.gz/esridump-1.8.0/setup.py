ac80a2b7664af1960ceb40a616c7eddcf7453bbc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:09:51 +0000

    Add package esridump/1.8.0/esridump-1.8.0.tar.gz

---
+++ packages/esridump/esridump-1.8.0.tar.gz/esridump-1.8.0/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+
+with open('README.md') as f:
+    readme = f.read()
+
+with open('LICENSE') as f:
+    license = f.read()
+
+setup(
+    name='esridump',
+    version='1.8.0',
+    description='Dump geodata from ESRI endpoints to GeoJSON',
+    long_description=readme,
+    author='Ian Dees',
+    author_email='ian.dees@gmail.com',
+    url='https://github.com/openaddresses/pyesridump',
+    license=license,
+    packages=find_packages(exclude=('tests', 'docs')),
+    install_requires=[
+        'requests',
+        'simplejson',
+        'six',
+    ],
+    entry_points={
+        'console_scripts': ['esri2geojson=esridump.cli:main'],
+    }
+)
