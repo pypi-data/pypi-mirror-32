24ef5be03b05b2ed0f81c5130322de030bfcace1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:45 +0000

    Add package test-name/1.0.0/test_name-1.0.0.tar.gz

---
+++ packages/test-name/test_name-1.0.0.tar.gz/test_name-1.0.0/test.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+	name		='test_name',
+	version		='1.0.0',
+	py_modules	=['nester'],
+	author		='Benjamin',
+	author_email	='wenzhetan@foxmail.com',
+	url		='http://www.headfirstlabs.com',
+	description	='A simple printer of nested lists',
+)
