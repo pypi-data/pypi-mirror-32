a13018ebd9eb1720a322702ff215995532dd5ce2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:28:45 +0000

    Add package saltdash/0.9.5/saltdash-0.9.5-py2.py3-none-any.whl

---
+++ packages/saltdash/saltdash-0.9.5-py2.py3-none-any.whl/saltdash/urls.py
@@ -0,0 +1,12 @@
+from django.conf import settings
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path(
+        "{}/".format(settings.SOCIAL_AUTH_URL_PREFIX),
+        include("social_django.urls", namespace="social"),
+    ),
+    path("", include("saltdash.dash.urls")),
+]
