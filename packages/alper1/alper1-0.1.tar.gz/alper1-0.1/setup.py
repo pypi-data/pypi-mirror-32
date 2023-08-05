6165273d9edbb1c9be1e15333e9713f8fd2f6bbf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:50 +0000

    Add package alper1/0.1/alper1-0.1.tar.gz

---
+++ packages/alper1/alper1-0.1.tar.gz/alper1-0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="alper1",
+    version=0.1,
+    description='My first package',
+    author='Alper Toygar',
+    author_email='alpertoygar@yandex.com',
+    zip_safe=False
+)
