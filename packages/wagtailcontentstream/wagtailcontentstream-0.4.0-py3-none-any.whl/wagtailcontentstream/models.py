a3582ff515ea209f83e6832f90f3dfb117fe2c82
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:32:08 +0000

    Add package wagtailcontentstream/0.4.0/wagtailcontentstream-0.4.0-py3-none-any.whl

---
+++ packages/wagtailcontentstream/wagtailcontentstream-0.4.0-py3-none-any.whl/wagtailcontentstream/models.py
@@ -0,0 +1,53 @@
+# Wagtail 2.0 compatibility - new package paths
+try:
+    from wagtail.admin.edit_handlers import StreamFieldPanel
+    from wagtail.core.fields import StreamField
+    from wagtail.core.models import Page
+except ImportError:
+    from wagtail.wagtailadmin.edit_handlers import StreamFieldPanel
+    from wagtail.wagtailcore.fields import StreamField
+    from wagtail.wagtailcore.models import Page
+
+from .blocks import ContentStreamBlock, ContentStreamBlockWithRawCode, SectionBlock
+
+
+class ContentStreamPage(Page):
+    body = StreamField(
+        ContentStreamBlock(),
+        blank=True,
+    )
+
+    content_panels = Page.content_panels + [
+        StreamFieldPanel('body'),
+    ]
+
+    class Meta:
+        abstract = True
+
+
+class ContentStreamPageWithRawCode(Page):
+    body = StreamField(
+        ContentStreamBlockWithRawCode(),
+        blank=True,
+    )
+
+    content_panels = Page.content_panels + [
+        StreamFieldPanel('body'),
+    ]
+
+    class Meta:
+        abstract = True
+
+
+class SectionContentStreamPage(Page):
+    body = StreamField(
+        SectionBlock(),
+        blank=True,
+    )
+
+    content_panels = Page.content_panels + [
+        StreamFieldPanel('body'),
+    ]
+
+    class Meta:
+        abstract = True
