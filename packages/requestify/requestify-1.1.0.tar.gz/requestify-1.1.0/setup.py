0b56c1a399e27753716479425b81757a798f4768
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:28:48 +0000

    Add package requestify/1.1.0/requestify-1.1.0.tar.gz

---
+++ packages/requestify/requestify-1.1.0.tar.gz/requestify-1.1.0/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(name='requestify',
+      version='1.1.0',
+      description='convert curl to python requests code',
+      author='Yue Minatsuki',
+      author_email='yue.official.jp@gmail.com',
+
+      classifiers=[
+          'Development Status :: 4 - Beta',
+          'Intended Audience :: Developers',
+          'Programming Language :: Python :: 3.6',
+      ],
+      python_requires='>=3.6',
+      url='https://github.com/minatsuki-yui/requestify',
+      license="MIT Licence",
+      packages=find_packages(),
+      install_requires=["requests"]
+      )
