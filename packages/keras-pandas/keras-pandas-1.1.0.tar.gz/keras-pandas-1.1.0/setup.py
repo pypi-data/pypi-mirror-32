c820eaff9cf76df15b3be6be30e80b73444e336c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:11:43 +0000

    Add package keras-pandas/1.1.0/keras-pandas-1.1.0.tar.gz

---
+++ packages/keras-pandas/keras-pandas-1.1.0.tar.gz/keras-pandas-1.1.0/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+# Set up requirements
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
+setup(
+    name='keras-pandas',
+    description='Easy and rapid deep learning',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    version='1.1.0',
+    url='https://github.com/bjherger/keras-pandas',
+    author='Brendan Herger',
+    author_email='13herger@gmail.com',
+    license='MIT',
+    packages=find_packages(),
+    install_requires=required,
+    zip_safe=False
+)
+
+
