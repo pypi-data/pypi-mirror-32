e061026f2887cbbb0653658eabf1bbac0bc102b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:48 +0000

    Add package gluoncv/0.2.0b20180516/gluoncv-0.2.0b20180516-py2.py3-none-any.whl

---
+++ packages/gluoncv/gluoncv-0.2.0b20180516-py2.py3-none-any.whl/gluoncv/utils/__init__.py
@@ -0,0 +1,17 @@
+"""Utility functions."""
+from __future__ import absolute_import
+
+from .download import download
+from .filesystem import makedirs
+from . import bbox
+from .bbox import bbox_iou
+from .block import set_lr_mult
+from . import viz
+from . import random
+from . import metrics
+
+from .lr_scheduler import PolyLRScheduler
+from .metrics.voc_segmentation import batch_pix_accuracy, batch_intersection_union
+from . import parallel
+
+from .plot_history import TrainingHistory
