39930604121eee4a51963a556f24a09f7204e3b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:58 +0000

    Add package fiduswriter-books/3.4.1/fiduswriter-books-3.4.1.tar.gz

---
+++ packages/fiduswriter-books/fiduswriter-books-3.4.1.tar.gz/fiduswriter-books-3.4.1/book/admin.py
@@ -0,0 +1,22 @@
+from __future__ import unicode_literals
+from django.contrib import admin
+
+from . import models
+
+
+class BookAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(models.Book, BookAdmin)
+
+
+class BookAccessRightAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(models.BookAccessRight, BookAccessRightAdmin)
+
+
+class ChapterAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(models.Chapter, ChapterAdmin)
