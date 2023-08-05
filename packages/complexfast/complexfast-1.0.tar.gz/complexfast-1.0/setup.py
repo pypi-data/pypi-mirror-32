1d1320b917dbafbf753ef1cdd0af24c5ad1c39f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:08:02 +0000

    Add package complexfast/1.0/complexfast-1.0.tar.gz

---
+++ packages/complexfast/complexfast-1.0.tar.gz/complexfast-1.0/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+ 
+setup(
+     name='complexfast',      # This is the name of your PyPI-package.    
+     version='1.0',           # Update the version number for new releases
+     packages=['complexfast'] # The name of your package, and also the command you'll be using for calling it
+ )
