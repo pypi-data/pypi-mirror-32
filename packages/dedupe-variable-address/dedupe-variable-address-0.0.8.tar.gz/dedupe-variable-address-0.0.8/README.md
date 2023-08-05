24ae125910936ec28c30da6e0eb05342a72990f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:24 +0000

    Add package dedupe-variable-address/0.0.8/dedupe-variable-address-0.0.8.tar.gz

---
+++ packages/dedupe-variable-address/dedupe-variable-address-0.0.8.tar.gz/dedupe-variable-address-0.0.8/README.md
@@ -0,0 +1,7 @@
+dedupe-variable-address
+=======================
+
+Provivde 'Address Variable Type' for dedupe
+
+[![Build Status](https://travis-ci.org/dedupeio/dedupe-variable-address.svg?branch=master)](https://travis-ci.org/dedupeio/dedupe-variable-address)
+[![Coverage Status](https://coveralls.io/repos/dedupeio/dedupe-variable-address/badge.png?branch=master)](https://coveralls.io/r/dedupeio/dedupe-variable-address?branch=master)
---
+++ packages/dedupe-variable-address/dedupe-variable-address-0.0.8.tar.gz/dedupe-variable-address-0.0.8/setup.py
@@ -0,0 +1,17 @@
+try:
+    from setuptools import setup, Extension
+except ImportError :
+    raise ImportError("setuptools module required, please go to https://pypi.python.org/pypi/setuptools and follow the instructions for installing setuptools")
+
+
+setup(
+    name='dedupe-variable-address',
+    url='https://github.com/datamade/dedupe-variables-address',
+    version='0.0.8',
+    description='Address variable type for dedupe',
+    packages=['dedupe.variables'],
+    install_requires=['usaddress',
+                      'parseratorvariable >= 0.0.17',
+                      'future'],
+    license='The MIT License: http://www.opensource.org/licenses/mit-license.php'
+    )
