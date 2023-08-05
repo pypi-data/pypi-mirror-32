386a3f353c70b63cffd810706f612ca96e1e3b7d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:35 +0000

    Add package hydrobox/0.1.3/hydrobox-0.1.3.tar.gz

---
+++ packages/hydrobox/hydrobox-0.1.3.tar.gz/hydrobox-0.1.3/hydrobox/tests/__init__.py
@@ -0,0 +1,6 @@
+from .decorators import TestAcceptDecorator, TestEnforceDecorator
+from .discharge import TestRegime, TestFlowDurationCurve, TestRichardBaker
+from .signal import TestSimplify
+from .io import TestTimeSeriesFromDistribution
+from .stats import TestMovingWindow, TestLinearRegression
+from .preprocessing import TestMerge, TestAggregate, TestCutPeriod
