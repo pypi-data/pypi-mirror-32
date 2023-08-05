93991c3829f6de58ab931819de7a2cac87825d75
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:43 +0000

    Add package easymunk/0.0.1/easymunk-0.0.1.tar.gz

---
+++ packages/easymunk/easymunk-0.0.1.tar.gz/easymunk-0.0.1/README.md
@@ -0,0 +1,22 @@
+# EasyMunk Python extension for Munkres
+EasyMunk provides a versatile interface alternative to the Munkres module for score-based (Hungarian algorithm) assignments.
+
+
+## Purpose
+The EasyMunk module interfaces with the Munkres module to:
+1. Provide an easy and adaptable method of pair assignment.
+2. Reduce the amount of pre-processing required for munkres.
+3. Automatically account for some special-case munkres decisions.
+4. Do all of the above without reducing munkres' functionality.
+
+See this module's easymunk.py for use instructions, or the [munkres documentation][] for more algorithm details.
+
+[munkres documentation]: https://github.com/bmc/munkres
+
+## Copyright
+
+EasyMunk module by Andrew M. Hogan. (EasyMunk &copy; 2018 Hogan Consulting Group)
+
+## License
+
+Licensed under the Apache License.
---
+++ packages/easymunk/easymunk-0.0.1.tar.gz/easymunk-0.0.1/setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="easymunk",
+    version="0.0.1",
+    author="Andrew M. Hogan",
+    author_email="drewthedruid@gmail.com",
+    description="An easy, versatile interface and processing handler for the munkres module.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Andrew-Hogan/easymunk",
+    packages=setuptools.find_packages(),
+    install_requires=['munkres'],
+    platforms=['any'],
+    classifiers=[
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Scientific/Engineering :: Mathematics",
+        "Topic :: Utilities",
+    ],
+)
