e5ad385a100e471f96db402069b38c41e51c7e2b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:15 +0000

    Add package repex/0.4.3.1/repex-0.4.3.1.tar.gz

---
+++ packages/repex/repex-0.4.3.1.tar.gz/repex-0.4.3.1/setup.py
@@ -0,0 +1,45 @@
+import os
+import codecs
+from setuptools import setup
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+
+def read(*parts):
+    # intentionally *not* adding an encoding option to open
+    return codecs.open(os.path.join(here, *parts), 'r').read()
+
+
+setup(
+    name='repex',
+    version="0.4.3.1",
+    url='https://github.com/cloudify-cosmo/repex',
+    author='Gigaspaces',
+    author_email='cosmo-admin@gigaspaces.com',
+    license='LICENSE',
+    platforms='All',
+    description='Replace Regular Expressions in files',
+    long_description=read('README.rst'),
+    packages=['repex'],
+    entry_points={
+        'console_scripts': [
+            'rpx = repex.repex:main',
+        ]
+    },
+    install_requires=[
+        "pyyaml==3.10",
+        "click==6.2",
+    ],
+    classifiers=[
+        'Programming Language :: Python',
+        'Natural Language :: English',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Information Technology',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: POSIX :: Linux',
+        'Operating System :: Microsoft',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+)
