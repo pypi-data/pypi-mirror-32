0af59fd80f188adfe5e73cc19ab3c007222d02a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:14 +0000

    Add package provbug/0.2.2/provbug-0.2.2.tar.gz

---
+++ packages/provbug/provbug-0.2.2.tar.gz/provbug-0.2.2/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+setup(
+    name="provbug",
+    packages=["provbug"],
+    version="0.2.2",    
+    scripts=['provbug/provbug.py', 'provbug/FunctionActivation.py', 'provbug/VariableState.py'],
+    entry_points={
+        "console_scripts": ["provbug=provbug:main"]
+    },
+    author=("Henrique Linhares, and Rômulo Ponciano"),
+    author_email="hlinhares@id.uff.br",
+    description="Supporting infrastructure to debug scientific experiments with noworkflow",
+    keywords=["scientific", "experiments", "provenance", "debug"],
+    url="https://github.com/linharesh/provbug",
+    python_requires='>=3.5',
+    classifiers=[
+    	'Development Status :: 3 - Alpha',
+
+    	'License :: OSI Approved :: MIT License',
+
+    	'Programming Language :: Python :: 3.5',
+    	'Programming Language :: Python :: 3.6',
+    ],
+)
