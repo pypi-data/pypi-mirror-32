721d54c23577c821a37d1a2432388526f7af81ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:13 +0000

    Add package bio2bel-entrez/0.0.1.dev0/bio2bel_entrez-0.0.1.dev0.tar.gz

---
+++ packages/bio2bel-entrez/bio2bel_entrez-0.0.1.dev0.tar.gz/bio2bel_entrez-0.0.1.dev0/src/bio2bel_entrez/__init__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+from . import cli
+from .manager import Manager
+
+__version__ = '0.0.1-dev'
+
+__title__ = 'bio2bel_entrez'
+__description__ = "A package for parsing and storing Entrez Gene"
+__url__ = 'https://github.com/cthoyt/pyentrez'
+
+__author__ = 'Charles Tapley Hoyt'
+__email__ = 'charles.hoyt@scai.fraunhofer.de'
+
+__license__ = 'Apache 2.0 License'
+__copyright__ = 'Copyright (c) 2017-2018 Charles Tapley Hoyt'
