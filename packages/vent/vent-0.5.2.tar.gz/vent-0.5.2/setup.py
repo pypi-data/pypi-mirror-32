a28bf36cbe7181aa279b81d04d9224b23df22ec8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:18 +0000

    Add package vent/0.5.2/vent-0.5.2.tar.gz

---
+++ packages/vent/vent-0.5.2.tar.gz/vent-0.5.2/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup(
+    name='vent',
+    version='v0.5.2',
+    packages=['vent', 'vent.core', 'vent.core.file_drop',
+              'vent.core.rq_worker', 'vent.core.rq_dashboard', 'vent.menus',
+              'vent.core.rmq_es_connector', 'vent.helpers', 'vent.api'],
+    install_requires=['docker>=2.5.1', 'npyscreen', 'pyyaml'],
+    scripts=['bin/vent'],
+    license='Apache License 2.0',
+    author='arpit',
+    author_email='',
+    maintainer='Charlie Lewis',
+    maintainer_email='clewis@iqt.org',
+    description=('A library that includes a CLI designed to serve as a'
+                 ' platform to collect and analyze data across a flexible set'
+                 ' of tools and technologies.'),
+    keywords='docker containers platform collection analysis tools devops',
+    url='https://github.com/CyberReboot/vent',
+)
