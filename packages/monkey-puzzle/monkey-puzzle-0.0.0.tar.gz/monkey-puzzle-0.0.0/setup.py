4fdbb8c279f168936127f00d1821f827bd81f4da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:28:10 +0000

    Add package monkey-puzzle/0.0.0/monkey-puzzle-0.0.0.tar.gz

---
+++ packages/monkey-puzzle/monkey-puzzle-0.0.0.tar.gz/monkey-puzzle-0.0.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='monkey-puzzle',
+    version='0.0.0',
+    description='Undisclosed project',
+    author="ASI Data Science",
+    author_email="info@asidatascience.com"
+)
