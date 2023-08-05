e4bff1de69dd326a412ae0de49b20160b9c90603
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:35 +0000

    Add package zops.anatomy/3.5.0/zops.anatomy-3.5.0.tar.gz

---
+++ packages/zops.anatomy/zops.anatomy-3.5.0.tar.gz/zops.anatomy-3.5.0/zops/anatomy/cli.py
@@ -0,0 +1,79 @@
+import os
+from zerotk.zops import Console
+
+import click
+
+
+click.disable_unicode_literals_warning = True
+
+
+@click.group('anatomy')
+def main():
+    pass
+
+
+@main.command('tree')
+@click.pass_context
+def tree(ctx):
+    from zops.anatomy.layers.feature import AnatomyFeatureRegistry
+    _register_features()
+
+    items = AnatomyFeatureRegistry.tree()
+    items = sorted([('/' not in filename, filename, fileid, feature) for (feature, fileid, filename) in items])
+
+    column_width = max([len(i[3]) for i in items])
+    item_format = '{{:{}}}  {{}}'.format(column_width)
+
+    for _priority, i_filename, i_fileid, i_feature in items:
+        Console.item(item_format.format(i_feature, i_filename))
+
+
+@main.command()
+@click.argument('directories', nargs=-1)
+@click.pass_context
+def apply(ctx, directories):
+    """
+    Apply templates.
+    """
+    from .layers.playbook import AnatomyPlaybook
+
+    _register_features()
+
+    if not directories:
+        directories = ('.',)
+    directories = [os.path.abspath(i) for i in directories]
+
+    for i_directory in directories:
+        Console.title(i_directory)
+        anatomy_playbook = AnatomyPlaybook.from_file(i_directory + '/anatomy-playbook.yml')
+        anatomy_playbook.apply(i_directory)
+
+
+@main.command('auto-apply')
+@click.pass_context
+def auto_apply(ctx):
+    """
+    Apply templates automatically configuring:
+    * Features file
+    * Search for anatomy-playbook.yml recursively.
+    """
+    from zerotk.lib.path import find_up, find_all
+    from zerotk.lib.gitignored import GitIgnored
+
+    features_filename = find_up('anatomy-features.yml', '.')
+    if features_filename is None:
+        Console.info("Can't find features file: anatomy-features.yml.")
+        return 1
+    Console.info("{}: Features file.".format(features_filename))
+
+    playbook_filenames = find_all('anatomy-playbook.yml', '.')
+    playbook_filenames = GitIgnored().filter(playbook_filenames)
+
+    os.environ['ZOPS_ANATOMY_FEATURES'] = features_filename
+    ctx.invoke(apply, directories=map(os.path.dirname, playbook_filenames))
+
+
+def _register_features():
+    from .layers.feature import AnatomyFeatureRegistry
+    filename = os.environ['ZOPS_ANATOMY_FEATURES']
+    AnatomyFeatureRegistry.register_from_file(filename)
---
+++ packages/zops.anatomy/zops.anatomy-3.5.0.tar.gz/zops.anatomy-3.5.0/zops/anatomy/yaml.py
@@ -0,0 +1,14 @@
+
+
+def yaml_from_file(filename_):
+    with open(filename_, 'r') as iss:
+        content = iss.read()
+        result = yaml_load(content)
+    return result
+
+
+def yaml_load(text):
+    from ruamel.yaml import YAML
+
+    yaml = YAML()
+    return yaml.load(text)
