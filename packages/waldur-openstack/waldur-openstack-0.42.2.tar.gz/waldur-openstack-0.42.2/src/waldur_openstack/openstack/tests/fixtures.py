4adfcfb46b979947ae6745c5e293fa8a1b904e9e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:44 +0000

    Add package waldur-openstack/0.42.2/waldur-openstack-0.42.2.tar.gz

---
+++ packages/waldur-openstack/waldur-openstack-0.42.2.tar.gz/waldur-openstack-0.42.2/src/waldur_openstack/openstack/tests/fixtures.py
@@ -0,0 +1,37 @@
+from django.utils.functional import cached_property
+
+from waldur_core.structure.tests.fixtures import ProjectFixture
+
+from . import factories
+from .. import models
+
+
+class OpenStackFixture(ProjectFixture):
+    @cached_property
+    def openstack_service_settings(self):
+        return factories.OpenStackServiceSettingsFactory(customer=self.customer)
+
+    @cached_property
+    def openstack_service(self):
+        return factories.OpenStackServiceFactory(
+            customer=self.customer, settings=self.openstack_service_settings)
+
+    @cached_property
+    def openstack_spl(self):
+        return factories.OpenStackServiceProjectLinkFactory(
+            project=self.project, service=self.openstack_service)
+
+    @cached_property
+    def tenant(self):
+        return factories.TenantFactory(service_project_link=self.openstack_spl)
+
+    @cached_property
+    def network(self):
+        return factories.NetworkFactory(service_project_link=self.openstack_spl,
+                                        tenant=self.tenant,
+                                        state=models.Network.States.OK)
+
+    @cached_property
+    def subnet(self):
+        return factories.SubNetFactory(service_project_link=self.openstack_spl,
+                                       state=models.SubNet.States.OK)
