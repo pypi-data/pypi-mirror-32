e5912ef8c702402b0c67f9bb7e36ba03f030f9c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:33 +0000

    Add package mytestpro/1.0.0/mytestpro-1.0.0-py3-none-any.whl

---
+++ packages/mytestpro/mytestpro-1.0.0-py3-none-any.whl/Tool.py
@@ -0,0 +1,11 @@
+# from mytestlib import my_lib_one as mlo
+# 
+# mlo.run()
+
+# 导入包的时候,直接关联my_lib_one,只是测试
+# import mytestlib as mtl
+# 
+# mtl.my_lib_one.run()
+
+def test():
+    print("Tool test")
---
+++ packages/mytestpro/mytestpro-1.0.0-py3-none-any.whl/mytestlib/__init__.py
@@ -0,0 +1,4 @@
+# import my_lib_one
+
+# 包内导入,用相对导入
+from . import my_lib_one
