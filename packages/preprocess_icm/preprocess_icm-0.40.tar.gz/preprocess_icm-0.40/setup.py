b6a52ae1004882e9e74f3c24503b03977e721e20
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:09:57 +0000

    Add package preprocess_icm/0.40/preprocess_icm-0.40.tar.gz

---
+++ packages/preprocess_icm/preprocess_icm-0.40.tar.gz/preprocess_icm-0.40/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='preprocess_icm',
+      version='0.40',
+      description='Preprocessing incoming messages',
+      author='Yotam Rosenthal',
+      author_email='yotam.rosenthal@servicefriend.com',
+      license='MIT',
+      packages=['preprocess_icm'],
+      zip_safe=False)
