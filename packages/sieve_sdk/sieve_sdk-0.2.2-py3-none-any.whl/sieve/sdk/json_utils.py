8b5883cd055285a5a8d69ccaa629336aad524c0a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:05 +0000

    Add package sieve_sdk/0.2.2/sieve_sdk-0.2.2-py3-none-any.whl

---
+++ packages/sieve_sdk/sieve_sdk-0.2.2-py3-none-any.whl/sieve/sdk/json_utils.py
@@ -0,0 +1,19 @@
+import json
+import datetime
+from functools import partial
+
+from sieve.sdk import config
+from pathdict.collection import StringIndexableList, PathDict
+
+
+def default_handler(value):
+    if isinstance(value, datetime.datetime):
+        return value.strftime(config.DATETIME_FORMAT)
+    elif isinstance(value, PathDict):
+        return dict(value)
+    elif isinstance(value, StringIndexableList):
+        return list(value)
+    raise TypeError("Unexpected type")
+
+
+dumps = partial(json.dumps, default=default_handler)
