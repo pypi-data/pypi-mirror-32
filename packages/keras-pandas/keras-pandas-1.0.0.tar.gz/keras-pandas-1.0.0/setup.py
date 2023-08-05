b7affc995dbadff78809e9a94674d189488e8f86
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:01 +0000

    Add package keras-pandas/1.0.0/keras-pandas-1.0.0.tar.gz

---
+++ packages/keras-pandas/keras-pandas-1.0.0.tar.gz/keras-pandas-1.0.0/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+# Set up requirements
+
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
+setup(
+    name='keras-pandas',
+    description='test',
+    version='1.0.0',  # Update the version number for new releases
+    url='https://github.com/bjherger/keras-pandas',
+    author='Brendan Herger',
+    author_email='13herger@gmail.com',
+    license='MIT',
+    packages=['keras_pandas'],
+    install_requires=required,
+    zip_safe=False
+)
+
+
