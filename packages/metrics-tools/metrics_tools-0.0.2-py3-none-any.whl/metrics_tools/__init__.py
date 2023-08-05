1f81fb9ac2ef8fa085bfb6f4b7fbed5a25dfc445
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:52 +0000

    Add package metrics-tools/0.0.2/metrics_tools-0.0.2-py3-none-any.whl

---
+++ packages/metrics-tools/metrics_tools-0.0.2-py3-none-any.whl/metrics_tools/__init__.py
@@ -0,0 +1,8 @@
+__author__ = 'Johannes Kunze, https://github.com/Jonnyblacklabel'
+__credits__ = ['Johannes Kunze']
+__maintainer__ = 'Johannes Kunze'
+__email__ = 'mail@jonnyblacklabel.de'
+__license__ = 'MIT License'
+__version__ = '0.0.2'
+
+from .metrics_tools import MetricsTools
---
+++ packages/metrics-tools/metrics_tools-0.0.2-py3-none-any.whl/metrics_tools/endpoint_base.py
@@ -0,0 +1,7 @@
+class BaseEndpoint():
+
+	def __repr__(self):
+		return f'<metrics.tools Endpoint [{self.endpoint}]>'
+
+if __name__ == '__main__':
+	pass
