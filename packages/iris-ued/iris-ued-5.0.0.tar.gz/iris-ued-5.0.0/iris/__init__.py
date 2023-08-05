6c75f5f960ab93838a1e56980339c292c1cfe93e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:15:58 +0000

    Add package iris-ued/5.0.0/iris-ued-5.0.0.tar.gz

---
+++ packages/iris-ued/iris-ued-5.0.0.tar.gz/iris-ued-5.0.0/iris/__init__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+__author__  = 'Laurent P. René de Cotret'
+__email__   = 'laurent.renedecotret@mail.mcgill.ca'
+__license__ = 'MIT'
+__version__ = '5.0.0'
+
+# Versioning should be compliant with PyPI guide 
+# https://packaging.python.org/tutorials/distributing-packages/#choosing-a-versioning-scheme
+
+from .raw import AbstractRawDataset, check_raw_bounds
+from .mcgill import McGillRawDataset, LegacyMcGillRawDataset
+from .dataset import DiffractionDataset, PowderDiffractionDataset
+from .meta import ExperimentalParameter
+
+from . import plugins
