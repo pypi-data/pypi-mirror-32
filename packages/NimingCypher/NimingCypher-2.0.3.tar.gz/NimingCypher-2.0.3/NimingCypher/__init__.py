2784a4d88b95a7b3677221cf14cde250a932fd41
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:09:52 +0000

    Add package NimingCypher/2.0.3/NimingCypher-2.0.3.tar.gz

---
+++ packages/NimingCypher/NimingCypher-2.0.3.tar.gz/NimingCypher-2.0.3/NimingCypher/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+
+##    Ce module permet de chiffrer des chaines de caractères et des fichier.
+##    Il permet une grande sécurité, et rapidité pour les
+##    conversations instantanées.
+
+__all__ = ["NCrypter"]
+from NimingCypher.Niming import NCrypter
