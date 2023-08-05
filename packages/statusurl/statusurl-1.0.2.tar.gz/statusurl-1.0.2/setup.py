4a6da9a0718ac2655331adf91fb36588696b1412
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:56 +0000

    Add package statusurl/1.0.2/statusurl-1.0.2.tar.gz

---
+++ packages/statusurl/statusurl-1.0.2.tar.gz/statusurl-1.0.2/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+setup(
+  name = 'statusurl',
+  py_modules = ['statusurl'],	
+  install_requires = ['requests'],	
+  version = '1.0.2',
+  description = 'A test with lists of urls',
+  author = 'Alisson Castro',
+  author_email = 'alissoncastroskt@gmail.com',
+  url = 'https://github.com/alicastrogit/', # use the URL to the github repo
+  #download_url = 'https://github.com/0.1.tar.gz', # I'll explain this in a second
+  keywords = ['testing', 'list', 'example'], # arbitrary keywords
+  classifiers = [],
+)
