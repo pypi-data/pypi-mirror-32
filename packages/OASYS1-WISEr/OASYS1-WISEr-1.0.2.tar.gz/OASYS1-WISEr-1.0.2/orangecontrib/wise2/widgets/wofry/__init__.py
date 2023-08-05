0ad07a8025916217fc3c79fa6a3c822b78f490bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:47 +0000

    Add package OASYS1-WISEr/1.0.2/OASYS1-WISEr-1.0.2.tar.gz

---
+++ packages/OASYS1-WISEr/OASYS1-WISEr-1.0.2.tar.gz/OASYS1-WISEr-1.0.2/orangecontrib/wise2/widgets/wofry/__init__.py
@@ -0,0 +1,20 @@
+"""
+=========
+Associate
+=========
+
+Widgets for association rules.
+
+"""
+
+# Category description for the widget registry
+
+NAME = "WISE 2 - WOFRY"
+
+DESCRIPTION = "Widgets for WISE 2"
+
+BACKGROUND = "#FD8D8D"
+
+ICON = "icons/wavefront_propagation.png"
+
+PRIORITY = 304
