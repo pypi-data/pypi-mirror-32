6fd588e523d222b3f16aeb32ddb75aafadb34506
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:06 +0000

    Add package xgp/0.0.1/xgp-0.0.1.tar.gz

---
+++ packages/xgp/xgp-0.0.1.tar.gz/xgp-0.0.1/README.md
@@ -0,0 +1,14 @@
+# XGP Python package
+
+[![Travis build status](https://travis-ci.org/MaxHalford/xgp-python.svg?branch=master)](https://travis-ci.org/MaxHalford/xgp-python)
+[![Coverage Status](https://coveralls.io/repos/github/MaxHalford/xgp-python/badge.svg?branch=master)](https://coveralls.io/github/MaxHalford/xgp-python?branch=master)
+
+This repository contains Python bindings to the [XGP library](https://github.com/MaxHalford/xgp). It is a simple wrapper that calls the XGP dynamic shared library and exposes a scikit-learn interface.
+
+## Documentation
+
+Please refer to the [Python section of the XGP website](https://maxhalford.github.io/xgp/python/).
+
+## Installation
+
+Installation instructions are available [here](https://maxhalford.github.io/xgp/cli/#installation).
---
+++ packages/xgp/xgp-0.0.1.tar.gz/xgp-0.0.1/setup.py
@@ -0,0 +1,32 @@
+from __future__ import print_function
+import sys
+from setuptools import Extension, setup, find_packages
+
+with open('requirements.txt') as f:
+    INSTALL_REQUIRES = [l.strip() for l in f.readlines() if l]
+
+
+try:
+    import numpy
+except ImportError:
+    print('numpy is required during installation')
+    sys.exit(1)
+
+try:
+    import scipy
+except ImportError:
+    print('scipy is required during installation')
+    sys.exit(1)
+
+setup(name='xgp',
+      version='0.0.1',
+      description='XGP scikit-learn API',
+      author='Max Halford',
+      packages=find_packages(),
+      install_requires=INSTALL_REQUIRES,
+      author_email='maxhalford25@gmail.com',
+      url='https://maxhalford.github.io/xgp',
+      build_golang={'root': 'github.com/MaxHalford/xgp-python'},
+      ext_modules=[Extension('xgp', ['xgp/xgp.go'])],
+      setup_requires=['setuptools-golang>=0.2.0'],
+      )
