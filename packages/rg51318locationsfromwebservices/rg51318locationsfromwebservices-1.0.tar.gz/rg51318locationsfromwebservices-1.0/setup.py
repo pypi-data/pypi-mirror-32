dde96b936bc5dcca48f1b164d2c19c06c1b3365f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:09:12 +0000

    Add package rg51318locationsfromwebservices/1.0/rg51318locationsfromwebservices-1.0.tar.gz

---
+++ packages/rg51318locationsfromwebservices/rg51318locationsfromwebservices-1.0.tar.gz/rg51318locationsfromwebservices-1.0/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(name='rg51318locationsfromwebservices',
+      version='1.0',
+      description='rg51318locationsfromwebservices',
+      url='http://drupal.psu.edu/geog489/',
+      author='GEOG489',
+      author_email='GEOG489@psu.edu',
+      license='BSD',
+      packages=find_packages(),
+      #install_requires=['pyqt'],
+      zip_safe=False)
+
