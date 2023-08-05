d96003539e6357b7671e41e0fec7d4e3a42c003c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:12 +0000

    Add package dictsort/0.1/dictsort-0.1.tar.gz

---
+++ packages/dictsort/dictsort-0.1.tar.gz/dictsort-0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='dictsort',
+    version='0.1',
+    keywords=("pip"),
+    description='sort dict data',
+    url='https://github.com/danyubaoqi/dictsort',
+    author='danyubaoqi',
+    author_email='1600093003@pku.edu.cn',
+    license='MIT',
+    zip_safe=False)
+
