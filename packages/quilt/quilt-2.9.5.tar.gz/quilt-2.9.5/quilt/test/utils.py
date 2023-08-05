492c0a76c5fe690e7b9579a5151adcdaf6e751f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:33:03 +0000

    Add package quilt/2.9.5/quilt-2.9.5.tar.gz

---
+++ packages/quilt/quilt-2.9.5.tar.gz/quilt-2.9.5/quilt/test/utils.py
@@ -0,0 +1,78 @@
+"""
+Unittest setup.
+"""
+
+import os
+import shutil
+import tempfile
+import unittest
+
+import quilt
+from ..tools.const import PACKAGE_DIR_NAME
+
+try:
+    # Python3
+    from unittest.mock import patch
+except ImportError:
+    # Python2 - external dependency.
+    from mock import patch
+
+import responses
+
+
+def quilt_dev_mode(func):
+    """Sets quilt._DEV_MODE and restores original state on context exit
+
+    Decorator.
+
+    Use to enable dev mode during a function call -- for example, to
+    disable input() prompts during a specific test.
+    """
+    def decorated(*args, **kwargs):
+        dev_mode = quilt._DEV_MODE
+        try:
+            quilt._DEV_MODE = True
+            return func(*args, **kwargs)
+        finally:
+            quilt._DEV_MODE = dev_mode
+    return decorated
+
+
+class BasicQuiltTestCase(unittest.TestCase):
+    """
+    Base class for unittests.
+    - Creates a temporary directory
+    """
+    def setUp(self):
+        self._old_dir = os.getcwd()
+        self._test_dir = tempfile.mkdtemp(prefix='quilt-test-')
+        os.chdir(self._test_dir)
+
+    def tearDown(self):
+        os.chdir(self._old_dir)
+        shutil.rmtree(self._test_dir)
+
+class QuiltTestCase(BasicQuiltTestCase):
+    """
+    - Mocks requests
+    - (And inherits temp directory from superclass)
+    """
+    def setUp(self):
+        super(QuiltTestCase, self).setUp()
+
+        self.auth_patcher = patch('quilt.tools.command._load_auth', lambda: {})
+        self.auth_patcher.start()
+
+        self._store_dir = os.path.join(self._test_dir, PACKAGE_DIR_NAME)
+        self.store_patcher = patch.dict(os.environ, {'QUILT_PRIMARY_PACKAGE_DIR': self._store_dir})
+        self.store_patcher.start()
+
+        self.requests_mock = responses.RequestsMock(assert_all_requests_are_fired=True)
+        self.requests_mock.start()
+
+    def tearDown(self):
+        self.requests_mock.stop()
+        self.auth_patcher.stop()
+        self.store_patcher.stop()
+
+        super(QuiltTestCase, self).tearDown()
---
+++ packages/quilt/quilt-2.9.5.tar.gz/quilt-2.9.5/setup.py
@@ -0,0 +1,66 @@
+from setuptools import setup, find_packages
+
+
+def readme():
+    readme_short = """
+    ``quilt`` is a command-line utility that builds, pushes, and installs
+    data packages. A `data package <https://blog.quiltdata.com/data-packages-for-fast-reproducible-python-analysis-c74b78015c7f>`_
+    is a versioned bundle of serialized data wrapped in a Python module.
+
+    ``quilt`` pushes to and pulls from the package registry at quiltdata.com.
+
+    Visit `quiltdata.com <https://quiltdata.com>`_ for docs and more.
+    """
+    return readme_short
+
+
+setup(
+    name="quilt",
+    version="2.9.5",
+    packages=find_packages(),
+    description='Quilt is a data package manager',
+    long_description=readme(),
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+    author='quiltdata',
+    author_email='contact@quiltdata.io',
+    license='LICENSE',
+    url='https://github.com/quiltdata/quilt',
+    download_url='https://github.com/quiltdata/quilt/releases/tag/2.9.5',
+    keywords='quilt quiltdata shareable data dataframe package platform pandas',
+    install_requires=[
+        'appdirs>=1.4.0',
+        'enum34; python_version<"3.0"',     # stdlib backport
+        'future>=0.16.0',                   # stdlib backport: 'from builtins import xxx', plus others.
+        'packaging>=16.8',
+        'pandas>=0.19.2',
+        'pathlib2; python_version<"3.6"',   # stdlib backport
+        'pyarrow>=0.4.0,<0.8.0',            # TODO(dima): Make unit tests work with 0.8.*.
+        'pyOpenSSL>=16.2.0',                # Note: not actually used at the moment.
+        'pyyaml>=3.12',
+        'requests>=2.12.4',
+        'six>=1.10.0',
+        'tqdm>=4.11.2',
+        'xlrd>=1.0.0',
+    ],
+    extras_require={
+        # Use: pip install --editable ./[tests]
+        'tests': [
+            'funcsigs; python_version<"3.4"',   # stdlib backport
+            'mock; python_version<"3.3"',
+            'pytest',
+            'responses>=0.7.0',
+        ]
+    },
+    include_package_data=True,
+    entry_points={
+        'console_scripts': ['quilt=quilt.tools.main:main'],
+    }
+)
