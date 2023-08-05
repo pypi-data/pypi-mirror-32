2e21f77bb8933933b776a13eccc3d3e8d692dc69
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:39 +0000

    Add package fastent/0.5.4/fastent-0.5.4.tar.gz

---
+++ packages/fastent/fastent-0.5.4.tar.gz/fastent-0.5.4/setup.py
@@ -0,0 +1,29 @@
+from distutils.core import setup
+import os
+import subprocess
+
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
+setup(
+  name = 'fastent',
+  packages = ['fastent'], # this must be the same as the name above
+  version = '0.5.4',
+  description = 'Automated Custom NER tool',
+  author = 'Erik Arakelyan',
+  author_email = 'erikarakelyan1997@gmail.com',
+  scripts=['fastent/fastent_install.sh'],
+  install_requires=requirements,
+  url = 'https://github.com/fastent/fastent', # use the URL to the github repo
+  long_description=readme(),
+  download_url = 'https://github.com/fastent/fastent/archive/0.5.tar.gz', # I'll explain this in a second
+  keywords = ['NER', 'Anotation', 'Contextualization'], # arbitrary keywords
+  classifiers = [],
+)
+
+subprocess.call(['./fastent_install.sh'])
+
