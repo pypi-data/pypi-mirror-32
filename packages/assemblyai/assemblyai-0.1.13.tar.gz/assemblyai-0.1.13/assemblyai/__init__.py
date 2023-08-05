6c067674d06bb34aa0ff13f020004eaa7c65812d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:23 +0000

    Add package assemblyai/0.1.13/assemblyai-0.1.13.tar.gz

---
+++ packages/assemblyai/assemblyai-0.1.13.tar.gz/assemblyai-0.1.13/assemblyai/__init__.py
@@ -0,0 +1,7 @@
+"""Top-level package."""
+
+__author__ = """AssemblyAI"""
+__email__ = 'robot@assemblyai.com'
+__version__ = '0.1.13'
+
+from .client import Client
---
+++ packages/assemblyai/assemblyai-0.1.13.tar.gz/assemblyai-0.1.13/tests/test_client.py
@@ -0,0 +1,62 @@
+"""Tests for client."""
+
+import os
+
+import pytest
+
+from assemblyai import Client
+from assemblyai.exceptions import ClientError
+# import wikipedia
+
+
+ASSEMBLYAI_URL = os.environ.get('ASSEMBLYAI_URL', 'https://api.assemblyai.com')
+ASSEMBLYAI_TOKEN = os.environ.get('ASSEMBLYAI_TOKEN')
+AUDIO_URL = 'https://s3-us-west-2.amazonaws.com/assemblyai.prooflab/office_nine_degrees.wav'
+
+
+def test_client_auth_error():
+    """Test client without token throws auth error."""
+    with pytest.raises(ClientError):
+        aai = Client(token='foobar')
+        aai.transcribe(audio_url=AUDIO_URL)
+
+
+def test_client_transcribe():
+    """Test client token authenticates and creates transcript."""
+    aai = Client(token=ASSEMBLYAI_TOKEN)
+    transcript = aai.transcribe(audio_url=AUDIO_URL)
+    assert transcript.status == 'queued'
+    transcript_id = transcript.id
+    while transcript.status != 'completed':
+        transcript = transcript.get()
+    assert transcript.status == 'completed'
+    assert transcript_id == transcript.id
+    transcript = transcript.get(id=transcript_id)
+    assert transcript_id == transcript.id
+
+
+def test_client_train():
+    """Test client token authenticates and creates transcript."""
+    aai = Client(token=ASSEMBLYAI_TOKEN)
+    model = aai.train(['foo', 'bar'])
+    assert model.status == 'training'
+    model_id = model.id
+    model = model.get()
+    assert model_id == model.id
+
+
+def test_client_train_transcribe():
+    """Test client token authenticates and creates transcript."""
+    aai = Client(token=ASSEMBLYAI_TOKEN)
+    model = aai.train(['foo', 'bar'])
+    assert model.status == 'training'
+    model_id = model.id
+    model = model.get()
+    assert model_id == model.id
+    model = model.get(id=model_id)
+    assert model_id == model.id
+    transcript = aai.transcribe(audio_url=AUDIO_URL, model=model)
+    assert transcript.id is None
+    transcript = transcript.get()
+    assert transcript.id is None
+    assert transcript.status == 'waiting for model'
