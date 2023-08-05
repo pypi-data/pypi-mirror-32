540bd18a865f4746a2913b7ca666020d06af0d41
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:11:56 +0000

    Add package apistar-autoapp/0.5.1/apistar-autoapp-0.5.1.tar.gz

---
+++ packages/apistar-autoapp/apistar-autoapp-0.5.1.tar.gz/apistar-autoapp-0.5.1/apistar_autoapp/__init__.py
@@ -0,0 +1,11 @@
+from apistar_autoapp.autoapp import (AutoApp, AutoASyncApp, app_args,
+                                     print_components, print_include,
+                                     print_route, print_routes)
+
+__version__ = '0.5.1'
+
+
+__all__ = [
+    'app_args', 'print_include', 'print_route', 'print_routes', 'print_components',
+    'AutoApp', 'AutoASyncApp'
+]
---
+++ packages/apistar-autoapp/apistar-autoapp-0.5.1.tar.gz/apistar-autoapp-0.5.1/setup.py
@@ -0,0 +1,61 @@
+#
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements. See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership. The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License. You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied. See the License for the
+# specific language governing permissions and limitations
+# under the License.
+#
+import os
+import re
+
+from setuptools import setup
+
+
+def get_version(package):
+    """
+    Return package version as listed in `__version__` in `init.py`.
+    """
+    init_py = open(os.path.join(package, '__init__.py')).read()
+    return re.search("__version__ = ['\"]([^'\"]+)['\"]", init_py).group(1)
+
+
+version = get_version('apistar_autoapp')
+
+
+setup(
+    name='apistar-autoapp',
+    version=version,
+    description='Automatically import APIStar sub apps',
+    url='https://github.com/jeffbuttars/apistar-autoapp',
+    author='Jeff Buttars',
+    author_email='jeff@jeffbuttars.com',
+    license='Apache License 2.0',
+    packages=['apistar_autoapp'],
+    install_requires=[
+        'apistar',
+        'whitenoise',
+        'aiofiles',
+    ],
+    classifiers=[
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Topic :: Internet :: WWW/HTTP',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+)
