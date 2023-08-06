668ed7f4aefb6274c740f5b3acfe08f776de363f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:13:15 +0000

    Add package grow/0.5.1/grow-0.5.1.tar.gz

---
+++ packages/grow/grow-0.5.1.tar.gz/grow-0.5.1/grow/extensions/hooks/base_hook.py
@@ -0,0 +1,24 @@
+"""Base class for writing hooks."""
+
+class BaseHook(object):
+    """Base class for all hooks."""
+
+    KEY = 'hook'
+    NAME = 'Hook'
+
+    def __init__(self, extension):
+        self.extension = extension
+
+    @property
+    def pod(self):
+        """Reference to the pod."""
+        return self.extension.pod
+
+    # pylint:disable=no-self-use
+    def should_trigger(self, *_args, **_kwargs):
+        """Determine if the hook should trigger."""
+        return True
+
+    def trigger(self, previous_result, *_args, **_kwargs):
+        """Trigger the hook."""
+        raise NotImplementedError()
