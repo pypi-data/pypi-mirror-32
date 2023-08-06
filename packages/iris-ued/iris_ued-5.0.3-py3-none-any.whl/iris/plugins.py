c43858f2f06c098500c19fa6b64fd04809415840
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:53:10 +0000

    Add package iris-ued/5.0.3/iris_ued-5.0.3-py3-none-any.whl

---
+++ packages/iris-ued/iris_ued-5.0.3-py3-none-any.whl/iris/plugins.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Plugin managemenent
+===================
+
+Plug-ins are modules implementing a subclass of AbstractRawDataset. 
+Plug-ins should be placed in ~\iris_plugins.
+
+Plug-in classes can be imported from ``iris.plugins``.
+"""
+
+from pathlib import Path
+from runpy import run_path
+
+# Pluging location is ~\iris_plugins
+PLUGIN_DIR = Path.home() / Path('iris_plugins')
+
+# Create the installation directory if it does not exist
+if not PLUGIN_DIR.exists():
+    PLUGIN_DIR.mkdir()
+
+for fname in PLUGIN_DIR.rglob('*.py'):
+    globals().update(run_path(PLUGIN_DIR / fname, run_name = 'iris.plugins'))
