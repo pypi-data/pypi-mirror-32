d18b636c1bdbfa0bffe54717c2191fe44049046c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:36:34 +0000

    Add package readN/0.0.1/readN-0.0.1.tar.gz

---
+++ packages/readN/readN-0.0.1.tar.gz/readN-0.0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='readN',
+    version='0.0.1',
+    packages=[''],
+    url='https://github.com/chrismcmorran/readN',
+    license='MIT',
+    author='Christopher McMorran',
+    author_email='christopher.mcmorran@carleton.ca',
+    description='A small package for retrieving the first and last n characters in a text based file.'
+)
