04dfb3173d98acf9d19810733dbbd9496e0d8779
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:14:03 +0000

    Add package hxl-proxy/1.8/hxl-proxy-1.8.tar.gz

---
+++ packages/hxl-proxy/hxl-proxy-1.8.tar.gz/hxl-proxy-1.8/setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+"""Install, build, or test the HXL Proxy.
+For details, try
+  python setup.py -h
+"""
+
+import sys, setuptools
+
+from hxl_proxy import __version__
+
+if sys.version_info.major != 3:
+    raise SystemExit("The HXL Proxy requires Python 3.x")
+
+setuptools.setup(
+    name = 'hxl-proxy',
+    packages = ['hxl_proxy'],
+    package_data={'hxl_proxy': ['*.sql']},
+    version = __version__,
+    description = 'Flask-based web proxy for HXL',
+    author='David Megginson',
+    author_email='contact@megginson.com',
+    url='https://github.com/HXLStandard/hxl-proxy',
+    include_package_data = True,
+    zip_safe = False,
+    install_requires=['flask-cache>=0.13', 'libhxl==4.8', 'ckanapi>=3.5', 'flask==0.12.4', 'requests_cache', 'mysql-connector-python'],
+    test_suite = "tests",
+    tests_require = ['mock']
+)
