a781d5cab38bfa7cb9b8d7600950516d09d6282c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:25:35 +0000

    Add package IntexfyLibs/0.0.6/IntexfyLibs-0.0.6.tar.gz

---
+++ packages/IntexfyLibs/IntexfyLibs-0.0.6.tar.gz/IntexfyLibs-0.0.6/setup.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+import setuptools
+from distutils.core import setup
+
+setup(
+    name='IntexfyLibs',
+    version='0.0.6',
+    py_modules=['IntexfyLibs'],
+    author='Heitor Tancredo',
+    author_email='heitor@intexfy.com',
+    keywords='Intexfy Libs',
+    description='Package with intexfy libs',
+)
