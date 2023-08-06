85856e2e59f64e1fd2c02875a809aba80d24e941
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:14:03 +0000

    Add package citellus/0.1.9.386386521.20180531214630/citellus-0.1.9.386386521.20180531214630.tar.gz

---
+++ packages/citellus/citellus-0.1.9.386386521.20180531214630.tar.gz/citellus-0.1.9.386386521.20180531214630/tests/test_magui.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+import os
+from unittest import TestCase
+
+import citellusclient.shell as citellus
+from maguiclient import magui
+
+testplugins = os.path.join(citellus.citellusdir, 'plugins', 'test')
+
+
+class MaguiTest(TestCase):
+    def test_commonpath(self):
+        res = magui.commonpath(['/etc/path', '/etc/common'])
+        assert res == '/etc'
+
+    def test_domagui(self):
+        # Call with no arguments
+        res = magui.domagui(sosreports=[], citellusplugins=[])
+        assert res == {}
