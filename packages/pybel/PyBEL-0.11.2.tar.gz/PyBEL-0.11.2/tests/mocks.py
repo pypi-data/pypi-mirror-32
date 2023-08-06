d9892c8005443f46ee20c6f78a954d06f8c31fc2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:40 +0000

    Add package pybel/0.11.2/PyBEL-0.11.2.tar.gz

---
+++ packages/pybel/PyBEL-0.11.2.tar.gz/PyBEL-0.11.2/tests/mocks.py
@@ -0,0 +1,72 @@
+# -*- coding: utf-8 -*-
+
+"""This module holds the mocks used in testing"""
+
+import os
+
+from pybel.constants import GOCC_LATEST
+from tests.constants import bel_dir_path, belanno_dir_path, belns_dir_path, get_uri_name
+
+try:
+    from unittest import mock
+except ImportError:
+    import mock
+
+
+class MockResponse:
+    """See http://stackoverflow.com/questions/15753390/python-mock-requests-and-the-response"""
+
+    def __init__(self, mock_url):
+        if mock_url == GOCC_LATEST:
+            self.path = os.path.join(belns_dir_path, 'go-cellular-component.belns')
+        elif mock_url.endswith('hgnc-human-genes-20170725.belns'):
+            self.path = os.path.join(belns_dir_path, 'hgnc-human-genes.belns')
+        elif mock_url.endswith('chebi-20170725.belns'):
+            self.path = os.path.join(belns_dir_path, 'chebi.belns')
+        elif mock_url.endswith('go-biological-process-20170725.belns'):
+            self.path = os.path.join(belns_dir_path, 'go-biological-process.belns')
+        elif mock_url.endswith('species-taxonomy-id-20170511.belanno'):
+            self.path = os.path.join(belanno_dir_path, 'species-taxonomy-id.belanno')
+        elif mock_url.endswith('confidence-1.0.0.belanno'):
+            self.path = os.path.join(belanno_dir_path, 'confidence-1.0.0.belanno')
+        elif mock_url.endswith('.belns'):
+            self.path = os.path.join(belns_dir_path, get_uri_name(mock_url))
+        elif mock_url.endswith('.belanno'):
+            self.path = os.path.join(belanno_dir_path, get_uri_name(mock_url))
+        elif mock_url.endswith('.bel'):
+            self.path = os.path.join(bel_dir_path, get_uri_name(mock_url))
+        else:
+            raise ValueError('Invalid extension')
+
+        if not os.path.exists(self.path):
+            raise ValueError("file doesn't exist: {}".format(self.path))
+
+    def iter_lines(self):
+        with open(self.path, 'rb') as file:
+            lines = list(file)
+
+        for line in lines:
+            yield line
+
+    def raise_for_status(self):
+        pass
+
+
+class MockSession:
+    """Patches the session object so requests can be redirected through the filesystem without rewriting BEL files"""
+
+    def __init__(self):
+        pass
+
+    def mount(self, prefix, adapter):
+        pass
+
+    @staticmethod
+    def get(url):
+        return MockResponse(url)
+
+    def close(self):
+        pass
+
+
+mock_bel_resources = mock.patch('pybel.resources.utils.requests.Session', side_effect=MockSession)
---
+++ packages/pybel/PyBEL-0.11.2.tar.gz/PyBEL-0.11.2/tests/utils.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+
+from uuid import uuid4
+
+from pybel.manager.models import Annotation, AnnotationEntry, Namespace, NamespaceEntry
+
+
+def n():
+    """Returns a UUID string for testing
+
+    :rtype: str
+    """
+    return str(uuid4())[:15]
+
+
+def make_dummy_namespaces(manager, graph, namespaces):
+    """
+    :param pybel.manager.Manager manager:
+    :param pybel.BELGraph graph:
+    :param dict[str,iter[str]] namespaces:
+    """
+    for keyword, names in namespaces.items():
+        url = n()
+        graph.namespace_url[keyword] = url
+
+        namespace = Namespace(keyword=keyword, url=url)
+        manager.session.add(namespace)
+        manager.namespace_model[url] = namespace
+
+        for name in names:
+            entry = manager.namespace_object_cache[url][entry.name] = NamespaceEntry(name=name, namespace=namespace)
+            manager.session.add(entry)
+
+        manager.session.commit()
+
+
+def make_dummy_annotations(manager, graph, annotations):
+    """
+    :param pybel.manager.Manager manager:
+    :param pybel.BELGraph graph:
+    :param dict[str,iter[str]] annotations:
+    """
+    for keyword, names in annotations.items():
+        url = n()
+        graph.annotation_url[keyword] = url
+
+        annotation = Annotation(keyword=keyword, url=url)
+        manager.session.add(annotation)
+        manager.annotation_model[url] = annotation
+
+        for name in names:
+            entry = manager.annotation_object_cache[url][entry.name] = AnnotationEntry(name=name, annotation=annotation)
+            manager.session.add(entry)
+
+        manager.session.commit()
