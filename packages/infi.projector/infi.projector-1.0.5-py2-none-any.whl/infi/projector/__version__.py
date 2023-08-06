0ddbff45e962c2fdbdaae58a2c82748534d41620
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:55:50 +0000

    Add package infi.projector/1.0.5/infi.projector-1.0.5-py2-none-any.whl

---
+++ packages/infi.projector/infi.projector-1.0.5-py2-none-any.whl/infi/projector/__version__.py
@@ -0,0 +1,11 @@
+__version__ = "1.0.5"
+__git_commiter_name__ = "Arnon Yaari"
+__git_commiter_email__ = "arnony@infinidat.com"
+__git_branch__ = u'(HEAD detached at v1.0.5)'
+__git_remote_tracking_branch__ = '(No remote tracking)'
+__git_remote_url__ = '(Not remote tracking)'
+__git_head_hash__ = 'bb78d4fa787a42def37234582f30fc4eb122a35f'
+__git_head_subject__ = u'Finished Release v1.0.5'
+__git_head_message__ = u''
+__git_dirty_diff__ = u''
+__git_commit_date__ = '2018-06-10 12:25:11'
