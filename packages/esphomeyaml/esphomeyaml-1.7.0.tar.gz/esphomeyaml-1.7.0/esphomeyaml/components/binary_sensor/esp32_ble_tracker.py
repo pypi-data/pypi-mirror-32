70165284d9fb4b3f392e3578f062997a159f7d78
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:36:06 +0000

    Add package esphomeyaml/1.7.0/esphomeyaml-1.7.0.tar.gz

---
+++ packages/esphomeyaml/esphomeyaml-1.7.0.tar.gz/esphomeyaml-1.7.0/esphomeyaml/components/binary_sensor/esp32_ble_tracker.py
@@ -0,0 +1,30 @@
+import voluptuous as vol
+
+import esphomeyaml.config_validation as cv
+from esphomeyaml.components import binary_sensor
+from esphomeyaml.components.esp32_ble_tracker import ESP32BLETracker
+from esphomeyaml.const import CONF_MAC_ADDRESS, CONF_NAME, ESP_PLATFORM_ESP32
+from esphomeyaml.core import HexInt
+from esphomeyaml.helpers import ArrayInitializer, get_variable
+
+ESP_PLATFORMS = [ESP_PLATFORM_ESP32]
+DEPENDENCIES = ['esp32_ble_tracker']
+
+CONF_ESP32_BLE_ID = 'esp32_ble_id'
+
+PLATFORM_SCHEMA = cv.nameable(binary_sensor.BINARY_SENSOR_PLATFORM_SCHEMA.extend({
+    vol.Required(CONF_MAC_ADDRESS): cv.mac_address,
+    cv.GenerateID(CONF_ESP32_BLE_ID): cv.use_variable_id(ESP32BLETracker)
+}))
+
+
+def to_code(config):
+    hub = None
+    for hub in get_variable(config[CONF_ESP32_BLE_ID]):
+        yield
+    addr = [HexInt(i) for i in config[CONF_MAC_ADDRESS].parts]
+    rhs = hub.make_device(config[CONF_NAME], ArrayInitializer(*addr, multiline=False))
+    binary_sensor.register_binary_sensor(rhs, config)
+
+
+BUILD_FLAGS = '-DUSE_ESP32_BLE_TRACKER'
