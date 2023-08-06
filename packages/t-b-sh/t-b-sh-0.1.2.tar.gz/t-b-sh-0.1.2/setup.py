49454932e73ee20a91432dd453e58a8a0e4813a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:16:50 +0000

    Add package t-b-sh/0.1.2/t-b-sh-0.1.2.tar.gz

---
+++ packages/t-b-sh/t-b-sh-0.1.2.tar.gz/t-b-sh-0.1.2/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+ 
+setup(
+    name='t-b-sh',    # This is the name of your PyPI-package.
+    version='0.1.2',                          # Update the version number for new releases
+    scripts=['t-b-sh'],                  # The name of your scipt, and also the command you'll be using for calling it    
+    author="Sriharsha Sammeta",
+    author_email="sriharsha4444@icloud.com",
+    install_requires=[
+   'python>=3.6'
+    ]
+)
