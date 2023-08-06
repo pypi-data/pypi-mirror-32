fb0d294de7d41511a5580dee8be72e2b349be3ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:12:56 +0000

    Add package yxspkg_encrypt/1.2.1/yxspkg_encrypt-1.2.1.tar.gz

---
+++ packages/yxspkg_encrypt/yxspkg_encrypt-1.2.1.tar.gz/yxspkg_encrypt-1.2.1/setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+from setuptools import setup 
+import yxspkg_encrypt
+setup(name='yxspkg_encrypt',   
+      version=yxspkg_encrypt.__version__,    
+      description='A simple API to encrypt data. Encrypt algorithm is based on linear congruence',    
+      author=yxspkg_encrypt.__author__,    
+      install_requires=['rsa'],
+      py_modules=['yxspkg_encrypt'],
+      platforms='any',
+      author_email='blacksong@yeah.net',      
+      entry_points={
+        'console_scripts': ['encrypt=yxspkg_encrypt:main'],
+    },
+      url='https://github.com/blacksong',
+      classifiers = [
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+)   
