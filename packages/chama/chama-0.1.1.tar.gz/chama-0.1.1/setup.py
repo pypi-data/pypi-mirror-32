8e3fc6d20eb342693dc4909173418fa041aa27d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:08:40 +0000

    Add package chama/0.1.1/chama-0.1.1.tar.gz

---
+++ packages/chama/chama-0.1.1.tar.gz/chama-0.1.1/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+from distutils.core import Extension
+
+DISTNAME = 'chama'
+VERSION = '0.1.1'
+PACKAGES = ['chama']
+EXTENSIONS = []
+DESCRIPTION = 'Sensor Placement Optimization.'
+LONG_DESCRIPTION = open('README.md').read()
+AUTHOR = 'Chama developers'
+MAINTAINER_EMAIL = 'kaklise@sandia.gov'
+LICENSE = 'Revised BSD'
+URL = 'https://github.com/sandialabs/chama'
+
+setuptools_kwargs = {
+    'zip_safe': False,
+    'install_requires': [],
+    'scripts': [],
+    'include_package_data': True
+}
+
+setup(name=DISTNAME,
+      version=VERSION,
+      packages=PACKAGES,
+      ext_modules=EXTENSIONS,
+      description=DESCRIPTION,
+      long_description=LONG_DESCRIPTION,
+      author=AUTHOR,
+      maintainer_email=MAINTAINER_EMAIL,
+      license=LICENSE,
+      url=URL,
+      **setuptools_kwargs)
