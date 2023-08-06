5698a4ee04476a39e0782ea78e6f581c24233ba9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:13:00 +0000

    Add package pqmarkup/0.6/pqmarkup-0.6.tar.gz

---
+++ packages/pqmarkup/pqmarkup-0.6.tar.gz/pqmarkup-0.6/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+setup(
+    name = 'pqmarkup',
+    py_modules = ['pqmarkup'],
+    version = '0.6',
+    description = 'A Python implementation of pq markup to HTML converter.',
+    author = 'Alexander Tretyak',
+    author_email = 'pqmarkup@gmail.com',
+    url = 'http://pqmarkup.org',
+    download_url = 'https://bitbucket.org/pqmarkup/pqmarkup/get/default.zip',
+    license = "BSD",
+    classifiers = [
+        'Development Status :: 4 - Beta',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: BSD License',
+        'Topic :: Text Processing :: Markup :: HTML',
+    ],
+)
