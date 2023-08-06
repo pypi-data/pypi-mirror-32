a45b16f6beca807671d57bcfbc47764ad6a016ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:56:46 +0000

    Add package target-datadotworld/1.0.1/target_datadotworld-1.0.1-py2.py3-none-any.whl

---
+++ packages/target-datadotworld/target_datadotworld-1.0.1-py2.py3-none-any.whl/target_datadotworld/api_client.py
@@ -0,0 +1,388 @@
+# target-datadotworld
+# Copyright 2017 data.world, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the
+# License.
+#
+# You may obtain a copy of the License at
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied. See the License for the specific language governing
+# permissions and limitations under the License.
+#
+# This product includes software developed at
+# data.world, Inc.(http://data.world/).
+import functools
+import gzip
+from concurrent.futures import ThreadPoolExecutor
+from time import sleep
+
+import backoff
+import requests
+from requests.adapters import HTTPAdapter, BaseAdapter
+from requests.exceptions import RequestException
+from singer import metrics
+from target_datadotworld import logger
+from target_datadotworld.exceptions import convert_requests_exception
+from target_datadotworld.utils import to_chunks, to_jsonlines, to_table_name
+
+MAX_TRIES = 10  # necessary to configure backoff decorator
+
+
+class ApiClient(object):
+    def __init__(self, api_token, **kwargs):
+        """Simple client for data.world API
+
+        :param api_token: API Authorization Token
+        :type api_token: str
+        """
+        from target_datadotworld import __version__
+
+        # The following properties can be overwritten for testing/tuning
+        self._api_url = kwargs.get('api_url', 'https://api.data.world/v0')
+        self._conn_timeout = kwargs.get('connect_timeout', 3.05)
+        self._read_timeout = kwargs.get('read_timeout', 600)
+        self._max_threads = kwargs.get('max_threads', 10)
+
+        self._session = requests.Session()
+        default_headers = {
+            'Accept': 'application/json',
+            'Authorization': 'Bearer {}'.format(api_token),
+            'Content-Type': 'application/json',
+            'User-Agent': 'target-datadotworld - {}'.format(__version__)
+        }
+        self._session.headers.update(default_headers)
+
+        # TODO Fix and turn GzipAdapter back on (GH Issue #10)
+        self._session.mount(self._api_url,
+                            BackoffAdapter(HTTPAdapter()))
+
+        # Create a limited thread pool.
+        self._executor = ThreadPoolExecutor(
+            max_workers=self._max_threads
+        )
+
+    def connection_check(self):
+        """Verify network connectivity
+
+        Ensures that the client can communicate with data.world's API
+        """
+        with metrics.http_request_timer('user'):
+            try:
+                self._session.get(
+                    '{}/user'.format(self._api_url),
+                    timeout=(self._conn_timeout, self._read_timeout)
+                ).raise_for_status()
+            except RequestException as e:
+                raise convert_requests_exception(e)
+
+    def append_stream(self, owner, dataset, stream, records):
+        """Append records to a stream in a data.world dataset
+
+        :param owner: User or organization ID of the owner of the dataset
+        :type owner: str
+        :param dataset: Dataset ID
+        :type dataset: str
+        :param stream: Stream ID
+        :type stream: str
+        :param records: Objects to be appended to the stream
+        :type records: iterable
+
+        :raises ApiError: Failure invoking data.world API
+        """
+        with metrics.http_request_timer('append') as t:
+            t.tags['stream'] = stream
+
+            try:
+                self._session.post(
+                    '{}/streams/{}/{}/{}'.format(
+                        self._api_url, owner, dataset, stream),
+                    data=to_jsonlines(records).encode('utf-8'),
+                    headers={'Content-Type':
+                             'application/json-l; charset=utf-8'}
+                ).raise_for_status()
+            except RequestException as e:
+                raise convert_requests_exception(e)
+
+    async def append_stream_chunked(
+            self, owner, dataset, stream, queue, chunk_size, loop):
+        """Asynchronously append records to a stream in a data.world dataset
+
+        :param owner: User or organization ID of the owner of the dataset
+        :type owner: str
+        :param dataset: Dataset ID
+        :type dataset: str
+        :param stream: Stream ID
+        :type stream: str
+        :param queue: Queue with objects to be appended to the stream
+        :type queue: asyncio.Queue
+        :param chunk_size: Chunk or batch size
+        :type chunk_size: int
+
+        :raises ApiError: Failure invoking data.world API
+        """
+        with metrics.Counter(
+                'batch_count', tags={'stream': stream}) as counter:
+
+            delayed_exception = None
+            # noinspection PyTypeChecker
+            pending_task = None
+            async for chunk in to_chunks(queue, chunk_size):
+                if delayed_exception is None:
+                    try:
+                        logger.info('Uploading {} records in batch #{} '
+                                    'from {} stream '.format(
+                                        len(chunk), counter.value, stream))
+
+                        if pending_task is not None:
+                            # Sequentially processes chunks of the same stream
+                            await pending_task
+
+                        # Call API on separate thread
+                        # Parallel processes different streams
+                        pending_task = loop.run_in_executor(
+                            self._executor,
+                            functools.partial(self.append_stream,
+                                              owner, dataset, stream, chunk)
+                        )
+                        counter.increment()
+                    except Exception as e:
+                        delayed_exception = e
+                else:
+                    pass  # Must exhaust queue
+
+            if pending_task is not None:
+                await pending_task
+
+            if delayed_exception is not None:
+                raise delayed_exception
+
+    def create_dataset(self, owner, dataset, **kwargs):
+        """Create a new dataset
+
+        :param owner: User or organization ID of the owner of the dataset
+        :type owner: str
+        :param dataset: Dataset ID
+        :type dataset: str
+        :param kwargs: Dataset properties
+        :type kwargs: dict
+
+        :returns: Response object
+        :rtype: object
+
+        :raises ApiError: Failure invoking data.world API
+
+        .. seealso:: `Dataset properties
+            <https://apidocs.data.world/v0/models/datasetcreaterequest>`_
+        """
+        with metrics.http_request_timer('create_dataset'):
+            try:
+                resp = self._session.put(
+                    '{}/datasets/{}/{}'.format(self._api_url, owner, dataset),
+                    json=kwargs,
+                    timeout=(self._conn_timeout, self._read_timeout)
+                )
+                resp.raise_for_status()
+                return resp.json()
+            except RequestException as e:
+                raise convert_requests_exception(e)
+
+    def get_dataset(self, owner, dataset):
+        """Fetch dataset info
+
+        :param owner: User or organization ID of the owner of the dataset
+        :type owner: str
+        :param dataset: Dataset ID
+        :type dataset: str
+
+        :returns: Dataset object
+        :rtype: object
+
+        :raises ApiError: Failure invoking data.world API
+        """
+        with metrics.http_request_timer('dataset'):
+            try:
+                resp = self._session.get(
+                    '{}/datasets/{}/{}'.format(self._api_url, owner, dataset),
+                    timeout=(self._conn_timeout, self._read_timeout)
+                )
+                resp.raise_for_status()
+                return resp.json()
+            except RequestException as e:
+                raise convert_requests_exception(e)
+
+    def get_current_version(self, owner, dataset, stream):
+        """Returns version of a sample record from a given stream
+
+        :param owner: User or organization ID of the owner of the dataset
+        :type owner: str
+        :param dataset: Dataset ID
+        :type dataset: str
+        :param stream: Stream ID
+        :type stream: str
+
+        :returns: Response object
+        :rtype: object
+
+        :raises ApiError: Failure invoking data.world API
+        """
+        with metrics.http_request_timer('fetch_latest_version'):
+            try:
+                resp = self._session.get(
+                    '{}/sql/{}/{}'.format(self._api_url, owner, dataset),
+                    params={
+                        'query': 'SELECT * '
+                                 'FROM `{}`.`{}`.`{}` '
+                                 'LIMIT 1'.format(
+                                     owner, dataset, to_table_name(stream))},
+                    timeout=(self._conn_timeout, self._read_timeout))
+                resp.raise_for_status()
+                rows = resp.json()
+                return (None if len(rows) == 0
+                        else rows[0].get('singer_version'))
+            except RequestException as e:
+                if e.response.status_code == 400:
+                    logger.warn('Unable fetch latest version. '
+                                'Expected if table doesn\'t exist yet. '
+                                'Server message: {}'.format(e.response.text))
+                    return None
+                raise convert_requests_exception(e)
+
+    def set_stream_schema(self, owner, dataset, stream, **kwargs):
+        """Sets schema of a stream in a data.world dataset
+
+        :param owner: User or organization ID of the owner of the dataset
+        :type owner: str
+        :param dataset: Dataset ID
+        :type dataset: str
+        :param stream: Stream ID
+        :type stream: str
+        :param kwargs: Schema properties (primaryKeyFields, sequenceField,
+        updateMethod)
+        :type kwargs: dict
+
+        :returns: Response object
+        :rtype: object
+
+        :raises ApiError: Failure invoking data.world API
+        """
+        with metrics.http_request_timer('set_stream_schema'):
+            try:
+                resp = self._session.patch(
+                    '{}/streams/{}/{}/{}/schema'.format(self._api_url, owner,
+                                                        dataset, stream),
+                    json=kwargs,
+                    timeout=(self._conn_timeout, self._read_timeout)
+                )
+                resp.raise_for_status()
+                return resp.json()
+            except RequestException as e:
+                raise convert_requests_exception(e)
+
+    def sync(self, owner, dataset):
+        """Triggers ingest of streamed records
+
+        :param owner: User or organization ID of the owner of the dataset
+        :type owner: str
+        :param dataset: Dataset ID
+        :type dataset: str
+
+        :returns: Response object
+        :rtype: object
+
+        :raises ApiError: Failure invoking data.world API
+        """
+        with metrics.http_request_timer('sync'):
+            try:
+                resp = self._session.post(
+                    '{}/datasets/{}/{}/sync'.format(
+                        self._api_url, owner, dataset),
+                    timeout=(self._conn_timeout, self._read_timeout))
+                resp.raise_for_status()
+                return resp.json()
+            except RequestException as e:
+                raise convert_requests_exception(e)
+
+    def truncate_stream_records(self, owner, dataset, stream):
+        """Truncates records of a stream in a data.world dataset
+
+        :param owner: User or organization ID of the owner of the dataset
+        :type owner: str
+        :param dataset: Dataset ID
+        :type dataset: str
+        :param stream: Stream ID
+        :type stream: str
+
+        :returns: Response object
+        :rtype: object
+
+        :raises ApiError: Failure invoking data.world API
+        """
+        with metrics.http_request_timer('truncate_stream_records'):
+            try:
+                resp = self._session.delete(
+                    '{}/streams/{}/{}/{}/records'.format(self._api_url, owner,
+                                                         dataset, stream),
+                    timeout=(self._conn_timeout, self._read_timeout)
+                )
+                resp.raise_for_status()
+                return resp.json()
+            except RequestException as e:
+                raise convert_requests_exception(e)
+
+
+# TODO Re-enable test coverage (GH issue #10)
+class GzipAdapter(BaseAdapter):  # pragma: no cover
+    def __init__(self, delegate):
+        """Requests adapter for compressing request bodies
+
+        :param delegate: Adapter to delegate final request processing to
+        :type delegate: requests.adapters.BaseAdapter
+        """
+        self._delegate = delegate
+        super(GzipAdapter, self).__init__()
+
+    def send(self, request, stream=False, **kwargs):
+        if request.body is not None:
+            if stream is True:
+                request.body = gzip.GzipFile(filename=request.url,
+                                             fileobj=request.body)
+                #  TODO Confirm that requests will set Content-Length correctly
+            else:
+                request.body = gzip.compress(request.body)
+                request.headers['Content-Length'] = len(request.body)
+
+            request.headers['Content-Encoding'] = 'gzip'
+        return self._delegate.send(request, stream=stream, **kwargs)
+
+    def close(self):
+        self._delegate.close()
+
+
+class BackoffAdapter(BaseAdapter):
+    def __init__(self, delegate):
+        """Requests adapter for retrying throttled requests (HTTP 429)
+
+        :param delegate: Adapter to delegate final request processing to
+        :type delegate: requests.adapters.BaseAdapter
+        """
+        self._delegate = delegate
+        super(BackoffAdapter, self).__init__()
+
+    @backoff.on_predicate(backoff.expo,
+                          predicate=lambda r: r.status_code == 429,
+                          max_tries=lambda: MAX_TRIES)
+    def send(self, request, **kwargs):
+        resp = self._delegate.send(request, **kwargs)
+        if (resp.status_code == 429 and
+                resp.headers.get('Retry-After')):
+            sleep(int(resp.headers.get('Retry-After')))
+
+        return resp
+
+    def close(self):
+        self._delegate.close()
---
+++ packages/target-datadotworld/target_datadotworld-1.0.1-py2.py3-none-any.whl/target_datadotworld/exceptions.py
@@ -0,0 +1,214 @@
+# target-datadotworld
+# Copyright 2017 data.world, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the
+# License.
+#
+# You may obtain a copy of the License at
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied. See the License for the specific language governing
+# permissions and limitations under the License.
+#
+# This product includes software developed at
+# data.world, Inc.(http://data.world/).
+
+import requests.exceptions as rqex
+
+
+def convert_requests_exception(req_exception):
+    """Convert common HTTP errors to the appropriate ApiError sub-type"""
+    wrappers = {
+        401: UnauthorizedError,
+        403: ForbiddenError,
+        404: NotFoundError,
+        429: TooManyRequestsError
+    }
+
+    req = req_exception.request
+    if (isinstance(req_exception, rqex.HTTPError) and
+            req_exception.response is not None):
+        resp = req_exception.response
+        wrapper = wrappers.get(resp.status_code, ApiError)
+        return wrapper(request=req, response=resp)
+    elif isinstance(req_exception, rqex.ConnectionError):
+        return ConnectionError(req_exception.request)
+    else:
+        return req_exception
+
+
+class Error(Exception):
+    """Base class for all custom exceptions"""
+
+    def __init__(self, message):
+        self.message = message
+
+    def __str__(self):
+        return '{}: {}'.format(
+            self.__class__.__name__, self.message)
+
+    def __repr__(self):
+        return '{}(message=\'{}\')'.format(
+            self.__class__.__name__, self.message)
+
+
+class ConfigError(Error):
+    """Target configuration error
+
+    Used to indicate that the provided config file fails to satisfy this
+    target's requirements
+    """
+
+    def __init__(self, cause=None):
+        super(ConfigError, self).__init__(
+            'Invalid configuration (Cause: {})'.format(cause or 'unspecified'))
+
+
+class TokenError(ConfigError):
+    """Token configuration error
+
+    Used to indicate that the token provided in the configuration file is
+    invalid (i.e. not a valid JWT)
+    """
+
+    def __init__(self):
+        super(TokenError, self).__init__('Invalid API token')
+
+
+class MissingSchemaError(Error):
+    """Missing schema error
+
+    Used to indicate that the tap emitted a RECORD message before emitting
+    a SCHEMA message for the respective stream
+    """
+
+    def __init__(self, stream):
+        super(MissingSchemaError, self).__init__(
+            'Found record for stream {} before corresponding '
+            'schema'.format(stream))
+
+
+class UnparseableMessageError(Error):
+    """Unparseable message error
+
+    Used to indicate that the tap emitted a message that cannot be parsed
+    """
+
+    def __init__(self, message, cause):
+        super(UnparseableMessageError, self).__init__(
+            'Unable to parse message {} (Cause: {})'.format(message, cause))
+
+
+class InvalidRecordError(Error):
+    """Invalid record error
+
+    Used to indicate that the tap emitted a message whose payload fail to
+    satisfy the required JSON schema
+    """
+
+    def __init__(self, stream, cause):
+        super(InvalidRecordError, self).__init__(
+            'Found invalid record for stream {} (Cause: {})'.format(stream,
+                                                                    cause))
+
+
+class InvalidDatasetStateError(Error):
+    """Invalid dataset state error
+
+    Used to indicate that the target dataset is not in a LOADED state, which
+    means that it might still be processing data transferred by the target
+    the last time it ran.
+    """
+
+    def __init__(self, owner, dataset):
+        super(InvalidDatasetStateError, self).__init__(
+            'Dataset ({}/{}) is not in LOADED state. '
+            'Try again once done processing previously uploaded data.'.format(
+                owner, dataset))
+
+
+class ApiError(Error):
+    """Base class for all API exceptions"""
+
+    def __init__(self, request, response,
+                 cause='API Error', solution='Check server message'):
+        try:
+            server_message = response.json().get('message', 'unspecified')
+        except (ValueError, AttributeError):
+            server_message = 'unspecified'
+
+        message = 'Error invoking {}: {}. {}. Server message: {}'.format(
+            request.url if hasattr(request, 'url') else 'unspecified',
+            cause, solution, server_message
+        )
+        super(ApiError, self).__init__(message)
+
+
+class ConnectionError(ApiError):
+    """Connection error
+
+    Used to indicate that the target is unable to connect to data.world's API
+    """
+
+    def __init__(self, request,
+                 cause='Unable to connect',
+                 solution='Check network connection'):
+        super(ConnectionError, self).__init__(
+            request, None, cause, solution)
+
+
+class UnauthorizedError(ApiError):
+    """Unauthorized error
+
+    Used to indicate that the server returned an HTTP 401 error
+    """
+
+    def __init__(self, request, response,
+                 cause='Unauthorized',
+                 solution='Check your API token'):
+        super(UnauthorizedError, self).__init__(
+            request, response, cause, solution)
+
+
+class ForbiddenError(ApiError):
+    """Forbidden error
+
+    Used to indicate that the server returned an HTTP 403 error
+    """
+
+    def __init__(
+            self, request, response,
+            cause='Access denied',
+            solution='Check permissions to access resource (e.g. dataset)'):
+        super(ForbiddenError, self).__init__(
+            request, response, cause, solution)
+
+
+class NotFoundError(ApiError):
+    """Not Found error
+
+    Used to indicate that the server returned an HTTP 404 error
+    """
+
+    def __init__(self, request, response,
+                 cause='Resource (e.g. dataset) does not exist',
+                 solution='Check IDs in URL'):
+        super(NotFoundError, self).__init__(
+            request, response, cause, solution)
+
+
+class TooManyRequestsError(ApiError):
+    """Too Many Requests error
+
+    Used to indicate that the server returned an HTTP 429 error
+    """
+
+    def __init__(self, request, response,
+                 cause='Too many requests',
+                 solution='Make less API requests less frequently'):
+        super(TooManyRequestsError, self).__init__(
+            request, response, cause, solution)
