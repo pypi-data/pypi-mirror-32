caa694135228970473e4570a39ccd63a28b8ac07
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:34:49 +0000

    Add package dsdobjects/0.6.2/dsdobjects-0.6.2.tar.gz

---
+++ packages/dsdobjects/dsdobjects-0.6.2.tar.gz/dsdobjects-0.6.2/dsdobjects/parser/__init__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+#
+# dsdobjects.parser
+#
+# Written by Stefan Badelt (badelt@caltech.edu)
+#
+# Distributed under the MIT License, use at your own risk.
+#
+
+from pyparsing import ParseException
+from pil_kernel_format import parse_kernel_file, parse_kernel_string
+from pil_extended_format import parse_pil_file, parse_pil_string, PilFormatError
+
