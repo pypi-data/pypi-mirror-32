cb59859a13d22ad1f62f2c45345f4e5accbbd05e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:02:20 +0000

    Add package autocnn-helper/0.0.1/autocnn-helper-0.0.1.tar.gz

---
+++ packages/autocnn-helper/autocnn-helper-0.0.1.tar.gz/autocnn-helper-0.0.1/autocnn_helper/__init__.py
@@ -0,0 +1,14 @@
+from autocnn_helper.config import (
+    get_cluster_def,
+    get_parameter,
+    get_task_info,
+    get_job_info,
+    get_outputs_path,
+    get_data_path,
+    get_tf_config,
+    get_log_level,
+    get_api,
+    get_user_token,
+    send_metrics
+)
+# from autocnn_helper.deploy import Deploy as deploy
