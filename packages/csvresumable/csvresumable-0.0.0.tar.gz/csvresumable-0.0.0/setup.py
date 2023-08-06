f8b20e7f6636f8d733088ce2b9eaa3fa9856a9cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:35:20 +0000

    Add package csvresumable/0.0.0/csvresumable-0.0.0.tar.gz

---
+++ packages/csvresumable/csvresumable-0.0.0.tar.gz/csvresumable-0.0.0/setup.py
@@ -0,0 +1,49 @@
+# -*- coding:utf-8 -*-
+
+import os
+import sys
+
+from setuptools import setup, find_packages
+here = os.path.abspath(os.path.dirname(__file__))
+try:
+    with open(os.path.join(here, 'README.rst')) as f:
+        README = f.read()
+    with open(os.path.join(here, 'CHANGES.txt')) as f:
+        CHANGES = f.read()
+except IOError:
+    README = CHANGES = ''
+
+install_requires = []
+
+docs_extras = ["kamidana"]
+
+tests_require = []
+
+testing_extras = tests_require + []
+
+setup(
+    name='csvresumable',
+    version='0.0.0',
+    description='adding tiny resume function, for csv reading iterator',
+    long_description=README + '\n\n' + CHANGES,
+    classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Python :: Implementation :: CPython",
+    ],
+    keywords='csv, resume',
+    author="podhmo",
+    author_email="ababjam61+pypi@gmail.com",
+    url="https://github.com/podhmo/cssresumable",
+    packages=find_packages(exclude=["csvresumable.tests"]),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=install_requires,
+    extras_require={
+        'testing': testing_extras,
+        'docs': docs_extras,
+    },
+    tests_require=tests_require,
+    test_suite="csvresumable.tests",
+    entry_points="""
+"""
+)
