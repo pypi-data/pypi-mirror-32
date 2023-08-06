38df007b0f87b6e7af317f4d7bb4c29689d17ff1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:35:45 +0000

    Add package filebrowser-safe/0.5.0/filebrowser_safe-0.5.0.tar.gz

---
+++ packages/filebrowser-safe/filebrowser_safe-0.5.0.tar.gz/filebrowser_safe-0.5.0/setup.py
@@ -0,0 +1,25 @@
+
+from setuptools import setup, find_packages
+
+setup(
+    name="filebrowser_safe",
+    version="0.5.0",
+    description="A snapshot of the filebrowser_3 branch of django-filebrowser, "
+                "packaged as a dependency for the Mezzanine CMS for Django.",
+    long_description=open("README.rst").read(),
+    author="Patrick Kranzlmueller, Axel Swoboda (vonautomatisch)",
+    author_email="werkstaetten@vonautomatisch.at",
+    maintainer="Stephen McDonald",
+    maintainer_email="stephen.mc@gmail.com",
+    url="http://github.com/stephenmcd/filebrowser-safe",
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+    ],
+)
