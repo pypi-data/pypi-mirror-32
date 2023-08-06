5b512df50e95f7708f3c3a2aa8cd0b815c87d9c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:38:40 +0000

    Add package very-good-setuptools-git-version/1.0.5/very-good-setuptools-git-version-1.0.5.tar.gz

---
+++ packages/very-good-setuptools-git-version/very-good-setuptools-git-version-1.0.5.tar.gz/very-good-setuptools-git-version-1.0.5/setup.py
@@ -0,0 +1,41 @@
+from setuptools import setup
+
+
+def README():
+    """Return the contents of the README file for this project."""
+    with open('README.md') as README_file:
+        return README_file.read()
+
+
+setup(
+    name='very-good-setuptools-git-version',
+    version='1.0.5',
+    url='https://github.com/Kautenja/very-good-setuptools-git-version',
+    author='Chritian Kauten',
+    author_email='kautencreations@gmail.com',
+    description='Automatically set package version from Git.',
+    long_description=README(),
+    long_description_content_type='text/markdown',
+    keywords='setuptools git version-control',
+    license='MIT',
+    classifiers=[
+        'Framework :: Setuptools Plugin',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Development Status :: 5 - Production/Stable',
+    ],
+    py_modules=['very_good_setuptools_git_version'],
+    install_requires=[
+        'setuptools >= 8.0',
+    ],
+    entry_points={
+        'distutils.setup_keywords': [
+            'version_format = very_good_setuptools_git_version:validate_version_format'
+        ],
+        'console_scripts': [
+            'very-good-setuptools-git-version = very_good_setuptools_git_version:get_version'
+        ]
+    }
+)
