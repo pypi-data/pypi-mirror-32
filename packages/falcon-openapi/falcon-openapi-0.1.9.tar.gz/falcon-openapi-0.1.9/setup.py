a67f603a7071bb28fb6492da5a5902fb2041b2b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:11:15 +0000

    Add package falcon-openapi/0.1.9/falcon-openapi-0.1.9.tar.gz

---
+++ packages/falcon-openapi/falcon-openapi-0.1.9.tar.gz/falcon-openapi-0.1.9/setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+with open('README.md') as f:
+    readme = f.read()
+
+setup(
+    name='falcon-openapi',
+    python_requires='>3.5.0',
+    version='0.1.9',
+    description='Falcon router to map openapi spec to resources',
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    author='Sam Kleiner',
+    author_email='sam@skleiner.com',
+    license='MIT',
+    url='https://github.com/StoicPerlman/falcon-openapi/',
+    download_url = 'https://github.com/StoicPerlman/falcon-openapi/archive/0.1.9.tar.gz',
+    keywords = ['falcon', 'openapi', 'api'],
+    packages=['falcon_openapi'],
+    install_requires=[
+        'falcon',
+        'pyyaml'
+    ],
+    extras_require={
+        'dev': [
+            'unittest'
+        ]
+    }
+)
