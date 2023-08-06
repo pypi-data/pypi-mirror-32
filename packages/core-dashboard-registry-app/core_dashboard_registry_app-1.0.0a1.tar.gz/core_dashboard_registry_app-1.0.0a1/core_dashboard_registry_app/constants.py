5087c82882c6ee017b54539ac6686e86e6d70288
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:26:43 +0000

    Add package core-dashboard-registry-app/1.0.0a1/core_dashboard_registry_app-1.0.0a1.tar.gz

---
+++ packages/core-dashboard-registry-app/core_dashboard_registry_app-1.0.0a1.tar.gz/core_dashboard_registry_app-1.0.0a1/core_dashboard_registry_app/constants.py
@@ -0,0 +1,6 @@
+"""
+    Registry User Dashboard constants
+"""
+# Templates
+DASHBOARD_HOME_TEMPLATE = 'core_dashboard_registry_app/home.html'
+
---
+++ packages/core-dashboard-registry-app/core_dashboard_registry_app-1.0.0a1.tar.gz/core_dashboard_registry_app-1.0.0a1/core_dashboard_registry_app/views/common/views.py
@@ -0,0 +1,28 @@
+"""
+    Common views
+"""
+
+from core_main_app.utils.rendering import render
+from core_dashboard_registry_app import constants as dashboard_constants
+from core_dashboard_registry_app.settings import INSTALLED_APPS
+
+
+def home(request):
+    """ Home page.
+
+    Args:
+        request:
+
+    Returns:
+    """
+
+    context = {}
+    if 'core_curate_app' in INSTALLED_APPS:
+        context.update({'draft': True})
+
+    assets = {
+        "css": ['core_dashboard_registry_app/user/css/home.css'],
+    }
+
+    return render(request, dashboard_constants.DASHBOARD_HOME_TEMPLATE, context=context,
+                  assets=assets)
