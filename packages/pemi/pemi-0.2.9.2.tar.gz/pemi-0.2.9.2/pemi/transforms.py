bdd4f297f29945e2383117f3ea914763eb8ceecc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:38:38 +0000

    Add package pemi/0.2.9.2/pemi-0.2.9.2.tar.gz

---
+++ packages/pemi/pemi-0.2.9.2.tar.gz/pemi-0.2.9.2/pemi/transforms.py
@@ -0,0 +1,28 @@
+import pandas as pd
+
+# TODO: prefix, postfix, ifblank, blankif
+
+def validate_no_null(field):
+    def _validate(value):
+        if value == field.null:
+            raise ValueError("null is not allowed for field '{}'".format(field.name))
+        return value
+    return _validate
+
+def isblank(value):
+    return (
+        value is not False
+        and value != 0
+        and value != float(0)
+        and (value is None or pd.isnull(value) or not value)
+    )
+
+def concatenate(delimiter=''):
+    def _concatenate(row):
+        return delimiter.join(row)
+    return _concatenate
+
+def nvl(default=''):
+    def _nvl(row):
+        return next((v for v in row if not isblank(v)), default)
+    return _nvl
