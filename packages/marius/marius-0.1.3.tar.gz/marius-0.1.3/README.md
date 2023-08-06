6aefb072f5495344328f20e3749bf695338ccf33
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:09:07 +0000

    Add package marius/0.1.3/marius-0.1.3.tar.gz

---
+++ packages/marius/marius-0.1.3.tar.gz/marius-0.1.3/README.md
@@ -0,0 +1,42 @@
+## Marius
+--------
+Light weight job scheduling tool with more performance and scalability.<br/>
+Name come from [Gaius Marius](https://en.wikipedia.org/wiki/Gaius_Marius)
+
+### Installation
+
+```shell
+pip install marius
+```
+
+### Usage Example
+
+```python
+import time
+from marius import Task, TimeLine
+
+
+def func(num):
+    print('task-{0}'.format(num))
+
+
+if __name__ == '__main__':
+    tl = TimeLine()
+    now = time.time()
+    tl.add(Task(iter([now + i for i in [2, 3, 5]]), func, 3))
+    while tl.has_tasks():
+        tl.wait_next()
+        tl.run()
+    else:
+        print("all job run over")
+
+```
+
+### TODO
+1. ~~add automatic test support~~
+2. ~~add more helper function~~
+3. add doc
+
+### Welcome to contribute
+Feel free to open a issue or start a pull request. <br/>
+You are welcome.
---
+++ packages/marius/marius-0.1.3.tar.gz/marius-0.1.3/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+setup(
+    name='marius',
+    packages=['marius', 'marius.core', 'marius.helper'],
+    version='0.1.3',
+    url='https://github.com/Jack-Kingdom/marius.git',
+    description='Light weight job scheduling tool with more performance and scalability.',
+    long_description=open('README.md').read(),
+
+    author='Jack King',
+    author_email='email@qiaohong.org',
+    license='MIT',
+
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Natural Language :: English',
+    ],
+)
