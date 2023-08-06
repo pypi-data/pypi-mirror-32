c9f60cb488ef10c017b4fddf5fe6e5ab31830d81
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:55:50 +0000

    Add package infi.pypi-manager/0.4.43/infi.pypi_manager-0.4.43.tar.gz

---
+++ packages/infi.pypi-manager/infi.pypi_manager-0.4.43.tar.gz/infi.pypi_manager-0.4.43/src/infi/pypi_manager/__version__.py
@@ -0,0 +1,11 @@
+__version__ = "0.4.43"
+__git_commiter_name__ = "Arnon Yaari"
+__git_commiter_email__ = "arnony@infinidat.com"
+__git_branch__ = u'(HEAD detached at v0.4.43)'
+__git_remote_tracking_branch__ = '(No remote tracking)'
+__git_remote_url__ = '(Not remote tracking)'
+__git_head_hash__ = '2f23c2e76cb349b719361d3bef29dc59e8e1935d'
+__git_head_subject__ = u'Finished Release v0.4.43'
+__git_head_message__ = u''
+__git_dirty_diff__ = u''
+__git_commit_date__ = '2018-06-10 12:29:00'
