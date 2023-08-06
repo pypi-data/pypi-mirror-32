367b4f48a48de4dbc6fb767ba928d7a8403bc82a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:34 +0000

    Add package flowcraft/1.1.0.dev1/flowcraft-1.1.0.dev1.tar.gz

---
+++ packages/flowcraft/flowcraft-1.1.0.dev1.tar.gz/flowcraft-1.1.0.dev1/flowcraft/__init__.py
@@ -0,0 +1,8 @@
+
+__version__ = "1.1.0"
+__build__ = "31052018"
+__author__ = "Diogo N. Silva, Tiago F. Jesus, Ines Mendes, Bruno Ribeiro-Gonçalves"
+__copyright__ = "Diogo N. Silva"
+__license__ = "GPL3"
+__maintainer__ = "Diogo N. Silva"
+__email__ = "o.diogosilva@gmail.com"
