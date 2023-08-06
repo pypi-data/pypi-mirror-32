9402486ce0e245880fc20141d00271986b1252ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:56:21 +0000

    Add package ansible-run/1.5/ansible-run-1.5.tar.gz

---
+++ packages/ansible-run/ansible-run-1.5.tar.gz/ansible-run-1.5/setup.py
@@ -0,0 +1,20 @@
+
+from subprocess import check_call
+from setuptools import setup
+from setuptools.command.install import install
+import os
+cwd = os.path.dirname(os.path.realpath(__file__))
+
+
+class PostInstallCommand(install):
+    def run(self):
+        check_call("cp %s/ansible-run /usr/local/bin" % cwd, shell=True)
+        install.run(self)
+
+
+setup(
+    name = 'ansible-run', version = '1.5',
+    url = 'https://www/github.com/moshloop/ansible-run',
+    author = 'Moshe Immerman', author_email = 'firstname.surname@gmail.com',
+    cmdclass={'install': PostInstallCommand }
+    )
