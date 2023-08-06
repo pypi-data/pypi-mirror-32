b92323815e06d99c5880616894077c98f38f126b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:14:14 +0000

    Add package poetry/0.10.2/poetry-0.10.2-py2.py3-none-any.whl

---
+++ packages/poetry/poetry-0.10.2-py2.py3-none-any.whl/poetry/puzzle/exceptions.py
@@ -0,0 +1,9 @@
+class SolverProblemError(Exception):
+    def __init__(self, error):
+        self._error = error
+
+        super(SolverProblemError, self).__init__(str(error))
+
+    @property
+    def error(self):
+        return self._error
