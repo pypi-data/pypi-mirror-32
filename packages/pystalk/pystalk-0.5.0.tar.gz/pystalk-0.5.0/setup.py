12a27df5f0481392f7a5a0123584b00a6ae576f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:16:45 +0000

    Add package pystalk/0.5.0/pystalk-0.5.0.tar.gz

---
+++ packages/pystalk/pystalk-0.5.0.tar.gz/pystalk-0.5.0/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+
+
+install_requires = []
+with open('requirements.txt', 'r') as f:
+    for line in f:
+        install_requires.append(line.rstrip())
+
+
+setup(
+    name="pystalk",
+    version="0.5.0",
+    author="EasyPost",
+    author_email="oss@easypost.com",
+    url="https://github.com/easypost/pystalk",
+    description="Very simple beanstalkd client",
+    license="ISC",
+    install_requires=install_requires,
+    packages=find_packages(exclude=['tests']),
+    python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, <4',
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Intended Audience :: System Administrators",
+        "Operating System :: OS Independent",
+        "Topic :: Database",
+        "License :: OSI Approved :: ISC License (ISCL)",
+    ]
+)
