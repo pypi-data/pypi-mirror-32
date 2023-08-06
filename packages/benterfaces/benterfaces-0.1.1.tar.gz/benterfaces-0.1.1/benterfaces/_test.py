a0aba67c4506b05e07a456743945f3026894fc15
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:35:20 +0000

    Add package benterfaces/0.1.1/benterfaces-0.1.1.tar.gz

---
+++ packages/benterfaces/benterfaces-0.1.1.tar.gz/benterfaces-0.1.1/benterfaces/_test.py
@@ -0,0 +1,53 @@
+"""miscelaneous test helpers. This should only be used for benterface tests!"""
+from zope.interface import Interface, implementer
+
+
+class ITestPlugin(Interface):
+    """A test interface."""
+    def get_id(self):
+        """return an id string."""
+        pass
+
+
+class IPriorityTestPlugin(Interface):
+    """A test interface."""
+    def get_id(self):
+        """return an id string."""
+        pass
+
+
+class IRequirementTestPlugin(Interface):
+    """A test interface."""
+    def get_id(self):
+        """return an id string."""
+        pass
+
+
+class IUseSpecificTestPlugin(Interface):
+    """A test interface."""
+    def get_id(self):
+        """return an id string."""
+        pass
+
+
+class IDoNotImplement(Interface):
+    """An interface to test not implemented interfaces."""
+    def do_nothing(self):
+        """do nothing."""
+        pass
+
+
+@implementer(ITestPlugin)
+class TestPluginForExlusion(object):
+    """A test plugin for testing exclusion."""
+
+    def get_id(self):
+        return "test_plugin_for_exclusion"
+
+
+@implementer(ITestPlugin)
+class TestPluginForInclusion(object):
+    """A test plugin for testing inclusion."""
+
+    def get_id(self):
+        return "test_plugin_for_inclusion"
---
+++ packages/benterfaces/benterfaces-0.1.1.tar.gz/benterfaces-0.1.1/setup.py
@@ -0,0 +1,35 @@
+"""setup.py for benterfaces"""
+
+from setuptools import setup
+
+
+setup(
+    name="benterfaces",
+    version="0.1.1",
+    author="bennr01",
+    author_email="benjamin99.vogt@web.de",
+    description="interface utilities",
+    long_description=open("README.md").read(),
+    license="MIT",
+    keywords="interface interfaces utilities plugin plugins",
+    url="https://github.com/bennr01/benterfaces/",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Topic :: Utilities",
+        "Topic :: Software Development :: Quality Assurance",
+        "Topic :: Software Development :: Testing",
+        "Programming Language :: Python",
+        "Development Status :: 3 - Alpha",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        ],
+    packages=[
+        "benterfaces",
+        ],
+    install_requires=[
+        "zope.interface",
+        ],
+    )
