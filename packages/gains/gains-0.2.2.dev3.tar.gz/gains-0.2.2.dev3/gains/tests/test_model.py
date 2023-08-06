90b4a0635faa413cb81c1d993375e6e234553ab3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:28:02 +0000

    Add package gains/0.2.2.dev3/gains-0.2.2.dev3.tar.gz

---
+++ packages/gains/gains-0.2.2.dev3.tar.gz/gains-0.2.2.dev3/gains/tests/test_model.py
@@ -0,0 +1,15 @@
+from __future__ import absolute_import, division, print_function
+from gains.salt_generator import generate_solvent
+import unittest
+
+
+class GuessIonTests(unittest.TestCase):
+
+    def test_1_model(self):
+        target = [1000, 1000]
+        model_ID = ["cpt", "density"]
+        generate_solvent(target, model_ID, heavy_atom_limit=300, seed=123)
+
+
+if __name__ == '__main__':
+    unittest.main()
