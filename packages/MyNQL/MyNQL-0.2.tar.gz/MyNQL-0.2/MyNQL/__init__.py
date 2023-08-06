5e4c44e5ad1adff87271b1a84fdc07a4d401aa6f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:13:24 +0000

    Add package MyNQL/0.2/MyNQL-0.2.tar.gz

---
+++ packages/MyNQL/MyNQL-0.2.tar.gz/MyNQL-0.2/MyNQL/__init__.py
@@ -0,0 +1,16 @@
+"""
+MyNQL
+========
+MyNQL is a Python package that work as a Network Database
+it should be a Network Language in the future, for now its only a library
+
+Source::
+    https://github.com/livinter/MyNLQ
+Bug reports::
+    https://github.com/livinter/MyNLQ/issues
+
+"""
+import logging
+from logging import WARN, DEBUG, INFO
+from MyNQL.MyNQL import *
+
