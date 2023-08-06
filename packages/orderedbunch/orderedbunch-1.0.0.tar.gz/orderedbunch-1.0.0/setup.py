26d1ae644c0f95d73cd2fa50edd147daa6b2fc14
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:17:29 +0000

    Add package orderedbunch/1.0.0/orderedbunch-1.0.0.tar.gz

---
+++ packages/orderedbunch/orderedbunch-1.0.0.tar.gz/orderedbunch-1.0.0/setup.py
@@ -0,0 +1,21 @@
+#from distutils.core import setup
+from setuptools import setup, find_packages
+
+install_requires = [
+    'numpy'
+    ]
+
+# import __version__
+exec(open('orderedbunch/_version.py').read())
+
+setup(
+    name='orderedbunch',
+    version=__version__,
+    author='Nathan Longbotham',
+    author_email='longbotham@gmail.com',
+    packages=find_packages(),
+    description='Tab completable ordered dictionary.',
+    long_description=open('README.rst').read(),
+    install_requires=install_requires,
+    test_suite = 'orderedbunch.test'
+)
