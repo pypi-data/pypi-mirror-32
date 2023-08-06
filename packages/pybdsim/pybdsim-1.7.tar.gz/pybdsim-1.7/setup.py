21fe72fc439493076ca9e59706bb2a0daf74412d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:32 +0000

    Add package pybdsim/1.7/pybdsim-1.7.tar.gz

---
+++ packages/pybdsim/pybdsim-1.7.tar.gz/pybdsim-1.7/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pybdsim',
+    version='1.7',
+    packages=find_packages(exclude=["docs", "tests", "obsolete"]),
+    # Not sure how strict these need to be...
+    install_requires=["matplotlib",
+                      "numpy",
+                      "scipy",
+                      "fortranformat",
+                      "root-numpy",
+                      "pymadx",
+                      "pymad8",
+                      "pytransport"],
+    # Some version of python2.7
+    python_requires="==2.7.*",
+
+    author='JAI@RHUL',
+    author_email='laurie.nevay@rhul.ac.uk',
+    description=("Python utilities for the Monte Carlo"
+                 " Particle accelerator code BDSIM."),
+    license="GPL3",
+    url='https://bitbucket.org/jairhul/pybdsim/'
+)
