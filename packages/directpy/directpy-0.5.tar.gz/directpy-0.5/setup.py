7ed0c6a116d9d05cd25d429fcba001421f2143d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:10:43 +0000

    Add package directpy/0.5/directpy-0.5.tar.gz

---
+++ packages/directpy/directpy-0.5.tar.gz/directpy-0.5/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='directpy',
+    version='0.5',
+    description='A python library for interacting with DirecTV receivers',
+    author='Eric Walters',
+    author_email='sentry07@gmail.com',
+    url='http://github.com/sentry07/DirectPy',
+    download_url='https://github.com/sentry07/DirectPy/archive/master.tar.gz',
+    license='MIT',
+    py_modules=['DirectPy'])
