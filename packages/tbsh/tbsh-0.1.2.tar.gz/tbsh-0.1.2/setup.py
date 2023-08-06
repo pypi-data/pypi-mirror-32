586e8d95d70fd521946a7fbaa61f6bb7e49c5724
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:16:50 +0000

    Add package tbsh/0.1.2/tbsh-0.1.2.tar.gz

---
+++ packages/tbsh/tbsh-0.1.2.tar.gz/tbsh-0.1.2/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+ 
+setup(
+    name='tbsh',    # This is the name of your PyPI-package.
+    version='0.1.2',                          # Update the version number for new releases
+    scripts=['tbsh'],                  # The name of your scipt, and also the command you'll be using for calling it    
+    author="Sriharsha Sammeta",
+    author_email="sriharsha4444@icloud.com",
+    install_requires=[
+   'python>=3.6'
+    ]
+)
