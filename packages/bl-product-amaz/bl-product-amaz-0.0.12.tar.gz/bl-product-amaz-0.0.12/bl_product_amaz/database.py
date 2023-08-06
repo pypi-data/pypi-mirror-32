d3ce8a06860a64cc08c55d5ca7a3362cf8441789
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:08:37 +0000

    Add package bl-product-amaz/0.0.12/bl-product-amaz-0.0.12.tar.gz

---
+++ packages/bl-product-amaz/bl-product-amaz-0.0.12.tar.gz/bl-product-amaz-0.0.12/bl_product_amaz/database.py
@@ -0,0 +1,16 @@
+import os
+from pymongo import MongoClient
+
+
+DB_HOST = os.environ['DB_PRODUCT_AMZ_HOST']
+DB_PORT = os.environ['DB_PRODUCT_AMZ_PORT']
+DB_NAME = os.environ['DB_PRODUCT_AMZ_NAME']
+DB_USER = os.environ['DB_PRODUCT_AMZ_USER']
+DB_PASSWORD = os.environ['DB_PRODUCT_AMZ_PASSWORD']
+
+
+
+class DataBase(object):
+  def __init__(self):
+    self.client = MongoClient('mongodb://' + DB_USER + ':' + DB_PASSWORD + '@' + DB_HOST + ':' + DB_PORT)
+    self.db = self.client[DB_NAME]
