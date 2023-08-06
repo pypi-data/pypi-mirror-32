89427a5d051ba5c4ac087796a15f5ad004aecca3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:26:51 +0000

    Add package muchspace/0.3.1/muchspace-0.3.1.tar.gz

---
+++ packages/muchspace/muchspace-0.3.1.tar.gz/muchspace-0.3.1/HISTORY.rst
@@ -0,0 +1,18 @@
+=======
+History
+=======
+
+0.1.3 (09-06-2018)
+------------------
+
+* First release on PyPI.
+
+0.2.0 (10-06-2018)
+------------------
+
+* First Working release.
+
+0.3.1 (12-06-2018)
+------------------
+
+* First stable release with feature support.
---
+++ packages/muchspace/muchspace-0.3.1.tar.gz/muchspace-0.3.1/setup.py
@@ -0,0 +1,50 @@
+from setuptools import setup, find_packages
+
+with open('README.rst') as readme_file:
+    README = readme_file.read()
+
+with open('HISTORY.rst') as history_file:
+    HISTORY = history_file.read()
+
+setup(name = 'muchspace',
+      description = 'A CLI utility in python3 to calulate total disk space required for media links',
+      long_description=README + '\n\n' + HISTORY,
+      version = '0.3.1',
+      url = 'https://github.com/Hitoshirenu/muchspace',
+      download_url = 'https://github.com/Hitoshirenu/muchspace/archive/0.1.tar.gz',
+      author = 'abhiigatty',
+      author_email = 'abhiigatty@gmail.com',
+      license = 'MIT',
+      classifiers = [
+        'Development Status :: 3 - Alpha',
+        'Environment :: Console',   
+        'Intended Audience :: End Users/Desktop',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: MacOS',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3 :: Only',
+        'Topic :: System :: Logging',
+        'Topic :: System :: Filesystems',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Internet :: WWW/HTTP :: Indexing/Search',
+        'Topic :: Internet :: WWW/HTTP :: Site Management',
+        'Topic :: Internet :: WWW/HTTP :: Site Management :: Link Checking',
+        'Topic :: Utilities',
+      ],
+      keywords = ['disk-space', 'analyzer', 'cli', 'python3', 'logging', 'link checking', 'link health'],
+      maintainer = 'abhiigatty',
+      maintainer_email = 'abhiigatty@gmail.com',
+      packages=find_packages(),
+      install_requires = [
+          'fire',
+          'requests',
+          'more_itertools'],
+      entry_points = {
+          'console_scripts': [
+              'muchspace = muchspace.muchspace:main'
+          ]
+      }
+)
