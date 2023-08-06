bd61b53d138918845af6cb47c0a79258df04fca9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:55:01 +0000

    Add package PyDSWS/0.08/PyDSWS-0.08.tar.gz

---
+++ packages/PyDSWS/PyDSWS-0.08.tar.gz/PyDSWS-0.08/setup.py
@@ -0,0 +1,19 @@
+from distutils.core import setup
+
+setup(
+    name='PyDSWS',
+    version='0.08',
+    packages=['PyDSWS'],
+    url='https://github.com/hoenie-ams/PyDSWS',
+    license='MIT',
+    author='Joris Hoendervangers',
+    author_email='j.h.hoendervangers@gmail.com',
+    description='Python package for Thomson Reuters Datastream Webservices (DSWS)',
+    long_description='Python package for Thomson Reuters Datastream Webservices (DSWS)',
+    install_requires=[
+        'pandas',
+        'requests',
+      ],
+    python_requires='>=3'
+
+)
