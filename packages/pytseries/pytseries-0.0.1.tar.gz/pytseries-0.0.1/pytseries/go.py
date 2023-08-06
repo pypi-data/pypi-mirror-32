4c596ec5f5e6c1cf654c554c8ccb060be8c98348
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:25:28 +0000

    Add package pytseries/0.0.1/pytseries-0.0.1.tar.gz

---
+++ packages/pytseries/pytseries-0.0.1.tar.gz/pytseries-0.0.1/pytseries/go.py
@@ -0,0 +1,48 @@
+import os, glob, pandas, numpy
+import goenrich as go
+import matplotlib.pylab as plt
+
+
+
+
+raise NotImplementedError('An implementation of GO annotation is '
+                          'coming soon')
+
+
+class GO(object):
+    def __init__(self, features):
+        """
+
+        :param features: list
+            Gene symbols to be GOed
+        """
+        self.features = features
+        print(self.features)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
