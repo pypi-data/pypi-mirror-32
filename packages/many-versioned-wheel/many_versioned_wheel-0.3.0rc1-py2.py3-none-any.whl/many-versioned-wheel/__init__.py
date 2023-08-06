fcc072a77e90bc38f74dd4432e2407f10c0acf5a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:51:58 +0000

    Add package many-versioned-wheel/0.3.0rc1/many_versioned_wheel-0.3.0rc1-py2.py3-none-any.whl

---
+++ packages/many-versioned-wheel/many_versioned_wheel-0.3.0rc1-py2.py3-none-any.whl/many-versioned-wheel/__init__.py
@@ -0,0 +1,9 @@
+# behold, the amazing many-versioned-wheel in all its glory...
+
+# from __future__ import absolute_import, print_function
+
+from .version import __version__
+
+
+def print_version():
+    return __version__
---
+++ packages/many-versioned-wheel/many_versioned_wheel-0.3.0rc1-py2.py3-none-any.whl/many_versioned_wheel-0.3.0rc1.dist-info/DESCRIPTION.rst
@@ -0,0 +1,3 @@
+long_description: A amazing library for testing stuff dealing with pypi packages, or also determining the source of consciousness
+
+
