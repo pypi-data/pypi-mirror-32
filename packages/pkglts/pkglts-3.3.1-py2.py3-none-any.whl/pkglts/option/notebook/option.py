1e219ab44dc5310ccda48817caad05969ea6ba3b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:24:46 +0000

    Add package pkglts/3.3.1/pkglts-3.3.1-py2.py3-none-any.whl

---
+++ packages/pkglts/pkglts-3.3.1-py2.py3-none-any.whl/pkglts/option/notebook/option.py
@@ -0,0 +1,42 @@
+from os.path import dirname, exists
+
+from pkglts.dependency import Dependency
+from pkglts.option_object import Option
+
+from . import nbcompile
+
+
+class OptionNotebook(Option):
+    def root_dir(self):
+        return dirname(__file__)
+
+    def update_parameters(self, cfg):
+        sec = dict(
+            src_directory="example"
+        )
+        cfg['notebook'] = sec
+
+    def check(self, cfg):
+        invalids = []
+        src_directory = cfg['notebook']['src_directory']
+
+        if not exists(src_directory):
+            invalids.append("notebook.src_directory")
+
+        return invalids
+
+    def require(self, purpose, cfg):
+        del cfg
+
+        if purpose == 'option':
+            options = ['sphinx']
+            return [Dependency(name) for name in options]
+
+        # if purpose == 'dvlpt':
+        #     return [Dependency('nbconvert')]
+
+        return []
+
+    def tools(self, cfg):
+        del cfg
+        yield nbcompile.parser_nbcompile
