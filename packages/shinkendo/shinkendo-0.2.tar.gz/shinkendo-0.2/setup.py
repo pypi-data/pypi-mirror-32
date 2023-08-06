e6ea4fb594112c18054669e7057ccb9d1f030b49
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:25:44 +0000

    Add package shinkendo/0.2/shinkendo-0.2.tar.gz

---
+++ packages/shinkendo/shinkendo-0.2.tar.gz/shinkendo-0.2/setup.py
@@ -0,0 +1,28 @@
+import os
+
+from setuptools import setup, find_packages
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name='shinkendo',
+    version='0.2',
+    packages=find_packages(),
+    description='A Shinken CLI.',
+    author='Guillaume Subiron',
+    author_email="maethor+pip@subiron.org",
+    long_description=read('README.md'),
+    include_package_data=True,
+    url='http://github.com/sysnove/shinkendo',
+    install_requires=[
+        'Click',
+        'Requests',
+    ],
+    py_modules=['shinkendo'],
+    entry_points='''
+        [console_scripts]
+        shinkendo=shinkendo:cli
+    ''',
+    license="WTFPL",
+)
