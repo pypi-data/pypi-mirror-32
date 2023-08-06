dee99871accd7697ea36281e78f1ee4db39736aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:55:36 +0000

    Add package keras-utilities/0.5.0/keras-utilities-0.5.0.tar.gz

---
+++ packages/keras-utilities/keras-utilities-0.5.0.tar.gz/keras-utilities-0.5.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(name='keras-utilities',
+      version='0.5.0',
+      description='Utilities for Keras.',
+      url='https://github.com/cbaziotis/keras-utilities',
+      author='Christos Baziotis',
+      author_email='christos.baziotis@gmail.com',
+      license='MIT',
+      packages=find_packages(exclude=['docs', 'tests*']),
+      include_package_data=True
+      )
