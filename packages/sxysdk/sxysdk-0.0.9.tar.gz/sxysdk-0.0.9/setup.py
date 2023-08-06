310abc02d62a0d0c009ccefece67742d8ec72ea6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:33:36 +0000

    Add package sxysdk/0.0.9/sxysdk-0.0.9.tar.gz

---
+++ packages/sxysdk/sxysdk-0.0.9.tar.gz/sxysdk-0.0.9/setup.py
@@ -0,0 +1,17 @@
+# -*- coding:utf-8 -*-
+from setuptools import setup, find_packages
+
+setup(
+    name='sxysdk',
+    packages=find_packages(),
+    version="0.0.9",
+    description='sxy python sdk',
+    author="shenxiaoyang",
+    author_email="380944919@infocore.cn",
+    license="Apache License, Version 2.0",
+    url='https://github.com/shenxiaoyang/sxysdk-python.git',
+    download_url='https://github.com/shenxiaoyang/sxysdk-python.git',
+    keywords=['sdk'],
+    classifiers=[],
+    zip_safe=False,
+)
