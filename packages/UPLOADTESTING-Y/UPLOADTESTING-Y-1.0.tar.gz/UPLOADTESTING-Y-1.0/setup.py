d2f4dda3a65c767d9ccaf338ea503b74b6bf2943
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:15:24 +0000

    Add package UPLOADTESTING-Y/1.0/UPLOADTESTING-Y-1.0.tar.gz

---
+++ packages/UPLOADTESTING-Y/UPLOADTESTING-Y-1.0.tar.gz/UPLOADTESTING-Y-1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name = "UPLOADTESTING-Y",
+    version = '1.0',
+    py_modules = ['test'],
+    url = 'https://github.com/YeomJihoon/uploadtesting'
+)
+    
