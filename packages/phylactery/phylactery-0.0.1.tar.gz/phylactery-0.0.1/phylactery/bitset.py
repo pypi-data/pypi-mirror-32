f30eee4877b4c8af06a8111ca8ccfd680de2080f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:26 +0000

    Add package phylactery/0.0.1/phylactery-0.0.1.tar.gz

---
+++ packages/phylactery/phylactery-0.0.1.tar.gz/phylactery-0.0.1/phylactery/bitset.py
@@ -0,0 +1,85 @@
+# =============================================================================
+# Phylactery BitSet
+# =============================================================================
+#
+# A simple Python BitSet implementation relying on Numpy.
+#
+import math
+import numpy as np
+
+MODULO = 0x0000001f
+
+
+class BitSet(object):
+    """
+    The BitSet class.
+
+    Args:
+        capacity (number): Capacity of the bitset.
+
+    """
+
+    def __init__(self, capacity):
+        true_capacity = math.ceil(capacity / 32)
+
+        # Properties
+        self.capacity = capacity
+        self.integers = np.zeros(true_capacity, dtype=np.uint32)
+
+    def __len__(self):
+        return self.capacity
+
+    def get(self, index):
+
+        if index >= self.capacity:
+            raise IndexError
+
+        byte = index >> 5
+        pos = index & MODULO
+
+        return (self.integers[byte] >> pos) & 1
+
+    def has(self, index):
+        return self.get(index) == 1
+
+    def test(self, index):
+        return self.has(index)
+
+    def set(self, index, value=1):
+        if index >= self.capacity:
+            raise IndexError
+
+        byte = index >> 5
+        pos = index & MODULO
+
+        if not value:
+            self.integers[byte] &= ~(1 << pos)
+        else:
+            self.integers[byte] |= (1 << pos)
+
+    def reset(self, index):
+        if index >= self.capacity:
+            raise IndexError
+
+        byte = index >> 5
+        pos = index & MODULO
+
+        self.integers[byte] &= ~(1 << pos)
+
+    def add(self, index):
+        return self.set(index)
+
+    def __getitem__(self, index):
+        return self.get(index)
+
+    def __contains__(self, index):
+        return self.has(index)
+
+    def __setitem__(self, index, value):
+        return self.set(index, value)
+
+    def __delitem__(self, index):
+        return self.reset(index)
+
+    def __repr__(self):
+        return '<%s capacity=%i>' % (self.__class__.__name__, self.capacity)
