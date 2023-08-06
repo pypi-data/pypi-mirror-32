3e25e56fe8e0556008ea621f035fe88a9f78dd2e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:54 +0000

    Add package scylla_django_session_store/0.0.3/scylla_django_session_store-0.0.3.tar.gz

---
+++ packages/scylla_django_session_store/scylla_django_session_store-0.0.3.tar.gz/scylla_django_session_store-0.0.3/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name='scylla_django_session_store',
+    version='0.0.3',
+    packages=['tests', 'scylla_sessions', 'scylla_sessions.management',
+              'scylla_sessions.management.commands'],
+    url='https://github.com/mwaaas/scylla_django_session_store',
+    license='MIT',
+    install_requires=[
+        "cassandra_driver (>=3.14.0)",
+    ],
+    author='francismwangi',
+    author_email='francismwangi152@gmail.com',
+    description='Syclla/Cassandra django session store. '
+)
