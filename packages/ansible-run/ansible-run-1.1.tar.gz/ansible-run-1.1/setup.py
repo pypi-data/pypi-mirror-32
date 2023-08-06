5370a17608b915e22558008e3b1467e57b66c215
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:56:18 +0000

    Add package ansible-run/1.1/ansible-run-1.1.tar.gz

---
+++ packages/ansible-run/ansible-run-1.1.tar.gz/ansible-run-1.1/setup.py
@@ -0,0 +1,5 @@
+from distutils.core import setup
+
+setup(name='ansible-run', version='1.1', url='https://www/github.com/moshloop/ansible-run',
+    author='Moshe Immerman', author_email='firstname.surname@gmail.com',
+  data_files=[('/usr/local/bin/ansible-run', ['bin/ansible-run'])])
