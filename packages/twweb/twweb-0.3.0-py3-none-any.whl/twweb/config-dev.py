1696b6df2c8f98ef80777853e7540803600d2544
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:01:14 +0000

    Add package twweb/0.3.0/twweb-0.3.0-py3-none-any.whl

---
+++ packages/twweb/twweb-0.3.0-py3-none-any.whl/twweb/config-dev.py
@@ -0,0 +1,11 @@
+DB_ENGINE = 'sqlite' # engine name
+DB_HOST = 'twweb.sqlite' # host or path for sqlite. If empty for sqlite, represents :memory:
+DB_PORT = '' # port number
+DB_NAME = '' # database name
+DB_USER = '' # database username
+DB_PASSWORD = '' # database user's password
+
+SECRET_KEY = 'holy_hand_grenade'
+PIN = 'wersdf'
+
+TW_TASKRC = '' # path to existing alternate Taskwarrior's configuration file.
