2e386aeb04829385b533b86efb6bce0f2b7b08a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:14:47 +0000

    Add package fidding-cli/0.6/fidding_cli-0.6-py3-none-any.whl

---
+++ packages/fidding-cli/fidding_cli-0.6-py3-none-any.whl/python_cli/script/cli.py
@@ -0,0 +1,11 @@
+import click
+
+
+@click.command()
+def cli():
+    """Example script."""
+    file_object = open('data/data.txt', 'r')
+    for line in file_object:
+        click.echo(line)
+    file_object.close()
+    click.echo('Hello World!')
