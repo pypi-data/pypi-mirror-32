0275bb3154bed121c3b513d10035d0d4d3b116da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:26:40 +0000

    Add package eetools/0.0.11/eetools-0.0.11.tar.gz

---
+++ packages/eetools/eetools-0.0.11.tar.gz/eetools-0.0.11/eetools/str.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+import random, string
+def GenString(length=10):
+	numOfNum = random.randint(1,length-1)
+	numOfLetter = length - numOfNum
+	#选中numOfNum个数字
+	slcNum = [random.choice(string.digits) for i in range(numOfNum)]
+	#选中numOfLetter个字母
+	slcLetter = [random.choice(string.ascii_letters) for i in range(numOfLetter)]
+	#打乱这个组合
+	slcChar = slcNum + slcLetter
+	random.shuffle(slcChar)
+	#生成密码
+	genPwd = ''.join([i for i in slcChar])
+	return genPwd
