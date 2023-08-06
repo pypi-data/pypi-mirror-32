449dd0953019c4be2adc69c24ea311cfa13ef34c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:11:20 +0000

    Add package formulka-httpbin/0.0.1/formulka_httpbin-0.0.1.tar.gz

---
+++ packages/formulka-httpbin/formulka_httpbin-0.0.1.tar.gz/formulka_httpbin-0.0.1/formulka_httpbin/console.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2018 Petr Olah <djangoguru@gmail.com>
+#
+# this file is part of the project "Petr Olah HTTPBin Client" released under the "MIT" open-source license
+
+"""formulka-httpbin's command-line utility
+
+Ideally use a tool such as `click <http://click.pocoo.org/5/>`_
+
+For more information check py:func:`click.command` and py:class:`click.Command`
+"""
+
+from formulka_httpbin.client import HttpBinClient
+
+
+def entrypoint():
+    client = HttpBinClient()
+    print("your IP is: " + client.ip())
+    raise SystemExit(1)
+
+
+if __name__ == '__main__':
+    # this makes the script executable without needing to install
+    # the formulka-httpbin package
+    entrypoint()
