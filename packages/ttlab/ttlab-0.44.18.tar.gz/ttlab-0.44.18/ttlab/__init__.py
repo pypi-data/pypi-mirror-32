6de84a9b1a9061d5ec4a3f35c7e470fef424bf0c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:09:08 +0000

    Add package ttlab/0.44.18/ttlab-0.44.18.tar.gz

---
+++ packages/ttlab/ttlab-0.44.18.tar.gz/ttlab-0.44.18/ttlab/__init__.py
@@ -0,0 +1,6 @@
+from .xps import XPS, XPSFunctions
+from .X0_reactor import X0Reactor, X0Functions
+from .flow_reactor import FlowReactor
+from .mass_spectrometer import MassSpectrometer
+from .cary5000 import Cary5000, Cary5000Functions
+from .insplorion_light_spectroscopy import InsplorionLightSpectroscopy
