e69a7c83958a80c851ed9fcfc09cf4f25978811a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:00:04 +0000

    Add package andrew-wf-log/1.0.0/andrew_wf_log-1.0.0.tar.gz

---
+++ packages/andrew-wf-log/andrew_wf_log-1.0.0.tar.gz/andrew_wf_log-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name=               'andrew_wf_log',
+    version=            '1.0.0',
+    py_modules=         ['andrew_wf_logging'],
+    author=             'andrew_wf',
+    author_email=       'andrew_wf@sina.cn',
+    description=         'Loggint to file',
+)
