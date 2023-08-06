be0cd944f448af01feb30c2cd98b577d5f4e6665
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:00:00 +0000

    Add package adarnauth-esi/1.4.14/adarnauth-esi-1.4.14.tar.gz

---
+++ packages/adarnauth-esi/adarnauth-esi-1.4.14.tar.gz/adarnauth-esi-1.4.14/setup.py
@@ -0,0 +1,47 @@
+import os
+from setuptools import find_packages, setup
+from esi import __version__
+
+with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
+    README = readme.read()
+
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='adarnauth-esi',
+    version=__version__,
+    install_requires=[
+        'requests>=2.9.1,<3.0',
+        'requests_oauthlib>=0.8.0,<1.0',
+        'django>=1.10',
+        'bravado>=8.4.0,<9.3',
+        'celery>=4.0.2',
+    ],
+    packages=find_packages(),
+    include_package_data=True,
+    license='GNU General Public License v3 (GPLv3)',
+    description='Django app for accessing the EVE Swagger Interface.',
+    long_description=README,
+    url='https://github.com/adarnof/adarnauth-esi',
+    author='Adarnof',
+    author_email='adarnof@gmail.com',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Framework :: Django :: 1.10',
+        'Framework :: Django :: 1.11',
+        'Framework :: Django :: 2.0',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
+)
