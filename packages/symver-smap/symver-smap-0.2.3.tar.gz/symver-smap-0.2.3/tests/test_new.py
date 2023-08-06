2343368384bc402cdbddb982ca533cfe6b21fef2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:38:40 +0000

    Add package symver-smap/0.2.3/symver-smap-0.2.3.tar.gz

---
+++ packages/symver-smap/symver-smap-0.2.3.tar.gz/symver-smap-0.2.3/tests/test_new.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+"""Tests for new command"""
+
+from conftest import run_tc
+
+
+def test_new(testcases, datadir, capsys, caplog):
+    for tc in testcases:
+        run_tc(tc, datadir, capsys, caplog)
---
+++ packages/symver-smap/symver-smap-0.2.3.tar.gz/symver-smap-0.2.3/tests/test_update.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+"""Tests for update command"""
+
+from conftest import run_tc
+
+
+def test_update(testcases, datadir, capsys, caplog):
+    for tc in testcases:
+        run_tc(tc, datadir, capsys, caplog)
