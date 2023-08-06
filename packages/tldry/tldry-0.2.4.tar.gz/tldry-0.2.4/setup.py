b003d377d5233bde70a3b4a99da26eb590abc2e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:26 +0000

    Add package tldry/0.2.4/tldry-0.2.4.tar.gz

---
+++ packages/tldry/tldry-0.2.4.tar.gz/tldry-0.2.4/setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+
+
+requirements = [
+    'numpy>=1.14.3',
+    'PyStemmer==1.3.0',
+    'regex>=2018.2.21',
+]
+
+setup(
+    name='tldry',
+    version='0.2.4',
+    author='Artem Vang',
+    author_email='vangogius@gmail.com',
+    url='https://github.com/vangaa/tldry',
+    description='Automatic text summarizer based on textrank algorithm',
+    license='MIT License',
+    install_requires=requirements,
+    packages=find_packages('.', exclude='tests'),
+    classifiers=(
+        'Programming Language :: Python',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Programming Language :: Python :: Implementation :: CPython',
+    )
+)
