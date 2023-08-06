2d574fdb2536356f5af5f8ed64a1b1f77be3c3a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:12:46 +0000

    Add package sci-logging/0.2/sci-logging-0.2.tar.gz

---
+++ packages/sci-logging/sci-logging-0.2.tar.gz/sci-logging-0.2/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='sci-logging',
+    version='0.2',
+    packages=['scilogging',],
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    install_requires=[""],
+)
