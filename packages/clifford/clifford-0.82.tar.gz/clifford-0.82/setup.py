8b4c75d85bd3ceef0459f978d531900b731dce65
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:55:13 +0000

    Add package clifford/0.82/clifford-0.82.tar.gz

---
+++ packages/clifford/clifford-0.82.tar.gz/clifford-0.82/setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+#import ez_setup
+#ez_setup.use_setuptools()
+from setuptools import setup, find_packages
+from distutils.core import Extension
+
+VERSION = '0.82'
+LONG_DESCRIPTION = """
+A numerical geometric algebra module for python. BSD License.
+"""
+setup(name='clifford',
+	version=VERSION,
+	license='bsd',
+	description='Numerical Geometric Algebra Module',
+	long_description=LONG_DESCRIPTION,
+	author='Robert Kern',
+	author_email='alexarsenovic@gmail.com',
+	url='http://clifford.readthedocs.io',
+	packages=find_packages(),
+	install_requires = [
+		'numpy',
+		'numba',
+		'future',
+		],
+	package_dir={'clifford':'clifford'},
+	
+	)
+
