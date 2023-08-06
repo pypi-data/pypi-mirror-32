aa76902d369c557eb79694c00e9a7c244bb43d79
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:35:05 +0000

    Add package teslajsonpy2/0.1.24/teslajsonpy2-0.1.24.tar.gz

---
+++ packages/teslajsonpy2/teslajsonpy2-0.1.24.tar.gz/teslajsonpy2-0.1.24/README.rst
@@ -0,0 +1,8 @@
+teslajsonpy2
+===============
+
+Python module for Tesla API
+
+This is a work in progress.
+
+***** this library based on https://github.com/gglockner/teslajson code
---
+++ packages/teslajsonpy2/teslajsonpy2-0.1.24.tar.gz/teslajsonpy2-0.1.24/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+setup(
+    name='teslajsonpy2',
+    version='0.1.24',
+    packages=['teslajsonpy2'],
+    include_package_data=True,
+    python_requires='>=3',
+    license='WTFPL',
+    description='A library to work with Tesla API.',
+    long_description='A library to work with Tesla car API.',
+    url='https://github.com/rudical/teslajsonpy2',
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Topic :: Internet',
+    ],
+)
