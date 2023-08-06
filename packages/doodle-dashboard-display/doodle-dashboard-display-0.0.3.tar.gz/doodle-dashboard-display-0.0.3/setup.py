efde275eb25aebd1dfd0b9e8afa20a8a2f1f2269
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:37:01 +0000

    Add package doodle-dashboard-display/0.0.3/doodle-dashboard-display-0.0.3.tar.gz

---
+++ packages/doodle-dashboard-display/doodle-dashboard-display-0.0.3.tar.gz/doodle-dashboard-display-0.0.3/setup.py
@@ -0,0 +1,22 @@
+import os
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+about = {}
+with open(os.path.join(here, "sketchingdev", "__about__.py")) as f:
+    exec(f.read(), about)
+
+setup(
+    name=about['__name__'],
+    version=about['__version__'],
+    description="Console display for Doodle-Dashboard.",
+    url="https://github.com/SketchingDev/Doodle-Dashboard-Display",
+    license="MIT",
+    packages=["sketchingdev"],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Programming Language :: Python :: 3.4"
+    ],
+    python_requires=">=3.4"
+)
