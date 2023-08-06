47e9de30b28736288a46dd722506c976e364e193
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:25:56 +0000

    Add package tomso/0.0.7/tomso-0.0.7.tar.gz

---
+++ packages/tomso/tomso-0.0.7.tar.gz/tomso-0.0.7/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name = 'tomso',
+    packages = ['tomso'],
+    version = '0.0.7',
+    description = 'Tools for Modelling Stars and their Oscillations',
+    author = 'Warrick Ball',
+    author_email = 'wball@bison.ph.bham.ac.uk',
+    url = 'https://github.com/warrickball/tomso',
+    download_url = 'https://github.com/warrickball/tomso/archive/v0.0.7.tar.gz',
+    install_requires=['numpy'],
+    keywords = [],
+    classifiers = [],
+    license = 'MIT'
+    )
---
+++ packages/tomso/tomso-0.0.7.tar.gz/tomso-0.0.7/tomso/__init__.py
@@ -0,0 +1,10 @@
+__version__ = "0.0.7"
+
+__all__ = [
+    'adipls',
+    'common',
+    'gyre',
+    'io',
+    'mesa',
+    'stars'
+    ]
