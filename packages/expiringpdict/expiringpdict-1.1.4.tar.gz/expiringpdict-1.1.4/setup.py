2558e6b1bf272620c0f5ae4179af3f2c34911a57
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:17:18 +0000

    Add package expiringpdict/1.1.4/expiringpdict-1.1.4.tar.gz

---
+++ packages/expiringpdict/expiringpdict-1.1.4.tar.gz/expiringpdict-1.1.4/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+    name='expiringpdict',
+    packages=['expiringpdict'],
+    version='1.1.4',
+    description='A Python Expiring Dictionary',
+    author='Pi3rral',
+    author_email='pierre.leca@gmail.com',
+    url='https://github.com/Pi3rral/expiringpdict',
+    download_url='https://github.com/Pi3rral/expiringpdict/tarball/r1.1.3',
+    keywords=['python', 'expiring', 'dict'],
+    classifiers=[],
+)
