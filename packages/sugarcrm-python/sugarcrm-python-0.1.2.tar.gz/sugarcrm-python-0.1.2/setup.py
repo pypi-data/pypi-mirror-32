e3a434702d6722ff53c36602189e1eda405bbbe7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:25:01 +0000

    Add package sugarcrm-python/0.1.2/sugarcrm-python-0.1.2.tar.gz

---
+++ packages/sugarcrm-python/sugarcrm-python-0.1.2.tar.gz/sugarcrm-python-0.1.2/setup.py
@@ -0,0 +1,21 @@
+import os
+
+from setuptools import setup
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+setup(name='sugarcrm-python',
+      version='0.1.2',
+      description='API wrapper for SugarCRM written in Python',
+      url='https://github.com/GearPlug/sugarcrm-python',
+      author='Miguel Ferrer',
+      author_email='ingferrermiguel@gmail.com',
+      license='GPL',
+      packages=['sugarcrm'],
+      install_requires=[
+          'requests',
+      ],
+      zip_safe=False)
