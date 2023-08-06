d089535c12a93445e452a2c6c705ce0904668be2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:39:13 +0000

    Add package instruction-graph/0.2.21/instruction_graph-0.2.21-py2.py3-none-any.whl

---
+++ packages/instruction-graph/instruction_graph-0.2.21-py2.py3-none-any.whl/instruction_graph-0.2.21.data/scripts/run_itext_comm.py
@@ -0,0 +1,13 @@
+#!python
+from instruction_graph.interactive.InteractiveManager import InteractiveManager
+from instruction_graph.example.ExamplePrimitiveLibrary import ExamplePrimitiveLibrary
+from instruction_graph.example.DefaultMemory import DefaultMemory
+from instruction_graph.interactive.TextCommunicator import TextCommunicator
+
+
+if __name__ == "__main__":
+    library = ExamplePrimitiveLibrary()
+    memory = DefaultMemory()
+    im = InteractiveManager(library=library, memory=memory)
+    tc = TextCommunicator(im)
+    tc.run()
