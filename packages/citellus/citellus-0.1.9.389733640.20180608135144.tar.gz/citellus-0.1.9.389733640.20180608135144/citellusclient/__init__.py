382dfcd821fd0f6828ed988c0bfb6d284c6e2668
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:45 +0000

    Add package citellus/0.1.9.389733640.20180608135144/citellus-0.1.9.389733640.20180608135144.tar.gz

---
+++ packages/citellus/citellus-0.1.9.389733640.20180608135144.tar.gz/citellus-0.1.9.389733640.20180608135144/citellusclient/__init__.py
@@ -0,0 +1,3 @@
+#!/usr/bin/env python
+# encoding: utf-8
+# Modifications (2017, 2018) by Pablo Iranzo Gómez <Pablo.Iranzo@redhat.com>
---
+++ packages/citellus/citellus-0.1.9.389733640.20180608135144.tar.gz/citellus-0.1.9.389733640.20180608135144/magui.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# encoding: utf-8
+# Modifications (2017, 2018) by Pablo Iranzo Gómez <Pablo.Iranzo@redhat.com>
+# Modifications (2017) by Robin Černín <rcernin@redhat.com>
+import sys
+
+from maguiclient.magui import main
+
+if __name__ == "__main__":
+    sys.exit(main())
