3949bb1add7980a47e861bce3f42da8f5f2f16c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:14:51 +0000

    Add package jamaddr27/0.4.0/jamaddr27-0.4.0.tar.gz

---
+++ packages/jamaddr27/jamaddr27-0.4.0.tar.gz/jamaddr27-0.4.0/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='jamaddr27',
+    version='0.4.0',
+    packages=find_packages(),
+    url='https://github.com/talbiston/jamaddr27.git',
+    license='MIT',
+    author='Jamison Emilio & Todd Albiston',
+    author_email='foxtrot711@gmail.com',
+    description='IP Subnet package',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    classifiers=(
+        "Programming Language :: Python :: 2 ",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+)
