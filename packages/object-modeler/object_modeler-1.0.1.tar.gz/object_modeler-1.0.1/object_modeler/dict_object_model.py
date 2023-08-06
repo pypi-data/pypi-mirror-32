a567db7bca9816ae36691f33701c3c4d5a7eb2a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:24 +0000

    Add package object-modeler/1.0.1/object_modeler-1.0.1.tar.gz

---
+++ packages/object-modeler/object_modeler-1.0.1.tar.gz/object_modeler-1.0.1/object_modeler/dict_object_model.py
@@ -0,0 +1,38 @@
+from object_modeler.common import ObjectModelDictMetaclass, PrettyObjectModelDictMetaclass, BaseObjectModel, \
+    with_metaclass
+
+
+class _PrototypeDictObjectModel(BaseObjectModel):
+    """ Define a representation dict-based models """
+
+    def __repr__(self):
+        return str(self.to_dict())
+
+    def __name__(self):
+        return type(self).__name__
+
+    def to_dict(self, excluded_fields=None):
+        if excluded_fields is None:
+            return self.__dict__
+        return {k: self.__dict__[k] for k in self.__dict__ if k not in excluded_fields}
+
+
+class GenericDictObjectModel(with_metaclass(ObjectModelDictMetaclass, _PrototypeDictObjectModel)):
+    """ Standard model
+
+    all_fields - tuple of all fields names
+    optional_fields - tuple of optional (not required fields names)
+    default_values - dict of default values for fields (if value not received in init)
+    fields_types - dict of all fields types
+    """
+    all_fields = tuple()
+    optional_fields = tuple()
+    default_values = dict()
+    fields_types = dict()
+
+
+class ObjectModel(with_metaclass(PrettyObjectModelDictMetaclass, _PrototypeDictObjectModel)):
+    """ Pretty object model """
+
+    def __init__(self, data):
+        self.init_model(data)
---
+++ packages/object-modeler/object_modeler-1.0.1.tar.gz/object_modeler-1.0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    test_suite='test',
+    name='object_modeler',
+    version='1.0.1',
+    packages=['object_modeler'],
+    url='https://github.com/KeyStorke/ObjectModeler',
+    license='MIT',
+    author='Nickolay Ovdienko',
+    author_email='Nickolay.Ovdienko@emc.com',
+    description='Clearly and simply define objects schemes'
+)
