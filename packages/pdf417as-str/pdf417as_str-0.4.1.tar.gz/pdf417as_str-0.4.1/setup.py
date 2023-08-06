fa1d9cb1dbe50226485c07e5b3803a9c481589a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:57:07 +0000

    Add package pdf417as-str/0.4.1/pdf417as_str-0.4.1.tar.gz

---
+++ packages/pdf417as-str/pdf417as_str-0.4.1.tar.gz/pdf417as_str-0.4.1/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='pdf417as_str',
+    version='0.4.1',
+    packages=['pdf417as_str'],
+    url='https://github.com/ikvk/pdf417as_str',
+    license='LGPLv3',
+    long_description=open("README.rst").read(),
+    author='v.kaukin',
+    author_email='workkvk@gmail.com',
+    description='Create pdf417 barcode without using images.',
+)
