6946586feae12a31068899ded4bc315221b1ae6a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:22:52 +0000

    Add package nameko-cachetools/1.0.0/nameko_cachetools-1.0.0-py2.py3-none-any.whl

---
+++ packages/nameko-cachetools/nameko_cachetools-1.0.0-py2.py3-none-any.whl/nameko_cachetools/__init__.py
@@ -0,0 +1,6 @@
+from .nameko_cachetools import CachedRpcProxy, CacheFirstRpcProxy
+
+__author__ = 'Santiago Suarez Ordonez'
+__email__ = 'santiycr@gmail.com'
+__version__ = '1.0.0'
+__all__ = ['CachedRpcProxy', 'CacheFirstRpcProxy']
