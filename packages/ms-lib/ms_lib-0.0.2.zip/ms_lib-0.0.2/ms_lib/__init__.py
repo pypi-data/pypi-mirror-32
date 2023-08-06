3e86989022bff0719cc27ef22c950786018464aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:59:38 +0000

    Add package ms-lib/0.0.2/ms_lib-0.0.2.zip

---
+++ packages/ms-lib/ms_lib-0.0.2.zip/ms_lib-0.0.2/ms_lib/__init__.py
@@ -0,0 +1,8 @@
+
+from .storage_template import storage_template
+from .info_template import info_template
+from .credentials_template import credentials_template
+from .gateway_template import gateway_template
+
+from .backend_template import backend_template
+from .images_template import images_template
---
+++ packages/ms-lib/ms_lib-0.0.2.zip/ms_lib-0.0.2/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'ms_lib',
+  packages = ['ms_lib'], # this must be the same as the name above
+  version = '0.0.2',
+  description = 'This is the microservices library developed by eHealthz group.',
+  author = 'Jorge Sancho',
+  author_email = 'jslarraz@gmail.com',
+  url = 'https://github.com/jslarraz/ms_lib', # use the URL to the github repo
+  download_url = 'https://github.com/jslarraz/ms_lib/tarball/0.1',
+  keywords = ['microservices', 'development'],
+  classifiers = [],
+)
