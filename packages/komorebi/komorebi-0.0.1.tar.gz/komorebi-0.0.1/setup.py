d7efb618ab2a7aaaec9a09a79ba2a1b3f6d5ea2b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:23:40 +0000

    Add package komorebi/0.0.1/komorebi-0.0.1.tar.gz

---
+++ packages/komorebi/komorebi-0.0.1.tar.gz/komorebi-0.0.1/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+  name = 'komorebi',
+  packages = ['komorebi'],
+  version = '0.0.1',
+  description = 'Parallel Data API',
+  author = 'Liling Tan',
+  license = 'MIT',
+  package_data={'komorebi': ['data/kopitiam/*']},
+  url = 'https://github.com/alvations/komorebi',
+  keywords = [],
+  classifiers = [],
+)
