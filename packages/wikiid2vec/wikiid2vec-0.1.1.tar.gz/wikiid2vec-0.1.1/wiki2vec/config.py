83025e10ad3d0e087dd485ba1a961a48c792a9fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:08:22 +0000

    Add package wikiid2vec/0.1.1/wikiid2vec-0.1.1.tar.gz

---
+++ packages/wikiid2vec/wikiid2vec-0.1.1.tar.gz/wikiid2vec-0.1.1/wiki2vec/config.py
@@ -0,0 +1,3 @@
+WIKI_FILE_URL = 'https://dumps.wikimedia.org/idwiki/latest/idwiki-latest-pages-articles.xml.bz2'
+FILE_NAME_WIKI = 'wiki.id.non_case.text'
+FILE_NAME_OUTPUT_WIKI = 'w2vec_wiki_id_non_case'
