36f110561032ead5513a688a1e0e06030ad83931
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:22:05 +0000

    Add package mlconjug/1.0.1/mlconjug-1.0.1.tar.gz

---
+++ packages/mlconjug/mlconjug-1.0.1.tar.gz/mlconjug-1.0.1/setup.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""The setup script."""
+
+from setuptools import setup, find_packages
+
+with open('README.rst') as readme_file:
+    readme = readme_file.read()
+
+with open('HISTORY.rst') as history_file:
+    history = history_file.read()
+
+requirements = [
+    'Click>=6.0',
+    'numpy==1.14.3',
+    'scipy==1.1.0',
+    'scikit-learn==0.19.',
+]
+
+setup_requirements = [
+    'pytest-runner',
+    # TODO(SekouD): put setup requirements (distutils extensions, etc.) here
+]
+
+test_requirements = [
+    'pytest',
+    'Sphinx',
+    'docutils',
+    'pytest',
+    'pytest-cov',
+    'Click>=6.0',
+    'lxml',
+]
+
+setup(
+    name='mlconjug',
+    version='1.0.1',
+    description="A Python library to conjugate verbs using Machine Learning techiques.",
+    long_description=readme + '\n\n' + history,
+    author="SekouD",
+    author_email='sekoud.python@gmail.com',
+    url='https://github.com/SekouD/mlconjug',
+    packages=find_packages(include=['mlconjug']),
+    entry_points={
+        'console_scripts': [
+            'mlconjug=mlconjug.cli:main'
+        ]
+    },
+    include_package_data=True,
+    install_requires=requirements,
+    license="MIT license",
+    zip_safe=False,
+    keywords='mlconjug',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Intended Audience :: Developers',
+        'Intended Audience :: End Users/Desktop',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Natural Language :: French',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ],
+    test_suite='tests',
+    tests_require=test_requirements,
+    setup_requires=setup_requirements,
+)
---
+++ packages/mlconjug/mlconjug-1.0.1.tar.gz/mlconjug-1.0.1/tests/test_mlconjug.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Tests for `mlconjug` package."""
+
+import pytest
+import os
+
+from click.testing import CliRunner
+
+from collections import OrderedDict
+
+from mlconjug import mlconjug, PyVerbiste
+from mlconjug import cli
+
+
+conjug_aller = OrderedDict([
+    ('infinitive', OrderedDict([('infinitive-present', 'aller')])),
+    ('indicative', OrderedDict([
+        ('present', OrderedDict([('1s', 'vais'), ('2s', 'vas'), ('3s', 'va'),
+                                 ('1p', 'allons'), ('2p', 'allez'), ('3p', 'vont')])),
+        ('imperfect', OrderedDict([('1s', 'allais'), ('2s', 'allais'), ('3s', 'allait'),
+                                   ('1p', 'allions'), ('2p', 'alliez'), ('3p', 'allaient')])),
+        ('future', OrderedDict([('1s', 'irai'), ('2s', 'iras'), ('3s', 'ira'),
+                                ('1p', 'irons'), ('2p', 'irez'), ('3p', 'iront')])),
+        ('simple-past', OrderedDict([('1s', 'allai'), ('2s', 'allas'), ('3s', 'alla'),
+                                     ('1p', 'allâmes'), ('2p', 'allâtes'), ('3p', 'allèrent')]))])),
+    ('conditional', OrderedDict([
+        ('present', OrderedDict([('1s', 'irais'), ('2s', 'irais'), ('3s', 'irait'),
+                                 ('1p', 'irions'), ('2p', 'iriez'), ('3p', 'iraient')]))])),
+    ('subjunctive', OrderedDict([
+        ('present', OrderedDict([('1s', 'aille'), ('2s', 'ailles'), ('3s', 'aille'),
+                                 ('1p', 'allions'), ('2p', 'alliez'), ('3p', 'aillent')])),
+        ('imperfect', OrderedDict([('1s', 'allasse'), ('2s', 'allasses'), ('3s', 'allât'),
+                                   ('1p', 'allassions'), ('2p', 'allassiez'), ('3p', 'allassent')]))])),
+    ('imperative', OrderedDict([
+        ('imperative-present', OrderedDict([('2s :', 'va'), ('1p :', 'allons'), ('2p :', 'allez')]))])),
+    ('participle', OrderedDict([
+        ('present-participle', 'allant'),
+        ('past-participle', OrderedDict([('ms :', 'allé'), ('mp :', 'allés'),
+                                         ('fs :', 'allée'), ('fp :', 'allées')]))]))])
+
+class TestPyVerbiste:
+    directory = os.getcwd()
+    #directory = os.path.dirname(os.getcwd())
+    verbs_path = os.path.join(directory, 'mlconjug', 'data', 'verbs-fr.xml')
+    conjugations_path = os.path.join(directory, 'mlconjug', 'data', 'conjugation-fr.xml')
+    verbiste = PyVerbiste.Verbiste(verbs_path, conjugations_path)
+    def test_init_verbiste(self):
+        assert len(self.verbiste.templates) == len(self.verbiste.conjugations) == 149
+        assert self.verbiste.templates[0] == ':aller'
+        assert self.verbiste.templates[-1] == 'écri:re'
+        assert self.verbiste.conjugations[':aller'] == conjug_aller
+        assert len(self.verbiste.verbs) == 7015
+        assert self.verbiste.verbs['abaisser'] == {'template': 'aim:er', 'root': 'abaiss'}
+
+    def test_get_verb_info(self):
+        verb_info = self.verbiste._get_verb_info('aller')
+        assert verb_info == PyVerbiste.VerbInfo('aller', '', ':aller')
+        assert self.verbiste._get_verb_info('cacater') is None
+
+    def test_get_conjug_info(self):
+        conjug_info = self.verbiste._get_conjug_info(':aller')
+        assert conjug_info == self.verbiste.conjugations[':aller']
+        assert self.verbiste._get_conjug_info(':cacater') is None
+
+    def test_conjugate(self):
+        conjug = self.verbiste.conjugate('aller')
+        assert isinstance(conjug, PyVerbiste.Verb)
+        conjug = self.verbiste.conjugate('cacater')
+        assert conjug is None
+
+
+def test_command_line_interface():
+    """Test the CLI."""
+    runner = CliRunner()
+    result = runner.invoke(cli.main)
+    assert result.exit_code == 0
+    assert 'mlconjug.cli.main' in result.output
+    help_result = runner.invoke(cli.main, ['--help'])
+    assert help_result.exit_code == 0
+    assert '--help  Show this message and exit.' in help_result.output
