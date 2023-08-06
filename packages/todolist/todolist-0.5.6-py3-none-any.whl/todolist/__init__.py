7dcac714e46bfbf645c708f413d810f9641b5951
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:36:22 +0000

    Add package todolist/0.5.6/todolist-0.5.6-py3-none-any.whl

---
+++ packages/todolist/todolist-0.5.6-py3-none-any.whl/todolist/__init__.py
@@ -0,0 +1,11 @@
+__version__ = "0.5.6"
+__doc__ = (
+    "--version\n"
+    "--help\n"
+    "--add <what> <due> [ <category> <notes> <finished>]\n"
+    "--delete <id>\n"
+    "--list\n"
+    "\n\n"
+    "<License>\n"
+    "asciimatics: Apache License 2.0 (https://github.com/peterbrittain/asciimatics)"
+)
