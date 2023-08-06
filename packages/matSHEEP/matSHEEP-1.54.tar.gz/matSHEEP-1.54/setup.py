03d5b20eeaf356a7f620bbf72d95ac74eb933754
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:56:07 +0000

    Add package matSHEEP/1.54/matSHEEP-1.54.tar.gz

---
+++ packages/matSHEEP/matSHEEP-1.54.tar.gz/matSHEEP-1.54/setup.py
@@ -0,0 +1,21 @@
+from distutils.core import setup
+
+setup(
+    name='matSHEEP',
+    version='1.54',
+    author='Amartya Sanyal',
+    author_email='amartya18x@gmail.com',
+    packages=['matSHEEP'],
+    #scripts=['bin/stowe-towels.py', 'bin/wash-towels.py'],
+    # url='http://pypi.python.org/pypi/TowelStuff/',
+    license='LICENSE',
+    description='Programmatic interface to SHEEP',
+    long_description=open('README.txt').read(),
+    install_requires=[
+        "numpy",
+        "multiprocessing",
+        "networkx",
+        "tqdm",
+        "pillow"
+    ],
+)
