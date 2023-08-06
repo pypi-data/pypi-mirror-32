823125a65a99d771c5d08e59f581b3ebd234a582
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:37:55 +0000

    Add package yadisk/1.2.10/yadisk-1.2.10.tar.gz

---
+++ packages/yadisk/yadisk-1.2.10.tar.gz/yadisk-1.2.10/yadisk/functions/disk.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from ..api import DiskInfoRequest
+
+__all__ = ["get_disk_info"]
+
+def get_disk_info(session, *args, **kwargs):
+    """
+        Get disk information.
+
+        :param session: an instance of :any:`requests.Session` with prepared headers
+        :param fields: list of keys to be included in the response
+
+        :returns: :any:`DiskInfoObject`
+    """
+
+    request = DiskInfoRequest(session, *args, **kwargs)
+    request.send()
+
+    return request.process()
