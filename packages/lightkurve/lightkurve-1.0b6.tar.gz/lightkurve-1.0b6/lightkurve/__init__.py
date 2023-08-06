a15d016d0d9bb67cb9f9f792396ad9fb6b73c871
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:10 +0000

    Add package lightkurve/1.0b6/lightkurve-1.0b6.tar.gz

---
+++ packages/lightkurve/lightkurve-1.0b6.tar.gz/lightkurve-1.0b6/lightkurve/__init__.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import
+import os
+PACKAGEDIR = os.path.abspath(os.path.dirname(__file__))
+from .version import __version__
+from .prf import *
+from .lightcurve import *
+from .lightcurvefile import *
+from .correctors import *
+from .targetpixelfile import *
+from .utils import *
+from .convenience import *
