d15f726e77bacb5826c1253c98c7424fa8960927
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:56:07 +0000

    Add package pyleap/0.0.2/pyleap-0.0.2.tar.gz

---
+++ packages/pyleap/pyleap-0.0.2.tar.gz/pyleap-0.0.2/pyleap/event.py
@@ -0,0 +1,19 @@
+from pyleap.window import window
+from pyleap.mouse import mouse
+
+
+@window.event
+def on_mouse_motion(x, y, dx, dy):
+    mouse.x, mouse.y = x, y
+    mouse.move()
+
+
+@window.event
+def on_mouse_drag(x, y, dx, dy, buttons, modifiers):
+    mouse.x, mouse.y = x, y
+    mouse.move()
+
+
+@window.event
+def on_mouse_press(x, y, button, modifiers):
+    mouse.press()
