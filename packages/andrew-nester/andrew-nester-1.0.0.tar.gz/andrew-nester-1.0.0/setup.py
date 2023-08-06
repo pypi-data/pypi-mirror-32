ea2bfb1d345f83a8cf68ec4f61130232c423b3af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:14 +0000

    Add package andrew-nester/1.0.0/andrew-nester-1.0.0.tar.gz

---
+++ packages/andrew-nester/andrew-nester-1.0.0.tar.gz/andrew-nester-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name='andrew-nester',
+    version='1.0.0',
+    py_modules=['andrew-nester'],
+    author='hfpython',
+    author_email='29086846@qq.com',
+    url='http://www.baidu.com',
+    description='A simple printer of nested lists',
+)
