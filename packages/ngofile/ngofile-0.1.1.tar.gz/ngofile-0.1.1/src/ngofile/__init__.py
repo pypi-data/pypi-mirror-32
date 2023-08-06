a03a3fbab949f81b36d0ea03c819958be0b26b50
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:26:05 +0000

    Add package ngofile/0.1.1/ngofile-0.1.1.tar.gz

---
+++ packages/ngofile/ngofile-0.1.1.tar.gz/ngofile-0.1.1/src/ngofile/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import unicode_literals
+
+from . import _apipkg
+
+__author__ = """Cédric ROMAN"""
+__email__ = 'roman@numengo.com'
+__version__ = '0.1.1'
