938a3dfb80ace7cd383d8b31b471589ff165cd79
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:33:59 +0000

    Add package rgbnotes/1.0.1/rgbnotes-1.0.1-py2-none-any.whl

---
+++ packages/rgbnotes/rgbnotes-1.0.1-py2-none-any.whl/rgbnotes/__init__.py
@@ -0,0 +1,9 @@
+# Configuration variables
+
+# api_url = 'https://rgbnotes.com/api/v1'
+# api_version = None
+# client_key = None
+# client_id = None
+
+from rgbnotes.api import * # noqa
+from rgbnotes.api_config import * # noqa
