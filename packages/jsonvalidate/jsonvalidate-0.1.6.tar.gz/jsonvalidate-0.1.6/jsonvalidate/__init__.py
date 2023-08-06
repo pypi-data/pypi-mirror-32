cae1ffe077b9fbd70463080128d7340b2ec92387
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:49:24 +0000

    Add package jsonvalidate/0.1.6/jsonvalidate-0.1.6.tar.gz

---
+++ packages/jsonvalidate/jsonvalidate-0.1.6.tar.gz/jsonvalidate-0.1.6/jsonvalidate/__init__.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for jsonvalidate."""
+
+__author__ = """Robus Gauli"""
+__email__ = 'robusgauli@gmail.com'
+__version__ = '0.1.6'
+
+from .jsonvalidate import (
+    List,
+    Object,
+    NullContract,
+    EnumContract,
+    FloatContract,
+    RangeContract,
+    LengthContract,
+    StringContract,
+    IntegerContract,
+    BooleanContract,
+    KeyMissingContract,
+)
+
+
+__all__ = ['String', 'Integer', 'Float', 'Boolean', 'Object', 'List']
+
+
+class String(KeyMissingContract, NullContract, StringContract, LengthContract, EnumContract):
+    """Composition/Mixins for String"""
+    pass
+
+
+class Integer(KeyMissingContract, NullContract, IntegerContract, RangeContract, EnumContract):
+    """Composition/Mixins for Integer"""
+    pass
+
+
+class Float(KeyMissingContract, NullContract, FloatContract, RangeContract, EnumContract):
+    """Composition/Mixins for Float"""
+    pass
+
+
+class Boolean(KeyMissingContract, NullContract, BooleanContract):
+    """Composition/Mixins for Boolean"""
+    pass
