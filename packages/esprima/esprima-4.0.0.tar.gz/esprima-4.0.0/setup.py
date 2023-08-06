d65a8a771aae1bf4006ee6e3aabd32a64720e6e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:39:12 +0000

    Add package esprima/4.0.0/esprima-4.0.0.tar.gz

---
+++ packages/esprima/esprima-4.0.0.tar.gz/esprima-4.0.0/setup.py
@@ -0,0 +1,56 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+import os
+
+from esprima import version
+
+
+def read(fname):
+    try:
+        return open(os.path.join(os.path.dirname(__file__), fname)).read().strip()
+    except IOError:
+        return ''
+
+
+setup(
+    name='esprima',
+    version=version,
+    author='German M. Bravo (Kronuz)',
+    author_email='german.mb@gmail.com',
+    packages=[
+        'esprima',
+    ],
+    url='https://github.com/Kronuz/esprima-python',
+    license='BSD License',
+    keywords='esprima ecmascript javascript parser ast',
+    description="ECMAScript parsing infrastructure for multipurpose analysis in Python",
+    long_description=read('README'),
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Topic :: Software Development :: Code Generators",
+        "Topic :: Software Development :: Compilers",
+        "Topic :: Text Processing :: General",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+    ],
+    entry_points={
+        'console_scripts': [
+            'esprima = esprima.__main__:main',
+        ]
+    },
+)
