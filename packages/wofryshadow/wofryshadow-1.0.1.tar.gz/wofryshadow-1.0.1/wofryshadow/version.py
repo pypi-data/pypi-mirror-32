eb30d6713eb4f52f144d17aa349f3f16144c5a3d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:17:51 +0000

    Add package wofryshadow/1.0.1/wofryshadow-1.0.1.tar.gz

---
+++ packages/wofryshadow/wofryshadow-1.0.1.tar.gz/wofryshadow-1.0.1/wofryshadow/version.py
@@ -0,0 +1,11 @@
+
+# THIS FILE IS GENERATED FROM wofryshadow SETUP.PY
+short_version = '1.0.1'
+version = '1.0.1'
+full_version = '1.0.1.dev0+e907b96'
+git_revision = 'e907b969dfb64dad35cc440a59e6d7cf7fad6b9f'
+release = False
+
+if not release:
+    version = full_version
+    short_version += ".dev"
