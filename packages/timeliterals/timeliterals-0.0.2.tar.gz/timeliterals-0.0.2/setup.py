6aa55803f8cebc2ed13b872246e8640b7ff6419b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:56:03 +0000

    Add package timeliterals/0.0.2/timeliterals-0.0.2.tar.gz

---
+++ packages/timeliterals/timeliterals-0.0.2.tar.gz/timeliterals-0.0.2/setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+    name='timeliterals',
+    packages=[
+        'timeliterals',
+    ],
+    version='0.0.2',
+    author='pgdr',
+    author_email='pgdr@equinor.com',
+    description="Time literals",
+    test_suite="tests",
+)
