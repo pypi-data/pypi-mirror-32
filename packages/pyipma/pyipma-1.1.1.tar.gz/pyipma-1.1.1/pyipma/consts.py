6a007194b380682ed114e85e28fecc8f63dff4b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:32 +0000

    Add package pyipma/1.1.1/pyipma-1.1.1.tar.gz

---
+++ packages/pyipma/pyipma-1.1.1.tar.gz/pyipma-1.1.1/pyipma/consts.py
@@ -0,0 +1,5 @@
+API_DISTRITS = "http://api.ipma.pt/open-data/distrits-islands.json"
+API_FORECAST = "http://api.ipma.pt/open-data/forecast/meteorology/cities/daily/"
+API_OBSERVATION="http://pda.ipma.pt/observacao.jsp"
+API_XML_OBSERVATION="http://www.ipma.pt/resources.www/internal.user/pw_hh_pt.xml"
+
