846e852cd785b41fc76bc9ae95003cdc2484f102
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:56:34 +0000

    Add package ereuse-utils/0.3.0b1/ereuse-utils-0.3.0b1.tar.gz

---
+++ packages/ereuse-utils/ereuse-utils-0.3.0b1.tar.gz/ereuse-utils-0.3.0b1/ereuse_utils/__init__.py
@@ -0,0 +1,42 @@
+import locale
+from datetime import datetime, timedelta
+from distutils.version import StrictVersion
+from json import JSONEncoder as StandardJSONEncoder
+from uuid import UUID
+
+
+class JSONEncoder(StandardJSONEncoder):
+    """An overloaded JSON Encoder with extra type support."""
+
+    def default(self, obj):
+        if hasattr(obj, 'value'):  # an enumerated value
+            return obj.value
+        elif isinstance(obj, datetime):
+            return obj.isoformat()
+        elif isinstance(obj, timedelta):
+            return str(obj)
+        elif isinstance(obj, UUID):
+            return str(obj)
+        elif isinstance(obj, StrictVersion):
+            return str(obj)
+        return JSONEncoder.default(self, obj)
+
+
+def ensure_utf8(app_name_to_show_on_error: str):
+    """
+    Python3 uses by default the system set, but it expects it to be ‘utf-8’ to work correctly. This
+    can generate problems in reading and writing files and in ``.decode()`` method.
+    An example how to 'fix' it:
+
+    nano .bash_profile and add the following:
+    export LC_CTYPE=en_US.UTF-8
+    export LC_ALL=en_US.UTF-8
+    """
+    encoding = locale.getpreferredencoding()
+    if encoding.lower() != 'utf-8':
+        raise OSError('{} works only in UTF-8, but yours is set at {}'.format(app_name_to_show_on_error, encoding))
+
+
+def now() -> datetime:
+    """Returns a compatible 'now' with DeviceHub's API, this is as UTC and without microseconds."""
+    return datetime.utcnow().replace(microsecond=0)
