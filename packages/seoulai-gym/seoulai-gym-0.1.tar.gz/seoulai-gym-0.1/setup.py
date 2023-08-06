cc7cf261842d794a68d4c4b7d10238658ed147c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:02:20 +0000

    Add package seoulai-gym/0.1/seoulai-gym-0.1.tar.gz

---
+++ packages/seoulai-gym/seoulai-gym-0.1.tar.gz/seoulai-gym-0.1/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setup(name="seoulai-gym",
+      version="0.1",
+      description="The Seoul AI Gym: Seoul AI Gym is a toolkit for developing AI algorithms.",
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      url="https://github.com/seoulai/gym",
+      author="Seoul AI",
+      author_email="m.kersner@gmail.com",
+      license="The MIT License",
+      packages=["seoulai_gym"],
+      zip_safe=False,
+      install_requires=[
+          "pygame>=1.9.3",
+          "numpy>=1.14.2",
+          "pytest>=3.6.0",
+      ],
+      tests_require=["pytest"],
+      include_package_data=True,
+      keywords = ["gym", "ai", "machine-learning", "artificial-intelligence"],
+      )
