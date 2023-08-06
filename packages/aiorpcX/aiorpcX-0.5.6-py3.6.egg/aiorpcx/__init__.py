6346d1bfefcb57966d89e0f600f27d8b52452e8f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:07:52 +0000

    Add package aiorpcX/0.5.6/aiorpcX-0.5.6-py3.6.egg

---
+++ packages/aiorpcX/aiorpcX-0.5.6-py3.6.egg/aiorpcx/__init__.py
@@ -0,0 +1,16 @@
+from .framing import *
+from .jsonrpc import *
+from .rpc import *
+from .socks import *
+from .session import *
+from .util import *
+
+_version = (0, 5, 6)
+_version_str = '.'.join(str(part) for part in _version)
+
+__all__ = (framing.__all__ +
+           jsonrpc.__all__ +
+           rpc.__all__ +
+           socks.__all__ +
+           session.__all__ +
+           util.__all__)
