bc0063243ea6170a9284fe5a0bbf7683708ac3ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:27:47 +0000

    Add package cbthelper/0.1.1/cbthelper-0.1.1.tar.gz

---
+++ packages/cbthelper/cbthelper-0.1.1.tar.gz/cbthelper-0.1.1/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='cbthelper',
+    version='0.1.1',
+    author='Tim Hamilton',
+    author_email='timh@crossbrowsertesting.com',
+    description='a helper library for cross browser testing\'s selenium api',
+    long_description=long_description,
+    url='https://github.com/crossbrowsertesting/cbthelper-python',
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'fuzzywuzzy',
+    ],
+    classifiers=(
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ),
+)
