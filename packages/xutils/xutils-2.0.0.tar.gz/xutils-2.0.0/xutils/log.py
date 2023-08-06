21ef16900dd070b07487bb0d40d78b22bd9abd4a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:12:29 +0000

    Add package xutils/2.0.0/xutils-2.0.0.tar.gz

---
+++ packages/xutils/xutils-2.0.0.tar.gz/xutils-2.0.0/xutils/log.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+
+import logging
+
+from logging.handlers import TimedRotatingFileHandler
+
+
+def init(logger=None, level="INFO", file=None, handler_cls=None,
+         max_count=30, propagate=True, file_config=None, dict_config=None):
+    # Initialize the argument logger with the arguments, level and log_file.
+    if logger:
+        fmt = ("%(asctime)s - %(process)d - %(pathname)s - %(funcName)s - "
+               "%(lineno)d - %(levelname)s - %(message)s")
+        datefmt = "%Y-%m-%d %H:%M:%S"
+        formatter = logging.Formatter(fmt=fmt, datefmt=datefmt)
+
+        level = getattr(logging, level.upper())
+
+        if file:
+            if handler_cls:
+                handler = handler_cls(file, max_count)
+            else:
+                handler = TimedRotatingFileHandler(file, when="midnight",
+                                                   interval=1, backupCount=max_count)
+        else:
+            handler = logging.StreamHandler()
+        handler.setLevel(level)
+        handler.setFormatter(formatter)
+
+        loggers = logger if isinstance(logger, (list, tuple)) else [logger]
+        for logger in loggers:
+            logger.propagate = propagate
+            logger.setLevel(level)
+            logger.addHandler(handler)
+
+    # Initialize logging by the configuration file, file_config.
+    if file_config:
+        logging.config.fileConfig(file_config, disable_existing_loggers=False)
+
+    # Initialize logging by the dict configuration, dict_config.
+    if dict_config and hasattr(logging.config, "dictConfig"):
+        logging.config.dictConfig(dict_config)
