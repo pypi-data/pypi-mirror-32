b588a472dfa8f33769b17502d94e439501dea6dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:20 +0000

    Add package forkbuntu/0.2.10/forkbuntu-0.2.10.tar.gz

---
+++ packages/forkbuntu/forkbuntu-0.2.10.tar.gz/forkbuntu-0.2.10/forkbuntu/steps/unpack_iso.py
@@ -0,0 +1,29 @@
+from ..step import Step
+from munch import munchify
+from os import path
+
+class UnpackIso(Step):
+    messages = munchify({
+        'past': 'unpacked iso',
+        'present': 'unpacking iso'
+    })
+    requires = ['initialize']
+    root = True
+
+    def init(self):
+        c = self.app.conf
+        self.has_paths = [c.paths.output]
+        self.checksum_paths = [
+            c.paths.iso,
+            c.paths.src,
+            path.join(c.paths.cwd, 'config.yml'),
+            path.join(c.paths.cwd, 'extras'),
+            path.join(c.paths.cwd, 'filesystem'),
+            path.join(c.paths.cwd, 'initrd'),
+            path.join(c.paths.cwd, 'iso'),
+            path.join(c.paths.cwd, 'scripts'),
+        ]
+
+    def run(self):
+        s = self.app.services
+        s.iso.unpack()
