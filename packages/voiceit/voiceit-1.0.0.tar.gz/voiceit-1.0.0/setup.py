c4782bafad429b9bc331fabc56df15e20a3336d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:25:19 +0000

    Add package voiceit/1.0.0/voiceit-1.0.0.tar.gz

---
+++ packages/voiceit/voiceit-1.0.0.tar.gz/voiceit-1.0.0/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'voiceit',
+  packages = ['voiceit'], # this must be the same as the name above
+  version = '1.0.0',
+  description = 'VoiceIt API 1.0 Python Wrapper',
+  author = 'Armaan Bindra',
+  author_email = 'armaan@voiceit.io',
+  url = 'https://github.com/voiceittech/voiceit-python', # use the URL to the github repo
+  download_url = 'https://github.com/voiceittech/voiceit-python/archive/0.1.tar.gz', # I'll explain this in a second
+  keywords = ['biometrics', 'voice verification', 'voice biometrics'], # arbitrary keywords
+  classifiers = [],
+)
---
+++ packages/voiceit/voiceit-1.0.0.tar.gz/voiceit-1.0.0/voiceit/__init__.py
@@ -0,0 +1,2 @@
+name = "VoiceIt"
+from .VoiceIt import *
