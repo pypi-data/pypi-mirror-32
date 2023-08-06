a875dfe4553cb9c62a275badae6a6625bc7126de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:11:24 +0000

    Add package sciencedates/1.4.0/sciencedates-1.4.0-py3-none-any.whl

---
+++ packages/sciencedates/sciencedates-1.4.0-py3-none-any.whl/sciencedates-1.4.0.data/scripts/date2doy.py
@@ -0,0 +1,16 @@
+#!/home/thin/miniconda3/bin/python
+"""
+command-line utility to convert date to day of year
+"""
+from sciencedates import date2doy
+
+
+if __name__ == '__main__':
+    from argparse import ArgumentParser
+    p = ArgumentParser(description='convert date to day of year')
+    p.add_argument('date', help='yyyy-mm-dd')
+    P = p.parse_args()
+
+    doy, year = date2doy(P.date)
+
+    print(doy)
