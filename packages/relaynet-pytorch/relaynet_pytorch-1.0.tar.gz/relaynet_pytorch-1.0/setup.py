561423565e683fed1fe78aa8efb0b612c6e5fce9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:12:53 +0000

    Add package relaynet-pytorch/1.0/relaynet_pytorch-1.0.tar.gz

---
+++ packages/relaynet-pytorch/relaynet_pytorch-1.0.tar.gz/relaynet_pytorch-1.0/setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+setuptools.setup(name='relaynet_pytorch',
+      version='1.0',
+      description='Retinal Layer and Fluid Segmentation of Macular Optical Coherence Tomography using Fully Convolutional Networks',
+      url='https://github.com/abhi4ssj/relaynet_pytorch',
+      author='Abhijit Guha Roy',
+      author_email='abhi4ssj@gmail.com',
+      license='MIT',
+      packages=setuptools.find_packages(),
+      zip_safe=False)
