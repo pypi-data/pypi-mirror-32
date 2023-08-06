347a87dc7696bccdc3756cd1b7b2119cab2d4825
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:26:49 +0000

    Add package atomman/1.2.0/atomman-1.2.0.tar.gz

---
+++ packages/atomman/atomman-1.2.0.tar.gz/atomman-1.2.0/setup.py
@@ -0,0 +1,53 @@
+import os
+from setuptools import setup, find_packages
+from setuptools.extension import Extension
+
+# List cython extensions
+extensions = [Extension("atomman.core.cythonized",
+                       ["atomman/core/cythonized.c"])]
+
+def getversion():
+    """Fetches version information from VERSION file"""
+    with open(os.path.join('atomman', 'VERSION')) as version_file:
+        version = version_file.read().strip()
+    return version
+
+def getreadme():
+    """Fetches description from README.rst file"""
+    with open('README.rst') as f:
+        return f.read()
+
+setup(name = 'atomman',
+      version = getversion(),
+      description = 'Atomistic Manipulation Toolkit',
+      long_description = getreadme(),
+      ext_modules = extensions,
+      classifiers = [
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Science/Research',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Scientific/Engineering :: Physics'
+      ],
+      keywords = [
+        'atom', 
+        'atomic', 
+        'atomistic', 
+        'molecular dynamics'
+      ], 
+      url = 'https://github.com/usnistgov/atomman/',
+      author = 'Lucas Hale',
+      author_email = 'lucas.hale@nist.gov',
+      packages = find_packages(),
+      install_requires = [
+        'xmltodict',
+        'DataModelDict',
+        'numpy', 
+        'matplotlib',
+        'scipy',
+        'pandas',
+        'numericalunits'
+      ],
+      include_package_data = True,
+      zip_safe = False)
