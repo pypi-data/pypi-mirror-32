8884bd6b1d8236501555d856f0d8d4f8eee9b68d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:17:42 +0000

    Add package github-release-ci/0.0.4/github-release-ci-0.0.4.tar.gz

---
+++ packages/github-release-ci/github-release-ci-0.0.4.tar.gz/github-release-ci-0.0.4/github_release_ci/download.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Github Release asset download script."""
+
+from os import getcwd
+import click as cl
+
+from base import Release
+
+
+@cl.command()
+@cl.option("--out", "-o", default=getcwd(), help="Download destination path")
+@cl.argument("repo_slug")
+@cl.argument("release_slug")
+def main(out, repo_slug, release_slug):
+    """Entrypoint."""
+    release = Release(repo_slug, release_slug)
+    release.download_assets(out)
+
+
+if __name__ == '__main__':
+    main()
---
+++ packages/github-release-ci/github-release-ci-0.0.4.tar.gz/github-release-ci-0.0.4/github_release_ci/upload.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Upload script."""
+
+import click as cl
+
+from base import Release
+
+
+@cl.command()
+@cl.option(
+    '--destination-filename', '-d',
+    default=None,
+    help="The file name that should be the asset name at github."
+)
+@cl.argument("repo_slug")
+@cl.argument("release_slug")
+@cl.argument("file", type=cl.Path(exists=True))
+def main(destination_file_name, repo_slug, release_slug, file):
+    """Entrypoint."""
+    release = Release(repo_slug, release_slug)
+    release.upload_asset(file, destination_file_name)
+
+
+if __name__ == '__main__':
+    main()
