cd20ca3c03f8a6a9f1bb16d7ac257aed1db7a40e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:33:51 +0000

    Add package punk/3.0.0/punk-3.0.0.tar.gz

---
+++ packages/punk/punk-3.0.0.tar.gz/punk-3.0.0/README.rst
@@ -0,0 +1,18 @@
+PUNK - Primitives for Uncovering New Knwoledge
+===============================================
+
+Working on developing algorithms and pipelines to automate Machine Learning:
+
+- Data augmentation
+
+- Feature selection
+
+- Novelty detection
+
+D3M
+----
+For submition and usage with respect to the broader D3M community see `D3M <d3m>`_.
+
+Usage
+------
+For examples on how to use these primitives look at `examples <examples>`_.
---
+++ packages/punk/punk-3.0.0.tar.gz/punk-3.0.0/setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="punk",
+
+    version="3.0.0",
+
+    description="Primitives for Uncovering New Knowledge.",
+    long_description="Machine Learning pipeline elements.",
+
+    url="https://github.com/NewKnowledge/punk",
+
+    author="New Knowledge",
+    author_email="support@newknowledge.io",
+
+    license="MIT",
+
+    classifiers=[
+        "Development Status :: 4 - Beta",
+
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+
+        # Pick your license as you wish (should match "license" above)
+        "License :: OSI Approved :: MIT License",
+
+        "Programming Language :: Python :: 3.6",
+
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+    ],
+
+    keywords="TA1 primitive, feature selection, novelty detection",
+
+    packages=find_packages(exclude=['tests', 'd3m', 'examples']),
+
+    install_requires=["numpy", "scikit-learn", "scipy", "pandas", "python-dateutil"],
+)
