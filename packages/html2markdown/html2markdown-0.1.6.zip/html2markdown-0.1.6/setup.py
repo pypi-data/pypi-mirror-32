7e62898b99e448b012142bef9edb150d009bb6ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:00:38 +0000

    Add package html2markdown/0.1.6/html2markdown-0.1.6.zip

---
+++ packages/html2markdown/html2markdown-0.1.6.zip/html2markdown-0.1.6/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+	name='html2markdown',
+	py_modules=['html2markdown'],
+	version='0.1.6',
+	description='Conservatively convert html to markdown',
+	author='dlon',
+	author_email='dv.lnh.d@gmail.com',
+	url='https://github.com/dlon/html2markdown',
+	install_requires=[
+		'beautifulsoup4'
+	],
+)
