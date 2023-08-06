e8227c6b25662113c640e212e179bfc04bbd089f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:39:07 +0000

    Add package netdisco/1.5.0/netdisco-1.5.0.tar.gz

---
+++ packages/netdisco/netdisco-1.5.0.tar.gz/netdisco-1.5.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(name='netdisco',
+      version='1.5.0',
+      description='Discover devices on your local network',
+      url='https://github.com/home-assistant/netdisco',
+      author='Paulus Schoutsen',
+      author_email='Paulus@PaulusSchoutsen.nl',
+      license='Apache License 2.0',
+      install_requires=['requests>=2.0', 'zeroconf>=0.19.1'],
+      packages=find_packages(exclude=['tests', 'tests.*']),
+      zip_safe=False)
