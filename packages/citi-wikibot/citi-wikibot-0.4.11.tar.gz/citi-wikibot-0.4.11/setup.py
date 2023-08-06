23b14711f07c7401ed5f840fa47c148b005c0334
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:14:22 +0000

    Add package citi-wikibot/0.4.11/citi-wikibot-0.4.11.tar.gz

---
+++ packages/citi-wikibot/citi-wikibot-0.4.11.tar.gz/citi-wikibot-0.4.11/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+with open('README.rst', 'r') as f:
+  readme = f.read()
+
+with open('requirements.txt', 'r') as file:
+    requires = [line.strip('\n') for line in file]
+
+setup(
+  name='citi-wikibot',
+  packages=['wikibot'],
+  version='0.4.11',
+  description='CITI\'s Wiki page editing for lazy people.',
+  long_description=readme,
+  author='Vanessa Barreiros',
+  author_email='vanessa.barreiros@citi.org.br',
+  url='https://github.com/citi-ufpe/citi-wikibot',
+  keywords=['wiki', 'bot', 'wikibot', 'citi'],
+  install_requires=requires
+)
