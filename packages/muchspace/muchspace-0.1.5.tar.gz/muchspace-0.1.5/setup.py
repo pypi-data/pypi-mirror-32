e900d4f857a28544feb4b9c09861d8c5499ed578
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:22:52 +0000

    Add package muchspace/0.1.5/muchspace-0.1.5.tar.gz

---
+++ packages/muchspace/muchspace-0.1.5.tar.gz/muchspace-0.1.5/setup.py
@@ -0,0 +1,45 @@
+from setuptools import setup, find_packages
+
+
+with open('README.rst') as readme_file:
+    README = readme_file.read()
+
+with open('HISTORY.rst') as history_file:
+    HISTORY = history_file.read()
+
+setup(name = 'muchspace',
+      description = 'A CLI utility in python3 to calulate total disk space required for media links',
+      long_description=README + '\n\n' + HISTORY,
+      version = '0.1.5',
+      url = 'https://github.com/Hitoshirenu/muchspace',
+      download_url = 'https://github.com/Hitoshirenu/muchspace/archive/0.1.tar.gz',
+      author = 'abhiigatty',
+      author_email = 'abhiigatty@gmail.com',
+      license = 'MIT',
+      classifiers = [
+        'Development Status :: 1 - Planning',
+        'Environment :: Console',   
+        'Intended Audience :: End Users/Desktop',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Environment :: Win32 (MS Windows)',
+        'Operating System :: MacOS',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: POSIX',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Topic :: System :: Filesystems',
+        'Topic :: Software Development',
+        'Topic :: Utilities',
+      ],
+      keywords = ['band width', 'disk-space', 'analyzer', 'cli', 'python3'],
+      maintainer = 'abhiigatty',
+      maintainer_email = 'abhiigatty@gmail.com',
+      packages=find_packages(),
+      install_requires = ['fire', 'requests'],
+      entry_points = {
+          'console_scripts': [
+              'muchspace = main_muchspace.muchspace:main'
+          ]
+      }
+)
