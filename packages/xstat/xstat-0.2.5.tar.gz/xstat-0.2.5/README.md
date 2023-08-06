e223cf2770a3971c2f48c778c4ae9444da51a480
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:13:19 +0000

    Add package xstat/0.2.5/xstat-0.2.5.tar.gz

---
+++ packages/xstat/xstat-0.2.5.tar.gz/xstat-0.2.5/README.md
@@ -0,0 +1,4 @@
+# xstat
+make statsd work with flask, django, maple or other server
+
+python2, python3 supported
---
+++ packages/xstat/xstat-0.2.5.tar.gz/xstat-0.2.5/xstat/maple_stat.py
@@ -0,0 +1,66 @@
+# -*- coding: utf-8 -*-
+
+from statsd import StatsClient
+
+from .utils import catch_exc
+from . import constants
+
+
+class MapleStat(object):
+
+    _xstat_title = None
+    _xstat_host = None
+    _xstat_port = None
+
+    _stat_client = None
+
+    def __init__(self, app=None, config=None):
+        super(MapleStat, self).__init__()
+
+        if app:
+            self.init_app(app, config)
+
+    def init_app(self, app, config):
+        """
+        绑定app
+        """
+        self._xstat_title = config.get('XSTAT_TITLE')
+        self._xstat_host = config.get('XSTAT_HOST')
+        self._xstat_port = config.get('XSTAT_PORT') or constants.XSTAT_PORT
+        self._stat_client = StatsClient(host=self._xstat_host, port=self._xstat_port)
+
+        @app.before_request
+        @catch_exc
+        def prepare_stat(request):
+            if not request.endpoint:
+                return
+
+            request.xstat_timers = []
+            request.xstat_timers.append(
+                self._stat_client.timer('.'.join([
+                    self._xstat_title,
+                    'endpoint',
+                    request.endpoint,
+                    ])
+                )
+            )
+
+            request.xstat_timers.append(
+                self._stat_client.timer('.'.join([
+                    self._xstat_title,
+                    'all',
+                    ])
+                )
+            )
+
+            for stat in request.xstat_timers:
+                stat.start()
+
+        @app.after_request
+        @catch_exc
+        def send_stat(request, exc):
+            if not hasattr(request, 'xstat_timers'):
+                return
+
+            for stat in request.xstat_timers:
+                stat.stop()
