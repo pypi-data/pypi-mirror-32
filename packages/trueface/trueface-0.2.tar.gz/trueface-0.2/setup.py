4f37547f4b33dd614b4c9efa870d461e8906178c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:51:03 +0000

    Add package trueface/0.2/trueface-0.2.tar.gz

---
+++ packages/trueface/trueface-0.2.tar.gz/trueface-0.2/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup,find_packages
+
+setup(name='trueface',
+      version='0.2',
+      description='Trueface Face Recognition',
+      url='https://github.com/getchui/truefacesdk',
+      author='Nezare Chafni',
+      author_email='nchafni@truface.ai',
+      license='MIT',
+      packages=['trueface'],
+      package_data={'trueface': ['model/*json', 'model/*caffemodel', 'model/*prototxt','model/*params','logo.png']},
+      install_requires=[
+                'pubnub',
+                'imutils',
+                'opencv-python',
+                'mxnet',
+                'dlib',
+                'requests',
+                'requests_futures',
+                'mss',
+                'art',
+                'numpy',
+                'pandas',
+                'termcolor'
+            ],
+      entry_points={
+       'console_scripts': [
+           'trueface = trueface.trueface:main',
+           'enroll = trueface.enroll:main',
+       ],
+      },
+      zip_safe=False)
