b23fd4055f8edfb36a63cef6caae084f11e45079
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:25:02 +0000

    Add package instalooter/2.0.3/instalooter-2.0.3.tar.gz

---
+++ packages/instalooter/instalooter-2.0.3.tar.gz/instalooter-2.0.3/instalooter/cli/login.py
@@ -0,0 +1,26 @@
+# coding: utf-8
+from __future__ import absolute_import
+from __future__ import unicode_literals
+
+import getpass
+import logging
+
+import verboselogs
+
+from ..looters import InstaLooter
+
+
+logger = verboselogs.VerboseLogger(__name__)
+logger.addHandler(logging.StreamHandler())
+
+
+def login(args):
+    if args['--username']:
+        username = args['--username']
+        if not InstaLooter._logged_in():
+            password = args['--password'] or getpass.getpass()
+            InstaLooter._login(username, password)
+            if not args['--quiet']:
+                logger.success('Logged in.')
+        elif not args['--quiet']:
+            logger.success("Already logged in.")
