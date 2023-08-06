d62f60a6f2de2861dc85253cc48b956bfe9a0a1c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:35:37 +0000

    Add package bee_django_course/0.0.44/bee_django_course-0.0.44.tar.gz

---
+++ packages/bee_django_course/bee_django_course-0.0.44.tar.gz/bee_django_course-0.0.44/bee_django_course/signals.py
@@ -0,0 +1,11 @@
+# -*- coding:utf-8 -*-
+from django.dispatch import Signal
+
+# 作业被评分前发出的信号
+assignment_will_be_scored = Signal(providing_args=["user_course_section", "request"])
+
+# 作业评分后发出的信号
+assignment_was_scored = Signal(providing_args=["user_course_section", "request"])
+
+# 直播回调后发出的信号
+live_callback_finished = Signal(providing_args=["user"])
