2cd6137b695c83ae39da6037b4248cfdd3907d43
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:07:52 +0000

    Add package cleverbotio/1.1/cleverbotio-1.1.tar.gz

---
+++ packages/cleverbotio/cleverbotio-1.1.tar.gz/cleverbotio-1.1/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+with open('README.rst') as f:
+    readme = f.read()
+
+setup(name='cleverbotio',
+    version="1.1",
+    description="A Python wrapper for the Cleverbot.io API that has an async option",
+    long_description=readme,
+    url='https://github.com/shadeyg56/cleverbotio.py',
+    author='shadeyg56',
+    license="GNU",
+    packages=['Cleverbotio', 'Cleverbotio.async'],
+    install_requires=['requests>=1.0.0'],
+    extras_require={'async': ['aiohttp>=1.0.0']},
+    keywords = ["cleverbot", "async", "api-wrapper"]
+)
