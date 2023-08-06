7a07b1d98ec27f2f9f98bb3080c7d580fa0593ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:18:08 +0000

    Add package pythonpackagesample/0.2/pythonpackagesample-0.2.tar.gz

---
+++ packages/pythonpackagesample/pythonpackagesample-0.2.tar.gz/pythonpackagesample-0.2/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='pythonpackagesample',
+      version='0.2',  # Development release
+      description='Python Packaging Starting Kit - Simple Structure to creater your own package - for training purposes!',
+      url='https://github.com/manoelgadi/pythonpackagesample',
+      author='Prof. Manoel Gadi',
+      author_email='mfalonso@faculty.ie.edu',
+      license='MIT',
+      packages=['pythonpackagesample'],
+      zip_safe=False)
