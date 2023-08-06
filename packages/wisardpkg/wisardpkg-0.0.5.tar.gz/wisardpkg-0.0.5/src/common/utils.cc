c1245a56dc8c16f09b24ec32a51b4a1a9dab0b0a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:00:30 +0000

    Add package wisardpkg/0.0.5/wisardpkg-0.0.5.tar.gz

---
+++ packages/wisardpkg/wisardpkg-0.0.5.tar.gz/wisardpkg-0.0.5/src/common/utils.cc
@@ -0,0 +1,17 @@
+#include <time.h>
+
+inline int randint(int min, int max, bool isSeed=true){
+  if(isSeed)
+    std::srand(time(NULL));
+  return min + (std::rand() % (int)(max - min + 1));
+}
+
+
+template<typename T, typename Functor>
+void foreach(std::vector<T>& v, Functor& func, bool verbose=false){
+  for(unsigned int i=0; i<v.size(); i++){
+    if(verbose) std::cout << "\rforeach: " << i+1 << " of " << v.size();
+    v[i] = func(v[i]);
+  }
+  std::cout << "\r" << std::endl;
+}
