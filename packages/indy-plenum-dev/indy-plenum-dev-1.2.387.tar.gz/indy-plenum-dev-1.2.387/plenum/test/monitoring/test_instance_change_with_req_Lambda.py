ef13ddddb645292b3dc782ee6686a95d81071c58
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:13:10 +0000

    Add package indy-plenum-dev/1.2.387/indy-plenum-dev-1.2.387.tar.gz

---
+++ packages/indy-plenum-dev/indy-plenum-dev-1.2.387.tar.gz/indy-plenum-dev-1.2.387/plenum/test/monitoring/test_instance_change_with_req_Lambda.py
@@ -0,0 +1,69 @@
+import pytest
+
+from plenum.common.messages.node_messages import PrePrepare
+from stp_core.common.util import adict
+from plenum.test.helper import waitForViewChange
+from plenum.test.test_node import getPrimaryReplica
+from plenum.test.spy_helpers import getAllReturnVals
+from plenum.test.helper import sdk_send_random_and_check
+
+nodeCount = 7
+whitelist = ["discarding message"]
+
+"""
+We start with a 7 node consensus pool
+Lets call the master instance's primary replica P
+Drop threshold `Delta` to very low so that view change does not happen
+because of drop in throughput. Another approach could have been to send a lot of
+requests and delay only one request such that the ratio of throughputs does not
+drop below `Delta`.
+Make P faulty: slow to send PRE-PREPAREs to only a specific request
+verify that throughput has dropped
+Verify a view change happens
+"""
+
+
+@pytest.fixture()
+def setup(looper, tconf, txnPoolNodeSet, sdk_wallet_client, sdk_pool_handle):
+    sdk_send_random_and_check(looper, txnPoolNodeSet,
+                              sdk_pool_handle, sdk_wallet_client, 5)
+    P = getPrimaryReplica(txnPoolNodeSet)
+
+    # set LAMBDA smaller than the production config to make the test faster
+    testLambda = 30
+    delay_by = testLambda + 5
+
+    for node in txnPoolNodeSet:
+        # Make `Delta` small enough so throughput check passes.
+        node.monitor.Delta = .001
+        node.monitor.Lambda = testLambda
+        for r in node.replicas:
+            r.config.ACCEPTABLE_DEVIATION_PREPREPARE_SECS += delay_by
+
+    slowed_request = False
+
+    # make P (primary replica on master) faulty, i.e., slow to send
+    # PRE-PREPARE for a specific client request only
+    def specificPrePrepare(msg):
+        nonlocal slowed_request
+        if isinstance(msg, PrePrepare) and slowed_request is False:
+            slowed_request = True
+            return delay_by  # just more that LAMBDA
+
+    P.outBoxTestStasher.delay(specificPrePrepare)
+    # TODO select or create a timeout for this case in 'waits'
+    sdk_send_random_and_check(looper, txnPoolNodeSet,
+                              sdk_pool_handle, sdk_wallet_client, 5,
+                              customTimeoutPerReq=tconf.TestRunningTimeLimitSec)
+
+    return adict(nodes=txnPoolNodeSet)
+
+
+def testInstChangeWithMoreReqLat(looper, setup):
+    nodes = setup.nodes
+    for node in nodes:
+        node.checkPerformance()
+        assert any(getAllReturnVals(node.monitor,
+                                    node.monitor.isMasterReqLatencyTooHigh))
+
+    waitForViewChange(looper, nodes)
---
+++ packages/indy-plenum-dev/indy-plenum-dev-1.2.387.tar.gz/indy-plenum-dev-1.2.387/plenum/test/propagate/test_propagate_recvd_after_request.py
@@ -0,0 +1,52 @@
+import pytest
+
+from plenum.common.constants import PROPAGATE
+from plenum.test.helper import sdk_json_to_request_object
+from stp_core.loop.eventually import eventually
+from plenum.common.messages.node_messages import Propagate
+from plenum.test.delayers import delay, msg_rep_delay
+from plenum.test.propagate.helper import recvdRequest, recvdPropagate, \
+    sentPropagate
+from plenum.test.test_node import TestNode
+
+nodeCount = 4
+howlong = 5
+
+
+@pytest.fixture()
+def setup(txnPoolNodeSet):
+    A, B, C, D = txnPoolNodeSet  # type: TestNode
+    delay(Propagate, frm=[B, C, D], to=A, howlong=howlong)
+    # Delay MessageRep by long simulating loss as if Propagate is missing, it
+    # is requested
+    A.nodeIbStasher.delay(msg_rep_delay(10 * howlong, [PROPAGATE, ]))
+
+
+def testPropagateRecvdAfterRequest(setup, looper, txnPoolNodeSet, sent1):
+    A, B, C, D = txnPoolNodeSet  # type: TestNode
+    sent1 = sdk_json_to_request_object(sent1[0][0])
+
+    def x():
+        # A should have received a request from the client
+        assert len(recvdRequest(A)) == 1
+        # A should not have received a PROPAGATE
+        assert len(recvdPropagate(A)) == 0
+        # A should have sent a PROPAGATE
+        assert len(sentPropagate(A)) == 1
+
+    timeout = howlong - 2
+    looper.run(eventually(x, retryWait=.5, timeout=timeout))
+
+    def y():
+        # A should have received 3 PROPAGATEs
+        assert len(recvdPropagate(A)) == 3
+        # A should have total of 4 PROPAGATEs (3 from other nodes and 1 from
+        # itself)
+        key = sent1.identifier, sent1.reqId
+        assert key in A.requests
+        assert len(A.requests[key].propagates) == 4
+        # A should still have sent only one PROPAGATE
+        assert len(sentPropagate(A)) == 1
+
+    timeout = howlong + 2
+    looper.run(eventually(y, retryWait=.5, timeout=timeout))
