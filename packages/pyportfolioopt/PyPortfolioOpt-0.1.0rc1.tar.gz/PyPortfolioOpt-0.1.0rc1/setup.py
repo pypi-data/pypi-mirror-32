12b02f6b6dc0803dafdb15c74084542ef65f4a05
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:38 +0000

    Add package pyportfolioopt/0.1.0rc1/PyPortfolioOpt-0.1.0rc1.tar.gz

---
+++ packages/pyportfolioopt/PyPortfolioOpt-0.1.0rc1.tar.gz/PyPortfolioOpt-0.1.0rc1/setup.py
@@ -0,0 +1,43 @@
+from setuptools import setup
+
+with open("README.md", "r") as f:
+    desc = f.read()
+    desc = desc.split("</p>")[-1]
+
+setup(
+    name="PyPortfolioOpt",
+    version="0.1.0rc1",
+    description="Financial portfolio optimisation in python",
+    long_description=desc,
+    long_description_content_type="text/markdown",
+    url="https://github.com/robertmartin8/PyPortfolioOpt",
+    author="Robert Andrew Martin",
+    author_email="martin.robertandrew@gmail.com",
+    license="MIT",
+    packages=["pypfopt"],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Environment :: Console",
+        "Intended Audience :: Financial and Insurance Industry",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3 :: Only",
+        "Topic :: Office/Business :: Financial",
+        "Topic :: Office/Business :: Financial :: Investment",
+        "Topic :: Scientific/Engineering :: Mathematics",
+    ],
+    keywords="portfolio finance optimization quant trading investing",
+    install_requires=["numpy", "pandas", "scikit-learn", "scipy"],
+    setup_requires=["pytest-runner"],
+    tests_require=["pytest"],
+    python_requires=">=3",
+    project_urls={  # TODO add documentation link
+        "Issues": "https://github.com/robertmartin8/PyPortfolioOpt/issues",
+        "Personal website": "https://reasonabledeviations.science",
+    },
+)
