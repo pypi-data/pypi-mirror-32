1ab8137a69bdd8032b782621d217fc5dc91d0c22
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:10 +0000

    Add package heartbeatmonitor/0.1a12/heartbeatmonitor-0.1a12.tar.gz

---
+++ packages/heartbeatmonitor/heartbeatmonitor-0.1a12.tar.gz/heartbeatmonitor-0.1a12/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+setup(
+    name='heartbeatmonitor',
+    version='0.1a12',   # ALREADY MOVED FROM 10 --> 11
+    author='Hunter M. Allen',
+    author_email='allenhm@gmail.com',
+    license='MIT',
+    #packages=find_packages(),
+    packages=['heartbeatmonitor'],
+    #scripts=['bin/heartbeatmonitor.py'],
+    install_requires=['python-dateutil>=2.7.3',
+                      'slackclient>=1.2.1'],
+    description='Central heartbeat monitoring application for multi-program deployment.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/hmallen/heartbeatmonitor',
+    keywords=['heartbeat', 'monitor'],
+    classifiers=(
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ),
+)
