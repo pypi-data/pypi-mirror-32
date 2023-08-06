d4efadc3215fb42de849d20fddb1708b7239b08b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:49:22 +0000

    Add package smarturls/0.1.9/smarturls-0.1.9.tar.bz2

---
+++ packages/smarturls/smarturls-0.1.9.tar.bz2/smarturls-0.1.9/setup.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+try:
+    long_description = open('README.rst', 'rt').read()
+except IOError:
+    long_description = ""
+
+
+setup(
+    name="smarturls",
+    description="A URL construction helper for Django.",
+    long_description=long_description,
+
+    version="0.1.9",
+    author='Amit Upadhyay',
+    author_email="upadhyay@gmail.com",
+
+    url='https://amitu.com/smarturls/',
+    license='BSD',
+
+    py_modules=["smarturls"],
+
+    zip_safe=True,
+
+    install_requires=["Django>=1.5"],
+
+    tests_require=['tox'],
+
+    keywords=['helper', 'django', 'utils', 'regex', 'utility'],
+
+    classifiers=[
+
+        'Intended Audience :: Developers',
+
+        'Natural Language :: English',
+
+        'Operating System :: OS Independent',
+        'Operating System :: POSIX :: Linux',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: MacOS :: MacOS X',
+
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+
+        'Programming Language :: Python :: Implementation :: CPython',
+
+        'Topic :: Software Development',
+
+    ],
+)
---
+++ packages/smarturls/smarturls-0.1.9.tar.bz2/smarturls-0.1.9/smarturls.py
@@ -0,0 +1,62 @@
+# -*- coding: utf-8 -*-
+
+
+"""Smarturls an URL construction helper for Django."""
+
+
+import re
+
+from django.conf import settings
+
+try:
+    from django.conf.urls.defaults import url as ourl
+except ImportError:
+    from django.conf.urls import url as ourl
+
+
+__license__ = "BSD"
+__author__ = "Amit Upadhyay"
+__url__ = "https://github.com/amitu/smarturls#smart-urls-for-django"
+
+
+REGEXERS = {
+    "word": "\w+",
+    "digit": "\d",
+    "int": "\d+",
+    "int2": "\d{2,2}",
+    "int4": "\d{4,4}",
+    "slug": "[\w-]+",
+    "username": "[\w.@+-]+",
+    "uuid": "[A-Fa-f0-9]{8}-?[A-Fa-f0-9]{4}-?4[A-Fa-f0-9]{3}-?[89abAB][a-fA-F0-9]{3}-?[a-fA-F0-9]{12}",
+    "base64": "[0-9a-zA-Z\-_]+={0,2}",
+    "ekey": "\$?[0-9a-zA-Z\-_]+\.{0,2}",
+    "something": ".+",
+    "anything": ".*",
+}
+
+
+REGEXERS.update(getattr(settings, "SURL_REGEXERS", {}))
+_R = re.compile("<((\w+:)?\w+)>")
+
+
+def _regex_substituter(m):
+    """Regular Expression substitutions."""
+    name = m.groups()[0]
+    if ":" not in name:
+        name = "word:%s" % name
+    t, n = name.split(":")
+    return "(?P<%s>%s)" % (n, REGEXERS[t])
+
+
+def translate_regex(regex):
+    """Translate the Regular Expressions."""
+    return regex if not regex.startswith("/") else "^%s$" % _R.sub(
+        _regex_substituter, regex)[1:]
+
+
+def surl(regex, *args, **kw):
+    """Smart URL callback function."""
+    return ourl(translate_regex(regex), *args, **kw)
+
+
+url = surl
