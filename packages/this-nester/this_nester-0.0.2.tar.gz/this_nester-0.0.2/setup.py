a49783939249ccad611cc388b0b707a6c2de0ce8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:16 +0000

    Add package this-nester/0.0.2/this_nester-0.0.2.tar.gz

---
+++ packages/this-nester/this_nester-0.0.2.tar.gz/this_nester-0.0.2/setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+from distutils.core import setup
+
+setup(
+        name    = 'this_nester',
+        version = '0.0.2',
+        py_modules  = ['nester'],
+	author	= 'ioeory',
+        author_email    = '123@xac.com',
+        url = 'http://www.example.com',
+        description = 'nesty code',
+
+        )
