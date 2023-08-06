e12f7b67bed2d5f43f630f181f53aa75678f9060
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:18:20 +0000

    Add package gym-tetris/0.3.8/gym_tetris-0.3.8.tar.gz

---
+++ packages/gym-tetris/gym_tetris-0.3.8.tar.gz/gym_tetris-0.3.8/gym_tetris/wrappers/__init__.py
@@ -0,0 +1,18 @@
+"""Wrappers for altering the functionality of the game."""
+from .clip_reward_env import ClipRewardEnv
+from .downsample_env import DownsampleEnv
+from .frame_stack_env import FrameStackEnv
+from .max_frameskip_env import MaxFrameskipEnv
+from .penalize_death_env import PenalizeDeathEnv
+from .reward_cache_env import RewardCacheEnv
+
+
+# explicitly define the outward facing API of this package
+__all__ = [
+    ClipRewardEnv.__name__,
+    DownsampleEnv.__name__,
+    FrameStackEnv.__name__,
+    MaxFrameskipEnv.__name__,
+    PenalizeDeathEnv.__name__,
+    RewardCacheEnv.__name__,
+]
