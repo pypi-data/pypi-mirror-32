e529040498ebb819e1c7a49ec67be86d20ec2706
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:11:04 +0000

    Add package omc-scanalyzer/0.2.2/omc-scanalyzer-0.2.2.tar.gz

---
+++ packages/omc-scanalyzer/omc-scanalyzer-0.2.2.tar.gz/omc-scanalyzer-0.2.2/omc_scanalyzer/main.py
@@ -0,0 +1,184 @@
+__version__ = "0.2.2"
+
+import numpy as np
+import sys
+import os
+#from PIL import ImageTk, Image
+if __name__ == '__main__':
+    module_path = '.'
+else:
+    #print(sys.modules[__name__]).__file__
+    module_path = os.path.dirname(sys.modules[__name__].__file__)
+    #print(module_path)
+
+from subprocess import check_output
+if sys.version_info[0] < 3:
+    import Tkinter as tk
+    from exceptions import Exception
+else:
+    import tkinter as tk
+    
+from main_frame import MainApplication
+from tk_frames import RadioOption, OptionsParser, enable, disable
+from channels import *
+
+try:
+    import nds2
+except:
+    pass
+
+# IFO == H1 -> Hanford
+# IFO == L1 -> Livingston
+try:
+    IFO = os.environ['IFO']
+except Exception:
+    print('IFO environment variable not defined. Assuming H1')
+    IFO = 'H1'
+
+class WelcomeScreen(tk.Frame):
+
+    def __init__(self, parent, *args, **kwargs):
+        tk.Frame.__init__(self, parent, *args, **kwargs)
+        self.parent = parent
+        
+        self.omc_channel_name = IFO + ':OMC-DCPD_SUM_OUT_DQ'
+        self.pzt_channel_name = IFO + ':OMC-PZT2_EXC'
+        self.pzt_historic_channel_name = IFO + ':OMC-PZT2_MON_DC_OUT_DQ'
+        self.nds_server = IFO + 'nds1'
+        
+        self.t1 = None
+        self.t2 = None
+        self.duration = None
+        
+        if any([True for x in ['--verbose','-v'] if x in sys.argv]):
+            self.verbose = True
+        else:
+            self.verbose = False
+       
+        # t1=1208580738
+        
+        # t1=1210187830
+        
+        self.row1 = tk.Frame(self)
+        self.col1 = tk.Frame(self.row1)
+        self.col2 = tk.Frame(self.row1)
+        
+        self.duration_label = tk.Label(self.col1,text='duration=')
+        self.duration_entry = tk.Entry(self.col2)
+        self.duration_entry.insert(0, "20")
+        self.duration_label.pack(side='top',anchor='e')
+        self.duration_entry.pack(side='top')
+        
+        self.t1_label = tk.Label(self.col1,text='t1=')
+        self.t1_entry = tk.Entry(self.col2)
+        self.t1_entry.insert(0, "1210187830")
+        self.t1_label.pack(side='top',anchor='e')
+        self.t1_entry.pack(side='top')
+        
+        self.rec_time_label = tk.Label(self.col1,text='rec_time=')
+        self.rec_time_entry = tk.Entry(self.col2)
+        self.rec_time_entry.insert(0, "17500")
+        self.rec_time_label.pack(side='top',anchor='e')
+        self.rec_time_entry.pack(side='top')
+
+        self.modes = ['old_historic (debugging)','historic','realtime']
+        self.rb = RadioOption(self,self.modes,default=self.modes[1],command=self.on_rb_change)
+        self.on_rb_change()
+        self.startbutton = tk.Button(master=self, text='start', command=self.start)
+        
+        self.rb.pack(side='top',expand=True,anchor='n',pady=10)
+        self.col1.pack(side='left')
+        self.col2.pack(side='left')
+        self.row1.pack(side='top',pady=10)
+        self.startbutton.pack(side='top',expand=True,pady=10)
+        
+        # add uofa_logo
+        #self.img = Image.open("uofa_logo.png")
+        #aspect_ratio = self.img.size[0]/float(self.img.size[1])
+        #self.img = self.img.resize((int(250*aspect_ratio), 250), Image.ANTIALIAS)
+        self.img = tk.PhotoImage(file=module_path + '/uofa_logo_250.png')
+        self.panel = tk.Label(self,image=self.img)
+        self.panel.pack(pady=10)
+        
+    def on_rb_change(self):
+        if self.rb.var.get() == self.modes[0]:
+            # debug
+            disable(self.col2)
+        elif self.rb.var.get() == self.modes[1]:
+            # historic
+            enable(self.col2)
+        elif self.rb.var.get() == self.modes[2]:
+            # realtime
+            disable(self.col2)
+            enable(self.duration_entry)
+        
+    def acquire_channel(self,channel_name):
+        pass
+        
+    def start(self):
+    
+        self.t1 = int(self.t1_entry.get())
+        self.duration = int(self.duration_entry.get())
+        self.rec_time = int(self.rec_time_entry.get())
+        mode = self.rb.var.get()
+        if mode == 'old_historic (debugging)':
+            omc_data = np.load('../omc.npy')
+            pzt_data = np.load('../pzt.npy')
+            
+            t1 = 1208580738
+            t2 = 1208582718
+            Fs = 2**14 # 16384 Hz
+            
+            time_channel = np.linspace(t1,t2,len(pzt_data))
+            time_channel2 = np.linspace(t1,t2,len(omc_data))
+
+            # pzt channel is downampled, reinterpolate
+            pzt_data2 = np.interp(time_channel2,time_channel,pzt_data)
+
+            omc_chan = old_historic_channel(omc_data,Fs,t1,t2)
+            pzt_chan = old_historic_channel(pzt_data2,Fs,t1,t2)
+            
+        elif mode == 'historic':
+            omc_chan = historic_channel_continuous(self.nds_server,self.omc_channel_name,self.t1,self.rec_time)
+            print('omc loaded')
+            pzt_chan = historic_channel_continuous(self.nds_server,self.pzt_historic_channel_name,self.t1,self.rec_time)
+            print('pzt loaded')
+            
+        elif mode == 'realtime':           
+            omc_chan = realtime_channel(self.nds_server,self.omc_channel_name)
+            pzt_chan = realtime_channel(self.nds_server,self.pzt_channel_name)
+            print('Not yet fully tested')
+            
+            
+        # clear frame
+        self.destroy()
+        # insert mainapp frame
+        mainapp = MainApplication(self.parent,omc_chan,pzt_chan)
+        mainapp.verbose = self.verbose
+        mainapp.mode = mode
+        mainapp.omc_channel_name = self.omc_channel_name
+        mainapp.pzt_channel_name = self.pzt_channel_name
+        mainapp.pzt_historic_channel_name = self.pzt_historic_channel_name
+        mainapp.nds_server = self.nds_server
+        mainapp.acquire_channels = lambda: [realtime_channel(self.pzt_channel_name)
+        ,realtime_channel(self.omc_channel_name)]
+        mainapp.duration = self.duration
+        mainapp.pack(side="left", fill="both", expand=True)
+        # resize window for new frame
+        mainapp.parent.geometry("1200x620")
+        mainapp.start()
+
+def main():
+
+    root = tk.Tk()
+    root.title("OMC Scanalyzer")
+    root.geometry("600x480")
+    welcome = WelcomeScreen(root)
+    welcome.pack(side='top',expand=True)
+    
+    # mainapp.pack(side="top", fill="both", expand=True)
+    
+    root.mainloop()
+    
+if __name__ == "__main__":
+    main()
---
+++ packages/omc-scanalyzer/omc-scanalyzer-0.2.2.tar.gz/omc-scanalyzer-0.2.2/omc_scanalyzer/tk_frames.py
@@ -0,0 +1,232 @@
+import matplotlib as mpl
+mpl.use("TkAgg") # needs to be there
+from matplotlib.backends import backend_tkagg
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2TkAgg
+
+import sys
+if sys.version_info[0] < 3:
+    import Tkinter as tk
+    from exceptions import *
+else:
+    import tkinter as tk
+
+def disable(tk_obj):
+    try:
+        tk_obj.configure(state=tk.DISABLED)
+    except Exception:
+        pass
+    for child in tk_obj.winfo_children():
+        try:
+            disable(child)
+        except Exception:
+            pass
+            
+def enable(tk_obj):
+    try:
+        tk_obj.configure(state=tk.NORMAL)
+    except Exception:
+        pass
+    for child in tk_obj.winfo_children():
+        try:
+            enable(child)
+        except Exception:
+            pass
+
+class namespace:
+    def __init__(self, **kwds):
+        self.__dict__.update(kwds)
+    
+class HelpButton(tk.Frame):
+    def __init__(self, parent, help_text='NA', *args, **kwargs):
+        tk.Frame.__init__(self, parent, *args, **kwargs)
+        self.parent = parent
+        self.help_text = help_text
+        
+        self.button = tk.Button(self, text='Help', command=self.pop_help)
+        self.button.pack()
+        
+    def pop_help(self):
+        try:
+            if self.win0.winfo_exists():
+                self.win0.destroy()
+        except AttributeError:
+            # win0 doesn't exist so don't need to destroy it
+            pass
+        self.win0 = tk.Toplevel(master=self)
+        self.win0.geometry('640x480')
+        self.text = tk.Text(self.win0)
+        self.text.pack(side='top',fill='both',expand=False)
+        self.text.config(state=tk.NORMAL)
+        self.text.insert(tk.END, self.help_text)
+        self.text.config(state=tk.DISABLED)
+        
+class RadioOption(tk.Frame):
+    def __init__(self, parent, options, command=None, default=None, *args, **kwargs):
+        ''' 
+        options must be a list of strings and each string has to be a valid python variable name
+        '''
+        tk.Frame.__init__(self, parent, *args, **kwargs)
+        self.parent = parent
+        self.options = options
+        self.default = default
+        self.command = command
+        
+        self.var = tk.StringVar(master=self.parent)
+        
+        if default is not None:
+            self.var.set(default) # select default option
+                 
+        for option in self.options:
+            rb_str = 'rb_'+option
+            setattr(self,rb_str,tk.Radiobutton(
+            master=self, text=option, variable=self.var, value=option,command=command))
+            getattr(self,rb_str).pack(side='top',anchor='w')
+                
+class OptionsParser(tk.Frame):
+    def __init__(self, parent, target=None, initial_text='', include_button=True, on_submit=None, *args, **kwargs):
+        tk.Frame.__init__(self, parent, *args, **kwargs)
+        self.parent = parent
+        if target is None:
+            self.target = self.parent
+        else: self.target = target
+        self.on_submit = on_submit
+        
+        if include_button:
+            self.button = tk.Button(master=self, text='Submit', command=self.submit)
+            self.button.pack(side='left')
+        
+        self.entry = tk.Entry(master=self,width=500)
+        self.entry.insert(0, initial_text)
+        self.entry.pack(side='left')
+    
+    def submit(self):
+        todict = lambda **kwargs: kwargs
+        text = self.entry.get()
+        in_dict = eval('todict('+text+')')
+        for key in in_dict:
+            setattr(self.target, key, in_dict[key])
+        
+        if self.on_submit is not None:
+            self.on_submit(self)
+        
+class KillButton(tk.Frame):
+    def __init__(self, parent, target, text='Kill', *args, **kwargs):
+        tk.Frame.__init__(self, parent, *args, **kwargs)
+        self.parent = parent
+        self.target = target
+        
+        self.button = tk.Button(master=self, text=text, command=self.Kill)
+        self.button.pack(side='left')
+        
+    def Kill(self):
+        self.target.destroy()
+        
+class AxisLimit(tk.Frame):
+    def __init__(self, parent, target, text='', *args, **kwargs):
+        tk.Frame.__init__(self, parent, *args, **kwargs)
+        self.parent = parent
+        self.title_label = tk.Label(master=self,text=text)
+        self.target = target
+        
+        self._group1 = tk.Frame(self)
+        self._group1a = tk.Frame(self._group1)
+        self._group1b = tk.Frame(self._group1)
+        self._group2 = tk.Frame(self)
+        
+        self.button = tk.Button(master=self._group1, text='Submit',command=self.submit)
+        
+        self.max_label = tk.Label(master=self._group1a,text='max')
+        self.max_entry = tk.Entry(master=self._group1a,width=7)
+        self.max_label.pack(side='top')
+        self.max_entry.pack(side='top')
+        
+        self.min_label = tk.Label(master=self._group1b,text='min')
+        self.min_entry = tk.Entry(master=self._group1b,width=7)
+        self.min_label.pack(side='top')
+        self.min_entry.pack(side='top')
+        
+        self._group1b.pack(side='left')
+        self._group1a.pack(side='left')
+        self.button.pack(side='left',anchor='sw')
+               
+        self.rb_mode = RadioOption(self._group2,['automatic','manual'],default='automatic',command=self.onchange)
+        self.rb_mode.pack(side='top')
+        
+        self.title_label.pack(anchor='w')
+        self._group2.pack(anchor='w')
+        self._group1.pack(anchor='w')
+        
+    def submit(self):
+        self.target[0] = float(self.min_entry.get())
+        self.target[1] = float(self.max_entry.get())
+        
+    def onchange(self):
+        if self.rb_mode.var.get() == 'automatic':
+            disable(self._group1)
+        else:
+            enable(self._group1)
+        
+class GraphFrame(tk.Frame):
+    def __init__(self, parent, graph, title=None, *args, **kwargs):
+        tk.Frame.__init__(self, parent, *args, **kwargs)
+        self.graph = graph
+        self.parent = parent
+        
+        self.canvas = FigureCanvasTkAgg(self.graph, master=self)
+        self.canvas_widget = self.canvas.get_tk_widget()
+        self.graph.canvas.draw()
+        
+        self.title_label = tk.Label(self,text=title)
+        
+        self.title_label.pack(side="top",anchor='c')
+        self.canvas_widget.pack(side="top")
+        
+class MyFigure(mpl.figure.Figure):
+    def __init__(self, *args, **kwargs):
+        mpl.figure.Figure.__init__(self, *args, **kwargs)
+        self.ax = self.add_subplot(111)
+        self.line, = self.ax.plot([],[])
+        
+class DoubleSubplot(mpl.figure.Figure):
+    def __init__(self, *args, **kwargs):
+        mpl.figure.Figure.__init__(self, *args, **kwargs)
+        self.ax1 = self.add_subplot(211)
+        self.line1, = self.ax1.plot([],[])
+        self.ax2 = self.add_subplot(212)
+        self.line2, = self.ax2.plot([],[])
+        
+class PeakLabels(tk.Frame):
+    def __init__(self, parent, *args, **kwargs):
+        tk.Frame.__init__(self, parent, *args, **kwargs)
+        self.peak_range_var = tk.StringVar()
+        self.peak_loc_var = tk.StringVar()
+        self.peak_time_var = tk.StringVar()
+        self.peak_height_var = tk.StringVar()
+        
+        self.peak_range = tk.Label(self, textvariable=peak_range_var)
+        self.peak_loc = tk.Label(self, textvariable=peak_loc_var)
+        self.peak_time = tk.Label(self, textvariable=peak_time_var)
+        self.peak_height = tk.Label(self, textvariable=peak_height_var)
+        
+        self.peak_range.pack()
+        self.peak_loc.pack()
+        self.peak_time.pack()
+        self.peak_height.pack()
+    
+    def update(self, peak_range, peak_loc, peak_time, peak_height):
+        self.peak_range_var.set(str(peak_range))
+        self.peak_loc_var
+        
+# class MyRectSelec(mpl.widgets.RectangleSelector):
+    # def __init__(self, *args, **kwargs):
+        # mpl.widgets.RectangleSelector.__init__(self, *args, **kwargs)
+        
+class PlotWindow(tk.Toplevel):
+    def __init__(self, *args, **kwargs):
+        tk.Toplevel.__init__(self, *args, **kwargs)
+        
+    def destroy(self):
+        'destroying the plot window should also turn off rectselects'
+        #self.master.graph1.ax1.RS0.set_visible(False)
+        #self.master.graph1.ax1.RS2.set_visible(False)
+        tk.Toplevel.destroy(self)
