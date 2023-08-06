6e16de1e1b5ab100103682ab98646f3bc765bf8c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:14:06 +0000

    Add package audiomate/1.0.0/audiomate-1.0.0-py3-none-any.whl

---
+++ packages/audiomate/audiomate-1.0.0-py3-none-any.whl/audiomate/processing/pipeline/reduction.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+from . import base
+
+
+class Stack(base.Reduction):
+    """
+    Stack the features. All input matrices have to be of the same length (same number of frames).
+    """
+
+    def compute(self, chunk, sampling_rate, corpus=None, utterance=None):
+        return np.hstack(chunk.data)
