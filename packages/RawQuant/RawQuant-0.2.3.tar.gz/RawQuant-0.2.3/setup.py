934c92dd2229e06c244ace44a5cc6550118c78a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:59:54 +0000

    Add package RawQuant/0.2.3/RawQuant-0.2.3.tar.gz

---
+++ packages/RawQuant/RawQuant-0.2.3.tar.gz/RawQuant-0.2.3/setup.py
@@ -0,0 +1,34 @@
+from distutils.core import setup
+
+setup(
+    name='RawQuant',
+    packages=['RawQuant', 'RawQuant/RawFileReader'],
+    package_data={'RawQuant/RawFileReader': ['RawQuant/RawFileReader/*.dll',
+                                             'RawQuant/RawFileReader/.RawFileReaderLisence.txt']},
+    data_files=[('lib/site-packages/RawQuant/RawFileReader', ['RawQuant/RawFileReader/ThermoFisher.CommonCore.BackgroundSubtraction.dll',
+                                                              'RawQuant/RawFileReader/ThermoFisher.CommonCore.Data.dll',
+                                                              'RawQuant/RawFileReader/ThermoFisher.CommonCore.MassPrecisionEstimator.dll',
+                                                              'RawQuant/RawFileReader/ThermoFisher.CommonCore.RawFileReader.dll',
+                                                              'RawQuant/RawFileReader/RawFileReaderLicense.txt'])],
+    include_package_data=True,
+    version='0.2.3',
+    description='Package for extracting scan meta data and quantification information from Thermo .raw files',
+    long_description='RawQuant is a Python package for extracting scan meta data and quantification values from ' +
+                     'Thermo .raw files.',
+    author='Kevin Kovalchik',
+    author_email='kkovalchik@bcgsc.ca',
+    url='https://github.com/kevinkovalchik/RawQuant',
+    project_urls={'Documentation': 'https://github.com/kevinkovalchik/RawQuant/docs'},
+    keywords=['Orbitrap', 'mass spectrometery', 'proteomics', 'isobaric labelling', 'quantification', 'scan meta data'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Operating System :: Microsoft :: Windows',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Topic :: Scientific/Engineering :: Bio-Informatics',
+        'Topic :: Scientific/Engineering :: Chemistry',
+    ],
+    install_requires=['numpy', 'pandas', 'tqdm>=4', 'joblib', 'pythonnet'],
+    python_requires='>=3.6'
+)
