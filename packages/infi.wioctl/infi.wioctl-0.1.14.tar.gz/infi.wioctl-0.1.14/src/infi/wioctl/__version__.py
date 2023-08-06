67e1af51dcdedcfa36612c37abbbe92bb6c2c7ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:55:56 +0000

    Add package infi.wioctl/0.1.14/infi.wioctl-0.1.14.tar.gz

---
+++ packages/infi.wioctl/infi.wioctl-0.1.14.tar.gz/infi.wioctl-0.1.14/src/infi/wioctl/__version__.py
@@ -0,0 +1,11 @@
+__version__ = "0.1.14"
+__git_commiter_name__ = "Arnon Yaari"
+__git_commiter_email__ = "arnony@infinidat.com"
+__git_branch__ = u'(HEAD detached at v0.1.14)'
+__git_remote_tracking_branch__ = '(No remote tracking)'
+__git_remote_url__ = '(Not remote tracking)'
+__git_head_hash__ = '13306e3fc2b423a54ea34c7c3840715a9e56378e'
+__git_head_subject__ = u'Finished Release v0.1.14'
+__git_head_message__ = u''
+__git_dirty_diff__ = u''
+__git_commit_date__ = '2018-06-10 12:55:46'
