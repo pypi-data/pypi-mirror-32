0e177a4adc9f6f9e7362dd1d1d96443d2f4a3c58
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:26:11 +0000

    Add package django-simple-jsonfield/0.1/django-simple-jsonfield-0.1.tar.gz

---
+++ packages/django-simple-jsonfield/django-simple-jsonfield-0.1.tar.gz/django-simple-jsonfield-0.1/setup.py
@@ -0,0 +1,15 @@
+#! /usr/bin/env python3
+# -*- coding: UTF-8 -*-
+
+from setuptools import setup
+
+setup(name='django-simple-jsonfield',
+      version="0.1",
+      license="Public Domain",
+      url="http://github.org/devkral/django-simple-jsonfield/",
+      zip_safe=True,
+      platforms='Platform Independent',
+      packages=[
+        "jsonfield"
+      ],
+      test_suite="tests")
