c7aef6652b3f358babfda5317b3249ef71071114
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:27:15 +0000

    Add package jira-issue/0.0.3/jira-issue-0.0.3.tar.gz

---
+++ packages/jira-issue/jira-issue-0.0.3.tar.gz/jira-issue-0.0.3/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="jira-issue",
+    version="0.0.3",
+    author="Todd Wells",
+    author_email="todd@wells.ws",
+    description="Quickly create jira issues from the command line",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/twellspring/jira-issue",
+    install_requires=['jira', 'python-editor>=1.0.3'],
+    packages=setuptools.find_packages(),
+    scripts=['bin/jira-issue'],
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ),
+)
