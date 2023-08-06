dd69a3e9a52a43b96d7d6c6fc065433065de861f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:08:37 +0000

    Add package DIMEpy/0.1.1/dimepy-0.1.1.tar.gz

---
+++ packages/DIMEpy/dimepy-0.1.1.tar.gz/dimepy-0.1.1/dimepy/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+"""
+DIMEpy - direct infusion metablomics processing in python
+"""
+
+from Spectrum import Spectrum
+from SpectrumList import SpectrumList
+from Scans import Scans
+
+import numpy as np
+np.seterr("ignore")
