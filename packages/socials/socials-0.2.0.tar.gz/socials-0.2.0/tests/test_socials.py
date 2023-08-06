d42449628e1aec65ac30ac70a744edf186eaa76d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:13:09 +0000

    Add package socials/0.2.0/socials-0.2.0.tar.gz

---
+++ packages/socials/socials-0.2.0.tar.gz/socials-0.2.0/tests/test_socials.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Tests for `socials` package."""
+
+import pytest
+
+from click.testing import CliRunner
+
+import socials
+from socials import cli
+
+
+@pytest.fixture
+def response():
+    """Sample pytest fixture.
+
+    See more at: http://doc.pytest.org/en/latest/fixture.html
+    """
+    # import requests
+    # return requests.get('https://github.com/audreyr/cookiecutter-pypackage')
+
+
+def test_content(response):
+    """Sample pytest test function with the pytest fixture as an argument."""
+    # from bs4 import BeautifulSoup
+    # assert 'GitHub' in BeautifulSoup(response.content).title.string
+
+
+def test_command_line_interface():
+    """Test the CLI."""
+    runner = CliRunner()
+    result = runner.invoke(cli.main)
+    assert result.exit_code == 0
+    assert 'socials.cli.main' in result.output
+    help_result = runner.invoke(cli.main, ['--help'])
+    assert help_result.exit_code == 0
+    assert '--help  Show this message and exit.' in help_result.output
+
+
+def test_extract():
+    """Test the extract method."""
+    urls = [
+        'http://google.de',
+        'http://facebook.com',
+        'http://facebook.com/peterparker',
+        'mailto:bill@microsoft.com',
+        'steve@microsoft.com',
+    ]
+    extraction = socials.extract(urls)
+    matches = extraction.get_matches_per_platform()
+    assert 'facebook' in matches
+    assert matches['facebook'][0] == urls[2]
+    assert len(matches['facebook']) == 1
+
+    assert 'email' in matches
+    assert len(matches['email']) == 2
+    assert 'bill@microsoft.com' in matches['email']
+    assert 'steve@microsoft.com' in matches['email']
