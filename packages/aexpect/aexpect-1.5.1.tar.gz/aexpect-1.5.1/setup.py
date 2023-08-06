783804132b68f53209297c77d8ac7a73491005f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:35:22 +0000

    Add package aexpect/1.5.1/aexpect-1.5.1.tar.gz

---
+++ packages/aexpect/aexpect-1.5.1.tar.gz/aexpect-1.5.1/setup.py
@@ -0,0 +1,38 @@
+#!/bin/env python
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+#
+# See LICENSE for more details.
+#
+# Copyright: Red Hat Inc. 2013-2015
+# Author: Lucas Meneghel Rodrigues <lmr@redhat.com>
+
+import sys
+# pylint: disable=E0611
+
+from setuptools import setup
+
+
+if __name__ == '__main__':
+    if sys.version_info[0] == 2:
+        REQUIREMENTS = ['subprocess32>=3.2.6']
+    else:
+        REQUIREMENTS = []
+    setup(name='aexpect',
+          version='1.5.1',
+          description='Aexpect',
+          author='Aexpect developers',
+          author_email='avocado-devel@redhat.com',
+          url='http://avocado-framework.github.io/',
+          packages=['aexpect',
+                    'aexpect.utils'],
+          scripts=['scripts/aexpect-helper'],
+          use_2to3=True,
+          install_requires=REQUIREMENTS,
+          test_suite='tests')
