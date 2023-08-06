4fd4804abf78794dc114a34ef5cd9f4d2c4a7af6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:19:53 +0000

    Add package sharding/0.0.2a1/sharding-0.0.2a1-py3-none-any.whl

---
+++ packages/sharding/sharding-0.0.2a1-py3-none-any.whl/sharding/handler/utils/shard_tracker_utils.py
@@ -0,0 +1,23 @@
+from typing import (
+    Union,
+)
+
+from eth_utils import (
+    event_abi_to_log_topic,
+    to_checksum_address,
+)
+
+from sharding.contracts.utils.smc_utils import (
+    get_smc_json,
+)
+
+
+def to_log_topic_address(address: Union[bytes, str]) -> str:
+    return '0x' + to_checksum_address(address)[2:].rjust(64, '0')
+
+
+def get_event_signature_from_abi(event_name: str) -> bytes:
+    for function in get_smc_json()['abi']:
+        if function['name'] == event_name and function['type'] == 'event':
+            return event_abi_to_log_topic(function)
+    raise ValueError("Event with name {} not found".format(event_name))
