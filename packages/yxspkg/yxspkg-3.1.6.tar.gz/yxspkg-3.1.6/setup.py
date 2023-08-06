6b72a9ea5edd02eb6c89e44c7e5a69d4f6194cec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:19:55 +0000

    Add package yxspkg/3.1.6/yxspkg-3.1.6.tar.gz

---
+++ packages/yxspkg/yxspkg-3.1.6.tar.gz/yxspkg-3.1.6/setup.py
@@ -0,0 +1,50 @@
+from setuptools import setup  ,find_packages
+import yxspkg
+from pip._internal import main as _main
+import sys
+
+
+def install_(module):
+    args = ['install',module,'-U','--user']
+    _main(args)
+
+
+def run_install():
+    if sys.argv[1] == 'sdist':
+        return ''
+    install_modules = ['lxml', 'pandas', 'bs4', 'requests', 'PyQt5', 'imageio', 'rsa', 'scipy', 'matplotlib', 'opencv-python',
+                       'tushare', 'lulu', 'yxspkg_encrypt', 'yxspkg_tecfile', 'yxspkg_wget', 'lulu', 'ipython', 'sklearn',
+                       'yxspkg_songzgif', 'tensorflow', 'keras', 'pyinstaller']
+    module_name = {'pyinstaller': 'PyInstaller', 'opencv-python': 'cv2',
+                   'ipython': 'IPython'}
+    for i in install_modules:
+        try:
+            exec('import '+module_name.get(i, i))
+        except:
+            print('install ', i)
+            install_(i)
+    for i in install_modules:
+        try:
+            exec('import '+module_name.get(i, i))
+        except:
+            print(i, 'failed to install')
+    return ''
+
+
+install_modules = ['lxml', 'pandas', 'bs4', 'requests', 'PyQt5', 'imageio', 'rsa', 'scipy', 'matplotlib', 'opencv-python',
+                   'lulu', 'yxspkg_encrypt', 'yxspkg_tecfile', 'yxspkg_wget', 'lulu', 'ipython', 'sklearn',
+                   'yxspkg_songzgif', 'tensorflow', 'keras', 'pyinstaller']
+setup(name='yxspkg',   
+      version=yxspkg.__version__,    
+      description='My pypi module',    
+      author='Blacksong',    
+      install_requires=install_modules,
+      author_email='blacksong@yeah.net',       
+      url='https://github.com/blacksong',
+      packages=find_packages(), 
+      classifiers = [
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+)   
