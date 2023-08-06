f67d1c81a6e440d2bec8524c9253811baac40e41
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:25:48 +0000

    Add package medikit/0.6.3/medikit-0.6.3-py3.7.egg

---
+++ packages/medikit/medikit-0.6.3-py3.7.egg/medikit/settings.py
@@ -0,0 +1,13 @@
+DEFAULT_FEATURES = {
+    'git',
+    'make',
+}
+
+DEFAULT_FILES = {
+    'requirements',
+    'requirements-dev',
+    'classifiers',
+    'version',
+}
+
+YAPF_STYLE_CONFIG = 'pep8'
