ec9bb3841a36c7def9da24d8a8c51d74eabcf27f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:36:44 +0000

    Add package nesterLyc/1.0.0/nesterLyc-1.0.0.tar.gz

---
+++ packages/nesterLyc/nesterLyc-1.0.0.tar.gz/nesterLyc-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+        name        = 'nesterLyc',
+        version     = '1.0.0',
+        py_modules  = ['nester'],
+        author      = 'hfpython',
+        url         = 'http://www.headfirstlabs.com',
+        description = "A simple printer of nester lists",
+        )
