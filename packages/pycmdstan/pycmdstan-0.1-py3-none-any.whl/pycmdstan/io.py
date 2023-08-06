3af0e04deee0dac455d2963b82b9fd3b697623ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:07 +0000

    Add package pycmdstan/0.1/pycmdstan-0.1-py3-none-any.whl

---
+++ packages/pycmdstan/pycmdstan-0.1-py3-none-any.whl/pycmdstan/io.py
@@ -0,0 +1,214 @@
+"""
+I/O functions for working with CmdStan executables.
+
+"""
+
+import os
+import subprocess
+import numpy as np
+
+
+def _rdump_array(key, val):
+    c = 'c(' + ', '.join(map(str, val.T.flat)) + ')'
+    if (val.size, ) == val.shape:
+        return '{key} <- {c}'.format(key=key, c=c)
+    else:
+        dim = '.Dim = c{0}'.format(val.shape)
+        struct = '{key} <- structure({c}, {dim})'.format(key=key, c=c, dim=dim)
+        return struct
+
+
+def rdump(fname, data):
+    """Dump a dict of data to a R dump format file.
+    """
+    with open(fname, 'w') as fd:
+        for key, val in data.items():
+            if isinstance(val, np.ndarray) and val.size > 1:
+                line = _rdump_array(key, val)
+            else:
+                try:
+                    val = val.flat[0]
+                except:
+                    pass
+                line = '%s <- %s' % (key, val)
+            fd.write(line)
+            fd.write('\n')
+
+
+def rload(fname):
+    """Load a dict of data from an R dump format file.
+    """
+    with open(fname, 'r') as fd:
+        lines = fd.readlines()
+    data = {}
+    for line in lines:
+        lhs, rhs = [_.strip() for _ in line.split('<-')]
+        if rhs.startswith('structure'):
+            *_, vals, dim = rhs.replace('(', ' ').replace(')', ' ').split('c')
+            vals = [float(v) for v in vals.split(',')[:-1]]
+            dim = [int(v) for v in dim.split(',')]
+            val = np.array(vals).reshape(dim[::-1]).T
+        elif rhs.startswith('c'):
+            val = np.array([float(_) for _ in rhs[2:-1].split(',')])
+        else:
+            try:
+                val = int(rhs)
+            except:
+                try:
+                    val = float(rhs)
+                except:
+                    raise ValueError(rhs)
+        data[lhs] = val
+    return data
+
+
+def merge_csv_data(*csvs, skip=0):
+    data_ = {}
+    for csv in csvs:
+        for key, val in csv.items():
+            val = val[skip:]
+            if key in data_:
+                data_[key] = np.concatenate((data_[key], val), axis=0)
+            else:
+                data_[key] = val
+    return data_
+
+
+def parse_csv(fname, merge=True):
+    if '*' in fname:
+        import glob
+        return parse_csv(glob.glob(fname), merge=merge)
+    if isinstance(fname, (list, tuple)):
+        csv = []
+        for _ in fname:
+            try:
+                csv.append(parse_csv(_))
+            except Exception as e:
+                print('skipping ', fname, e)
+        if merge:
+            csv = merge_csv_data(*csv)
+        return csv
+
+    lines = []
+    with open(fname, 'r') as fd:
+        for line in fd.readlines():
+            if not line.startswith('#'):
+                lines.append(line.strip().split(','))
+    names = [field.split('.') for field in lines[0]]
+    data = np.array([[float(f) for f in line] for line in lines[1:]])
+
+    namemap = {}
+    maxdims = {}
+    for i, name in enumerate(names):
+        if name[0] not in namemap:
+            namemap[name[0]] = []
+        namemap[name[0]].append(i)
+        if len(name) > 1:
+            maxdims[name[0]] = name[1:]
+
+    for name in maxdims.keys():
+        dims = []
+        for dim in maxdims[name]:
+            dims.append(int(dim))
+        maxdims[name] = tuple(reversed(dims))
+
+    # data in linear order per Stan, e.g. mat is col maj
+    # TODO array is row maj, how to distinguish matrix v array[,]?
+    data_ = {}
+    for name, idx in namemap.items():
+        new_shape = (-1, ) + maxdims.get(name, ())
+        data_[name] = data[:, idx].reshape(new_shape)
+
+    return data_
+
+
+def parse_summary_csv(fname):
+    skeys = []
+    svals = []
+    with open(fname, 'r') as fd:
+        scols = fd.readline().strip().split(',')
+        for line in fd.readlines():
+            if '"' not in line:
+                continue
+            if line.startswith('#'):
+                break
+            _, k, v = line.split('"')
+            skeys.append(k)
+            svals.append(np.array([float(_) for _ in v.split(',')[1:]]))
+    svals = np.array(svals)
+
+    sdat = {}
+    sdims = {}
+    for skey, sval in zip(skeys, svals):
+        if '[' in skey:
+            name, dim = skey.replace('[', ']').split(']')[:-1]
+            dim = tuple(int(i) for i in dim.split(','))
+            sdims[name] = dim
+            if name not in sdat:
+                sdat[name] = []
+            sdat[name].append(sval)
+        else:
+            sdat[skey] = sval
+
+    for key in [_ for _ in sdat.keys()]:
+        if key in sdims:
+            sdat[key] = np.array(sdat[key]).reshape(sdims[key] + (-1, ))
+    return scols, sdat
+
+
+def csv2mode(csv_fname, mode=None):
+    csv = parse_csv(csv_fname)
+    data = {}
+    for key, val in csv.items():
+        if key.endswith('__'):
+            continue
+        if mode is None:
+            val_ = val[0]
+        elif mode == 'mean':
+            val_ = val.mean(axis=0)
+        elif mode[0] == 'p':
+            val_ = np.percentile(val, int(mode[1:]), axis=0)
+        data[key] = val_
+    return data
+
+
+def csv2r(csv_fname, r_fname=None, mode=None):
+    data = csv2mode(csv_fname, mode=mode)
+    r_fname = r_fname or csv_fname + '.R'
+    rdump(r_fname, data)
+
+
+# TODO class to run modules instead of %%bash in ipynb
+
+from threading import Thread
+
+
+class FollowCSV:
+    def __init__(self, csv_fname):
+        self.csv_fname = csv_fname
+        self.thread = Thread(target=self._run)
+        self.thread.start()
+        self._line = ''
+        self.read = True
+
+    @property
+    def line(self):
+        self.read = True
+        return self._line
+
+    def _run(self):
+        while True:
+            try:
+                self._follow()
+            except Exception as exc:
+                print(exc)
+
+    def _follow(self):
+        with open(self.csv_fname, 'r') as fd:
+            while True:
+                line = fd.readline()
+                if line:
+                    self._line = line
+                    self.read = False
+                else:
+                    time.sleep(0.2)
---
+++ packages/pycmdstan/pycmdstan-0.1-py3-none-any.whl/pycmdstan/model.py
@@ -0,0 +1,159 @@
+import os
+import enum
+import subprocess
+import hashlib
+import logging
+import tempfile
+import threading
+import filelock
+import numpy as np
+from . import io, psis
+
+logger = logging.getLogger('pycmdstan.model')
+
+
+class CmdStanNotFound(RuntimeError):
+    pass
+
+
+def _find_cmdstan(path=''):
+    if path:
+        path = os.path.expanduser(os.path.expandvars(path))
+        os.environ['CMDSTAN'] = path
+    path = os.environ.get('CMDSTAN', 'cmdstan')
+    if not os.path.exists(os.path.join(path, 'runCmdStanTests.py')):
+        raise CmdStanNotFound(
+            'please provide CmdStan path, e.g. lib.cmdstan_path("/path/to/")')
+    return path
+
+
+def preprocess_model(stan_fname, hpp_fname=None, overwrite=False):
+    hpp_fname = hpp_fname or stan_fname.replace('.stan', '.hpp')
+    stanc_path = os.path.join(_find_cmdstan(), 'bin', 'stanc')
+    cmd = [stanc_path, f'--o={hpp_fname}', f'{stan_fname}']
+    cwd = os.path.abspath(os.path.dirname(stan_fname))
+    subprocess.check_call(cmd, cwd=cwd)
+
+
+def compile_model(stan_fname):
+    path = os.path.abspath(os.path.dirname(stan_fname))
+    name = stan_fname[:-5]
+    target = os.path.join(path, name)
+    proc = subprocess.Popen(
+        ['make', target],
+        cwd=_find_cmdstan(),
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+    )
+    stdout = proc.stdout.read().decode('ascii').strip()
+    if stdout:
+        print(stdout)
+    stderr = proc.stderr.read().decode('ascii').strip()
+    if stderr:
+        print(stderr)
+
+
+def model_path() -> str:
+    key = 'PYCMDSTAN_MODEL_PATH'
+    home = os.path.expanduser('~')
+    if key not in os.environ:
+        os.environ[key] = os.path.join(home, '.cache', 'pycmdstan')
+    return os.environ[key]
+
+
+class Model:
+    """Stan model.
+    """
+
+    def __init__(self, code: str = None, fname: os.PathLike = None):
+        self.code = code
+        if fname and code is None:
+            with open(fname, 'r') as fd:
+                self.code = fd.read().decode('ascii')
+        # TODO replace w/ file lock
+        self._compile_lock = threading.Lock()
+
+    @property
+    def sha256(self) -> str:
+        sha = hashlib.sha256()
+        sha.update(self.code.encode('ascii'))
+        return f'sha256{sha.hexdigest()}'
+
+    def _compile(self, stan_fname):
+        with open(stan_fname, 'w') as fd:
+            fd.write(self.code)
+        compile_model(stan_fname)
+
+    def compile(self, path=None):
+        self.path = path or model_path()
+        self.stan_fname = os.path.join(self.path, f'{self.sha256}.stan')
+        self.exe, _ = stan_fname.rsplit('.stan')
+        self._lock = filelock.FileLock(f'{self.exe}.lock')
+        with self.lock:
+            if not os.path.exists(self.exe):
+                self._compile(stan_fname)
+
+
+class Method(enum.Enum):
+    optimize = 'optimize'
+    sample = 'sample'
+    variational = 'variational'
+
+
+class Run:
+    def __init__(self,
+                 model: Model,
+                 method: Method,
+                 data: dict = None,
+                 method_args: dict = None,
+                 id: int = None,
+                 log_lik: str = 'log_lik',
+                 start: bool = True):
+        self.model = model
+        self.id = id
+        self.log_lik = log_lik
+        self.method = method.value if isinstance(method, Method) else method
+        self.method_args = method_args
+        self.data = data
+        self.tmp_dir = tempfile.TemporaryDirectory()
+        self.output_csv_fname = os.path.join(self.tmp_dir.name, 'output.csv')
+        self.data_R_fname = os.path.join(self.tmp_dir.name, 'data.R')
+        io.rdump(self.data_R_fname, data or {})
+        self.start()
+
+    def start(self, wait=True):
+        if not hasattr(self.model, 'exe'):
+            self.model.compile()
+        self.cmd = cmd = [self.model.exe]
+        if self.id is not None:
+            cmd.append(f'id={self.id}')
+        cmd.append(self.method)
+        if self.method_args:
+            for key, val in self.method_args.items():
+                cmd.append(f'{key}={val}')
+        cmd.extend(['data', f'file={self.data_R_fname}'])
+        cmd.extend(['output', f'file={self.output_csv_fname}'])
+        logger.warning('starting run with cmd %s', ' '.join(cmd))
+        self.proc = subprocess.Popen(cmd)
+        if wait:
+            self.wait()
+
+    def wait(self):
+        if not hasattr(self, 'proc'):
+            self.start(wait=False)
+        self.proc.wait()
+        if self.proc.returncode != 0:
+            msg = 'Stan model exited with an error (%d)'
+            raise RuntimeError(msg, self.proc.returncode)
+
+    @property
+    def csv(self):
+        if not hasattr(self, '_csv'):
+            self.wait()
+            self._csv = io.parse_csv(self.output_csv_fname)
+            if self.log_lik in self._csv:
+                self._csv.update(psis.psisloo(-self._csv[self.log_lik]))
+        return self._csv
+
+    def __getitem__(self, key):
+        return self.csv[key]
