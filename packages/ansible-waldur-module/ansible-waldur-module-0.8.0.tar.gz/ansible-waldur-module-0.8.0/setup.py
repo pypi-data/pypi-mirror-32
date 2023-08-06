f3b17236aa9cf193225d17df92aa986190589339
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:37:03 +0000

    Add package ansible-waldur-module/0.8.0/ansible-waldur-module-0.8.0.tar.gz

---
+++ packages/ansible-waldur-module/ansible-waldur-module-0.8.0.tar.gz/ansible-waldur-module-0.8.0/setup.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+install_requires = [
+    'requests>=2.6.0',
+]
+
+tests_requires = [
+    'responses>=0.5.0',
+]
+
+
+setup(
+    name='ansible-waldur-module',
+    version='0.8.0',
+    author='OpenNode Team',
+    author_email='info@opennodecloud.com',
+    url='https://waldur.com',
+    license='MIT',
+    description='Ansible module for the Waldur API.',
+    long_description=open('README.rst').read(),
+    py_modules=['waldur_client'],
+    install_requires=install_requires,
+    tests_require=tests_requires,
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
