c7f3f78950613cf56730be70b81de6590a26d3e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:22:39 +0000

    Add package zhn-nester/1.0.0/zhn_nester-1.0.0.tar.gz

---
+++ packages/zhn-nester/zhn_nester-1.0.0.tar.gz/zhn_nester-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name='zhn_nester',
+    version='1.0.0',
+    py_modules=['zhn_nester'],
+    author='zhn',
+    author_email='zhengzhanpeng2013@126.com',
+    url='',
+    description='A test',
+)
