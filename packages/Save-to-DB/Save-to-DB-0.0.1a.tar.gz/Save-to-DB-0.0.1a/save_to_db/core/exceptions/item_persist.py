597b576e27b8b6378e5cf531ea63ff24b56d9ff7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:38:23 +0000

    Add package Save-to-DB/0.0.1a/Save-to-DB-0.0.1a.tar.gz

---
+++ packages/Save-to-DB/Save-to-DB-0.0.1a.tar.gz/Save-to-DB-0.0.1a/save_to_db/core/exceptions/item_persist.py
@@ -0,0 +1,19 @@
+""" This module contains exceptions for
+:py:class:`~save_to_db.core.item_base.ItemBase` class related to persistence.
+"""
+
+
+class ItemPersistError(Exception):
+    """ General exception for :py:class:`~save_to_db.core.item_base.ItemBase`
+    persistence.
+    """
+    
+
+class MultipleItemsMatch(ItemPersistError):
+    """ Raised when multiple items match the same model. Applicable to bulk
+    items.
+    """
+    
+    
+class MultipleModelsMatch(ItemPersistError):
+    """ Raised when multiple models match the same item. """
