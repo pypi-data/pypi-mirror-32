567e9016f50d324f48c6d6c693e4c18f3ab801bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:14:01 +0000

    Add package D3HRE/1.0.1/D3HRE-1.0.1.tar.gz

---
+++ packages/D3HRE/D3HRE-1.0.1.tar.gz/D3HRE-1.0.1/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='D3HRE',
+    version='1.0.1',
+    packages=['D3HRE', 'D3HRE.core', 'gsee', 'gsee.gsee', 'opendap_download'],
+    license='GPL-3',
+    author='caoyu',
+    author_email='tsaoyu@gmail.com',
+    url='https://github.com/tsaoyu/D3HRE',
+    description='Data Driven Dynamic Hybrid Renewable Energy suite'
+)
+
