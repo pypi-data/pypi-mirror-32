e302cbe3835f28f8e0170c4ac8525fc6a00c81a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:17 +0000

    Add package terraref-laser3d/1.0.1/terraref-laser3d-1.0.1.tar.gz

---
+++ packages/terraref-laser3d/terraref-laser3d-1.0.1.tar.gz/terraref-laser3d-1.0.1/setup.py
@@ -0,0 +1,18 @@
+from setuptools import find_packages, setup
+
+setup(name='terraref-laser3d',
+      version='1.0.1',
+      packages=find_packages(),
+      namespace_packages=['terraref'],
+      include_package_data=True,
+      url='https://github.com/terraref/laser3d',
+      install_requires=[
+            'numpy',
+            'scipy',
+            'multiprocessing',
+            'matplotlib',
+            'Pillow',
+            'laspy',
+            'plyfile'
+      ]
+      )
