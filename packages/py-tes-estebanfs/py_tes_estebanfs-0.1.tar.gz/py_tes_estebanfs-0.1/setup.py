7892564b4f5275eb9aa33b02cbfea8e9b8ffbe63
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:25:29 +0000

    Add package py-tes-estebanfs/0.1/py_tes_estebanfs-0.1.tar.gz

---
+++ packages/py-tes-estebanfs/py_tes_estebanfs-0.1.tar.gz/py_tes_estebanfs-0.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+  name = 'py_tes_estebanfs',
+  packages = ['py_tes_estebanfs'], # this must be the same as the name above
+  version = '0.1',
+  description = 'A random test lib',
+  author = 'Esteban Foronda',
+  author_email = 'seforonda@gmail.com',
+  url = 'https://github.com/EstebanFS/PypiTest', # use the URL to the github repo
+  download_url = '', # I'll explain this in a second
+  keywords = ['testing', 'logging', 'example'], # arbitrary keywords
+  classifiers = [],
+)
