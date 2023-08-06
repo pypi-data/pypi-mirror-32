b291f808732db9374c6b28ffd8cfc0bb42fefe8e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:56:58 +0000

    Add package pytwask/0.2.0/pytwask-0.2.0-py3-none-any.whl

---
+++ packages/pytwask/pytwask-0.2.0-py3-none-any.whl/pytwask-0.2.0.data/scripts/autopytwask.py
@@ -0,0 +1,7 @@
+#!python
+# -*- coding: utf-8 -*-
+
+import os
+
+from pytwask import create_app
+app = create_app(os.getenv('PYTWASK_ENV', 'dev'))
---
+++ packages/pytwask/pytwask-0.2.0-py3-none-any.whl/pytwask/__init__.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+"""Package pytwask"""
+
+import datetime
+
+from flask import Flask
+from flask_login import LoginManager
+from flask_moment import Moment
+from flask_debugtoolbar import DebugToolbarExtension
+
+from .config import config_by_name
+
+# Configure authentication
+login_manager = LoginManager()  # pylint: disable=invalid-name
+login_manager.session_protection = 'strong'
+login_manager.login_view = 'auth.index'
+
+# Enable debugtoolbar
+toolbar = DebugToolbarExtension()  # pylint: disable=invalid-name
+
+# For displaying timestamps
+moment = Moment()  # pylint: disable=invalid-name
+
+def create_app(config_name):
+    """Create and initialize the Flask application.
+
+    Here we follow the Flask typical pattern of "Application Factories".
+    For details, please refer to:
+
+    http://flask.pocoo.org/docs/0.12/patterns/appfactories/
+    """
+    app = Flask(__name__)
+    app.config.from_object(config_by_name[config_name])
+
+    login_manager.init_app(app)
+    toolbar.init_app(app)
+    moment.init_app(app)
+
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint, url_prefix='/')
+
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint, url_prefix='/auth')
+
+    from .tweets import tweets as tweets_blueprint
+    app.register_blueprint(tweets_blueprint, url_prefix='/tweets')
+
+    return app
