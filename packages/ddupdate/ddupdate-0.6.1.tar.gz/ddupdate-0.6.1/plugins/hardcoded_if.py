3ad0147e95ed88cda5644d0890cf0e48659607ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:51:24 +0000

    Add package ddupdate/0.6.1/ddupdate-0.6.1.tar.gz

---
+++ packages/ddupdate/ddupdate-0.6.1.tar.gz/ddupdate-0.6.1/plugins/hardcoded_if.py
@@ -0,0 +1,34 @@
+"""
+ddupdate plugin providing an ip address from an interface option.
+
+See: ddupdate(8)
+"""
+
+import subprocess
+import sys
+
+from ddupdate.ddplugin import AddressPlugin, IpAddr, dict_of_opts
+
+
+class HardcodedIfPlugin(AddressPlugin):
+    """
+    Use address on hardcoded interface.
+
+    Options:
+        if=interface
+    """
+
+    _name = 'hardcoded-if'
+    _oneliner = 'Get address from an configuration option interface'
+
+    def get_ip(self, log, options):
+        """Implement AddressPlugin.get_ip()."""
+        opts = dict_of_opts(options)
+        if 'if' not in opts:
+            log.error("Required option if= missing, giving up.")
+            sys.exit(2)
+        if_ = opts['if']
+        address = IpAddr()
+        output = subprocess.getoutput('ip address show dev ' + if_)
+        address.parse_ifconfig_output(output)
+        return address
