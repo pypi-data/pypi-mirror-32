edce777f4c95de9740aa90cd9d5c7416336e6f8a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:26:46 +0000

    Add package rainbowmindmachine/0.8.3/rainbowmindmachine-0.8.3.tar.gz

---
+++ packages/rainbowmindmachine/rainbowmindmachine-0.8.3.tar.gz/rainbowmindmachine-0.8.3/rainbowmindmachine/README.md
@@ -0,0 +1,113 @@
+# Code Layout
+
+Rainbow Mind Machine code is layed out as follows:
+
+## Lumberjack Logger
+
+The Lumberjack object creates a log and provides 
+a way to interface with the log.
+
+The default Lumberjack object uses python's
+`logging` module.
+
+The Lumberjack class can be extended to send log info
+to other destinations, such as message queues or 
+monitoring databases.
+
+## Sheep (Twitter Bot Objects)
+
+Bot objects own keys for a particular bot account.
+
+Bot objects own an interface to Twitter for that bot account.
+
+Bot objects own whatever things they need to tweet (poems, files, keys, accounts, etc.).
+
+Each bot object has two timescales: the "outer wait" (residence time, longer in-between wait), 
+and the "inner wait" (events timescale, in-betweeen individual lines or tweets).
+
+Twitter object takes care of interfacing with Twitter, creating instances of Twitter API, 
+uploading media, sending tweets, etc. For every bot, the tweet functionality works the same way.
+
+### Sheep Example
+
+I'll use the example of a Milton bot to illustrate how this works.
+
+Each Milton bot owns a poem, which it tweets, one line at a time, in perpetuity.
+
+Each Milton account also owns one Twitter account, which it is using to tweet the poem,
+and owns that account by owning an API key. This API key is contained in a JSON file.
+By giving each bot its own JSON file, we associate each bot with its own Twitter account.
+
+(This account setup is done by the Keymaker class.)
+
+The outer loop represents instances of finishing the whole poem.
+The outer wait is the amount of time the bot waits once it has finished
+the poem, before beginning it again.
+
+The inner loop controls the line-by-line events. 
+The inner wait is the amount of time the bot waits between each line of the poem.
+
+## Shepherd (Twitter Bot Manager)
+
+The Shepherd manages all of the Sheep bots.
+It iterates through keys/poems/items 
+and instantiates one Sheep for each item.
+Once the Sheep are instantiated, they are 
+pretty much roaming free on their own.
+The Sheep manage their own tweeting behavior,
+the Shepherd is mainly there to create the flock.
+
+When performing actions on the entire flock,
+the Shepherd is basically "wrapping" those actions
+for each sheep.
+
+Rather than define a wrapper for each individual action,
+specific actions are defined in Sheep, and are wrapped by
+a generic action method with an action keyword.
+
+Shepherds can run an action in serial, iterating over
+each sheep and performing the action. This is good for 
+one-time actions like setting profile information.
+
+Shepherds can also run actions in paralell, creating one 
+thread per sheep. This is good for forever actions
+like generating and sending tweets.
+
+
+# Other Notes
+
+## Keymaker
+
+Keymaker is the only class that uses apikeys.py, which contains
+the app's consumer tokens. these are used to get authentication 
+tokens.
+
+The app-level information in apikeys is also bundled with the 
+account-level information created by Keymaker, so Keymaker 
+puts both into the Json file it creates. 
+
+This way, apikeys.py is only required once, at time of key creation.
+When Sheep load the json file, they have all the information 
+they need.
+
+## Shepherd
+
+The Shepherd class is like a free-range parent.
+The Sheep manage all of their own information,
+and never need the Shepherd for anything.
+The Shepherd just sets them to pasture.
+
+## Sheep
+
+The Sheep class is intended to provide sensible 
+defaults, but also to be extremely flexible.
+By overriding the various methods of the Sheep class,
+we can obtain precisely the behavior we want - 
+whether it's tweeting once a day, once an hour, 
+or totally at random. 
+
+The Sheep class can also handle complicated procedures
+for populating the tweet queue. The Sheep class separates
+the population of a tweet queue from the tweet action itself,
+and this separation allows tweet population to be complicated.
+
---
+++ packages/rainbowmindmachine/rainbowmindmachine-0.8.3.tar.gz/rainbowmindmachine-0.8.3/tests/test_keymaker.py
@@ -0,0 +1,171 @@
+import rainbowmindmachine as rmm
+from unittest import TestCase
+import os, subprocess
+from .utils import captured_output
+
+
+"""
+Test Keymaker classes
+"""
+
+
+thisdir = os.path.abspath(os.path.dirname(__file__))
+
+class TestKeymaker(TestCase):
+    """
+    Test the Keymaker class.
+
+    This focuses on testing the constructors
+    and the API key initialization process.
+    """
+
+    @classmethod
+    def setUpClass(self):
+        self.keys_dir = os.path.join( thisdir, "test_keymaker_keys" )
+        self.api_keys = os.path.join( thisdir, "apikeys.json" )
+
+        # Note: this gives the same outcome as 
+        # hard-coding the path "tests"
+
+    def test_default_keymaker_apikeys_env(self):
+        """
+        Test ability to create single key using consumer token from environment vars
+        """
+        keymaker = rmm.Keymaker()
+
+        # Set application API keys
+        os.environ['CONSUMER_TOKEN'] = 'AAAAA'
+        os.environ['CONSUMER_TOKEN_SECRET'] = 'BBBBB'
+        keymaker.set_apikeys_env()
+
+        # Create bot API keys
+        with captured_output() as (out, err):
+            keymaker.make_a_key(
+                name = 'test_default_keymaker_apikeys_env',
+                json = 'test_default_keymaker_apikeys_env.json',
+                keys_out_dir=self.keys_dir,
+                interactive=False)
+
+        # Clean up
+        os.environ['CONSUMER_TOKEN'] = ''
+        os.environ['CONSUMER_TOKEN_SECRET'] = ''
+
+        # Assert
+        output = out.getvalue().strip()
+        self.assertIn('Did not export a key bundle', output)
+
+    def test_default_keymaker_apikeys_file(self):
+        """
+        Test ability to create single key using consumer token/secret from JSON file
+        """
+        keymaker = rmm.Keymaker()
+
+        # Set application API keys
+        apikeys = os.path.join(thisdir,'apikeys.json')
+        keymaker.set_apikeys_file(apikeys)
+
+        # Create bot API keys
+        with captured_output() as (out, err):
+            keymaker.make_a_key(
+                name = 'test_default_keymaker_apikeys_file',
+                json = 'test_default_keymaker_apikeys_file.json',
+                keys_out_dir=self.keys_dir,
+                interactive=False)
+
+        # Assert
+        output = out.getvalue().strip()
+        self.assertIn('Did not export a key bundle', output)
+
+    def test_default_keymaker_apikeys_dict(self):
+        """
+        Test ability to create single key using consumer token/secret from dictionary
+        """
+        keymaker = rmm.Keymaker()
+
+        # Set application API keys
+        keymaker.set_apikeys_dict({ 
+            'consumer_token': 'AAAAA',
+            'consumer_token_secret': 'BBBBB'
+        })
+
+        # Create bot API keys
+        with captured_output() as (out, err):
+            keymaker.make_a_key(
+                name = 'test_default_keymaker_apikeys_dict',
+                json = 'test_default_keymaker_apikeys_dict.json',
+                keys_out_dir = self.keys_dir,
+                interactive = False)
+
+        # Assert
+        output = out.getvalue().strip()
+        self.assertIn('Did not export a key bundle', output)
+
+    @classmethod
+    def tearDownClass(self):
+        # Remove the keys directory we created
+        subprocess.call(['rm','-rf',self.keys_dir])
+
+
+
+class TestFilesKeymaker(TestCase):
+    """
+    Test file-based keymaker classes.
+    """
+
+    @classmethod
+    def setUpClass(self):
+        self.keys_dir = os.path.join( thisdir, "test_keymaker_files_keys" )
+        self.api_keys = os.path.join( thisdir, "apikeys.json" )
+
+    def test_files_keymaker(self):
+        """
+        Test the FilesKeymaker, which makes one key
+        for each file with a given extension 
+        in a given directory.
+        """
+        keymaker = rmm.FilesKeymaker()
+
+        # Set application API keys
+        apikeys = os.path.join(thisdir,'apikeys.json')
+        keymaker.set_apikeys_file(apikeys)
+
+        # Create bot API keys
+        # from files in filesdir
+        filesdir = os.path.join(thisdir,'files/')
+        with captured_output() as (out, err):
+            keymaker.make_keys( filesdir,
+                                keys_out_dir=self.keys_dir,
+                                interactive=False)
+
+        # Assert
+        output = out.getvalue().strip()
+        self.assertIn('Did not export a key bundle', output)
+
+    def test_txt_keymaker(self):
+        """
+        Test the TxtKeymaker, which makes one key
+        for each .txt file in a given directory.
+        """
+        keymaker = rmm.TxtKeymaker()
+
+        # Set application API keys
+        apikeys = os.path.join(thisdir,'apikeys.json')
+        keymaker.set_apikeys_file(apikeys)
+
+        # Create bot API keys
+        txtdir = os.path.join(thisdir,'txt/')
+        with captured_output() as (out, err):
+            keymaker.make_keys(txtdir,
+                keys_out_dir=self.keys_dir,
+                interactive=False)
+
+        # Assert
+        output = out.getvalue().strip()
+        self.assertIn('Did not export a key bundle', output)
+
+
+    @classmethod
+    def tearDownClass(self):
+        # Remove the keys directory we created
+        subprocess.call(['rm','-rf',self.keys_dir])
+
