fedbf7739fe382f6b9d934d8ced99d092a73ba96
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:13:37 +0000

    Add package depynd/0.4.6/depynd-0.4.6.tar.gz

---
+++ packages/depynd/depynd-0.4.6.tar.gz/depynd-0.4.6/depynd/feature_selection/_mifs.py
@@ -0,0 +1,59 @@
+import numpy as np
+
+from depynd.information import conditional_mutual_information
+
+
+def mifs(X, y, lamb=0.0, **kwargs):
+    """Select effective features in X on predinting y using mutual-information-based feature selection [1]_.
+
+    Parameters
+    ----------
+    X : array-like, shape (n_samples, d)
+        Features.
+    y : array-like, shape (n_samples)
+        Target. Can be either continuous or discrete.
+    lamb: float
+        Threshold for independence tests.
+    kwargs : dict, default None
+        Optional parameters for MI estimation.
+
+    Returns
+    -------
+    indices : list
+        Indices for the selected features.
+
+    References
+    ----------
+    .. [1] Brown, Gavin, et al. "Conditional likelihood maximisation: a unifying framework for information theoretic
+    feature selection." Journal of machine learning research 13.Jan (2012): 27-66.
+    """
+    n, d = X.shape
+    selected = []
+    while True:
+        max_cmi = -np.inf
+        not_selected = set(range(d)) - set(selected)
+        z = X[:, selected]
+        for i in not_selected:
+            x = X[:, i]
+            cmi = conditional_mutual_information(x, y, z, **kwargs)
+            if max_cmi < cmi:
+                max_cmi = cmi
+                max_idx = i
+        if max_cmi <= lamb or len(selected) == d:
+            break
+        selected.append(max_idx)
+
+    while True:
+        min_cmi = np.inf
+        for i in selected:
+            x = X[:, i]
+            z = X[:, list(set(selected) - {i})]
+            cmi = conditional_mutual_information(x, y, z, **kwargs)
+            if min_cmi > cmi:
+                min_cmi = cmi
+                min_idx = i
+
+        if min_cmi >= lamb or len(selected) == 0:
+            return selected
+
+        selected.remove(min_idx)
---
+++ packages/depynd/depynd-0.4.6.tar.gz/depynd-0.4.6/depynd/feature_selection/_select.py
@@ -0,0 +1,31 @@
+from sklearn.utils.validation import check_X_y
+
+from depynd.feature_selection import mrmr, mifs
+
+
+def select(X, y, lamb=0.0, method='mifs', **kwargs):
+    """Select effective features in X on predinting y using
+
+    Parameters
+    ----------
+    X : array-like, shape (n_samples, d)
+        Features.
+    y : array-like, shape (n_samples)
+        Target. Can be either continuous or discrete.
+    lamb: float, default 0.0
+        Threshold for independence tests.
+    method: str, default 'mifs'
+        Method used for feature selection. Either 'mifs' or 'mrmr' can be chosen.
+
+    Returns
+    -------
+    indices : list
+        Indices for the selected features.
+    """
+    X, y = check_X_y(X, y)
+    if method == 'mifs':
+        return mifs(X, y, lamb=lamb, **kwargs)
+    elif method == 'mrmr':
+        return mrmr(X, y, lamb=lamb, **kwargs)
+    else:
+        raise NotImplementedError('Method %s is not implemented. Use mifs or mrmr.' % method)
