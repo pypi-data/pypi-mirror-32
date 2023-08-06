6473d9a8805250bb2fd87e5d7c64db9e95e0dbb0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:37:55 +0000

    Add package yadisk/1.2.10/yadisk-1.2.10-py3-none-any.whl

---
+++ packages/yadisk/yadisk-1.2.10-py3-none-any.whl/yadisk/functions/operations.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from ..api import GetOperationStatusRequest
+
+__all__ = ["get_operation_status"]
+
+def get_operation_status(session, operation_id, *args, **kwargs):
+    """
+        Get operation status.
+
+        :param session: an instance of :any:`requests.Session` with prepared headers
+        :param operation_id: ID of the operation or a link
+        :param fields: list of keys to be included in the response
+
+        :returns: `str`
+    """
+
+    request = GetOperationStatusRequest(session, operation_id, *args, **kwargs)
+    request.send()
+
+    return request.process().status
---
+++ packages/yadisk/yadisk-1.2.10-py3-none-any.whl/yadisk/objects/ErrorObject.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from .YaDiskObject import YaDiskObject
+
+__all__ = ["ErrorObject"]
+
+class ErrorObject(YaDiskObject):
+    """
+        Mirrors Yandex.Disk REST API error object.
+
+        :param error: `dict` or `None`
+
+        :ivar message: `str`, human-readable error message
+        :ivar description: `str`, technical error description
+        :ivar error: `str`, error code
+    """
+
+    def __init__(self, error=None):
+        YaDiskObject.__init__(self, {"message":     str,
+                                     "description": str,
+                                     "error":       str})
+        self.set_alias("error_description", "message")
+        self.import_fields(error)
