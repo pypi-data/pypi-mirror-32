286d80f3c853afe8b0a3225c102c1b73d379fb07
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:56:44 +0000

    Add package mypy/0.610/mypy-0.610.tar.gz

---
+++ packages/mypy/mypy-0.610.tar.gz/mypy-0.610/test-data/unit/lib-stub/mypy_extensions.pyi
@@ -0,0 +1,23 @@
+# NOTE: Requires fixtures/dict.pyi
+
+from typing import Dict, Type, TypeVar, Optional, Any
+
+_T = TypeVar('_T')
+
+
+def Arg(type: _T = ..., name: Optional[str] = ...) -> _T: ...
+
+def DefaultArg(type: _T = ..., name: Optional[str] = ...) -> _T: ...
+
+def NamedArg(type: _T = ..., name: Optional[str] = ...) -> _T: ...
+
+def DefaultNamedArg(type: _T = ..., name: Optional[str] = ...) -> _T: ...
+
+def VarArg(type: _T = ...) -> _T: ...
+
+def KwArg(type: _T = ...) -> _T: ...
+
+
+def TypedDict(typename: str, fields: Dict[str, Type[_T]], *, total: Any = ...) -> Type[dict]: ...
+
+class NoReturn: pass
