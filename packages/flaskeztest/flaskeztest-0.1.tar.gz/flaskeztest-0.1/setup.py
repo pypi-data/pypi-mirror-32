f9ed58ed9a971206d728f82fc93a00d3fda77316
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:12:11 +0000

    Add package flaskeztest/0.1/flaskeztest-0.1.tar.gz

---
+++ packages/flaskeztest/flaskeztest-0.1.tar.gz/flaskeztest-0.1/setup.py
@@ -0,0 +1,32 @@
+"""Setup flask-eztest."""
+
+from setuptools import setup
+
+requirements = [
+    'Flask',
+    'capybara-py',
+    'selenium',
+    'Flask-SQLAlchemy',
+    'termcolor'
+]
+
+description = "Flask test generation based on db models."
+
+long_description = open('README.rst').read()
+
+setup(
+    name='flaskeztest',
+    version='0.1',
+    author='Stanley Rokita',
+    author_email='srok35@gmail.com',
+    url='https://github.com/Srokit/flask-eztest',
+    description=description,
+    long_description=long_description,
+    install_requires=requirements,
+    packages=['flaskeztest'],
+    entry_points={
+        'console_scripts': [
+            'eztest = flaskeztest.__main__:flaskeztest_main'
+        ]
+    }
+)
