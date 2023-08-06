0f983e27347df1057c4e489594c13b7af21730cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:17:19 +0000

    Add package pmda/0.1.1/pmda-0.1.1.tar.gz

---
+++ packages/pmda/pmda-0.1.1.tar.gz/pmda-0.1.1/pmda/util.py
@@ -0,0 +1,32 @@
+# -*- Mode: python; tab-width: 4; indent-tabs-mode:nil; coding:utf-8 -*-
+# vim: tabstop=4 expandtab shiftwidth=4 softtabstop=4
+#
+# PMDA
+# Copyright (c) 2017 The MDAnalysis Development Team and contributors
+# (see the file AUTHORS for the full list of names)
+#
+# Released under the GNU Public Licence, v2 or any higher version
+"""
+Utility functions --- :mod:`pmda.util`
+=====================================================================
+
+
+"""
+from __future__ import absolute_import
+
+import time
+
+
+class timeit(object):
+    """measure time spend in context
+
+    """
+    def __enter__(self):
+        self._start_time = time.time()
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        end_time = time.time()
+        self.elapsed = end_time - self._start_time
+        # always propagate exceptions forward
+        return False
