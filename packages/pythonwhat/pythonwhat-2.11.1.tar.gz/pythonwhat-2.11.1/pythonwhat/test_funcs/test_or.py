d653f1610883f2b5704ba8ed660cc615e2b786e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:12:40 +0000

    Add package pythonwhat/2.11.1/pythonwhat-2.11.1.tar.gz

---
+++ packages/pythonwhat/pythonwhat-2.11.1.tar.gz/pythonwhat-2.11.1/pythonwhat/test_funcs/test_or.py
@@ -0,0 +1,25 @@
+from pythonwhat.Reporter import Reporter
+from pythonwhat.Test import TestFail
+from pythonwhat.check_funcs import multi
+
+def test_or(*tests, state=None):
+    """Test whether at least one SCT passes."""
+
+    rep = Reporter.active_reporter
+
+    success = False
+    first_feedback = None
+    for test in tests: 
+        try: 
+            multi(test, state=state)
+            success = True
+        except TestFail as e:
+            if not first_feedback: first_feedback = rep.feedback
+            rep.failed_test = False
+
+        if success: 
+            return
+    
+    rep.failed_test = True
+    rep.feedback = first_feedback
+    raise TestFail
