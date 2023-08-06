64c25528bb680164af43c00981494739015b2a58
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:14:46 +0000

    Add package lydia/0.1/lydia-0.1.tar.gz

---
+++ packages/lydia/lydia-0.1.tar.gz/lydia-0.1/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'lydia',
+  packages = ['lydia'],
+  version = '0.1',
+  description = 'A high level companion for data science',
+  author = 'Mycchaka Kleinbort',
+  author_email = 'mkleinbort@gmail.com',
+  url = 'https://github.com/mkleinbort/lydia',
+  download_url = 'https://github.com/mkleinbort/lydia/archive/0.1.tar.gz',
+  keywords = ['data science', 'pandas', 'python', 'modeling', 'jupyter'],
+  classifiers = [],
+)
