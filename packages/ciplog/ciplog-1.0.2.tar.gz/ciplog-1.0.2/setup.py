b8b8fc86cbbfa1a313becb25f4d99d5321b800d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:00:57 +0000

    Add package ciplog/1.0.2/ciplog-1.0.2.tar.gz

---
+++ packages/ciplog/ciplog-1.0.2.tar.gz/ciplog-1.0.2/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+VERSION='1.0.2'
+PACKAGES=['ciplog']
+
+setup(name='ciplog',
+      version=VERSION,
+      description='The easy and fast log.',
+      author='Adriano Canofre',
+      author_email='adrianoknofre@gmail.com',
+      license='MIT',
+      packages=PACKAGES,
+      install_requires=[],
+      zip_safe=False)
