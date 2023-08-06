088a0342cfd96b3d995eeef7ce22c5f16f0872f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:35:08 +0000

    Add package easysetuppy/1.1.6/easysetuppy-1.1.6.tar.gz

---
+++ packages/easysetuppy/easysetuppy-1.1.6.tar.gz/easysetuppy-1.1.6/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+packages = find_packages()
+classifiers = ['Development Status :: 5 - Production/Stable', 'Intended Audience :: Developers',
+               'License :: OSI Approved :: MIT License', 'Natural Language :: English',
+               'Operating System :: Microsoft :: Windows', 'Operating System :: Unix', 'Programming Language :: Python',
+               'Programming Language :: Python :: 3', 'Programming Language :: Python :: 3.6',
+               'Topic :: Software Development', 'Topic :: Utilities']
+version = '1.1.6'
+requirements = ['easygui', 'wheel', 'twine', 'setuptools']
+console_scripts = ['build = easysetuppy:build_and_upload']
+gui_scripts = ['easysetuppy = easysetuppy:main',
+               'EasySetupPy = easysetuppy:main',
+               ]
+setup(name='easysetuppy', version=version, url='https://www.github.com/PokeTec/EasySetup', author='PokestarFan',
+      author_email='pokestarfan@yahoo.com', maintainer='PokestarFan', maintainer_email='pokestarfan@yahoo.com',
+      description='A program to make easy setup.py files', platforms='Windows, Mac, Linux', license='MIT License',
+      packages=packages, install_requires=requirements, classifiers=classifiers, entry_points={
+        'console_scripts': console_scripts,
+        'gui_scripts': gui_scripts}
+      )
