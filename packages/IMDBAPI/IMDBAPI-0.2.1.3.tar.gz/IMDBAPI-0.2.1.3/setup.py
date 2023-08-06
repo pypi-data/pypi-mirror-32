6eff18f37388d29e43d7f00eb5a90b7e06b61170
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:48:07 +0000

    Add package IMDBAPI/0.2.1.3/IMDBAPI-0.2.1.3.tar.gz

---
+++ packages/IMDBAPI/IMDBAPI-0.2.1.3.tar.gz/IMDBAPI-0.2.1.3/setup.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*
+
+from __future__ import absolute_import
+
+import os
+
+from setuptools import find_packages, setup
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+with open('requirements.txt') as f:
+    install_requires = f.read().splitlines()
+
+setup(name='IMDBAPI',
+    version='0.2.1.3',
+    description='API for IMDB, to search for movie and get the details like rating, summary, director, cast etc',
+	long_description='API for IMDB, to search for movie and get the details like rating, summary, director, cast etc',
+    packages=['IMDBAPI'],
+    url='https://github.com/devilking15292/IMDB_Api_python.git',
+    author='devilking15292',
+    author_email='p.hari15292@gmail.com',
+    license='MIT',
+    install_requires=install_requires,
+	keywords='imdb, imdbapi, movie, movies',
+    zip_safe=False,
+    classifiers=[
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Topic :: Internet :: WWW/HTTP',
+    ]
+    )
