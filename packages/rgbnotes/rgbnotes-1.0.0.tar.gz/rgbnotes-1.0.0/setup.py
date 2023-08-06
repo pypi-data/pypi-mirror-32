13d2dd51fdf5feb9e10caff8194e29666b9a0a45
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:35:48 +0000

    Add package rgbnotes/1.0.0/rgbnotes-1.0.0.tar.gz

---
+++ packages/rgbnotes/rgbnotes-1.0.0.tar.gz/rgbnotes-1.0.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='rgbnotes',
+      version='1.0.0',
+      description='Python bindings for the RGB Notes API',
+      author='rgbnotes',
+      author_email='support@rgbnotes.com',
+      url='https://github.com/rgbnotes/rgb-api-python',
+      license='MIT',
+      install_requires=[
+          'requests >= 0.8.8',
+      ])
