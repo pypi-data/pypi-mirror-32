4714e052bee22745eb5c048d5c5affcdc11f9570
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:25 +0000

    Add package urlwatch/2.12/urlwatch-2.12.tar.gz

---
+++ packages/urlwatch/urlwatch-2.12.tar.gz/urlwatch-2.12/setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+from setuptools import setup
+
+import os
+import re
+
+main_py = open(os.path.join('lib', 'urlwatch', '__init__.py')).read()
+m = dict(re.findall("\n__([a-z]+)__ = '([^']+)'", main_py))
+docs = re.findall('"""(.*?)"""', main_py, re.DOTALL)
+
+m['name'] = 'urlwatch'
+m['author'], m['author_email'] = re.match(r'(.*) <(.*)>', m['author']).groups()
+m['description'], m['long_description'] = docs[0].strip().split('\n\n', 1)
+m['install_requires'] = ['minidb', 'PyYAML', 'requests', 'keyring', 'pycodestyle', 'appdirs']
+m['scripts'] = ['urlwatch']
+m['package_dir'] = {'': 'lib'}
+m['packages'] = ['urlwatch']
+m['data_files'] = [
+    ('share/man/man1', ['share/man/man1/urlwatch.1']),
+    ('share/urlwatch/examples', [
+        'share/urlwatch/examples/hooks.py.example',
+        'share/urlwatch/examples/urls.yaml.example',
+    ]),
+]
+
+del m['copyright']
+setup(**m)
