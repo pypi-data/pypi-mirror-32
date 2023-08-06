b12f189d8430c61054e4e651a1aa5aa65deed2ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:14:45 +0000

    Add package studio_ghibli_api_sdk/1.0.3/studio_ghibli_api_sdk-1.0.3.tar.gz

---
+++ packages/studio_ghibli_api_sdk/studio_ghibli_api_sdk-1.0.3.tar.gz/studio_ghibli_api_sdk-1.0.3/studio_ghibli_api_sdk/api/__init__.py
@@ -0,0 +1,10 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from studio_ghibli_api_sdk.api.films_api import FilmsApi
+from studio_ghibli_api_sdk.api.locations_api import LocationsApi
+from studio_ghibli_api_sdk.api.people_api import PeopleApi
+from studio_ghibli_api_sdk.api.species_api import SpeciesApi
+from studio_ghibli_api_sdk.api.vehicles_api import VehiclesApi
