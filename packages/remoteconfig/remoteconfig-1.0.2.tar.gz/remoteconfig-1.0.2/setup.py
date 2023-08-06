85d53d60825f53cf096e8b96217558d824be9e59
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:59:19 +0000

    Add package remoteconfig/1.0.2/remoteconfig-1.0.2.tar.gz

---
+++ packages/remoteconfig/remoteconfig-1.0.2.tar.gz/remoteconfig-1.0.2/setup.py
@@ -0,0 +1,39 @@
+import setuptools
+
+
+setuptools.setup(
+  name='remoteconfig',
+  version='1.0.2',
+
+  author='Max Zheng',
+  author_email='maxzheng.os @t gmail.com',
+
+  description='A simple wrapper for localconfig that allows for reading config from a remote server',
+  long_description=open('README.rst').read(),
+
+  url='https://github.com/maxzheng/remoteconfig',
+
+  install_requires=open('requirements.txt').read(),
+
+  license='MIT',
+
+  packages=setuptools.find_packages(),
+  include_package_data=True,
+
+  setup_requires=['setuptools-git'],
+
+  classifiers=[
+    'Development Status :: 5 - Production/Stable',
+
+    'Intended Audience :: Developers',
+    'Topic :: Software Development',
+
+    'License :: OSI Approved :: MIT License',
+
+    'Programming Language :: Python :: 2',
+    'Programming Language :: Python :: 2.6',
+    'Programming Language :: Python :: 2.7',
+  ],
+
+  keywords='configuration remote http config',
+)
---
+++ packages/remoteconfig/remoteconfig-1.0.2.tar.gz/remoteconfig-1.0.2/test/test_remoteconfig.py
@@ -0,0 +1,36 @@
+import time
+
+import httpretty
+
+from remoteconfig import config, RemoteConfig
+
+
+class TestRemoteConfig(object):
+    @httpretty.activate
+    def test_read(self):
+        config_url = 'http://test-remoteconfig.com/config.ini'
+
+        config_content = '[section]\n\nkey = value\n'
+        httpretty.register_uri(httpretty.GET, config_url, body=config_content)
+        config.read(config_url)
+        assert config_content == str(config)
+
+        # Cache duration on read
+        updated_config_content = '[section]\n\nkey = updated\n'
+        httpretty.register_uri(httpretty.GET, config_url, body=updated_config_content)
+        config.read(config_url, cache_duration=1)
+        assert config_content == str(config)
+
+        # Cache duration on init
+        config2 = RemoteConfig(config_url, cache_duration=1, kv_sep=': ')
+        assert config_content.replace(' = ', ': ') == str(config2)
+
+        # Using default cache duration
+        config2.read(config_url)
+        assert config_content.replace(' = ', ': ') == str(config2)
+
+        time.sleep(1)
+
+        # Should update after cache duration
+        config.read(config_url)
+        assert updated_config_content == str(config)
