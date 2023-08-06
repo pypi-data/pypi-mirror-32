4be19fd89093532a8985e7681701d88b8ae1d5d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:56:54 +0000

    Add package union/0.1.8/union-0.1.8.tar.gz

---
+++ packages/union/union-0.1.8.tar.gz/union-0.1.8/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+
+
+VERSION = (0, 1, 8)
+version = '.'.join(map(str, VERSION))
+
+setup(
+    name='union',
+    version=version,
+    author='Union Inc.',
+    author_email='hello@unionbilling.com',
+    description='A Python library for accessing the Union Billing API.',
+    url='https://github.com/unionbilling/union-python',
+    license='MIT',
+    keywords='union billing api',
+    packages=find_packages(),
+
+    install_requires=[
+        'setuptools',
+        'requests>=2.7.0',
+    ],
+
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+
+)
