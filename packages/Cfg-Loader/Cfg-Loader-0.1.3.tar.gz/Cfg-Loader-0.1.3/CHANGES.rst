16a1123652e6cbc36b4096df08b3e329a56449d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:26:40 +0000

    Add package Cfg-Loader/0.1.3/Cfg-Loader-0.1.3.tar.gz

---
+++ packages/Cfg-Loader/Cfg-Loader-0.1.3.tar.gz/Cfg-Loader-0.1.3/CHANGES.rst
@@ -0,0 +1,52 @@
+Changelog
+=========
+
+Here you can see the full list of changes between each releases of Cfg-Loader.
+
+Version 0.1.3
+-------------
+
+Released on May 30th 2018
+
+Docs
+
+- setup readthedocs
+
+Version 0.1.2
+-------------
+
+Released on May 30th 2018
+
+Docs
+
+- fix badges in README
+
+Version 0.1.1
+-------------
+
+Released on May 30th 2018
+
+No Change
+
+Version 0.1.0
+-------------
+
+Released on May 30th 2018
+
+Features
+
+- input data validation and automatic processing using `marshmallow`_
+- substitution of environment variables in input data (following `docker compose variable substitution syntax`_)
+- configuration loading from .yaml file
+
+.. _`marshmallow`: https://github.com/marshmallow-code/marshmallow
+.. _`docker compose variable substitution syntax`: https://docs.docker.com/compose/compose-file/#variable-substitution
+
+Version 0.0.0
+-------------
+
+Unreleased
+
+Chore
+
+- Project: Initialize project
---
+++ packages/Cfg-Loader/Cfg-Loader-0.1.3.tar.gz/Cfg-Loader-0.1.3/setup.py
@@ -0,0 +1,70 @@
+"""
+    Cfg-Loader
+    ~~~~~~~~~~
+
+    Config-Loader is an empty Python project.
+
+    It is meant to be forked when starting a new Python project to inherit structure.
+
+    :copyright: Copyright 2017 by ConsenSys France.
+    :license: BSD, see LICENSE for more details.
+"""
+
+import os
+
+from setuptools import setup, find_packages
+
+
+def read(file_name):
+    try:
+        return open(os.path.join(os.path.dirname(__file__), file_name)).read()
+    except FileNotFoundError:
+        return ''
+
+
+setup(
+    name='Cfg-Loader',
+    version='0.1.3',
+    license=read('LICENSE'),
+    url='https://github.com/nmvalera/cfg-loader',
+    author='Nicolas Maurice',
+    author_email='nicolas.maurice.valera@gmail.com',
+    maintainer='ConsenSys France',
+    description='A library that allows to easily load configuration settings.',
+    long_description=read('README.rst'),
+    packages=find_packages(),
+    install_requires=[
+        'marshmallow==3.0.0b11',
+        'PyYAML>=3.12',
+    ],
+    extras_require={
+        'dev': [
+            'flake8',
+            'autoflake',
+            'autopep8',
+            'coverage',
+            'pytest>=3',
+            'tox',
+            'sphinx',
+            'sphinx_rtd_theme',
+        ],
+        'doc': [
+            'sphinx',
+            'sphinx_rtd_theme',
+        ],
+    },
+    zip_safe=False,
+    platforms='any',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+    test_suite='tests'
+)
