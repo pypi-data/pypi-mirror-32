ba206e33a1ddd0d589a3a1d912865969f037e225
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:27:47 +0000

    Add package cbthelper/0.1.2/cbthelper-0.1.2.tar.gz

---
+++ packages/cbthelper/cbthelper-0.1.2.tar.gz/cbthelper-0.1.2/README.md
@@ -0,0 +1,12 @@
+## cbthelper (for python)
+cbthelper wraps CrossBrowserTesting's [selenium api](https://crossbrowsertesting.com/apidocs/v3/selenium.html) into an easy to use library.
+
+#### Install
+```
+pip install cbthelper
+```
+
+#### Usage
+You can find an example of how to use the library in the example directory. Don't forget to set username and authkey in example/main.py. You can also set the environment variables CBT\_USERNAME and CBT\_AUTHKEY.
+
+[Grab the documentation here](https://cbthelper.readthedocs.io/en/latest/)
---
+++ packages/cbthelper/cbthelper-0.1.2.tar.gz/cbthelper-0.1.2/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='cbthelper',
+    version='0.1.2',
+    author='Tim Hamilton',
+    author_email='timh@crossbrowsertesting.com',
+    description='a helper library for cross browser testing\'s selenium api',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/crossbrowsertesting/cbthelper-python',
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'fuzzywuzzy',
+    ],
+    classifiers=(
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ),
+)
