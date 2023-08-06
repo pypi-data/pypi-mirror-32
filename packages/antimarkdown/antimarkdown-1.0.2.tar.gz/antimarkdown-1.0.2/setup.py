95b84320958fbbdd3956e83763b3dadbdac26c9a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:10:57 +0000

    Add package antimarkdown/1.0.2/antimarkdown-1.0.2.tar.gz

---
+++ packages/antimarkdown/antimarkdown-1.0.2.tar.gz/antimarkdown-1.0.2/setup.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+"""setup.py -- setup file for antimarkdown
+"""
+import os
+from setuptools import setup
+
+README = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'README.rst')
+
+
+setup(
+    name = "antimarkdown",
+    packages = ['antimarkdown'],
+    install_requires = [
+        'lxml',
+    ],
+
+    package_data = {
+        '': ['*.txt', '*.html'],
+    },
+    zip_safe = False,
+
+    version = "1.0.2",
+    description = "HTML to Markdown converter.",
+    long_description = open(README).read(),
+    author = "David Eyk",
+    author_email = "deyk@crossway.org",
+    url = "http://github.com/Crossway/antimarkdown/",
+    license = 'BSD',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content :: CGI Tools/Libraries',
+        'Topic :: Internet :: WWW/HTTP :: Site Management',
+        'Topic :: Software Development :: Documentation',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Text Processing :: Filters',
+        'Topic :: Text Processing :: Markup :: HTML',
+        'Topic :: Communications :: Email :: Filters',
+    ],
+)
