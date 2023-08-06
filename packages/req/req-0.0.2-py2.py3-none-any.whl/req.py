8fa88a807fba1c46d71e12eb48ae3ff4d537a429
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:00:39 +0000

    Add package req/0.0.2/req-0.0.2-py2.py3-none-any.whl

---
+++ packages/req/req-0.0.2-py2.py3-none-any.whl/req.py
@@ -0,0 +1,81 @@
+from __future__ import absolute_import, division, print_function
+import six
+
+from collections import Sequence
+from io import BytesIO
+from six.moves import cStringIO as StringIO
+
+DEFAULT = object()
+
+def dump(obj, fp, startindex=1, separator=DEFAULT, index_separator=DEFAULT):
+    '''Dump an object in req format to the fp given.
+
+    :param Mapping obj: The object to serialize.  Must have a keys method.
+    :param fp: A writable that can accept all the types given.
+    :param separator: The separator between key and value.  Defaults to u'|' or b'|', depending on the types.
+    :param index_separator: The separator between key and index.  Defaults to u'_' or b'_', depending on the types.
+    '''
+
+    if startindex < 0:
+        raise ValueError('startindex must be non-negative, but was {}'.format(startindex))
+
+    try:
+        firstkey = next(iter(obj.keys()))
+    except StopIteration:
+        return
+
+    if isinstance(firstkey, six.text_type):
+        converter = six.u
+    else:
+        converter = six.b
+
+    default_separator = converter('|')
+    default_index_separator = converter('_')
+    newline = converter('\n')
+
+    if separator is DEFAULT:
+        separator = default_separator
+    if index_separator is DEFAULT:
+        index_separator = default_index_separator
+
+    for key, value in six.iteritems(obj):
+        if isinstance(value, (list, tuple, set)):
+            for index, item in enumerate(value, start=startindex):
+                fp.write(key)
+                fp.write(index_separator)
+                fp.write(converter(str(index)))
+                fp.write(separator)
+                fp.write(item)
+                fp.write(newline)
+        else:
+            fp.write(key)
+            fp.write(separator)
+            fp.write(value)
+            fp.write(newline)
+
+def dumps(obj, startindex=1, separator=DEFAULT, index_separator=DEFAULT):
+    '''Dump an object in req format to a string.
+
+    :param Mapping obj: The object to serialize.  Must have a keys method.
+    :param separator: The separator between key and value.  Defaults to u'|' or b'|', depending on the types.
+    :param index_separator: The separator between key and index.  Defaults to u'_' or b'_', depending on the types.
+    '''
+
+    try:
+        firstkey = next(iter(obj.keys()))
+    except StopIteration:
+        return str()
+
+    if isinstance(firstkey, six.text_type):
+        io = StringIO()
+    else:
+        io = BytesIO()
+
+    dump(
+        obj=obj,
+        fp=io,
+        startindex=startindex,
+        separator=separator,
+        index_separator=index_separator,
+        )
+    return io.getvalue()
