7039d1b277eaa7f1641f77510e5d32aed3b6c66c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:12:57 +0000

    Add package pywikipedia/0.0.1/pywikipedia-0.0.1.tar.gz

---
+++ packages/pywikipedia/pywikipedia-0.0.1.tar.gz/pywikipedia-0.0.1/setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+setuptools.setup(
+    name="pywikipedia",
+    version="0.0.1",
+    author="Kaleidagraph",
+    author_email="ben.webb.alpha@gmail.com",
+    description="Package for working wokring with wikipeida",
+    long_description= "Full description at: https://github.com/kaleidagraph/pywikipedia",
+    long_description_content_type="text/markdown",
+    url="https://github.com/kaleidagraph/pywikipedia",
+    packages=setuptools.find_packages(),
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+)
