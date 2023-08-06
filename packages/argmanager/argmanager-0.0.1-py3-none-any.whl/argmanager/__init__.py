1c3529caaf8dddc57e20f428be3dec4176f868ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:55:04 +0000

    Add package argmanager/0.0.1/argmanager-0.0.1-py3-none-any.whl

---
+++ packages/argmanager/argmanager-0.0.1-py3-none-any.whl/argmanager/__init__.py
@@ -0,0 +1,6 @@
+__version__ = '0.0.1'
+__url__ = 'https://github.com/obernard/argmanager'
+__description__ = 'Wrapper around ArgumentParser handling configuration files.'
+__license__ = 'MIT'
+__author__ = 'Olivier BERNARD'
+__email__ = 'obernard78+github@gmail.com'
