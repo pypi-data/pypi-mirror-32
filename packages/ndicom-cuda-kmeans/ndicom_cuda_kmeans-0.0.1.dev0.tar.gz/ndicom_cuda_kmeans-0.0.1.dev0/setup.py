faf43f2f9f4f10b12cbd5babbd0e410b96d8572b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:37 +0000

    Add package ndicom-cuda-kmeans/0.0.1.dev0/ndicom_cuda_kmeans-0.0.1.dev0.tar.gz

---
+++ packages/ndicom-cuda-kmeans/ndicom_cuda_kmeans-0.0.1.dev0.tar.gz/ndicom_cuda_kmeans-0.0.1.dev0/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+import os.path as path
+
+setup(
+    name='ndicom_cuda_kmeans',
+    version='0.0.1.dev0',
+    author='Roman Baigildin',
+    author_email='egdeveloper@mail.ru',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Scientific/Engineering :: Medical Science Apps.',
+    ],
+    url='http://github.com/reactmed/neurdicom-plugins',
+    license='MIT',
+    keywords='DICOM',
+    packages=find_packages(),
+    install_requires=[
+        'dipy', 'numpy', 'pycuda'
+    ],
+    dependency_links=[
+        "git+git://github.com/pydicom/pydicom"
+    ],
+    long_description=open(path.join(path.dirname(__file__), 'README.md')).read(),
+)
