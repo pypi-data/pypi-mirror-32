cc0eebcb5e2e1f7a9fdac4c617e7b8b0de2e2301
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:26:11 +0000

    Add package PyChecko/1.3.0/PyChecko-1.3.0.tar.gz

---
+++ packages/PyChecko/PyChecko-1.3.0.tar.gz/PyChecko-1.3.0/setup.py
@@ -0,0 +1,22 @@
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    from distutils.core import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='PyChecko',
+    packages=find_packages(),
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    version='1.3.0',
+    url='https://github.com/viniciusfeitosa/pychecko/',
+    license='MIT',
+    author='Vinicius Pacheco',
+    author_email='vfpweb@gmail.com',
+    description='MicroFramework to compose instances in execution time',
+    zip_safe=False,
+    platforms='any',
+)
