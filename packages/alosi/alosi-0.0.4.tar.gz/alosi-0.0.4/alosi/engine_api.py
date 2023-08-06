aad0419f3e55e95322b2ab64dabc05458db5b62e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:36:43 +0000

    Add package alosi/0.0.4/alosi-0.0.4.tar.gz

---
+++ packages/alosi/alosi-0.0.4.tar.gz/alosi-0.0.4/alosi/engine_api.py
@@ -0,0 +1,48 @@
+import requests
+
+
+class EngineApi(object):
+    """
+    General API interface to engine application
+    """
+
+    def __init__(self, host="http://localhost:8000", token=None):
+        self.host = host
+        self.base_url = self.host + '/engine/api'
+        self.headers = {'Authorization': 'Token {}'.format(token)} if token else {}
+        self.client = self.get_client()
+        self.client.headers.update(self.headers)
+
+    def get_client(self):
+        return requests.Session()
+
+    def create_activity(self, **kwargs):
+        return self.client.post(
+            self.base_url + '/activity',
+            json=kwargs
+        )
+
+    def recommend(self, learner=None, collection=None, sequence=None):
+        base_url = self.host + '/api/v2'
+        return self.client.post(
+            base_url + '/activity/recommend',
+            json=dict(learner=learner, collection=collection, sequence=sequence)
+        )
+
+    def submit_score(self, learner=None, activity=None, score=None):
+        return self.client.post(
+            self.base_url + '/score',
+            json=dict(learner=learner, activity=activity, score=score)
+        )
+
+    def bulk_update_mastery(self, data):
+        return self.client.put(
+            self.base_url + '/mastery/bulk_update',
+            json=data
+        )
+
+    def create_knowledge_component(self, **kwargs):
+        return self.client.post(
+            self.base_url + '/knowledge_component',
+            json=kwargs
+        )
