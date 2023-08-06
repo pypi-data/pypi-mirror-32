e1ffc56eda6cc47403e3a379cae454a546d80383
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:35:41 +0000

    Add package flask-msearch/0.1.7/flask-msearch-0.1.7.tar.gz

---
+++ packages/flask-msearch/flask-msearch-0.1.7.tar.gz/flask-msearch-0.1.7/setup.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# **************************************************************************
+# Copyright © 2017 jianglin
+# File Name: setup.py
+# Author: jianglin
+# Email: xiyang0807@gmail.com
+# Created: 2017-04-16 15:18:54 (CST)
+# Last Update: Friday 2018-06-15 10:29:40 (CST)
+#          By:
+# Description:
+# **************************************************************************
+from setuptools import setup
+
+
+setup(
+    name='flask-msearch',
+    version='0.1.7',
+    url='https://github.com/honmaple/flask-msearch',
+    license='BSD',
+    author='honmaple',
+    author_email='xiyang0807@gmail.com',
+    description='full text search with whoosh for flask',
+    long_description='Please visit https://github.com/honmaple/flask-msearch',
+    packages=['flask_msearch'],
+    zip_safe=False,
+    include_package_data=True,
+    platforms='any',
+    install_requires=[
+        'Flask',
+        'Flask-SQLAlchemy'
+    ],
+    classifiers=[
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
+)
