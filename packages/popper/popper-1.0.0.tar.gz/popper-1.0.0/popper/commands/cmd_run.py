b74213e39753e651b3679e57650dbee9b247f35d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:13:00 +0000

    Add package popper/1.0.0/popper-1.0.0.tar.gz

---
+++ packages/popper/popper-1.0.0.tar.gz/popper-1.0.0/popper/commands/cmd_run.py
@@ -0,0 +1,200 @@
+#!/usr/bin/env python
+
+import click
+import os
+import popper.utils as pu
+import subprocess
+import time
+import signal
+import sys
+
+from popper.cli import pass_context
+from subprocess import check_output
+
+
+@click.command('run', short_help='Run pipeline and report on its status.')
+@click.argument('pipeline', required=False)
+@click.option(
+    '--timeout',
+    help='Timeout limit for pipeline. Use s for seconds, m for minutes and h '
+         'for hours. A single integer can also be used to specify timeout '
+         'in seconds. Use double quotes if you wish to use more than one unit.'
+         'For example: --timeout "2m 20s" will mean 140 seconds.',
+    required=False,
+    show_default=True,
+    default="10800s"
+)
+@click.option(
+    '--skip',
+    help='Comma-separated list of stages to skip.',
+    required=False,
+)
+@click.option(
+    '--ignore-errors',
+    is_flag=True,
+    help='Execute all pipelines even if there is a failure, '
+         'only when no pipeline argument is provided ',
+    required=False,
+)
+@pass_context
+def cli(ctx, pipeline, timeout, skip, ignore_errors):
+    """Executes a pipeline and reports its status. When PIPELINE is given, it
+    executes only the pipeline with such a name. If the argument is omitted,
+    all pipelines are executed in lexicographical order. Reports an error if
+    no pipelines have been configured.
+    """
+    cwd = os.getcwd()
+    pipes = pu.read_config()['pipelines']
+    project_root = pu.get_project_root()
+    time_out = pu.parse_timeout(timeout)
+
+    if len(pipes) == 0:
+        pu.info("No pipelines defined in .popper.yml. "
+                "Run popper init --help for more info.", fg='yellow')
+        sys.exit(0)
+
+    if pipeline:
+        if ignore_errors:
+            pu.warn("ignore-errors flag is ignored when pipeline "
+                    "argument is provided")
+        if pipeline not in pipes:
+            pu.fail("Cannot find pipeline {} in .popper.yml".format(pipeline))
+        status = run_pipeline(project_root, pipes[pipeline], time_out, skip)
+    else:
+        if os.path.basename(cwd) in pipes:
+            # run just the one for CWD
+            status = run_pipeline(project_root, pipes[os.path.basename(cwd)],
+                                  time_out, skip)
+        else:
+            # run all
+            for pipe in pipes:
+                status = run_pipeline(
+                    project_root, pipes[pipe], time_out, skip
+                )
+
+                if status == 'FAIL' and not ignore_errors:
+                    break
+
+    os.chdir(cwd)
+
+    if status == 'FAIL':
+        pu.fail("Failed to execute pipeline")
+
+
+def run_pipeline(project_root, pipeline, timeout, skip):
+    abs_path = os.path.join(project_root, pipeline['path'])
+
+    pu.info("Executing " + os.path.basename(abs_path), fg='blue',
+            bold=True, blink=True)
+
+    os.chdir(abs_path)
+
+    check_output('rm -rf popper_logs/ popper_status', shell=True)
+    check_output('mkdir -p popper_logs/', shell=True)
+
+    STATUS = "SUCCESS"
+
+    with click.progressbar(pipeline['stages'], show_eta=False,
+                           label="Current stage: ", item_show_func=str,
+                           bar_template='[%(bar)s] %(label)s %(info)s',
+                           show_percent=False) as stages:
+
+        for stage in stages:
+
+            stage_file = pu.get_filename(abs_path, stage)
+
+            if skip:
+                skipped = skip.split(',')
+            else:
+                skipped = []
+
+            if not stage_file or stage in skipped:
+                continue
+
+            ecode = execute(stage_file, timeout, stages)
+
+            if ecode != 0:
+                pu.info("\nStage {} failed.".format(stage), fg='red',
+                        bold=True, blink=True)
+                STATUS = "FAIL"
+                pu.info("Logs for {}:.".format(stage), fg='red')
+                for t in ['.err', '.out']:
+                    logfile = 'popper_logs/{}{}'.format(stage_file, t)
+                    with open(logfile, 'r') as f:
+                        pu.info(f.read())
+
+                # Execute teardown when some stage fails and then break
+                teardown_file = pu.get_filename(abs_path, 'teardown')
+                if (teardown_file and
+                    stage != 'teardown' and
+                    'teardown' in pipeline['stages'] and
+                    'teardown' not in skipped):
+                        execute(teardown_file, timeout)
+
+                break
+
+            if 'valid' in stage:
+                STATUS = "GOLD"
+                with open('popper_logs/validate.sh.out', 'r') as f:
+                    validate_output = f.readlines()
+                    if len(validate_output) == 0:
+                        STATUS = "SUCCESS"
+                    for line in validate_output:
+                        if '[true]' not in line:
+                            STATUS = "SUCCESS"
+
+    with open('popper_status', 'w') as f:
+        f.write(STATUS + '\n')
+
+    fg = 'green' if STATUS == "SUCCESS" else 'red'
+    pu.info('status : ' + STATUS, fg=fg, bold=True)
+    sys.stdout.write('\n')
+
+    return STATUS
+
+
+def execute(stage, timeout, bar=None):
+    time_limit = time.time() + timeout
+    sleep_time = 1
+    out_fname = 'popper_logs/{}.{}'.format(stage, 'out')
+    err_fname = 'popper_logs/{}.{}'.format(stage, 'err')
+
+    with open(out_fname, "wb") as outf, open(err_fname, "wb") as errf:
+        p = subprocess.Popen('./' + stage, shell=True, stdout=outf,
+                             stderr=errf, preexec_fn=os.setsid)
+
+        if not bar:
+            pu.info("Running: {}".format(stage))
+
+        while p.poll() is None:
+
+            if time.time() > time_limit:
+                os.killpg(os.getpgid(p.pid), signal.SIGTERM)
+                sys.stdout.write(' time out!')
+                break
+
+            if sleep_time < 300:
+                sleep_time *= 2
+
+            if bar:
+                for i in range(sleep_time):
+                    bar.label = bar.label + '\b_'
+                    bar.render_progress()
+                    time.sleep(0.5)
+                    bar.label = bar.label + '\b '
+                    bar.render_progress()
+                    time.sleep(0.5)
+
+    return p.poll()
+
+
+class Unbuffered(object):
+    def __init__(self, stream):
+        self.stream = stream
+
+    def write(self, data):
+        self.stream.write(data)
+        self.stream.flush()
+
+    def __getattr__(self, attr):
+        return getattr(self.stream, attr)
---
+++ packages/popper/popper-1.0.0.tar.gz/popper-1.0.0/popper/utils.py
@@ -0,0 +1,169 @@
+import click
+import os
+import sys
+import yaml
+from subprocess import check_output, CalledProcessError
+
+noalias_dumper = yaml.dumper.SafeDumper
+noalias_dumper.ignore_aliases = lambda self, data: True
+
+
+def get_items(dict_object):
+    """Python 2/3 compatible way of iterating over a dictionary"""
+    for key in dict_object:
+        yield key, dict_object[key]
+
+
+def get_path_to_config():
+    """Obtains the path to the config file.
+
+    Returns:
+        path (str): string containing path to where config file is stored.
+    """
+
+    project_root = get_project_root()
+    config_path = os.path.join(project_root, '.popper.yml')
+
+    if os.path.isfile(config_path):
+        return os.getcwd()
+
+    return ""
+
+
+def get_project_root():
+    """Tries to find the root of the project with the following heuristic:
+
+      - Find the .git folder in cwd
+
+    Returns:
+        project_root (str): The fully qualified path to the root of project.
+    """
+
+    try:
+        base = check_output('git rev-parse --show-toplevel', shell=True)
+    except CalledProcessError:
+        fail(
+            "Unable to find the root of your project."
+            + "Initialize repository first."
+        )
+
+    return base.decode('utf-8').strip()
+
+
+def read_config():
+    """Reads config from .popper.yml file.
+
+    Returns:
+        config (dict): dictionary representing the YAML file contents.
+    """
+    config_filename = os.path.join(get_project_root(), '.popper.yml')
+
+    if not os.path.isfile(config_filename):
+        fail(".popper.yml file doesn't exist. See 'popper init --help'.")
+
+    with open(config_filename, 'r') as f:
+        config = yaml.load(f.read())
+        if not config:
+            fail(".popper.yml is empty. Consider deleting it and "
+                 "reinitializing the repo. See popper init --help for more.")
+        for key in ["metadata", "pipelines"]:
+            if key not in config:
+                fail(".popper.yml doesn't contain expected entries. "
+                     "Consider deleting it and reinitializing the repo. "
+                     "See popper init --help for more.")
+
+    return config
+
+
+def write_config(config):
+    """Writes config to .popper.yml file."""
+    config_filename = os.path.join(get_project_root(), '.popper.yml')
+
+    with open(config_filename, 'w') as f:
+        yaml.dump(config, f, default_flow_style=False, Dumper=noalias_dumper)
+
+
+def is_popperized():
+    """Determines if the current repo has already been popperized by checking
+    whether the '.popper.yml' file on the root of the project exits.
+
+    Returns:
+       True if the '.popper.yml' exists, False otherwise.
+    """
+    config_filename = os.path.join(get_project_root(), '.popper.yml')
+    return os.path.isfile(config_filename)
+
+
+def update_config(name, stages, envs, relative_path):
+    """Updates the configuration for a pipeline"""
+
+    if name == 'paper':
+        stages = 'build'
+
+    config = read_config()
+    config['pipelines'][name] = {
+        'stages': stages.split(','),
+        'envs': envs.split(','),
+        'path': relative_path
+    }
+    write_config(config)
+
+
+def get_filename(abs_path, stage):
+    """Returns filename for a stage"""
+    os.chdir(abs_path)
+    if os.path.isfile(stage):
+        return stage
+    elif os.path.isfile(stage + '.sh'):
+        return stage + '.sh'
+    else:
+        return None
+
+
+def fail(msg):
+    """Prints the error message on the terminal."""
+    click.secho('ERROR: ' + msg, fg='red', bold=True)
+    sys.exit(1)
+
+
+def warn(msg):
+    click.secho('WARNING: ' + msg, fg='magenta', bold=True)
+
+
+def info(msg, **styles):
+    """Prints the message on the terminal."""
+    click.secho(msg, **styles)
+
+
+def print_yaml(msg, **styles):
+    """Prints the messages in YAML's block format. """
+    click.secho(yaml.dump(msg, default_flow_style=False), **styles)
+
+
+def parse_timeout(timeout):
+    """Takes timeout as string and parses it to obtain the number of seconds.
+    Generates valid error if proper format is not used.
+
+    Returns:
+        Value of timeout in seconds (float).
+    """
+    time_out = 0
+    to_seconds = {"s": 1, "m": 60, "h": 3600}
+    try:
+        time_out = float(timeout)
+    except ValueError:
+        literals = timeout.split()
+        for literal in literals:
+            unit = literal[-1].lower()
+            try:
+                value = float(literal[:-1])
+            except ValueError:
+                fail("invalid timeout format used. "
+                     "See popper run --help for more.")
+            try:
+                time_out += value * to_seconds[unit]
+            except KeyError:
+                fail("invalid timeout format used. "
+                     "See popper run --help for more.")
+
+    return time_out
