5dea56b1edd533d610cbdedf48909c4634cdebf3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:14:23 +0000

    Add package forkbuntu/0.2.5/forkbuntu-0.2.5.tar.gz

---
+++ packages/forkbuntu/forkbuntu-0.2.5.tar.gz/forkbuntu-0.2.5/forkbuntu/steps/pack_filesystem.py
@@ -0,0 +1,14 @@
+from ..step import Step
+from munch import munchify
+from os import path
+
+class PackFilesystem(Step):
+    messages = munchify({
+        'past': 'packed filesystem',
+        'present': 'packing filesystem'
+    })
+    requires = ['configure_filesystem']
+
+    def run(self):
+        s = self.app.services
+        s.filesystem.pack()
