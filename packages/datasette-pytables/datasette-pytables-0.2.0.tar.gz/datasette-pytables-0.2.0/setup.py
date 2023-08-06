740d981c6c8528716247d44fb8f56544ceaffc75
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:12:55 +0000

    Add package datasette-pytables/0.2.0/datasette-pytables-0.2.0.tar.gz

---
+++ packages/datasette-pytables/datasette-pytables-0.2.0.tar.gz/datasette-pytables-0.2.0/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup
+import os
+
+def get_version():
+    with open('VERSION') as fd:
+        return fd.read()
+
+def get_long_description():
+    with open(os.path.join(
+        os.path.dirname(os.path.abspath(__file__)), 'README.md'
+    ), encoding='utf8') as fp:
+        return fp.read()
+
+
+setup(
+    name='datasette-pytables',
+    version=get_version(),
+    description='Datasette connector for loading pytables files (.h5)',
+    long_description=get_long_description(),
+    long_description_content_type='text/markdown',
+    author='Javier Sancho',
+    url='https://github.com/jsancho-gpl/datasette-pytables',
+    license='Apache License, Version 2.0',
+    packages=['datasette_pytables'],
+    entry_points={
+        'datasette.connectors': [
+            'pytables = datasette_pytables'
+        ],
+    },
+    install_requires=['datasette-core', 'tables', 'moz-sql-parser', 'mo-future'],
+    tests_require=['pytest']
+)
+
