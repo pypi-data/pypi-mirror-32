a3091dbfc71220534768814e13a7a6fcd7f5b9f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:27:30 +0000

    Add package csvprint/0.4.0/csvprint-0.4.0.tar.gz

---
+++ packages/csvprint/csvprint-0.4.0.tar.gz/csvprint-0.4.0/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+with open('README.md', 'r') as readme:
+    LONG_DESCRIPTION = readme.read()
+
+setup(
+    name='csvprint',
+    version='0.4.0',
+    description='Print csv files in columnated format, can also output in Markdown and LaTeX format',
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type="text/markdown",
+    packages=['csvprint'],
+    license='MIT',
+    scripts=['bin/csvprint'],
+    author='Vegard Stikbakke',
+    author_email='vegard.stikbakke@gmail.com',
+    url='https://github.com/vegarsti/csvprint',
+    keywords='csv',
+)
