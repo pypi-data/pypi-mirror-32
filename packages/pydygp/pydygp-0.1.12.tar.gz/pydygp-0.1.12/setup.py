d29c50cec8d9fe8ea8e9bcd9dea5ec0e28af0114
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:36:11 +0000

    Add package pydygp/0.1.12/pydygp-0.1.12.tar.gz

---
+++ packages/pydygp/pydygp-0.1.12.tar.gz/pydygp-0.1.12/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(name='pydygp',
+      version='0.1.012',
+      author='Daniel Tait',
+      author_email='tait.djk@gmail.com',
+      url='http://github.com/danieljtait/pydygp',
+      license='MIT',
+      packages=find_packages(exclude=['contrib', 'docs', 'tests']),
+      zip_safe=False)      
