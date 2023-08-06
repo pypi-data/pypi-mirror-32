1768a1034a73d8bb45a6d5ee78c871c0fe81d497
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:17:34 +0000

    Add package collective.mailchimp/2.2.2/collective.mailchimp-2.2.2.tar.gz

---
+++ packages/collective.mailchimp/collective.mailchimp-2.2.2.tar.gz/collective.mailchimp-2.2.2/src/collective/mailchimp/browser/controlpanel.py
@@ -0,0 +1,78 @@
+# -*- coding: utf-8 -*-
+from zope.interface import Invalid
+from z3c.form.interfaces import WidgetActionExecutionError
+from zope.component import getUtility
+from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+
+from ..exceptions import (
+    PostRequestError,
+    MailChimpException
+)
+
+from plone.app.registry.browser import controlpanel
+try:
+    from plone.protect.interfaces import IDisableCSRFProtection
+except ImportError:
+    # BBB for old plone.protect, default until at least Plone 4.3.7.
+    IDisableCSRFProtection = None
+from zope.interface import alsoProvides
+
+from collective.mailchimp.interfaces import IMailchimpSettings
+from collective.mailchimp.interfaces import IMailchimpLocator
+from collective.mailchimp import _
+
+
+class MailchimpSettingsEditForm(controlpanel.RegistryEditForm):
+
+    schema = IMailchimpSettings
+    label = _(u"MailChimp settings")
+    description = _(u"""""")
+
+    def update(self):
+        self.updateCache()
+        super(MailchimpSettingsEditForm, self).update()
+
+    def updateFields(self):
+        super(MailchimpSettingsEditForm, self).updateFields()
+
+    def updateWidgets(self):
+        super(MailchimpSettingsEditForm, self).updateWidgets()
+
+    def updateCache(self):
+        mailchimp = getUtility(IMailchimpLocator)
+        mailchimp.updateCache()
+
+
+class MailchimpSettingsControlPanel(controlpanel.ControlPanelFormWrapper):
+    form = MailchimpSettingsEditForm
+    index = ViewPageTemplateFile('controlpanel.pt')
+
+    def mailchimp_account(self):
+        if IDisableCSRFProtection is not None:
+            alsoProvides(self.request, IDisableCSRFProtection)
+        mailchimp = getUtility(IMailchimpLocator)
+        try:
+            return mailchimp.account()
+        except PostRequestError:
+            return []
+        except MailChimpException, error:
+            raise WidgetActionExecutionError(
+                Invalid(
+                    u"Could not fetch account details from MailChimp. " +
+                    u"Please check your MailChimp API key: %s" % error
+                )
+            )
+
+    def available_lists(self):
+        if IDisableCSRFProtection is not None:
+            alsoProvides(self.request, IDisableCSRFProtection)
+        mailchimp = getUtility(IMailchimpLocator)
+        try:
+            return mailchimp.lists()
+        except MailChimpException, error:
+            raise WidgetActionExecutionError(
+                Invalid(
+                    u"Could not fetch available lists from MailChimp. " +
+                    u"Please check your MailChimp API key: %s" % error
+                )
+            )
