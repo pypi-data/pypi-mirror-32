88490d33fec1a8b73749c680585386f5afe8b038
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:00:37 +0000

    Add package menumaker/1.4.1/menumaker-1.4.1.tar.gz

---
+++ packages/menumaker/menumaker-1.4.1.tar.gz/menumaker-1.4.1/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+setup(name="menumaker",
+      version="1.4.1",
+      description="An easy solution for creating menus for TkInter applications in just a few lines.",
+      author="DeflatedPickle",
+      author_email="DeflatedPickle@gmail.com",
+      url="https://github.com/DeflatedPickle/MenuMaker",
+      license="MIT",
+      classifiers=[
+          "Intended Audience :: Developers",
+          "License :: OSI Approved :: MIT License",
+          "Programming Language :: Python :: 2.6",
+          "Programming Language :: Python :: 2.7",
+          "Programming Language :: Python :: 3.0",
+          "Programming Language :: Python :: 3.1",
+          "Programming Language :: Python :: 3.2",
+          "Programming Language :: Python :: 3.3",
+          "Programming Language :: Python :: 3.4",
+          "Programming Language :: Python :: 3.5",
+          "Programming Language :: Python :: 3.6",
+      ],
+      keywords=["menumaker", "tkinter", "GUI", "menu"],
+      packages=["menumaker"])
