e87728402ec9e81de261de46d7fdc1e45f7d7b32
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:38:12 +0000

    Add package calendar-script-task/0.1.0/calendar-script-task-0.1.0.tar.gz

---
+++ packages/calendar-script-task/calendar-script-task-0.1.0.tar.gz/calendar-script-task-0.1.0/setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+setuptools.setup(
+    name='calendar-script-task',
+    version='0.1.0',
+    description='A Python package example',
+    author='Velveeta',
+    author_email='hackton@hackton.com',
+    long_description_content_type="text/markdown",
+    url="https://github.com/pypa/example-project",
+    packages=setuptools.find_packages(),
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+)
