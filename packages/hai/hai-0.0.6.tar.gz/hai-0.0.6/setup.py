16b0afb99036cd04e4948a276c229d8b11b58564
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:12:27 +0000

    Add package hai/0.0.6/hai-0.0.6.tar.gz

---
+++ packages/hai/hai-0.0.6.tar.gz/hai-0.0.6/setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+dev_dependencies = [
+    'flake8',
+    'isort',
+    'pydocstyle',
+    'pytest-cov',
+    'moto',
+    'boto3',
+]
+
+if __name__ == '__main__':
+    setuptools.setup(
+        name='hai',
+        description='Toolbelt library',
+        version='0.0.6',
+        url='https://github.com/valohai/hai',
+        author='Valohai',
+        author_email='hait@valohai.com',
+        maintainer='Aarni Koskela',
+        maintainer_email='akx@iki.fi',
+        license='MIT',
+        install_requires=[],
+        tests_require=dev_dependencies,
+        extras_require={'dev': dev_dependencies},
+        packages=setuptools.find_packages('.', exclude=('hai_tests', 'hai_tests.*',)),
+        include_package_data=True,
+    )
