e8d031512f9792392fef928355766132d6773b76
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:57 +0000

    Add package shuup/1.6.0/shuup-1.6.0-py2.py3-none-any.whl

---
+++ packages/shuup/shuup-1.6.0-py2.py3-none-any.whl/shuup/front/checkout/confirm.py
@@ -0,0 +1,138 @@
+# -*- coding: utf-8 -*-
+# This file is part of Shuup.
+#
+# Copyright (c) 2012-2018, Shuup Inc. All rights reserved.
+#
+# This source code is licensed under the OSL-3.0 license found in the
+# LICENSE file in the root directory of this source tree.
+from logging import getLogger
+
+from django import forms
+from django.conf import settings
+from django.contrib.auth import get_user_model
+from django.core.urlresolvers import reverse
+from django.shortcuts import redirect
+from django.utils.translation import ugettext_lazy as _
+from django.views.generic import FormView
+
+from shuup.core.models import OrderStatus
+from shuup.front.basket import get_basket_order_creator
+from shuup.front.checkout import CheckoutPhaseViewMixin
+from shuup.utils.djangoenv import has_installed
+
+logger = getLogger(__name__)
+
+
+class ConfirmForm(forms.Form):
+    product_ids = forms.CharField(widget=forms.HiddenInput(), required=True)
+    accept_terms = forms.BooleanField(required=True, label=_(u"I accept the terms and conditions"))
+    marketing = forms.BooleanField(required=False, label=_(u"I want to receive marketing material"), initial=False)
+    comment = forms.CharField(widget=forms.Textarea(), required=False, label=_(u"Comment"))
+
+    def __init__(self, *args, **kwargs):
+        self.request = kwargs.pop("request")
+        self.current_product_ids = kwargs.pop("current_product_ids", "")
+        super(ConfirmForm, self).__init__(*args, **kwargs)
+
+        if has_installed("shuup.gdpr"):
+            from shuup.gdpr.models import GDPRSettings
+            if GDPRSettings.get_for_shop(self.request.shop).enabled:
+                from shuup.simple_cms.models import Page, PageType
+                gdpr_documents = Page.objects.visible(self.request.shop).filter(
+                    page_type=PageType.GDPR_CONSENT_DOCUMENT
+                )
+                if gdpr_documents.exists():
+                    self.fields.pop("accept_terms")
+                    for page in gdpr_documents:
+                        self.fields["accept_{}".format(page.id)] = forms.BooleanField(
+                            label=_("I have read and accept the {}").format(page.title),
+                            help_text=_("Read the <a href='{}' target='_blank'>{}</a>.").format(
+                                reverse("shuup:cms_page", kwargs=dict(url=page.url)),
+                                page.title
+                            ),
+                            error_messages=dict(required=_("You must accept to this to confirm the order."))
+                        )
+
+        field_properties = settings.SHUUP_CHECKOUT_CONFIRM_FORM_PROPERTIES
+        for field, properties in field_properties.items():
+            for prop in properties:
+                setattr(self.fields[field], prop, properties[prop])
+
+    def clean(self):
+        product_ids = set(self.cleaned_data.get('product_ids', "").split(','))
+        if product_ids != self.current_product_ids:
+            raise forms.ValidationError(
+                _("There has been a change in product availability. Please review your cart and reconfirm your order."))
+
+
+class ConfirmPhase(CheckoutPhaseViewMixin, FormView):
+    identifier = "confirm"
+    title = _("Confirmation")
+
+    template_name = "shuup/front/checkout/confirm.jinja"
+    form_class = ConfirmForm
+
+    def process(self):
+        self.basket.customer_comment = self.storage.get("comment")
+        self.basket.marketing_permission = self.storage.get("marketing")
+
+    def is_valid(self):
+        # check that all form keys starting with "accept_" must have a valid value
+        not_accepted_keys = [
+            key for key in self.storage.keys() if key.startswith("accept_") and not self.storage.get(key)
+        ]
+        return bool(len(not_accepted_keys) == 0)
+
+    def _get_product_ids(self):
+        return [str(product_id) for product_id in self.basket.get_product_ids_and_quantities().keys()]
+
+    def get_form_kwargs(self):
+        kwargs = super(ConfirmPhase, self).get_form_kwargs()
+        kwargs["request"] = self.request
+        kwargs["current_product_ids"] = set(self._get_product_ids())
+        return kwargs
+
+    def get_context_data(self, **kwargs):
+        context = super(ConfirmPhase, self).get_context_data(**kwargs)
+        basket = self.basket
+
+        basket.calculate_taxes()
+        errors = list(basket.get_validation_errors())
+        context["basket"] = basket
+        context["errors"] = errors
+        context["orderable"] = (not errors)
+        context["product_ids"] = ','.join(self._get_product_ids())
+        return context
+
+    def form_valid(self, form):
+        for key, value in form.cleaned_data.items():
+            self.storage[key] = value
+        self.process()
+        order = self.create_order()
+        self.checkout_process.complete()  # Inform the checkout process it's completed
+
+        if order.require_verification:
+            response = redirect("shuup:order_requires_verification", pk=order.pk, key=order.key)
+        else:
+            response = redirect("shuup:order_process_payment", pk=order.pk, key=order.key)
+
+        user = self.request.user
+        if has_installed("shuup.gdpr") and user.is_authenticated():
+            from shuup.gdpr.utils import create_user_consent_for_all_documents
+            create_user_consent_for_all_documents(order.shop, user)
+
+        return response
+
+    def create_order(self):
+        basket = self.basket
+        assert basket.shop == self.request.shop
+        basket.orderer = self.request.person
+        basket.customer = self.request.customer
+        basket.creator = self.request.user
+        if "impersonator_user_id" in self.request.session:
+            basket.creator = get_user_model().objects.get(pk=self.request.session["impersonator_user_id"])
+        basket.status = OrderStatus.objects.get_default_initial()
+        order_creator = get_basket_order_creator()
+        order = order_creator.create_order(basket)
+        basket.finalize()
+        return order
---
+++ packages/shuup/shuup-1.6.0-py2.py3-none-any.whl/shuup/simple_cms/models.py
@@ -0,0 +1,185 @@
+# -*- coding: utf-8 -*-
+# This file is part of Shuup.
+#
+# Copyright (c) 2012-2018, Shuup Inc. All rights reserved.
+#
+# This source code is licensed under the OSL-3.0 license found in the
+# LICENSE file in the root directory of this source tree.
+from django.conf import settings
+from django.core.exceptions import ValidationError
+from django.db import models
+from django.db.models import Q
+from django.utils.encoding import force_text, python_2_unicode_compatible
+from django.utils.text import slugify
+from django.utils.timezone import now
+from django.utils.translation import ugettext_lazy as _
+from enumfields import Enum, EnumIntegerField
+from mptt.managers import TreeManager
+from mptt.models import MPTTModel, TreeForeignKey
+from parler.managers import TranslatableQuerySet
+from parler.models import TranslatableModel, TranslatedFields
+
+from shuup.core.fields import InternalIdentifierField
+from shuup.utils.analog import define_log_model, LogEntryKind
+
+
+class PageType(Enum):
+    NORMAL = 0
+    GDPR_CONSENT_DOCUMENT = 1
+
+    class Labels:
+        NORMAL = _('normal')
+        GDPR_CONSENT_DOCUMENT = _('GDPR consent document')
+
+
+class PageQuerySet(TranslatableQuerySet):
+    def visible(self, shop, dt=None):
+        """
+        Get pages that should be publicly visible.
+
+        This does not do permission checking.
+
+        :param dt: Datetime for visibility check
+        :type dt: datetime.datetime
+        :return: QuerySet of pages.
+        :rtype: QuerySet[Page]
+        """
+        if not dt:
+            dt = now()
+        q = Q(
+            Q(deleted=False) &
+            Q(available_from__lte=dt) &
+            (Q(available_to__gte=dt) | Q(available_to__isnull=True))
+        )
+        qs = self.for_shop(shop).filter(q)
+        return qs
+
+    def for_shop(self, shop):
+        return self.filter(shop=shop)
+
+
+@python_2_unicode_compatible
+class Page(MPTTModel, TranslatableModel):
+    shop = models.ForeignKey("shuup.Shop", verbose_name=_('shop'))
+    available_from = models.DateTimeField(null=True, blank=True, verbose_name=_('available from'), help_text=_(
+        "Set an available from date to restrict the page to be available only after a certain date and time. "
+        "This is useful for pages describing sales campaigns or other time-sensitive pages."
+    ))
+    available_to = models.DateTimeField(null=True, blank=True, verbose_name=_('available to'), help_text=_(
+        "Set an available to date to restrict the page to be available only after a certain date and time. "
+        "This is useful for pages describing sales campaigns or other time-sensitive pages."
+    ))
+
+    created_by = models.ForeignKey(
+        settings.AUTH_USER_MODEL, blank=True, null=True, related_name="+", on_delete=models.SET_NULL,
+        verbose_name=_('created by')
+    )
+    modified_by = models.ForeignKey(
+        settings.AUTH_USER_MODEL, blank=True, null=True, related_name="+", on_delete=models.SET_NULL,
+        verbose_name=_('modified by')
+    )
+
+    created_on = models.DateTimeField(auto_now_add=True, editable=False, verbose_name=_('created on'))
+    modified_on = models.DateTimeField(auto_now=True, editable=False, verbose_name=_('modified on'))
+
+    identifier = InternalIdentifierField(
+        unique=False,
+        help_text=_('This identifier can be used in templates to create URLs'),
+        editable=True
+    )
+
+    visible_in_menu = models.BooleanField(verbose_name=_("visible in menu"), default=False, help_text=_(
+        "Check this if this page should have a link in the top menu of the store front."
+    ))
+    parent = TreeForeignKey(
+        "self", blank=True, null=True, related_name="children", verbose_name=_("parent"), help_text=_(
+            "Set this to a parent page if this page should be subcategorized under another page."
+        ))
+    list_children_on_page = models.BooleanField(verbose_name=_("list children on page"), default=False, help_text=_(
+        "Check this if this page should list its children pages."
+    ))
+    page_type = EnumIntegerField(PageType, default=PageType.NORMAL, db_index=True, verbose_name=_("page type"))
+    deleted = models.BooleanField(default=False, verbose_name=_("deleted"))
+
+    translations = TranslatedFields(
+        title=models.CharField(max_length=256, verbose_name=_('title'), help_text=_(
+            "The page title. This is shown anywhere links to your page are shown."
+        )),
+        url=models.CharField(
+            max_length=100, verbose_name=_('URL'),
+            default=None,
+            blank=True,
+            null=True,
+            help_text=_(
+                "The page url. Choose a descriptive url so that search engines can rank your page higher. "
+                "Often the best url is simply the page title with spaces replaced with dashes."
+            )
+        ),
+        content=models.TextField(verbose_name=_('content'), help_text=_(
+            "The page content. This is the text that is displayed when customers click on your page link."
+        )),
+    )
+
+    objects = TreeManager.from_queryset(PageQuerySet)()
+
+    class Meta:
+        ordering = ('-id',)
+        verbose_name = _('page')
+        verbose_name_plural = _('pages')
+        unique_together = ("shop", "identifier")
+
+    def delete(self, using=None):
+        raise NotImplementedError("Not implemented: Use `soft_delete()`")
+
+    def soft_delete(self, user=None):
+        if not self.deleted:
+            self.deleted = True
+            self.add_log_entry("Deleted.", kind=LogEntryKind.DELETION, user=user)
+            # Bypassing local `save()` on purpose.
+            super(Page, self).save(update_fields=("deleted",))
+
+    def make_gdpr_old_version(self):
+        self.identifier = slugify("{}-{}".format(self.identifier, self.pk))
+        self.save(update_fields=["identifier"])
+        page_translation_model = self._meta.model._parler_meta.root_model
+        for page_translation in page_translation_model.objects.filter(master_id=self.pk):
+            page_translation.url = "{}-{}".format(page_translation.url, self.pk)
+            page_translation.save(update_fields=["url"])
+
+    def clean(self):
+        url = getattr(self, "url", None)
+        if url:
+            page_translation = self._meta.model._parler_meta.root_model
+            shop_pages = Page.objects.for_shop(self.shop).values_list("id", flat=True)
+            url_checker = page_translation.objects.filter(url=url, master_id__in=shop_pages)
+            if self.pk:
+                url_checker = url_checker.exclude(master_id=self.pk)
+            if url_checker.exists():
+                raise ValidationError(_("URL already exists."), code="invalid_url")
+
+        if self.pk:
+            original_page = Page.objects.get(id=self.pk)
+            if original_page.page_type == PageType.GDPR_CONSENT_DOCUMENT:
+                # prevent changing content when page type is GDPR_CONSENT_DOCUMENT
+                content = getattr(self, "content", None)
+                if original_page.content != content or original_page.page_type != self.page_type:
+                    msg = _("This page is protected against changes because it is a GDPR consent document.")
+                    raise ValidationError(msg, code="gdpr-protected")
+
+    def is_visible(self, dt=None):
+        if not dt:
+            dt = now()
+
+        return (
+            (self.available_from and self.available_from <= dt) and
+            (self.available_to is None or self.available_to >= dt)
+        )
+
+    def get_html(self):
+        return self.content
+
+    def __str__(self):
+        return force_text(self.safe_translation_getter("title", any_language=True, default=_("Untitled")))
+
+
+PageLogEntry = define_log_model(Page)
