eb035d3dee922497ab3f90571974dd9a28b1eade
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:14:24 +0000

    Add package forkbuntu/0.2.7/forkbuntu-0.2.7-py2.py3-none-any.whl

---
+++ packages/forkbuntu/forkbuntu-0.2.7-py2.py3-none-any.whl/forkbuntu/steps/create_extras.py
@@ -0,0 +1,23 @@
+from ..step import Step
+from munch import munchify
+from os import path
+
+class CreateExtras(Step):
+    agnostic = True
+    messages = munchify({
+        'past': 'created extras',
+        'present': 'creating extras',
+    })
+    requires = ['build_keyring']
+
+    def __init__(self, name, app):
+        super().__init__(name, app)
+        c = app.conf
+        self.checksum_paths = [
+            c.paths.apt_ftparchive,
+            c.paths.indices
+        ]
+
+    def run(self):
+        s = self.app.services
+        s.extras.create()
