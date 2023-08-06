6131da95164242285fb1594c0f889252faad9152
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:24:41 +0000

    Add package mastercard-parameter-management-services/1.0.5/mastercard_parameter_management_services-1.0.5-py2-none-any.whl

---
+++ packages/mastercard-parameter-management-services/mastercard_parameter_management_services-1.0.5-py2-none-any.whl/mastercardparametermanagementservices/__init__.py
@@ -0,0 +1,9 @@
+from mastercardapicore import *
+
+# import apis resourceconfig
+from mastercardparametermanagementservices.resourceconfig import ResourceConfig
+
+
+# import apis into api package
+from mastercardparametermanagementservices.dailycreditlimit import *
+from mastercardparametermanagementservices.partnerstatus import *
