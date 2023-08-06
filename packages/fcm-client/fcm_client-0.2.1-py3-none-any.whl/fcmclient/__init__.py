bc6b6f7c82267af30a29e44b7a02a09862acb762
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:48:25 +0000

    Add package fcm-client/0.2.1/fcm_client-0.2.1-py3-none-any.whl

---
+++ packages/fcm-client/fcm_client-0.2.1-py3-none-any.whl/fcmclient/__init__.py
@@ -0,0 +1,24 @@
+# Copyright 2017 Joya Communications, John Loehrer
+# Based on work by Sardar Yumatov
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+__title__ = 'FCM client'
+__author__ = "John Loehrer"
+__contact__ = "72squared@gmail.com"
+__license__ = "Apache 2.0"
+__homepage__ = "https://github/happybits/fcm-client/"
+__copyright__ = 'Copyright 2018 Joya Communications, John Loehrer'
+
+from .version import __version__  # noqa
+from .api import *  # noqa
