605ec98f1b03ec67ac90065bbe955a13c473b917
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:51:14 +0000

    Add package with-aws-mfa/0.1/with-aws-mfa-0.1.tar.gz

---
+++ packages/with-aws-mfa/with-aws-mfa-0.1.tar.gz/with-aws-mfa-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+ 
+setup(
+     name='with-aws-mfa',    # This is the name of your PyPI-package.
+     version='0.1',                          # Update the version number for new releases
+     scripts=['with-aws-mfa'],                  # The name of your scipt, and also the command you'll be using for calling it
+     author='Dan Aronson',
+     author_email='dan.aronson@gmail.com',
+     url='https://github.com/danaronson/with_aws_mfa'
+
+ )
