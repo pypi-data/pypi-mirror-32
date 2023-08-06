5acc24e0b805837a266548d083bcc591b8953175
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:50:44 +0000

    Add package nester123456/1.0.0/nester123456-1.0.0.tar.gz

---
+++ packages/nester123456/nester123456-1.0.0.tar.gz/nester123456-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name='nester123456',
+    version='1.0.0',
+    py_modules=['nester123456'],
+    author='kikujiang',
+    author_email='1014637386@qq.com',
+    url='http://www.haha.com',
+    description='递归函数'
+)
