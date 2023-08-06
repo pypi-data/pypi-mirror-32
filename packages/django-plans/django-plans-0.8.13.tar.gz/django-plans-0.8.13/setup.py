09ae9c7b6ed5c9dcd83d33bf49766ea4edffc111
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:49:46 +0000

    Add package django-plans/0.8.13/django-plans-0.8.13.tar.gz

---
+++ packages/django-plans/django-plans-0.8.13.tar.gz/django-plans-0.8.13/setup.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import find_packages, setup
+from plans import __version__ as VERSION
+
+with open('README.rst') as file:
+    long_description = file.read()
+
+setup(
+    name='django-plans',
+    version=VERSION,
+    description='Pluggable django app for managing pricing plans with quotas and accounts expiration',
+    long_description=long_description,
+    author='Krzysztof Dorosz',
+    author_email='cypreess@gmail.com',
+    url='https://github.com/cypreess/django-plans',
+    license='MIT',
+
+    packages=find_packages(),
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.6',
+    ],
+    install_requires=[
+        'django-countries>=4.5',
+        'pytz>=2017.2',
+        'django-ordered-model>=1.4.1',
+        'vatnumber>=1.2',
+        'celery',
+        'suds-jurko',
+        'six',
+    ],
+    extras_require={
+        'i18n': [
+            'django-modeltranslation>=0.5b1',
+        ],
+    },
+    include_package_data=True,
+    zip_safe=False,
+)
