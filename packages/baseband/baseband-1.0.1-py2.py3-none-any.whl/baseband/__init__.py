f9d5b942e48e3d1e6d8cc0384f707ccc48497a47
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:43 +0000

    Add package baseband/1.0.1/baseband-1.0.1-py2.py3-none-any.whl

---
+++ packages/baseband/baseband-1.0.1-py2.py3-none-any.whl/baseband/__init__.py
@@ -0,0 +1,17 @@
+# Licensed under the GPLv3 - see LICENSE
+"""
+Reading and writing VLBI and other radio baseband files.
+
+It relies on numpy and astropy.
+"""
+
+# Affiliated packages may add whatever they like to this file, but
+# should keep this content at the top.
+# ----------------------------------------------------------------------------
+from ._astropy_init import *
+# ----------------------------------------------------------------------------
+
+# For egg_info test builds to pass, put package imports here.
+if not _ASTROPY_SETUP_:
+    # from example_mod import *
+    pass
