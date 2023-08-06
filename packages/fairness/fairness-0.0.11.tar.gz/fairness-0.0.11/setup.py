7743ab248275d141c51a9883c5f1c81ebc1ef661
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:12:33 +0000

    Add package fairness/0.0.11/fairness-0.0.11.tar.gz

---
+++ packages/fairness/fairness-0.0.11.tar.gz/fairness-0.0.11/setup.py
@@ -0,0 +1,45 @@
+import setuptools
+
+VERSION = '0.0.11'
+
+INSTALL_REQUIRES = [
+  'pandas>=0.21.1',
+  'pyparsing>=2.1.4',
+  'python-dateutil>=2.6.0',
+  'pytz',
+  'readline>=6.2',
+  'scikit-learn>=0.18.1',
+  'scipy>=0.19.0',
+  'six>=1.10.0',
+  'wheel>=0.29.0',
+  'fire',
+  'BlackBoxAuditing>=0.1.26'
+  'ggplot'
+]
+
+PACKAGE_DATA = {
+  'fairness' : ['data/raw/*.csv','data/preprocessed/*.csv']
+}
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+    setuptools.setup(
+    name="fairness",
+    version=VERSION,
+    author="See Authors.txt",
+    author_email="fairness@haverford.edu",
+    description="Fairness-aware machine learning: algorithms, comparisons, bechmarking",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/algofairness/fairness-comparison",
+    packages=setuptools.find_packages(),
+    package_data = PACKAGE_DATA,
+    classifiers=(
+                 "Programming Language :: Python :: 3",
+                 "License :: OSI Approved :: Apache Software License",
+                 "Operating System :: OS Independent",
+                ),
+    install_requires=INSTALL_REQUIRES
+    )
+
