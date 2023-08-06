cec7aadd72092e06726411ce84af0be3d0836a42
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:37:20 +0000

    Add package django-discard/0.0.2/django-discard-0.0.2.tar.gz

---
+++ packages/django-discard/django-discard-0.0.2.tar.gz/django-discard-0.0.2/setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='django-discard',
+    version='0.0.2',
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    license='MIT License',
+    description='A simple package to add discard (soft-delete) functionality to\
+                 desired models',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/tcostam/django-discard',
+    author='Tiago Melo',
+    author_email='to_be_provided@email.com',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Framework :: Django :: 2.0',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6'
+    ],
+)
