557b9a3dcd0ba13ad6c3a6186fefe4e1e306fafa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:21 +0000

    Add package climt/0.14.8/climt-0.14.8-cp36-cp36m-manylinux1_x86_64.whl

---
+++ packages/climt/climt-0.14.8-cp36-cp36m-manylinux1_x86_64.whl/climt/__init__.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+
+import sympl
+
+from ._core import (
+    get_default_state, mass_to_volume_mixing_ratio,
+    get_interface_values, climt_quantity_descriptions, numpy_version_of,
+    bolton_q_sat, bolton_dqsat_dT, calculate_q_sat,
+    ClimtImplicitPrognostic, ClimtPrognostic, ClimtImplicit, ClimtDiagnostic,
+    ClimtSpectralDynamicalCore,
+    set_constant, get_constant, reset_constants,
+    list_available_constants, set_constants_from_dict)
+
+from ._components import (
+    Frierson06LongwaveOpticalDepth, GrayLongwaveRadiation, HeldSuarez,
+    GridScaleCondensation, BergerSolarInsolation, SimplePhysics, RRTMGLongwave, RRTMGShortwave,
+    EmanuelConvection, SlabSurface, GFSDynamicalCore,
+    DcmipInitialConditions, IceSheet, Instellation)
+
+
+sympl.add_direction_names(z=['interface_levels', 'mid_levels'])
+
+__all__ = (
+    get_default_state, mass_to_volume_mixing_ratio, numpy_version_of,
+    get_interface_values, get_constant, set_constant,
+    reset_constants, list_available_constants,
+    set_constants_from_dict,
+    bolton_q_sat, bolton_dqsat_dT, climt_quantity_descriptions, calculate_q_sat,
+    ClimtImplicitPrognostic, ClimtPrognostic, ClimtImplicit, ClimtDiagnostic,
+    ClimtSpectralDynamicalCore,
+    Frierson06LongwaveOpticalDepth, GrayLongwaveRadiation, HeldSuarez,
+    GridScaleCondensation, BergerSolarInsolation, SimplePhysics, RRTMGLongwave, RRTMGShortwave,
+    EmanuelConvection, SlabSurface, GFSDynamicalCore, DcmipInitialConditions, IceSheet,
+    Instellation)
+
+__version__ = '0.14.8'
