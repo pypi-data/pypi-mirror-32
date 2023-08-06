e5319cf818b3f9a04b6c7a985166dd7027fc06d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:40 +0000

    Add package Numerical-CFS/0.1.9.5/Numerical_CFS-0.1.9.5-py3-none-any.whl

---
+++ packages/Numerical-CFS/Numerical_CFS-0.1.9.5-py3-none-any.whl/Numerical_CFS/foo.py
@@ -0,0 +1,20 @@
+
+class cha():
+    def __init__(self, a, d):
+        self.a = a
+        self.d = d
+    def f(self):
+        return self.a - 1
+
+    def set_a(val):
+        self.a = val
+
+    def g(self):
+        self.d[0] = [3]
+        return  self.d
+
+if __name__ == "__main__":
+    x = cha(1,[[1],[2]])
+    print(x.g())
+
+
