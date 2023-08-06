2248801847a8d6f6462ed02e287ffbed849416fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:16:56 +0000

    Add package tbsh/0.2.2/tbsh-0.2.2.tar.gz

---
+++ packages/tbsh/tbsh-0.2.2.tar.gz/tbsh-0.2.2/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+ 
+setup(
+    name='tbsh',    # This is the name of your PyPI-package.
+    version='0.2.2',                          # Update the version number for new releases    
+    author="Sriharsha Sammeta",
+    author_email="sriharsha4444@icloud.com",
+    description='The funniest joke in the world',
+    url='http://github.com/storborg/funniest',
+    license='MIT',
+    packages=['tbsh'],    
+    zip_safe=False
+)
