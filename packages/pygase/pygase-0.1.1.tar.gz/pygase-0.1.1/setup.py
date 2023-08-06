437da3c254940b6d17188c0c2248d36fa4a12073
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:14:06 +0000

    Add package pygase/0.1.1/pygase-0.1.1.tar.gz

---
+++ packages/pygase/pygase-0.1.1.tar.gz/pygase-0.1.1/setup.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+setup(
+    name='pygase',
+    version='0.1.1',
+    description='A high-performance game server, client and UDP-based network protocol for real-time online gaming.',
+    author='Silas Bischoff',
+    author_email='silas.bischoff@stud.uni-due.de',
+    license='MIT',
+    url='https://github.com/sbischoff-ai/python-game-service',
+    download_url= 'https://github.com/sbischoff-ai/python-game-service/archive/0.1.1.tar.gz',
+    keywords=['server', 'client', 'games', 'gaming', 'real-time', 'network', 'UDP', 'protocol', 'game server', 'game service', 'gameservice'],
+    classifiers=[
+        'Programming Language :: Python',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: POSIX',
+        'Operating System :: MacOS',
+        'Topic :: Games/Entertainment',
+        'Topic :: Internet',
+        'Topic :: Software Development',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: System :: Networking',
+        'Intended Audience :: Developers',
+        'Development Status :: 3 - Alpha'
+    ],
+    packages=['pygase'],
+    install_requires=['umsgpack']
+)
