9c5e0eb3f63a832ba88bdb2909cd825871a3a933
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:18 +0000

    Add package djangolint/0.2.1/djangolint-0.2.1-py2.py3-none-any.whl

---
+++ packages/djangolint/djangolint-0.2.1-py2.py3-none-any.whl/djangolint-0.2.1.dist-info/DESCRIPTION.rst
@@ -0,0 +1,3 @@
+django验证库,准备集成swagger文档功能,得益于wtforms,所以支持json验证
+
+
---
+++ packages/djangolint/djangolint-0.2.1-py2.py3-none-any.whl/djangolint/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+from djangolint import validators, fields
+from djangolint.fields import *
+from djangolint.jsons import Json
+from djangolint.validators import ValidationError
+
+__version__ = '0.2.1dev'
