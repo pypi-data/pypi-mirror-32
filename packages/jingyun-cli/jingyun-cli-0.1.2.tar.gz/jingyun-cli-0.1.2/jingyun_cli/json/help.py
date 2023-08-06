833cec88452a6698c0110fcbffa3bb8acf35f070
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:13:27 +0000

    Add package jingyun-cli/0.1.2/jingyun-cli-0.1.2.tar.gz

---
+++ packages/jingyun-cli/jingyun-cli-0.1.2.tar.gz/jingyun-cli-0.1.2/jingyun_cli/json/help.py
@@ -0,0 +1,30 @@
+#! /usr/bin/env python
+# coding: utf-8
+
+import sys
+from functools import partial
+from jingyun_cli.util.help import help_value
+
+__author__ = '鹛桑够'
+
+
+output_help = {"en": "the output of file, default is stdout.", "cn": "输出文件的路径，默认输出到标准输出流"}
+json_file_help = {"en": "the path of json file", "cn": "json 文件的路径"}
+file_not_exist_help = {"en": "file %s not exist", "cn": "文件%s不存在"}
+read_error_help = {"en": "read file %s error", "cn": "读取文件%s失败"}
+content_error_help = {"en": "file %s not json content", "cn": "文件%s里的内容不是json格式"}
+
+
+help_keys = filter(lambda x: x.endswith("_help"), locals().keys())
+help_dict = dict()
+for key in help_keys:
+    help_dict[key[:-5]] = locals()[key]
+
+
+g_help = partial(help_value, help_dict)
+
+
+def error_and_exit(msg, error_code=1):
+    sys.stderr.write(msg)
+    sys.stderr.write("\n")
+    sys.exit(error_code)
