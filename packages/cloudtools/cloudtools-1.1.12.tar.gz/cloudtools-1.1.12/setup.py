ec045f772f67a825841eb68d383ebe1dd2594cb9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:38:50 +0000

    Add package cloudtools/1.1.12/cloudtools-1.1.12.tar.gz

---
+++ packages/cloudtools/cloudtools-1.1.12.tar.gz/cloudtools-1.1.12/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+setup(name='cloudtools',
+	  version='1.1.12',
+	  description='Collection of utilities for working on the Google Cloud Platform.',
+	  url='https://github.com/Nealelab/cloudtools',
+	  author='Liam Abbott',
+	  author_email='labbott@broadinstitute.org',
+	  license='MIT',
+	  classifiers=[
+	      'Development Status :: 3 - Alpha',
+	      'License :: OSI Approved :: MIT License',
+	      'Programming Language :: Python :: 2.7'
+	  ],
+	  keywords='google cloud dataproc spark jupyter hail notebook ipython',
+	  packages=['cloudtools'],
+	  entry_points={
+	      'console_scripts': [
+	          'cluster = cloudtools.__main__:main'
+	      ]
+	  },
+	  )
