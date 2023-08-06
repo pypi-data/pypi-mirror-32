8664e33cea8aadc74eb1d1fd9541eec4b4cea2d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:22:13 +0000

    Add package pyls-mypy/0.1.3/pyls-mypy-0.1.3.tar.gz

---
+++ packages/pyls-mypy/pyls-mypy-0.1.3.tar.gz/pyls-mypy-0.1.3/pyls_mypy/plugin.py
@@ -0,0 +1,42 @@
+import re
+from mypy import api as mypy_api
+from pyls import hookimpl
+
+line_pattern = r"([^:]+):(?:(\d+):)?(?:(\d+):)? (\w+): (.*)"
+
+
+def parse_line(line):
+    result = re.match(line_pattern, line)
+    if result:
+        _, lineno, offset, severity, msg = result.groups()
+        lineno = int(lineno or 1)
+        offset = int(offset or 0)
+        errno = 2
+        if severity == 'error':
+            errno = 1
+        return {
+            'source': 'mypy',
+            'range': {
+                'start': {'line': lineno - 1, 'character': offset},
+                # There may be a better solution, but mypy does not provide end
+                'end': {'line': lineno - 1, 'character': offset + 1}
+            },
+            'message': msg,
+            'severity': errno
+        }
+
+
+@hookimpl
+def pyls_lint(document):
+    args = ('--incremental',
+            '--show-column-numbers',
+            '--command', document.source)
+    report, errors, _ = mypy_api.run(args)
+
+    diagnostics = []
+    for line in report.splitlines():
+        diag = parse_line(line)
+        if diag:
+            diagnostics.append(diag)
+
+    return diagnostics
---
+++ packages/pyls-mypy/pyls-mypy-0.1.3.tar.gz/pyls-mypy-0.1.3/test/test_plugin.py
@@ -0,0 +1,43 @@
+from pyls.workspace import Document
+from pyls_mypy import plugin
+
+DOC_URI = __file__
+DOC_TYPE_ERR = """{}.append(3)
+"""
+TYPE_ERR_MSG = '"Dict[<nothing>, <nothing>]" has no attribute "append"'
+
+TEST_LINE = 'main.py:279:8: error: "Request" has no attribute "id"'
+TEST_LINE_WITHOUT_COL = 'main.py:279: error: "Request" has no attribute "id"'
+TEST_LINE_WITHOUT_LINE = 'main.py: error: "Request" has no attribute "id"'
+
+
+def test_plugin():
+    doc = Document(DOC_URI, DOC_TYPE_ERR)
+    diags = plugin.pyls_lint(doc)
+
+    assert len(diags) == 1
+    diag = diags[0]
+    assert diag['message'] == TYPE_ERR_MSG
+    assert diag['range']['start'] == {'line': 0, 'character': 1}
+    assert diag['range']['end'] == {'line': 0, 'character': 2}
+
+
+def test_parse_full_line():
+    diag = plugin.parse_line(TEST_LINE)
+    assert diag['message'] == '"Request" has no attribute "id"'
+    assert diag['range']['start'] == {'line': 278, 'character': 8}
+    assert diag['range']['end'] == {'line': 278, 'character': 9}
+
+
+def test_parse_line_without_col():
+    diag = plugin.parse_line(TEST_LINE_WITHOUT_COL)
+    assert diag['message'] == '"Request" has no attribute "id"'
+    assert diag['range']['start'] == {'line': 278, 'character': 0}
+    assert diag['range']['end'] == {'line': 278, 'character': 1}
+
+
+def test_parse_line_without_line():
+    diag = plugin.parse_line(TEST_LINE_WITHOUT_LINE)
+    assert diag['message'] == '"Request" has no attribute "id"'
+    assert diag['range']['start'] == {'line': 0, 'character': 0}
+    assert diag['range']['end'] == {'line': 0, 'character': 1}
