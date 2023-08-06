1a891c82e3c59624c0cf76ee93211e6ee546657d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:49:16 +0000

    Add package magfacy/0.0.2/magfacy-0.0.2.tar.gz

---
+++ packages/magfacy/magfacy-0.0.2.tar.gz/magfacy-0.0.2/setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+
+setuptools.setup(
+    name="magfacy",
+    version="0.0.2",
+    author="gowthambruse",
+    author_email="gowthambruse@gmail.com",
+    description="A small example package",
+    url="https://github.com/pypa/example-project",
+    packages=setuptools.find_packages(),
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+)
