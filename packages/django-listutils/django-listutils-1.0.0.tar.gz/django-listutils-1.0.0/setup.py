18822653a0c8e424c4b81aba069c4e0f4d01e981
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:10:35 +0000

    Add package django-listutils/1.0.0/django-listutils-1.0.0.tar.gz

---
+++ packages/django-listutils/django-listutils-1.0.0.tar.gz/django-listutils-1.0.0/setup.py
@@ -0,0 +1,70 @@
+#! /usr/bin/env python
+
+from distutils.core import setup
+
+setup(name="django-listutils",
+      version="1.0.0",
+      description="Django template tag to split list into sub lists.",
+      packages=["listutils", 'listutils.templatetags'],
+      author="Iury O. G. Figueiredo",
+      author_email="ioliveira@id.uff.br",
+      url='https://github.com/iogf/listutils',
+      download_url='https://github.com/iogf/listutils/releases',
+      keywords=['django', 'listutils', 'split', 'lists'],
+      classifiers=[])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
