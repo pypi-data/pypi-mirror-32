4694383900d1dd163758a1de9df23e83e649d337
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:33 +0000

    Add package pylivestream/1.6.5/PyLivestream-1.6.5-py3-none-any.whl

---
+++ packages/pylivestream/PyLivestream-1.6.5-py3-none-any.whl/PyLivestream-1.6.5.data/scripts/ScreenCapture2disk.py
@@ -0,0 +1,37 @@
+#!/home/thin/miniconda3/bin/python
+"""
+Save to disk screen capture for upload to YOUTUBE using FFMPEG
+
+https://www.scivision.co/youtube-ffmpeg-screen-capture-with-audio/
+https://trac.ffmpeg.org/wiki/Capture/Desktop
+https://support.google.com/youtube/answer/2853702
+
+Windows: get DirectShow device list from:
+   ffmpeg -list_devices true -f dshow -i dummy
+"""
+import PyLivestream
+
+if __name__ == '__main__':
+    import signal
+    signal.signal(signal.SIGINT, signal.SIG_DFL)
+
+    from argparse import ArgumentParser
+    p = ArgumentParser()
+    p.add_argument('outfn', help='video file to save to disk.')
+    p.add_argument('-c', '--clobber', help='overwrite file with same name',
+                   action='store_true')
+    p.add_argument('-i', '--ini', help='*.ini file with stream parameters',
+                   default='stream.ini')
+    p.add_argument('-y', '--yes', help='no confirmation dialog',
+                   action='store_true')
+    P = p.parse_args()
+
+    s = PyLivestream.SaveDisk(P.ini, P.outfn, P.clobber)
+# %%
+    if P.yes:
+        print('saving screen capture to', s.outfn)
+    else:
+        input(f"Press Enter to screen capture to file {s.outfn}."
+              "Or Ctrl C to abort.")
+
+    s.save()
