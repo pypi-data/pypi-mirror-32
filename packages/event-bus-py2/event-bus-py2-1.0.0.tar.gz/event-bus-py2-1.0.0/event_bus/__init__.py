c8f94318b837f16fb1aa987aa5e48e8e2e280bf3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:13:00 +0000

    Add package event-bus-py2/1.0.0/event-bus-py2-1.0.0.tar.gz

---
+++ packages/event-bus-py2/event-bus-py2-1.0.0.tar.gz/event-bus-py2-1.0.0/event_bus/__init__.py
@@ -0,0 +1,5 @@
+from bus import EventBus
+
+__version__ = '1.0.0'
+
+__all__ = ['EventBus']
---
+++ packages/event-bus-py2/event-bus-py2-1.0.0.tar.gz/event-bus-py2-1.0.0/setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+setuptools.setup(
+    name="event-bus-py2",
+    version="1.0.0",
+    author="yxb",
+    author_email="345252408@qq.com",
+    description="event_bus for 2.7",
+    url="https://github.com/pypa/example-project",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 2.7",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
