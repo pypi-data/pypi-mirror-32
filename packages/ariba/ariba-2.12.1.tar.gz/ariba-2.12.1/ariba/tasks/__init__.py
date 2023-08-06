bf284d8d8ba17dd1cb8a9163f8f9206f37b0c0e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:39:09 +0000

    Add package ariba/2.12.1/ariba-2.12.1.tar.gz

---
+++ packages/ariba/ariba-2.12.1.tar.gz/ariba-2.12.1/ariba/tasks/__init__.py
@@ -0,0 +1,18 @@
+__all__ = [
+    'aln2meta',
+    'expandflag',
+    'flag',
+    'getref',
+    'micplot',
+    'prepareref',
+    'pubmlstget',
+    'pubmlstspecies',
+    'refquery',
+    'reportfilter',
+    'run',
+    'summary',
+    'test',
+    'version',
+]
+
+from ariba.tasks import *
