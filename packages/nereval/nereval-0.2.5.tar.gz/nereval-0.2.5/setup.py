8087c83209426b8f25c734c4b4e8e9c70c2a8150
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:59:34 +0000

    Add package nereval/0.2.5/nereval-0.2.5.tar.gz

---
+++ packages/nereval/nereval-0.2.5.tar.gz/nereval-0.2.5/setup.py
@@ -0,0 +1,22 @@
+import os
+from setuptools import setup
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name='nereval',
+    version='0.2.5',
+    author='Jan Trienes',
+    author_email='jan.trienes@googlemail.com',
+    url='https://github.com/jantrienes/nereval',
+    description='Evaluation script for named entity recognition systems based on F1 score.',
+    long_description=read('README.rst'),
+    keywords=['ner', 'nlp', 'evaluation', 'f1_score', 'f1', 'linguistics', 'machine_learning'],
+    license='MIT',
+    py_modules=['nereval'],
+    tests_require=[
+        'pytest',
+        'pytest-cov',
+    ]
+)
