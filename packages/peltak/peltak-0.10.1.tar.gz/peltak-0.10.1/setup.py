775b1f950fe9a112dc3eb75043685f560d6af3b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:49:31 +0000

    Add package peltak/0.10.1/peltak-0.10.1.tar.gz

---
+++ packages/peltak/peltak-0.10.1.tar.gz/peltak-0.10.1/setup.py
@@ -0,0 +1,36 @@
+import os
+from setuptools import setup, find_packages
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+setup(
+    name="peltak",
+    version=read('VERSION').strip(),
+    author="Mateusz 'novo' Klos",
+    author_email="novopl@gmail.com",
+    license="MIT",
+    keywords="fabric ops devops",
+    url="http://github.com/novopl/peltak",
+    description="Set of CLI commands to help manage a project",
+    long_description=read('README.rst'),
+    package_dir={'': 'src'},
+    packages=find_packages('src'),
+    install_requires=[
+        l.strip() for l in read('requirements.txt').split() if '==' in l
+    ],
+    entry_points={
+        'console_scripts': [
+            'peltak = peltak.main:cli',
+        ]
+    },
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Topic :: Utilities",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python",
+        "License :: OSI Approved :: MIT License",
+    ],
+)
