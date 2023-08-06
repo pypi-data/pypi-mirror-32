922ef807b64f9c5d52d64fc56b36c52080ba673b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:19:22 +0000

    Add package radware_os_lb_v2_pike/4.5.0-1/radware_os_lb_v2_pike-4.5.0-1.tar.gz

---
+++ packages/radware_os_lb_v2_pike/radware_os_lb_v2_pike-4.5.0-1.tar.gz/radware_os_lb_v2_pike-4.5.0-1/setup.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+# Copyright (c) 2017 Radware LTD. All rights reserved
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# THIS FILE IS MANAGED BY THE GLOBAL REQUIREMENTS REPO - DO NOT EDIT
+from distutils import log
+import os
+from setuptools import setup
+from setuptools.command.install import install
+
+
+class OverrideInstall(install):
+
+    def run(self):
+        mode = 0o644
+        install.run(self)
+        for filepath in self.get_outputs():
+            log.info("Changing permissions of %s to %s" %
+                     (filepath, oct(mode)))
+            os.chmod(filepath, mode)
+
+
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
+setup(name='radware_os_lb_v2_pike',
+      version='4.5.0-1',
+      description='Radware LBaaS v2 driver for Openstack Pike',
+      long_description = readme(),
+      classifiers=[
+        'Environment :: OpenStack',
+        'Intended Audience :: Information Technology',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 2.7'
+      ],
+      keywords=['openstack','radware','lbaasv2'],
+      url='https://pypi.python.org/pypi/radware_os_lb_v2_pike',
+      author='Evgeny Fedoruk, Radware',
+      author_email='evgenyf@radware.com',
+      packages=['radware_os_lb_v2_pike'],
+      zip_safe=False,
+      cmdclass={'install': OverrideInstall})
