e0ea10a8f7e74ff2366533d544396119cc8c1950
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:25:52 +0000

    Add package ipydatawidgets/3.1.0/ipydatawidgets-3.1.0-py2.py3-none-any.whl

---
+++ packages/ipydatawidgets/ipydatawidgets-3.1.0-py2.py3-none-any.whl/ipydatawidgets/nbextension/static/lib/version.d.ts
@@ -0,0 +1,9 @@
+/**
+ * The version of the Jupyter data widgets attribute spec that this package
+ * implements.
+ */
+export declare const EXTENSION_SPEC_VERSION = "4.0.0";
+/**
+ * The current package version.
+ */
+export declare const version: any;
