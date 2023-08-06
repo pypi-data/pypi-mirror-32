e77e19f59e05779665793768df9345dc5c3ea8d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:29 +0000

    Add package django-redirect-urls/1.0/django_redirect_urls-1.0-py2.py3-none-any.whl

---
+++ packages/django-redirect-urls/django_redirect_urls-1.0-py2.py3-none-any.whl/redirect_urls/middleware.py
@@ -0,0 +1,23 @@
+from django.urls import Resolver404
+
+from redirect_urls.utils import get_resolver
+
+
+class RedirectsMiddleware(object):
+    def __init__(self, get_response=None, resolver=None):
+        self.get_response = get_response
+        self.resolver = resolver or get_resolver()
+        super(RedirectsMiddleware, self).__init__()
+
+    def __call__(self, request):
+        try:
+            resolver_match = self.resolver.resolve(request.path_info)
+        except Resolver404:
+            if self.get_response is None:
+                return None
+            else:
+                return self.get_response(request)
+
+        callback, callback_args, callback_kwargs = resolver_match
+        request.resolver_match = resolver_match
+        return callback(request, *callback_args, **callback_kwargs)
