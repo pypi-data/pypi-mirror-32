aaa429c2b0ab17a3026186fc434eaae8c75a7a06
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:23:11 +0000

    Add package featureflagclient/0.0/featureflagclient-0.0.tar.gz

---
+++ packages/featureflagclient/featureflagclient-0.0.tar.gz/featureflagclient-0.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+setup(
+  name = 'featureflagclient',
+  packages = ['featureflagclient'],
+  version = '0.0',
+  description = 'Service agnostic featureflag client',
+  author = 'Tom Maslen',
+  author_email = 'tom_maslen@hotmail.com',
+  url = 'https://github.com/tmaslen/featureflagclient-python',
+  download_url = 'https://github.com/tmaslen/featureflagclient-python/tarball/master',
+  keywords = ['feature flag', 'feature toggle'],
+)
