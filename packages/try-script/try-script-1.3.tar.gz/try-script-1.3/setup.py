933373d060ef8f77330ecdf8c1bfa326ad97f81a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:25:29 +0000

    Add package try-script/1.3/try-script-1.3.tar.gz

---
+++ packages/try-script/try-script-1.3.tar.gz/try-script-1.3/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+VERSION = '1.3'
+
+setup(
+    name='try-script',
+    packages=['try_pkg'],
+    version=VERSION,
+    description='Just script that helps me learn foreign languages,',
+    author='Jan Kaifer',
+    author_email='kaifer741@gamil.com',
+    url='https://github.com.jankaifer/app.py',
+    keywords=['learning', 'school', 'german', 'english', 'awesome'],
+    classifiers=[],
+    entry_points={
+        'console_scripts': [
+            'try = try_pkg.app:main',
+        ],
+    }
+)
