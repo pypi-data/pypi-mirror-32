b168aec4b5fb415d6b8968850869d9b7d6e723a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:51:03 +0000

    Add package pylendingclub/v1.7/pylendingclub-v1.7.zip

---
+++ packages/pylendingclub/pylendingclub-v1.7.zip/pylendingclub-v1.7/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name = 'pylendingclub',
+    packages = ['pylendingclub'],
+    version = 'v1.7',  # Ideally should be same as your GitHub release tag version
+    description = 'A Python wrapper for the Lending Club API.',
+    author = 'Brandon Barney',
+    author_email = 'brandon.barney213@yahoo.com',
+    url = 'https://github.com/bbarney213/PyLendingClub-Wrapper',
+    download_url = 'https://github.com/bbarney213/PyLendingClub-Wrapper/archive/v1.7.tar.gz',
+    keywords = ['python', 'lendingclub'],
+    classifiers = [],
+)
