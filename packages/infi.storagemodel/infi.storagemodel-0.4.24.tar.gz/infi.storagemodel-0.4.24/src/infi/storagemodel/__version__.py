b22066ea14d4cb2767d656993d36c22f9fd5ee5e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:55:54 +0000

    Add package infi.storagemodel/0.4.24/infi.storagemodel-0.4.24.tar.gz

---
+++ packages/infi.storagemodel/infi.storagemodel-0.4.24.tar.gz/infi.storagemodel-0.4.24/src/infi/storagemodel/__version__.py
@@ -0,0 +1,11 @@
+__version__ = "0.4.24"
+__git_commiter_name__ = "Arnon Yaari"
+__git_commiter_email__ = "arnony@infinidat.com"
+__git_branch__ = u'(HEAD detached at v0.4.24)'
+__git_remote_tracking_branch__ = '(No remote tracking)'
+__git_remote_url__ = '(Not remote tracking)'
+__git_head_hash__ = '2263615a6ae3adb4f71fe2e97598c6c2bbd8bf53'
+__git_head_subject__ = u'Finished Release v0.4.24'
+__git_head_message__ = u''
+__git_dirty_diff__ = u''
+__git_commit_date__ = '2018-06-10 12:46:41'
