f187ae76f55c8287608e56fcfc96b3a8f1b22ae2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:38:45 +0000

    Add package pingouin/0.1.3/pingouin-0.1.3.tar.gz

---
+++ packages/pingouin/pingouin-0.1.3.tar.gz/pingouin-0.1.3/pingouin/__init__.py
@@ -0,0 +1,12 @@
+# Import pingouin objects
+from .utils import *
+from .bayesian import *
+from .effsize import *
+from .multicomp import *
+from .parametric import *
+from .nonparametric import *
+from .correlation import *
+from .pairwise import *
+from .power import *
+
+__version__ = "0.1.3"
