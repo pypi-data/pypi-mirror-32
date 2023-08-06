d2e5b8de49c523ef9e94af5aef292a7190ca4172
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:22:13 +0000

    Add package nameko-cachetools/0.1.0/nameko-cachetools-0.1.0.tar.gz

---
+++ packages/nameko-cachetools/nameko-cachetools-0.1.0.tar.gz/nameko-cachetools-0.1.0/test/test_nameko_cachetools.py
@@ -0,0 +1,19 @@
+"""
+Tests for `nameko-cachetools` module.
+"""
+import pytest
+from nameko_cachetools import nameko_cachetools
+
+
+class TestNamekoCachetools(object):
+
+    @classmethod
+    def setup_class(cls):
+        pass
+
+    def test_something(self):
+        pass
+
+    @classmethod
+    def teardown_class(cls):
+        pass
