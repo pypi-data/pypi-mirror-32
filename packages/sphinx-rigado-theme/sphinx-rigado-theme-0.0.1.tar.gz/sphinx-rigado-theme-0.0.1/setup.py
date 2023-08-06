1c6bb6649e3f15768d151585b6afadd47d167201
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:58:53 +0000

    Add package sphinx-rigado-theme/0.0.1/sphinx-rigado-theme-0.0.1.tar.gz

---
+++ packages/sphinx-rigado-theme/sphinx-rigado-theme-0.0.1.tar.gz/sphinx-rigado-theme-0.0.1/setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(
+    name='sphinx-rigado-theme',
+    version='0.0.1',
+    author='Spencer Williams',
+    author_email='spencer.williams@rigado.com',
+    description='A Sphinx theme for Rigado',
+    url='https://git.rigado.com/documentation/sphinx-rigado-theme',
+    packages=find_packages(),
+)
