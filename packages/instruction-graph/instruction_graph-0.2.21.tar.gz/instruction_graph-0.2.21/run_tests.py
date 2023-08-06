0807ebfec24bf9af56159d5fa0638f0fc2e33547
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:39:14 +0000

    Add package instruction-graph/0.2.21/instruction_graph-0.2.21.tar.gz

---
+++ packages/instruction-graph/instruction_graph-0.2.21.tar.gz/instruction_graph-0.2.21/run_tests.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+import unittest
+
+
+if __name__ == "__main__":
+    loader = unittest.TestLoader()
+    suite = loader.discover('tests')
+    runner = unittest.TextTestRunner()
+    runner.run(suite)
