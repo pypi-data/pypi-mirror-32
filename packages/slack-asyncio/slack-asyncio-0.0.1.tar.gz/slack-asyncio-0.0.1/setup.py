7d5447dac881a0773305411f9e13a0232b57a71c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:14:51 +0000

    Add package slack-asyncio/0.0.1/slack-asyncio-0.0.1.tar.gz

---
+++ packages/slack-asyncio/slack-asyncio-0.0.1.tar.gz/slack-asyncio-0.0.1/setup.py
@@ -0,0 +1,27 @@
+from setuptools import find_packages
+from setuptools import setup
+
+
+setup(
+    name='slack-asyncio',
+    description='Slack bot and API library using asyncio',
+    url='https://github.com/chriskuehl/slack-asyncio',
+    version='0.0.1',
+
+    author='Chris Kuehl',
+    author_email='ckuehl@ckuehl.me',
+
+    classifiers=[
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: Implementation :: CPython',
+    ],
+
+    packages=find_packages(exclude=('tests*', 'testing*')),
+    install_requires=[
+        'aiohttp',
+        'asyncio-extras',
+        'websockets',
+    ],
+)
