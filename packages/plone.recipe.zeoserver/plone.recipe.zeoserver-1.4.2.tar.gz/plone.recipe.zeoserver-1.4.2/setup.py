70f2305e5750dc2fcd576d718d205b47e957fca7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:10:40 +0000

    Add package plone.recipe.zeoserver/1.4.2/plone.recipe.zeoserver-1.4.2.tar.gz

---
+++ packages/plone.recipe.zeoserver/plone.recipe.zeoserver-1.4.2.tar.gz/plone.recipe.zeoserver-1.4.2/setup.py
@@ -0,0 +1,60 @@
+# -*- coding: utf-8 -*-
+from setuptools import find_packages
+from setuptools import setup
+
+import sys
+
+
+version = '1.4.2'
+
+additional_install_requires = []
+
+if sys.platform[:3].lower() == 'win':
+    additional_install_requires += ['nt_svcutils']
+
+
+setup(
+    name="plone.recipe.zeoserver",
+    version=version,
+    author='Hanno Schlichting',
+    author_email='hannosch@plone.org',
+    description='ZC Buildout recipe for installing a ZEO server',
+    long_description=(
+        open('README.rst').read() +
+        '\n' +
+        open('CHANGES.rst').read()
+    ),
+    license='ZPL 2.1',
+    keywords='zope2 zeo zodb buildout',
+    url='https://github.com/plone/plone.recipe.zeoserver',
+    download_url='https://pypi.python.org/pypi/plone.recipe.zeoserver',
+    classifiers=[
+        'License :: OSI Approved :: Zope Public License',
+        'Framework :: Buildout',
+        'Framework :: Plone',
+        'Framework :: Plone :: 5.1',
+        'Framework :: Plone :: 5.2',
+        'Framework :: Zope2',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+    ],
+    packages=find_packages('src'),
+    include_package_data=True,
+    package_dir={'': 'src'},
+    namespace_packages=['plone', 'plone.recipe'],
+    install_requires=[
+        'setuptools',
+        'zc.buildout',
+        'zc.recipe.egg',
+        'ZODB3 >= 3.8',
+        'zope.mkzeoinstance >=4',
+        'ZopeUndo',
+    ] + additional_install_requires,
+    extras_require={
+        'zrs': ['zc.zrs']
+    },
+    zip_safe=False,
+    entry_points={
+        'zc.buildout': ['default = plone.recipe.zeoserver.recipe:Recipe'],
+    },
+)
