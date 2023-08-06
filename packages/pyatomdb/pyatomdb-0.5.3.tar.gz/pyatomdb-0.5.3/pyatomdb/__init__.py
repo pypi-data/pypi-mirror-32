72385499347cac730c45eb183d86fa859d0901db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:38:02 +0000

    Add package pyatomdb/0.5.3/pyatomdb-0.5.3.tar.gz

---
+++ packages/pyatomdb/pyatomdb-0.5.3.tar.gz/pyatomdb-0.5.3/pyatomdb/__init__.py
@@ -0,0 +1,23 @@
+__all__=["atomdb","util","atomic","spectrum","const","apec"]
+
+from .atomdb import *
+import spectrum
+import atomic
+import ctypes
+import apec
+import sys
+__version__="0.5.3"
+
+try:
+  PATH = os.path.dirname(__file__)
+  liblinapprox_file = os.path.join(PATH,"../linear_approx.dylib")
+  if not os.path.isfile(liblinapprox_file):
+    liblinapprox_file = os.path.join(PATH,"../linear_approx.so")
+   
+  liblinapprox = ctypes.CDLL( liblinapprox_file, ctypes.RTLD_GLOBAL)
+except OSError:
+  on_rtd=os.environ.get('READTHEDOCS')=='True'
+  if on_rtd:
+    pass
+  else:
+    raise
