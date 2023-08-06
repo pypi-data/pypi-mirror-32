3cb4e5633736da22d34e7348a88b6d97210f5842
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:26:21 +0000

    Add package ereuse-workbench/10.0b6/ereuse-workbench-10.0b6.tar.gz

---
+++ packages/ereuse-workbench/ereuse-workbench-10.0b6.tar.gz/ereuse-workbench-10.0b6/setup.py
@@ -0,0 +1,46 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='ereuse-workbench',
+    version='10.0b6',
+    packages=find_packages(),
+    license='AGPLv3 License',
+    description='The eReuse Workbench is '
+                'a toolset to help with the diagnostic, benchmarking, '
+                'inventory and installation of computers, '
+                'with the optional assistance of a local server.',
+    scripts=['scripts/erwb'],
+    url='https://github.com/eReuse/workbench',
+    author='eReuse.org team',
+    author_email='x.bustamante@ereuse.org',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: Information Technology',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: GNU Affero General Public License v3',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.5',
+        'Topic :: System :: Logging',
+        'Topic :: Utilities',
+    ],
+    install_requires=[
+        'python-dateutil',
+        'pydash',
+        'tqdm',
+        'pySMART.smartx',
+        'pyudev',
+        'requests',
+        'ereuse-utils [usb_flash_drive]',
+        'colorama',
+        'requests-toolbelt'
+    ],
+    setup_requires=[
+        'pytest-runner'
+    ],
+    tests_require=[
+        'pytest',
+        'pytest-mock'
+    ],
+)
