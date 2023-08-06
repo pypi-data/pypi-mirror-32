fa4e856d78a66a2183ccae4fcfc7c17b498da71e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:24 +0000

    Add package kicstextclassification/0.1/kicstextclassification-0.1.tar.gz

---
+++ packages/kicstextclassification/kicstextclassification-0.1.tar.gz/kicstextclassification-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='kicstextclassification',
+      version='0.1',
+      description='text classification in urdu',
+      url='https://github.com/minixain/UrduTextClassification',
+      author='minixain',
+      author_email='ali.ibrahim@kics.edu.pk',
+      license='KICS',
+      packages=['kicstextclassification'],
+      zip_safe=False)
