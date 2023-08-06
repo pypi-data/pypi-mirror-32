992502565ee49a77d138a2f8d4b787d23a110bdb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:37:59 +0000

    Add package creaturecast-project/0.0.2/creaturecast_project-0.0.2.tar.gz

---
+++ packages/creaturecast-project/creaturecast_project-0.0.2.tar.gz/creaturecast_project-0.0.2/creaturecast_project/__init__.py
@@ -0,0 +1,8 @@
+from functions import *
+
+__version__ = '0.0.2'
+__description__ = 'Base classes for projects, assets and components'
+__author__ = 'Paxton Gerrish'
+__email__ = 'creaturecastlabs@gmail.com'
+__url__ = 'https://github.com/Paxtongerrish/creaturecast_project.git'
+
