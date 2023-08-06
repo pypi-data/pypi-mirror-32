0abdbf65f30d18d337ca05ae168d04de356d6cef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:01:09 +0000

    Add package fem/0.0.47/fem-0.0.47.tar.gz

---
+++ packages/fem/fem-0.0.47.tar.gz/fem-0.0.47/fem/multiindex.py
@@ -0,0 +1,28 @@
+import numpy as np
+import combinatorics
+
+
+def get_multiindex(n, deg):
+
+    if deg > n:
+        return []
+
+    p = combinatorics.binomial_coefficients(n, deg)[-1]
+
+    vars = np.empty((p, deg), dtype=int)
+    var = np.arange(deg)
+    vars[0] = var.copy()
+
+    for i in range(1, p):
+
+        idx = deg - 1
+        var[idx] += 1
+        while var[idx] + deg - idx > n:
+            idx -= 1
+            var[idx] += 1
+        for idx in range(idx + 1, deg):
+            var[idx] = var[idx - 1] + 1
+
+        vars[i] = var.copy()
+
+    return vars
