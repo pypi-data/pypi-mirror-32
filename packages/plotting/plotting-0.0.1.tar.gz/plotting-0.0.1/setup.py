f1cec8f90280315290d071eccdcfd1810770a6a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:34:07 +0000

    Add package plotting/0.0.1/plotting-0.0.1.tar.gz

---
+++ packages/plotting/plotting-0.0.1.tar.gz/plotting-0.0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+install_requires = ["matplotlib", "seaborn"]
+
+setup(
+    name="plotting",
+    version="0.0.1",
+    author="Michael Rossol",
+    author_email="mrossol@gmail.com",
+    long_description=open("README.md").read(),
+    packages=find_packages(),
+    install_requires=install_requires,
+)
