0365135ea4f56226aa96ad6a51bd1e0747738201
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:31 +0000

    Add package bubble3/2018.6.4/bubble3-2018.6.4-py2.py3-none-any.whl

---
+++ packages/bubble3/bubble3-2018.6.4-py2.py3-none-any.whl/bubble3/metadata.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# Part of bubble. See LICENSE file for full copyright and licensing details.
+
+"""Project metadata
+
+Information describing the project.
+"""
+
+# The package name, which is also the 'UNIX name' for the project.
+package = 'bubble3'
+project = 'cli tool to bubble information from one to another service, with rule based transformations, PY3 only version'
+
+#version = '2016.02.23'  #date versioning
+version = "2018.06.04"
+
+description = 'cli tool to bubble information from one to another service, with rule based transformations ">>>"'
+authors = ['Erdal Yildiz']
+authors_string = ', '.join(authors)
+emails = ['erdalyildiz1974@gmail.com']
+license = 'GPL-3.0'
+copyright = '2018 ' + authors_string
+url = 'https://github.com/e7dal/bubble3'
+
