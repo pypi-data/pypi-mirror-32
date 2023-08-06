52b5b5cc705b7ce3020b90dfd1ae3a4aa1a236ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:35:22 +0000

    Add package aexpect/1.5.1/aexpect-1.5.1-py2-none-any.whl

---
+++ packages/aexpect/aexpect-1.5.1-py2-none-any.whl/aexpect/utils/genio.py
@@ -0,0 +1,27 @@
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+#
+# See LICENSE for more details.
+
+import os
+
+_open_log_files = {}
+
+
+def close_log_file(filename):
+    global _open_log_files
+    remove = []
+    for k in _open_log_files:
+        if os.path.basename(k) == filename:
+            f = _open_log_files[k]
+            f.close()
+            remove.append(k)
+    if remove:
+        for key_to_remove in remove:
+            _open_log_files.pop(key_to_remove)
