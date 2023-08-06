faef778a05af35fce132539c300cbfca2d080a3c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:14:35 +0000

    Add package peercoin-rpc/0.57/peercoin_rpc-0.57.tar.gz

---
+++ packages/peercoin-rpc/peercoin_rpc-0.57.tar.gz/peercoin_rpc-0.57/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+setup(name='peercoin_rpc',
+      version='0.57',
+      description='Library to communicate with peercoin daemon via JSON-RPC protocol.',
+      url='https://github.com/peercoin/peercoin_rpc',
+      author='Peerchemist',
+      author_email='peerchemist@protonmail.ch',
+      license='MIT',
+      packages=['peercoin_rpc'],
+      install_requires=['requests'],
+      keywords=['peercoin', 'json-rpc', 'cryptocurrency', 'blockchain'],
+      classifiers=[
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3',
+          'Programming Language :: Python :: 3.3',
+          'Programming Language :: Python :: 3.4',
+          'Programming Language :: Python :: 3.5',
+          'Programming Language :: Python :: 3.6'
+      ],
+      platforms="any")
