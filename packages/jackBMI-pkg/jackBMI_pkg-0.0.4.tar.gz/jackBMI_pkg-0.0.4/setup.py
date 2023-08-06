4a76d8bb7c66d8607865da6e7dc5f8e743a6516c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:50:28 +0000

    Add package jackBMI-pkg/0.0.4/jackBMI_pkg-0.0.4.tar.gz

---
+++ packages/jackBMI-pkg/jackBMI_pkg-0.0.4.tar.gz/jackBMI_pkg-0.0.4/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="jackBMI_pkg",
+    version="0.0.4",
+    author="Sundeep",
+    author_email="pidugusundeep5@gmail.com",
+    description="A small example package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://www.pidugusundeep.in",
+    packages=setuptools.find_packages(),
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+)
