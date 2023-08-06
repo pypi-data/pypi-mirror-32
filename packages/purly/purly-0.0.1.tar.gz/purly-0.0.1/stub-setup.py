2161e4eadafea863f88f63b38dfdf4eb6013bc7b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:59:19 +0000

    Add package purly/0.0.1/purly-0.0.1.tar.gz

---
+++ packages/purly/purly-0.0.1.tar.gz/purly-0.0.1/stub-setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='purly',
+    version='0.0.1',
+    description='Control the web with Python.',
+    long_description='# Purly',
+    url='https://github.com/rmorshea/purly',
+    author='Ryan Morshead',
+    author_email='ryan.morshead@gmail.com',
+    packages=[],
+    classifiers=['Development Status :: 1 - Planning'],
+)
