dc45cff675f814181c7aecd7d8fe9c7f804c77a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:25:13 +0000

    Add package reentry/1.2.1a3/reentry-1.2.1a3-py2-none-any.whl

---
+++ packages/reentry/reentry-1.2.1a3-py2-none-any.whl/reentry/config.py
@@ -0,0 +1,76 @@
+"""Find and read user settings."""
+import os
+import sys
+
+import six
+from six.moves import configparser
+from py import path as py_path  # pylint: disable=no-name-in-module
+
+__all__ = ['find_config', 'get_config', 'get_datafile']
+
+
+def find_config():
+    """
+    Search for a config file in the following places and order:
+
+        * <HOME>/.reentryrc
+        * <HOME>/.config/reentry/config
+    """
+    home = py_path.local(os.path.expanduser('~'))
+    rc_file = home.join('.reentryrc')
+    config_file = home.join('.config', 'reentry', 'config')
+    if home.exists():
+        return rc_file
+    elif config_file.exists():
+        return config_file
+    return rc_file
+
+
+def make_config_parser(*args, **kwargs):
+    """Get the correct ConfigParser class depending on python version."""
+    if six.PY2:
+        return configparser.SafeConfigParser(*args, **kwargs)
+    elif six.PY3:
+        return configparser.ConfigParser(*args, **kwargs)
+    return None
+
+
+def get_config(config_file_name=find_config().strpath):
+    """Create config parser with defaults and read in the config file."""
+    default_config_dir = py_path.local(os.path.expanduser('~/.config/reentry'))
+    parser = make_config_parser({'datadir': default_config_dir.join('data').strpath})
+    parser.add_section('general')
+    parser.read([config_file_name])
+
+    env_datadir = os.getenv('REENTRY_DATADIR')
+    if env_datadir:
+        env_datadir_path = py_path.local(env_datadir)
+        if env_datadir_path.exists() and not env_datadir_path.isdir():
+            raise ValueError('environment variable $REENTRY_DATADIR={} exists, but is not a directory'.format(env_datadir.strpath))
+        parser.set('general', 'datadir', env_datadir_path.strpath)
+
+    return parser
+
+
+def make_data_file_name():
+    """Find the path to the reentry executable and mangle it into a file name."""
+    sep = os.path.sep
+    python_bin_dir = py_path.local(sys.executable).dirname
+    py_version = 'UNKNOWN'
+    if six.PY2:
+        py_version = 'PY2'
+    elif six.PY3:
+        py_version = 'PY3'
+    file_name = python_bin_dir.lstrip(sep).replace(sep, '.').replace('.', '_') + '_' + py_version
+    return file_name
+
+
+def get_datafile():
+    """Create the path to the data file used to store entry points."""
+    config = get_config()
+    pkg_path_filename = make_data_file_name()
+    datafile = py_path.local(config.get('general', 'datadir')).join(pkg_path_filename)
+    datafile.ensure()
+    if not datafile.read():
+        datafile.write('{}')
+    return datafile.strpath
---
+++ packages/reentry/reentry-1.2.1a3-py2-none-any.whl/reentry/tests/fixtures.py
@@ -0,0 +1,41 @@
+# pylint: disable=unused-import,redefined-outer-name
+"""Unit test fixtures"""
+from os.path import join, dirname
+
+import pytest
+
+TEST_DATA_FILE = join(dirname(__file__), 'test_data.json')
+
+
+@pytest.fixture
+def test_data():
+    """yield a temporary test cache file"""
+    import tempfile
+    import os
+    test_data = None
+    with open(TEST_DATA_FILE, 'r') as test_data_file_obj:
+        test_data = test_data_file_obj.read()
+
+    file_handle, test_file = tempfile.mkstemp()
+    with open(test_file, 'w') as test_file_obj:
+        test_file_obj.write(test_data)
+
+    yield test_file
+
+    os.close(file_handle)
+    os.remove(test_file)
+
+
+@pytest.fixture
+def bkend(test_data):
+    """create a backend with test data"""
+    from reentry.jsonbackend import JsonBackend
+    test_file = test_data
+    return JsonBackend(datafile=test_file)
+
+
+@pytest.fixture
+def manager(bkend):
+    from reentry.default_manager import PluginManager
+    manager = PluginManager(backend=bkend)
+    yield manager
