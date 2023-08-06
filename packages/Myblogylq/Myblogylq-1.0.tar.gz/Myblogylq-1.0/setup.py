2cbc2a3133a817c3792520f02c1c5acb49dda139
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:19:51 +0000

    Add package Myblogylq/1.0/Myblogylq-1.0.tar.gz

---
+++ packages/Myblogylq/Myblogylq-1.0.tar.gz/Myblogylq-1.0/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup #如果没有需要先安装
+setup(name='Myblogylq',  #打包后的包文件名
+      version='1.0',  #版本
+     description='description',
+      author='ylq',
+      author_email='123@qq.com',
+      url=' ',
+      py_modules=['nesterylq'],  #与前面的新建文件名一致
+)
