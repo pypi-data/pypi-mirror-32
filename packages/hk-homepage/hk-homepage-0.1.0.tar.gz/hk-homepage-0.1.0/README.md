ba6012389db3abb94440c2064d96921c006522e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:59:54 +0000

    Add package hk-homepage/0.1.0/hk-homepage-0.1.0.tar.gz

---
+++ packages/hk-homepage/hk-homepage-0.1.0.tar.gz/hk-homepage-0.1.0/README.md
@@ -0,0 +1,20 @@
+# Homepage
+
+|master|develop|
+|:----:|:-----:|
+|[![build status](https://gitlab.namibsun.net/namibsun/python/hk-homepage/badges/master/build.svg)](https://gitlab.namibsun.net/namibsun/python/hk-homepage/commits/master)|[![build status](https://gitlab.namibsun.net/namibsun/python/hk-homepage/badges/develop/build.svg)](https://gitlab.namibsun.net/namibsun/python/hk-homepage/commits/develop)|
+
+![Logo](resources/logo/logo-readme.png)
+
+This is my personal homepage.
+
+## Further Information
+
+## Further Information
+
+* [Changelog](CHANGELOG)
+* [License (GPLv3)](LICENSE)
+* [Gitlab](https://gitlab.namibsun.net/namibsun/python/hk-homepage)
+* [Github](https://github.com/namboy94/hk-homepage)
+* [Progstats](https://progstats.namibsun.net/projects/hk-homepage)
+* [PyPi](https://pypi.org/project/hk-homepage)
---
+++ packages/hk-homepage/hk-homepage-0.1.0.tar.gz/hk-homepage-0.1.0/hk_homepage/router.py
@@ -0,0 +1,26 @@
+"""LICENSE
+Copyright 2015 Hermann Krumrey <hermann@krumreyh.com>
+
+This file is part of hk-homepage.
+
+hk-homepage is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+hk-homepage is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with hk-homepage.  If not, see <http://www.gnu.org/licenses/>.
+LICENSE"""
+
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return "Hello World!"
