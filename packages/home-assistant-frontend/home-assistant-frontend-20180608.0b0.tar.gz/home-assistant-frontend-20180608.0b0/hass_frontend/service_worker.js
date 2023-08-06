37805dc4229a5655bc57a9ccaf01d708745edfd9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:55:59 +0000

    Add package home-assistant-frontend/20180608.0b0/home-assistant-frontend-20180608.0b0.tar.gz

---
+++ packages/home-assistant-frontend/home-assistant-frontend-20180608.0b0.tar.gz/home-assistant-frontend-20180608.0b0/hass_frontend/service_worker.js
@@ -0,0 +1,5 @@
+importScripts("/frontend_latest/precache-manifest.9287bd749634555ef48ffbbf2f7010ca.js", "/frontend_latest/workbox-v3.2.0/workbox-sw.js");
+workbox.setConfig({modulePathPrefix: "/frontend_latest/workbox-v3.2.0"});
+/* global importScripts */
+importScripts('/static/service-worker-hass.js');
+
---
+++ packages/home-assistant-frontend/home-assistant-frontend-20180608.0b0.tar.gz/home-assistant-frontend-20180608.0b0/hass_frontend_es5/service_worker.js
@@ -0,0 +1,5 @@
+importScripts("/frontend_es5/precache-manifest.0036478405dd5531dec7c2288c2bd955.js", "/frontend_es5/workbox-v3.2.0/workbox-sw.js");
+workbox.setConfig({modulePathPrefix: "/frontend_es5/workbox-v3.2.0"});
+/* global importScripts */
+importScripts('/static/service-worker-hass.js');
+
