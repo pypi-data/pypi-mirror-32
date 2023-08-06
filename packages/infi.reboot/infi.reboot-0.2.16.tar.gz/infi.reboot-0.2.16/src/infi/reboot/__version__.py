c91cacb2bea952fe9f5099f88338a9ece442d159
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:55:52 +0000

    Add package infi.reboot/0.2.16/infi.reboot-0.2.16.tar.gz

---
+++ packages/infi.reboot/infi.reboot-0.2.16.tar.gz/infi.reboot-0.2.16/src/infi/reboot/__version__.py
@@ -0,0 +1,11 @@
+__version__ = "0.2.16"
+__git_commiter_name__ = "Arnon Yaari"
+__git_commiter_email__ = "arnony@infinidat.com"
+__git_branch__ = u'(HEAD detached at v0.2.16)'
+__git_remote_tracking_branch__ = '(No remote tracking)'
+__git_remote_url__ = '(Not remote tracking)'
+__git_head_hash__ = 'ef402cd52ed8fab7707fff44886f20728836ef7d'
+__git_head_subject__ = u'Finished Release v0.2.16'
+__git_head_message__ = u''
+__git_dirty_diff__ = u''
+__git_commit_date__ = '2018-06-10 12:32:40'
