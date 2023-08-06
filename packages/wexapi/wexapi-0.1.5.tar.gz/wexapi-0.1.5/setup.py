e592f83c1673f7865f381d0a308743eee2fc23e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:36:56 +0000

    Add package wexapi/0.1.5/wexapi-0.1.5.tar.gz

---
+++ packages/wexapi/wexapi-0.1.5.tar.gz/wexapi-0.1.5/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="wexapi",
+    version="0.1.5",
+    license='MIT',
+    author="Machin Dmytro",
+    author_email="machin.dmitry+pypi.org@gmail.com",
+    description="Wex.nz API Client",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/madmis/wexapi",
+    packages=setuptools.find_packages(),
+    classifiers=(
+        "Programming Language :: Python :: 3.6",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+)
