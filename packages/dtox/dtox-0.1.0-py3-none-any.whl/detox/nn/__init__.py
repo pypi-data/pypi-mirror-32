adbba7b19172b0b66bf71bd45734735e54a0f851
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:07:46 +0000

    Add package dtox/0.1.0/dtox-0.1.0-py3-none-any.whl

---
+++ packages/dtox/dtox-0.1.0-py3-none-any.whl/detox/nn/__init__.py
@@ -0,0 +1,13 @@
+from __future__ import absolute_import, division, print_function
+
+from detox.nn.autoencoder import Autoencoder, autoencoder
+from detox.nn.wgan import WGAN, wgan
+
+__all__ = [
+    # Classes
+    'Autoencoder',
+    'WGAN',
+    # Functionals
+    'autoencoder',
+    'wgan',
+]
