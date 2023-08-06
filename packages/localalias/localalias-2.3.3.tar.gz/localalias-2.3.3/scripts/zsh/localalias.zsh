7f0f2d0573265793f7aab67a3e13f0652563265c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:49:32 +0000

    Add package localalias/2.3.3/localalias-2.3.3.tar.gz

---
+++ packages/localalias/localalias-2.3.3.tar.gz/localalias-2.3.3/scripts/zsh/localalias.zsh
@@ -0,0 +1,72 @@
+#########################################
+#  localalias Shell Integration Script  #
+#########################################
+
+##### WHAT DOES THIS SCRIPT DO?
+# - Disables single letter aliases and builtins (these are desirable for use as local aliases)
+# - Source local and global aliases on startup.
+# - Source local aliases everytime the directory changes.
+# - Define two wrapper functions, `la` and `al`.
+
+##### INSTALLATION
+# In order to take advantage of the full benefits of localalias, this script must either be used as
+# an oh-my-zsh plugin or be sourced directly into your .bashrc / .zshrc file. See the official docs
+# for more information:
+# 
+# https://localalias.readthedocs.io/en/latest/installation.html#additional-installation-steps
+
+## Disable aliases
+for i in {a..z}; do
+    unalias "$i" &> /dev/null
+done
+
+for i in {0..9}; do
+    unalias "$i" &> /dev/null
+done
+
+arr=("la" "ls" "ll")
+for i in "${arr[@]}"; do
+    unalias "$i" &> /dev/null
+done
+
+## Disable builtins
+disable r
+
+## Function used for sourcing aliases
+_source_globals() { localalias --global | source /dev/stdin; }
+_source_locals() { localalias | source /dev/stdin; }
+
+_maybe_source_locals() {
+    if [[ -f $PWD/.localalias ]]; then
+        _source_locals
+    fi
+}
+
+## Source aliases on startup
+if [[ -f ~/.globalalias ]]; then
+    _source_globals
+fi
+
+_maybe_source_locals
+
+## Source local aliases everytime the directory is changed
+chpwd() { _maybe_source_locals; }
+
+## Wrapper used to interact with local aliases
+la() {
+    touch /tmp/localalias.timestamp
+    localalias --color "$@"
+    if [[ .localalias -nt /tmp/localalias.timestamp ]]; then
+        _source_locals
+    fi
+}
+
+## Wrapper used to interact with global aliases
+al() {
+    touch /tmp/localalias.timestamp
+    localalias --global --color "$@"
+    if [[ ~/.globalalias -nt /tmp/localalias.timestamp ]]; then
+        _source_globals
+        _maybe_source_locals
+    fi
+}
