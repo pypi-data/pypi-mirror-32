9182989c00bc7d7d9fc3b7a4e9acef6ab096cdb1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:12:40 +0000

    Add package flaskeztest/0.1.4/flaskeztest-0.1.4.tar.gz

---
+++ packages/flaskeztest/flaskeztest-0.1.4.tar.gz/flaskeztest-0.1.4/setup.py
@@ -0,0 +1,32 @@
+"""Setup flask-eztest."""
+
+from setuptools import setup
+
+requirements = [
+    'Flask',
+    'capybara-py',
+    'selenium',
+    'Flask-SQLAlchemy',
+    'termcolor'
+]
+
+description = "Tool to make integration testing flask app's using flask_sqlalchemy package easy to set up and organized"
+
+long_description = open('README.rst').read()
+
+setup(
+    name='flaskeztest',
+    version='0.1.4',
+    author='Stanley Rokita',
+    author_email='srok35@gmail.com',
+    url='https://github.com/Srokit/flask-eztest',
+    description=description,
+    long_description=long_description,
+    install_requires=requirements,
+    packages=['flaskeztest'],
+    entry_points={
+        'console_scripts': [
+            'eztest = flaskeztest.__main__:flaskeztest_main'
+        ]
+    }
+)
