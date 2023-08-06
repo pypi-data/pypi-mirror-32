9ef26a08cc51d26a42b19de929069bb605ad59cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:13:18 +0000

    Add package pybcoin/0.13/pybcoin-0.13.tar.gz

---
+++ packages/pybcoin/pybcoin-0.13.tar.gz/pybcoin-0.13/setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+setuptools.setup(
+    name='pybcoin',
+    version='0.13',
+    description='Bitcoin Trend Forecast',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author='Pushottam Shivraj',
+    author_email='pshivraj@uw.edu',
+    url='https://github.com/rguptauw/pybcoin',
+    keywords=['testing', 'Forecasting', 'Timeseries'],
+    packages=setuptools.find_packages(),
+    classifiers=("Programming Language :: Python :: 3",),
+)
