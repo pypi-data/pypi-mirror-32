b954d96f1c9a71cd91c153503681badf4f083698
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:34:47 +0000

    Add package i3-workspace-names/0.1.2/i3-workspace-names-0.1.2.tar.gz

---
+++ packages/i3-workspace-names/i3-workspace-names-0.1.2.tar.gz/i3-workspace-names-0.1.2/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+with open('README.md') as f:
+    long_description = f.read()
+
+setup(name='i3-workspace-names',
+      version='0.1.2',
+      description='Dynamically rename i3wm workspaces depending on windows',
+      long_description=long_description,
+      url='https://gitlab.com/flib99/i3-workspace-names',
+      author='Josh Walls',
+      author_email='me@joshwalls.co.uk',
+      license='GPL',
+      zip_safe=False,
+      install_requires=requirements,
+      scripts=['i3-workspace-names'])
