d65c3f53a61c302a08176c97726c25f18e5d24ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:14:02 +0000

    Add package http-server-base/1.3.0.2/http-server-base-1.3.0.2.tar.gz

---
+++ packages/http-server-base/http-server-base-1.3.0.2.tar.gz/http-server-base-1.3.0.2/src/http_server_base/tools/logging.py
@@ -0,0 +1,45 @@
+import logging, logging.config
+from logging import getLoggerClass, addLevelName, setLoggerClass, NOTSET
+import os
+import json
+
+def setup_logging \
+(
+    default_path='configs/logging.json',
+    default_level=logging.INFO,
+    env_key='LOG_CFG'
+):
+    """Setup logging configuration
+
+    """
+    path = default_path
+    value = os.getenv(env_key, None)
+    if value:
+        path = value
+    if os.path.exists(path):
+        with open(path, 'rt') as f:
+            config = json.load(f)
+        logging.config.dictConfig(config)
+    else:
+        logging.basicConfig(level=default_level)
+
+TRACE_LEVEL_NUM = 5
+DEVELOP_LEVEL_NUM = 60
+
+class ExtendedLogger(getLoggerClass()):
+    def __init__(self, name, level=NOTSET):
+        super().__init__(name, level)
+
+        addLevelName(TRACE_LEVEL_NUM, "TRACE")
+        addLevelName(DEVELOP_LEVEL_NUM, "DEVELOP")
+        
+    def trace(self, message, *args, **kws):
+        if (self.isEnabledFor(TRACE_LEVEL_NUM)):
+            self._log(TRACE_LEVEL_NUM, message, args, **kws)
+
+    def develop(self, message, *args, **kws):
+        if (self.isEnabledFor(DEVELOP_LEVEL_NUM)):
+            self._log(DEVELOP_LEVEL_NUM, message, args, **kws)
+
+
+setLoggerClass(ExtendedLogger)
