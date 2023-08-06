1d13ab0e604bc5b0a515f85667eedbb884c22121
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:18 +0000

    Add package kount-ris-sdk/3.0.2/kount_ris_sdk-3.0.2-py2.py3-none-any.whl

---
+++ packages/kount-ris-sdk/kount_ris_sdk-3.0.2-py2.py3-none-any.whl/kount/resources/correct_key_cryp.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# This file is part of the Kount python sdk project
+# https://github.com/Kount/kount-ris-python-sdk/)
+# Copyright (C) 2017 Kount Inc. All Rights Reserved.
+"correct configurationKey - sha-256"
+
+from kount.version import VERSION
+
+__author__ = "Kount SDK"
+__version__ = VERSION
+__maintainer__ = "Kount SDK"
+__email__ = "sdkadmin@kount.com"
+__status__ = "Development"
+
+
+correct_key_cryp = "ce1638f9cf6ce31b4ead12ca49712f1e538a055a6152ec83c7285e56ce917bd6"
