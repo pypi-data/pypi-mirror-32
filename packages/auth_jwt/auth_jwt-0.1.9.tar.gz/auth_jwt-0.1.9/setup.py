881dca2bf140b76722bb0ece97ecf591a4348e24
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:17:19 +0000

    Add package auth_jwt/0.1.9/auth_jwt-0.1.9.tar.gz

---
+++ packages/auth_jwt/auth_jwt-0.1.9.tar.gz/auth_jwt-0.1.9/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='auth_jwt',
+      version='0.1.9',
+      description='Module to give secure the api with jwt token',
+      url='https://github.com/handersonc/auth_jwt.git',
+      author='Handerson Contreras',
+      author_email='handerson.contreras@gmail.com',
+      license='MIT',
+      packages=['auth_jwt'],
+      install_requires=['PyJWT'],
+      zip_safe=False)
