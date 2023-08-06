b109c777f893b67328b9b812ab8bfef4240c8b4a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:13:06 +0000

    Add package download-openaq/0.0.5/download_openaq-0.0.5.tar.gz

---
+++ packages/download-openaq/download_openaq-0.0.5.tar.gz/download_openaq-0.0.5/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='download_openaq',
+      version='0.0.5',
+      description='Downloads OpenAQ CSV data from S3',
+      url='',
+      author='Development Seed',
+      author_email='aimee@developmentseed.org,rosariosicuranza@gmail.com',
+      license='MIT',
+      py_modules=['download_openaq'],
+      install_requires=['boto3', 'pandas'])
