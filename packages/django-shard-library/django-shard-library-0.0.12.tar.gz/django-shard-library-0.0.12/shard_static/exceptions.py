0569a70135c90f48b5a0e5a251e387c2ae9ce53f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:27 +0000

    Add package django-shard-library/0.0.12/django-shard-library-0.0.12.tar.gz

---
+++ packages/django-shard-library/django-shard-library-0.0.12.tar.gz/django-shard-library-0.0.12/shard_static/exceptions.py
@@ -0,0 +1,24 @@
+
+
+class NotShardStaticException(Exception):
+    pass
+
+
+class NotTransmitException(Exception):
+    pass
+
+
+class InvalidDatabaseAliasException(Exception):
+    pass
+
+
+class NotExistsOriginalDataException(Exception):
+    pass
+
+
+class DontExecuteException(Exception):
+    pass
+
+
+class DontLinkException(Exception):
+    pass
---
+++ packages/django-shard-library/django-shard-library-0.0.12.tar.gz/django-shard-library-0.0.12/shard_static/managers.py
@@ -0,0 +1,44 @@
+from datetime import datetime
+from typing import Optional
+
+from shard.managers import BaseShardManager
+from shard_static.exceptions import NotExistsOriginalDataException, DontExecuteException
+
+
+def _wrap_for_static(func_name):
+    def wrapped(self, *args, **kwargs):
+        if not self.model.transmit:
+            raise NotExistsOriginalDataException()
+
+        return getattr(self.get_queryset(), func_name)(*args, **kwargs)
+
+    wrapped.__name__ = func_name
+    return wrapped
+
+
+def _wrap_for_status(func_name):
+    def wrapped(self, *args, **kwargs):
+        raise DontExecuteException()
+
+    wrapped.__name__ = func_name
+    return wrapped
+
+
+class ShardStaticStatusManager(BaseShardManager):
+    filter = _wrap_for_status('filter')
+    get = _wrap_for_status('get')
+    create = _wrap_for_status('create')
+    get_or_create = _wrap_for_status('get_or_create')
+    update_or_create = _wrap_for_status('update_or_create')
+
+
+class ShardStaticManager(BaseShardManager):
+    filter = _wrap_for_static('filter')
+    get = _wrap_for_static('get')
+    create = _wrap_for_static('create')
+    get_or_create = _wrap_for_static('get_or_create')
+    update_or_create = _wrap_for_static('update_or_create')
+
+    def find_by_last_modified(self, last_modified: Optional[datetime], offset: int, limit: int):
+        qs = self.filter(last_modified__gte=last_modified).order_by('last_modified')
+        return qs[offset:offset + limit]
