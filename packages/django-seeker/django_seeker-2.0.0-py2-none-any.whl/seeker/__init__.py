eb9b2162cad9b4a68a65ac281243ca1ff2b13577
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:52:30 +0000

    Add package django-seeker/2.0.0/django_seeker-2.0.0-py2-none-any.whl

---
+++ packages/django-seeker/django_seeker-2.0.0-py2-none-any.whl/seeker/__init__.py
@@ -0,0 +1,9 @@
+__version__ = '2.0.0'
+
+from .mapping import Indexable, ModelIndex, document_from_model, build_mapping, document_field, deep_field_factory, RawString, RawMultiString, DEFAULT_ANALYZER
+from .facets import Facet, TermsFacet, GlobalTermsFacet, YearHistogram, RangeFilter
+from .views import SeekerView, Column
+from .utils import search, index
+from .registry import register, documents, model_documents, app_documents
+
+default_app_config = 'seeker.apps.SeekerConfig'
