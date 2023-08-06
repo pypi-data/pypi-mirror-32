782efdffc5366acd45f05d83f0712f0f34c8cc9a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:57:08 +0000

    Add package machinerry/0.2.4/machinerry-0.2.4.tar.gz

---
+++ packages/machinerry/machinerry-0.2.4.tar.gz/machinerry-0.2.4/setup.py
@@ -0,0 +1,92 @@
+#!/usr/bin/env python
+
+#
+# Extract the project identity from the README.
+#
+import io
+import setuptools
+
+with io.open('README.rst', encoding='utf-8') as readme:
+    readme = readme.read()
+
+
+def get_definition(prefix):
+    for line in readme.split('\n'):
+        if line.startswith(prefix):
+            return line[len(prefix):].strip()
+    err = 'no line in README.rst with prefix {!r}'.format(prefix)
+    raise AssertionError(err)
+
+
+def get_description():
+    d_start = '|summary|\n'
+    i_start = readme.index(d_start) + len(d_start)
+    return readme[i_start:].strip()
+
+
+name = get_definition('.. |name| replace:: ')
+
+#
+# End extraction code.
+#
+
+params = dict(
+    name=name,
+    use_scm_version=True,
+    author="Allan Crooks",
+    author_email="allan@increment.one",
+    description=get_definition('.. |summary| replace:: '),
+    long_description=get_description(),
+    license='MIT',
+    url=get_definition('.. _repository: '),
+    keywords=[],
+    py_modules=['machinerry'],
+    include_package_data=True,
+    namespace_packages=name.split('.')[:-1],
+    python_requires='>=2.7',
+    install_requires=[
+        'CherryPy',
+        'requests',
+        'six',
+    ],
+    extras_require={
+        'testing': [
+            'pytest>=2.8',
+            'pytest-sugar>=0.9.1',
+            'collective.checkdocs',
+        ],
+        'docs': [
+            'sphinx',
+            'jaraco.packaging>=3.2',
+            'rst.linker>=1.9',
+            'allanc-sphinx[yeen]>=0.2',
+        ],
+    },
+    setup_requires=[
+        'setuptools_scm>=1.15.0',
+    ],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        # "Development Status :: 5 - Production/Stable",
+        "Environment :: Web Environment",
+        "Framework :: CherryPy",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Topic :: Software Development :: Libraries",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
+        "Topic :: Internet :: WWW/HTTP :: HTTP Servers",
+        "Topic :: Internet :: WWW/HTTP :: WSGI",
+        "Topic :: Internet :: WWW/HTTP :: WSGI :: Application",
+        "Topic :: Internet :: WWW/HTTP :: WSGI :: Server",
+    ],
+    entry_points={
+    },
+)
+
+if __name__ == '__main__':
+    setuptools.setup(**params)
