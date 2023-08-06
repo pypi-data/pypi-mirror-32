353e010132b55cb30fa41e3843bf9fd25134eb3b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:50:20 +0000

    Add package eprocess/0.4/eprocess-0.4.tar.gz

---
+++ packages/eprocess/eprocess-0.4.tar.gz/eprocess-0.4/eprocess/decorators.py
@@ -0,0 +1,11 @@
+import time
+
+
+def profile(task_function):
+    def wrapper(*args, **kwargs):
+        starts = time.time()
+        task_function(*args, **kwargs)
+        ends = time.time()
+        print("--- {} seconds ---".format((ends - starts)))
+
+    return wrapper
