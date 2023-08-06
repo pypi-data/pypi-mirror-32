6bc586a4b56d230ca83ea51d711c965e274a7100
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:55:43 +0000

    Add package infi.diskmanagement/0.3.14/infi.diskmanagement-0.3.14-py2-none-any.whl

---
+++ packages/infi.diskmanagement/infi.diskmanagement-0.3.14-py2-none-any.whl/infi.diskmanagement-0.3.14.dist-info/DESCRIPTION.rst
@@ -0,0 +1,3 @@
+This module gives the same functionality as diskpart. But unlike diskpart, it does not use VDS, it uses SetupAPI and direct IOCTLs to the disks, volumes, and the mount and partitions managers
+
+
---
+++ packages/infi.diskmanagement/infi.diskmanagement-0.3.14-py2-none-any.whl/infi/diskmanagement/__version__.py
@@ -0,0 +1,11 @@
+__version__ = "0.3.14"
+__git_commiter_name__ = "Arnon Yaari"
+__git_commiter_email__ = "arnony@infinidat.com"
+__git_branch__ = u'(HEAD detached at v0.3.14)'
+__git_remote_tracking_branch__ = '(No remote tracking)'
+__git_remote_url__ = '(Not remote tracking)'
+__git_head_hash__ = '86e35b9f53d150dfbba0e3c0f4aa593369938908'
+__git_head_subject__ = u'Finished Release v0.3.14'
+__git_head_message__ = u''
+__git_dirty_diff__ = u''
+__git_commit_date__ = '2018-06-10 11:46:49'
