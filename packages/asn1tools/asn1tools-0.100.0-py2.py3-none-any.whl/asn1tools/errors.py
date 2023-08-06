c7561fc26730e812dcd9e3494ad19738f0d72798
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:18:13 +0000

    Add package asn1tools/0.100.0/asn1tools-0.100.0-py2.py3-none-any.whl

---
+++ packages/asn1tools/asn1tools-0.100.0-py2.py3-none-any.whl/asn1tools/errors.py
@@ -0,0 +1,33 @@
+"""Exceptions.
+
+"""
+
+
+class Error(Exception):
+    """Base exception of all asn1tools exceptions.
+
+    """
+
+
+class CompileError(Error):
+    """General ASN.1 compile error.
+
+    """
+
+
+class EncodeError(Error):
+    """General ASN.1 encode error.
+
+    """
+
+
+class DecodeError(Error):
+    """General ASN.1 decode error.
+
+    """
+
+
+class ConstraintsError(Error):
+    """General ASN.1 constraints.
+
+    """
