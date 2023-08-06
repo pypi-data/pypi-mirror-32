38835009db29bb780021e78887a1411b334a1516
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:34:57 +0000

    Add package twinthread/0.4.0/twinthread-0.4.0-py3-none-any.whl

---
+++ packages/twinthread/twinthread-0.4.0-py3-none-any.whl/twinthread/__init__.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for twinthread."""
+
+__author__ = """Brent Baumgartner"""
+__email__ = 'brent.baumgartner@twinthread.com'
+__version__ = '0.4.0'
+
+def fetch(token):
+    """
+    Fetch data from TwinThread's servers
+    :param token: Authenticates access to one group of datasets. Fetch from TwinThread UI.
+    :return: data: Pandas dataframe
+    """
+    print("Fetching data...")
+    from azure.storage.common import CloudStorageAccount
+    import pandas as pd
+    import os
+    X = token.split(",")
+    X[3] = X[3] + "=="
+    (blob, container, name, key) = X
+    account = CloudStorageAccount(account_name=name, account_key=key)
+    service = account.create_block_blob_service()
+    service.get_blob_to_path(container, blob, "tempfile")
+    print("Parsing data...")
+    data = pd.read_csv("tempfile")
+    os.remove("tempfile")
+    from IPython.display import clear_output
+    clear_output()
+    return data
