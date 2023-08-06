f1cbfcabe5c2f3c008efd8657d60aaea99da7ed6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:11:40 +0000

    Add package bdtdecimaltowordconverter/2.0.2/bdtdecimaltowordconverter-2.0.2.tar.gz

---
+++ packages/bdtdecimaltowordconverter/bdtdecimaltowordconverter-2.0.2.tar.gz/bdtdecimaltowordconverter-2.0.2/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="bdtdecimaltowordconverter",
+    version="2.0.2",
+    author="Mahfuz Ahmed",
+    author_email="mahfuzcmt@gmail.com",
+    description="bdtdecimaltowordconverter is created for converting decimal to word in both bangla and english in BDT format",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/mahfuzcmt/BanglaDecimalToWordConverter",
+    packages=setuptools.find_packages(),
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+)
