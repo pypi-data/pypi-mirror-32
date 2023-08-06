025ffb110e7dc0183e2281ee648046e5d022af5a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:34 +0000

    Add package nist-asd/1.3/nist-asd-1.3.tar.gz

---
+++ packages/nist-asd/nist-asd-1.3.tar.gz/nist-asd-1.3/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+setup(name='nist-asd',
+      version='1.3',
+      description='Basically a class which parses the NIST Atomic Spectra Database and saves the data to a dictionary '
+                  'on HDD. You can pass an matplotlib.axis, and the emissions lines will be plotted with an optional '
+                  'normalization factor.',
+      url='https://github.com/SirJohnFranklin/nist-asd',
+      author='SirJohnFranklin',
+      author_email='sirjfu@gmail.com',
+      license='MIT',
+      packages=['nist_asd'],
+      zip_safe=False,
+      classifiers=(
+          "Programming Language :: Python :: 3",
+          "Programming Language :: Python :: 2",
+          "License :: OSI Approved :: MIT License",
+          "Operating System :: OS Independent",
+      ),
+      install_requires=[
+        'HTMLParser',
+        'pprint',
+        'logzero',
+        'mpldatacursor',
+        'pathlib'
+       ],
+)
