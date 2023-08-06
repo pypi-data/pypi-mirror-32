52bd6c5769dcf904585bc9ca732a3b7196cfdbaf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:46 +0000

    Add package pyod/0.4.1/pyod-0.4.1.tar.gz

---
+++ packages/pyod/pyod-0.4.1.tar.gz/pyod-0.4.1/pyod/models/feat_bagging.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+# %%
+
+from __future__ import division
+from __future__ import print_function
+
+from sklearn.ensemble import BaggingClassifier
+
+from sklearn.utils.estimator_checks import check_estimator
+from pyod.models.knn import KNN
+from pyod.utils.load_data import generate_data
+from sklearn.base import BaseEstimator
+
+from .base import BaseDetector
+
+
+# TODO: place holder only
+class FeatureBagging(BaseDetector):
+    def __init__(self, base_estimator, n_estimators=10, contamination=0.1,
+                 min_features=0.5):
+        super(FeatureBagging, self).__init__(contamination=contamination)
+        self.base_estimator_ = base_estimator
+        self.n_estimators_ = n_estimators
+        self.min_features_ = min_features
+
+    def fit(self, X, y=None):
+        pass
+
+    def decision_function(self, X):
+        pass
