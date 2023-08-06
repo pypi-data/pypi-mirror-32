07c6df64fa6dc9fe12319b9dfcd71a57ee88e684
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:28:01 +0000

    Add package poppy/0.7.0/poppy-0.7.0.tar.gz

---
+++ packages/poppy/poppy-0.7.0.tar.gz/poppy-0.7.0/poppy/__init__.py
@@ -0,0 +1,127 @@
+# Licensed under a 3-clause BSD style license - see LICENSE.rst
+
+"""Physical Optics Propagation in PYthon (POPPY)
+
+POPPY is a Python package that simulates physical optical propagation including diffraction.
+It implements a flexible framework for modeling Fraunhofer (far-field) diffraction
+and point spread function formation, particularly in the context of astronomical telescopes.
+POPPY was developed as part of a simulation package for JWST, but is more broadly applicable to many kinds of
+imaging simulations.
+
+Developed by Marshall Perrin and colleagues at STScI, 2010-2017, for use simulating the James Webb Space Telescope.
+
+Documentation can be found online at https://poppy-optics.readthedocs.io/
+"""
+
+# Affiliated packages may add whatever they like to this file, but
+# should keep this content at the top.
+# ----------------------------------------------------------------------------
+# make use of astropy affiliate framework to set __version__, __githash__, and
+# add the test() helper function
+from ._astropy_init import *
+# ----------------------------------------------------------------------------
+
+
+import astropy as _astropy
+
+if _astropy.version.major + _astropy.version.minor * 0.1 < 0.4:  # pragma: no cover
+    raise ImportError("astropy >= 0.4 is required for this version of poppy.")
+
+from astropy import config as _config
+
+
+class Conf(_config.ConfigNamespace):
+    """
+    Configuration parameters for `poppy`.
+    """
+
+    use_multiprocessing = _config.ConfigItem(False,
+                                             'Should PSF calculations run in parallel using multiple processors'
+                                             'using the Python multiprocessing framework (if True; faster but '
+                                             'does not allow display of each wavelength) or run serially in a '
+                                             'single process (if False; slower but shows the calculation in '
+                                             'progress. Also a bit more robust.)')
+
+    # Caution: Do not make this next too large on high-CPU-count machines
+    # because this is a memory-intensive calculation and you will
+    # just end up thrashing IO and swapping out a ton, so everything
+    # becomes super slow.
+    n_processes = _config.ConfigItem(4, 'Maximum number of additional ' +
+                                     'worker processes to spawn, if multiprocessing is enabled. ' +
+                                     'Set to 0 for autoselect. Note, PSF calculations are likely RAM ' +
+                                     'limited more than CPU limited for higher N on modern machines.')
+
+    use_fftw = _config.ConfigItem(False, 'Use FFTW for FFTs (assuming it' +
+                                  'is available)?  Set to False to force numpy.fft always, True to' +
+                                  'try importing and using FFTW via PyFFTW.')
+    autosave_fftw_wisdom = _config.ConfigItem(True, 'Should POPPY ' +
+                                              'automatically save and reload FFTW ' +
+                                              '"wisdom" for improved speed?')
+
+    use_cuda = _config.ConfigItem(False, 'Use cuda for FFTs on GPU (assuming it' +
+            'is available)?')
+    use_opencl = _config.ConfigItem(False, 'Use OpenCL for FFTs on GPU (assuming it' +
+            'is available)?')
+    use_numexpr = _config.ConfigItem(True, 'Use NumExpr to accelarate array math (assuming it' +
+            'is available)?')
+
+    double_precision = _config.ConfigItem(True, 'Floating point values use float64 and complex128 if True,' +
+            'otherwise float32 and complex64.')
+
+    default_image_display_fov = _config.ConfigItem(5.0, 'Default image' +
+                                                   'display field of view, in arcseconds. Adjust this to display ' +
+                                                   'only a subregion of a larger output array.')
+
+    default_logging_level = _config.ConfigItem('INFO', 'Logging ' +
+                                               'verbosity: one of {DEBUG, INFO, WARN, ERROR, or CRITICAL}')
+
+    enable_speed_tests = _config.ConfigItem(False, 'Enable additional ' +
+                                            'verbose printout of computation times. Useful for benchmarking.')
+    enable_flux_tests = _config.ConfigItem(False, 'Enable additional ' +
+                                           'verbose printout of fluxes and flux conservation during ' +
+                                           'calculations. Useful for testing.')
+    cmap_sequential = _config.ConfigItem(
+        'gist_heat',
+        'Select a default colormap to represent sequential data (e.g. intensity)'
+    )
+    cmap_diverging = _config.ConfigItem(
+        'RdBu_r',
+        'Select a default colormap to represent diverging data (e.g. OPD)'
+    )
+    cmap_pupil_intensity = _config.ConfigItem(
+        'gray',
+        'Select a default colormap to represent intensity at pupils or aperture masks'
+    )
+
+
+conf = Conf()
+
+from . import poppy_core
+from . import utils
+from . import optics
+from . import misc
+from . import fresnel
+from . import physical_wavefront
+from . import wfe
+from . import dms
+
+from .poppy_core import *
+from .utils import *
+from .optics import *
+from .wfe import *
+from .fresnel import *
+from .physical_wavefront import *
+from .special_prop import *
+
+from .instrument import Instrument
+
+# if we might have autosaved, then auto reload as well
+if conf.use_fftw and conf.autosave_fftw_wisdom:
+    try:
+        import pyfftw
+
+        utils.fftw_load_wisdom()
+    except ImportError:
+        pyfftw = None
+
+__all__ = ['conf', 'Instrument'] + utils.__all__ + poppy_core.__all__ + optics.__all__ + fresnel.__all__ + wfe.__all__
---
+++ packages/poppy/poppy-0.7.0.tar.gz/poppy-0.7.0/poppy/tests/test_zernike.py
@@ -0,0 +1,271 @@
+import numpy as np
+from poppy import poppy_core
+from poppy import optics
+from poppy import zernike
+
+
+def test_zernikes_rms(nterms=10, size=500):
+    """Verify RMS(Zernike[n,m]) == 1."""
+    assert np.nanstd(zernike.zernike1(1)) == 0.0, "Zernike(j=0) has nonzero RMS"
+    for j in range(2, nterms):
+        n, m = zernike.noll_indices(j)
+        z = zernike.zernike(n, m, npix=size)
+        rms = np.nanstd(z)  # exclude masked pixels
+        assert abs(1.0 - rms) < 0.001, "Zernike(j={}) has RMS value of {}".format(j, rms)
+
+
+def test_ones_zernikes(nterms=10):
+    """Verify the radial scaling function is correctly normalized"""
+    rho = np.ones(3)
+    for j in np.arange(nterms) + 1:
+        n, m = zernike.noll_indices(j)
+        rs = zernike.R(n, m, rho)
+        print("j=%d\tZ_(%d,%d) [1] = \t %s" % (j, n, m, str(rs)))
+        assert rs[0] == rs[1] == rs[2], "Radial polynomial is not radially symmetric"
+
+
+def test_cached_zernike1(nterms=10):
+    radius = 1.1
+
+    osys = poppy_core.OpticalSystem()
+    osys.addPupil(optics.CircularAperture(radius=radius))
+    wave = osys.inputWavefront()
+
+    y, x = wave.coordinates()
+    rho = np.sqrt(y ** 2 + x ** 2) / radius
+    theta = np.arctan2(y, x)
+
+    cached_results = []
+
+    for j in range(1, nterms + 1):
+        cached_output = zernike.cached_zernike1(j, wave.shape, wave.pixelscale, radius, outside=0.0)
+        cached_results.append(cached_output)
+        uncached_output = zernike.zernike1(j, rho=rho, theta=theta, outside=0.0)
+        assert np.allclose(cached_output, uncached_output)
+
+    try:
+        cached_output[0, 0] = np.nan
+        assert False, "Shouldn't be able to assign to a cached output array!"
+    except ValueError:
+        pass
+
+    # Check that we're getting cached copies
+    for j, array_ref in enumerate(cached_results, start=1):
+        cached_array_ref = zernike.cached_zernike1(j, wave.shape, wave.pixelscale, radius, outside=0.0)
+        assert id(array_ref) == id(cached_array_ref), "cached_zernike1 returned a new object for the same arguments"
+
+
+def _test_cross_zernikes(testj=4, nterms=10, npix=500):
+    """Verify the functions are orthogonal, by taking the
+    integrals of a given Zernike times N other ones.
+
+    Parameters :
+    --------------
+    testj : int
+        Index of the Zernike polynomial to test against the others
+    nterms : int
+        Test that polynomial against those from 1 to this N
+    npix : int
+        Size of array to use for this test
+    """
+
+    zj = zernike.zernike1(testj, npix=npix)
+    assert np.sum(np.isfinite(zj)) > 0, "Zernike calculation failure; all NaNs."
+    zbasis = zernike.zernike_basis(nterms=nterms, npix=npix)
+    for idx, z in enumerate(zbasis):
+        j = idx + 1
+        if j == testj or j == 1:
+            continue  # discard piston term and self
+        prod = z * zj
+        wg = np.where(np.isfinite(prod))
+        cross_sum = np.abs(prod[wg].sum())
+        assert cross_sum < 1e-9, (
+            "orthogonality failure, Sum[Zernike(j={}) * Zernike(j={})] = {} (> 1e-9)".format(
+                j, testj, cross_sum)
+        )
+
+
+def test_cross_zernikes():
+    """Verify orthogonality for a subset of Zernikes by taking the integral of
+    that Zernike times N other ones.
+
+    Note that the Zernikes are only strictly orthonormal over a
+    fully circular aperture evauated analytically. For any discrete
+    aperture the orthonormality is only approximate.
+    """
+    for testj in (2, 3, 4, 5, 6):
+        _test_cross_zernikes(testj=testj, nterms=6)
+
+
+def _test_cross_hexikes(testj=4, nterms=10, npix=500):
+    """Verify the functions are orthogonal, by taking the
+    integrals of a given Hexike times N other ones.
+
+    This is a helper function for test_cross_hexike.
+
+    Parameters :
+    --------------
+    testj : int
+        Index of the Zernike polynomial to test against the others
+    nterms : int
+        Test that polynomial against those from 1 to this N
+    npix : int
+        Size of array to use for this test
+    """
+
+    hexike_basis = zernike.hexike_basis(nterms=nterms, npix=npix)
+    test_hexike = hexike_basis[testj - 1]
+    assert np.sum(np.isfinite(test_hexike)) > 0, "Hexike calculation failure; all NaNs."
+    for idx, hexike_array in enumerate(hexike_basis):
+        j = idx + 1
+        if j == testj or j == 1:
+            continue  # discard piston term and self
+        prod = hexike_array * test_hexike
+        wg = np.where(np.isfinite(prod))
+        cross_sum = np.abs(prod[wg].sum())
+
+        # Threshold was originally 1e-9, but we ended up getting 1.19e-9 on some machines (not always)
+        # this seems acceptable, so relaxing criteria slightly
+        assert cross_sum < 2e-9, (
+            "orthogonality failure, Sum[Hexike(j={}) * Hexike(j={})] = {} (> 2e-9)".format(
+                j, testj, cross_sum)
+        )
+
+
+def test_cross_hexikes():
+    """Verify orthogonality for a subset of Hexikes by taking the integral of
+    that Hexike times N other ones.
+
+    Note that the Hexike are only strictly orthonormal over a
+    fully hexagonal aperture evauated analytically. For any discrete
+    aperture the orthonormality is only approximate.
+    """
+    for testj in (2, 3, 4, 5, 6):
+        _test_cross_hexikes(testj=testj, nterms=6)
+
+
+def test_arbitrary_basis_rms(nterms=10, size=500):
+    """Verify RMS(Zernike[n,m]) == 1."""
+    # choose a square aperture for the test
+    square_aperture = optics.SquareAperture(size=1.).sample(npix=size,grid_size=1.1)
+    square_basis = zernike.arbitrary_basis(square_aperture,nterms=nterms)
+
+    assert np.nanstd(square_basis[0]) == 0.0, "Mode(j=0) has nonzero RMS"
+    for j in range(1, nterms):
+        rms = np.nanstd(square_basis[j])  # exclude masked pixels
+        assert abs(1.0 - rms) < 0.001, "Mode(j={}) has RMS value of {}".format(j, rms)
+
+
+def _test_cross_arbitrary_basis(testj=4, nterms=10, npix=500):
+    """Verify the functions are orthogonal, by taking the
+    integrals of a given mode times N other ones.
+
+    This is a helper function for test_cross_arbitrary_basis.
+
+    Parameters :
+    --------------
+    testj : int
+        Index of the Zernike-like polynomial to test against the others
+    nterms : int
+        Test that polynomial against those from 1 to this N
+    npix : int
+        Size of array to use for this test
+    """
+    # choose a square aperture for the test
+    square_aperture = optics.SquareAperture(size=1.).sample(npix=npix,grid_size=1.1)
+    square_basis = zernike.arbitrary_basis(square_aperture,nterms=nterms)
+
+    test_mode = square_basis[testj - 1]
+    assert np.sum(np.isfinite(test_mode)) > 0, "Basis function calculation failure; all NaNs."
+    for idx, array in enumerate(square_basis):
+        j = idx + 1
+        if j == testj or j == 1:
+            continue  # discard piston term and self
+        prod = array * test_mode
+        wg = np.where(np.isfinite(prod))
+        cross_sum = np.abs(prod[wg].sum())
+
+        # Threshold was originally 1e-9, but we ended up getting 1.19e-9 on some machines (not always)
+        # this seems acceptable, so relaxing criteria slightly
+        assert cross_sum < 2e-9, (
+            "orthogonality failure, Sum[Mode(j={}) * Mode(j={})] = {} (> 2e-9)".format(
+                j, testj, cross_sum)
+        )
+
+
+def test_cross_arbitrary_basis():
+    """Verify orthogonality for a subset of basis functions by taking the integral of
+    each function times N other ones.
+
+    Note that the Hexike are only strictly orthonormal over a
+    fully hexagonal aperture evauated analytically. For any discrete
+    aperture the orthonormality is only approximate.
+    """
+    for testj in (2, 3, 4, 5, 6):
+        _test_cross_arbitrary_basis(testj=testj, nterms=6)
+
+
+def test_opd_expand(npix=512, input_coefficients=(0.1, 0.2, 0.3, 0.4, 0.5)):
+    basis = zernike.zernike_basis(nterms=len(input_coefficients), npix=npix)
+    for idx, coeff in enumerate(input_coefficients):
+        basis[idx] *= coeff
+
+    opd = basis.sum(axis=0)
+    recovered_coeffs = zernike.opd_expand(opd, nterms=len(input_coefficients))
+    max_diff = np.max(np.abs(np.asarray(input_coefficients) - np.asarray(recovered_coeffs)))
+    assert max_diff < 1e-3, "recovered coefficients from wf_expand more than 0.1% off"
+
+
+    # Test the nonorthonormal version too
+    # At a minimum, fitting with this variant version shouldn't be
+    # worse than the regular one on a clear circular aperture.
+    # We do the test in this same function for efficiency
+
+
+    recovered_coeffs_v2 = zernike.opd_expand_nonorthonormal(opd, nterms=len(input_coefficients))
+    max_diff_v2 = np.max(np.abs(np.asarray(input_coefficients) - np.asarray(recovered_coeffs_v2)))
+    assert max_diff_v2 < 1e-3, "recovered coefficients from wf_expand more than 0.1% off"
+
+
+def test_opd_from_zernikes():
+    coeffs = [0,0.1, 0.4, 2, -0.3]
+    opd = zernike.opd_from_zernikes(coeffs, npix=256)
+
+    outcoeffs = zernike.opd_expand(opd, nterms=len(coeffs))
+
+    # only compare on indices 1-3 to avoid divide by zero on piston
+    diffs = np.abs(np.asarray(coeffs[1:5]) - np.asarray(outcoeffs[1:5]))/np.asarray(coeffs[1:5])
+    max_diff = np.max(diffs )
+    assert max_diff < 2e-3, "recovered coefficients from opd_expand differ more than expected"
+
+
+def test_hex_aperture():
+    """ Ensure the hex aperture used for Zernikes is consistent with the regular
+    poppy HexagonAperture
+    """
+
+    npix_to_try = [10, 11, 12, 13, 100, 101, 512, 513]
+
+    for npix in npix_to_try:
+        assert np.all(optics.HexagonAperture(side=1).sample(npix=npix) - zernike.hex_aperture(
+            npix=npix) == 0), "hex_aperture and HexagonAperture outputs differ for npix={}".format(npix)
+
+
+def test_zern_name():
+    assert zernike.zern_name(3)=='Tilt Y', "Unexpected return value"
+    assert zernike.zern_name(11)=='Spherical', "Unexpected return value"
+    assert zernike.zern_name(20)=='Pentafoil X', "Unexpected return value"
+    assert zernike.zern_name(352)=='Z352', "Unexpected return value"
+
+
+def test_str_zernike():
+    assert zernike.str_zernike(4,0) == 'sqrt(5)* ( 6 r^4  -6 r^2  +1 r^0  ) ', "Unexpected return value"
+    assert zernike.str_zernike(5,5) == '\\sqrt{12}* ( 1 r^5  ) * \\cos(5 \\theta)', "Unexpected return value"
+
+
+def test_zernike_basis_faster():
+    bf = zernike.zernike_basis_faster(12, outside=0)
+    bs = zernike.zernike_basis(12, outside=0)
+    assert np.allclose(bf,bs), "Fast zernike basis calculation doesn't match the slow calculation"
+
+
