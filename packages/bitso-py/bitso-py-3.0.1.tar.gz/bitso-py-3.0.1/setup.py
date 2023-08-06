372860e69810ec04ceb1bc43849a0d67a630e4de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:22:31 +0000

    Add package bitso-py/3.0.1/bitso-py-3.0.1.tar.gz

---
+++ packages/bitso-py/bitso-py-3.0.1.tar.gz/bitso-py-3.0.1/setup.py
@@ -0,0 +1,26 @@
+import os
+from setuptools import setup
+
+if os.path.exists('README.md'):
+    long_description = open('README.md').read()
+else:
+    long_description = 'A python wrapper for the Bitso API.'
+
+setup(
+    name='bitso-py',
+    version='3.0.1',
+    author='Mario Romero',
+    author_email='mario@bitso.com',
+    packages=['bitso', 'tests'],
+    url='https://github.com/bitsoex/bitso-py',
+    license='LICENSE.txt',
+    description='A python wrapper for the Bitso API.',
+    long_description=long_description,
+    install_requires=[
+        "requests >= 2.2.1",
+        "websocket-client == 0.40.0",
+        "python-dateutil >= 1.5",
+        "mock >= 2.0.0",
+        "future >= 0.16.0"
+    ],
+)
