fd09d993bca7c8d8eac4bf3817c3b2c8d0defaff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:17:19 +0000

    Add package pylendingclub/v1.2/pylendingclub-v1.2.tar.gz

---
+++ packages/pylendingclub/pylendingclub-v1.2.tar.gz/pylendingclub-v1.2/pylendingclub/session.py
@@ -0,0 +1,23 @@
+import os
+
+from .base import Base
+from .account import Account
+from .loan import Loan
+from .config.config import LC_API_VERSION
+
+class LendingClubSession(Base):
+    """
+    Serves as the primary wrapper for the LendingClub API.
+
+    The API resources are available as public properties.
+    """
+
+    def __init__(self, api_key, investor_id):
+        self._url = 'https://api.lendingclub.com/api/investor/{}/'.format(LC_API_VERSION)
+        self._headers = {'Authorization' : api_key}
+        self.account = Account(self.join_url(self._url, 'accounts', True), self._headers, investor_id)
+        self.loan = Loan(self.join_url(self._url, 'loans', True), self._headers)
+
+    @classmethod
+    def from_environment_variables(cls):
+        return cls(os.environ['LC_API_KEY'], os.environ['LC_INVESTOR_ID'])
