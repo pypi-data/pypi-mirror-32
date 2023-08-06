280c466916a9d1c2c30677d9a442fb8b684a39ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:27:03 +0000

    Add package django_matialvarezs_grafana_customers/0.1.122/django_matialvarezs_grafana_customers-0.1.122.tar.gz

---
+++ packages/django_matialvarezs_grafana_customers/django_matialvarezs_grafana_customers-0.1.122.tar.gz/django_matialvarezs_grafana_customers-0.1.122/matialvarezs_grafana_customers/manage_rows_dashboard.py
@@ -0,0 +1,26 @@
+from . import base_row_dashboard
+from . import models
+from ohm2_handlers_light import utils as ohm2_handlers_light_utils
+import json
+
+
+def create_row_dashboard(title, dashboard_uid, object_project_id, object_project_panel_list):
+    dashboard = models.DashboardGrafana.objects.get(dashboard_uid=dashboard_uid)
+    random_row_id = ohm2_handlers_light_utils.get_random_string_number(3)
+    json_row = base_row_dashboard.get_base_row(title, random_row_id)
+
+    panles_row = list()
+
+    row = models.RowsDashboardGrafana(dashboard=dashboard, object_project_id=object_project_id, title=title,
+                                      row_content=json_row)
+    row.save()
+    json_row['panels'] = []
+
+    for obj in object_project_panel_list:
+        grafana_panel = models.PanelGrafana.objects.get(object_project_id=obj.id)
+        json_row['panels'].append(grafana_panel.panel_content)
+        panles_row.append(grafana_panel.panel_content)
+        row.panels.add(grafana_panel.id)
+    # json_row['panels'] = panles_row
+    row.row_content = json_row
+    row.save()
