b4149b38dc6f8926c789a5c6bbe81ebfad76ab62
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:11 +0000

    Add package apistar-websocket/0.1.2/apistar-websocket-0.1.2.tar.gz

---
+++ packages/apistar-websocket/apistar-websocket-0.1.2.tar.gz/apistar-websocket-0.1.2/apistar_websocket/__init__.py
@@ -0,0 +1,8 @@
+from apistar_websocket.websocket import (WebSocket, WebSocketAutoHook,
+                                         WebSocketComponent)
+
+__all__ = [
+    'WebSocketComponent', 'WebSocketAutoHook', 'WebSocket'
+]
+
+__version__ = '0.1.2'
