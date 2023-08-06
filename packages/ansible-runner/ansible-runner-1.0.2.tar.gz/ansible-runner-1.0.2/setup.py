d5a2be9be36c6e0c26a452854aaf29772c52b17f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:25:35 +0000

    Add package ansible-runner/1.0.2/ansible-runner-1.0.2.tar.gz

---
+++ packages/ansible-runner/ansible-runner-1.0.2.tar.gz/ansible-runner-1.0.2/setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+# Copyright (c) 2018 Red Hat, Inc.
+# All Rights Reserved.
+
+from setuptools import setup, find_packages
+
+setup(
+    name="ansible-runner",
+    version="1.0.2",
+    author='Red Hat Ansible',
+    url="https://github.com/ansible/ansible-runner",
+    packages=find_packages(),
+    install_requires=[
+        'psutil',
+        'pexpect==4.5.0',
+        'python-daemon',
+        'PyYAML',
+        'six',
+    ],
+    zip_safe=False,
+    entry_points={
+        'console_scripts': [
+            'ansible-runner = ansible_runner.__main__:main'
+        ]
+    }
+)
