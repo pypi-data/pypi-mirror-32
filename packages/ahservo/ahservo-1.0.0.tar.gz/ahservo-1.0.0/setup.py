8aa3d0e74caac7f21391e179b60208e8e7df933b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:01:29 +0000

    Add package ahservo/1.0.0/ahservo-1.0.0.tar.gz

---
+++ packages/ahservo/ahservo-1.0.0.tar.gz/ahservo-1.0.0/setup.py
@@ -0,0 +1,13 @@
+import setuptools
+
+setuptools.setup(
+        name = 'ahservo',
+        version = '1.0.0',
+        packages = setuptools.find_packages(),
+        author = 'sashankMNS',
+        license = 'MIT',
+        install_requires = ['Adafruit_PCA9685',],
+        description = 'Control servo with an angle between 0 and 180 degrees',
+)
+        
+
