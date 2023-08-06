e570c24513d689aa6726cd0df7954071178e0b0e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:04 +0000

    Add package wofrysrw/1.0.9/wofrysrw-1.0.9.tar.gz

---
+++ packages/wofrysrw/wofrysrw-1.0.9.tar.gz/wofrysrw-1.0.9/wofrysrw/version.py
@@ -0,0 +1,11 @@
+
+# THIS FILE IS GENERATED FROM wofrysrw SETUP.PY
+short_version = '1.0.9'
+version = '1.0.9'
+full_version = '1.0.9.dev0+f7cb50c'
+git_revision = 'f7cb50cf994e7ee184af5c22598553e1df810f0c'
+release = False
+
+if not release:
+    version = full_version
+    short_version += ".dev"
