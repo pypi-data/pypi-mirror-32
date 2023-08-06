40d6c5165757f918e4a739944e4e4235822af781
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:00:54 +0000

    Add package ciplog/1.0.1/ciplog-1.0.1.tar.gz

---
+++ packages/ciplog/ciplog-1.0.1.tar.gz/ciplog-1.0.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+VERSION='1.0.1'
+PACKAGES=['ciplog']
+
+setup(name='ciplog',
+      version=VERSION,
+      description='The funniest joke in the world',
+      author='Adriano Canofre',
+      author_email='adrianoknofre@gmail.com',
+      license='MIT',
+      packages=PACKAGES,
+      install_requires=[],
+      zip_safe=False)
