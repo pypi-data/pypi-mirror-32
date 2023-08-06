4a672a000590d11e5b61650c8b2f14b972ca4fbf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:07:40 +0000

    Add package screengrid/0.0.8/screengrid-0.0.8-py3-none-any.whl

---
+++ packages/screengrid/screengrid-0.0.8-py3-none-any.whl/screengrid/main.py
@@ -0,0 +1,29 @@
+import win32api, win32con, win32gui, win32ui
+import threading
+import time
+import string
+import ctypes
+import keyboard
+import functools
+
+import screencanvas
+import grid
+import mouse
+
+done = False
+
+LETTERS = set(string.ascii_lowercase)
+
+def foo():
+    main_grid = grid.Grid()
+    main_grid.draw_letter_grid()
+    s = time.time()
+    while time.time() - s < 100:
+        win32gui.PumpWaitingMessages()
+
+def main():
+    threading.Thread(target=foo, daemon=True).start()
+    input()
+
+if __name__ == '__main__':
+    main()
