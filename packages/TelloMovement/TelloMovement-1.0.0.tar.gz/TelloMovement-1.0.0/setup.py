175dd83b18e65404d477375b074f74bc981fad5b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:28:14 +0000

    Add package TelloMovement/1.0.0/TelloMovement-1.0.0.tar.gz

---
+++ packages/TelloMovement/TelloMovement-1.0.0.tar.gz/TelloMovement-1.0.0/setup.py
@@ -0,0 +1,11 @@
+import setuptools
+from distutils.core import setup
+
+setup(
+    name="TelloMovement",
+    description='Library with all methods compatible with the Tello Drones SDK',
+    author="Hudson Nobles",
+    version="1.0.0",
+    packages=['TelloMovement'],
+    license='LICENSE.txt'
+    )
