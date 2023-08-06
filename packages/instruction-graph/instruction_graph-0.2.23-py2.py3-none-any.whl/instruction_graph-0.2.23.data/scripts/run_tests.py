13decc5fa88f2329eef0b54ba6b9056bfd8e5143
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:34:35 +0000

    Add package instruction-graph/0.2.23/instruction_graph-0.2.23-py2.py3-none-any.whl

---
+++ packages/instruction-graph/instruction_graph-0.2.23-py2.py3-none-any.whl/instruction_graph-0.2.23.data/scripts/run_tests.py
@@ -0,0 +1,9 @@
+#!python
+import unittest
+
+
+if __name__ == "__main__":
+    loader = unittest.TestLoader()
+    suite = loader.discover('tests')
+    runner = unittest.TextTestRunner()
+    runner.run(suite)
