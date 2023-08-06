fe1c3689f078fbb656c2ed0d717893549ed761ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:39:10 +0000

    Add package GeNESiS-SDK/0.0.0/GeNESiS-SDK-0.0.0.tar.gz

---
+++ packages/GeNESiS-SDK/GeNESiS-SDK-0.0.0.tar.gz/GeNESiS-SDK-0.0.0/GeNESiS-SDK/main.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# Author:  Name name
+# Contact: bsc.mail@bsc.es
+''' **GeNESiS** kernel.
+'''
+
+
+def print_header():
+    print('Hello world')
+    return
---
+++ packages/GeNESiS-SDK/GeNESiS-SDK-0.0.0.tar.gz/GeNESiS-SDK-0.0.0/kernel.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# Author:  Name name
+# Contact: bsc.mail@bsc.es
+''' **GeNESiS** kernel. 
+'''
+
+if __name__ == '__main__':
+    print('Hello world')
+else:
+    pass
