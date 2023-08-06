98d2d601c2e719706e8395a201c5bc182df0e86e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:22:11 +0000

    Add package kafka-tfrx/0.12/kafka-tfrx-0.12.tar.gz

---
+++ packages/kafka-tfrx/kafka-tfrx-0.12.tar.gz/kafka-tfrx-0.12/kafka_tfrx/skeleton.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+This is a skeleton file that can serve as a starting point for a Python
+console script. To run this script uncomment the following line in the
+entry_points section in setup.py:
+
+    [console_scripts]
+    fibonacci = kafka_tfrx.skeleton:run
+
+Then run `python setup.py install` which will install the command `fibonacci`
+inside your current environment.
+Besides console scripts, the header (i.e. until _logger...) of this file can
+also be used as template for Python modules.
+
+Note: This skeleton file can be safely removed if not needed!
+"""
+from __future__ import division, print_function, absolute_import
+
+import sys
+import logging
+
+__author__ = "Carlo Mazzaferro"
+__copyright__ = "Carlo Mazzaferro"
+__license__ = "mit"
+
+_logger = logging.getLogger(__name__)
+
+
+def fib(n):
+    """Fibonacci example function
+
+    Args:
+      n (int): integer
+
+    Returns:
+      int: n-th Fibonacci number
+    """
+    assert n > 0
+    a, b = 1, 1
+    for i in range(n-1):
+        a, b = b, a+b
+    return a
+
+
+def setup_logging(loglevel):
+    """Setup basic logging
+
+    Args:
+      loglevel (int): minimum loglevel for emitting messages
+    """
+    logformat = "[%(asctime)s] %(levelname)s:%(name)s:%(message)s"
+    logging.basicConfig(level=loglevel, stream=sys.stdout,
+                        format=logformat, datefmt="%Y-%m-%d %H:%M:%S")
+
