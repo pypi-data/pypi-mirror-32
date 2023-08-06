aec35c5283ba2db36d0e9199f429b3874e111d18
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:08 +0000

    Add package psyshort/0.0.4/psyshort-0.0.4.tar.gz

---
+++ packages/psyshort/psyshort-0.0.4.tar.gz/psyshort-0.0.4/setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+with open("README.md", "rb") as f:
+    long_description = f.read().decode("utf-8")
+    
+setuptools.setup(
+    name="psyshort",
+    version="0.0.4",
+    author="Quit3Simpl3",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url="https://github.com/Quit3Simpl3/psyshort",
+    packages=setuptools.find_packages(),
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        )
+    )
