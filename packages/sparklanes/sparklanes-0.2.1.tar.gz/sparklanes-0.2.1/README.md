5a8e1a715b344bf235ec4d4900a964d731fb7ae2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:18:26 +0000

    Add package sparklanes/0.2.1/sparklanes-0.2.1.tar.gz

---
+++ packages/sparklanes/sparklanes-0.2.1.tar.gz/sparklanes-0.2.1/README.md
@@ -0,0 +1,31 @@
+sparklanes
+==========
+
+[![PyPI version](https://badge.fury.io/py/sparklanes.svg)](https://badge.fury.io/py/sparklanes)s
+[![Build Status](https://travis-ci.org/ksbg/sparklanes.svg?branch=master)](https://travis-ci.org/ksbg/sparklanes?branch=master)
+[![Coverage Status](https://coveralls.io/repos/github/ksbg/sparklanes/badge.svg?branch=master)](https://coveralls.io/github/ksbg/sparklanes?branch=master)
+[![Doc status](https://sparklanes.readthedocs.io/en/latest/?badge=latest)](https://sparklanes.readthedocs.io)
+![pylint Score](https://mperlet.github.io/pybadge/badges/9.88.svg)
+![license](https://img.shields.io/github/license/ksbg/sparklanes.svg)
+
+
+sparklanes is a lightweight data processing framework for Apache Spark
+written in Python. It was built with the intention to make building
+complex spark processing pipelines simpler, by shifting the focus
+towards writing data processing code without having to spent much time
+on the surrounding application architecture.
+
+Data processing pipelines, or *lanes*, are built by stringing together
+encapsulated processor classes, which allows creation of lane definitions
+with an arbitrary processor order, where processors can be easily
+removed, added or swapped.
+
+Processing pipelines can be defined using *lane configuration YAML files*,
+to then be packaged and submitted to spark using a single command.
+Alternatively, the same can be achieved manually by using the framework's
+API.
+
+Documentation
+-------------
+
+Find the documentation on [sparklanes.readthedocs.io](https://sparklanes.readthedocs.io)
---
+++ packages/sparklanes/sparklanes-0.2.1.tar.gz/sparklanes-0.2.1/setup.py
@@ -0,0 +1,44 @@
+import os
+from setuptools import setup
+
+with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'README.md')) as readme_file:
+    long_description = readme_file.read()
+
+setup(
+    name='sparklanes',
+    version='0.2.1',
+    url='https://github.com/ksbg/sparklanes',
+    project_urls={
+        'sparklanes documentation': 'https://sparklanes.readthedocs.io/',
+    },
+    author='Kevin Baumgarten',
+    author_email='kevin@ksbg.io',
+    description='A lightweight framework to build and execute data processing pipelines in pyspark '
+                '(Apache Spark\'s python API)',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    packages=['sparklanes', 'sparklanes._submit', 'sparklanes._framework'],
+    install_requires=[
+        'py4j==0.10.6',
+        'pyspark==2.3.0',
+        'PyYAML==3.12',
+        'schema==0.6.7',
+        'six==1.11.0',
+    ],
+    package_data={'sparklanes._submit': ['requirements-submit.txt']},
+    entry_points={'console_scripts': ['lane-submit=sparklanes._submit.submit:submit_to_spark']},
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+    keywords=['spark', 'pyspark', 'data', 'processing', 'preprocessing', 'pipelines'],
+    python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, <4',
+
+)
