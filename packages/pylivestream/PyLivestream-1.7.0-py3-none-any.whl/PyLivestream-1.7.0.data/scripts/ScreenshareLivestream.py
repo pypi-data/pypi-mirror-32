35d7401476004250b25cc31b59869ac577c26361
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:13:06 +0000

    Add package pylivestream/1.7.0/PyLivestream-1.7.0-py3-none-any.whl

---
+++ packages/pylivestream/PyLivestream-1.7.0-py3-none-any.whl/PyLivestream-1.7.0.data/scripts/ScreenshareLivestream.py
@@ -0,0 +1,42 @@
+#!/home/thin/miniconda3/bin/python
+"""
+LIVE STREAM using FFmpeg -- screenshare
+
+https://www.scivision.co/youtube-live-ffmpeg-livestream/
+
+Windows: get DirectShow device list from::
+
+   ffmpeg -list_devices true -f dshow -i dummy
+"""
+from typing import List
+from pathlib import Path
+import PyLivestream
+
+R = Path(__file__).parent
+
+if __name__ == '__main__':
+    import signal
+    signal.signal(signal.SIGINT, signal.SIG_DFL)
+
+    from argparse import ArgumentParser
+    p = ArgumentParser(description="livestream screenshare")
+    p.add_argument('site',
+                   help='site to stream: [youtube,periscope,facebook,twitch]',
+                   nargs='?', default='localhost')
+    p.add_argument('-i', '--ini', help='*.ini file with stream parameters',
+                   default=R/'stream.ini')
+    p.add_argument('-y', '--yes', help='no confirmation dialog',
+                   action='store_true')
+    P = p.parse_args()
+
+    site = P.site.split()
+
+    S = PyLivestream.Screenshare(P.ini, site, yes=P.yes)
+    sites: List[str] = list(S.streams.keys())
+# %% Go live
+    if P.yes:
+        print('going live on', sites)
+    else:
+        input(f"Press Enter to go live on {sites}    Or Ctrl C to abort.")
+
+    S.golive()
