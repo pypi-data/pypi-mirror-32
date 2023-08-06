f3a7d279b2b34d6f9b9d3adc4fbceab496ca67cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:25:18 +0000

    Add package mainutils/0.2.1/mainutils-0.2.1.tar.gz

---
+++ packages/mainutils/mainutils-0.2.1.tar.gz/mainutils-0.2.1/mainutils/__init__.py
@@ -0,0 +1,43 @@
+import sys
+import inspect
+from pathlib import Path
+
+
+def module_parent(level=1, caller_file=None):
+    """
+    activate relative import in __name__ == '__main__'
+
+    example:
+    __package__ = module_parent()
+    importlib.import_module(__package__)
+
+    """
+    if not caller_file:
+        stack = inspect.stack()
+        caller_file = stack[1].filename
+
+    file = Path(caller_file).resolve()
+    parent, top = file.parent, file.parents[level]
+
+    sys.path.append(str(top))
+    try:
+        sys.path.remove(str(parent))
+    except ValueError:
+        pass
+
+    return '.'.join(parent.parts[len(top.parts):])
+
+
+def disable_requests_warnings():
+    """
+    disable requests warnings when verify=False option.
+    """
+    try:
+        import urllib3
+        from urllib3.exceptions import InsecureRequestWarning
+        urllib3.disable_warnings(InsecureRequestWarning)
+    except ImportError:
+        import requests
+        from requests.packages.urllib3.exceptions \
+            import InsecureRequestWarning
+        requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
