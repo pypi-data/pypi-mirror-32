7991e67a4c61ae2303384ac46ad530c8971a8f41
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:00:51 +0000

    Add package django-push/1.1/django-push-1.1.tar.gz

---
+++ packages/django-push/django-push-1.1.tar.gz/django-push-1.1/setup.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+from distutils.core import setup
+from setuptools import find_packages
+
+
+with open('README.rst') as readme:
+    long_description = readme.read()
+
+setup(
+    name='django-push',
+    version=__import__('django_push').__version__,
+    author='Bruno Renié',
+    author_email='bruno@renie.fr',
+    url='https://github.com/brutasse/django-push',
+    license='BSD',
+    description='PubSubHubbub (PuSH) support for Django',
+    long_description=long_description,
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'Django',
+        'requests',
+    ],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Framework :: Django :: 1.11',
+        'Framework :: Django :: 2.0',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+    test_suite='runtests.runtests',
+    zip_safe=False,
+)
