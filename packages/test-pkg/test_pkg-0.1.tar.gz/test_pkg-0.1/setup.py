de82085e8abcc660893c3f9a09bc654ca65792cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:09 +0000

    Add package test-pkg/0.1/test_pkg-0.1.tar.gz

---
+++ packages/test-pkg/test_pkg-0.1.tar.gz/test_pkg-0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='test_pkg',
+      version='0.1',
+      description='The funniest joke in the world',
+      url='https://github.com/IanNMarshall/test_pkg/',
+      author='IanNMarshall',
+      author_email='ian.nicholas.marshall@gmail.com',
+      packages=['test_pkg'],
+      zip_safe=False)
