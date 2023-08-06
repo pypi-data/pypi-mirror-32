e2e369e3347f56a8a41e1d489c4f8b218543cf8b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:33:40 +0000

    Add package devconf/0.1/devconf-0.1.tar.gz

---
+++ packages/devconf/devconf-0.1.tar.gz/devconf-0.1/setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+VERSION = '0.1'
+
+long_description = '''Builder scripts for DevConf sites'''
+
+setup_info = dict(
+    # Metadata
+    name='devconf',
+    version=VERSION,
+    author='Chris Ward',
+    author_email='cward@redhat.com',
+    description=long_description,
+    long_description=long_description,
+    # Package info
+    packages=['devconf', ],
+    scripts=[
+    ],
+    install_requires=[
+        'comdev==0.2.1',
+    ],
+)
+
+
+setup(**setup_info)
