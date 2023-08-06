bcad1830941dcf5608d821696e2683ca7ef37bd1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:17:05 +0000

    Add package luaparser/2.0.0/luaparser-2.0.0.tar.gz

---
+++ packages/luaparser/luaparser-2.0.0.tar.gz/luaparser-2.0.0/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup
+import luaparser
+
+
+setup(
+    name = 'luaparser',
+    version = luaparser.__version__,
+    description = 'A lua parser in Python',
+    url = 'https://github.com/boolangery/py-lua-parser',
+    download_url = 'https://github.com/boolangery/py-lua-parser/archive/' + luaparser.__version__ + '.tar.gz',
+    author = 'Eliott Dumeix',
+    author_email = 'eliott.dumeix@gmail.com',
+    license = 'MIT',
+    packages = ['luaparser', 'luaparser.parser', 'luaparser.utils', 'luaparser.tests'],
+    zip_safe = False,
+    classifiers = [
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6'
+    ],
+    install_requires = [
+        'antlr4-python3-runtime<=4.7.1',
+    ],
+    entry_points={
+        'console_scripts': [
+            'luaparser = luaparser.__main__:main'
+        ]
+    }
+)
