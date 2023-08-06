73734d21789861698e3a3d8e5aa7fca33ee6a4fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:56:15 +0000

    Add package vampireingalaxy/0.0.6/vampireingalaxy-0.0.6.tar.gz

---
+++ packages/vampireingalaxy/vampireingalaxy-0.0.6.tar.gz/vampireingalaxy-0.0.6/setup.py
@@ -0,0 +1,31 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="vampireingalaxy",
+    version="0.0.6",
+    author="Kyu Sang Han",
+    author_email="khan21@jhu.edu",
+    description="Vampire Image Analysis Package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://http://wirtzlab.johnshopkins.edu",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'scipy',
+        'pandas',
+        'numpy',
+        'pillow',
+        'matplotlib',
+        'scikit-learn',
+        'imageio',
+    ],
+    scripts=['bin/vampire-run.py','bin/sort-run.py'],
+    classifiers=(
+        "Programming Language :: Python :: 2.7",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: MacOS :: MacOS X",
+    ),
+)
