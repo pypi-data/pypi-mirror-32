2350ed13a9588dc43bce13abbc33e040454b7361
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:34 +0000

    Add package plone.app.locales/5.1.9/plone.app.locales-5.1.9.tar.gz

---
+++ packages/plone.app.locales/plone.app.locales-5.1.9.tar.gz/plone.app.locales-5.1.9/setup.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+from setuptools import find_packages
+from setuptools import setup
+
+version = '5.1.9'
+
+setup(
+    name='plone.app.locales',
+    version=version,
+    description='Translation files for Plone',
+    long_description=(open('README.rst').read() + '\n' +
+                      open('CHANGES.rst').read()),
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Plone',
+        'Framework :: Plone :: 5.1',
+        'Framework :: Zope2',
+        'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+    ],
+    keywords='plone i18n locale translation',
+    author='Plone Foundation',
+    author_email='plone-developers@lists.sourceforge.net',
+    url='https://github.com/collective/plone.app.locales',
+    license='GPL version 2',
+    packages=find_packages(exclude=['ez_setup']),
+    namespace_packages=['plone', 'plone.app'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'setuptools',
+    ],
+)
