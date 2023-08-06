d20b06ccc17cf6c1d14ec780b9f8405cdbf777b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:51:00 +0000

    Add package cxflow-tensorflow/0.5.0/cxflow-tensorflow-0.5.0.tar.gz

---
+++ packages/cxflow-tensorflow/cxflow-tensorflow-0.5.0.tar.gz/cxflow-tensorflow-0.5.0/README.md
@@ -0,0 +1,33 @@
+# cxflow-tensorflow
+[![CircleCI](https://circleci.com/gh/Cognexa/cxflow-tensorflow/tree/master.svg?style=shield)](https://circleci.com/gh/Cognexa/cxflow-tensorflow/tree/master)
+[![PyPI version](https://badge.fury.io/py/cxflow-tensorflow.svg)](https://badge.fury.io/py/cxflow-tensorflow)
+[![Coverage 
+Status](https://coveralls.io/repos/github/Cognexa/cxflow-tensorflow/badge.svg?branch=master)](https://coveralls.io/github/Cognexa/cxflow-tensorflow?branch=master)
+[![Development Status](https://img.shields.io/badge/status-CX%20Regular-brightgreen.svg?style=flat)]()
+[![MIT license](https://img.shields.io/badge/license-MIT-blue.svg?style=flat)]()
+[![Master Developer](https://img.shields.io/badge/master-Adam%20Blažek-lightgrey.svg?style=flat)]()
+
+TensorFlow extension for [cxflow framework](https://github.com/cognexa/cxflow) allowing to train models defined in TensorFlow.
+
+## Installation
+
+1. Install TensorFlow according to the [official instructions](https://www.tensorflow.org/install/)
+
+2. Install cxflow-tensorflow with pip
+```
+pip install cxflow-tensorflow
+```
+
+## Quick start
+
+- [5 minutes tutorial](https://tensorflow.cxflow.org/tutorial) ([code](https://github.com/Cognexa/cxflow-examples/tree/master/mnist_convnet))
+
+- [Documentation & API Reference](https://tensorflow.cxflow.org/)
+
+- [Additional examples](https://github.com/cognexa/cxflow-examples)
+
+
+## Contributions
+
+All contributions are welcomed. Please read our [contributor's guide](CONTRIBUTING.md).
+
---
+++ packages/cxflow-tensorflow/cxflow-tensorflow-0.5.0.tar.gz/cxflow-tensorflow-0.5.0/setup.py
@@ -0,0 +1,40 @@
+from setuptools import setup
+
+setup(name='cxflow-tensorflow',
+      version='0.5.0',
+      description='TensorFlow extension for cxflow.',
+      long_description='Plugin that enables cxflow to work with TensorFlow.',
+      classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Education',
+        'Intended Audience :: Science/Research',
+        'Operating System :: Unix',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6'
+      ],
+      keywords='tensorflow wrapper',
+      url='https://github.com/Cognexa/cxflow-tensorflow',
+      author='Cognexa Solutions s.r.o.',
+      author_email='info@cognexa.com',
+      license='MIT',
+      packages=[
+          'cxflow_tensorflow',
+          'cxflow_tensorflow.hooks',
+          'cxflow_tensorflow.models',
+          'cxflow_tensorflow.ops',
+          'cxflow_tensorflow.utils',
+          'cxflow_tensorflow.metrics',
+          'cxflow_tensorflow.third_party',
+          'cxflow_tensorflow.third_party.tensorflow'
+      ],
+      include_package_data=True,
+      zip_safe=False,
+      test_suite='cxflow_tensorflow.tests',
+      tests_require=['testfixtures>=5.1.1'],
+      install_requires=[line for line in open('requirements.txt', 'r').readlines() if not line.startswith('#')],
+     )
