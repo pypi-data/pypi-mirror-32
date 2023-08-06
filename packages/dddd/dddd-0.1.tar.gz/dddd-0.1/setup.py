5ff2c20d4b77d9eabcd7128983286e01fa8fedae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:49:26 +0000

    Add package dddd/0.1/dddd-0.1.tar.gz

---
+++ packages/dddd/dddd-0.1.tar.gz/dddd-0.1/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+#setup(
+ #    name='demo_logger',
+  #   version='0.1',                          
+   #  scripts=['logger.py']                  
+#)
+
+setup(name='dddd',
+  version='0.1',
+  description='randomtry',
+  author='shailesh1996',
+  author_email='shailesh199691@gmail.com',
+  scripts= ['logger.py']
+ )
