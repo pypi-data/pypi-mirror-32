a2a8c0fb8223985469aae21cc509c6b99ff64486
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:01:49 +0000

    Add package resolwe/10.0.0a3/resolwe-10.0.0a3-py3-none-any.whl

---
+++ packages/resolwe/resolwe-10.0.0a3-py3-none-any.whl/resolwe/elastic/management/commands/elastic_purge.py
@@ -0,0 +1,34 @@
+""".. Ignore pydocstyle D400.
+
+======================
+Command: elastic_purge
+======================
+
+"""
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+from django.core.management.base import BaseCommand
+
+from resolwe.elastic.builder import index_builder
+from resolwe.elastic.mixins import ElasticIndexFilterMixin
+
+
+class Command(ElasticIndexFilterMixin, BaseCommand):
+    """Purge ElasticSearch indexes."""
+
+    help = "Purge ElasticSearch indexes."
+
+    def handle_index(self, index):
+        """Process index."""
+        index.destroy()
+        index.create_mapping()
+
+    def handle(self, *args, **options):
+        """Command handle."""
+        verbosity = int(options['verbosity'])
+
+        if self.has_filter(options):
+            self.filter_indices(options, verbosity)
+        else:
+            # Process all indices.
+            index_builder.delete()
