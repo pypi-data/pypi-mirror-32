905ddb42df0a89f9979c02eb8e355f7e5ff764c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:59:59 +0000

    Add package ntcir-math-density/0.1.2/ntcir_math_density-0.1.2-py2.py3-none-any.whl

---
+++ packages/ntcir-math-density/ntcir_math_density-0.1.2-py2.py3-none-any.whl/ntcir_math_density/__init__.py
@@ -0,0 +1,12 @@
+"""
+The NTCIR Math Density Estimator package uses NTCIR-10 Math, NTCIR-11 Math-2, and NTCIR-12 MathIR
+datasets to compute density, and probability estimators.
+"""
+
+from .estimator import get_judged_identifiers, get_all_positions, get_estimators, get_estimates
+from .view import plot_estimates
+
+
+__author__ = "Vit Novotny"
+__version__ = "0.1.2"
+__license__ = "MIT"
