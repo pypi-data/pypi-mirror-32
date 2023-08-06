81b6256d8d25e80b1c607fd8aa384a7724896ca6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:36:44 +0000

    Add package creaturecast-node/0.0.9/creaturecast_node-0.0.9.tar.gz

---
+++ packages/creaturecast-node/creaturecast_node-0.0.9.tar.gz/creaturecast_node-0.0.9/creaturecast_node/__init__.py
@@ -0,0 +1,6 @@
+__version__ = '0.0.9'
+__description__ = 'Base node for creaturecast'
+__author__ = 'Paxton Gerrish'
+__email__ = 'creaturecastlabs@gmail.com'
+__url__ = 'https://github.com/Paxtongerrish/creaturecast_node.git'
+
