6a8daba5b78b56a119b801f4bcfaff62794ae2c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:11 +0000

    Add package gym-super-mario-bros/0.13.0/gym_super_mario_bros-0.13.0.tar.gz

---
+++ packages/gym-super-mario-bros/gym_super_mario_bros-0.13.0.tar.gz/gym_super_mario_bros-0.13.0/gym_super_mario_bros/__init__.py
@@ -0,0 +1,15 @@
+"""Registration code of Gym environments in this package."""
+from .smb_env import SuperMarioBrosEnv
+from ._registration import make
+from .wrappers import wrap
+
+
+# define the outward facing API of this module (none, gym provides the API)
+__all__ = [
+    SuperMarioBrosEnv.__name__,
+    make.__name__,
+    wrap.__name__,
+]
+
+if __name__ == '__main__':
+    print('init')
