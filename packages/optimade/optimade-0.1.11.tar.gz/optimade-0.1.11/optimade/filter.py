15ed8681e1af6b96bf2210779b5baa7e516b6e98
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:37:45 +0000

    Add package optimade/0.1.11/optimade-0.1.11.tar.gz

---
+++ packages/optimade/optimade-0.1.11.tar.gz/optimade-0.1.11/optimade/filter.py
@@ -0,0 +1,37 @@
+import os
+import re
+from glob import glob
+
+from lark import Lark
+
+parser = {}
+for name in glob(os.path.join(os.path.dirname(__file__), "grammar", "*.g")):
+    with open(name) as f:
+        ver = tuple(
+            int(n) for n in
+            re.findall(r"\d+", str(os.path.basename(name).split(".g")[0]))
+        )
+        parser[ver] = Lark(f.read())
+
+
+class Parser:
+    def __init__(self, version=None):
+        if version is None:
+            self.version = sorted(parser.keys())[-1]
+            self.lark = parser[self.version]
+        elif version in parser:
+            self.lark = parser[version]
+            self.version = version
+        else:
+            raise ValueError("Unknown parser grammar version: {}"
+                             .format(version))
+        self.tree = None
+        self.filter = None
+
+    def parse(self, filter_):
+        self.tree = self.lark.parse(filter_)
+        self.filter = filter_
+        return self.tree
+
+    def __repr__(self):
+        return self.tree.pretty()
