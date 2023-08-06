75ecb99db733b3707b2b47fcb2aab0c60f803cd4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:55:56 +0000

    Add package graphene-django/2.1rc1/graphene-django-2.1rc1.tar.gz

---
+++ packages/graphene-django/graphene-django-2.1rc1.tar.gz/graphene-django-2.1rc1/graphene_django/__init__.py
@@ -0,0 +1,14 @@
+from .types import (
+    DjangoObjectType,
+)
+from .fields import (
+    DjangoConnectionField,
+)
+
+__version__ = '2.1rc1'
+
+__all__ = [
+    '__version__',
+    'DjangoObjectType',
+    'DjangoConnectionField'
+]
