7f4e8a8da8758c213748966746942067de2d2edb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:13:13 +0000

    Add package http-okapi/1.0.0/http_okapi-1.0.0-py2.py3-none-any.whl

---
+++ packages/http-okapi/http_okapi-1.0.0-py2.py3-none-any.whl/http_okapi/cherrypy.py
@@ -0,0 +1,28 @@
+from __future__ import absolute_import
+
+try:
+	import importlib.resources
+except ImportError:
+	import importlib
+	importlib.resources = importlib.import_module('importlib_resources')
+
+import cherrypy
+
+import http_okapi
+
+
+class Server:
+	@cherrypy.expose
+	def index(self):
+		"""
+		>>> srv = Server()
+		>>> srv.index()
+		b'...'
+		"""
+		cherrypy.response.headers['Content-Type'] = 'text/html'
+		doc = importlib.resources.read_text(http_okapi, 'okapi.html')
+		return doc.encode('utf-8')
+
+	@cherrypy.expose
+	def okapibg_png(self):
+		return importlib.resources.read_binary(http_okapi, 'okapibg.png')
