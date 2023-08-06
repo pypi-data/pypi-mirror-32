ad03649a55cce7e9f5ca18ffecfa2258bbb5b799
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:27:08 +0000

    Add package citi-wikibot/0.4.5/citi-wikibot-0.4.5.tar.gz

---
+++ packages/citi-wikibot/citi-wikibot-0.4.5.tar.gz/citi-wikibot-0.4.5/setup.py
@@ -0,0 +1,21 @@
+from distutils.core import setup
+
+with open('README.rst', 'r') as f:
+  readme = f.read()
+
+setup(
+  name='citi-wikibot',
+  packages=['wikibot'],
+  version='0.4.5',
+  description='CITI\'s Wiki page editing for lazy people.',
+  long_description=readme,
+  long_description_content_type='text/x-rst',
+  author='Vanessa Barreiros',
+  author_email='vanessa.barreiros@citi.org.br',
+  url='https://github.com/citi-ufpe/citi-wikibot',
+  keywords=['wiki', 'bot', 'wikibot', 'citi'],
+  install_requires=[
+    'requests',
+    'misaka'
+  ]
+)
