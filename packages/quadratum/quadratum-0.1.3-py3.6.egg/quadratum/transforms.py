d692685e6e69d73a098bbaf96178393ba80bdde7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:13:12 +0000

    Add package quadratum/0.1.3/quadratum-0.1.3-py3.6.egg

---
+++ packages/quadratum/quadratum-0.1.3-py3.6.egg/quadratum/transforms.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+
+from . import functional as F
+
+__all__ = ["Whiten", "Dominofy", "Contain"]
+
+
+class Whiten(object):
+    """Make all transparent pixels white (if there is an alpha channel)."""
+
+    def __init__(self, threshold=255):
+        self.threshold = threshold
+
+    def __call__(self, image):
+        return F.whiten(image, self.threshold)
+
+
+class Dominofy(object):
+    """Limits the ratio of an image."""
+
+    def __init__(self, threshold=2):
+        self.threshold = threshold
+
+    def __call__(self, image):
+        return F.dominofy(image, self.threshold)
+
+
+class Contain(object):
+    """Contains an image into given canvas, like good-old `background-size: contain;` from CSS."""
+
+    def __init__(self, size, fill_black=False):
+        self.size = size
+        self.fill_black = fill_black
+
+    def __call__(self, image):
+        return F.contain(image, self.size, fill_black=self.fill_black)
