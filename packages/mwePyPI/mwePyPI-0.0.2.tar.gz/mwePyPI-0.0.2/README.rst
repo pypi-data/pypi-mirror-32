50dc33903eeff8182085aa6e8291c782431f9df2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:36:04 +0000

    Add package mwePyPI/0.0.2/mwePyPI-0.0.2.tar.gz

---
+++ packages/mwePyPI/mwePyPI-0.0.2.tar.gz/mwePyPI-0.0.2/README.rst
@@ -0,0 +1,13 @@
+README
+======
+
+I'm just testing how to upload a package in PyPI.
+
+
+Links that I helped me to make this package:
+
+http://python-packaging.readthedocs.io/en/latest/minimal.html
+
+https://www.youtube.com/watch?v=YIdXBq2_RN8
+
+https://www.youtube.com/watch?v=Svk0z3kNwd0 (yes I'm from Brazil)
---
+++ packages/mwePyPI/mwePyPI-0.0.2.tar.gz/mwePyPI-0.0.2/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='mwePyPI',
+      version='0.0.2',
+      description='It is a minimal work exemple of a package to be uploaded in PyPI.',
+      url='https://github.com/PedroRegisPOAR/mwePyPi',
+      author='Pedro Osvaldo Alencar Regis',
+      author_email='pedroalencarregis@hotmail.com',
+      license='MIT',
+      packages=['mwePyPI'],
+      zip_safe=False)
