b612cdc544de80080c94c5a5e0d80c2b096e1123
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:55:43 +0000

    Add package infi.caching/0.0.1/infi.caching-0.0.1-py2-none-any.whl

---
+++ packages/infi.caching/infi.caching-0.0.1-py2-none-any.whl/infi/caching/__version__.py
@@ -0,0 +1,11 @@
+__version__ = "0.0.1"
+__git_commiter_name__ = "Arnon Yaari"
+__git_commiter_email__ = "arnony@infinidat.com"
+__git_branch__ = u'(HEAD detached at v0.0.1)'
+__git_remote_tracking_branch__ = '(No remote tracking)'
+__git_remote_url__ = '(Not remote tracking)'
+__git_head_hash__ = 'f0d5964a264f3214d9e0d162de9b3e06af07733d'
+__git_head_subject__ = u'Finished Release v0.0.1'
+__git_head_message__ = u''
+__git_dirty_diff__ = u''
+__git_commit_date__ = '2018-06-10 11:41:48'
