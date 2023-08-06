f139f856c51a15bddfb5089f8d9a11c2ecefc41a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:47 +0000

    Add package osafe/0.1.0/osafe-0.1.0.tar.gz

---
+++ packages/osafe/osafe-0.1.0.tar.gz/osafe-0.1.0/osafe/cli.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+
+from getpass import getpass
+from subprocess import check_call
+from tempfile import NamedTemporaryFile
+import os
+
+from .encryption import Encryption
+from .storage import Storage
+
+
+def main():
+    storage = Storage()
+
+    message = storage.get()
+    content = None
+
+    while content is None:
+        if message:
+            passphrase = getpass("Enter your passphrase: ")
+        else:
+            passphrase = None
+            passphrase_confirmation = None
+            while not passphrase or passphrase != passphrase_confirmation:
+                passphrase = getpass("Enter your new passphrase: ")
+                passphrase_confirmation = getpass("Enter the same passphrase again: ")
+
+        encryption = Encryption(passphrase)
+
+        if message:
+            content = encryption.decrypt(message)
+        else:
+            content = ""
+
+    with NamedTemporaryFile(prefix="osafe-", mode='w+t', encoding='utf-8', newline='\n') as file:
+        file.write(content)
+        file.seek(0)
+
+        editor = os.environ.get('EDITOR')
+        if not editor:
+            print("EDITOR environment variable not set.")
+            while not editor:
+                editor = input("Type your preferred editor: ")
+
+        check_call([editor, file.name])
+
+        new_content = file.read()
+        if new_content != content:
+            storage.set(encryption.encrypt(new_content))
+
+
+if __name__ == '__main__':
+    main()
