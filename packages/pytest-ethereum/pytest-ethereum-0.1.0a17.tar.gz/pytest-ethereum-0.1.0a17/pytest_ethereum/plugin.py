3491f5528e1fdda48953762a010c6b56b235830c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:34 +0000

    Add package pytest-ethereum/0.1.0a17/pytest-ethereum-0.1.0a17.tar.gz

---
+++ packages/pytest-ethereum/pytest-ethereum-0.1.0a17.tar.gz/pytest-ethereum-0.1.0a17/pytest_ethereum/plugin.py
@@ -0,0 +1,34 @@
+import pytest
+
+from .assets import get_assets
+from .tester import Tester
+
+
+# User can specify these options through execution flags
+def pytest_addoption(parser):
+    """Add options for contract assets file handling"""
+
+    group = parser.getgroup('ethereum', 'ethereum testing support')
+    group.addoption('--assets-file', action='store', default=None, metavar='path',
+            help='assets file for coverage, default: none')
+
+
+# Load assets file into memory
+@pytest.fixture(scope='session')
+def assets(pytestconfig):
+    assets = {}
+    assets_file = pytestconfig.option.assets_file
+    if assets_file:
+        assets = get_assets(assets_file)
+    return assets
+
+
+# Fixture is initialized from default for every test
+@pytest.fixture
+def tester(assets):
+    return Tester(assets)
+
+# alias fixture
+@pytest.fixture
+def t(tester):
+    return tester
