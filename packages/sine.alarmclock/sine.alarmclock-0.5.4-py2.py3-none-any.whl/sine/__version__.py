f9e1e10a71b8e6a08c12981cc029b1d15fe5531c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:09:17 +0000

    Add package sine.alarmclock/0.5.4/sine.alarmclock-0.5.4-py2.py3-none-any.whl

---
+++ packages/sine.alarmclock/sine.alarmclock-0.5.4-py2.py3-none-any.whl/sine/__version__.py
@@ -0,0 +1,8 @@
+#    db    88        db    88""Yb  8b    d8  dP""b8 88      dP""b8   dP""b8 88  dP
+#   dPYb   88       dPYb   88__dP  88b  d88 dP   `" 88     dP    qb dP   `" 88odP 
+#  dP__Yb  88      dP__Yb  88""8.  88YbdP88 Yb      88     Yb    dp Yb      88"Yb 
+# dP""""Yb 888888 dP""""Yb 88  "8. 88 YY 88  YboodP 888888  YboodP   YboodP 88  Yb
+
+VERSION = (0, 5, 4)
+
+__version__ = '.'.join(map(str, VERSION))
