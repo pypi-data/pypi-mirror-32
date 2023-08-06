5a7fe07ffce478013dad942c6302e46baae7420e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:49:21 +0000

    Add package dicthash/0.0.2/dicthash-0.0.2.tar.gz

---
+++ packages/dicthash/dicthash-0.0.2.tar.gz/dicthash-0.0.2/setup.py
@@ -0,0 +1,28 @@
+# encoding: utf8
+from setuptools import setup
+
+setup(
+    name='dicthash',
+    version='0.0.2',
+    author='Jakob Jordan, Maximilian Schmidt',
+    author_email='j.jordan@fz-juelich.de',
+    description=('Generate portable md5 hashes from (arbitrarily nested) dictionaries.'),
+    license='MIT',
+    keywords='hashing hash',
+    url='https://github.com/INM-6/python-dicthash',
+    python_requires='>=2.7,!=3.0.*,!=3.1.*,!=3.2.*,!=3.3.*, !=3.4.*, <4',
+    install_requires=['future'],
+    packages=['dicthash'],
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Utilities',
+    ],
+)
