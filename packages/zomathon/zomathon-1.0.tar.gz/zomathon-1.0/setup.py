5e0b99bab2f013f2470aba2131696b59590a28e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:49:24 +0000

    Add package zomathon/1.0/zomathon-1.0.tar.gz

---
+++ packages/zomathon/zomathon-1.0.tar.gz/zomathon-1.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='zomathon',
+    version=1.0,
+    description='Zomato API Python Module',
+
+    author='abhishtagatya',
+    author_email='gatya.dev@yahoo.com',
+    url='https://github.com/abhishtagatya/zomathon',
+    
+    license='MIT',
+    packages=['zomathon'],
+    zip_safe=True
+)
