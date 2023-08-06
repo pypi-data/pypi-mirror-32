0f9eecf928b9f41688daf2b9f50a011c6dbb6871
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:36:12 +0000

    Add package cy/0.1.1/cy-0.1.1.tar.gz

---
+++ packages/cy/cy-0.1.1.tar.gz/cy-0.1.1/setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# Copyright (C) 2018 Emanuel Goncalves
+
+import setuptools
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='cy',
+    version='0.1.1',
+    author='Emanuel Goncalves',
+    author_email='eg14@sanger.ac.uk',
+    description='Modelling CRISPR dropout data',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/EmanuelGoncalves/crispy',
+    packages=setuptools.find_packages(),
+    classifiers=(
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Intended Audience :: Science/Research',
+        'Topic :: Scientific/Engineering :: Bio-Informatics',
+        'Topic :: Scientific/Engineering :: Information Analysis'
+    ),
+)
