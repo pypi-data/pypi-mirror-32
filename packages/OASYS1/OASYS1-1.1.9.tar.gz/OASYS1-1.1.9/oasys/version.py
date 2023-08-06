ffbc97daa7053ac18aaebccb4c0eb47a0f4170a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:39:18 +0000

    Add package OASYS1/1.1.9/OASYS1-1.1.9.tar.gz

---
+++ packages/OASYS1/OASYS1-1.1.9.tar.gz/OASYS1-1.1.9/oasys/version.py
@@ -0,0 +1,11 @@
+
+# THIS FILE IS GENERATED FROM OASYS SETUP.PY
+short_version = '1.1.9'
+version = '1.1.9'
+full_version = '1.1.9'
+git_revision = 'ed4e485cef7c2e32cfea56272b329eb323911ec2'
+release = True
+
+if not release:
+    version = full_version
+    short_version += ".dev"
