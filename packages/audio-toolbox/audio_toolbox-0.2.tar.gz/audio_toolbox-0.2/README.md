be0c1cd54e40bc72759b10346d4ce43b66864290
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:17:50 +0000

    Add package audio-toolbox/0.2/audio_toolbox-0.2.tar.gz

---
+++ packages/audio-toolbox/audio_toolbox-0.2.tar.gz/audio_toolbox-0.2/README.md
@@ -0,0 +1,14 @@
+# audio_toolbox
+
+Handy wrapper around sox and ffmpeg.
+
+## Changelog
+
+### 0.2
+
+* Added missing routines
+* Improved build scripts
+
+### 0.1
+
+* Initial commit
---
+++ packages/audio-toolbox/audio_toolbox-0.2.tar.gz/audio_toolbox-0.2/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(name='audio_toolbox',
+      packages=['audio_toolbox'],
+      version='0.2',
+      description='Handy wrapper around sox and ffmpeg',
+      author='Tomasz Oponowicz',
+      author_email='tomasz@zaxo.biz',
+      url='https://github.com/tomasz-oponowicz/audio_toolbox',
+      license='MIT',
+      classifiers=[
+        'Programming Language :: Python :: 3'
+      ],
+      python_requires='>=3')
