c38ead6c4b99cf4d9796f5738c9a42776a7dcbd7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:30 +0000

    Add package etimos/0.2/etimos-0.2.tar.gz

---
+++ packages/etimos/etimos-0.2.tar.gz/etimos-0.2/etimos/visualizer.py
@@ -0,0 +1,89 @@
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+class DataExplorer(object):
+	def __init__(self, label_tag, problem="classification"):
+		super(DataExplorer, self).__init__()
+		self.label_tag = label_tag
+		self.problem = problem
+
+	def first_exploration(self, df, rows=5):
+		
+		print("First %s Rows of the dataset." %rows)
+		print(df.head(rows))
+		print("Last %s Rows of the dataset." %rows)
+		print(df.head(rows))
+		
+		if self.problem == "classification":
+			print("Number of samples for each target")
+			target = pd.Series(df[self.label_tag])
+			print(target.value_counts())
+		else:
+			print("Target MEAN %s, STD %s " %(df[self.label_tag].mean(), df[self.label_tag].std()))
+			sns.distplot(df[self.label_tag])
+			plt.show()
+		return
+
+	def features_correlation(self, df, width=25, height=12):
+		colormap = plt.cm.RdBu
+		plt.figure(figsize=(width, height))
+		plt.title('Pearson Correlation of Features', y=1.05, size=15)
+		sns.heatmap(df.astype(float).corr(), cmap=colormap)
+		plt.show()
+		return
+
+	def pair_plots(self, df):
+		g = sns.pairplot(df, hue=self.label_tag)
+		g.set(xticklabels=[])
+		plt.show()
+		return
+
+	def pair_plots_kde(self, df):
+		g = sns.PairGrid(df)
+		g.map_diag(sns.kdeplot)
+		g.map_offdiag(sns.kdeplot, cmap="Blues_d", n_levels=6)
+		plt.show()
+		return
+
+	def plot_features_distribution(self, df, rows=4, height=13, width=25):
+		features = df.columns
+		columns = int(len(features)/rows) + 1
+		fig = plt.figure(figsize=(width, height))
+		for i, feat_name in enumerate(features):
+			ax = fig.add_subplot(rows, columns, i+1)
+			sns.distplot(df[feat_name], ax=ax)
+		plt.show()
+		return
+
+	def plot_features_box_plot(self, df, rows=4, height=13, width=25):
+		features = df.columns
+		columns = int(len(features)/rows) + 1
+		fig = plt.figure(figsize=(width, height))
+		for i, feat_name in enumerate(features):
+			ax = fig.add_subplot(rows, columns, i+1)
+			sns.boxplot(y=df[feat_name], ax=ax)
+		plt.show()
+		return
+
+	def plot_train_test_distribution(self, X_train, X_test, rows=4, height=13, width=25):
+		"""
+			The method plots the distribution of each feature in the train 
+			and test set. 
+
+			@args
+				X_train : numpy array
+				X_test : numpy array
+				rows : int -> number of rows in the grid to display
+				height, width : int -> dimension of the window
+		"""
+		columns = int(X_train.shape[1]/rows) + 1
+		fig = plt.figure(figsize=(width, height))
+		for i in range(0, X_train.shape[1]):
+			ax = fig.add_subplot(rows, columns, i+1)
+			sns.distplot(X_train[:,i], ax=ax)
+			sns.distplot(X_test[:,i], ax=ax)
+		plt.show()
+		return
+
+		
---
+++ packages/etimos/etimos-0.2.tar.gz/etimos-0.2/setup.py
@@ -0,0 +1,35 @@
+from distutils.core import setup
+from setuptools import setup, find_packages
+
+def check_dependencies():
+    install_requires = []
+    
+    try:
+        import numpy
+    except ImportError:
+        install_requires.append('seaborn')
+    try:
+        import pandas
+    except ImportError:
+        install_requires.append('pandas')
+
+install_requires = check_dependencies()
+
+setup(
+  name = 'etimos',
+  version = '0.2',
+  description = 'Data Visualization Library',
+  author = 'Skopos-team',
+  packages=find_packages(),
+  install_requires=install_requires,
+  author_email = 'skopos.library@gmail.com',
+  url = 'https://github.com/Skopos-team/Etimos', 
+  license='Apache2',
+  download_url = 'https://github.com/Skopos-team/Etimos/archive/0.2.tar.gz',
+  keywords = ['testing', 'logging', 'example'],
+  classifiers = ['Programming Language :: Python :: 3.5',
+                  'Operating System :: POSIX',
+                  'Operating System :: Unix',
+                  'Operating System :: MacOS'
+                  ],
+)
