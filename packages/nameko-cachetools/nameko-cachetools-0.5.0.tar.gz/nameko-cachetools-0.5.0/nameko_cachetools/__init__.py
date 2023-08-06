63cecdf0deb5786525c68f6580b7757112c3d794
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:22:35 +0000

    Add package nameko-cachetools/0.5.0/nameko-cachetools-0.5.0.tar.gz

---
+++ packages/nameko-cachetools/nameko-cachetools-0.5.0.tar.gz/nameko-cachetools-0.5.0/nameko_cachetools/__init__.py
@@ -0,0 +1,6 @@
+from .nameko_cachetools import CachedRpcProxy, CacheFirstRpcProxy
+
+__author__ = 'Santiago Suarez Ordonez'
+__email__ = 'santiycr@gmail.com'
+__version__ = '0.5.0'
+__all__ = ['CachedRpcProxy', 'CacheFirstRpcProxy']
---
+++ packages/nameko-cachetools/nameko-cachetools-0.5.0.tar.gz/nameko-cachetools-0.5.0/setup.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+
+import os
+import sys
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+
+if sys.argv[-1] == 'publish':
+    os.system('python setup.py sdist upload')
+    sys.exit()
+
+readme = open('README.rst').read()
+doclink = """
+Documentation
+-------------
+
+The full documentation is at http://nameko-cachetools.rtfd.org."""
+history = open('HISTORY.rst').read().replace('.. :changelog:', '')
+
+setup(
+    name='nameko-cachetools',
+    version='0.5.0',
+    description=(
+        'A few tools to cache interactions between your nameko services,'
+        ' increasing resiliency and performance at the expense of consistency,'
+        ' when it makes sense.'),
+    long_description=readme + '\n\n' + doclink + '\n\n' + history,
+    author='Santiago Suarez Ordonez',
+    author_email='santiycr@gmail.com',
+    url='https://github.com/santiycr/nameko-cachetools',
+    packages=[
+        'nameko_cachetools',
+    ],
+    package_dir={'nameko_cachetools': 'nameko_cachetools'},
+    include_package_data=True,
+    install_requires=[
+        'nameko>=2.9.0',
+    ],
+    license='MIT',
+    zip_safe=False,
+    keywords='nameko-cachetools nameko cachetools cache rpc',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: Implementation :: PyPy',
+    ],
+)
