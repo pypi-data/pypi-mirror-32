a033afe69e36ba9af560275dd1c0217b68b498d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:51:00 +0000

    Add package tinycards/0.21/tinycards-0.21.tar.gz

---
+++ packages/tinycards/tinycards-0.21.tar.gz/tinycards-0.21/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+NAME = 'tinycards'
+
+requirements = list(open('requirements.txt', 'r').readlines())
+
+setup(
+    name=NAME,
+    version='0.21',
+    description="An unofficial Python API for Tinycards by Duolingo",
+    url='https://github.com/floscha/tinycards-python-api',
+    author='Florian Schäfer',
+    author_email='florian.joh.schaefer@gmail.com',
+    license='MIT',
+    packages=find_packages(),
+    install_requires=requirements,
+    zip_safe=False,
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3 :: Only',
+        'Topic :: Software Development'
+    ]
+)
