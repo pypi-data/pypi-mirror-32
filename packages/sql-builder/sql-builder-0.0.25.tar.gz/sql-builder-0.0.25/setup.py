da20f5222d6fc619689fd842fc8df2722925359d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:33:36 +0000

    Add package sql-builder/0.0.25/sql-builder-0.0.25.tar.gz

---
+++ packages/sql-builder/sql-builder-0.0.25.tar.gz/sql-builder-0.0.25/setup.py
@@ -0,0 +1,35 @@
+# coding: utf-8
+# Author: Allen Zou
+# 2017/4/15 下午10:13
+
+from setuptools import setup, find_packages
+from sql_builder import __version__
+
+setup(
+    name='sql-builder',
+    version=".".join(str(x) for x in __version__),
+    description='A builder for SQL statement',
+    url='https://github.com/it-allen/sql-builder',
+    author='Allen Zou',
+    author_email='zyl_work@163.com',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+    keywords='sql',
+    # packages=['sql_builder']
+    packages=find_packages(exclude=["tests"]),
+    install_requires=[
+        "six>=1.11.0"
+    ]
+)
