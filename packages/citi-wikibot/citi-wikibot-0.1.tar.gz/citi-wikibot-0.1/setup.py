506c1536a4e8d911beea77fbec93aaed49f7a931
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:27:04 +0000

    Add package citi-wikibot/0.1/citi-wikibot-0.1.tar.gz

---
+++ packages/citi-wikibot/citi-wikibot-0.1.tar.gz/citi-wikibot-0.1/setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+setup(
+  name = 'citi-wikibot',
+  packages = ['wikibot'],
+  version = '0.1',
+  description = 'CITI\'s Wiki page editing for lazy people.',
+  author = 'Vanessa Barreiros',
+  author_email = 'vanessa.barreiros@citi.org.br',
+  url = 'https://github.com/citi-ufpe/citi-wikibot',
+  download_url = 'https://github.com/CITi-UFPE/citi-wikibot/archive/0.1.tar.gz',
+  keywords = ['wiki', 'bot', 'wikibot', 'citi'],
+  classifiers = [],
+  license = 'MIT'
+)
