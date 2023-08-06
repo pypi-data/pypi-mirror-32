d31ac3223caccb58d3a4ef386dc9669f93b8c1af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:33 +0000

    Add package stackby/0.2/stackby-0.2.tar.gz

---
+++ packages/stackby/stackby-0.2.tar.gz/stackby-0.2/stackby/stackby.py
@@ -0,0 +1,33 @@
+#! /usr/bin/env python3
+
+import fire
+from os import getcwd
+from scripts import sb_date, sb_type, sb_ext, sb_undo
+
+class StackBy:
+  """ Fire Class for StackBy Operations """
+  def __init__(self):
+    print("StackBy v0.01 Alpha")
+
+
+  """ Method to revert stacking of files """
+  def undo(self, dir = getcwd()):
+    sb_undo.undo(dir)
+
+  """ Function to stack the given directory based on extensions"""
+  def ext(self, dir = getcwd()):
+   sb_ext.stackby_extension(dir)
+    
+  """ Function to stack files based on type of predetermined filetypes """
+  def type(self, dir = getcwd()):
+    sb_type.stackby_type(dir)
+            
+  """ Function to stack files based on created date """
+  def date(self, dir = getcwd()):
+    sb_date.stackby_date(dir)
+
+def main():
+  fire.Fire(StackBy)
+
+if __name__ == '__main__':
+  main()
