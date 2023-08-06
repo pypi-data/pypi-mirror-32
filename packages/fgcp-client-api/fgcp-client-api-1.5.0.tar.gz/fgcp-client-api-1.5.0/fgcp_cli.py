6ed67b4fa4bdb82a40872cf629abdc6ebc1a5dec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:34:44 +0000

    Add package fgcp-client-api/1.5.0/fgcp-client-api-1.5.0.tar.gz

---
+++ packages/fgcp-client-api/fgcp-client-api-1.5.0.tar.gz/fgcp-client-api-1.5.0/fgcp_cli.py
@@ -0,0 +1,211 @@
+#!/usr/bin/python2.7
+from __future__ import print_function
+from builtins import input
+from builtins import str
+from builtins import zip
+from builtins import object
+import os
+import sys
+from fgcp.resource import FGCPVDataCenter
+import json
+
+
+class FGCP_CLI(object):
+
+    def __init__(self, key_file='client.pem', region='uk'):
+        self.key_file = key_file
+        self.region = region
+        self.verbose = False
+        #self.debug = False
+        #self.wait = True
+        self.wait = False
+        if self.wait:
+            self.verbose = True
+        self.vdc = None
+        self.design = None
+        self.vsystem = None
+        self.vsysName = None
+
+    def show_usage(self, argv):
+        print(argv[0], '<vsysName> <vserverName> status|start|stop|restart')
+
+    def run(self, argv):
+        print(argv)
+        if len(argv) < 2:
+            self.show_usage(argv)
+            return self.list_vsystems()
+        #if os.path.isfile(argv[1]):
+        #   return self.load(argv[1])
+        if len(argv) < 3:
+            return self.list_vservers(argv[1])
+        vserver = self.get_vserver(argv[1], argv[2])
+        status = vserver.status()
+        if len(argv) < 4:
+            print(status)
+            result = vserver
+        elif argv[3] == 'status':
+            result = status
+        elif argv[3] == 'start':
+            result = vserver.start(self.wait)
+        elif argv[3] == 'stop':
+            result = vserver.stop(self.wait)
+        elif argv[3] == 'restart':
+            result = vserver.reboot(self.wait)
+        elif argv[3] == 'halt':
+            result = vserver.stop(self.wait, force=True)
+        else:
+            print('Invalid option %s' % argv[3])
+            result = vserver
+        return result
+
+    def dump(self, var):
+        design = self.get_design()
+        return json.dumps(var, default=lambda o: design.to_var(o), indent=4)
+
+    def get_design(self):
+        if not self.design:
+            self.design = self.get_vdc().get_vsystem_design()
+        return self.design
+
+    def get_vdc(self):
+        if not self.vdc:
+            self.vdc = FGCPVDataCenter(self.key_file, self.region, self.verbose)
+        return self.vdc
+
+    def list_vsystems(self):
+        return self.get_vdc().list_vsystems()
+
+    def get_vsystem(self, vsysName):
+        if not self.vsystem or vsysName != self.vsysName:
+            self.vsystem = self.get_vdc().get_vsystem(vsysName)
+            self.vsysName = vsysName
+        return self.vsystem
+
+    def list_vservers(self, vsysName=None):
+        return self.get_vsystem(vsysName).list_vservers()
+
+    def get_vserver(self, vsysName=None, vserverName=None):
+        return self.get_vsystem(vsysName).get_vserver(vserverName)
+
+
+class FGCP_Menu(FGCP_CLI):
+
+    def loop(self, obj=None, depth=0):
+        if not obj:
+            obj = self.get_vdc()
+        print(self.dump(obj))
+        while True:
+            print(depth, type(obj).__name__)
+            method = self.select_method(obj)
+            if not method:
+                break
+            result = self.run_method(obj, method)
+            print(self.dump(result))
+            item = self.select_result(obj, method, result)
+            if not item:
+                continue
+            #print self.dump(item)
+            self.loop(item, depth + 1)
+
+    def select_method(self, obj, default='1'):
+        if not type(obj).__name__.startswith('FGCP'):
+            return
+        idx = 1
+        selected = {}
+        #methodlist = sorted(self.get_methods(obj, 'list_'))
+        methodlist = sorted(self.get_methods(obj))
+        for method in methodlist:
+            selected[str(idx)] = method
+            print('%s. %s' % (idx, method))
+            idx += 1
+        print('%s. %s' % (0, 'Return'))
+        if len(methodlist) < 1:
+            default = '0'
+        idx = input('Please select %s method [%s] ' % (type(obj).__name__, default))
+        if not idx:
+            idx = default
+        if idx in selected:
+            return selected[idx]
+        return
+
+    def run_method(self, obj, method):
+        params = self.get_method_params(obj, method)
+        if not params:
+            return getattr(obj, method)()
+        print(params)
+        return getattr(obj, method)(*params)
+
+    def get_method_params(self, obj, method):
+        func = getattr(obj, method)
+        argcount = func.__code__.co_argcount
+        if argcount < 2:
+            return
+        args = list(func.__code__.co_varnames[:argcount])
+        defaults = func.__defaults__ or ()
+        values = dict(list(zip(reversed(args), reversed(defaults))))
+        args.pop(0)
+        params = []
+        for arg in args:
+            if arg in values:
+                val = input('%s [%s] ' % (arg, values[arg]))
+                if val:
+                    params.append(val)
+                #if not val:
+                #   val = values[arg]
+            else:
+                val = input('%s ' % arg)
+                params.append(val)
+            #params.append({arg: val})
+        return params
+
+    def select_result(self, obj, method, result):
+        if not isinstance(result, list):
+            if type(result).__name__.startswith('FGCP'):
+                return result
+            return
+        if len(result) < 1:
+            return
+        if not method.startswith('list_'):
+            return
+        get_method = method.replace('list_', 'get_')[:-1]
+        if not hasattr(obj, get_method):
+            return
+        print(get_method)
+        idx = 1
+        selected = {}
+        for item in result:
+            if hasattr(item, 'getid'):
+                id = item.getid()
+            else:
+                id = item
+            selected[str(idx)] = id
+            print('%s. %s' % (idx, id))
+            idx += 1
+        default = '1'
+        print('%s. %s' % (0, 'Return'))
+        idx = input('Please select %s item [%s] ' % (type(result[0]).__name__, default))
+        if not idx:
+            idx = default
+        if idx in selected:
+            return getattr(obj, get_method)(selected[idx])
+        return
+
+    def get_methods(self, obj, prefix=None):
+        # check for local class methods here, not any inherited ones
+        #attrlist = dir(obj)
+        attrlist = list(type(obj).__dict__.keys())
+        if not prefix:
+            return [attr for attr in attrlist if not attr.startswith('_') and callable(getattr(obj, attr))]
+        return [attr for attr in attrlist if attr.startswith(prefix) and callable(getattr(obj, attr))]
+
+
+if __name__ == "__main__":
+    key_file = 'client.pem'
+    region = 'de'
+    if len(sys.argv) < 2:
+        menu = FGCP_Menu(key_file, region)
+        menu.loop()
+        sys.exit()
+    cli = FGCP_CLI(key_file, region)
+    result = cli.run(sys.argv)
+    print(cli.dump(result))
---
+++ packages/fgcp-client-api/fgcp-client-api-1.5.0.tar.gz/fgcp-client-api-1.5.0/setup.py
@@ -0,0 +1,212 @@
+from __future__ import print_function
+# http://www.packtpub.com/article/writing-a-package-in-python
+#from builtins import str
+from future import standard_library
+standard_library.install_aliases()
+from setuptools import setup, Command
+
+
+# adapted from Apache libcloud setup.py
+class DocsCommand(Command):
+    description = "generate API documentation"
+    user_options = []
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        import os
+        import pydoc
+        todo = []
+        modlist = ['fgcp', 'fgcp.libcloud', 'tests']
+        for curmod in modlist:
+            pydoc.writedoc(curmod)
+            todo.append(curmod)
+            for root, dirs, files in os.walk(curmod):
+                for name in files:
+                    if name.startswith('__'):
+                        continue
+                    if not name.endswith('.py'):
+                        continue
+                    #modname = '%s.%s' % (curmod, name.replace('.py', ''))
+                    filepath = os.path.join(root, name)
+                    modname = filepath.replace('/', '.').replace('\\', '.').replace('.py', '')
+                    pydoc.writedoc(modname)
+                    todo.append(modname)
+
+        import re
+
+        # replace file:/// link with link to google code
+        def clean_link(match):
+            parts = match.group(1).split('/')
+            file = parts.pop()
+            dir = parts.pop()
+            #return '"http://code.google.com/p/fgcp-client-api/source/browse/%s/%s"' % (dir, file)
+            #return '"https://github.com/mikespub/fgcp-client-api/tree/master/%s/%s"' % (dir, file)
+            return '"https://github.com/mikespub/fgcp-client-api/blob/master/%s/%s"' % (dir, file)
+        p1 = re.compile(r'"(file:[^"]+)"')
+
+        # replace c:\... file with local file
+        def clean_file(match):
+            name = match.group(1).split('\\').pop()
+            return '>%s<' % name
+        p2 = re.compile(r'>(\w:[^<]+)<')
+        for modname in todo:
+            filename = modname + '.html'
+            if not os.path.exists(filename):
+                continue
+            print(filename)
+            f = open(filename)
+            lines = f.read()
+            f.close()
+            lines = p1.sub(clean_link, lines)
+            lines = p2.sub(clean_file, lines)
+            # write new file in docs
+            f = open(os.path.join('docs', filename), 'w')
+            f.write(lines)
+            f.close()
+
+        # get latest version of project pages
+        self.get_project_pages('fgcp-client-api', ['ClientMethods', 'ResourceActions', 'APICommands', 'ClassDiagrams', 'TestServer', 'RelayServer', 'LibcloudDriver', 'REST_API'], modlist)
+
+    def get_project_pages(self, project, wikilist, modlist):
+        footerlinks = []
+        #pages = {'index.html': 'http://code.google.com/p/%s/' % project}
+        pages = {'index.html': 'https://github.com/mikespub/%s/wiki' % project}
+        # add links to project pages
+        for file in pages:
+            footerlinks.append('<a href="%s">%s</a>' % (file, file.replace('.html', '')))
+        wikipages = {}
+        wikireplace = {}
+        # define wiki pages + add links to them
+        for wiki in wikilist:
+            file = '%s.html' % wiki
+            #url = 'http://code.google.com/p/%s/wiki/%s' % (project, wiki)
+            #link = '/p/%s/wiki/%s' % (project, wiki)
+            url = 'https://github.com/mikespub/%s/wiki/%s' % (project, wiki)
+            link = '%s' % wiki
+            wikipages[file] = url
+            wikireplace[link] = file
+            #wikireplace[link] = link
+            #footerlinks.append('<a href="%s">%s</a>' % (file, wiki))
+            footerlinks.append('<a href="%s">%s</a>' % (wiki, wiki))
+        # add links to module documentation
+        for mod in modlist:
+            footerlinks.append('<a href="%s">pydoc %s</a>' % ('%s.html' % mod, mod))
+        # build footer
+        footer = '<p>Content: %s</p></body></html>' % '&nbsp;&nbsp;'.join(footerlinks)
+        # get project pages
+        for file in pages:
+            #self.get_html(file, pages[file], '<td id="wikicontent" class="psdescription">', '</td>', wikireplace, footer)
+            self.get_html(file, pages[file], '<div class="markdown-body">', '</div>', wikireplace, footer)
+        # get wiki pages
+        for file in wikipages:
+            #self.get_html(file, wikipages[file], '<div class="vt" id="wikimaincol">', '</div>', wikireplace, footer)
+            self.get_html(file, wikipages[file], '<div class="markdown-body">', '</div>', wikireplace, footer)
+
+    def get_html(self, file, url, start_seq='<body>', end_seq='</body>', links={}, footer='<br /></body></html>'):
+        print(file)
+        #import urllib.request, urllib.error, urllib.parse
+        import urllib.request
+        f = urllib.request.urlopen(url)
+        lines = f.read()
+        f.close()
+        # remove start_seq
+        m = lines.find(start_seq)
+        if m > 0:
+            m += len(start_seq)
+            lines = lines[m:]
+        # remove end_seq
+        m = lines.find(end_seq)
+        if m > 0:
+            lines = lines[:m]
+        lines += footer
+        # replace title
+        import re
+
+        def add_title(match):
+            title = match.group(1)
+            title = re.sub(r'<a [^>]+>.*</a>', '', title, flags=re.DOTALL)
+            #return '<html><head><title>%s</title></head><body><h1>%s</h1>' % (title, title)
+            return '<html><head><title>' + title + '</title></head><body><h1>' + title + '</h1>'
+        lines = re.sub(r'<h1>(.+)</h1>', add_title, lines, flags=re.DOTALL)
+        # replace links
+        for link in links:
+            lines = lines.replace(link, links[link])
+        # remove wiki/* links
+        lines = lines.replace('wiki/', '')
+        # write new file in docs
+        import os
+        f = open(os.path.join('docs', file), 'w')
+        f.write(lines)
+        f.close()
+
+
+class Pep8Command(Command):
+    description = "run pep8 script"
+    user_options = []
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        import sys
+        try:
+            import pep8
+            pep8
+        except ImportError:
+            print ('Missing "pep8" library. You can install it using pip: '
+                   'pip install pep8')
+            sys.exit(1)
+
+        import os
+        import subprocess
+        #cwd = os.getcwd()
+        cwd = '.'
+        retcode = subprocess.call((r'C:\Python27\Scripts\pycodestyle --show-source --ignore=E501,E265 --filename=*.py %s/fgcp/ %s/tests/ %s/fgcp_demo.py %s/fgcp_cli.py' %
+                                  (cwd, cwd, cwd, cwd)).split(' '))
+        sys.exit(retcode)
+
+
+f = open('README.txt')
+long_description = f.read()
+f.close()
+
+setup(
+    name='fgcp-client-api',
+    description='Client API Library for the Fujitsu Global Cloud Platform (FGCP)',
+    version='1.5.0',
+    author='mikespub',
+    author_email='fgcp@mikespub.net',
+    packages=['fgcp'],
+    license='Apache License 2.0',
+    url='https://github.com/mikespub/fgcp-client-api',
+    long_description=long_description,
+    entry_points={
+        'distutils.commands': [
+            'docs = DocsCommand'
+        ]
+    },
+    cmdclass={
+        'docs': DocsCommand,
+        'pep8': Pep8Command,
+    },
+    classifiers=[
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Environment :: Console',
+        'Environment :: Web Environment',
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: System Administrators',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.6',
+    ]
+)
