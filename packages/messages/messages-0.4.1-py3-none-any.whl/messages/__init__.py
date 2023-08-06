347106e5873bfa7fc06cf0b96cb1631ffae3309b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:23 +0000

    Add package messages/0.4.1/messages-0.4.1-py3-none-any.whl

---
+++ packages/messages/messages-0.4.1-py3-none-any.whl/messages/__init__.py
@@ -0,0 +1,38 @@
+"""
+messages - A package designed to make sending various types of messages easy.
+"""
+
+from .api import send
+
+from .email_ import Email
+from .slack import SlackWebhook
+from .slack import SlackPost
+from .telegram import TelegramBot
+from .text import Twilio
+
+
+__version__ = '0.4.1'
+
+
+MESSAGES = {
+    'email': {
+        'defaults': ['from_', 'server', 'port',],
+        'credentials': ['password']
+    },
+    'slackwebhook': {
+        'defaults': ['from_', 'url'],
+        'credentials': []
+    },
+    'slackpost': {
+        'defaults': ['channel'],
+        'credentials': ['token']
+    },
+    'telegrambot': {
+        'defaults': ['from_', 'chat_id'],
+        'credentials': ['bot_token']
+    },
+    'twilio': {
+        'defaults': ['from_', 'acct_sid'],
+        'credentials': ['auth_token']
+    },
+}
