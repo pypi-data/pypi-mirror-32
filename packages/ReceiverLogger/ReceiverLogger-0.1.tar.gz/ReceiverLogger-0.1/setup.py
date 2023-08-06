f3cf3c60e0e5eab7a579d52ef472c072bdce6157
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:49:19 +0000

    Add package ReceiverLogger/0.1/ReceiverLogger-0.1.tar.gz

---
+++ packages/ReceiverLogger/ReceiverLogger-0.1.tar.gz/ReceiverLogger-0.1/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+#setup(
+ #    name='demo_logger',
+  #   version='0.1',                          
+   #  scripts=['logger.py']                  
+#)
+
+setup(name='ReceiverLogger',
+  version='0.1',
+  description='logger_library',
+  author='shailesh1996',
+  author_email='shailesh199691@gmail.com',
+  scripts= ['logger.py']
+ )
