66a82c323dcc62c2de161befbb43703f2fbd51b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:55:43 +0000

    Add package infi.credentials_store/0.1.7/infi.credentials_store-0.1.7.tar.gz

---
+++ packages/infi.credentials_store/infi.credentials_store-0.1.7.tar.gz/infi.credentials_store-0.1.7/src/infi/credentials_store/__version__.py
@@ -0,0 +1,11 @@
+__version__ = "0.1.7"
+__git_commiter_name__ = "Arnon Yaari"
+__git_commiter_email__ = "arnony@infinidat.com"
+__git_branch__ = u'(HEAD detached at v0.1.7)'
+__git_remote_tracking_branch__ = '(No remote tracking)'
+__git_remote_url__ = '(Not remote tracking)'
+__git_head_hash__ = '2465c25916200150ab162e72e642efb10f785b4d'
+__git_head_subject__ = u'Finished Release v0.1.7'
+__git_head_message__ = u''
+__git_dirty_diff__ = u''
+__git_commit_date__ = '2018-06-10 11:43:29'
