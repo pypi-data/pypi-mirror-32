4a76fca274c5f399a460af42cd04b5db9c957bf7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:59:19 +0000

    Add package iml-sos-plugin/2.2.0/iml_sos_plugin-2.2.0.tar.gz

---
+++ packages/iml-sos-plugin/iml_sos_plugin-2.2.0.tar.gz/iml_sos_plugin-2.2.0/iml_sos_plugin/cli.py
@@ -0,0 +1,39 @@
+# Copyright (c) 2018 Intel Corporation. All rights reserved.
+# Use of this source code is governed by a MIT-style
+# license that can be found in the LICENSE file.
+
+import sys
+from subprocess import call
+from itertools import chain, imap
+
+
+def flat_map(fn, xs):
+    return chain.from_iterable(imap(fn, xs))
+
+
+def main():
+    args = sys.argv[1:]
+
+    plugins = [
+        "iml",
+        "corosync",
+        "pacemaker",
+        "kernel",
+        "pci",
+        "logs",
+        "processor",
+        "memory",
+        "multipath",
+        "filesys",
+        "block",
+        "yum",
+        "system",
+    ]
+
+    code = call(["sosreport"] + args + ["--batch", "--log-size=0"] +
+                list(flat_map(lambda x: ["--only", x], plugins)))
+
+    sys.exit(code)
+
+def chroma_diagnostics():
+    print "chroma-diagnostics no longer exists. Please use 'iml-diagnostics' instead."
