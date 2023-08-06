38ce0d2b8c6d9731372cc586a89844c10d142351
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:03 +0000

    Add package tsuru_dashboard/0.9.2/tsuru_dashboard-0.9.2.tar.gz

---
+++ packages/tsuru_dashboard/tsuru_dashboard-0.9.2.tar.gz/tsuru_dashboard-0.9.2/tsuru_dashboard/static/__tests__/pool.js
@@ -0,0 +1,78 @@
+import React from "react"
+import { mount } from "enzyme"
+import { Pool, PoolNode } from "../js/src/components/pool"
+import { RequestManager } from "../js/src/lib/request-manager"
+import sinon from "sinon"
+
+describe("Pool", () => {
+  let defaultAddRequest
+
+  beforeEach(() => {
+    defaultAddRequest = RequestManager.prototype.add
+    RequestManager.prototype.add = sinon.spy()
+  })
+
+  afterEach(() => {
+    RequestManager.prototype.add = defaultAddRequest
+  })
+
+  it("renders the pool nodes", () => {
+    const nodes = [
+      { address: "1.2.3.4", status: "running" },
+      { address: "5.6.7.8", status: "stopped" }
+    ]
+    const poolList = mount(
+      <Pool nodes={nodes} />
+    )
+    const poolNodes = poolList.find(PoolNode)
+
+    expect(poolNodes.length).toEqual(2)
+    expect(poolNodes.first().props()).toEqual(nodes[0])
+    expect(poolNodes.last().props()).toEqual(nodes[1])
+  })
+
+  it("fetches nodes info", () => {
+    const nodes = [
+      { address: "1.2.3.4", status: "running" },
+      { address: "5.6.7.8", status: "stopped" },
+      { address: "9.10.11.12", status: "running" }
+    ]
+    mount(<Pool nodes={nodes} />)
+
+    expect(RequestManager.prototype.add.callCount).toEqual(3)
+    expect(RequestManager.prototype.add.getCall(0).args[0].url).toEqual("/admin/1.2.3.4/containers/")
+    expect(RequestManager.prototype.add.getCall(1).args[0].url).toEqual("/admin/5.6.7.8/containers/")
+    expect(RequestManager.prototype.add.getCall(2).args[0].url).toEqual("/admin/9.10.11.12/containers/")
+  })
+
+  it("renders node info", () => {
+    const node = {
+      address: "1.2.3.4",
+      status: "running",
+      last_success: "2018-01-16 11:15",
+      units_stats: {
+        started: 5,
+        stopped: 1,
+        total: 6
+      }
+    }
+    const table = global.document.createElement("table")
+    global.document.body.appendChild(table)
+    const tbody = global.document.createElement("tbody")
+    table.appendChild(tbody)
+    const poolNode = mount(
+      <PoolNode address={node.address} status={node.status} />,
+      { attachTo: tbody }
+    )
+
+    poolNode.setState({ node: { info: node }, loading: false })
+    const cols = poolNode.find("td")
+    expect(cols.length).toEqual(6)
+    expect(cols.at(0).text()).toEqual("1.2.3.4")
+    expect(cols.at(1).text()).toEqual("5")
+    expect(cols.at(2).text()).toEqual("1")
+    expect(cols.at(3).text()).toEqual("6")
+    expect(cols.at(4).text()).toMatch(/^2018\/01\/16 11:15:00/)
+    expect(cols.at(5).text()).toEqual("running")
+  })
+})
