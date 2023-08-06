25cba3491253c116398817acaca3bf2ce0bde906
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:19:21 +0000

    Add package napalm-ros/0.3.4/napalm-ros-0.3.4.tar.gz

---
+++ packages/napalm-ros/napalm-ros-0.3.4.tar.gz/napalm-ros-0.3.4/setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup, find_packages
+
+__author__ = 'Matt Ryan <inetuid@gmail.com>'
+
+
+def parse_reqs(file_path):
+    with open(file_path, 'rt') as fobj:
+        lines = map(str.strip, fobj)
+        lines = filter(None, lines)
+        lines = filter(lambda x: x.startswith("#"), lines)
+        return tuple(lines)
+
+
+setup(
+    name="napalm-ros",
+    version="0.3.4",
+    packages=find_packages(),
+    author="Matt Ryan",
+    author_email="inetuid@gmail.com",
+    description="Network Automation and Programmability Abstraction Layer driver for Mikrotik ROS",
+    classifiers=[
+        'Topic :: Utilities',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Operating System :: POSIX :: Linux',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: MacOS',
+    ],
+    url="https://github.com/napalm-automation-community/napalm-ros",
+    include_package_data=True,
+    install_requires=parse_reqs('requirements.txt'),
+)
