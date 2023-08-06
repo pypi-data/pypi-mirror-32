68070f80883fc8e9a367d492de7c9c42d178e6e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:22:59 +0000

    Add package djocker/0.2.1/djocker-0.2.1.tar.gz

---
+++ packages/djocker/djocker-0.2.1.tar.gz/djocker-0.2.1/djocker/dockerize/handlers/compose.py
@@ -0,0 +1,28 @@
+from djocker.dockerize.handlers.base import BaseHandler
+from djocker.dockerize.utils.constants import (
+    CACHE_DEFAULT_PORT,
+    DATABASE_DEFAULT_CREDENTIALS,
+    DATABASE_DEFAULT_PORT,
+    DATABASE_ENV_VARS,
+    DATABASE_VOLUME_PATH,
+)
+
+
+class ComposeHandler(BaseHandler):
+    template_file = 'docker-compose.j2'
+    out_file = 'docker-compose.yml'
+
+    def handle(self):
+        data = {
+            'db_image': self.config.database_image,
+            'db_type': self.config.database_type,
+            'db_env_vars': DATABASE_ENV_VARS.get(self.config.database_type),
+            'db_port': DATABASE_DEFAULT_PORT.get(self.config.database_type),
+            'db_credentials': DATABASE_DEFAULT_CREDENTIALS,
+            'db_volume_path': DATABASE_VOLUME_PATH.get(self.config.database_type),
+            'cache_image': self.config.cache_image,
+            'cache_port': CACHE_DEFAULT_PORT.get(self.config.cache_type),
+            'python_version': self.config.python_version,
+        }
+
+        return self.write_template(data)
