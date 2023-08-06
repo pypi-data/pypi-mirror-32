339a59f45c3181087176d035b7ac4e776077d86d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:25:35 +0000

    Add package meshio/2.0.0/meshio-2.0.0-py2.py3-none-any.whl

---
+++ packages/meshio/meshio-2.0.0-py2.py3-none-any.whl/meshio/__init__.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+#
+from __future__ import print_function
+
+from .__about__ import __version__, __author__, __author_email__, __website__
+
+from .helpers import read, write, write_points_cells
+from .mesh import Mesh
+
+
+__all__ = [
+    "read",
+    "write",
+    "write_points_cells",
+    "Mesh",
+    "__version__",
+    "__author__",
+    "__author_email__",
+    "__website__",
+]
+
+try:
+    import pipdate
+except ImportError:
+    pass
+else:
+    if pipdate.needs_checking(__name__):
+        print(pipdate.check(__name__, __version__), end="")
