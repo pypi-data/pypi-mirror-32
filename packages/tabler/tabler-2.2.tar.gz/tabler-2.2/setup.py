64ccc3c9ad81d4a1bc4d360ecf2ed9ecb1447306
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:03 +0000

    Add package tabler/2.2/tabler-2.2.tar.gz

---
+++ packages/tabler/tabler-2.2.tar.gz/tabler-2.2/setup.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+"""Setup for tabler package."""
+
+import setuptools
+
+with open("README.rst", "r") as readme:
+    long_description = readme.read()
+
+setuptools.setup(
+    name="tabler",
+    version="2.2",
+    description="Simple interface for tabulated data and .csv files",
+    long_description=long_description,
+    url="https://github.com/axevalley/tabler.git",
+    author="Luke Shiner",
+    author_email="luke@lukeshiner.com",
+    keywords=["table", "csv", "simple"],
+    install_requires=[
+        "requests", "ezodf", "lxml", "openpyxl", "odswriter", "jinja2"],
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    python_requires='>=3.6.0',
+    classifiers=[
+        "Programming Language :: Python :: 3.6",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Development Status :: 4 - Beta",
+        "Natural Language :: English",
+        "Topic :: Utilities",
+        "Topic :: Other/Nonlisted Topic"],
+    )
