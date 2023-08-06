146b822445c2b21f77c605d43b7a061d78894582
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:14:39 +0000

    Add package eeapp/0.0.14/eeapp-0.0.14.tar.gz

---
+++ packages/eeapp/eeapp-0.0.14.tar.gz/eeapp-0.0.14/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="eeapp",
+    version="0.0.14",
+    author="zhuxietong",
+    author_email="zhuxietong@me.com",
+    description="A small example package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pypa/example-project",
+    packages=setuptools.find_packages(),
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+    install_requires=[
+        'django',
+        'djangorestframework',
+        'six'
+    ]
+)
