83ca6f73ebc30986d7a82bb9f01826d2e7a52aeb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:19:14 +0000

    Add package alignak_backend/1.4.10/alignak_backend-1.4.10.tar.gz

---
+++ packages/alignak_backend/alignak_backend-1.4.10.tar.gz/alignak_backend-1.4.10/alignak_backend/__init__.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+"""
+    Alignak REST backend
+
+    This module is an Alignak REST backend
+"""
+# Application version and manifest
+VERSION = (1, 4, 10)
+
+__application__ = u"Alignak_Backend"
+__short_version__ = '.'.join((str(each) for each in VERSION[:2]))
+__version__ = '.'.join((str(each) for each in VERSION[:4]))
+__author__ = u"Alignak team"
+__author_email__ = u"david.durieux@alignak.net"
+__copyright__ = u"(c) 2015-2017 - %s" % __author__
+__license__ = u"GNU Affero General Public License, version 3"
+__description__ = u"Alignak REST backend"
+__releasenotes__ = u"""Alignak REST Backend"""
+__git_url__ = "https://github.com/Alignak-monitoring-contrib/alignak-backend"
+__doc_url__ = "http://alignak-backend.readthedocs.org"
+
+__classifiers__ = [
+    'Development Status :: 5 - Production/Stable',
+    'Environment :: Console',
+    'Intended Audience :: Developers',
+    'Intended Audience :: System Administrators',
+    'License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)',
+    'Natural Language :: English',
+    'Programming Language :: Python',
+    'Topic :: System :: Monitoring',
+    'Topic :: System :: Systems Administration'
+]
+
+# Application manifest
+manifest = {
+    'name': __application__,
+    'version': __version__,
+    'author': __author__,
+    'description': __description__,
+    'copyright': __copyright__,
+    'license': __license__,
+    'release': __releasenotes__,
+    'doc': __doc_url__
+}
---
+++ packages/alignak_backend/alignak_backend-1.4.10.tar.gz/alignak_backend-1.4.10/setup.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import print_function
+import sys
+
+from importlib import import_module
+
+try:
+    from setuptools import setup, find_packages
+except:
+    sys.exit("Error: missing python-setuptools library")
+
+try:
+    python_version = sys.version_info
+except:
+    python_version = (1, 5)
+if python_version < (2, 7):
+    sys.exit("This application requires a minimum Python 2.7.x, sorry!")
+
+from alignak_backend import __application__, __version__, __copyright__
+from alignak_backend import __releasenotes__, __license__, __doc_url__, __git_url__
+from alignak_backend import __author__, __author_email__, __classifiers__
+from alignak_backend import __name__ as __pkg_name__
+
+package = import_module('alignak_backend')
+
+data_files = [('etc/alignak-backend',
+               ['etc/settings.json', 'etc/alignak-backend-logger.json', 'etc/uwsgi.ini',
+                'etc/grafana_queries.json', 'etc/grafana_tables.json', 'etc/alignak-backend.wsgi']),
+              ('bin',
+               ['bin/alignak-backend-uwsgi']),
+              ('var/log/alignak-backend', [])]
+if 'bsd' in sys.platform or 'dragonfly' in sys.platform:
+    data_files.append(('etc/rc.d', ['bin/rc.d/alignak-backend']))
+
+with open('README.rst') as f:
+    long_description = f.read()
+
+setup(
+    name=__pkg_name__,
+    version=__version__,
+
+    license=__license__,
+
+    # metadata for upload to PyPI
+    author=__author__,
+    author_email=__author_email__,
+    keywords="alignak monitoring backend",
+    url=__git_url__,
+    description=package.__doc__.strip(),
+    long_description=long_description,
+    long_description_content_type='',
+
+    classifiers = __classifiers__,
+
+    zip_safe=False,
+
+    packages=find_packages(),
+
+    # Where to install distributed files
+    data_files = data_files,
+
+    # Dependencies (if some) ...
+    # Set Flask dependency because of a forced dependency in Eve...
+    install_requires=[
+        'python-dateutil', 'flask-bootstrap', 'docopt', 'jsonschema',
+        'eve-swagger', 'configparser', 'future', 'influxdb', 'flask-apscheduler',
+        'werkzeug', 'flask', 'Eve', 'statsd'
+    ],
+
+    # Entry points (if some) ...
+    entry_points={
+        'console_scripts': [
+            'alignak-backend = alignak_backend.main:main'
+        ],
+    }
+)
