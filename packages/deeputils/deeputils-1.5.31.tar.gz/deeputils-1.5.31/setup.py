8729c9f127935df062c00501103c9c32204dd333
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:11:24 +0000

    Add package deeputils/1.5.31/deeputils-1.5.31.tar.gz

---
+++ packages/deeputils/deeputils-1.5.31.tar.gz/deeputils-1.5.31/setup.py
@@ -0,0 +1,18 @@
+from distutils.core import setup
+
+setup(
+    name='deeputils',
+    version='1.5.31',
+    packages=['deeputils'],
+    url='https://github.com/valency/deeputils',
+    download_url='https://github.com/valency/deeputils/releases/download/v1.5.31/deeputils-1.5.31.tar.gz',
+    license='CPL-3.0',
+    author='Deepera Co., Ltd.',
+    author_email='yding@deepera.com',
+    description='Deepera Development Utils',
+    keywords=['utils', 'logging'],
+    install_requires=[
+        'django',
+        'djangorestframework'
+    ],
+)
