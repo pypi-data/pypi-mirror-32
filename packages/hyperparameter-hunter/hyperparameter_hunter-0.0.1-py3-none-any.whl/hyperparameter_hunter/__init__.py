d66e96bf7dcea197f227a01ebc8d2c221c6cf173
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:36:35 +0000

    Add package hyperparameter-hunter/0.0.1/hyperparameter_hunter-0.0.1-py3-none-any.whl

---
+++ packages/hyperparameter-hunter/hyperparameter_hunter-0.0.1-py3-none-any.whl/hyperparameter_hunter/__init__.py
@@ -0,0 +1,45 @@
+##################################################
+# Nullify Excess Documentation
+##################################################
+from .importer import nullify_module_docstrings
+
+# try:
+#     importer.nullify_module_docstrings('hyperparameter_hunter.utils.boltons_utils')
+# except Exception:
+#     pass
+
+##################################################
+# Execute Import Interceptors
+##################################################
+# TODO: importer.hook_keras_layer
+
+
+##################################################
+# Set __all__
+##################################################
+from .environment import Environment
+from .experiments import CrossValidationExperiment
+from .optimization import BayesianOptimization
+from .optimization import GradientBoostedRegressionTreeOptimization
+from .optimization import RandomForestOptimization
+from .optimization import ExtraTreesOptimization
+from .optimization import DummySearch
+from .space import Real
+from .space import Integer
+from .space import Categorical
+
+__all__ = [
+    'Environment',
+
+    'CrossValidationExperiment',
+
+    'BayesianOptimization',
+    'GradientBoostedRegressionTreeOptimization',
+    'RandomForestOptimization',
+    'ExtraTreesOptimization',
+    'DummySearch',
+
+    'Real',
+    'Integer',
+    'Categorical',
+]
