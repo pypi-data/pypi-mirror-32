b64d2238ac41a3088a8ec7c8f16132b5cee28c52
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:24:49 +0000

    Add package spot-motion-monitor/0.1.1/spot_motion_monitor-0.1.1-py2.py3-none-any.whl

---
+++ packages/spot-motion-monitor/spot_motion_monitor-0.1.1-py2.py3-none-any.whl/spot_motion_monitor-0.1.1.data/scripts/smm_ui.py
@@ -0,0 +1,9 @@
+#!/home/travis/virtualenv/python3.6.3/bin/python
+#------------------------------------------------------------------------------
+# Copyright (c) 2018 LSST Systems Engineering
+# Distributed under the MIT License. See LICENSE for more information.
+#------------------------------------------------------------------------------
+import spot_motion_monitor.views as smmViews
+
+if __name__ == '__main__':
+    smmViews.main_window.main()
