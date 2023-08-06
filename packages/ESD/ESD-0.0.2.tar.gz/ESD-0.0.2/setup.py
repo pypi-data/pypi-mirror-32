5336759adce16aca5af1a9a6fc42da88139cdb36
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:08:58 +0000

    Add package ESD/0.0.2/ESD-0.0.2.tar.gz

---
+++ packages/ESD/ESD-0.0.2.tar.gz/ESD-0.0.2/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name='ESD',
+    version='0.0.2',
+    author='Feei',
+    author_email='feei@feei.cn',
+    description='Enumeration sub domains(枚举子域名)',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/FeeiCN/ESD',
+    packages=setuptools.find_packages(),
+    classifiers=(
+        "Topic :: Security",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)"
+    )
+)
