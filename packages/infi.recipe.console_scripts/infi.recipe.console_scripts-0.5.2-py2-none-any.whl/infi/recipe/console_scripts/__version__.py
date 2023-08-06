83f434c67aea99f9f50045137a6c3b023ff23326
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:55:53 +0000

    Add package infi.recipe.console_scripts/0.5.2/infi.recipe.console_scripts-0.5.2-py2-none-any.whl

---
+++ packages/infi.recipe.console_scripts/infi.recipe.console_scripts-0.5.2-py2-none-any.whl/infi/recipe/console_scripts/__version__.py
@@ -0,0 +1,11 @@
+__version__ = "0.5.2"
+__git_commiter_name__ = "Arnon Yaari"
+__git_commiter_email__ = "arnony@infinidat.com"
+__git_branch__ = u'(HEAD detached at v0.5.2)'
+__git_remote_tracking_branch__ = '(No remote tracking)'
+__git_remote_url__ = '(Not remote tracking)'
+__git_head_hash__ = 'ac46cae9dda98135e6981abddc45dccdca978a85'
+__git_head_subject__ = u'Finished Release v0.5.2'
+__git_head_message__ = u''
+__git_dirty_diff__ = u''
+__git_commit_date__ = '2018-06-10 12:39:25'
