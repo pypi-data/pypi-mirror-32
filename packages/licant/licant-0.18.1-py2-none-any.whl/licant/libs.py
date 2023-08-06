28da64aaf0eb6478f3a090267d161bef3cce45cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:57:15 +0000

    Add package licant/0.18.1/licant-0.18.1-py2-none-any.whl

---
+++ packages/licant/licant-0.18.1-py2-none-any.whl/licant/libs.py
@@ -0,0 +1,36 @@
+from licant.scripter import scriptq
+
+import os
+import sys
+import json
+
+gpath = "/etc/licant"
+lpath = os.path.expanduser("~/.config/licant")
+
+libs = None
+
+def merge_two_dicts(x, y):
+    z = x.copy()   # start with x's keys and values
+    z.update(y)    # modifies z with y's keys and values & returns None
+    return z
+
+def include(lib):
+	global libs
+	if libs == None:
+		glibs = {}
+		llibs = {}
+
+		if os.path.exists(gpath):
+			glibs = json.load(open(gpath))
+
+		if os.path.exists(lpath):
+			llibs = json.load(open(lpath))
+
+		libs =  merge_two_dicts(glibs, llibs)
+
+	if not lib in libs:
+		print("Unregistred library")
+		exit(-1)
+
+	#print(libs[lib])
+	scriptq.execute(libs[lib])	
