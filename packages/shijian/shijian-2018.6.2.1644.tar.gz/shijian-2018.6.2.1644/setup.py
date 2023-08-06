0d8f9fa3cd627bcb2487e2b82927d07d5e6c38ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:14:40 +0000

    Add package shijian/2018.6.2.1644/shijian-2018.6.2.1644.tar.gz

---
+++ packages/shijian/shijian-2018.6.2.1644.tar.gz/shijian-2018.6.2.1644/setup.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import os
+
+import setuptools
+
+def main():
+
+    setuptools.setup(
+        name             = "shijian",
+        version          = "2018.06.02.1644",
+        description      = "change, time, file, list, statistics, language and other utilities",
+        long_description = long_description(),
+        url              = "https://github.com/wdbm/shijian",
+        author           = "Will Breaden Madden",
+        author_email     = "wbm@protonmail.ch",
+        license          = "GPLv3",
+        py_modules       = [
+                           "shijian"
+                           ],
+        install_requires = [
+                           "ipywidgets",
+                           "pandas",
+                           "python-dateutil",
+                           "matplotlib",
+                           "numpy",
+                           "pyprel",
+                           "scipy",
+                           "seaborn",
+                           "sklearn",
+                           "subprocess32;python_version<'3.0'",
+                           "technicolor"
+                           ],
+        entry_points     = """
+                           [console_scripts]
+                           shijian = shijian:shijian
+                           """
+    )
+
+def long_description(
+    filename = "README.md"
+    ):
+
+    if os.path.isfile(os.path.expandvars(filename)):
+        try:
+            import pypandoc
+            long_description = pypandoc.convert_file(filename, "rst")
+        except ImportError:
+            long_description = open(filename).read()
+    else:
+        long_description = ""
+    return long_description
+
+if __name__ == "__main__":
+    main()
