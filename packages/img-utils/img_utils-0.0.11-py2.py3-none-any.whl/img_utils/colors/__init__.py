e86f04ba818d1643839cf2b8403dfd4f267a877d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:24:38 +0000

    Add package img-utils/0.0.11/img_utils-0.0.11-py2.py3-none-any.whl

---
+++ packages/img-utils/img_utils-0.0.11-py2.py3-none-any.whl/img_utils/colors/__init__.py
@@ -0,0 +1,6 @@
+# author:	dew.maple
+
+# set the version number
+__version__ = "0.0.5"
+
+from .color import random_colors
---
+++ packages/img-utils/img_utils-0.0.11-py2.py3-none-any.whl/img_utils/images/__init__.py
@@ -0,0 +1,11 @@
+# author:	dew.maple
+
+# set the version number
+__version__ = "0.0.11"
+from .geometry import centroid
+from .geometry import point_on_axis
+from .geometry import point_on_line_with_distance
+from .geometry import point_respect_line
+from .image import find_outer_corner
+from .image import images2video
+from .image import put_text
