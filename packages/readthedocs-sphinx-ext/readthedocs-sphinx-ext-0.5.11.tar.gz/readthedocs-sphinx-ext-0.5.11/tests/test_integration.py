644272ed76f625a564b50ec13dd3395189592938
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:01:04 +0000

    Add package readthedocs-sphinx-ext/0.5.11/readthedocs-sphinx-ext-0.5.11.tar.gz

---
+++ packages/readthedocs-sphinx-ext/readthedocs-sphinx-ext-0.5.11.tar.gz/readthedocs-sphinx-ext-0.5.11/tests/test_integration.py
@@ -0,0 +1,58 @@
+import unittest
+
+from readthedocs_ext.readthedocs import HtmlBuilderMixin
+
+from .util import sphinx_build, build_output
+
+
+class LanguageIntegrationTests(unittest.TestCase):
+
+    def _run_test(self, test_dir, test_file, test_string, builder='html'):
+        with build_output(test_dir, test_file, builder) as data:
+            self.assertIn(test_string, data)
+
+
+class IntegrationTests(LanguageIntegrationTests):
+
+    def test_integration(self):
+        self._run_test(
+            'pyexample',
+            '_build/readthedocs/index.html',
+            'Hey there friend!',
+            builder='readthedocs',
+        )
+
+    def test_media_integration(self):
+        self._run_test(
+            'pyexample',
+            '_build/readthedocs/index.html',
+            'media.readthedocs.org',
+            builder='readthedocs',
+        )
+
+    def test_included_js(self):
+        self._run_test(
+            'pyexample',
+            '_build/readthedocs/index.html',
+            'readthedocs-analytics.js',
+            builder='readthedocs',
+        )
+
+    def test_replacement_pattern(self):
+        pattern = HtmlBuilderMixin.REPLACEMENT_PATTERN
+        src = "$(document).ready(function() {\n  Search.init();\n});"
+        self.assertRegexpMatches(src, pattern)
+        # Minor changes to spacing, just to ensure rule is correct. This should
+        # never happen as this block of code is 10 years old
+        src = "$(document).ready(function    ()     {\n    Search.init();\n});"
+        self.assertRegexpMatches(src, pattern)
+
+    def test_searchtools_is_patched(self):
+        with build_output('pyexample', '_build/readthedocs/_static/searchtools.js',
+                          builder='readthedocs') as data:
+            self.assertNotIn('Search.init();', data)
+            self.assertNotRegexpMatches(
+                data,
+                HtmlBuilderMixin.REPLACEMENT_PATTERN
+            )
+            self.assertIn(HtmlBuilderMixin.REPLACEMENT_TEXT, data)
