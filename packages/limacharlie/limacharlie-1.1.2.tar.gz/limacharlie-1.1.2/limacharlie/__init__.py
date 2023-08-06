47c016b0ebf8e6115a5bfe244509ecef6730468c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:10:54 +0000

    Add package limacharlie/1.1.2/limacharlie-1.1.2.tar.gz

---
+++ packages/limacharlie/limacharlie-1.1.2.tar.gz/limacharlie-1.1.2/limacharlie/__init__.py
@@ -0,0 +1,12 @@
+"""limacharlie API for limacharlie.io"""
+
+__version__ = "1.1.2"
+__author__ = "Maxime Lamothe-Brassard ( Refraction Point, Inc )"
+__author_email__ = "maxime@refractionpoint.com"
+__license__ = "Apache v2"
+__copyright__ = "Copyright (c) 2018 Refraction Point, Inc"
+
+from .Manager import Manager
+from .Firehose import Firehose
+from .Hunter import Hunter
+from .utils import LcApiException
