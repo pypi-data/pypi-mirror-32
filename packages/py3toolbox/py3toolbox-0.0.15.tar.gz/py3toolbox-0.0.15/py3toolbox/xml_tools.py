cb8aea7727ecaf6c9fb832c5f08370334ca6fad5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:08:25 +0000

    Add package py3toolbox/0.0.15/py3toolbox-0.0.15.tar.gz

---
+++ packages/py3toolbox/py3toolbox-0.0.15.tar.gz/py3toolbox-0.0.15/py3toolbox/xml_tools.py
@@ -0,0 +1,55 @@
+import xml.etree.ElementTree as et
+
+def is_xml(xml_text) :
+  try :
+    x = et.fromstring(xml_text)
+    return True
+  except Exception as e:
+    return False  
+
+
+def pretty_xml(xml_file) :
+  xml_doc = et.parse(xml_file)
+  return (et.tostring(xml_doc, pretty_print=True).decode("utf-8")) 
+  
+  
+def parse_xml(xml_text) :
+  try :
+    xml_obj = et.fromstring(xml_text)
+    return xml_obj
+  except Exception as e:
+    return None  
+
+  
+def get_xml_element(xml_obj,xpath):
+  xml_element = None
+  xml_element = xml_obj.find(xpath)
+  if xml_element is not None : 
+    return xml_element.text
+  else :
+    return ""
+
+def get_xml_elements(xml_obj,xpath):
+  xml_values = []
+  xml_elements = xml_obj.findall(xpath)
+  for e in xml_elements:
+    xml_values.append(e.text)
+  return xml_values
+
+def get_xml_attributes(xml_obj,xpath, attrib):
+  xml_attributes = []
+  xml_elements = xml_obj.findall(xpath)
+  for e in xml_elements:
+    xml_attributes.append(e.get(attrib))
+  return xml_attributes
+
+  
+if __name__ == "__main__":
+  import fs_tools
+  xml = parse_xml(fs_tools.read_file('C:/TEMP/test.xml'))
+  print (xml)
+  #print (get_xml_element(xml, "./LearningProductOrganisationOffering/LearningProductOrganisationOffering-LearningProductOrganisationOfferingRelationshipList/LearningProductOrganisationOffering-LearningProductOrganisationOfferingRelationship/LearningProductOrganisationOfferingTo/LearningProduct/ProductType/Code"))
+  
+  print (get_xml_elements(xml, './country/rank'))
+  
+  pass    
