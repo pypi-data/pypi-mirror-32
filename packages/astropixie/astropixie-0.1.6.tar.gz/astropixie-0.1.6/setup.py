f69a621824e3da206fb494538498431c53cb9283
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:26:06 +0000

    Add package astropixie/0.1.6/astropixie-0.1.6.tar.gz

---
+++ packages/astropixie/astropixie-0.1.6.tar.gz/astropixie-0.1.6/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(name='astropixie',
+      version='0.1.6',
+      description='LSST EPO python library.',
+      url='https://github.com/lsst-epo/vela/astropixie',
+      author='LSST EPO Team',
+      author_email='epo-team@lists.lsst.org',
+      license='MIT',
+      packages=['astropixie'],
+      include_package_data=True,
+      package_data={'astropixie': ['sample_data/*']},
+      install_requires=[
+          'astropy>=3.0.1,<3.1',
+          'numpy>=1.14,<1.15',
+          'pandas>=0.23,<0.24',
+          'pytest>=3.5,<3.6'
+      ])
