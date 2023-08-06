2f322f62f32da5073f485212092dffe0346cf0d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:56:14 +0000

    Add package npr/2.3.0/npr-2.3.0.tar.gz

---
+++ packages/npr/npr-2.3.0.tar.gz/npr-2.3.0/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup
+from setuptools import find_packages
+import os
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(name='npr',
+      version='2.3.0',
+      description='NPR cloud framework',
+      long_description=read('README.rst'),
+      classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Internet :: WWW/HTTP',
+      ],
+      url='http://github.com/perrydc/npr',
+      author='Demian Perry',
+      author_email='dperry@npr.org',
+      license='MIT',
+      install_requires=[
+          'requests','future','requests[security];python_version<"2.9"','algoliasearch',
+      ],
+      packages=find_packages(exclude=['tests*']),
+      keywords='public, radio, stream, metadata, api, service, npr')
