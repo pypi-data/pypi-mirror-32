998dccb8cc9d7aed50a195e74ee9053a28b879d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:22:47 +0000

    Add package guillotina-prometheus/1.0.7/guillotina_prometheus-1.0.7.tar.gz

---
+++ packages/guillotina-prometheus/guillotina_prometheus-1.0.7.tar.gz/guillotina_prometheus-1.0.7/setup.py
@@ -0,0 +1,35 @@
+from setuptools import find_packages
+from setuptools import setup
+
+
+try:
+    README = open('README.rst').read() + '\n\n' + open('CHANGELOG.rst').read()
+except IOError:
+    README = None
+
+setup(
+    name='guillotina_prometheus',
+    version='1.0.7',
+    description='prometheus integration into guillotina',
+    long_description=README,
+    install_requires=[
+        'guillotina>=2.1.5',
+        'prometheus_client'
+    ],
+    author='Nathan Van Gheem',
+    author_email='vangheem@gmail.com',
+    url='https://github.com/guillotinaweb/guillotina_prometheus',
+    packages=find_packages(exclude=['demo']),
+    include_package_data=True,
+    tests_require=[
+        'pytest',
+    ],
+    extras_require={
+        'test': [
+            'pytest'
+        ]
+    },
+    classifiers=[],
+    entry_points={
+    }
+)
