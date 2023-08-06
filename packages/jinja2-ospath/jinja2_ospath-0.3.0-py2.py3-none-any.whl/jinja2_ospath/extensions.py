be679daaabdbe975431b25c5b452e999a0190871
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:37:37 +0000

    Add package jinja2-ospath/0.3.0/jinja2_ospath-0.3.0-py2.py3-none-any.whl

---
+++ packages/jinja2-ospath/jinja2_ospath-0.3.0-py2.py3-none-any.whl/jinja2_ospath/extensions.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+
+"""
+Extensions that introduce `basename` and `dirname` as Jinja2 filters.
+
+Examples
+--------
+
+    my_path = "/some/absolute/path/with/file.txt"
+    {{ my_path | basename }}
+
+Will fill in `file.txt`.
+"""
+
+from __future__ import absolute_import
+
+import os.path
+
+from jinja2.ext import Extension
+
+__all__ = ("OSPathExtension",)
+
+
+class OSPathExtension(Extension):
+    """A Jinja2 extension that introduces `os.path` functionality."""
+
+    tags = frozenset(["basename", "dirname", "abspath"])
+
+    def __init__(self, environment):
+        """Initialize the extension and prepare the Jinja2 environment."""
+        super(OSPathExtension, self).__init__(environment)
+        for name in self.tags:
+            environment.filters[name] = getattr(os.path, name)
