73f161fdf90c82b0e0fa43a41b07f7a02fd1965f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:28:01 +0000

    Add package collective.pwexpiry/0.12.0/collective.pwexpiry-0.12.0.zip

---
+++ packages/collective.pwexpiry/collective.pwexpiry-0.12.0.zip/collective.pwexpiry-0.12.0/collective/pwexpiry/tests/test_doctests.py
@@ -0,0 +1,19 @@
+import doctest
+
+import unittest
+from collective.pwexpiry.tests import base
+from plone.testing import layered
+from Testing import ZopeTestCase as ztc
+
+
+def test_suite():
+    suite = unittest.TestSuite()
+    suite.addTests([
+        layered(doctest.DocFileSuite(
+                'tests/test.txt',
+#                'tests/test2.txt',
+                package='collective.pwexpiry',
+                optionflags=doctest.NORMALIZE_WHITESPACE | doctest.ELLIPSIS),
+                layer=base.INTEGRATION_TESTING),
+        ])
+    return suite
---
+++ packages/collective.pwexpiry/collective.pwexpiry-0.12.0.zip/collective.pwexpiry-0.12.0/setup.py
@@ -0,0 +1,54 @@
+from setuptools import setup, find_packages
+import os
+
+
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+
+version = '0.12.0'
+
+setup(name='collective.pwexpiry',
+      version=version,
+      description="Emulate Active Directory password complexity requirements \
+                  in Plone",
+      long_description=read("README.rst") + "\n" + read("CHANGES.txt"),
+      classifiers=[
+          "Programming Language :: Python",
+          "Topic :: Software Development :: Libraries :: Python Modules",
+          "Topic :: Security",
+          "Framework :: Plone",
+          "Framework :: Plone :: 4.1",
+          "Framework :: Plone :: 4.2",
+          "Framework :: Plone :: 4.3",
+          "License :: OSI Approved :: Zope Public License",
+      ],
+      keywords='',
+      author='Enfold Systems, Inc.',
+      author_email='info@enfoldsystems.com',
+      url='http://www.enfoldsystems.com',
+      license='ZPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['collective'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          # -*- Extra requirements: -*-
+          'plone.api',
+          'collective.monkeypatcher',
+      ],
+      extras_require={
+          "test": [
+              "Plone",
+              "plone.app.testing",
+          ],
+      },
+      entry_points="""
+      # -*- Entry points: -*-
+      [zopectl.command]
+      notify_and_expire = collective.pwexpiry.scripts.notify_and_expire:entrypoint
+
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      )
