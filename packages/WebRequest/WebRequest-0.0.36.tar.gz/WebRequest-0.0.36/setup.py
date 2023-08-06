4b65d5e5a835ee5f30b1bd90215bb8c3bf79bf2c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:55:32 +0000

    Add package WebRequest/0.0.36/WebRequest-0.0.36.tar.gz

---
+++ packages/WebRequest/WebRequest-0.0.36.tar.gz/WebRequest-0.0.36/setup.py
@@ -0,0 +1,40 @@
+
+import setuptools
+from distutils.core import setup
+import sys
+import time
+
+install_requires = open("requirements.txt").readlines()
+install_requires = [tmp.strip() for tmp in install_requires]
+
+setup(
+	# Application name:
+	name="WebRequest",
+
+	# Version number (initial):
+	version="0.0.36",
+
+	# Application author details:
+	author="Connor Wolf",
+	author_email="github@imaginaryindustries.com",
+
+	# Packages
+	packages=setuptools.find_packages(),
+	package_dir = {'WebRequest': 'WebRequest'},
+
+
+	# Details
+	url="https://github.com/fake-name/WebRequest",
+
+	#
+	# license="LICENSE.txt",
+	description="Like `requests`, but shittier.",
+
+	long_description              = open("README.md").read(),
+	long_description_content_type = "text/markdown",
+
+	package_data={'': ['requirements.txt']},
+
+	# Dependent packages (distributions)
+	install_requires=install_requires,
+)
