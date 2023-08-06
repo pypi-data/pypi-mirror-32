997a50a8865351a1e6ec57b8c7688f9e9c87418d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:16:58 +0000

    Add package apscheduler-bundle/1.1.2rc0/apscheduler_bundle-1.1.2rc0.tar.gz

---
+++ packages/apscheduler-bundle/apscheduler_bundle-1.1.2rc0.tar.gz/apscheduler_bundle-1.1.2rc0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+setup(
+    name='apscheduler_bundle',
+    packages=['apscheduler_bundle'],
+    version='1.1.2-rc',
+    description='APScheduler support for applauncher',
+    author='Alvaro Garcia Gomez',
+    author_email='maxpowel@gmail.com',
+    url='https://github.com/applauncher-team/apscheduler_bundle',
+    download_url='https://github.com/applauncher-team/apscheduler_bundle',
+    keywords=['applauncher', 'apscheduler', 'scheduler'],
+    classifiers=['Topic :: Adaptive Technologies', 'Topic :: Software Development', 'Topic :: System',
+                 'Topic :: Utilities'],
+    install_requires=['applauncher', 'apscheduler']
+)
