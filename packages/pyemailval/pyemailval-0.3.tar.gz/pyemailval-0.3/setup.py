81808013d0be963dc5746c3d3345e5517ab561a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:13:34 +0000

    Add package pyemailval/0.3/pyemailval-0.3.tar.gz

---
+++ packages/pyemailval/pyemailval-0.3.tar.gz/pyemailval-0.3/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="pyemailval",
+    version="0.3",
+    py_modules=("pyemailval",),
+    author="Ben Baert",
+    author_email="ben_b@gmx.com",
+    description="pyemailval verifies if an email address really exists.",
+    long_description=open("README.rst").read(),
+    keywords="email validation verification mx verify",
+    url="http://github.com/ben-baert/pyemailval",
+    download_url="http://github.com/ben-baert/pyemailval/archive/0.1.tar.gz",
+    license="LGPL",
+)
