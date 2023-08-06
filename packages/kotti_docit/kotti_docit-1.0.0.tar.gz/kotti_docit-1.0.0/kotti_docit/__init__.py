4465674f22ae754726aaa71fad10201f06fce37b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:19 +0000

    Add package kotti_docit/1.0.0/kotti_docit-1.0.0.tar.gz

---
+++ packages/kotti_docit/kotti_docit-1.0.0.tar.gz/kotti_docit-1.0.0/kotti_docit/__init__.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+
+"""
+Created on 2016-09-20
+:author: Oshane Bailey (oshane@alteroo.com)
+"""
+
+from kotti.resources import File, Image, Document
+from pyramid.i18n import TranslationStringFactory
+
+from kotti_pdf.resources import PDF
+from kotti_contenttypes.resources import Folder
+
+_ = TranslationStringFactory('kotti_docit')
+
+
+def kotti_configure(settings):
+    """ Add a line like this to you .ini file::
+
+            kotti.configurators =
+                kotti_docit.kotti_configure
+
+        to enable the ``kotti_docit`` add-on.
+
+    :param settings: Kotti configuration dictionary.
+    :type settings: dict
+    """
+
+    settings['pyramid.includes'] += ' kotti_docit'
+    settings['kotti.alembic_dirs'] += ' kotti_docit:alembic'
+    settings['kotti.available_types'] += ' kotti_docit.resources.AdminManual'
+    settings['kotti.fanstatic.view_needed'] += ' kotti_docit.fanstatic.css_and_js'
+    File.type_info.addable_to.append('AdminManual')
+    Image.type_info.addable_to.append('AdminManual')
+    PDF.type_info.addable_to.append('AdminManual')
+    Document.type_info.addable_to.append('AdminManual')
+    Folder.type_info.addable_to.append('AdminManual')
+
+
+def includeme(config):
+    """ Don't add this to your ``pyramid_includes``, but add the
+    ``kotti_configure`` above to your ``kotti.configurators`` instead.
+
+    :param config: Pyramid configurator object.
+    :type config: :class:`pyramid.config.Configurator`
+    """
+
+    config.add_translation_dirs('kotti_docit:locale')
+    config.add_static_view('static-kotti_docit', 'kotti_docit:static')
+
+    config.scan(__name__)
---
+++ packages/kotti_docit/kotti_docit-1.0.0.tar.gz/kotti_docit-1.0.0/setup.py
@@ -0,0 +1,60 @@
+import os
+
+from setuptools import setup
+from setuptools import find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+try:
+    README = open(os.path.join(here, 'README.rst')).read()
+except IOError:
+    README = ''
+try:
+    CHANGES = open(os.path.join(here, 'CHANGES.rst')).read()
+except IOError:
+    CHANGES = ''
+
+version = '1.0.0'
+
+install_requires = [
+    'Kotti>=1.0.0',
+    'kotti_tinymce',
+    'kotti_pdf>=0.3.5',
+    'kotti_alert>=0.2.5',
+    'click>=6.6',
+    'kotti_survey',
+    'kotti_contenttypes>=0.1.0'
+]
+
+
+setup(
+    name='kotti_docit',
+    version=version,
+    description="Documentation site for your Kotti site",
+    long_description='\n\n'.join([README, CHANGES]),
+    classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.7",
+        "Framework :: Pylons",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
+        "Topic :: Internet :: WWW/HTTP :: WSGI :: Application",
+        "License :: Repoze Public License",
+    ],
+    author='Oshane Bailey',
+    author_email='b4.oshany@gmail.com',
+    url='https://github.com/b4oshany/kotti_docit',
+    keywords='kotti web cms wcms pylons pyramid sqlalchemy bootstrap',
+    license="BSD-derived (http://www.repoze.org/LICENSE.txt)",
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=install_requires,
+    tests_require=[],
+    dependency_links=[],
+    entry_points={
+        'fanstatic.libraries': [
+            'kotti_docit = kotti_docit.fanstatic:library',
+        ],
+    },
+    extras_require={},
+)
