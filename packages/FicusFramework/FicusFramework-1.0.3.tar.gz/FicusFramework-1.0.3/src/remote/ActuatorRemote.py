485d5bab483e4e9a73a428d96753fa7506a5c8bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:33:59 +0000

    Add package FicusFramework/1.0.3/FicusFramework-1.0.3.tar.gz

---
+++ packages/FicusFramework/FicusFramework-1.0.3.tar.gz/FicusFramework-1.0.3/src/remote/ActuatorRemote.py
@@ -0,0 +1,14 @@
+from flask import jsonify
+
+from . import remote
+# from app.remote import remote
+
+
+@remote.route('/actuator/info', methods=['GET'])
+def actuator_info():
+    return jsonify({})
+
+
+@remote.route('/actuator/health', methods=['GET'])
+def actuator_health():
+    return jsonify({"status": "UP"})
