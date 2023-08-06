6b6d3a085f1f20402f611ff0073cbc47d30b5a54
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:08 +0000

    Add package psyshort/0.0.3/psyshort-0.0.3.tar.gz

---
+++ packages/psyshort/psyshort-0.0.3.tar.gz/psyshort-0.0.3/README.md
@@ -0,0 +1,7 @@
+Making psycopg2 a little simpler to use.
+
+Usage:
+```python
+from psyshort import Psyshort
+psy = Psyshort(my_server_address, my_database, my_username, my_password)
+```
---
+++ packages/psyshort/psyshort-0.0.3.tar.gz/psyshort-0.0.3/setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+with open("README.md", "rb") as f:
+    long_description = f.read().decode("utf-8")
+    
+setuptools.setup(
+    name="psyshort",
+    version="0.0.3",
+    author="Quit3Simpl3",
+    long_description=long_description,
+    url="https://github.com/Quit3Simpl3/psyshort",
+    packages=setuptools.find_packages(),
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        )
+    )
