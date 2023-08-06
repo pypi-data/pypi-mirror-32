0560a8bc7a2b616a3c328d0d8bd5ac4f4649b690
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:25:58 +0000

    Add package django-timeline-logger/1.1.2/django-timeline-logger-1.1.2.tar.gz

---
+++ packages/django-timeline-logger/django-timeline-logger-1.1.2.tar.gz/django-timeline-logger-1.1.2/timeline_logger/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+
+from .models import TimelineLog
+
+
+@admin.register(TimelineLog)
+class TimelineLogAdmin(admin.ModelAdmin):
+    list_display = ['__str__', 'timestamp', 'template']
+    list_filter = ['timestamp', 'content_type']
+    list_select_related = ['content_type']
---
+++ packages/django-timeline-logger/django-timeline-logger-1.1.2.tar.gz/django-timeline-logger-1.1.2/timeline_logger/conf.py
@@ -0,0 +1,31 @@
+from __future__ import unicode_literals
+
+from appconf import AppConf
+from django.conf import settings  # noqa
+from django.utils.translation import ugettext_lazy as _
+
+
+class TimelineLoggerConf(AppConf):
+
+    DEFAULT_TEMPLATE = 'timeline_logger/default.txt'
+
+    PAGINATE_BY = 25
+
+    USER_EMAIL_FIELD = 'email'
+
+    DIGEST_EMAIL_RECIPIENTS = None
+    DIGEST_EMAIL_SUBJECT = _('Events timeline')
+    DIGEST_FROM_EMAIL = None
+
+    class Meta:
+        prefix = 'timeline'
+
+    def configure_digest_email_recipients(self, value):
+        if value is None:
+            return []
+        return value
+
+    def configure_digest_from_email(self, value):
+        if value is None:
+            return settings.DEFAULT_FROM_EMAIL
+        return value
