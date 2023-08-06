42d0859659d1025b2d362e811a77c2421c5bd9e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:13:23 +0000

    Add package MyNQL/0.1/MyNQL-0.1.tar.gz

---
+++ packages/MyNQL/MyNQL-0.1.tar.gz/MyNQL-0.1/MyNQL/utils.py
@@ -0,0 +1,28 @@
+import networkx as nx
+
+fake_db = {}
+# replace this with your favourite database
+# should be a key value store, the key may be converted to/from tuple and the data-field should be text
+def fake_db_serializer(action, key, text):
+    global fake_db
+    # the key is a tuple and can consist of integer or strings
+    # print (action, key, text)
+    if action == "INSERT":
+        fake_db[key] = text
+    if action == "UPDATE":
+        fake_db[key] = text
+    if action == "DELETE":
+        del fake_db[key]
+
+
+# node-link like in the d3.js example http://bl.ocks.org/mbostock/4062045
+# use this function to save/export the network as a .json file that can be embedded as a nice html
+def save_node_link_data(G, file):
+    import json
+    open(file, "w").write(json.dumps(nx.node_link_data(G)))
+
+
+
+
+
+
