6a8aa289cf48af1af72f2c4eaa205fdf27cbb98a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:00:54 +0000

    Add package pynmr/0.0/pynmr-0.0.tar.gz

---
+++ packages/pynmr/pynmr-0.0.tar.gz/pynmr-0.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='pynmr',
+    version='0.0',
+    author='Daniel Cosmo Pizetta',
+    author_email='daniel.pizetta@usp.br',
+    packages=['pynmr'],
+    url='http://pypi.python.org/pypi/pynmr/',
+    description='PyNMR Framework',
+    long_description='PyNMR: To hold this name, because we lost two of them ultim now :)',
+)
