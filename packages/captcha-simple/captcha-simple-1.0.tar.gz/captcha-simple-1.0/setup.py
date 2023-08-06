4bd4e380df3b33731ac7bffea9fe587a1cbd8ce7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:00:43 +0000

    Add package captcha-simple/1.0/captcha-simple-1.0.tar.gz

---
+++ packages/captcha-simple/captcha-simple-1.0.tar.gz/captcha-simple-1.0/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+
+install_requires = [
+    'setuptools',
+    'Django >= 2.0.2',
+    'Pillow >= 2.2.2'
+]
+
+setup(
+    name="captcha-simple",
+    version='1.0',
+    url='https://github.com/varisbhalala/captcha-simple.git',
+    install_requires='install_requires',
+    licence='MIT'
+)
