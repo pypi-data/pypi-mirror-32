b7ea3928e349fff7d5c7d2b04d3e4bf489280568
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:55:46 +0000

    Add package infi.greeter/0.0.10/infi.greeter-0.0.10-py2-none-any.whl

---
+++ packages/infi.greeter/infi.greeter-0.0.10-py2-none-any.whl/infi/greeter/__version__.py
@@ -0,0 +1,11 @@
+__version__ = "0.0.10"
+__git_commiter_name__ = "Arnon Yaari"
+__git_commiter_email__ = "arnony@infinidat.com"
+__git_branch__ = u'(HEAD detached at v0.0.10)'
+__git_remote_tracking_branch__ = '(No remote tracking)'
+__git_remote_url__ = '(Not remote tracking)'
+__git_head_hash__ = '43e1b0d2936bfe194784361e09703c1ba619b891'
+__git_head_subject__ = u'Finished Release v0.0.10'
+__git_head_message__ = u''
+__git_dirty_diff__ = u''
+__git_commit_date__ = '2018-06-10 11:56:10'
