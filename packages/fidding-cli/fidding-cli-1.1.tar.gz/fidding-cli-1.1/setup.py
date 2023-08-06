bd81cb6df5927b9fda0454908023ee7812859480
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:14:48 +0000

    Add package fidding-cli/1.1/fidding-cli-1.1.tar.gz

---
+++ packages/fidding-cli/fidding-cli-1.1.tar.gz/fidding-cli-1.1/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='fidding-cli',
+    version='1.1',
+    description='A python cli project',
+    url='https://github.com/fidding/python-cli.git',
+    packages=find_packages(),
+    author='fidding',
+    author_email='395455856@qq.com',
+    license='MIT',
+    include_package_data=True,
+    install_requires=[
+        'Click',
+    ],
+    # package_dir={'': 'lib'},
+    # data_files=[],
+    entry_points='''
+        [console_scripts]
+        fidding-cli=python_cli.script.cli:cli
+    ''',
+)
