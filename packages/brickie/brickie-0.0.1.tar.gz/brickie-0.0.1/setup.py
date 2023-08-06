3dcd70c10ed6cd60af5a3f775ed29dff5d299110
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:12:59 +0000

    Add package brickie/0.0.1/brickie-0.0.1.tar.gz

---
+++ packages/brickie/brickie-0.0.1.tar.gz/brickie-0.0.1/setup.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+
+setup(
+    name='brickie',
+    version='0.0.1',
+    packages=['brickie'],
+    url='',
+    license='MIT',
+    description='Brickie',
+    install_requires=[
+    ],
+    extras_require={
+    },
+)
