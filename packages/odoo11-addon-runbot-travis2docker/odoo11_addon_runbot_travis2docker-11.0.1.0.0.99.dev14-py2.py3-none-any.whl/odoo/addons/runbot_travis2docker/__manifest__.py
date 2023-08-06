097b0475a46fe4d66674cec97b21ebb20ee2d763
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:10 +0000

    Add package odoo11-addon-runbot-travis2docker/11.0.1.0.0.99.dev14/odoo11_addon_runbot_travis2docker-11.0.1.0.0.99.dev14-py2.py3-none-any.whl

---
+++ packages/odoo11-addon-runbot-travis2docker/odoo11_addon_runbot_travis2docker-11.0.1.0.0.99.dev14-py2.py3-none-any.whl/odoo/addons/runbot_travis2docker/__manifest__.py
@@ -0,0 +1,35 @@
+# Copyright <2015> <Vauxoo info@vauxoo.com>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Runbot travis to docker",
+    "summary": "Generate docker with odoo instance based on .travis.yml",
+    "version": "11.0.1.0.0",
+    "category": "runbot",
+    "website": "https://github.com/OCA/runbot-addons",
+    "author": "Vauxoo,Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "depends": [
+        "runbot",
+    ],
+    "external_dependencies": {
+        "python": [
+            "travis2docker",
+        ],
+        "bin": [
+            "docker",
+        ],
+    },
+    "data": [
+        "views/runbot_repo_view.xml",
+        "views/runbot_build_view.xml",
+        "views/runbot_branch_view.xml",
+        "data/ir_cron_data.xml",
+        "templates/build.xml",
+    ],
+    "demo": [
+        "demo/runbot_repo_demo.xml",
+    ],
+    "application": False,
+    "installable": True,
+}
