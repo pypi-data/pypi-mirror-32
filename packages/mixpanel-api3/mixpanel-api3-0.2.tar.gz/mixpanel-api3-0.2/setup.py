eb46b93c0312cceb279317f34ad52d804c65c2d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:24:16 +0000

    Add package mixpanel-api3/0.2/mixpanel-api3-0.2.tar.gz

---
+++ packages/mixpanel-api3/mixpanel-api3-0.2.tar.gz/mixpanel-api3-0.2/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+from distutils.core import setup
+setup(
+  name='mixpanel-api3',
+  packages=['mixpanel_api'],
+  version='0.2',
+  description='Python3 Version of mixpanel-api. A utilities-only module for exporting and importing data into Mixpanel via their APIs. This is NOT for server-side app tracking.',
+  long_description='Python3 Version of mixpanel-api. A utilities-only module for exporting and importing data into Mixpanel via their APIs. This is NOT for server-side app tracking.',
+  author='Avinash Anand',
+  author_email='asy8uo81p3g4@abinemail.com',
+  url='https://github.com/aavnsh/mixpanel_api3',
+  keywords=['mixpanel', 'utilities', 'python3'],
+  classifiers=[
+      # Trove classifiers
+      # Full list: https://pypi.python.org/pypi?%3Aaction=list_classifiers
+      'Development Status :: 4 - Beta',
+      'Intended Audience :: Developers',
+      'Programming Language :: Python :: 3',
+      'Programming Language :: Python :: 3.6',
+      'Topic :: Software Development :: Libraries :: Python Modules',
+      'Topic :: Utilities'
+  ],
+)
