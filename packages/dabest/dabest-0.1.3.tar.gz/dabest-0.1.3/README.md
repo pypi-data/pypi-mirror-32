0140aa9daabeffcca08ebd9fac7281f87a2f5fb8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:34:08 +0000

    Add package dabest/0.1.3/dabest-0.1.3.tar.gz

---
+++ packages/dabest/dabest-0.1.3.tar.gz/dabest-0.1.3/README.md
@@ -0,0 +1,51 @@
+# DABEST (Python)
+[![Travis CI](https://travis-ci.org/ACCLAB/DABEST-python.svg?branch=master)](https://travis-ci.org/ACCLAB/DABEST-python)
+[![PyPI](https://img.shields.io/pypi/v/dabest.svg)](https://pypi.python.org/pypi/dabest/0.1.2)
+
+## About
+
+DABEST is a package for **D**ata **A**nalysis using **B**ootstrapped **EST**imation.
+
+![Gardner-Altman plot](https://acclab.github.io/DABEST-python-docs/_images/f1.png)
+
+## Requirements
+
+Python 3.6 is strongly recommended, although this has been tested with Python 2.7 and Python 3.5.
+
+In addition, the following packages are also required:
+- [numpy](https://www.numpy.org/) (1.13.x)
+- [scipy](https://www.scipy.org/) (1.0.x)
+- [matplotlib](https://www.matplotlib.org/) (2.0.x)
+- [seaborn](https://seaborn.pydata.org/) (0.8.x)
+- [pandas](https://pandas.pydata.org/) (0.23.x).
+
+To obtain these package dependencies easily, it is highly recommended to download the [Anaconda distribution](https://www.continuum.io/downloads) of Python.
+
+## Installation
+
+You can install this package via `pip`.
+
+To install, at the command line run
+<!-- ```shell
+conda config --add channels conda-forge
+conda install dabest
+```
+or -->
+```shell
+pip install --upgrade dabest
+```
+You can also clone this repo locally (see intstructions [here](https://help.github.com/articles/cloning-a-repository/)).
+
+Then, navigate to the cloned repo in the command line and run
+
+```shell
+pip install .
+```
+
+## Usage
+
+Please refer to the [documentation](https://acclab.github.io/DABEST-python-docs/index.html).
+
+## Matlab version
+
+There is also a [Matlab version](https://github.com/ACCLAB/DABEST-Matlab) of DABEST.
---
+++ packages/dabest/dabest-0.1.3.tar.gz/dabest-0.1.3/setup.py
@@ -0,0 +1,60 @@
+from setuptools import setup, find_packages
+import os
+# Taken from setup.py in seaborn.
+# temporarily redirect config directory to prevent matplotlib importing
+# testing that for writeable directory which results in sandbox error in
+# certain easy_install versions
+os.environ["MPLCONFIGDIR"]="."
+
+# Modified from from setup.py in seaborn.
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+def check_dependencies():
+    to_install=[]
+
+    try:
+        import numpy
+        if int(numpy.__version__.split('.')[1])<=12:
+            to_install.append('numpy==1.13')
+    except ImportError:
+        to_install.append('numpy==1.13')
+
+    try:
+        import scipy
+        if int(scipy.__version__.split('.')[0])==0:
+            to_install.append('scipy==1.0')
+    except ImportError:
+        to_install.append('scipy==1.0')
+
+    try:
+        import pandas
+        if int(pandas.__version__.split('.')[1])<=23:
+            to_install.append('pandas==0.23')
+    except ImportError:
+        to_install.append('pandas==0.23')
+
+    try:
+        import seaborn
+        if int(seaborn.__version__.split('.')[1])<=7:
+            to_install.append('seaborn==0.8')
+    except ImportError:
+        to_install.append('seaborn==0.8')
+
+    return to_install
+
+if __name__=="__main__":
+
+    installs=check_dependencies()
+    setup(name='dabest',
+    author='Joses W. Ho',
+    author_email='joseshowh@gmail.com',
+    version='0.1.3',
+    description='Data Analysis and Visualization using Bootstrapped Estimation.',
+    packages=find_packages(),
+    install_requires=installs,
+    url='https://acclab.github.io/DABEST-python-docs/index.html',
+    license='BSD 3-clause Clear License'
+    )
