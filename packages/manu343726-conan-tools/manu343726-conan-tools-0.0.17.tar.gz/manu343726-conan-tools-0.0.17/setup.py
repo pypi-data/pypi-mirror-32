faae699c6a2d9d1bc28d18f0c8376842437f3a0d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:36 +0000

    Add package manu343726-conan-tools/0.0.17/manu343726-conan-tools-0.0.17.tar.gz

---
+++ packages/manu343726-conan-tools/manu343726-conan-tools-0.0.17.tar.gz/manu343726-conan-tools-0.0.17/setup.py
@@ -0,0 +1,33 @@
+from distutils.core import setup
+import os
+
+VERSION=os.environ.get('CONAN_TOOLS_VERSION', '0.0.17')
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+  name = 'manu343726-conan-tools',
+  packages = ['manu343726_conan_tools'],
+  version = VERSION,
+  description = 'Miscellaneous tools for development of conan.io recipes',
+  long_description = long_description,
+  author = 'Manu Sanchez',
+  author_email = 'Manu343726@gmail.com',
+  url = 'https://gitlab.com/Manu343726/conan-tools',
+  download_url = 'https://gitlab.com/Manu343726/conan-tools/-/archive/v{version}/conan-tools-v{version}.tar.gz'.format(version=VERSION),
+  keywords = ['conan'],
+  entry_points={
+    'console_scripts': [
+        'manu343726-conan-tools=manu343726_conan_tools.main:main'
+    ]
+  },
+  classifiers=[
+    'Development Status :: 1 - Planning',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.6'
+  ],
+)
