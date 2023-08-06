c89546000f3f057e6d15110732d89c38534392bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:12 +0000

    Add package pyobjc-framework-iTunesLibrary/4.2.2/pyobjc-framework-iTunesLibrary-4.2.2.tar.gz

---
+++ packages/pyobjc-framework-iTunesLibrary/pyobjc-framework-iTunesLibrary-4.2.2.tar.gz/pyobjc-framework-iTunesLibrary-4.2.2/PyObjCTest/test_itlibrary.py
@@ -0,0 +1,25 @@
+from PyObjCTools.TestSupport import *
+
+import iTunesLibrary
+import objc
+
+class TestITLibrary (TestCase):
+    def test_classes(self):
+        self.assertIsInstance(iTunesLibrary.ITLibrary, objc.objc_class)
+
+    def testMethods(self):
+        self.assertResultIsBOOL(iTunesLibrary.ITLibrary.shouldShowContentRating)
+
+        self.assertArgIsOut(iTunesLibrary.ITLibrary.libraryWithAPIVersion_error_, 1)
+        self.assertArgIsOut(iTunesLibrary.ITLibrary.initWithAPIVersion_error_, 1)
+
+    @min_os_level('10.7')
+    def testMethods10_7(self):
+        self.assertResultIsBOOL(iTunesLibrary.ITLibrary.reloadData)
+
+    def testConstants(self):
+        self.assertEqual(iTunesLibrary.ITLibExportFeatureNone, 0)
+
+
+if __name__ == "__main__":
+    main()
