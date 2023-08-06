f1285b80ef5569188c445e027fb879bc67266907
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:18 +0000

    Add package Flask-Minify/0.4/Flask-Minify-0.4.tar.gz

---
+++ packages/Flask-Minify/Flask-Minify-0.4.tar.gz/Flask-Minify-0.4/setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup
+with open('README.md') as readme:
+    ld = readme.read()
+
+
+setup(
+    name='Flask-Minify',
+    version='0.4',
+    url='https://github.com/mrf345/flask_minify/',
+    download_url='https://github.com/mrf345/flask_minify/archive/0.4.tar.gz',
+    license='MIT',
+    author='Mohamed Feddad',
+    author_email='mrf345@gmail.com',
+    description='flask extension to minify html, css, js and less',
+    long_description=ld,
+    py_modules=['minify'],
+    packages=['flask_minify'],
+    zip_safe=False,
+    include_package_data=True,
+    platforms='any',
+    install_requires=[
+        'Flask',
+        'htmlmin',
+        'jsmin',
+        'lesscpy'
+    ],
+    keywords=['flask', 'extension', 'minifer', 'htmlmin', 'lesscpy',
+              'jsmin', 'html', 'js', 'less', 'css'],
+    classifiers=[
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
+)
