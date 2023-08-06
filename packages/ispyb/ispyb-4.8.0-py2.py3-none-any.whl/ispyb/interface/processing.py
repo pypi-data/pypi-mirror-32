ebe055927cb28f4487f3c7d8d6d0ceb8b1d873c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:37:51 +0000

    Add package ispyb/4.8.0/ispyb-4.8.0-py2.py3-none-any.whl

---
+++ packages/ispyb/ispyb-4.8.0-py2.py3-none-any.whl/ispyb/interface/processing.py
@@ -0,0 +1,28 @@
+from __future__ import absolute_import, division, print_function
+
+import abc
+
+from ispyb.interface.dataarea import DataArea
+import ispyb.model.processingjob
+
+class IF(DataArea):
+
+  @abc.abstractmethod
+  def get_program_params(self):
+      pass
+
+  @abc.abstractmethod
+  def get_program_attachment_params(self):
+      pass
+
+  @abc.abstractmethod
+  def get_processing_params(self):
+      pass
+
+  @abc.abstractmethod
+  def get_quality_indicators_params(self):
+      pass
+
+  def get_processing_job(self, jobid):
+    '''Return a ProcessingJob object representing the information about the selected processing job'''
+    return ispyb.model.processingjob.ProcessingJob(jobid, self)
