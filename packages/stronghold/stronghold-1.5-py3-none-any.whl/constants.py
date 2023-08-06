860e23f6db28067be44f81e400c35f71a4719ab2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:27:03 +0000

    Add package stronghold/1.5/stronghold-1.5-py3-none-any.whl

---
+++ packages/stronghold/stronghold-1.5-py3-none-any.whl/constants.py
@@ -0,0 +1,8 @@
+class Constants:
+	PROJECT_NAME = 'stronghold'
+	VERSION = '1.5'
+	AUTHOR_GITHUB = 'alichtman'
+	AUTHOR_FULL_NAME = 'Aaron Lichtman'
+	DESCRIPTION = "Securely configure your Mac from the terminal."
+	URL = 'https://github.com/alichtman/stronghold',
+	AUTHOR_EMAIL = 'aaronlichtman@gmail.com',
