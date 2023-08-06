41a17dcfae66637a9aa42c7f13c3e58000558094
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:13:14 +0000

    Add package raw2clean/1.0.3/raw2clean-1.0.3.tar.gz

---
+++ packages/raw2clean/raw2clean-1.0.3.tar.gz/raw2clean-1.0.3/setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+from __future__ import print_function  
+from setuptools import setup, find_packages  
+import sys 
+
+setup(
+    name = "raw2clean",
+    version = "1.0.3",
+    packages = ['raw2clean'],
+    author="Yong Deng",
+    author_email = "yodeng@tju.edu.cn",
+    description = "QC for rawdata without adapter",
+    url="https://github.com/yodeng/raw2clean",
+    license="MIT",
+    entry_points = {
+        'console_scripts': [  
+            'raw2clean = raw2clean.raw2clean:main',
+            'sumfq = raw2clean.summary_fastq:main'
+        ]
+    }
+
+)
+
