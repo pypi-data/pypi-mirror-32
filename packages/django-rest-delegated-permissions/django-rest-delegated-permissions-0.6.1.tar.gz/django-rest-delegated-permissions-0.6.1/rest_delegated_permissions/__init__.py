250868d05867ce91edb520206df555d966cdee74
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:14:46 +0000

    Add package django-rest-delegated-permissions/0.6.1/django-rest-delegated-permissions-0.6.1.tar.gz

---
+++ packages/django-rest-delegated-permissions/django-rest-delegated-permissions-0.6.1.tar.gz/django-rest-delegated-permissions-0.6.1/rest_delegated_permissions/__init__.py
@@ -0,0 +1,4 @@
+from .permissions import RestPermissions, DelegatedPermission, BasePermission
+from . import permissions
+
+__all__ = ('permissions', 'BasePermission', 'RestPermissions', 'DelegatedPermission')
