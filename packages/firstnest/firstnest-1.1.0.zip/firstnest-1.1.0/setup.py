a87810a16e36b2c21bd77b84b3705fc1366be0ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:00:39 +0000

    Add package firstnest/1.1.0/firstnest-1.1.0.zip

---
+++ packages/firstnest/firstnest-1.1.0.zip/firstnest-1.1.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name      = 'firstnest',
+    version   = '1.1.0',
+    py_modules= ['firstnest'],
+    author    = 'sthhhh',
+    url       = 'http://www.headfirstlabs.com',
+    description= 'A simple printer of nested lists',
+
+
+    )
